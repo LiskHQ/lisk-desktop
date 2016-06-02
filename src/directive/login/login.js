553f025b827037c843a9486d24b516cd62d42588
rferro <ricardo.ferro@gmail.com>
2016-06-02 02:51:55 -0300

    Login directive improvements and random creation

--- src/directive/login/login.js
+++ src/directive/login/login.js
@@ -1,34 +1,145 @@
 
 import './login.less'
 
+import crypto from 'crypto'
 import mnemonic from 'bitcore-mnemonic'
 
 export default (app) => {
-  let fix = v => v.replace(/ +/g, ' ').trim().toLowerCase()
+  let fix = v => {
+    return (v || '').replace(/ +/g, ' ').trim().toLowerCase()
+  }
 
-  app.directive('login', () => {
+  let lpad = (str, pad, length) => {
+    while (str.length < length) str = pad + str
+    return str
+  }
+
+  let rpad = (str, pad, length) => {
+    while (str.length < length) str = str + pad
+    return str
+  }
+
+  app.directive('login', ($timeout) => {
     return {
       restrict: 'E',
       template: require('./login.pug'),
       scope: {},
       link (scope, elem, attrs) {
-        scope.validPassphrase = (ev) => {
-          let value = fix(scope.passphrase)
+        scope.focus = () => {
+          elem.find('input').focus()
+        }
+      },
+      controller ($scope, $document, $timeout) {
+        $scope.passphrase = {
+          isValid (value) {
+            value = fix(value)
 
-          if (value.split(' ').length !== 12 || !mnemonic.isValid(value)) {
-            scope.valid = false
-          }
-          else {
-            scope.valid = true
-
-            if (ev.keyCode === 13) {
-              // ok
+            if (value === '') {
+              $scope.passphrase.valid = 2
+            } else if (value.split(' ').length !== 12 || !mnemonic.isValid(value)) {
+              $scope.passphrase.valid = 0
+            } else {
+              $scope.passphrase.valid = 1
             }
           }
         }
-      },
-      controller () {
 
+        $scope.$watch('passphrase.value', $scope.passphrase.isValid)
+
+        $scope.reset = () => {
+          $scope.passphrase.value = ''
+        }
+
+        $scope.random = {
+          empty () {
+            return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          },
+          reset () {
+            $scope.random.progress = 0
+            $scope.random.tmp = $scope.random.empty()
+          },
+          stop () {
+            $scope.random.started = false
+            $document.unbind('mousemove', $scope.random.listener)
+          },
+          start () {
+            $scope.random.started = true
+
+            $scope.reset()
+            $scope.random.reset()
+
+            let last = [0, 0]
+            let used = $scope.random.empty()
+
+            let turns = 10 + parseInt(Math.random() * 10)
+            let steps = 1
+            let total = turns * used.length
+            let count = 0
+
+            console.log('entropy', { turns, steps, total })
+
+            $scope.random.listener = (ev) => {
+              let distance = Math.sqrt(Math.pow(ev.pageX - last[0], 2) + Math.pow(ev.pageY - last[1], 2))
+
+              if (distance > 60) {
+                for (let p = 0; p < steps; p++) {
+                  let pos
+                  let available = []
+
+                  for (let i in used) {
+                    if (!used[i]) {
+                      available.push(i)
+                    }
+                  }
+
+                  if (!available.length) {
+                    used = used.map(v => 0)
+                    pos = parseInt(Math.random() * used.length)
+                  } else {
+                    pos = available[parseInt(Math.random() * available.length)]
+                  }
+
+                  count++
+
+                  last = [ev.pageX, ev.pageY]
+                  used[pos] = 1
+
+                  $scope.$apply(() => {
+                    $scope.random.tmp[pos] = crypto.randomBytes(1)[0]
+                    $scope.random.progress = parseInt(count / total * 100)
+                  })
+
+                  if (count >= total) {
+                    $timeout(() => {
+                      let hex = $scope.random.tmp.map(v => lpad(v.toString(16), '0', 2)).join('')
+                      $scope.passphrase.value = (new mnemonic(new Buffer(hex, 'hex'))).toString()
+
+                      $scope.random.stop()
+                    })
+
+                    return
+                  }
+                }
+              }
+            }
+
+            $timeout(() => $document.mousemove($scope.random.listener), 300)
+          }
+        }
+      }
+    }
+  })
+
+  app.directive('byte', () => {
+    return {
+      restrict: 'E',
+      template: require('./byte.pug'),
+      scope: { data: '=ngData' },
+      link (scope, elem, attrs) {
+        scope.$watch('data', (nv) => {
+          scope.dec = lpad(nv.toString(), '0', 3)
+          scope.hex = lpad(nv.toString(16), '0', 2)
+        })
       }
     }
   })
