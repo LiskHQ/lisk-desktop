9817d06f4f1d73bae07339e8cbef0a2979c94feb
rferro <ricardo.ferro@gmail.com>
2016-06-04 15:24:58 -0300

    New webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,15 @@
 
+import webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import ngAnnotatePlugin from 'ng-annotate-webpack-plugin'
 
 export default {
+  entry: "./src/index.js",
+  output: {
+    path: './dist',
+    filename: "app.[hash].js"
+  },
   module: {
     loaders: [
       {
@@ -12,7 +19,7 @@ export default {
       },
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract("style", "css!less"),
+        loader: 'style!css!less',
       },
       {
         test: /\.pug$/,
@@ -32,10 +39,17 @@ export default {
     angular: 'angular',
   },
   plugins: [
-    new ExtractTextPlugin("app.css"),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/view/index.pug',
+      minify: {
+        minifyCSS: true,
+      }
+    }),
+    new ngAnnotatePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      comments: false,
     })
   ]
 }
