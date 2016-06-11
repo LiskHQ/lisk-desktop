b1175383e1ddf85c382fbbb2821098768886ac30
rferro <ricardo.ferro@gmail.com>
2016-06-11 04:54:37 -0300

    LSK filter improvements

--- src/filters/lsk.js
+++ src/filters/lsk.js
@@ -5,6 +5,6 @@ import app from '../app'
 
 app.filter('lsk', () => {
   return (value) => {
-    return numeral(parseInt(value) || 0).divide(Math.pow(10, 8)).format('0[.]00000000') + ' LSK'
+    return numeral(parseInt(value) || 0).divide(Math.pow(10, 8)).format('0.00000000') + ' LSK'
   }
 })
