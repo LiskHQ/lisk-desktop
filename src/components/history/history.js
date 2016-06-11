41e7ac1c3b8a0fa5be9c688ec8cb683911baa2f0
rferro <ricardo.ferro@gmail.com>
2016-06-11 05:48:06 -0300

    New history component

--- src/components/history/history.js
+++ src/components/history/history.js
@@ -3,7 +3,9 @@ import './history.less'
 
 import app from '../../app'
 
-app.directive('history', () => {
+const UPDATE_INTERVAL = 20000
+
+app.directive('history', ($timeout) => {
   return {
     restrict: 'E',
     template: require('./history.jade'),
@@ -12,12 +14,29 @@ app.directive('history', () => {
 
       scope.$on('login', () => {
         elem.show()
+        scope.updateHistory()
       })
 
       scope.$on('logout', () => {
         elem.hide()
+        scope.history = []
       })
     },
-    controller: ($scope) => {}
+    controller: ($scope) => {
+      $scope.updateHistory = () => {
+        return $scope.peer.getHistory($scope.address)
+          .then(res => {
+            $scope.history = res
+
+            if ($scope.prelogged || $scope.logged) {
+              $scope.timeouts.history = $timeout($scope.updateHistory, UPDATE_INTERVAL)
+            }
+          })
+          .catch(() => {
+            $scope.$emit('error')
+            return $q.reject()
+          })
+      }
+    }
   }
 })
