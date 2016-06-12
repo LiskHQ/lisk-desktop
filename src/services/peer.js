f104040bddd53d3779980bfcc8b3bb783eb3c3d3
rferro <ricardo.ferro@gmail.com>
2016-06-11 21:44:56 -0300

    Create getNetHash and sendTransaction methods in peer directive

--- src/services/peer.js
+++ src/services/peer.js
@@ -1,9 +1,13 @@
 
 import app from '../app'
 
+import lisk from 'lisk-js'
+
 const API_PEERS = '/api/peers'
 const API_BALANCE = '/api/accounts/getBalance'
 const API_HISTORY = '/api/transactions'
+const API_NETHASH = '/api/blocks/getNetHash'
+const API_TRANSACTION = '/peer/transactions'
 
 const REQUEST_TIMEOUT = 5000
 
@@ -100,6 +104,36 @@ app.factory('peer', ($http, $log, $q) => {
         .then(res => {
           $log.info('getHistory %s: %s', address, res.length)
           return res
+        })
+    }
+
+    getNetHash () {
+      return this.get(API_NETHASH)
+        .then(res => res.data.nethash)
+        .then(res => {
+          $log.info('getNetHash: %s', res)
+          return res
+        })
+    }
+
+    sendTransaction (passphrase, recipient, amount) {
+      let transaction = lisk.transaction.createTransaction(
+        recipient,
+        parseInt(amount * Math.pow(10, 8)),
+        passphrase
+      )
+
+      return this.getNetHash()
+        .then(nethash => {
+          let headers = {
+            'Content-Type': 'application/json',
+            os: 'nanowallet',
+            version: '0.0.0',
+            port: 8000,
+            nethash,
+          }
+
+          return this.post(API_TRANSACTION, { transaction }, headers)
         })
     }
   }
