ed685e4f078fcaa1db23c7eee023242c3d64e23e
rferro <ricardo.ferro@gmail.com>
2016-06-12 17:57:17 -0300

    Peer service improvements and final version of sendTransaction

--- src/services/peer.js
+++ src/services/peer.js
@@ -11,6 +11,10 @@ const API_TRANSACTION = '/peer/transactions'
 
 const REQUEST_TIMEOUT = 5000
 
+const TRANSACTION_HEADER_OS = 'nanowallet'
+const TRANSACTION_HEADER_PORT = '8000'
+const TRANSACTION_HEADER_VERSION = '0.0.0'
+
 app.factory('peer', ($http, $log, $q) => {
   return class peer {
     constructor ({ host, port, ssl = false }) {
@@ -27,67 +31,53 @@ app.factory('peer', ($http, $log, $q) => {
       return (this.ssl ? 'https' : 'http') + `://${this.uri}`
     }
 
-    request (method, api, params, data, headers) {
+    request (method, api, data, headers) {
       let config = {
         url: this.url + api,
         method,
-        params,
-        data,
         headers,
         timeout: REQUEST_TIMEOUT,
       }
 
-      return $http(config)
-        .then(
-          (res) => {
-            let success = !!res.data.success
+      if (method === 'post') {
+        config.data = data
+      } else {
+        config.params = data
+      }
 
-            if (!success) {
-              return $q.reject()
-            }
-
-            $log.debug('%s %s %s%s', method.toUpperCase(), res.status, this.url, api)
-            return res
-          },
-          (res) => {
-            $log.error('%s %s %s%s', method.toUpperCase(), res.status, this.url, api)
-            return $q.reject()
-          }
-        )
+      return $http(config).then(res => {
+        if (!res.data.success) {
+          return $q.reject({ message: res.data.message || null })
+        } else {
+          return res
+        }
+      })
     }
 
     get (api, data, headers) {
-      return this.request('GET', api, data, null, headers)
+      return this.request('get', api, data, headers)
     }
 
     post (api, data, headers) {
-      return this.request('POST', api, null, data, headers)
+      return this.request('post', api, data, headers)
     }
 
     getPeers () {
       let data = {
-        state: 2
+        state: 2,
       }
 
       return this.get(API_PEERS, data)
         .then(res => res.data.peers)
-        .then(res => {
-          $log.info('getPeers: %s', res.length)
-          return res
-        })
     }
 
     getBalance (address) {
       let data = {
-        address
+        address,
       }
 
       return this.get(API_BALANCE, data)
         .then(res => res.data.balance)
-        .then(res => {
-          $log.info('getBalance %s: %s', address, res)
-          return res
-        })
     }
 
     getHistory (address, limit = 10, orderBy = 't_timestamp:desc') {
@@ -100,36 +90,36 @@ app.factory('peer', ($http, $log, $q) => {
 
       return this.get(API_HISTORY, data)
         .then(res => res.data.transactions)
-        .then(res => {
-          $log.info('getHistory %s: %s', address, res.length)
-          return res
-        })
     }
 
     getNetHash () {
       return this.get(API_NETHASH)
         .then(res => res.data.nethash)
-        .then(res => {
-          $log.info('getNetHash: %s', res)
-          return res
-        })
     }
 
-    sendTransaction (passphrase, recipient, amount) {
-      let transaction = lisk.transaction.createTransaction(
-        recipient,
-        parseInt(amount * Math.pow(10, 8)),
-        passphrase
-      )
+    sendTransaction (passphrase, secondPassphrase, recipient, amount) {
+      let transaction
+
+      amount = parseInt(amount * Math.pow(10, 8))
+
+      try {
+        transaction = lisk.transaction.createTransaction(
+          recipient,
+          amount,
+          passphrase,
+          secondPassphrase
+        )
+      } catch (e) {
+        return $q.reject()
+      }
 
       return this.getNetHash()
         .then(nethash => {
           let headers = {
-            'Content-Type': 'application/json',
-            os: 'nanowallet',
-            version: '0.0.0',
-            port: 8000,
             nethash,
+            os: TRANSACTION_HEADER_OS,
+            version: TRANSACTION_HEADER_VERSION,
+            port: TRANSACTION_HEADER_PORT,
           }
 
           return this.post(API_TRANSACTION, { transaction }, headers)
