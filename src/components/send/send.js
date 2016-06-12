75f97dcac511d1a9c9a16f4b482d0dabc972f3b6
rferro <ricardo.ferro@gmail.com>
2016-06-12 17:58:50 -0300

    New send component

--- src/components/send/send.js
+++ src/components/send/send.js
@@ -3,7 +3,10 @@ import './send.less'
 
 import app from '../../app'
 
-app.directive('send', () => {
+const ADDRESS_VALID_RE = '^[0-9]{1,21}[L|l]$'
+const AMOUNT_VALID_RE = '^[0-9]+(\.[0-9]{1,8})?$'
+
+app.directive('send', (error, success) => {
   return {
     restrict: 'E',
     template: require('./send.jade'),
@@ -11,6 +14,7 @@ app.directive('send', () => {
       elem.hide()
 
       scope.$on('login', () => {
+        scope.send.reset()
         elem.show()
       })
 
@@ -18,6 +22,45 @@ app.directive('send', () => {
         elem.hide()
       })
     },
-    controller: ($scope) => {}
+    controller: ($scope) => {
+      $scope.send = {
+        reset () {
+          $scope.send.recipient.value = '8127389478970013735L'
+          $scope.send.amount.value = parseFloat((1 + (Math.random() * 3)).toFixed(8))
+        },
+        go () {
+          $scope.send.loading = true
+
+          $scope.peer.sendTransaction(
+            $scope.passphrase,
+            $scope.secondPassphrase,
+            $scope.send.recipient.value,
+            $scope.send.amount.value
+          )
+          .then(
+            (res) => {
+              return success.dialog({ text: `${$scope.send.amount.value} sent to ${$scope.send.recipient.value}` })
+                .then(() => {
+                  $scope.send.reset()
+                  $scope.updateBalance()
+                  $scope.updateHistory()
+                })
+            },
+            (res) => {
+              error.dialog({ text: res && res.message ? res.message : 'An error occurred while sending the transaction.' })
+            }
+          )
+          .finally(() => {
+            $scope.send.loading = false
+          })
+        },
+        recipient: {
+          regexp: ADDRESS_VALID_RE,
+        },
+        amount: {
+          regexp: AMOUNT_VALID_RE,
+        }
+      }
+    }
   }
 })
