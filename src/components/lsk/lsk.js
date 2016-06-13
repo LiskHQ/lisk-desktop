14bf1687edc43f73a9fa621db14cf1a884eeacdb
rferro <ricardo.ferro@gmail.com>
2016-06-13 00:56:55 -0300

    Update lsk directive

--- src/components/lsk/lsk.js
+++ src/components/lsk/lsk.js
@@ -9,12 +9,14 @@ app.directive('lsk', () => {
   return {
     restrict: 'C',
     template: require('./lsk.jade'),
-    scope: { data: '=' },
+    scope: { data: '=', fee: '=?' },
     link (scope, elem, attrs) {},
     controller: ($scope) => {
       let fix = value => {
         return numeral(parseInt(value) || 0).divide(Math.pow(10, 8)).format('0.00000000')
       }
+
+      $scope.fee = $scope.fee ? fix($scope.fee) : null
 
       $scope.$watch('data', () => {
         $scope.amount = fix($scope.data)
