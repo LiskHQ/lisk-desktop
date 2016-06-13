a4db0d2c51b9ee046437d3bd580825e27364b9ad
rferro <ricardo.ferro@gmail.com>
2016-06-13 00:36:33 -0300

    Create address component

---
+++ src/components/address/address.js
@@ -0,0 +1,17 @@
+
+import './address.less'
+
+import app from '../../app'
+
+app.directive('address', () => {
+  return {
+    restrict: 'C',
+    template: require('./address.jade'),
+    scope: { data: '=', current: '=' },
+    link (scope, elem, attrs) {},
+    controller: ($scope) => {
+      $scope.$watch('data', () => {
+      })
+    }
+  }
+})
