1dda2935d78a428a0fd2c80c4017e89694decceb
rferro <ricardo.ferro@gmail.com>
2016-06-13 01:37:32 -0300

    Improve updating in timestamp directive

--- src/components/timestamp/timestamp.js
+++ src/components/timestamp/timestamp.js
@@ -11,16 +11,29 @@ app.directive('timestamp', () => {
     template: require('./timestamp.jade'),
     scope: { data: '=' },
     link (scope, elem, attrs) {},
-    controller: ($scope) => {
+    controller: ($scope, $timeout) => {
       let fixLiskTimestamp = (value) => {
         return new Date((((Date.UTC(2016, 4, 24, 17, 0, 0, 0) / 1000) + value) * 1000))
       }
 
-      $scope.$watch('data', () => {
-        let obj = moment(fixLiskTimestamp($scope.data))
+      let timeout
 
+      let update = () => {
+        $timeout.cancel(timeout)
+
+        let obj = moment(fixLiskTimestamp($scope.data))
         $scope.full = obj.format('llll')
         $scope.time_ago = obj.fromNow()
+
+        timeout = $timeout(update, 60000)
+      }
+
+      $scope.$watch('data', () => {
+        update()
+      })
+
+      $scope.$on('$destroy', () => {
+        $timeout.cancel(timeout)
       })
     }
   }
