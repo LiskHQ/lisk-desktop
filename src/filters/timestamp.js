1c087ebc8654a7a26e2e03c0180debac89c19d77
rferro <ricardo.ferro@gmail.com>
2016-06-13 00:01:27 -0300

    Beautifying code

--- src/filters/timestamp.js
+++ src/filters/timestamp.js
@@ -3,35 +3,35 @@ import app from '../app'
 
 app.filter('timestamp', (epochStampFilter) => {
   return (timestamp) => {
-    let d = epochStampFilter(timestamp);
-    let month = d.getMonth() + 1;
+    let d = epochStampFilter(timestamp)
+    let month = d.getMonth() + 1
 
     if (month < 10) {
-        month = '0' + month;
+      month = '0' + month
     }
 
-    let day = d.getDate();
+    let day = d.getDate()
 
     if (day < 10) {
-        day = '0' + day;
+      day = '0' + day
     }
 
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
+    let h = d.getHours()
+    let m = d.getMinutes()
+    let s = d.getSeconds()
 
     if (h < 10) {
-        h = '0' + h;
+      h = '0' + h
     }
 
     if (m < 10) {
-        m = '0' + m;
+      m = '0' + m
     }
 
     if (s < 10) {
-        s = '0' + s;
+      s = '0' + s
     }
 
-    return d.getFullYear() + '/' + month + '/' + day + ' ' + h + ':' + m + ':' + s;
+    return d.getFullYear() + '/' + month + '/' + day + ' ' + h + ':' + m + ':' + s
   }
 })
