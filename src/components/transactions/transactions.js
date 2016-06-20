c7cba07baed6ca80ebd6a681e25ca9241e7c904e
rferro <ricardo.ferro@gmail.com>
2016-06-19 22:47:25 -0300

    New transactions directive with "load more" button and isolated scope

--- src/components/transactions/transactions.js
+++ src/components/transactions/transactions.js
@@ -1,33 +1,53 @@
 
 import './transactions.less'
 
-import moment from 'moment'
-
 import app from '../../app'
 
-const UPDATE_INTERVAL = 5000
+const UPDATE_INTERVAL = 10000
 
 app.directive('transactions', ($timeout, $q) => {
   return {
     restrict: 'E',
     template: require('./transactions.jade'),
+    scope: { address: '=', peer: '=' },
     link (scope, elem, attrs) {},
-    controller: ($scope, timestampFilter) => {
-      $scope.updateTransactions = () => {
-        $timeout.cancel($scope.timeouts.transactions)
+    controller: ($scope) => {
+      $scope.transactions = []
 
-        return $scope.peer.getTransactions($scope.address)
+      $scope.updateTransactions = (more) => {
+        if (more) {
+          $scope.loading_more = true
+        }
+
+        $timeout.cancel($scope.timeout)
+
+        return $scope.peer.getTransactions($scope.address, ($scope.transactions.length || 10) + (more ? 10 : 0))
           .then(res => {
-            $scope.transactions = res
-            $scope.timeouts.transactions = $timeout($scope.updateTransactions, UPDATE_INTERVAL)
+            $scope.transactions = res.transactions
+            $scope.total = res.count
+
+            if ($scope.total > $scope.transactions.length) {
+              $scope.more = $scope.total - $scope.transactions.length
+            } else {
+              $scope.more = 0
+            }
+
+            if (more) {
+              $scope.loading_more = false
+            }
+
+            $scope.timeout = $timeout($scope.updateTransactions, UPDATE_INTERVAL)
           })
           .catch(() => {
-            $scope.$emit('error')
             return $q.reject()
           })
       }
 
       $scope.updateTransactions()
+
+      $scope.$on('$destroy', () => {
+        $timeout.cancel($scope.timeout)
+      })
     }
   }
 })
