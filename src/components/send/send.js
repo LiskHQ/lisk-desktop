6c9e9cc74db5e94deeebae0b3419a1c9b922dd7e
rferro <ricardo.ferro@gmail.com>
2016-06-20 01:43:54 -0300

    Send directive improvements to isolated scope

--- src/components/send/send.js
+++ src/components/send/send.js
@@ -10,45 +10,46 @@ app.directive('send', (error, success) => {
   return {
     restrict: 'E',
     template: require('./send.jade'),
-    link (scope, elem, attrs) {},
+    scope: { account: '=', peer: '=', passphrase: '=' },
     controller: ($scope) => {
-      $scope.send = {
-        reset () {
-          $scope.send.recipient.value = ''
-          $scope.send.amount.value = ''
-        },
-        go () {
-          $scope.send.loading = true
+      $scope.recipient = {
+        regexp: ADDRESS_VALID_RE,
+      }
 
-          $scope.peer.sendTransaction(
-            $scope.passphrase,
-            $scope.secondPassphrase,
-            $scope.send.recipient.value,
-            $scope.send.amount.value
-          )
-          .then(
-            (res) => {
-              return success.dialog({ text: `${$scope.send.amount.value} sent to ${$scope.send.recipient.value}` })
-                .then(() => {
-                  $scope.send.reset()
-                  $scope.updateBalance()
-                  $scope.updateTransactions()
-                })
-            },
-            (res) => {
-              error.dialog({ text: res && res.message ? res.message : 'An error occurred while sending the transaction.' })
-            }
-          )
-          .finally(() => {
-            $scope.send.loading = false
-          })
-        },
-        recipient: {
-          regexp: ADDRESS_VALID_RE,
-        },
-        amount: {
-          regexp: AMOUNT_VALID_RE,
-        }
+      $scope.amount = {
+        regexp: AMOUNT_VALID_RE,
+      }
+
+      $scope.reset = () => {
+        $scope.recipient.value = ''
+        $scope.amount.value = ''
+      }
+
+      $scope.go = () => {
+        $scope.loading = true
+
+        $scope.peer.sendTransaction(
+          $scope.passphrase,
+          $scope.secondPassphrase,
+          $scope.recipient.value,
+          $scope.amount.value
+        )
+        .then(
+          (res) => {
+            return success.dialog({ text: `${$scope.amount.value} sent to ${$scope.recipient.value}` })
+              .then(() => {
+                $scope.reset()
+                $scope.updateBalance()
+                $scope.updateTransactions()
+              })
+          },
+          (res) => {
+            error.dialog({ text: res && res.message ? res.message : 'An error occurred while sending the transaction.' })
+          }
+        )
+        .finally(() => {
+          $scope.loading = false
+        })
       }
     }
   }
