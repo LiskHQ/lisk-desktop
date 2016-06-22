b4f15349497d66a100b359992eff25695509713b
rferro <ricardo.ferro@gmail.com>
2016-06-22 20:52:38 -0300

    Change address directive to component and fix calls

--- src/components/address/address.js
+++ src/components/address/address.js
@@ -3,12 +3,7 @@ import './address.less'
 
 import app from '../../app'
 
-app.directive('address', () => {
-  return {
-    restrict: 'C',
-    template: require('./address.jade'),
-    scope: { data: '=' },
-    link (scope, elem, attrs) {},
-    controller: ($scope) => {}
-  }
+app.component('address', {
+  template: require('./address.jade')(),
+  bindings: { data: '<' },
 })
