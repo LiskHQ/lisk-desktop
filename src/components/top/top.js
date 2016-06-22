e412faf528dbc3707c07785ea82ea645e8409fd4
rferro <ricardo.ferro@gmail.com>
2016-06-22 01:49:24 -0300

    Change top directive to component

--- src/components/top/top.js
+++ src/components/top/top.js
@@ -3,13 +3,16 @@ import './top.less'
 
 import app from '../../app'
 
-app.directive('top', () => {
-  return {
-    restrict: 'E',
-    template: require('./top.jade'),
-    scope: { account: '=', peer: '=' },
-    controller: ($scope, peers) => {
-      $scope.peers = peers.official
+app.component('top', {
+  template: require('./top.jade')(),
+  bindings: {
+    account: '=',
+    peer: '=',
+  },
+  controller: class main {
+    constructor (peers) {
+      this.ricardo = 123
+      this.peers = peers.official
     }
   }
 })
