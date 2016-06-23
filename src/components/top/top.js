c94b4c49398067441f0410fb0b15b15557c92489
rferro <ricardo.ferro@gmail.com>
2016-06-23 00:41:32 -0300

    New top component

--- src/components/top/top.js
+++ src/components/top/top.js
@@ -7,17 +7,9 @@ app.component('top', {
   template: require('./top.jade')(),
   bindings: {
     account: '<',
-    peer: '=',
   },
   controller: class top {
-    constructor ($scope, $rootScope, peers) {
-      this.peers = peers.official
-
-      $scope.$watch('$ctrl.peer', (peer, old) => {
-        if (peer != old) {
-          $rootScope.$broadcast('peerUpdate')
-        }
-      })
+    constructor ($scope) {
     }
   }
 })
