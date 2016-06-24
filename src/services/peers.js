6f55d2cd35176a2c3bc8f8f071ff052a04aed742
rferro <ricardo.ferro@gmail.com>
2016-06-24 01:21:08 -0300

    New peers service

--- src/services/peers.js
+++ src/services/peers.js
@@ -1,28 +1,40 @@
 
+import _ from 'lodash'
+
 import app from '../app'
 
 const UPDATE_INTERVAL = 60000
 
-app.factory('peers', ($log, $q, $timeout, peer) => {
-  class peers {
+app.factory('$peers', ($log, $q, $timeout, $peer, $interval) => {
+  class $peers {
     constructor () {
-      this.official = [
-        new peer({ host: 'testnet.lisk.io', port: null, ssl: true }),
-        new peer({ host: 'node01.lisk.io' }),
-        new peer({ host: 'node02.lisk.io' }),
-        new peer({ host: 'node03.lisk.io' }),
-        new peer({ host: 'node04.lisk.io' }),
-        new peer({ host: 'node05.lisk.io' }),
-        new peer({ host: 'node06.lisk.io' }),
-        new peer({ host: 'node07.lisk.io' }),
-        new peer({ host: 'node08.lisk.io' }),
-      ]
+      this.stack = {
+        official: [
+          new $peer({ host: 'node01.lisk.io' }),
+          new $peer({ host: 'node02.lisk.io' }),
+          new $peer({ host: 'node03.lisk.io' }),
+          new $peer({ host: 'node04.lisk.io' }),
+          new $peer({ host: 'node05.lisk.io' }),
+          new $peer({ host: 'node06.lisk.io' }),
+          new $peer({ host: 'node07.lisk.io' }),
+          new $peer({ host: 'node08.lisk.io' }),
+        ],
+        public: [],
+        testnet: [
+          new $peer({ host: 'testnet.lisk.io', port: null, ssl: true }),
+        ]
+      }
     }
 
     random (type) {
-      return this.official[parseInt(Math.random() * this.official.length)]
+      let stack = _.concat(this.stack.official, this.stack.public)
+      return stack[parseInt(Math.random() * stack.length)]
+    }
+
+    add (data) {
+      this.stack.public.push(new $peer(data))
     }
   }
 
-  return new peers()
+  return new $peers()
 })
