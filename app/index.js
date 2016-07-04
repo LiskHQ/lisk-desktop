a34dd329d531cff70e3e60f2817ca875762a30c1
rferro <ricardo.ferro@gmail.com>
2016-07-03 21:58:34 -0300

    Fix imports of vendor dependencies

--- app/index.js
+++ app/index.js
@@ -1,4 +1,12 @@
 
+import 'jquery'
+
+import 'angular'
+import 'angular-animate'
+import 'angular-aria'
+import 'angular-messages'
+import 'angular-material'
+import 'angular-material/angular-material.css'
 import 'angular-material-data-table/dist/md-data-table.js'
 import 'angular-material-data-table/dist/md-data-table.css'
 
