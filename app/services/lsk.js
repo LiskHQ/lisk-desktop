fe6283b7def4c1225c2821ac4bb49acea4b986d3
rferro <ricardo.ferro@gmail.com>
2016-07-14 02:47:20 -0300

    Create the ‘from’ method in lsk service to convert amount to raw value

--- app/services/lsk.js
+++ app/services/lsk.js
@@ -5,6 +5,9 @@ app.factory('lsk', () => {
   return {
     normalize (value) {
       return numeral(parseInt(value) || 0).divide(Math.pow(10, 8)).format('0.0[0000000]')
+    },
+    from (value) {
+      return parseInt(value * Math.pow(10, 8))
     }
   }
 })
