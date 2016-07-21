652268ba67b9625cd65d59448fc837a0b8dfd67b
rferro <ricardo.ferro@gmail.com>
2016-07-21 02:54:45 -0300

    Update build

---
+++ app/main.js
@@ -0,0 +1,29 @@
+
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win
+
+function createWindow () {
+  let { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+  win = new BrowserWindow({ width: width - 100, height: height - 100, center: true })
+
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  win.on('closed', () => win = null)
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
