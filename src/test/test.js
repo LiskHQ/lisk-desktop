4df62d90122998eb6d9df28deb2ac82db5d58840
Oliver Beddows <oliver@lisk.io>
2017-03-06 12:18:20 +0100

    Standardising code

--- src/test/test.js
+++ src/test/test.js
@@ -1,14 +1,9 @@
-var should = require("should");
+var should = require('should');
 
-describe('Example Test', function() {
+describe('Example Test', function () {
 
-
-	it("should show an example", function() {
-
+	it('should show an example', function () {
 		var x = 5;
-
 		(x).should.be.equal(5);
-
 	});
-
 });
