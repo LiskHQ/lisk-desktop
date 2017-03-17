c655ee5c6b747e0bf35f59073c52ffff75c48f5e
Vit Stanislav <slaweet@gmail.com>
2017-03-17 13:53:30 +0100

    Add login component controller tests

--- src/test/components/login/login.spec.js
+++ src/test/components/login/login.spec.js
@@ -21,3 +21,47 @@ describe('Login component', function() {
   });
 });
 
+describe('Login controller', function() {
+  beforeEach(angular.mock.module("app"));
+
+  var $controller,
+      $rootScope;
+
+  beforeEach(inject(function(_$componentController_, _$rootScope_){
+    $componentController = _$componentController_;
+    $rootScope = _$rootScope_;
+  }));
+
+  describe('$scope.emptyBytes', function() {
+    it('it returns array of 12 zeros', function() {
+      var $scope = $rootScope.$new();
+      var controller = $componentController('login', $scope, { });
+      expect(controller.emptyBytes()).to.equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    });
+  });
+
+  describe('$scope.isValid(value)', function() {
+    var $scope,
+        controller;
+
+    beforeEach(function() {
+      $scope = $rootScope.$new();
+      controller = $componentController('login', $scope, { });
+    });
+
+    it('sets $scope.valid = 2 if  value is empty', function() {
+      controller.isValid('');
+      expect(controller.valid).to.equal(2);
+    });
+
+    it('sets $scope.valid = 1 if value is valid', function() {
+      controller.isValid('ability theme abandon abandon abandon abandon abandon abandon abandon abandon abandon absorb');
+      expect(controller.valid).to.equal(1);
+    });
+
+    it('sets $scope.valid = 0 if value is invalid', function() {
+      controller.isValid('INVALID VALUE');
+      expect(controller.valid).to.equal(0);
+    });
+  });
+});
