f3ecc1d41498375ddf53aa094406ea352940b14f
Vit Stanislav <slaweet@gmail.com>
2017-03-22 13:53:44 +0100

    Separate test dependencies into a separate bundle

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -6,9 +6,11 @@ var preprocessors = {};
 preprocessors['**/*.html'] = ['ng-html2js'];
 var libs = path.join(__dirname, 'app', 'libs.js');
 var app = path.join(__dirname, 'app', 'lisk-nano.js');
+var testLibs = path.join(__dirname, 'test', 'libs.js');
 var test = path.join(__dirname, 'test', 'test.js');
 preprocessors[libs] = ['webpack'];
 preprocessors[app] = ['webpack', 'coverage'];
+preprocessors[testLibs] = ['webpack'];
 preprocessors[test] = ['webpack'];
 
 var opts = {
@@ -27,7 +29,7 @@ module.exports = function(config) {
 		frameworks: ['mocha', 'chai'],
 
 		// list of files / patterns to load in the browser
-		files: [libs, app, test],
+		files: [libs, app, testLibs, test],
 		webpack: webpackConfig,
 
 		// list of files to exclude
--- src/test/test.js
+++ src/test/test.js
@@ -1,7 +1,3 @@
-
-require('angular-mocks/angular-mocks');
-require('chai');
-
 require('./components/login/login.spec');
 require('./components/top/top.spec');
 require('./components/timestamp/timestamp.spec');
