4f7ab2d9a07c1e3c0f9a3e08b8a52cd645252107
Vit Stanislav <vit@lightcurve.io>
2017-03-23 08:20:51 +0100

    Remove progress reporter from karma
    to make the output more readable

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'coverage', 'mocha'].concat(opts.onTravis ? ['coveralls'] : []),
+		reporters: ['coverage', 'mocha'].concat(opts.onTravis ? ['coveralls'] : []),
 
 		preprocessors: preprocessors,
 
