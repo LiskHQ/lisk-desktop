ba06511c28e1654f520cad44431549cc969be9d0
Vit Stanislav <vit@lightcurve.io>
2017-03-24 12:51:15 +0100

    Fix coverage of animate-on-change directive

--- src/test/util/animateOnChange/animateOnChange.spec.js
+++ src/test/util/animateOnChange/animateOnChange.spec.js
@@ -6,16 +6,21 @@ describe('animate-on-change directive', () => {
   let $compile;
   let $rootScope;
   let element;
+  let $animate;
+  let $timeout;
 
   // Load the myApp module, which contains the directive
   beforeEach(angular.mock.module('app'));
+  beforeEach(angular.mock.module('ngAnimateMock'));
 
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
-  beforeEach(inject((_$compile_, _$rootScope_) => {
+  beforeEach(inject((_$compile_, _$rootScope_, _$animate_, _$timeout_) => {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $compile = _$compile_;
     $rootScope = _$rootScope_;
+    $animate = _$animate_;
+    $timeout = _$timeout_;
   }));
 
   beforeEach(() => {
@@ -25,11 +30,14 @@ describe('animate-on-change directive', () => {
     $rootScope.$digest();
   });
 
-  it('adds class "change" to the element on change of the attribtde', () => {
+  it('adds and removes class "change" to the element on change of the attribtde', () => {
     expect(element.hasClass('change')).to.equal(false);
     $rootScope.byte = '01';
     $rootScope.$digest();
     expect(element.hasClass('change')).to.equal(true);
+    $animate.flush();
+    $timeout.flush();
+    expect(element.hasClass('change')).to.equal(false);
   });
 });
 
