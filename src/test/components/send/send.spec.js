cf76ed1f5699c49fca5d418cb3a0a7b0eb00cb59
Vit Stanislav <vit@lightcurve.io>
2017-03-27 09:41:49 +0200

    Unit test send component

--- src/test/components/send/send.spec.js
+++ src/test/components/send/send.spec.js
@@ -5,6 +5,86 @@ const sinonChai = require('sinon-chai');
 const expect = chai.expect;
 chai.use(sinonChai);
 
+describe('Send component', () => {
+  let $compile;
+  let $rootScope;
+  let element;
+  let $scope;
+
+  beforeEach(angular.mock.module('app'));
+
+  beforeEach(inject((_$compile_, _$rootScope_) => {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  beforeEach(() => {
+    $scope = $rootScope.$new();
+    $scope.passphrase = 'robust swift grocery peasant forget share enable convince deputy road keep cheap';
+    $scope.account = {
+      address: '8273455169423958419L',
+      balance: '10000',
+    };
+    element = $compile('<send passphrase="passphrase" account="account"></send>')($scope);
+    $scope.$digest();
+  });
+
+  const HEADER_TEXT = 'Send';
+  it(`should contain header saying "${HEADER_TEXT}"`, () => {
+    expect(element.find('.md-title').text()).to.equal(HEADER_TEXT);
+  });
+
+  const RECIPIENT_LABEL_TEXT = 'Recipient Address';
+  it(`should contain a form with label saying "${RECIPIENT_LABEL_TEXT}"`, () => {
+    expect(element.find('form label:first').text()).to.equal(RECIPIENT_LABEL_TEXT);
+  });
+
+  const AMOUT_LABEL_TEXT = 'Transaction Amount';
+  it(`should contain a form with label saying "${AMOUT_LABEL_TEXT}"`, () => {
+    expect(element.find('form label:last').text()).to.equal(AMOUT_LABEL_TEXT);
+  });
+
+  const LOGIN_BUTTON_TEXT = 'Send';
+  it(`should contain a button saying "${LOGIN_BUTTON_TEXT}"`, () => {
+    expect(element.find('button.md-raised').text()).to.equal(LOGIN_BUTTON_TEXT);
+  });
+
+  describe('send transaction', () => {
+    let $q;
+    let $peers;
+    let success;
+
+    beforeEach(inject((_success_, _$q_, _$peers_) => {
+      success = _success_;
+      $q = _$q_;
+      $peers = _$peers_;
+    }));
+
+    it('should allow to send a transaction', () => {
+      const RECIPIENT_ADDRESS = '5932438298200837883L';
+      const AMOUNT = '10';
+
+      $peers.active = { sendTransaction() {} };
+      const mock = sinon.mock($peers.active);
+      const deffered = $q.defer();
+      mock.expects('sendTransaction').returns(deffered.promise);
+
+      const spy = sinon.spy(success, 'dialog');
+
+
+      element.find('form input[name="amount"]').val(AMOUNT).trigger('input');
+      element.find('form input[name="recipient"]').val(RECIPIENT_ADDRESS).trigger('input');
+      $scope.$apply();
+      element.find('button.md-raised').click();
+
+      deffered.resolve();
+      $scope.$apply();
+      expect(spy).to.have.been.calledWith();
+      mock.verify();
+    });
+  });
+});
+
 describe('send component controller', () => {
   beforeEach(angular.mock.module('app'));
 
