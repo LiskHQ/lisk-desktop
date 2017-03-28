4a5bcdfe7bb6acfe9371e0c12cfd751524b4599b
Vit Stanislav <vit@lightcurve.io>
2017-03-28 16:31:05 +0200

    Show pending transactions
    - Send compoment brodcasts an event with the pending transaction
    - Transactions component keeps list of pending transactions and removes those once it receives a transaction with the same id from the server

--- src/test/components/send/send.spec.js
+++ src/test/components/send/send.spec.js
@@ -152,7 +152,7 @@ describe('send component controller', () => {
       controller.go();
 
       const spy = sinon.spy(controller.success, 'dialog');
-      deffered.resolve();
+      deffered.resolve({});
       $scope.$apply();
       expect(spy).to.have.been.calledWith({
         text: `${controller.amount.value} sent to ${controller.recipient.value}`,
