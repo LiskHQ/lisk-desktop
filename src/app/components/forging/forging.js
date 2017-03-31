1c6b33be94f46796b92eab826f7ae8cfb4d69f06
Vit Stanislav <vit@lightcurve.io>
2017-03-31 12:46:37 +0200

    Add forging mockups for more states

--- src/app/components/forging/forging.js
+++ src/app/components/forging/forging.js
@@ -6,25 +6,32 @@ app.component('forging', {
     account: '=',
   },
   controller: class forging {
-    constructor($scope) {
+    constructor($scope, $location) {
       this.$scope = $scope;
-      this.$scope.delegate = {
+      this.$scope.delegate = $location.search().noforging ? { } : {
         username: 'slaweet',
         time: 26829751,
         rank: 77,
       };
-      this.$scope.statistics = {
-        today: 200000,
-        last24h: 2000000,
-        last7d: 20000000,
-        last30d: 200000000,
-      };
-      this.$scope.totalForged = 43200000000;
-      this.$scope.rank = 77;
-      this.$scope.productivity = 20;
-      this.$scope.approval = 80;
-      this.$scope.showAllColumns = false;
-      this.$scope.blocks = [{
+      if (!$location.search().noblocks) {
+        this.$scope.statistics = {
+          today: 200000,
+          last24h: 2000000,
+          last7d: 20000000,
+          last30d: 200000000,
+        };
+        this.$scope.totalForged = 43200000000;
+        this.$scope.rank = 77;
+        this.$scope.productivity = 20;
+        this.$scope.approval = 80;
+        this.$scope.showAllColumns = false;
+      } else {
+        this.$scope.rank = 377;
+        this.$scope.productivity = 0;
+        this.$scope.approval = 0;
+        this.$scope.showAllColumns = false;
+      }
+      this.$scope.blocks = $location.search().noblocks ? [] : [{
         id: '3881345055239321586',
         timestamp: 26836720,
         height: 7933,
