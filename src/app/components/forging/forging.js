52daba9b0629f164147000b8b31300fce49e9c63
Vit Stanislav <vit@lightcurve.io>
2017-04-07 14:25:02 +0200

    Start using real data in forging center
    instead of mocked data

--- src/app/components/forging/forging.js
+++ src/app/components/forging/forging.js
@@ -1,4 +1,8 @@
+import moment from 'moment';
+
 import './forging.less';
+
+const UPDATE_INTERVAL = 20000;
 
 app.component('forging', {
   template: require('./forging.pug')(),
@@ -6,69 +10,82 @@ app.component('forging', {
     account: '=',
   },
   controller: class forging {
-    constructor($scope, $location) {
+    constructor($scope, $timeout, $peers) {
       this.$scope = $scope;
-      this.$scope.delegate = $location.search().noforging ? { } : {
-        username: 'slaweet',
-        time: 26829751,
-        rank: 77,
-      };
-      if (!$location.search().noblocks) {
-        this.$scope.statistics = {
-          today: 200000,
-          last24h: 2000000,
-          last7d: 20000000,
-          last30d: 200000000,
-        };
-        this.$scope.totalForged = 43200000000;
-        this.$scope.rank = 77;
-        this.$scope.productivity = 20;
-        this.$scope.approval = 80;
-        this.$scope.showAllColumns = false;
-      } else {
-        this.$scope.rank = 377;
-        this.$scope.productivity = 0;
-        this.$scope.approval = 0;
-        this.$scope.showAllColumns = false;
-      }
-      this.$scope.blocks = $location.search().noblocks ? [] : [{
-        id: '3881345055239321586',
-        timestamp: 26836720,
-        height: 7933,
-        totalFee: 0,
-        reward: 0,
-        generatorId: '15441829200899900957L',
-        confirmations: 1,
-        totalForged: '0',
-      }, {
-        id: '13266308849752923233',
-        version: 0,
-        timestamp: 26836710,
-        height: 7932,
-        numberOfTransactions: 0,
-        totalAmount: 0,
-        totalFee: 0,
-        reward: 0,
-        payloadLength: 0,
-        generatorId: '2003981962043442425L',
-        confirmations: 2,
-        totalForged: '0',
-      }, {
-        id: '16048816357878795927',
-        version: 0,
-        timestamp: 26836700,
-        height: 7931,
-        numberOfTransactions: 0,
-        totalAmount: 0,
-        totalFee: 0,
-        reward: 0,
-        payloadLength: 0,
-        generatorId: '6253486079725348800L',
-        confirmations: 3,
-        totalForged: '0',
-      }];
+      this.$timeout = $timeout;
+      this.$peers = $peers;
 
-      this.loaded = true;
+      this.statistics = {};
+      this.blocks = [];
+
+      this.loadAllData();
+    }
+
+    $onDestroy() {
+      this.$timeout.cancel(this.timeout);
+    }
+
+    loadAllData() {
+      this.loadDelegate();
+      this.loadForgedBlocks(10);
+
+      this.loadForgingStats('today', moment().format('YYYY-MM-DD'));
+      this.loadForgingStats('last24h', moment().subtract(1, 'days'));
+      this.loadForgingStats('last7d', moment().subtract(7, 'days'));
+      this.loadForgingStats('last30d', moment().subtract(30, 'days'));
+      this.loadForgingStats('total', moment('2016-04-24 17:00'));
+    }
+
+    loadDelegate() {
+      this.$peers.active.sendRequest('delegates/get', {
+        publicKey: this.account.publicKey,
+      }, (data) => {
+        if (data.success) {
+          this.delegate = data.delegate;
+        } else {
+          this.delegate = {};
+        }
+      });
+    }
+
+    loadForgedBlocks(limit, offset) {
+      this.$timeout.cancel(this.timeout);
+
+      this.$peers.active.sendRequest('blocks', {
+        limit,
+        offset: offset || 0,
+        generatorPublicKey: this.account.publicKey,
+      }, (data) => {
+        if (data.success) {
+          if (this.blocks.length === 0) {
+            this.blocks = data.blocks;
+          } else if (offset) {
+            Array.prototype.push.apply(this.blocks, data.blocks);
+          } else if (this.blocks[0].id !== data.blocks[0].id) {
+            Array.prototype.unshift.apply(this.blocks,
+              data.blocks.filter(block => block.timestamp < this.blocks[0].timestamp));
+          }
+          this.blocksLoaded = true;
+          this.moreBlocksExist = this.blocks.length < data.count;
+        }
+
+        this.timeout = this.$timeout(this.loadAllData.bind(this), UPDATE_INTERVAL);
+      });
+    }
+
+    loadMoreBlocks() {
+      this.blocksLoaded = false;
+      this.loadForgedBlocks(20, this.blocks.length);
+    }
+
+    loadForgingStats(key, startMoment) {
+      this.$peers.active.sendRequest('delegates/forging/getForgedByAccount', {
+        generatorPublicKey: this.account.publicKey,
+        start: moment(startMoment).unix(),
+        end: moment().unix(),
+      }, (data) => {
+        this.statistics[key] = data.forged;
+      });
     }
   },
 });
