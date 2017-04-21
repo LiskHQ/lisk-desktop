0ae2df4521e9809b136b4e9d02aa699196878df2
Vit Stanislav <vit@lightcurve.io>
2017-04-21 13:12:15 +0200

    Setup e2e tests to run in travis

---
+++ .travis/before_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+
+psql -c 'drop database lisk_test;' -U postgres
+psql -c 'create database lisk_test;' -U postgres
+psql -d lisk_test -c "alter user "$USER" with password 'password';"
+git clone https://github.com/LiskHQ/lisk
+git checkout master
+cd lisk/
+npm install
+cd test/lisk-js/; npm install; cd ../..
+cp test/config.json test/genesisBlock.json .
+
+node app.js &> ../.lisk-core.log &
+sleep 5
+cd ..
+for i in {1..20}
+do 
+  curl -k -H "Content-Type: application/json" -X PUT -d '{"secret":"wagon stock borrow episode laundry kitten salute link globe zero feed marble","amount":'"$i"0000000',"recipientId":"537318935439898807L"}' http://localhost:4000/api/transactions
+  echo ''
+done
+sleep 10
--- src/spec/conf.js
+++ src/spec/conf.js
@@ -4,15 +4,22 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js'],
   capabilities: {
-    browserName: 'chrome',
+    browserName: 'phantomjs',
   },
   onPrepare() {
     const env = jasmine.getEnv();
 
-    env.clearReporters();
-    jasmine.getEnv().addReporter(new SpecReporter({
+    // FIXME the following like can make the output cleaner
+    // but then shell return code is always 0 even if some tests fail.
+    // env.clearReporters();
+
+    env.addReporter(new SpecReporter({
       spec: {
         displayStacktrace: false,
+        displayDuration: true,
+      },
+      summary: {
+        displayDuration: true,
       },
     }));
   },
