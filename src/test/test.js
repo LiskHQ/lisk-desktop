4eca9a8f5a90353aaf99fb47a9bbe266722d03ae
alihaghighatkhah <ali@lightcurve.io>
2017-04-23 19:03:30 +0200

    create setSecondPass service: uses mdDialog to show/hide modals of second passphrase

--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,14 @@
 require('./components/forging/forging.spec');
 require('./components/login/login.spec');
+require('./components/login/passphrase.spec');
 require('./components/main/main.spec');
+require('./components/main/setSecondPassDirective.spec');
 require('./components/send/send.spec');
 require('./components/top/top.spec');
 require('./components/timestamp/timestamp.spec');
 require('./components/transactions/transactions.spec');
 
 require('./services/peers/peers.spec');
+require('./services/passphrase.spec');
 
 require('./util/animateOnChange/animateOnChange.spec');
