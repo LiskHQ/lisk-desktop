0ba7b650f887dfc652a1f994e844c984fa2eeae3
alihaghighatkhah <ali@lightcurve.io>
2017-04-25 17:17:26 +0200

    increate test converage

--- src/test/components/login/passphrase.spec.js
+++ src/test/components/login/passphrase.spec.js
@@ -7,10 +7,10 @@ chai.use(sinonChai);
 
 describe('Passphrase Directive', () => {
   let $compile;
-  let $scope;
   let $rootScope;
   let $document;
   let Passphrase;
+  let $isolateScope;
 
   beforeEach(() => {
     // Load the myApp module, which contains the directive
@@ -23,41 +23,38 @@ describe('Passphrase Directive', () => {
       $rootScope = _$rootScope_;
       $document = _$document_;
       Passphrase = _Passphrase_;
-      $scope = $rootScope.$new();
     });
 
     // Compile a piece of HTML containing the directive
-    $compile('<div><passphrase data-target="primary-pass"></passphrase></div>')($scope);
-    $scope.$digest();
+    const element = angular.element('<passphrase data-target="primary-pass"></passphrase>');
+    const e = $compile(element)($rootScope);
+    e.scope().$digest();
+    $isolateScope = e.isolateScope();
   });
 
   describe('PassphraseLink', () => {
-    it.skip('should intiate Passphrase service', () => {
-      expect(Passphrase.init).called();
-    });
-
-    it.skip('should asign progress to its own $scope', () => {
-      expect($scope.progress).to.not.equal(undefined);
-      expect($scope.progress).to.equal(Passphrase.progress);
+    it('should asign progress to its own $scope', () => {
+      expect($isolateScope.progress).to.not.equal(undefined);
+      expect($isolateScope.progress).to.equal(Passphrase.progress);
     });
   });
 
   describe('$scope.simulateMousemove()', () => {
-    it.skip('calls $document.mousemove()', () => {
+    it('calls $document.mousemove()', () => {
       const spy = sinon.spy($document, 'mousemove');
-      $scope.simulateMousemove();
+      $isolateScope.simulateMousemove();
       expect(spy).to.have.been.calledWith();
     });
   });
 
   describe('$scope.mobileAndTabletcheck()', () => {
-    it.skip('checks if the useAgent is a device', () => {
+    it('checks if the useAgent is a device', () => {
       const agents = [
         'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
         'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
       ];
       let isDevice = true;
-      agents.forEach(agent => isDevice = isDevice && $scope.mobileAndTabletcheck(agent));
+      agents.forEach(agent => isDevice = isDevice && $isolateScope.mobileAndTabletcheck(agent));
       expect(isDevice).to.equal(true);
     });
   });
