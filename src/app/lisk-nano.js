5b78215a8a7884c6fa082556f1bf9b84c14f4f73
alihaghighatkhah <ali@lightcurve.io>
2017-04-28 13:59:17 +0200

    add header to project modules list

--- src/app/lisk-nano.js
+++ src/app/lisk-nano.js
@@ -8,6 +8,7 @@ import './components/main/setSecondPassDirective';
 import './components/login/login';
 import './components/login/passphrase';
 import './components/top/top';
+import './components/header/header';
 import './components/send/send';
 import './components/send/sendModalService';
 import './components/send/sendModalDirective';
