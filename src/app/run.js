3a0b022e089fe11ec6a9b603dbb2870a07817883
alihaghighatkhah <ali@lightcurve.io>
2017-05-11 17:09:14 +0200

    Create Sync service to manage Api call intervalls

--- src/app/run.js
+++ src/app/run.js
@@ -1,5 +1,6 @@
-app.run(($rootScope, $timeout, $state, $transitions, $mdDialog, Peers, Account) => {
+app.run(($rootScope, $timeout, $state, $transitions, $mdDialog, Peers, Account, Sync) => {
   $rootScope.peers = Peers;
+  Sync.init();
 
   $transitions.onStart({ to: '*' }, () => {
     $mdDialog.cancel();
