60b44c35cc6708508022378324e21633ff2a9ed3
Vit Stanislav <vit@lightcurve.io>
2017-05-17 14:15:00 +0200

    Don't set isDelegate flag directly on delegate registration

--- src/app/components/delegate-registration/delegateRegistration.js
+++ src/app/components/delegate-registration/delegateRegistration.js
@@ -1,28 +1,39 @@
 import './delegateRegistration.less';
 
-app.directive('delegateRegistration', ($mdDialog, delegateService, Account, dialog) => {
+app.directive('delegateRegistration', ($mdDialog, delegateService, Account, dialog, $timeout) => {
   const DelegateRegistrationLink = function ($scope, $element) {
+    function checkPendingRegistration() {
+      delegateService.getDelegate({
+        username: $scope.username,
+      }).then((data) => {
+        Account.set({
+          isDelegate: true,
+          username: data.delegate.username,
+        });
+      }).catch(() => {
+        $timeout(checkPendingRegistration, 10000);
+      });
+    }
+
     $scope.form = {
       name: '',
       fee: 25,
       error: '',
       onSubmit: (form) => {
         if (form.$valid) {
+          $scope.username = $scope.form.name.toLowerCase();
           delegateService.registerDelegate(
-              $scope.form.name.toLowerCase(),
+              $scope.username,
               Account.get().passphrase,
               $scope.form.secondPassphrase,
             )
             .then(() => {
               dialog.successAlert({
                 title: 'Success',
-                text: 'Account was successfully registered as delegate.',
+                text: 'Delegate registration was successfully submitted. It can take several seconds before it is confirmed.',
               })
                 .then(() => {
-                  Account.set({
-                    isDelegate: true,
-                    username: $scope.form.name.toLowerCase(),
-                  });
+                  checkPendingRegistration();
                   $scope.reset(form);
                   $mdDialog.hide();
                 });
