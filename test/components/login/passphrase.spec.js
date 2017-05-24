a1b481698b4f803c086092a99d855caa2fe66638
Aleksey Popov <alepooop@gmail.com>
2017-05-25 00:35:52 +0300

    Fix lint errors

--- test/components/login/passphrase.spec.js
+++ test/components/login/passphrase.spec.js
@@ -54,7 +54,7 @@ describe('Passphrase Directive', () => {
         'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
       ];
       let isDevice = true;
-      agents.forEach(agent => isDevice = isDevice && $isolateScope.mobileAndTabletcheck(agent));
+      agents.forEach(agent => (isDevice = isDevice && $isolateScope.mobileAndTabletcheck(agent)));
       expect(isDevice).to.equal(true);
     });
   });
