fead4efc80c431804128c094386017daad02a108
yashar <darkyashi@gmail.com>
2017-06-02 14:38:24 +0430

    Fix eslint errors in dialog.spec.js

--- test/services/dialog.spec.js
+++ test/services/dialog.spec.js
@@ -6,25 +6,25 @@ chai.use(sinonChai);
 chai.should();
 
 describe('Factory: dialog', () => {
-  let $mdToast;
   let $mdDialog;
-
+  let dialog;
+  const component = 'send';
   beforeEach(angular.mock.module('app'));
 
-  beforeEach(inject((_$mdToast_, _$mdDialog_) => {
-    $mdToast = _$mdToast_;
+  beforeEach(inject((_$mdDialog_, _dialog_) => {
     $mdDialog = _$mdDialog_;
+    dialog = _dialog_;
   }));
 
   describe('modal(component, options)', () => {
     it('opens a $mdDialog', () => {
-      let mock = sinon.mock($mdDialog);
+      const mock = sinon.mock($mdDialog);
       dialog.modal();
       mock.expects('show').withArgs();
     });
 
     it.skip('only accepts a non empty string as component', () => {
       component.should.be.a('string');
-    })
+    });
   });
 });
