c3dcb1e612c4b7a7cd528b049e314173956b76a6
reyraa <ali@lightcurve.io>
2017-06-07 16:25:52 +0200

    Renamed and converted directive to component

--- src/components/main/secondPass.js
+++ src/components/main/secondPass.js
@@ -4,22 +4,18 @@ import './secondPass.less';
  * The directive to show the second passphrase form and register it using AccountApi
  *
  * @module app
- * @submodule setSecondPass
+ * @submodule SetSecondPassCtrl
  */
-app.directive('setSecondPass', (setSecondPass, Account, $rootScope, dialog, AccountApi) => {
-  /* eslint no-param-reassign: ["error", { "props": false }] */
-  const SetSecondPassLink = function (scope, element) {
-    element.bind('click', () => {
-      setSecondPass.show();
-    });
-
+app.component('setsecondpass', {
+  template: require('./secondPass.pug')(),
+  controller: function ($scope, Account, $rootScope, dialog, AccountApi) {
     /**
      * We call this after second passphrase is generated.
      * Shows an alert with appropriate message in case the request fails.
      *
      * @param {String} secondSecret - The validated passphrase to register as second secret
      */
-    scope.passConfirmSubmit = (secondSecret) => {
+    $scope.passConfirmSubmit = (secondSecret) => {
       AccountApi.setSecondSecret(secondSecret, Account.get().publicKey, Account.get().passphrase)
         .then(() => {
           dialog.successAlert({
@@ -42,14 +38,11 @@ app.directive('setSecondPass', (setSecondPass, Account, $rootScope, dialog, Acco
     /**
      * @todo Why we're listening for onAfterSignup here?
      */
-    scope.$on('onAfterSignup', (ev, args) => {
+    $scope.$on('onAfterSignup', (ev, args) => {
       if (args.target === 'second-pass') {
-        scope.passConfirmSubmit(args.passphrase);
+        $scope.passConfirmSubmit(args.passphrase);
       }
     });
-  };
-  return {
-    restrict: 'A',
-    link: SetSecondPassLink,
-  };
+  },
+  // controllerAs: 'md',
 });
