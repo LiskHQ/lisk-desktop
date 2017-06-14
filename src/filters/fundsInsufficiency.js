f0df4ea5c58f5b908945eb5a7183ce153f75ed4d
Vit Stanislav <vit@lightcurve.io>
2017-06-14 16:11:22 +0200

    Fix fundsInsufficiency filter name

--- src/filters/fundsInsufficiency.js
+++ src/filters/fundsInsufficiency.js
@@ -1,8 +1,8 @@
 
 /**
- * This filter
+ * This filter returns bool value which is true if account balance is less then input value
  *
  * @module app
- * @submodule notEnoughBalance
+ * @submodule fundsInsufficiency
  */
-app.filter('notEnoughBalance', (Account, lsk) => amount => lsk.normalize(Account.get().balance) < amount);
+app.filter('fundsInsufficiency', (Account, lsk) => amount => lsk.normalize(Account.get().balance) < amount);
