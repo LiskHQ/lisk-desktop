7c5fd3860a2de75d7c8ca2fcc65fcb642ea02c29
yashar <darkyashi@gmail.com>
2017-06-19 15:50:30 +0430

    Fix eslint errors in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,40 @@
 // Karma configuration
 const webpackEnv = { test: true };
 const webpackConfig = require('./webpack.config')(webpackEnv);
+
 const fileGlob = 'src/**/*.test.js';
 const onJenkins = process.env.ON_JENKINS;
-process.env.BABEL_ENV = "test";
-module.exports = function(config) {
+process.env.BABEL_ENV = 'test';
+module.exports = function (config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
     frameworks: ['mocha', 'chai'],
     files: [fileGlob],
     preprocessors: {
-      [fileGlob]: ['webpack']
+      [fileGlob]: ['webpack'],
     },
     reporters: ['progress', 'coverage'],
     coverageReporter: {
       reporters: [
-        { 
+        {
           type: 'text',
           dir: 'coverage/',
-         }, 
-         {
+        },
+        {
           type: onJenkins ? 'lcov' : 'html',
           dir: 'coverage/',
-        }
-      ]
+        },
+      ],
     },
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true,
       // and use stats to turn off verbose output
       stats: {
-        // options i.e. 
-        chunks: false
-      }
+        // options i.e.
+        chunks: false,
+      },
     },
     port: 9876,
     colors: true,
@@ -41,6 +42,6 @@ module.exports = function(config) {
     autoWatch: false,
     browsers: ['Chrome'],
     singleRun: true,
-    concurrency: Infinity
-  })
-}
+    concurrency: Infinity,
+  });
+};
