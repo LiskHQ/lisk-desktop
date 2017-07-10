d49ba1aa5e186ca6426a9fc0ed2a1776a4f4c33d
reyraa <ali@lightcurve.io>
2017-07-10 11:20:36 +0200

    Remove Counter and Static components as they are not required anymore

--- src/main.js
+++ src/main.js
@@ -2,51 +2,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import ReduxCounter from './components/counter';
 import Metronome from './utils/metronome';
 import styles from './main.css';
-import Static from './components/static';
 import Header from './components/header';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.ReduxCounter = ReduxCounter;
-  }
+const App = () => {
+  // start dispatching sync ticks
+  const metronome = new Metronome();
+  metronome.init();
 
-  componentDidMount() {
-    // start dispatching sync ticks
-    this.metronome = new Metronome();
-    this.metronome.init();
-  }
-
-  render() {
-    return (
-      <section className={styles['body-wrapper']}>
-      <Header></Header>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">home</Link>
-                </li>
-                <li>
-                  <Link to="/counter">counter</Link>
-                </li>
-                <li>
-                  <Link to="/static">Static</Link>
-                </li>
-              </ul>
-            </nav>
-            <Route exact path="/" render={() => <p>Home</p>} />
-            <Route path="/static" component={Static} />
-            <Route path="/counter" component={this.ReduxCounter} />
-          </div>
-        </Router>
-      </section>
-    );
-  }
-}
+  return (
+    <section className={styles['body-wrapper']}>
+    <Header></Header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">home</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route exact path="/" render={() => <p>Home</p>} />
+        </div>
+      </Router>
+    </section>
+  );
+};
 
 ReactDom.render(<App />, document.getElementById('app'));
