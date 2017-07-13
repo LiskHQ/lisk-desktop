da1be883b87ebe9428c22caac75189a4eda2fc2b
yashar <darkyashi@gmail.com>
2017-07-13 16:36:02 +0430

    Add Bignumber.js to formattedNumber component and use it in that

--- src/components/formattedNumber/index.js
+++ src/components/formattedNumber/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import BigNumber from 'bignumber.js';
+
+/**
+ *
+ * @param {*} num - it is a number that we want to normalize it
+ * @return {string} - normalized version of input number
+ */
+const normalize = value => new BigNumber(value || 0).dividedBy(new BigNumber(10).pow(8)).toFixed();
 /**
  *
  * @param {*} num - it is a number that we want to format it as formatted number
  * @return {string} - formatted version of input number
  */
 const formatNumber = (num) => {
-  num = parseFloat(num);
+  num = parseFloat(normalize(num));
   const sign = num < 0 ? '-' : '';
   const absVal = String(parseInt(num = Math.abs(Number(num) || 0), 10));
   let remaining = absVal.length;
--- src/components/formattedNumber/index.test.js
+++ src/components/formattedNumber/index.test.js
@@ -4,22 +4,23 @@ import { shallow } from 'enzyme';
 import FormattedNumber from '../formattedNumber/index';
 
 describe('<FormattedNumber />', () => {
+  const normalizeNumber = 100000000;
   it('expect "12932689.645" to be equal "12,932,689.645"', () => {
-    const inputValue = '12932689.645';
+    const inputValue = '12932689.645' * normalizeNumber;
     const expectedValue = '12,932,689.645';
     const wrapper = shallow(<FormattedNumber val={inputValue} />);
     expect(wrapper.find('span').text()).to.be.equal(expectedValue);
   });
 
   it('expect "2500" to be equal "2,500"', () => {
-    const inputValue = '2500';
+    const inputValue = '2500' * normalizeNumber;
     const expectedValue = '2,500';
     const wrapper = shallow(<FormattedNumber val={inputValue} />);
     expect(wrapper.find('span').text()).to.be.equal(expectedValue);
   });
 
   it('expect "-78945" to be equal "-78,945"', () => {
-    const inputValue = '78945';
+    const inputValue = '78945' * normalizeNumber;
     const expectedValue = '78,945';
     const wrapper = shallow(<FormattedNumber val={inputValue} />);
     expect(wrapper.find('span').text()).to.be.equal(expectedValue);
@@ -33,7 +34,7 @@ describe('<FormattedNumber />', () => {
   });
 
   it('expect "500.12345678" to be equal "500.12345678"', () => {
-    const inputValue = '500.12345678';
+    const inputValue = '500.12345678' * normalizeNumber;
     const expectedValue = '500.12345678';
     const wrapper = shallow(<FormattedNumber val={inputValue} />);
     expect(wrapper.find('span').text()).to.be.equal(expectedValue);
