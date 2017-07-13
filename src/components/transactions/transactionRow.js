e8a1f06ff91529a480e096f031b9d5e2e790008a
yashar <darkyashi@gmail.com>
2017-07-13 20:05:29 +0430

    Add amount and status component to transactionRow component

--- src/components/transactions/transactionRow.js
+++ src/components/transactions/transactionRow.js
@@ -3,28 +3,30 @@ import FormattedNumber from '../formattedNumber';
 import { TooltipTime, TooltipWrapper } from '../timestamp';
 import TransactionType from './transactionType';
 import styles from './transactions.css';
+import Status from './status';
+import Amount from './amount';
 
-const TransactionRow = ({ tableStyle, value }) => (
+const TransactionRow = props => (
   <tr>
-    <td className={`${tableStyle.rowCell} ${styles.centerText}`}>
-        <TooltipTime label={value.timestamp}></TooltipTime>
+    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <TooltipTime label={props.value.timestamp}></TooltipTime>
     </td>
-    <td className={`${tableStyle.rowCell} ${styles.centerText}`}>
-        <TooltipWrapper tooltip={`${value.confirmations} confirmations`}>{value.id}</TooltipWrapper>
+    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <TooltipWrapper tooltip={`${props.value.confirmations} confirmations`}>{props.value.id}</TooltipWrapper>
     </td>
-    <td className={`${tableStyle.rowCell} ${styles.centerText}`}>
-      <TransactionType {...value}></TransactionType>
+    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+      <TransactionType {...props.value}></TransactionType>
     </td>
-    <td className={`${tableStyle.rowCell} ${styles.centerText}`}>
-      {value.type}
+    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+      <Status {...props}></Status>
     </td>
-    <td className={`${tableStyle.rowCell} ${styles.centerText}`}>
-        <FormattedNumber val={value.amount}></FormattedNumber>
+    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+      <Amount {...props}></Amount>
     </td>
-    <td className={`${tableStyle.rowCell} ${styles.centerText}`}>
-        <span className={styles.grayButton}>
-          <FormattedNumber val={value.fee}></FormattedNumber>
-        </span>
+    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+      <span className={styles.grayButton}>
+        <FormattedNumber val={props.value.fee}></FormattedNumber>
+      </span>
     </td>
   </tr>
 );
