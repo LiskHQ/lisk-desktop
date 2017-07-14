6bcc0363c39efc2b65435cc9bd09133f1a967241
reyraa <ali@lightcurve.io>
2017-07-14 16:18:14 +0200

    - Change the approach of passing store. - Add Api calls and update functionily

--- src/components/account/index.js
+++ src/components/account/index.js
@@ -1,54 +1,91 @@
 import React from 'react';
-import grid from '../../../node_modules/flexboxgrid/dist/flexboxgrid.css';
+import { connect } from 'react-redux';
+import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import styles from './account.css';
 import Address from './address';
 import FormattedNumber from '../formattedNumber';
+import { getAccountStatus } from '../../utils/api/account';
+import { activePeerUpdate } from '../../actions/peers';
 
 /**
  * Contains some of the important and basic information about the account
  *
  * @param {object} props - include properties of component
  */
-const Account = (props) => {
-  const status = props.peers.online ?
-    <i className="material-icons online">check</i>
-    : <i className="material-icons offline">error</i>;
-  return (
-    <section className={`${grid.row} ${styles.wrapper}`}>
-      <article className={grid['col-xs-4']}>
-        <Address {...props.account}></Address>
-      </article>
-      <article className={grid['col-xs-4']}>
-        <div className="box">
-          <h3 className={styles.title}>Peer</h3>
-          <div className={styles['value-wrapper']}>
-            <span id="accountStatus" className="status">
-              {status}
-            </span>
-            <p className="inner primary">
-              {props.peers.active.options.name}
-            </p>
-            <p className="inner secondary">
-              {props.peers.active.currentPeer}
-              <span> : {props.peers.active.port}</span>
-            </p>
+class AccountComponent extends React.Component {
+  componentDidMount() {
+    this.update();
+    document.addEventListener('beat', this.update.bind(this));
+  }
+
+  update() {
+    const { onActivePeerUpdated } = this.props;
+    return getAccountStatus(this.props.peers.data).then(() => {
+      onActivePeerUpdated({ online: true });
+    }).catch(() => {
+      onActivePeerUpdated({ online: false });
+    });
+  }
+
+  render() {
+    const status = (this.props.peers.status && this.props.peers.status.online) ?
+      <i className="material-icons online">check</i>
+      : <i className="material-icons offline">error</i>;
+    return (
+      <section className={`${grid.row} ${styles.wrapper}`}>
+        <article className={grid['col-xs-4']}>
+          <Address {...this.props.account}></Address>
+        </article>
+        <article className={grid['col-xs-4']}>
+          <div className="box">
+            <h3 className={styles.title}>Peer</h3>
+            <div className={styles['value-wrapper']}>
+              <span id="accountStatus" className="status">
+                {status}
+              </span>
+              <p className="inner primary">
+                {this.props.peers.data.options.name}
+              </p>
+              <p className="inner secondary">
+                {this.props.peers.data.currentPeer}
+                <span> : {this.props.peers.data.port}</span>
+              </p>
+            </div>
           </div>
-        </div>
-      </article>
-      <article className={grid['col-xs-4']}>
-        <div className="box">
-          <h3 className={styles.title}>Balance</h3>
-          <div className={styles['value-wrapper']}>
-            <p className="inner primary full hasTip">
-              <FormattedNumber val={props.balance}></FormattedNumber> Lsk
-            </p>
-            <p className="inner secondary tooltip">
-              Click to send all funds
-            </p>
+        </article>
+        <article className={grid['col-xs-4']}>
+          <div className="box">
+            <h3 className={styles.title}>Balance</h3>
+            <div className={styles['value-wrapper']}>
+              <p className="inner primary full hasTip">
+                <FormattedNumber val={this.props.balance}></FormattedNumber> Lsk
+              </p>
+              <p className="inner secondary tooltip">
+                Click to send all funds
+              </p>
+            </div>
           </div>
-        </div>
-      </article>
-    </section>
-  );
-};
+        </article>
+      </section>
+    );
+  }
+}
+
+/**
+ * Passing state
+ */
+const mapStateToProps = state => ({
+  peers: state.peers,
+  account: state.account,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onActivePeerUpdated: data => dispatch(activePeerUpdate(data)),
+});
+
+const Account = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AccountComponent);
+
 export default Account;
--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, browserHistory, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from '../header';
 import Account from '../account';
 import Login from '../login';
@@ -11,6 +11,8 @@ import Metronome from '../../utils/metronome';
 import { setActivePeer } from '../../utils/api/peers';
 import { accountUpdated } from '../../actions/account';
 import Dialog from '../dialog';
+// temporary
+import { getAccount } from '../../utils/api/account';
 
 // temporarily hard-coded
 const network = {
@@ -19,25 +21,6 @@ const network = {
   nethash: '198f2b61a8eb95fbeed58b8216780b68f697f26b849acf00c8c93bb9b24f783d',
 };
 
-const accountInfo = {
-  account: {
-    isDelegate: false,
-    address: '16313739661670634666L',
-    username: 'lisk-nano',
-  },
-  address: '16313739661670634666L',
-  peers: {
-    online: true,
-    active: {
-      currentPeer: 'localhost',
-      port: 4000,
-      options: {
-        name: 'Custom Node',
-      },
-    },
-  },
-  balance: '99992689.6',
-};
 
 const App = (props) => {
   // start dispatching sync ticks
@@ -45,18 +28,19 @@ const App = (props) => {
   metronome.init();
 
   // temporarily
-  props.store.dispatch(accountUpdated(accountInfo));
   setActivePeer(props.store, network);
+  getAccount(props.store.getState().peers.data, '16313739661670634666L').then((result) => {
+    props.store.dispatch(accountUpdated(result));
+  });
 
-  const state = props.store.getState();
   return (
-    <Router history={browserHistory}>
+    <Router>
       <section className={styles['body-wrapper']}>
         <Header />
         <main className=''>
           <Route path="/main" render={({ match }) => (
             <main className=''>
-              <Account {...state.account}></Account>
+              <Account></Account>
               <Route path={`${match.url}/transactions`} component={Transactions}/>
               <Route path={`${match.url}/voting`} component={Voting}/>
               <Route path={`${match.url}/forging`} component={Forging}/>
