87394298139d9186a62d9ff3d09192ab3deb8de2
reyraa <ali@lightcurve.io>
2017-07-14 16:16:06 +0200

    Add peer update action to constants

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -2,6 +2,7 @@ const actionTypes = {
   accountUpdated: 'ACCOUNT_UPDATED',
   accountLoggedOut: 'ACCOUNT_LOGGED_OUT',
   activePeerSet: 'ACTIVE_PEER_SET',
+  activePeerUpdate: 'ACTIVE_PEER_UPDATE',
   activePeerReset: 'ACTIVE_PEER_RESET',
   dialogDisplayed: 'DIALOG_DISPLAYED',
   dialogHidden: 'DIALOG_HIDDEN',
