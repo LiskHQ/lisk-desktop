0ae9392bce9475396afb5cdc434c2a1dadb6089e
willclarktech <willclarktech@users.noreply.github.com>
2017-07-14 17:54:53 +0200

    Add basic stories for FormattedNumber, Account, Address

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,53 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import FormattedNumber from '../src/components/formattedNumber';
+import Account from '../src/components/account/accountComponent';
+import Address from '../src/components/account/address';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('FormattedNumber', module)
+  .add('with val', () => (
+    <FormattedNumber val="-3.1415926535" />
+  ));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Account', module)
+  .add('delegate', () => (
+    <Account
+      peers={{
+        status: {
+          online: true,
+        },
+        data: {
+          options: {
+            name: 'testy',
+          },
+          currentPeer: 'testpeer',
+          port: 8000,
+        },
+      }}
+      account={{
+        isDelegate: true,
+        username: 'testy',
+        address: '9396639332432599292L',
+      }}
+      balance="3.1415926535"
+    />
+  ));
+
+storiesOf('Address', module)
+  .add('delegate', () => (
+    <Address
+      isDelegate={true}
+      username="testy"
+      address="9396639332432599292L"
+    />
+  ))
+  .add('non-delegate', () => (
+    <Address
+      isDelegate={false}
+      address="9396639332432599292L"
+    />
+  ));
