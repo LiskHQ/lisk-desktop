7374dde4363a866a886571e8e7cd79297c1658ec
reyraa <ali@lightcurve.io>
2017-07-19 13:04:16 +0200

    Move activePeerSet and activePeerReset to actions

--- src/utils/api/peers.js
+++ src/utils/api/peers.js
@@ -1,36 +1,3 @@
-import Lisk from 'lisk-js';
-import store from '../../store';
-import { activePeerSet, activePeerReset } from '../../actions/peers';
-
-export const resetActivePeer = () => {
-  store.dispatch(activePeerReset());
-};
-
-export const setActivePeer = (network) => {
-  const addHttp = (url) => {
-    const reg = /^(?:f|ht)tps?:\/\//i;
-    return reg.test(url) ? url : `http://${url}`;
-  };
-
-  // this.network = network;
-  let config = { };
-  if (network) {
-    config = network;
-    if (network.address) {
-      const normalizedUrl = new URL(addHttp(network.address));
-
-      config.node = normalizedUrl.hostname;
-      config.port = normalizedUrl.port;
-      config.ssl = normalizedUrl.protocol === 'https';
-    }
-    if (config.testnet === undefined && config.port !== undefined) {
-      config.testnet = config.port === '7000';
-    }
-  }
-
-  return store.dispatch(activePeerSet(Lisk.api(config)));
-};
-
 export const requestToActivePeer = (activePeer, path, urlParams) =>
   new Promise((resolve, reject) => {
     activePeer.sendRequest(path, urlParams, (data) => {
