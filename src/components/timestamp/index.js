463fb0250c2ab93f1a61c953873be996b757044e
yashar <darkyashi@gmail.com>
2017-07-19 17:20:20 +0430

    Remove loadash.omit and replace it with some native codes

--- src/components/timestamp/index.js
+++ src/components/timestamp/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import moment from 'moment';
 import Tooltip from 'react-toolbox/lib/tooltip';
-import Link from 'react-toolbox/lib/link'; // eslint-disable-line
-import omit from 'lodash/omit';
+// import omit from 'lodash/omit';
 import theme from 'react-toolbox/lib/tooltip/theme.css';
 
 const _fix = value => new Date((((Date.UTC(2016, 4, 24, 17, 0, 0, 0) / 1000) + value) * 1000));
+/**
+ * Remove an array of keys from object
+ * @param {object} obj - an object that we want to remove some properties from that
+ * @param {array} arr - list of name of properties that we want to remove them
+ * @return {object} list - an object that hasn't any of items in arr
+ */
+const _remove = (obj, arr) => {
+  let list = []; // eslint-disable-line
+  const temp = Object.keys(obj)
+    .filter(key => !arr.includes(key));
+  temp.forEach((item) => {
+    list[item] = obj[item];
+  });
+  return list;
+};
 
 const Div = (props) => {
-  const rest = omit(props,
-    ['theme', 'tooltip', 'tooltipDelay', 'tooltipHideOnClick']);
+  const rest = _remove(props, ['theme', 'tooltip', 'tooltipDelay', 'tooltipHideOnClick']);
   return (<div {...rest} />);
 };
 
 /**
  * This wrapper add theme style and default delay, and disable tooltip when `tooltip` is empty.
  * @param props
- * @constructor
  */
 export const TooltipWrapper = (props) => {
   const Tip = Tooltip(Div); // eslint-disable-line
