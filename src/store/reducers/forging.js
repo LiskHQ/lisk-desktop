a0bee86ebd1ea595bd55047b2663fdb5001f3edb
Vit Stanislav <vit@lightcurve.io>
2017-07-19 11:19:03 +0200

    Fix forging reducers to append and prepend forgedBlocks

--- src/store/reducers/forging.js
+++ src/store/reducers/forging.js
@@ -6,10 +6,21 @@ import actionTypes from '../../constants/actions';
  * @param {Object} action
  */
 const forging = (state = { forgedBlocks: [], statistics: {} }, action) => {
+  let startTimesamp;
+  let endTimesamp;
+
   switch (action.type) {
     case actionTypes.forgedBlocksUpdated:
+      startTimesamp = state.forgedBlocks.length ? state.forgedBlocks[0].timestamp : 0;
+      endTimesamp = state.forgedBlocks.length ?
+        state.forgedBlocks[state.forgedBlocks.length - 1].timestamp :
+        0;
       return Object.assign({}, state, {
-        forgedBlocks: (state.forgedBlocks || []).concat(action.data),
+        forgedBlocks: [].concat(
+          action.data.filter(block => block.timestamp > startTimesamp),
+          state.forgedBlocks,
+          action.data.filter(block => block.timestamp < endTimesamp),
+        ),
       });
     case actionTypes.forgingStatsUpdated:
       return Object.assign({}, state, {
