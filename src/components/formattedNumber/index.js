bc941c465b1ad8ca5271d6949ef255ddf841bd4d
yashar <darkyashi@gmail.com>
2017-07-19 17:29:17 +0430

    Fix a minor bug in formattedNumbers component

--- src/components/formattedNumber/index.js
+++ src/components/formattedNumber/index.js
@@ -13,16 +13,16 @@ const normalize = value => new BigNumber(value || 0).dividedBy(new BigNumber(10)
  * @return {string} - formatted version of input number
  */
 const formatNumber = (num) => {
-  num = parseFloat(normalize(num));
-  const sign = num < 0 ? '-' : '';
-  const absVal = String(parseInt(num = Math.abs(Number(num) || 0), 10));
+  let normalizeNum = parseFloat(normalize(num));
+  const sign = normalizeNum < 0 ? '-' : '';
+  const absVal = String(parseInt(normalizeNum = Math.abs(Number(normalizeNum) || 0), 10));
   let remaining = absVal.length;
   remaining = (remaining) > 3 ? remaining % 3 : 0;
   const intPart = sign + (remaining ? `${absVal.substr(0, remaining)},` : '') +
      absVal.substr(remaining).replace(/(\d{3})(?=\d)/g, '$1,');
-  const floatPart = num.toString().split('.')[1];
-  num = floatPart ? `${intPart}.${floatPart || ''}` : intPart;
-  return num;
+  const floatPart = normalizeNum.toString().split('.')[1];
+  normalizeNum = floatPart ? `${intPart}.${floatPart || ''}` : intPart;
+  return normalizeNum;
 };
 const FormattedNumber = (props) => {
   const formatedNumber = formatNumber(props.val);
