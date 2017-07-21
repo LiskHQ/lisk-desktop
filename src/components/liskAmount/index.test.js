71fa3cedb3db1f6b8ef07103271f4bc0436f194d
Vit Stanislav <vit@lightcurve.io>
2017-07-21 14:20:57 +0200

    Unify descriptions of LiskAmount test

--- src/components/liskAmount/index.test.js
+++ src/components/liskAmount/index.test.js
@@ -3,9 +3,9 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import LiskAmount from './';
 
-describe('<LiskAmount />', () => {
+describe('LiskAmount', () => {
   const normalizeNumber = 100000000;
-  it('should render "12932689.645" as "12,932,689.645"', () => {
+  it('should normalize "12932689.645" as "12,932,689.645"', () => {
     const inputValue = '12932689.645' * normalizeNumber;
     const expectedValue = '12,932,689.645';
     const wrapper = mount(<LiskAmount val={inputValue} />);
