d769567c2d0ebfcc667c50e33f76ed96700749b0
Aleksey Popov <alepooop@gmail.com>
2017-07-25 00:01:10 +0300

    refactor `<App />` component tests

--- src/components/app/index.test.js
+++ src/components/app/index.test.js
@@ -3,12 +3,14 @@ import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import { expect } from 'chai';
-import store from '../../store';
+import configureStore from 'redux-mock-store';
 import App from './';
 import Login from '../login';
 import Transactions from '../transactions';
 import Voting from '../voting';
 import Forging from '../forging';
+
+const fakeStore = configureStore();
 
 const addRouter = Component => (props, path) =>
     mount(
@@ -19,18 +21,58 @@ const addRouter = Component => (props, path) =>
       </Provider>,
     );
 
-const routesComponent = [
+const publicComponent = [
   { route: '/', component: Login },
-  { route: '/main/transactions', component: Transactions },
-  { route: '/main/voting', component: Voting },
-  { route: '/main/forging', component: Forging },
+];
+
+const privateComponent = [
+  { route: '/transactions', component: Transactions },
+  { route: '/voting', component: Voting },
+  { route: '/forging', component: Forging },
 ];
 
 describe('App', () => {
+  const navigateTo = addRouter(App);
   describe('renders correct routes', () => {
-    const navigateTo = addRouter(App);
-    routesComponent.forEach(({ route, component }) => {
-      it.skip(`should render ${component.name} component at "${route}" route`, () => {
+    const store = fakeStore({
+      account: {},
+      dialog: {},
+      peers: {},
+    });
+    publicComponent.forEach(({ route, component }) => {
+      it(`should render ${component.name} component at "${route}" route`, () => {
+        const wrapper = navigateTo({ store }, [route]);
+        expect(wrapper.find(component).exists()).to.be.equal(true);
+      });
+    });
+
+    privateComponent.forEach(({ route, component }) => {
+      it(`should redirect from ${component.name} component if user is not authenticated`, () => {
+        const wrapper = navigateTo({ store }, [route]);
+        expect(wrapper.find(component).exists()).to.be.equal(false);
+        expect(wrapper.find(Login).exists()).to.be.equal(true);
+      });
+    });
+  });
+  describe('allow to render private components after logged in', () => {
+    const store = fakeStore({
+      account: {
+        publicKey: '000',
+      },
+      dialog: {},
+      peers: {
+        status: {
+          online: true,
+        },
+        data: {
+          options: {
+            name: 'Test',
+          },
+        },
+      },
+    });
+    privateComponent.forEach(({ route, component }) => {
+      it(`should reder ${component.name} component at "${route}" route if user is authenticated`, () => {
         const wrapper = navigateTo({ store }, [route]);
         expect(wrapper.find(component).exists()).to.be.equal(true);
       });
