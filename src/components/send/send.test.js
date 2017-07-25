33fa7ee5ef1004d4e4fa2fe922e0686e0906e62b
Vit Stanislav <vit@lightcurve.io>
2017-07-25 16:32:08 +0200

    Add unit tests for send component

---
+++ src/components/send/send.test.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import chai, { expect } from 'chai';
+import { mount } from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+import sinonChai from 'sinon-chai';
+import sinon from 'sinon';
+import Send from './send';
+import * as accountApi from '../../utils/api/account';
+
+
+chai.use(sinonChai);
+chai.use(chaiEnzyme()); // Note the invocation at the end
+describe('Send', () => {
+  let wrapper;
+  let accountApiMock;
+
+  beforeEach(() => {
+    accountApiMock = sinon.mock(accountApi);
+    const props = {
+      activePeer: {},
+      account: {
+        balance: 1000e8,
+      },
+      closeDialog: () => {},
+    };
+    wrapper = mount(<Send {...props} />);
+  });
+
+  afterEach(() => {
+    accountApiMock.verify();
+    accountApiMock.restore();
+  });
+
+  it('renders two Input components', () => {
+    expect(wrapper.find('Input')).to.have.length(2);
+  });
+
+  it('renders two Button components', () => {
+    expect(wrapper.find('Button')).to.have.length(2);
+  });
+
+  it('accepts valid amount', () => {
+    wrapper.find('.amount input').simulate('change', { target: { value: '120.25' } });
+    expect(wrapper.find('.amount').text()).to.not.contain('Invalid LSK amount');
+  });
+
+  it('recognizes invalid amount', () => {
+    wrapper.find('.amount input').simulate('change', { target: { value: '120 INVALID' } });
+    expect(wrapper.find('.amount').text()).to.contain('Invalid LSK amount');
+  });
+
+  it('recognizes too high amount', () => {
+    wrapper.find('.amount input').simulate('change', { target: { value: '12000' } });
+    expect(wrapper.find('.amount').text()).to.contain('Insufficient funds');
+  });
+
+  it('accepts valid recipient', () => {
+    wrapper.find('.recipient input').simulate('change', { target: { value: '11004588490103196952L' } });
+    expect(wrapper.find('.recipient').text()).to.not.contain('Invalid address');
+  });
+
+  it('recognizes invalid recipient', () => {
+    wrapper.find('.recipient input').simulate('change', { target: { value: '11004588490103196952' } });
+    expect(wrapper.find('.recipient').text()).to.contain('Invalid address');
+  });
+
+  it('allows to set maximum amount', () => {
+    wrapper.find('.transaction-amount').simulate('click');
+    wrapper.find('.send-maximum-amount').simulate('click');
+    expect(wrapper.find('.amount input').props().value).to.equal('999.9');
+  });
+
+  it('allows to send a transaction and handles success', () => {
+    accountApiMock.expects('send').resolves({ success: true });
+
+    wrapper.find('.amount input').simulate('change', { target: { value: '120.25' } });
+    wrapper.find('.recipient input').simulate('change', { target: { value: '11004588490103196952L' } });
+    wrapper.find('.send-button').simulate('click');
+  });
+
+  it('allows to send a transaction and handles error response with message', () => {
+    accountApiMock.expects('send').rejects({ message: 'Some server-side error' });
+    wrapper.find('.amount input').simulate('change', { target: { value: '120.25' } });
+    wrapper.find('.recipient input').simulate('change', { target: { value: '11004588490103196952L' } });
+    wrapper.find('.send-button').simulate('click');
+  });
+
+  it('allows to send a transaction and handles error response without message', () => {
+    accountApiMock.expects('send').rejects({ success: false });
+    wrapper.find('.amount input').simulate('change', { target: { value: '120.25' } });
+    wrapper.find('.recipient input').simulate('change', { target: { value: '11004588490103196952L' } });
+    wrapper.find('.send-button').simulate('click');
+  });
+});
