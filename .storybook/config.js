5cdfc4c62be1513e2cc64d215a1d38335e08876f
Vit Stanislav <vit@lightcurve.io>
2017-07-26 16:55:30 +0200

    Add toaster to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,7 @@ function loadStories() {
   require('../src/components/account/stories');
   require('../src/components/dialog/stories');
   require('../src/components/formattedNumber/stories');
+  require('../src/components/toaster/stories');
 }
 
 configure(loadStories, module);
