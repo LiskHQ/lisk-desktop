1df909102fb58b08f26bc593ed266ae082fb3e4f
Vit Stanislav <vit@lightcurve.io>
2017-07-26 11:01:14 +0200

    Use fromRawLsk in liskAmount

--- src/components/liskAmount/index.js
+++ src/components/liskAmount/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import BigNumber from 'bignumber.js';
+import { fromRawLsk } from '../../utils/lsk';
 import FormattedNumber from '../formattedNumber';
 
-/**
- *
- * @param {*} num - it is a number that we want to normalize it
- * @return {string} - normalized version of input number
- */
-const normalize = value => new BigNumber(value || 0).dividedBy(new BigNumber(10).pow(8)).toFixed();
-
-const LiskValue = props => (<FormattedNumber val={parseFloat(normalize(props.val))} />);
+const LiskValue = props => (<FormattedNumber val={parseFloat(fromRawLsk(props.val))} />);
 
 export default LiskValue;
 
