2d6a23f6181bc1629af38fbc1a2ec47c1cf02e84
yashar <darkyashi@gmail.com>
2017-07-29 22:16:21 +0430

    Add voting reducer to main reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,4 +2,4 @@ export { default as account } from './account';
 export { default as peers } from './peers';
 export { default as dialog } from './dialog';
 export { default as forging } from './forging';
-
+export { default as voting } from './voting';
