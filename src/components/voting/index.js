edf34d95e460c5e7ca59646a53bd2593e50f0e42
yashar <darkyashi@gmail.com>
2017-07-29 22:14:51 +0430

    Add mapDispatchToProps to voting component

--- src/components/voting/index.js
+++ src/components/voting/index.js
@@ -1,9 +1,23 @@
 import { connect } from 'react-redux';
 import VotingComponent from './votingComponent';
+import {
+          addToVotedList,
+          removeFromVotedList,
+          addToUnvotedList,
+          removeFromUnvotedList,
+       } from '../../actions/voting';
 
 const mapStateToProps = state => ({
   address: state.account.address,
   activePeer: state.peers.data,
+  votedList: state.voting.votedList,
+  unvotedList: state.voting.unvotedList,
+});
+const mapDispatchToProps = dispatch => ({
+  addToVoted: data => dispatch(addToVotedList(data)),
+  removeFromVoted: data => dispatch(removeFromVotedList(data)),
+  addToUnvoted: data => dispatch(addToUnvotedList(data)),
+  removeFromUnvoted: data => dispatch(removeFromUnvotedList(data)),
 });
 
-export default connect(mapStateToProps)(VotingComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(VotingComponent);
