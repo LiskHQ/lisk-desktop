e53f40c81637f9bdf4b435d6567c7449741f1ed3
yashar <darkyashi@gmail.com>
2017-08-02 00:09:22 +0430

    Fix some bugs in voting reducer

--- src/store/reducers/voting.js
+++ src/store/reducers/voting.js
@@ -1,8 +1,19 @@
 import actionTypes from '../../constants/actions';
 
-const _removeFromList = (list, item) => {
+const removeFromList = (list, item) => {
   const address = item.address;
   return list.filter(delegate => delegate.address !== address);
+};
+const findItemInList = (list, item) => {
+  const username = item.username;
+  let idx = -1;
+  list.forEach((delegate, index) => {
+    if (delegate.address === username) {
+      idx = index;
+    }
+  });
+  console.log(idx);
+  return idx;
 };
 /**
  *
@@ -12,20 +23,31 @@ const _removeFromList = (list, item) => {
 const voting = (state = { votedList: [], unvotedList: [] }, action) => {
   switch (action.type) {
     case actionTypes.addToVotedList:
+      if (findItemInList(state.votedList, action.data) > -1) {
+        console.log(action.data);
+        return Object.assign({}, state, {
+          votedList: [...removeFromList(state.votedList, action.data), action.data],
+        });
+      }
       return Object.assign({}, state, {
         votedList: [...state.votedList, action.data],
       });
     case actionTypes.removeFromVotedList:
       return Object.assign({}, state, {
-        votedList: _removeFromList(state.votedList, action.data),
+        votedList: removeFromList(state.votedList, action.data),
       });
     case actionTypes.addToUnvotedList:
+      if (findItemInList(state.unvotedList, action.data) > -1) {
+        return Object.assign({}, state, {
+          unvotedList: [...removeFromList(state.unvotedList, action.data), action.data],
+        });
+      }
       return Object.assign({}, state, {
         unvotedList: [...state.unvotedList, action.data],
       });
     case actionTypes.removeFromUnvotedList:
       return Object.assign({}, state, {
-        unvotedList: _removeFromList(state.unvotedList, action.data),
+        unvotedList: removeFromList(state.unvotedList, action.data),
       });
     default:
       return state;
