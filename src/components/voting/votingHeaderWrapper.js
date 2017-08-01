5d4281d978f1d9533b009e051184d8b34d0163ab
yashar <darkyashi@gmail.com>
2017-08-02 00:13:40 +0430

    Add git addToUnvoted dispacher to votingHeaderWrapper

--- src/components/voting/votingHeaderWrapper.js
+++ src/components/voting/votingHeaderWrapper.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import VotingHeader from './votingHeader';
 import { dialogDisplayed } from '../../actions/dialog';
+import { addToUnvotedList } from '../../actions/voting';
 
 
 const mapDispatchToProps = dispatch => ({
   setActiveDialog: data => dispatch(dialogDisplayed(data)),
+  addToUnvoted: data => dispatch(addToUnvotedList(data)),
 });
 const mapStateToProps = state => ({
   votedList: state.voting.votedList,
