48bb7d5550a4ce02908428d54183162a2687f5f5
yashar <darkyashi@gmail.com>
2017-08-02 14:37:25 +0430

    Optimize voting actions

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -8,10 +8,8 @@ const actionTypes = {
   dialogHidden: 'DIALOG_HIDDEN',
   forgedBlocksUpdated: 'FORGED_BLOCKS_UPDATED',
   forgingStatsUpdated: 'FORGING_STATS_UPDATED',
-  addToVotedList: 'ADD_TO_VOTED_LIST',
-  removeFromVotedList: 'REMOVE_FROM_VOTED_LIST',
-  addToUnvotedList: 'ADD_TO_UNVOTED_LIST',
-  removeFromUnvotedList: 'REMOVE_FROM_UNVOTED_LIST',
+  addToVoteList: 'ADD_TO_VOTE_LIST',
+  removeFromVoteList: 'REMOVE_FROM_VOTE_LIST',
 };
 
 export default actionTypes;
