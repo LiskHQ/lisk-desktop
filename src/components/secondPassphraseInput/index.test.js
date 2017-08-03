0a06d5a584fa8b3fbe8e109216eed6acabed837a
Vit Stanislav <vit@lightcurve.io>
2017-08-03 14:23:14 +0200

    Add unit tests for secondPassphraseInput

---
+++ src/components/secondPassphraseInput/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import SecondPassphraseInputContainer from './';
+import store from '../../store';
+
+chai.use(sinonChai);
+
+
+describe('SecondPassphraseInputContainer', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<Provider store={store}><SecondPassphraseInputContainer /></Provider>);
+  });
+
+  it('should render SecondPassphraseInput', () => {
+    expect(wrapper.find('SecondPassphraseInput')).to.have.lengthOf(1);
+  });
+});
---
+++ src/components/secondPassphraseInput/secondPassphraseInput.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+import SecondPassphraseInput from './secondPassphraseInput';
+
+chai.use(sinonChai);
+
+
+describe('SecondPassphraseInput', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      onChange: sinon.spy(),
+      onError: sinon.spy(),
+    }
+  });
+
+  it('should render Input if props.hasSecondPassphrase', () => {
+    props.hasSecondPassphrase = true;
+    wrapper = mount(<SecondPassphraseInput {...props} />);
+    expect(wrapper.find('Input')).to.have.lengthOf(1);
+  });
+
+  it('should render null if !props.hasSecondPassphrase', () => {
+    props.hasSecondPassphrase = false;
+    wrapper = mount(<SecondPassphraseInput {...props} />);
+    expect(wrapper.html()).to.equal('');
+  });
+
+  it('should render null if !props.hasSecondPassphrase', () => {
+    props.hasSecondPassphrase = false;
+    wrapper = mount(<SecondPassphraseInput {...props} />);
+    expect(wrapper.html()).to.equal('');
+  });
+    wrapper.find('.amount input').simulate('change', { target: { value: '0' } });
+});
