044c2f5eda3c24a5b99ba05f6d87250ec9402d62
reyraa <ali@lightcurve.io>
2017-08-03 13:39:00 +0200

    Create required methods to derive public key and address from passphrase

--- src/utils/api/account.test.js
+++ src/utils/api/account.test.js
@@ -2,7 +2,8 @@ import chai, { expect } from 'chai';
 import { mock } from 'sinon';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from 'chai-as-promised';
-import { getAccount, setSecondSecret, send, transactions } from './account';
+import { getAccount, setSecondSecret, send, transactions,
+  extractPublicKey, extractAddress } from './account';
 import { activePeerSet } from '../../actions/peers';
 
 chai.use(chaiAsPromised);
@@ -83,6 +84,28 @@ describe('Utils: Account', () => {
     it('should return a promise', () => {
       const promise = transactions();
       expect(typeof promise.then).to.be.equal('function');
+    });
+  });
+
+  describe('extractPublicKey', () => {
+    it('should return a Hex string from any given string', () => {
+      const passphrase = 'field organ country moon fancy glare pencil combine derive fringe security pave';
+      const publicKey = 'a89751689c446067cc2107ec2690f612eb47b5939d5570d0d54b81eafaf328de';
+      expect(extractPublicKey(passphrase)).to.be.equal(publicKey);
+    });
+  });
+
+  describe('extractAddress', () => {
+    it('should return the account address from given passphrase', () => {
+      const passphrase = 'field organ country moon fancy glare pencil combine derive fringe security pave';
+      const derivedAddress = '440670704090200331L';
+      expect(extractAddress(passphrase)).to.be.equal(derivedAddress);
+    });
+
+    it('should return the account address from given public key', () => {
+      const publicKey = 'a89751689c446067cc2107ec2690f612eb47b5939d5570d0d54b81eafaf328de';
+      const derivedAddress = '440670704090200331L';
+      expect(extractAddress(publicKey)).to.be.equal(derivedAddress);
     });
   });
 });
