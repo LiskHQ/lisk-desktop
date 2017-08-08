e84184b3fd3bc75874f21314622b3c36e5b01e78
Vit Stanislav <vit@lightcurve.io>
2017-08-08 15:08:59 +0200

    Remove inline styles from actionBar

--- src/components/actionBar/index.js
+++ src/components/actionBar/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Button from 'react-toolbox/lib/button';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import styles from './actionBar.css';
 
 const ActionBar = props => (
-  <section style={{ margin: '0' }}
-    className={`${grid.row} ${grid['between-xs']}`} >
+  <section className={`${grid.row} ${grid['between-xs']} ${styles.wrapper}`} >
     <Button label={props.secondaryButton.label || 'Cancel'}
       className={props.secondaryButton.className || 'cancel-button'}
       onClick={props.secondaryButton.onClick} />
