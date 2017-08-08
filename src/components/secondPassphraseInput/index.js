2db5f35b7a2f90d11248abdf59168354ff9a3069
Vit Stanislav <vit@lightcurve.io>
2017-08-08 15:11:50 +0200

    Add unit tests for secondPassphraseInput connect props

--- src/components/secondPassphraseInput/index.js
+++ src/components/secondPassphraseInput/index.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import SecondPassphraseInput from './secondPassphraseInput';
 
 const mapStateToProps = state => ({
-  hasSecondPassphrase: state.account.secondSignature,
+  hasSecondPassphrase: !!state.account.secondSignature,
 });
 
 export default connect(mapStateToProps)(SecondPassphraseInput);
--- src/components/secondPassphraseInput/index.test.js
+++ src/components/secondPassphraseInput/index.test.js
@@ -3,19 +3,28 @@ import chai, { expect } from 'chai';
 import sinonChai from 'sinon-chai';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import SecondPassphraseInputContainer from './index';
-import store from '../../store';
 
 chai.use(sinonChai);
 
 describe('SecondPassphraseInputContainer', () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = mount(<Provider store={store}><SecondPassphraseInputContainer /></Provider>);
+  it('should render SecondPassphraseInput with props.hasSecondPassphrase if store.account.secondSignature is truthy', () => {
+    const store = configureMockStore([])({ account: { secondSignature: 1 } });
+    wrapper = mount(<Provider store={store}>
+        <SecondPassphraseInputContainer onError={ () => {} } />
+      </Provider>);
+    expect(wrapper.find('SecondPassphraseInput')).to.have.lengthOf(1);
+    expect(wrapper.find('SecondPassphraseInput').props().hasSecondPassphrase).to.equal(true);
   });
 
-  it('should render SecondPassphraseInput', () => {
-    expect(wrapper.find('SecondPassphraseInput')).to.have.lengthOf(1);
+  it('should render SecondPassphraseInput with !props.hasSecondPassphrase if store.account.secondSignature is falsy', () => {
+    const store = configureMockStore([])({ account: { secondSignature: 0 } });
+    wrapper = mount(<Provider store={store}>
+        <SecondPassphraseInputContainer onError={ () => {} } />
+      </Provider>);
+    expect(wrapper.find('SecondPassphraseInput').props().hasSecondPassphrase).to.equal(false);
   });
 });
