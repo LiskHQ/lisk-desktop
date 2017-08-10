2f250c746d52249ce576d69b8b927eb6ae66aaff
reyraa <ali@lightcurve.io>
2017-08-10 15:17:13 +0200

    Add the new transactions to pernding tr. list

--- src/components/secondPassphrase/index.js
+++ src/components/secondPassphrase/index.js
@@ -4,6 +4,7 @@ import { MenuItem } from 'react-toolbox/lib/menu';
 import Passphrase from '../passphrase';
 import { setSecondPassphrase } from '../../utils/api/account';
 import { dialogDisplayed, successAlertDialogDisplayed } from '../../actions/dialog';
+import { transactionAdded } from '../../actions/transactions';
 import styles from './secondPassphrase.css';
 import Fees from '../../constants/fees';
 
@@ -11,8 +12,16 @@ export const SecondPassphrase = (props) => {
   const onLoginSubmission = (secondPassphrase) => {
     setSecondPassphrase(props.peers.data, secondPassphrase, props.account.publicKey,
       props.account.passphrase)
-      .then(() => {
+      .then((data) => {
         props.showSuccessAlert({ text: 'Second passphrase registration was successfully submitted. It can take several seconds before it is processed.' });
+        // add to pending transactions
+        props.addTransaction({
+          id: data.transactionId,
+          senderPublicKey: props.account.publicKey,
+          senderId: props.account.address,
+          amount: 0,
+          fee: Fees.setSecondPassphrase,
+        });
       }).catch((error) => {
         const text = (error && error.message) ? error.message : 'An error occurred while registering your second passphrase. Please try again.';
         props.showSuccessAlert({ text });
@@ -29,7 +38,7 @@ export const SecondPassphrase = (props) => {
             onPassGenerated: onLoginSubmission,
             keepModal: true,
             fee: Fees.setSecondPassphrase,
-            approveButton: 'Register',
+            confirmButton: 'Register',
             useCaseNote: 'your second passphrase will be required for all transactions sent from this account',
             securityNote: 'Losing access to this passphrase will mean no funds can be sent from this account.',
           },
@@ -48,6 +57,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   setActiveDialog: data => dispatch(dialogDisplayed(data)),
   showSuccessAlert: data => dispatch(successAlertDialogDisplayed(data)),
+  addTransaction: data => dispatch(transactionAdded(data)),
 });
 
 export default connect(
