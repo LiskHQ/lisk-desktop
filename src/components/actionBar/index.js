437243e33284b841b9a583ad48a1714b9acfc622
reyraa <ali@lightcurve.io>
2017-08-10 15:15:36 +0200

    Integrte pricedButton in actionBar

--- src/components/actionBar/index.js
+++ src/components/actionBar/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Button from 'react-toolbox/lib/button';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import PricedButton from '../pricedButton';
 import styles from './actionBar.css';
 
 const ActionBar = props => (
   <section className={`${grid.row} ${grid['between-xs']} ${styles.wrapper}`} >
-    <Button label={props.secondaryButton.label || 'Cancel'}
+    <Button
+      label={props.secondaryButton.label || 'Cancel'}
       className={props.secondaryButton.className || 'cancel-button'}
       onClick={props.secondaryButton.onClick} />
-    <Button primary={true} raised={true}
+
+    <PricedButton
+      primary={true}
+      raised={true}
       label={props.primaryButton.label}
+      fee={props.primaryButton.fee}
+      balance={props.account ? props.account.balance : 0}
       className={props.primaryButton.className || 'submit-button'}
       disabled={props.primaryButton.disabled}
       onClick={props.primaryButton.onClick}/>
