69dfd9435dce4c550476aec9b7518999d54a056d
yashar <darkyashi@gmail.com>
2017-08-10 13:35:57 +0430

    Add second passphrase field to voting confirm

--- src/components/voting/confirmVotes.js
+++ src/components/voting/confirmVotes.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import { Button } from 'react-toolbox/lib/button';
+import Input from 'react-toolbox/lib/input';
 import { vote } from '../../utils/api/delegate';
 import { alertDialogDisplayed } from '../../actions/dialog';
-import { clearVoteLists, penddingVotes } from '../../actions/voting';
-// import Alert from '../dialog/alert';
+import { clearVoteLists, pendingVotes } from '../../actions/voting';
+import InfoParagraph from '../infoParagraph';
 
+const delay = 10000;
 class ConfirmVotes extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      secondSecret: '',
+    };
+  }
   confrim() {
+    const secondSecret = this.state.secondSecret.length === 0 ? null : this.state.secondSecret;
     const text = 'Your votes were successfully  submitted. It can take several seconds before they are processed.';
     vote(
       this.props.activePeer,
@@ -16,12 +25,27 @@ class ConfirmVotes extends React.Component {
       this.props.account.publicKey,
       this.props.votedList,
       this.props.unvotedList,
-    ).then(() => this.props.clearVoteLists());
+      secondSecret,
+    ).then(() => {
+      this.props.pendingVotes();
+      setTimeout(() => {
+        this.props.clearVoteLists();
+      }, delay);
+      this.props.showSuccessAlert({
+        title: 'Success',
+        type: 'success',
+        text,
+      });
+    }); // this.props.clearVoteLists());
     // this.props.closeDialog();
-    this.props.penddingVotes();
-    this.props.showSuccessAlert({ text });
+  }
+  setSecondPass(name, value) {
+    this.setState({ ...this.state, [name]: value });
   }
   render() {
+    const secondPass = this.props.account.secondSignature === 0 ? null :
+      <Input type='text' label='Second Passphrase' name='secondSecret'
+        value={this.state.secondSecret} onChange={this.setSecondPass.bind(this, 'secondSecret')}/>;
     return (
       <article>
         <h3>Add vote to</h3>
@@ -32,6 +56,11 @@ class ConfirmVotes extends React.Component {
         <ul>
           {this.props.unvotedList.map(item => <li key={item.username}>{item.username}</li>)}
         </ul>
+        {secondPass}
+        <InfoParagraph>
+          <p>You can select up to 33 delegates in one voting turn.</p>
+          <p>You can vote for up to 101 delegates in total.</p>
+        </InfoParagraph>
         <footer className={`${grid.row} ${grid['between-xs']}`}>
             <Button key={0} label='Cancel'
               className='cancel-button'
@@ -60,7 +89,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   showSuccessAlert: data => dispatch(alertDialogDisplayed(data)),
   clearVoteLists: () => dispatch(clearVoteLists()),
-  penddingVotes: () => dispatch(penddingVotes()),
+  pendingVotes: () => dispatch(pendingVotes()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ConfirmVotes);
