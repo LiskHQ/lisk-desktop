da0f0822bfd5c9ff8c1077a7eca375ec8cb42e43
yashar <darkyashi@gmail.com>
2017-08-10 13:23:58 +0430

    Remove some store dispachs from voting component

--- src/components/voting/index.js
+++ src/components/voting/index.js
@@ -1,19 +1,12 @@
 import { connect } from 'react-redux';
 import VotingComponent from './votingComponent';
-import {
-          addToVoteList,
-          removeFromVoteList,
-       } from '../../actions/voting';
 
 const mapStateToProps = state => ({
   address: state.account.address,
   activePeer: state.peers.data,
   votedList: state.voting.votedList,
   unvotedList: state.voting.unvotedList,
-});
-const mapDispatchToProps = dispatch => ({
-  addToVoteList: data => dispatch(addToVoteList(data)),
-  removeFromVoteList: data => dispatch(removeFromVoteList(data)),
+  refreshDelegates: state.voting.refresh,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(VotingComponent);
+export default connect(mapStateToProps)(VotingComponent);
