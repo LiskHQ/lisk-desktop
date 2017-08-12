9544b7aa1a7fbd080920add19b69c04029bd69bd
yashar <darkyashi@gmail.com>
2017-08-13 01:49:50 +0430

    Rename some action names in voting actions file

--- src/actions/voting.js
+++ src/actions/voting.js
@@ -2,34 +2,30 @@ import actionTypes from '../constants/actions';
 
 /**
  * Add data to the list of voted delegates
- *
  */
-export const addToVoteList = data => ({
-  type: actionTypes.addToVoteList,
+export const addedToVoteList = data => ({
+  type: actionTypes.addedToVoteList,
   data,
 });
 
 /**
  * Remove data from the list of voted delegates
- *
  */
-export const removeFromVoteList = data => ({
-  type: actionTypes.removeFromVoteList,
+export const removedFromVoteList = data => ({
+  type: actionTypes.removedFromVoteList,
   data,
 });
 
 /**
  * Remove all data from the list of voted delegates and list of unvoted delegates
- *
  */
 export const clearVoteLists = () => ({
-  type: actionTypes.clearVotes,
+  type: actionTypes.votesCleared,
 });
 
 /**
  * Add pending variable to the list of voted delegates and list of unvoted delegates
- *
  */
-export const pendingVotes = () => ({
-  type: actionTypes.pendingVotes,
+export const pendingVotesAdded = () => ({
+  type: actionTypes.pendingVotesAdded,
 });
