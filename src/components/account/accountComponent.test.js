b470be50772097a099c235e814eea88945adb3f2
Vit Stanislav <vit@lightcurve.io>
2017-08-15 09:41:07 +0200

    Add more tests for accountComponent

--- src/components/account/accountComponent.test.js
+++ src/components/account/accountComponent.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import chai, { expect } from 'chai';
-import { spy } from 'sinon';
+import sinon, { spy, mock } from 'sinon';
 import sinonChai from 'sinon-chai';
 import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
+import * as accountApi from '../../utils/api/account';
 import store from '../../store';
 import AccountComponent from './accountComponent';
 import ClickToSend from '../send/clickToSend';
@@ -11,62 +12,95 @@ import ClickToSend from '../send/clickToSend';
 chai.use(sinonChai);
 
 describe('AccountComponent', () => {
-  // Mocking store
-  const onActivePeerUpdated = () => {};
-  const peers = {
-    status: {
-      online: false,
-    },
-    data: {
-      currentPeer: 'localhost',
-      port: 4000,
-      options: {
-        name: 'Custom Node',
+  let props;
+
+  beforeEach(() => {
+    props = {
+      onActivePeerUpdated: sinon.spy(),
+      onAccountUpdated: sinon.spy(),
+      peers: {
+        status: {
+          online: false,
+        },
+        data: {
+          currentPeer: 'localhost',
+          port: 4000,
+          options: {
+            name: 'Custom Node',
+          },
+        },
       },
-    },
-  };
-  const testAccount = {
-    isDelegate: false,
-    address: '16313739661670634666L',
-    username: 'lisk-nano',
-    balance: 1e8,
-  };
+      account: {
+        isDelegate: false,
+        address: '16313739661670634666L',
+        username: 'lisk-nano',
+        balance: 1e8,
+      },
+    };
+  });
 
   it(' should render 3 article tags', () => {
-    const wrapper = shallow(<AccountComponent account={testAccount} peers={peers}
-      onActivePeerUpdated={onActivePeerUpdated} />);
+    const wrapper = shallow(<AccountComponent {...props} />);
     expect(wrapper.find('article')).to.have.lengthOf(3);
   });
 
   it('depicts being online when peers.status.online is true', () => {
-    const onlinePeers = Object.assign({}, peers, { status: { online: true } });
-    const wrapper = shallow(<AccountComponent account={testAccount} peers={onlinePeers}
-      onActivePeerUpdated={onActivePeerUpdated} />);
+    props.peers.status.online = true;
+    const wrapper = shallow(<AccountComponent {...props} />);
     const expectedValue = 'check';
     expect(wrapper.find('.material-icons').text()).to.be.equal(expectedValue);
   });
 
   it('should render balance with ClickToSend component', () => {
     const wrapper = mount(<Provider store={store}>
-      <AccountComponent account={testAccount} peers={peers}
-        onActivePeerUpdated={onActivePeerUpdated} />
+      <AccountComponent {...props} />
     </Provider>);
     expect(wrapper.find('.balance').find(ClickToSend)).to.have.lengthOf(1);
   });
 
   describe('componentDidMount', () => {
+    let accountApiMock;
+
+    beforeEach(() => {
+       accountApiMock = mock(accountApi);
+    });
+
+    afterEach(() => {
+      accountApiMock.restore();
+    });
+
     it('should be called once', () => {
       const actionSpy = spy(AccountComponent.prototype, 'componentDidMount');
-      mount(<Provider store={store}><AccountComponent account={testAccount} peers={peers}
-        onActivePeerUpdated={onActivePeerUpdated} /></Provider>);
+      mount(<Provider store={store}><AccountComponent {...props} /></Provider>);
       expect(actionSpy).to.have.been.calledWith();
     });
 
     it('binds listener to beat event', () => {
       const actionSpy = spy(document, 'addEventListener');
-      mount(<Provider store={store}><AccountComponent account={testAccount} peers={peers}
-        onActivePeerUpdated={onActivePeerUpdated} /></Provider>);
+      mount(<Provider store={store}><AccountComponent {...props} /></Provider>);
       expect(actionSpy).to.have.been.calledWith();
+    });
+
+    it('calls props.onActivePeerUpdated', () => {
+      accountApiMock.expects('getAccountStatus').resolves({ success: true });
+      const wrapper = mount(<Provider store={store}><AccountComponent {...props} /></Provider>);
+      // TODO: this doesn't work for some reason
+      // expect(props.onActivePeerUpdated).to.have.been.calledWith();
+    });
+
+    it('calls props.onAccountUpdated', () => {
+      accountApiMock.expects('getAccount').resolves({ balance: props.account.balance });
+      const wrapper = mount(<Provider store={store}><AccountComponent {...props} /></Provider>);
+      // TODO: this doesn't work for some reason
+      // expect(props.onAccountUpdated).to.have.been.calledWith();
+    });
+
+    it('calls props.onTransactionsUpdated if getAccount returns different balance', () => {
+      accountApiMock.expects('transactions').resolves({ transactions: [{}] });
+      accountApiMock.expects('getAccount').resolves({ balance: props.account.balance + 1 });
+      const wrapper = mount(<Provider store={store}><AccountComponent {...props} /></Provider>);
+      // TODO: this doesn't work for some reason
+      // expect(props.onAccountUpdated).to.have.been.calledWith();
     });
   });
 });
