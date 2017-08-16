994ea889121cb6af94586068d541c1a02fb7a43a
reyraa <ali@lightcurve.io>
2017-08-16 18:15:47 +0200

    Add redux-thunk package

--- src/store/middlewares/index.js
+++ src/store/middlewares/index.js
@@ -1,9 +1,11 @@
+import thunk from 'redux-thunk';
 import metronomeMiddleware from './metronome';
 import accountMiddleware from './account';
 import loginMiddleware from './login';
 import addedTransactionMiddleware from './addedTransaction';
 
 export default [
+  thunk,
   addedTransactionMiddleware,
   loginMiddleware,
   metronomeMiddleware,
