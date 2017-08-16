fa9fdc357bd4edb59fece6b17f5fb47f8d643051
Aleksey Popov <alepooop@gmail.com>
2017-08-16 17:35:50 +0300

    Add notification middleware

--- src/store/middlewares/index.js
+++ src/store/middlewares/index.js
@@ -1,9 +1,11 @@
 import metronomeMiddleware from './metronome';
 import accountMiddleware from './account';
 import loginMiddleware from './login';
+import notificationMiddleware from './notification';
 
 export default [
   loginMiddleware,
   metronomeMiddleware,
   accountMiddleware,
+  notificationMiddleware,
 ];
