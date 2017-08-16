b08d39771ed1982e16c744c533f87782e9557de9
reyraa <ali@lightcurve.io>
2017-08-16 18:10:30 +0200

    Create the middleware

--- src/store/middlewares/index.js
+++ src/store/middlewares/index.js
@@ -1,8 +1,10 @@
 import metronomeMiddleware from './metronome';
 import accountMiddleware from './account';
 import loginMiddleware from './login';
+import addedTransactionMiddleware from './addedTransaction';
 
 export default [
+  addedTransactionMiddleware,
   loginMiddleware,
   metronomeMiddleware,
   accountMiddleware,
