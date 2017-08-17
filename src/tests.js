890cbace52d06bd90bf42dd6eaee06a2d5048a34
Vit Stanislav <vit@lightcurve.io>
2017-08-17 16:27:41 +0200

    Centralise test setup - Closes #589

--- src/tests.js
+++ src/tests.js
@@ -1,3 +1,12 @@
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+import chaiEnzyme from 'chai-enzyme';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(sinonChai);
+chai.use(chaiEnzyme());
+chai.use(chaiAsPromised);
+
 // load all tests into one bundle
 const testsContext = require.context('.', true, /\.test\.js$/);
 testsContext.keys().forEach(testsContext);
