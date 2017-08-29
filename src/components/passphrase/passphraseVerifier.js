c377ea631aa4088e649e2e7a9998242ff20e7155
Vit Stanislav <vit@lightcurve.io>
2017-08-29 17:00:56 +0200

    Fix passphraseVerifier to not ask for the 13th word of 12

--- src/components/passphrase/passphraseVerifier.js
+++ src/components/passphrase/passphraseVerifier.js
@@ -20,7 +20,7 @@ class PassphraseConfirmator extends React.Component {
 
   hideRandomWord(rand = Math.random()) {
     const words = this.props.passphrase.trim().split(/\s+/);
-    const index = Math.floor(rand * words.length);
+    const index = Math.floor(rand * (words.length - 1));
 
     this.setState({
       passphraseParts: this.props.passphrase.split(` ${words[index]} `),
