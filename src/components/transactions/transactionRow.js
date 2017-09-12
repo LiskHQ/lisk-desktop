8f72edf37ba5887e741e66af54d670b153d14cc1
Vit Stanislav <vit@lightcurve.io>
2017-09-12 10:43:29 +0200

    Re-render transactionRow only if confirmations < 1000
    ... because transactions table was slow when it was re-rendering all
    rows.

--- src/components/transactions/transactionRow.js
+++ src/components/transactions/transactionRow.js
@@ -7,31 +7,39 @@ import Status from './status';
 import Amount from './amount';
 import Spinner from '../spinner';
 
-const TransactionRow = props => (
-  <tr>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+class TransactionRow extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  shouldComponentUpdate(nextProps) {
+    return nextProps.value.confirmations <= 1000;
+  }
+
+  render() {
+    const props = this.props;
+    return (<tr>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
         {props.value.confirmations ?
           <TooltipTime label={props.value.timestamp}></TooltipTime> :
           <Spinner />}
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText} ${styles.hiddenXs}`}>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText} ${styles.hiddenXs}`}>
         <TooltipWrapper tooltip={`${props.value.confirmations || 0} confirmation${props.value.confirmations !== 1 ? 's' : ''}`}>{props.value.id}</TooltipWrapper>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <TransactionType {...props.value} address={props.address}></TransactionType>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <Status {...props}></Status>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <Amount {...props}></Amount>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <span className={styles.grayButton}>
-        <LiskAmount val={props.value.fee} />
-      </span>
-    </td>
-  </tr>
-);
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <TransactionType {...props.value} address={props.address}></TransactionType>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <Status {...props}></Status>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <Amount {...props}></Amount>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <span className={styles.grayButton}>
+          <LiskAmount val={props.value.fee} />
+        </span>
+      </td>
+    </tr>);
+  }
+}
 
 export default TransactionRow;
