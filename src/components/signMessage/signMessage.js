869af110cc3990228937a80bfe0bb1218c50e358
Vit Stanislav <vit@lightcurve.io>
2017-09-15 09:22:14 +0200

    Fix indentation in signMessage

--- src/components/signMessage/signMessage.js
+++ src/components/signMessage/signMessage.js
@@ -55,40 +55,40 @@ class SignMessageComponent extends React.Component {
   render() {
     return (
       <div className='sign-message'>
-          <InfoParagraph>
-              Signing a message with this tool indicates ownership of a privateKey (secret) and
-              provides a level of proof that you are the owner of the key.
-              Its important to bear in mind that this is not a 100% proof as computer systems
-              can be compromised, but is still an effective tool for proving ownership
-              of a particular publicKey/address pair.
-              <br />
-              Note: Digital Signatures and signed messages are not encrypted!
-          </InfoParagraph>
-          <section>
-            <Input className='message' multiline label='Message'
-              autoFocus={true}
-              value={this.state.message}
-              onChange={this.sign.bind(this)} />
-            <AuthInputs
-              passphrase={this.state.passphrase}
-              secondPassphrase={this.state.secondPassphrase}
-              onChange={this.handleChange.bind(this)} />
-          </section>
-          {this.state.resultIsShown ?
-            <SignVerifyResult result={this.state.result} title='Result' /> :
-            <ActionBar
-              secondaryButton={{
-                onClick: this.props.closeDialog,
-              }}
-              primaryButton={{
-                label: 'Sign and copy result to clipboard',
-                className: 'sign-button',
-                disabled: (!this.state.result ||
-                  this.state.resultIsShown ||
-                  !authStateIsValid(this.state)),
-                onClick: this.showResult.bind(this),
-              }} />
-          }
+        <InfoParagraph>
+            Signing a message with this tool indicates ownership of a privateKey (secret) and
+            provides a level of proof that you are the owner of the key.
+            Its important to bear in mind that this is not a 100% proof as computer systems
+            can be compromised, but is still an effective tool for proving ownership
+            of a particular publicKey/address pair.
+          <br />
+            Note: Digital Signatures and signed messages are not encrypted!
+        </InfoParagraph>
+        <section>
+          <Input className='message' multiline label='Message'
+            autoFocus={true}
+            value={this.state.message}
+            onChange={this.sign.bind(this)} />
+          <AuthInputs
+            passphrase={this.state.passphrase}
+            secondPassphrase={this.state.secondPassphrase}
+            onChange={this.handleChange.bind(this)} />
+        </section>
+        {this.state.resultIsShown ?
+          <SignVerifyResult result={this.state.result} title='Result' /> :
+          <ActionBar
+            secondaryButton={{
+              onClick: this.props.closeDialog,
+            }}
+            primaryButton={{
+              label: 'Sign and copy result to clipboard',
+              className: 'sign-button',
+              disabled: (!this.state.result ||
+                this.state.resultIsShown ||
+                !authStateIsValid(this.state)),
+              onClick: this.showResult.bind(this),
+            }} />
+        }
       </div>
     );
   }
