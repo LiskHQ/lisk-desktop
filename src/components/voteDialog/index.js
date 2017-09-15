9574797f663086ff58929b2de3c94d278e06832f
reyraa <ali@lightcurve.io>
2017-09-15 09:36:32 +0200

    Pass the list of delegates of votes to votingDialog using connect

--- src/components/voteDialog/index.js
+++ src/components/voteDialog/index.js
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux';
-import { votePlaced, addedToVoteList, removedFromVoteList } from '../../actions/voting';
+import { votePlaced, voteToggled } from '../../actions/voting';
+import { transactionAdded } from '../../actions/transactions';
 import VoteDialog from './voteDialog';
 
 const mapStateToProps = state => ({
-  votedList: state.voting.votedList,
-  unvotedList: state.voting.unvotedList,
+  votes: state.voting.votes,
+  delegates: state.voting.delegates,
   account: state.account,
   activePeer: state.peers.data,
 });
 
 const mapDispatchToProps = dispatch => ({
   votePlaced: data => dispatch(votePlaced(data)),
-  addedToVoteList: data => dispatch(addedToVoteList(data)),
-  removedFromVoteList: data => dispatch(removedFromVoteList(data)),
+  voteToggled: data => dispatch(voteToggled(data)),
+  addTransaction: data => dispatch(transactionAdded(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(VoteDialog);
