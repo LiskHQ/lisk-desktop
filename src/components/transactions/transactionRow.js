dce60ef6baa5814b4558f116a5a22059decd03f5
yashar <darkyashi@gmail.com>
2017-09-19 16:42:04 +0430

    fix a bug in transactionRow component

--- src/components/transactions/transactionRow.js
+++ src/components/transactions/transactionRow.js
@@ -21,28 +21,30 @@ class TransactionRow extends React.Component {
         {props.value.confirmations ?
           <TooltipTime label={props.value.timestamp}></TooltipTime> :
           <Spinner />}
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText} ${styles.hiddenXs}`}>
-        <TooltipWrapper
-          tooltip={`${numeral(props.value.confirmations || 0).format('0a')} confirmation${props.value.confirmations !== 1 ? 's' : ''}`}>
-          {props.value.id}
-        </TooltipWrapper>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <TransactionType {...props.value} address={props.address}></TransactionType>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <Status {...props}></Status>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <Amount {...props}></Amount>
-    </td>
-    <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
-      <span className={styles.grayButton}>
-        <LiskAmount val={props.value.fee} />
-      </span>
-    </td>
-  </tr>
-);
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText} ${styles.hiddenXs}`}>
+          <TooltipWrapper
+            tooltip={`${numeral(props.value.confirmations || 0).format('0a')} confirmation${props.value.confirmations !== 1 ? 's' : ''}`}>
+            {props.value.id}
+          </TooltipWrapper>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <TransactionType {...props.value} address={props.address}></TransactionType>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <Status {...props}></Status>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <Amount {...props}></Amount>
+      </td>
+      <td className={`${props.tableStyle.rowCell} ${styles.centerText}`}>
+        <span className={styles.grayButton}>
+          <LiskAmount val={props.value.fee} />
+        </span>
+      </td>
+    </tr>
+    );
+  }
+}
 
 export default TransactionRow;
