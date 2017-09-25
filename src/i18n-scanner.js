17b192ed9334d831aa4937a77aa4ca40f28fedf7
Vit Stanislav <vit@lightcurve.io>
2017-09-25 18:23:09 +0200

    Add newline to end of locales/en/common.json

--- src/i18n-scanner.js
+++ src/i18n-scanner.js
@@ -20,5 +20,5 @@ files.forEach((file) => {
 const translations = parser.get({ sort: true }).en.translation;
 const count = Object.keys(translations).length;
 const outputJSON = JSON.stringify(translations, null, 2);
-fs.writeFileSync(outputFilePath, outputJSON);
+fs.writeFileSync(outputFilePath, `${outputJSON}\n`);
 process.stdout.write(`${count} translation keys parsed and written to '${outputFilePath}'`);
