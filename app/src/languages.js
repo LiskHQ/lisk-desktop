482ee3282620f19c78e5e1990c5d52fcf51b3609
yashar <darkyashi@gmail.com>
2017-10-16 00:52:36 +0330

    Create language constant in app folder

---
+++ app/src/languages.js
@@ -0,0 +1,12 @@
+const languages = {
+  en: {
+    name: 'English',
+    common: require('../dist/locales/en/common.json'),
+  },
+  de: {
+    name: 'Deutsch',
+    common: require('../dist/locales/de/common.json'),
+  },
+};
+
+export default languages;
