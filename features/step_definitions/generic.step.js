c1ae8444bebf710e9396ef04ecdc8394565a0c71
Vit Stanislav <vit@lightcurve.io>
2017-10-20 14:38:20 +0200

    Increase wait time for select to open

--- features/step_definitions/generic.step.js
+++ features/step_definitions/generic.step.js
@@ -76,7 +76,7 @@ defineSupportCode(({ Given, When, Then, setDefaultTimeout }) => {
 
   When('I select option no. {index} from "{selectName}" select', (index, selectName, callback) => {
     waitForElemAndClickIt(`.${selectName}`);
-    browser.sleep(500);
+    browser.sleep(1000);
     const selector = `.${selectName} ul li`;
     const optionElem = element.all(by.css(selector)).get(index - 1);
     browser.wait(EC.presenceOf(optionElem), waitTime)
