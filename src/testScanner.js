852394c7271da92ea1354f8ef5fe074df8a4041b
yasharAyari <yashar@lightcurve.io>
2017-10-30 18:00:46 +0100

    Fix eslint errors in testScanner.js

--- src/testScanner.js
+++ src/testScanner.js
@@ -1,27 +1,22 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var scanFolder = require("scan-folder");
+const scanFolder = require('scan-folder');
 // process.argv.forEach(function (val, index, array) {
 //     console.log(index + ': ' + val);
 // });
-const min = parseInt(process.argv[2]);
-const max = parseInt(process.argv[3]);
+const min = parseInt(process.argv[2], 10);
+const max = parseInt(process.argv[3], 10);
 
 // find js files in all dirs  
-const files = scanFolder(__dirname + '/', "test.js", true);
-console.log('Length', files.length);
+const files = scanFolder(`${__dirname}/`, 'test.js', true);
 const makePathRelative = path => path.replace(__dirname, '.');
 const modules = files
-    .map( source => `require('${makePathRelative(source)}');` )
-    .filter((item, index) => {
-        return (min < index && index < max);
-    })
-    .join('\n');
-console.log(__dirname);
-fs.writeFile(__dirname+"/test.js", modules, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
-}); 
+  .map(source => `require('${makePathRelative(source)}');`)
+  .filter((item, index) => (min < index && index < max))
+  .join('\n');
+fs.writeFile(`${__dirname}/test.js`, `${modules}\n`, (err) => {
+  if (err) {
+    return console.log(err); //eslint-disable-line
+  }
+  return console.log('The file was saved!'); //eslint-disable-line
+});
