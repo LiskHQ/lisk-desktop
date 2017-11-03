97288b1731fa3dd41e4525244fef7cd199f2df81
yasharAyari <yashar@lightcurve.io>
2017-11-03 11:30:01 +0100

    Delete getNetworks and add its data as networksDetail to networks.js

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -1,7 +1,24 @@
-const networks = {
+export const networksCode = {
   mainnet: 0,
   testnet: 1,
   customNode: 2,
 };
 
-export default networks;
+export const networksDetail = [
+  { // network name translation t('Mainnet');
+    name: 'Mainnet',
+    ssl: true,
+    port: 443,
+  },
+  { // network name translation t('Testnet');
+    name: 'Testnet',
+    testnet: true,
+    ssl: true,
+    port: 443,
+  },
+  { // network name translation t('Custom Node');
+    name: 'Custom Node',
+    custom: true,
+    address: 'http://localhost:4000',
+  },
+];
