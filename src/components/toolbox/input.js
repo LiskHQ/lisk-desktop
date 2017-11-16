aed09876d5f97753ad0e8613e2d22413b5750e45
reyraa <ali@lightcurve.io>
2017-11-16 10:50:03 +0100

    Add default theme to input

--- src/components/toolbox/input.js
+++ src/components/toolbox/input.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Input from 'react-toolbox/lib/input';
+import { themr } from 'react-css-themr';
+import inputTheme from './input.css';
 
-const ToolBoxInput = props => <Input {...props} />;
+const ToolBoxInput = props => <Input {...props}/>;
 
-export default ToolBoxInput;
+// export default ToolBoxInput;
+export default themr('input1', inputTheme)(ToolBoxInput);
