5b3c23fa0fcd66144c6ed285afdb0d4ae912007c
yasharAyari <yashar@lightcurve.io>
2017-11-20 10:27:12 +0100

    Implement new design of tab component

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Tab, Tabs as ToolboxTabs } from 'react-toolbox';
 import styles from './tabs.css';
+import logo from '../../assets/images/Lisk-Logo.svg';
+import * as menuLogos from '../../assets/images/sidebar-icons/*.svg'; //eslint-disable-line
 
+console.log(menuLogos);
 const getTabs = (isDelegate, tabs) => tabs.filter(t => t.id !== 'forging' || isDelegate);
 
 const getIndex = (history, tabs) =>
@@ -17,32 +20,61 @@ const navigate = (history, tabs, index) => {
   }
 };
 
+const Temp = props => (
+  <div>
+    <img src={props.img} />
+    <span>{props.label}</span>
+  </div>
+);
 const Tabs = ({ history, isDelegate, t }) => {
   const tabs = [
     {
-      label: t('Transactions'),
+      label: t('Dashboard'),
       id: 'transactions',
+      image: menuLogos.dashboard,
     }, {
-      label: t('Voting'),
+      label: t('Wallet'),
+      id: 'wallet',
+      image: menuLogos.wallet,
+    }, {
+      label: t('Buy Lisk'),
+      id: 'butLisk',
+      image: menuLogos.buyLisk,
+    }, {
+      label: t('Delegates'),
       id: 'voting',
+      image: menuLogos.delegates,
+    }, {
+      label: t('Sidechains'),
+      id: 'sidechains',
+      image: menuLogos.sidechains,
+    }, {
+      label: t('Search'),
+      id: 'search',
+      image: menuLogos.search,
     }, {
       label: t('Forging'),
       id: 'forging',
+      image: menuLogos.sidechains,
     },
   ];
-
+  // let tabContent;
   return (
-    <ToolboxTabs index={getIndex(history, tabs)}
-      theme={styles}
-      onChange={navigate.bind(null, history, tabs)}
-      className={`${styles.tabs} main-tabs`}>
-      {getTabs(isDelegate, tabs).map(({ label }, index) =>
-        <Tab
-          key={index}
-          label={label}
-          className={styles.tab}
-          disabled={isCurrent(history, index, tabs)} />)}
-    </ToolboxTabs>
+    <div className={styles.sideBarWrapper}>
+      <img src={logo} className={styles.logo} />
+      <ToolboxTabs index={getIndex(history, tabs)}
+        theme={styles}
+        onChange={navigate.bind(null, history, tabs)}
+        disableAnimatedBottomBorder={true}
+        className={`${styles.tabs} main-tabs`}>
+        {getTabs(isDelegate, tabs).map(({ label, image }, index) =>
+          <Tab
+            key={index}
+            label={<Temp label={label} img={image} />}
+            className={styles.tab}
+            disabled={isCurrent(history, index, tabs)} />)}
+      </ToolboxTabs>
+    </div>
   );
 };
 
