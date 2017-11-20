e230e0521cbc59274c3456c4f0862924fb7045bb
Vit Stanislav <vit@lightcurve.io>
2017-11-20 11:02:40 +0100

    Add more unit tests for autoUpdater

--- app/src/modules/autoUpdater.test.js
+++ app/src/modules/autoUpdater.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'; // eslint-disable-line import/no-extraneous-depen
 import sinon, { spy } from 'sinon'; // eslint-disable-line import/no-extraneous-dependencies
 import autoUpdater from './autoUpdater';
 
-describe('autoUpdater', () => {
+describe.only('autoUpdater', () => {
   const version = '1.2.3';
   let params;
   let callbacks;
@@ -20,12 +20,18 @@ describe('autoUpdater', () => {
           callbacks[name] = callback;
         },
         quitAndInstall: spy(),
+        downloadUpdate: spy(),
       },
       dialog: {
         showMessageBox: (options, callback) => {
           callbacks.dialog = callback;
         },
         showErrorBox: spy(),
+      },
+      win: {
+        browser: {
+          setProgressBar: spy(),
+        },
       },
     };
 
@@ -67,7 +73,17 @@ describe('autoUpdater', () => {
     expect(dialogSpy).to.have.been.calledWith();
   });
 
-  it.skip('should params.autoUpdater.quitAndInstall() in params.dialog.showMessageBox callback if the first button was pressed', () => {
+  it('should call params.dialog.showMessageBox on params.autoUpdater.on("update-not-available", ...) if checkForUpdates was called', () => {
+    const dialogSpy = spy(params.dialog, 'showMessageBox');
+
+    const checkForUpdates = autoUpdater(params);
+    checkForUpdates({});
+    callbacks['update-not-available']({ version });
+
+    expect(dialogSpy).to.have.been.calledWith();
+  });
+
+  it('should params.autoUpdater.quitAndInstall() on "update-downloaded" in params.dialog.showMessageBox callback if the first button was pressed', () => {
     autoUpdater(params);
     callbacks['update-downloaded']({ version });
     callbacks.dialog(0);
@@ -75,12 +91,34 @@ describe('autoUpdater', () => {
     expect(params.autoUpdater.quitAndInstall).to.have.been.calledWithExactly();
   });
 
-  it('should not params.autoUpdater.quitAndInstall() in params.dialog.showMessageBox callback if the second button was pressed', () => {
+  it('should not params.autoUpdater.quitAndInstall() on "update-downloaded" in params.dialog.showMessageBox callback if the second button was pressed', () => {
     autoUpdater(params);
     callbacks['update-downloaded']({ version });
     callbacks.dialog(1);
 
     expect(params.autoUpdater.quitAndInstall).to.not.have.been.calledWith();
+  });
+
+  it('should params.autoUpdater.downloadUpdate() on "update-available" in params.dialog.showMessageBox callback if the first button was pressed', () => {
+    autoUpdater(params);
+    callbacks['update-available']({ version });
+    callbacks.dialog(0);
+
+    expect(params.autoUpdater.downloadUpdate).to.have.been.calledWithExactly();
+  });
+
+  it('should not params.autoUpdater.downloadUpdate() on "update-available" in params.dialog.showMessageBox callback if the second button was pressed', () => {
+    autoUpdater(params);
+    callbacks['update-available']({ version });
+    callbacks.dialog(1);
+
+    expect(params.autoUpdater.downloadUpdate).to.not.have.been.calledWith();
+  });
+
+  it('should call win.browser.setProgressBar() on "download-progress"', () => {
+    autoUpdater(params);
+    callbacks['download-progress']({ transferred: 50, total: 100 });
+    expect(params.win.browser.setProgressBar).to.have.been.calledWith(50 / 100);
   });
 
   it('should console.error any error from params.autoUpdater.on("error", ...) ', () => {
