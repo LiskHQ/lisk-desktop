90834b9fa69aadf2435c2b8037e9954ad50c594d
yasharAyari <yashar@lightcurve.io>
2017-11-21 16:10:40 +0100

    Remove account component from app component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PrivateRoutes from '../privateRoute';
-// import Account from '../account';
 import Header from '../header';
 import Login from '../login';
 import Transactions from '../transactions';
