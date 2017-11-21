50ebffa21e751c15ac0d100f361cac4b935e45ee
yasharAyari <yashar@lightcurve.io>
2017-11-21 16:11:51 +0100

    Add setting to menu

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -19,12 +19,21 @@ const navigate = (history, tabs, index) => {
   }
 };
 
-const Temp = props => (
+const Setting = ({ label }) => (
   <div>
-    <img src={props.img} />
-    <span>{props.label}</span>
+    <i className='material-icons'>settings</i>
+    <span>{label}</span>
   </div>
 );
+
+const TabTemplate = ({ img, label }) => (
+  <div>
+    <img src={img} />
+    <span>{label}</span>
+  </div>
+);
+
+
 const Tabs = ({ history, isDelegate, t }) => {
   const tabs = [
     {
@@ -52,12 +61,16 @@ const Tabs = ({ history, isDelegate, t }) => {
       id: 'search',
       image: menuLogos.search,
     }, {
+      label: t('Setting'),
+      id: 'setting',
+      image: menuLogos.search,
+    }, {
       label: t('Forging'),
       id: 'forging',
       image: menuLogos.sidechains,
     },
   ];
-  // let tabContent;
+
   return (
     <div className={styles.sideBarWrapper}>
       <img src={logo} className={styles.logo} />
@@ -66,12 +79,17 @@ const Tabs = ({ history, isDelegate, t }) => {
         onChange={navigate.bind(null, history, tabs)}
         disableAnimatedBottomBorder={true}
         className={`${styles.tabs} main-tabs`}>
-        {getTabs(isDelegate, tabs).map(({ label, image }, index) =>
-          <Tab
-            key={index}
-            label={<Temp label={label} img={image} />}
-            className={styles.tab}
-            disabled={isCurrent(history, index, tabs)} />)}
+        {getTabs(isDelegate, tabs).filter((item, index) => index < tabs.length - 2)
+          .map(({ label, image }, index) =>
+            <Tab
+              key={index}
+              label={<TabTemplate label={label} img={image} />}
+              className={styles.tab}
+              disabled={isCurrent(history, index, tabs)} />)}
+        <Tab
+          key={tabs[tabs.length - 2].index}
+          className={`${styles.tab} ${styles.setting}`}
+          label={<Setting label={tabs[tabs.length - 2].label} />} />
       </ToolboxTabs>
     </div>
   );
