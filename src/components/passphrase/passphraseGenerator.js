1e36e0f273e6bbd79981d9a28c7aad43a21d6cee
yasharAyari <yashar@lightcurve.io>
2017-11-30 17:22:44 +0100

    Add animation to shapes

--- src/components/passphrase/passphraseGenerator.js
+++ src/components/passphrase/passphraseGenerator.js
@@ -1,30 +1,69 @@
 import React from 'react';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
-import { generateSeed, generatePassphrase, emptyByte } from '../../utils/passphrase';
+import { generateSeed, emptyByte, generatePassphrase } from '../../utils/passphrase';
+import { extractAddress } from '../../utils/api/account';
 import styles from './passphrase.css';
 import Input from '../toolbox/inputs/input';
 import ProgressBar from '../toolbox/progressBar/progressBar';
 import * as shapes from '../../assets/images/register-shapes/*.svg'; //eslint-disable-line
+import AnimateShape from './animateShape';
+import { PrimaryButton } from '../toolbox/buttons/button';
 
 class PassphraseGenerator extends React.Component {
   constructor() {
     super();
     this.state = {
       step: 'info',
+      address: null,
       lastCaptured: {
         x: 0,
         y: 0,
       },
+      firstHeadingClass: '',
+      secondHeadingClass: '',
+      headingClass: '',
       zeroSeed: emptyByte('00'),
       seedDiff: emptyByte(0),
     };
+    this.shapes = [];
+  }
+
+  componentDidMount() {
+    this.container = document.getElementById('generatorContainer');
     this.seedGeneratorBoundToThis = this.seedGenerator.bind(this);
-    document.addEventListener('mousemove', this.seedGeneratorBoundToThis, true);
+    this.container.addEventListener('mousemove', this.seedGeneratorBoundToThis, true);
+
+    this.shapes.push(new AnimateShape({ x: 550, y: 50 }, '#rCircle', true));
+    this.shapes.push(new AnimateShape({ x: 450, y: 230 }, '#rTriangle', true));
+    this.shapes.push(new AnimateShape({ x: 450, y: 180 }, '#rSmallTriangle', true));
+    this.shapes.push(new AnimateShape({ x: 300, y: 220 }, '#rRightRectangle', true));
+    this.shapes.push(new AnimateShape({ x: 250, y: 350 }, '#rCircleOutline', false));
+    this.shapes.push(new AnimateShape({ x: 100, y: 200 }, '#rStripe', false));
+    this.shapes.push(new AnimateShape({ x: 280, y: 315 }, '#rSmallStripe', true));
+    this.shapes.push(new AnimateShape({ x: 500, y: 240 }, '#rSmallCircle', false));
+    this.shapes.push(new AnimateShape({ x: 530, y: 150 }, '#rLeftRectangle', false));
+  }
+
+  updateShapes() {
+    setTimeout(() => {
+      const { data } = this.state;
+      if (data.percentage % 20 > 17 && data.percentage % 20 !== 0) {
+        this.setState({
+          headingClass: styles.goToTop,
+        });
+        this.shapes.forEach((shape) => {
+          shape.startMovement(data.percentage);
+        });
+      }
+    }, 10);
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+    this.updateShapes(nextState.data);
   }
 
   componentWillUnmount() {
-    // const container = document.getElementById('generatorContainer')
-    document.removeEventListener('mousemove', this.seedGeneratorBoundToThis, true);
+    this.container.removeEventListener('mousemove', this.seedGeneratorBoundToThis, true);
   }
 
   /**
@@ -67,31 +106,41 @@ class PassphraseGenerator extends React.Component {
         ((item !== data.seed[index]) ? index : null))
         .filter(item => item !== null);
       this.setState({ data, seedDiff });
+      // this.circle.startMovement(this.state.data.percentage);
     } else if (this.state.data && this.state.data.percentage >= 100 && !this.state.passphrase) {
       // also change the step here
       const phrase = generatePassphrase(this.state.data);
+      const address = extractAddress(phrase);
       this.setState({
         passphrase: phrase,
+        firstHeadingClass: styles.firstHeading,
+        secondHeadingClass: styles.secondHeading,
+        address,
       });
-      this.props.changeHandler('passphrase', phrase);
-      this.props.changeHandler('current', 'show');
+      // this.props.changeHandler('passphrase', phrase);
+      // this.props.changeHandler('current', 'show');
+      console.log(extractAddress(phrase));
+      this.shapes[4].css('opacity', 0);
+      this.shapes[8].css('opacity', 0);
+      this.shapes[3].css('opacity', 0);
     }
   }
 
   render() {
     const isTouch = this.isTouchDevice(this.props.agent);
+    const percentage = this.state.data ? this.state.data.percentage : 0;
     return (
-      <div className={`${grid.row} ${grid['center-xs']} ${styles.wrapper}`} >
+      <div className={`${grid.row} ${grid['center-xs']} ${styles.wrapper}`} id="generatorContainer" >
         <div className={grid['col-xs-12']}>
-          <img className={styles.circle} src={shapes.circle} />
-          <img className={styles.smallCircle} src={shapes.smallCircle} />
-          <img className={styles.triangle} src={shapes.triangle} />
-          <img className={styles.smallTriangle} src={shapes.smallTriangle} />
-          <img className={styles.circleOutline} src={shapes.circleOutline} />
-          <img className={styles.stripe} src={shapes.stripe} />
-          <img className={styles.smallStripe} src={shapes.smallStripe} />
-          <img className={styles.rightRectangle} src={shapes.rightRectangle} />
-          <img className={styles.leftRectangle} src={shapes.leftRectangle} />
+          <img className={styles.circle} id="rCircle" src={shapes.circle} />
+          <img className={styles.smallCircle} id="rSmallCircle" src={shapes.smallCircle} />
+          <img className={styles.triangle} id="rTriangle" src={shapes.triangle} />
+          <img className={styles.smallTriangle} id="rSmallTriangle" src={shapes.smallTriangle} />
+          <img className={styles.circleOutline} id="rCircleOutline" src={shapes.circleOutline} />
+          <img className={styles.stripe} id="rStripe" src={shapes.stripe} />
+          <img className={styles.smallStripe} id="rSmallStripe" src={shapes.smallStripe} />
+          <img className={styles.rightRectangle} id="rRightRectangle" src={shapes.rightRectangle} />
+          <img className={styles.leftRectangle} id="rLeftRectangle" src={shapes.leftRectangle} />
 
           {isTouch ?
             <div>
@@ -99,16 +148,30 @@ class PassphraseGenerator extends React.Component {
               <Input onChange={this.seedGeneratorBoundToThis}
                 className='touch-fallback' autoFocus={true} multiline={true} />
             </div> :
-            <h2 className={styles.generatorHeader}>
-              {this.props.t('Create your Lisk ID')}
-              <br/>
-              {this.props.t('by moving your mouse.')}
-            </h2>
+            <header>
+              <h2 className={`${styles.generatorHeader} ${this.state.headingClass} ${this.state.firstHeadingClass}`}
+                id="generatorHeader" >
+                {this.props.t('Create your Lisk ID')}
+                <br/>
+                {this.props.t('by moving your mouse.')}
+              </h2>
+              <h2
+                className={`${styles.generatorHeader} ${this.state.headingClass} ${this.state.secondHeadingClass}`}
+                id="generatorHeader" >
+                {this.props.t('Create your Lisk ID')}
+              </h2>
+            </header>
           }
+          {this.state.address ?
+            <div className={styles.addressContainer}>
+              <h4 className={styles.address}>{this.state.address}</h4>
+              <PrimaryButton theme={styles} label='Get passphrase' />
+            </div>
+            : ''}
         </div>
         <div className={grid['col-xs-12']}>
           <ProgressBar mode='determinate' theme={styles}
-            value={this.state.data ? this.state.data.percentage : 0} />
+            value={percentage} />
         </div>
       </div>
     );
