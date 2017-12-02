626c9fe14b1e0a720ad5a9251c6528a458ebe755
reyraa <ali@lightcurve.io>
2017-12-02 11:58:44 +0100

    Add unit test for Parallaxer component

--- src/components/parallax/index.test.js
+++ src/components/parallax/index.test.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import Parallax from './index';
+import Parallaxer from './index';
 
-describe.only('Parallax', () => {
+describe('Parallaxer', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = mount(<Parallax className='parent'>
+    wrapper = mount(<Parallaxer className='parent' originX='0px'>
       <div className='child'>Child 1</div>
       <div className='child'>Child 2</div>
-    </Parallax>);
+    </Parallaxer>);
   });
 
   it('should mount its children', () => {
-    expect(wrapper.find('.child').length).to.be.equal(2);
+    expect(wrapper.find('.child')).to.have.lengthOf(2);
+  });
+
+  it('should run Parallax on its children', () => {
+    const child0Html = wrapper.find('.child').at(0).html();
+    const child1Html = wrapper.find('.child').at(1).html();
+    expect(child0Html).to.include('transform: translate3d');
+    expect(child1Html).to.include('transform: translate3d');
   });
 });
