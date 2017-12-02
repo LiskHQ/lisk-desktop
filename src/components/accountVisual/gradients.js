6ef9fc43189a21b8251cec3de859d5523921858e
Vit Stanislav <vit@lightcurve.io>
2017-12-02 12:28:34 +0100

    Move gradients into a separate file

---
+++ src/components/accountVisual/gradients.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const gradientSpecs = [
+  {
+    id: 'purpleGradient',
+    colors: ['#3023AE', '#C86DD7'],
+  },
+  {
+    id: 'lightBlueGradient',
+    colors: ['#DBD4FF', '#0792FF'],
+  },
+  {
+    id: 'darkBlueGradient',
+    colors: ['#DBD4FF', '#519AEC'],
+  },
+  {
+    id: 'greenBlueGradient',
+    colors: ['#3023AE', '#53A0FD', '#B4EC51'],
+  },
+  {
+    id: 'redPurpleGradient',
+    colors: ['#FF3537', '#4D04AD'],
+  },
+];
+
+export const gradientIds = gradientSpecs.map(spec => `url(#${spec.id})`);
+
+export const Gradients = () => (
+  <defs>
+    {gradientSpecs.map(spec => (
+      <linearGradient id={spec.id} key={spec.id}>
+        {spec.colors.map((color, i) => (
+          <stop stopColor={color} offset={`${i * (100 / (spec.colors.length - 1))}%`} key={i}/>
+        ))}
+      </linearGradient>
+    ))}
+  </defs>
+);
--- src/components/accountVisual/index.js
+++ src/components/accountVisual/index.js
@@ -1,40 +1,5 @@
 import React from 'react';
-
-const gradientSpecs = [
-  {
-    id: 'purpleGradient',
-    colors: ['#3023AE', '#C86DD7'],
-  },
-  {
-    id: 'lightBlueGradient',
-    colors: ['#DBD4FF', '#0792FF'],
-  },
-  {
-    id: 'darkBlueGradient',
-    colors: ['#DBD4FF', '#519AEC'],
-  },
-  {
-    id: 'greenBlueGradient',
-    colors: ['#3023AE', '#53A0FD', '#B4EC51'],
-  },
-  {
-    id: 'redPurpleGradient',
-    colors: ['#FF3537', '#4D04AD'],
-  },
-];
-const gradients = gradientSpecs.map(spec => `url(#${spec.id})`);
-
-const Gradients = ({ specs }) => (
-  <defs>
-    {specs.map(spec => (
-      <linearGradient id={spec.id} key={spec.id}>
-        {spec.colors.map((color, i) => (
-          <stop stopColor={color} offset={`${i * (100 / (spec.colors.length - 1))}%`} key={i}/>
-        ))}
-      </linearGradient>
-    ))}
-  </defs>
-);
+import { gradientIds, Gradients } from './gradients';
 
 const pickOne = (items, address, relevantIndex) => {
   const index = address.padStart(20, '0')[relevantIndex] % items.length;
@@ -71,32 +36,32 @@ const AccountVisual = ({ address, size = 200 }) => {
       cx: pickOne(sizes, address, 0) + (size / 4),
       cy: pickOne(sizes, address, 1) + (size / 4),
       r: pickOne(sizes, address, 2) / 2,
-      fill: pickOne(gradients, address, 3),
+      fill: pickOne(gradientIds, address, 3),
     },
     square: {
       x: pickOne(sizes, address, 5),
       y: pickOne(sizes, address, 6),
       height: pickOne(sizes, address, 7),
       width: pickOne(sizes, address, 7),
-      fill: pickOne(gradients, address, 8),
+      fill: pickOne(gradientIds, address, 8),
     },
     rect: {
       x: pickOne(sizes, address, 10),
       y: pickOne(sizes, address, 11) * 5,
       height: pickOne(sizes, address, 12) / 5,
       width: pickOne(sizes, address, 12),
-      fill: pickOne(gradients, address, 13),
+      fill: pickOne(gradientIds, address, 13),
     },
     triangle: computeTriangle({
       x: pickOne(sizes, address, 15),
       y: pickOne(sizes, address, 15),
       size: pickOne(sizes, address, 16),
-      fill: pickOne(gradients, address, 17),
+      fill: pickOne(gradientIds, address, 17),
     }),
   };
   return (
     <svg height={size} width={size}>
-      <Gradients specs={gradientSpecs} />
+      <Gradients />
       <circle {...shapes.circle} />
       <polygon {...shapes.triangle} />
       <rect {...shapes.square} />
