4312482fa3220e105c1f5204a5087a04d874c44a
Vit Stanislav <vit@lightcurve.io>
2017-12-06 10:00:38 +0100

    Move savedAccounts subscriber to it's own file

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import throttle from 'lodash.throttle';
 
-import { setSavedAccounts, setLastActiveAccount } from '../utils/savedAccounts';
 import * as reducers from './reducers';
 import middleWares from './middlewares';
+import savedAccountsSubscriber from './subscribers/savedAccounts';
 
 const App = combineReducers(reducers);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(App, composeEnhancers(applyMiddleware(...middleWares)));
+
+store.subscribe(throttle(savedAccountsSubscriber.bind(null, store), 1000));
 
 // ignore this in coverage as it is hard to test and does not run in production
 /* istanbul ignore if */
@@ -19,13 +21,5 @@ if (module.hot) {
     store.replaceReducer(nextReducer);
   });
 }
-
-store.subscribe(throttle(() => {
-  const savedAccounts = store.getState();
-  if (savedAccounts && savedAccounts.lastActive) {
-    setSavedAccounts(savedAccounts.accounts);
-    setLastActiveAccount(savedAccounts.lastActive);
-  }
-}, 1000));
 
 export default store;
