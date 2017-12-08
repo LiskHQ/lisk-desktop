8e2c197fab1d46c0d2a7be05f1d04cd00ea5b9ff
yasharAyari <yashar@lightcurve.io>
2017-12-08 14:02:10 +0100

    Create unit test file for movableShape component

---
+++ src/components/passphrase/movableShape.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { expect } from 'chai';
+import { spy, useFakeTimers } from 'sinon';
+import { mount } from 'enzyme';
+import MovableShape from './movableShape';
+import styles from './passphrase.css';
+// import * as shapesSrc from '../../assets/images/register-shapes/*.svg'; //eslint-disable-line
+
+describe('MovableShape', () => {
+  const props = {
+    hidden: 1,
+    src: 'test.svg',
+    percentage: 0,
+    className: styles.circle,
+    end: { x: 550, y: 50 },
+    reverse: true,
+  };
+  let clock;
+  let wrapper;
+  beforeEach(() => {
+    spy(MovableShape.prototype, 'moveShape');
+    clock = useFakeTimers({
+      toFake: ['setTimeout', 'clearTimeout', 'Date'],
+    });
+    wrapper = mount(<MovableShape {...props} />);
+  });
+
+  afterEach(() => {
+    MovableShape.prototype.moveShape.restore();
+    clock.restore();
+  });
+
+  it('shows an img tag', () => {
+    expect(wrapper.find('img').props().src).to.be.equal('test.svg');
+    expect(wrapper.find('img').props().className).to.be.equal(styles.circle);
+  });
+
+  it("When props.hidden is equal to '1' opacity of img should be equal '1'", () => {
+    expect(wrapper.find('img').props().style.opacity).to.be.equal(1);
+  });
+
+});
+
