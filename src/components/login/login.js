eb53b5adf88124ee0bcc173860762469b7ca48d1
Vit Stanislav <vit@lightcurve.io>
2017-12-21 17:46:02 +0100

    Improve styles of login on mobile

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -194,9 +194,9 @@ class Login extends React.Component {
         <section className={`${styles.login} ${styles[this.state.passInputState]}`}>
           <section className={styles.table}>
             <header>
-              <a className={styles.backButton} href='https://list.io' target='_blank' rel='noopener noreferrer'>
+              <a className={styles.backButton} href='https://lisk.io' target='_blank' rel='noopener noreferrer'>
                 <FontIcon className={styles.icon}>arrow-left</FontIcon>
-                <b>Back to lisk.io</b>
+                {this.props.t('Back to lisk.io')}
               </a>
             </header>
             <div className={`${styles.tableCell} text-left`}>
