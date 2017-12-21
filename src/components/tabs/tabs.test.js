e9e77fd8b9724776295749ad3d6eae06c47108ef
yasharAyari <yashar@lightcurve.io>
2017-12-21 17:59:30 +0100

    Add some test for offcanvas menu

--- src/components/tabs/tabs.test.js
+++ src/components/tabs/tabs.test.js
@@ -23,17 +23,24 @@ describe('Tabs', () => {
 
   it('should render 8 Button components if props.isDelegate', () => {
     const wrapper = mount(<Tabs isDelegate={true} history={history} t={t} />);
-    expect(wrapper.find(Tab)).to.have.lengthOf(8);
+    expect(wrapper.find(Tab)).to.have.lengthOf(7);
   });
 
   it('should render 7 Tab components if !props.isDelegate', () => {
     const wrapper = mount(<Tabs isDelegate={false} history={history} t={t} />);
-    expect(wrapper.find(Tab)).to.have.lengthOf(7);
+    expect(wrapper.find(Tab)).to.have.lengthOf(6);
   });
 
   it('should allow to change active tab', () => {
     const wrapper = mount(<Tabs isDelegate={false} history={history} t={t} />);
     wrapper.find(Tab).at(0).simulate('click');
     expect(history.push).to.have.been.calledWith('/main/transactions');
+  });
+
+  it('should click on more activate the drawer', () => {
+    const wrapper = mount(<Tabs isDelegate={false} history={history} t={t} />);
+    wrapper.find('#moreMenu').simulate('click');
+    wrapper.update();
+    expect(wrapper.state('active')).to.be.equal(true);
   });
 });
