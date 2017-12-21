ba9a7280d8117974bd128e26e2046b369d2dc615
yasharAyari <yashar@lightcurve.io>
2017-12-21 17:37:10 +0100

    Add off canvas menu to tabs component

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Tab, Tabs as ToolboxTabs } from 'react-toolbox';
+import { Tab, Tabs as ToolboxTabs, Drawer } from 'react-toolbox';
 import styles from './tabs.css';
 import logo from '../../assets/images/Lisk-Logo.svg';
 import * as menuLogos from '../../assets/images/sidebar-icons/*.svg'; //eslint-disable-line
+import { FontIcon } from '../fontIcon';
 
 const getTabs = (isDelegate, tabs) => tabs.filter(t => t.id !== 'forging' || isDelegate);
 
@@ -13,19 +14,6 @@ const getIndex = (history, tabs) =>
 const isCurrent = (history, index, tabs) =>
   history.location.pathname.indexOf(tabs[index].id) === 6; // after: /main/
 
-const navigate = (history, tabs, index) => {
-  if (!isCurrent(history, index, tabs)) {
-    history.push(`/main/${tabs[index].id}`);
-  }
-};
-
-const Setting = ({ label }) => (
-  <div>
-    <i className='material-icons'>settings</i>
-    <span>{label}</span>
-  </div>
-);
-
 const TabTemplate = ({ img, label }) => (
   <div>
     <img src={img} />
@@ -34,66 +22,103 @@ const TabTemplate = ({ img, label }) => (
 );
 
 
-const Tabs = ({ history, isDelegate, t }) => {
-  const tabs = [
-    {
-      label: t('Dashboard'),
-      id: 'transactions',
-      image: menuLogos.dashboard,
-    }, {
-      label: t('Delegates'),
-      id: 'voting',
-      image: menuLogos.delegates,
-    }, {
-      label: t('Forging'),
-      id: 'forging',
-      image: menuLogos.sidechains,
-    }, {
-      label: t('Wallet'),
-      id: 'wallet',
-      image: menuLogos.wallet,
-    }, {
-      label: t('Buy Lisk'),
-      id: 'butLisk',
-      image: menuLogos.buyLisk,
-    }, {
-      label: t('Sidechains'),
-      id: 'sidechains',
-      image: menuLogos.sidechains,
-    }, {
-      label: t('Search'),
-      id: 'search',
-      image: menuLogos.search,
-    }, {
-      label: t('Setting'),
-      id: 'setting',
-      image: menuLogos.search,
-    },
-  ];
-  const filterTabs = getTabs(isDelegate, tabs);
-  return (
-    <div className={styles.sideBarWrapper}>
-      <img src={logo} className={styles.logo} />
-      <ToolboxTabs index={getIndex(history, tabs)}
-        theme={styles}
-        onChange={navigate.bind(null, history, tabs)}
-        disableAnimatedBottomBorder={true}
-        className={`${styles.tabs} main-tabs`}>
-        {filterTabs.filter((item, index) => index < filterTabs.length - 1)
-          .map(({ label, image, id }, index) =>
+class Tabs extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      active: false,
+    };
+  }
+
+  menuToggle() {
+    this.setState({ active: !this.state.active });
+  }
+
+  navigate(history, tabs, index) {
+    if (!isCurrent(history, index, tabs)) {
+      this.setState({ active: false });
+      history.push(`/main/${tabs[index].id}`);
+    }
+  }
+
+  render() {
+    const { history, isDelegate, t } = this.props;
+    const tabs = [
+      {
+        label: t('Dashboard'),
+        id: 'transactions',
+        image: menuLogos.dashboard,
+      }, {
+        label: t('Delegates'),
+        id: 'voting',
+        image: menuLogos.delegates,
+      }, {
+        label: t('Forging'),
+        id: 'forging',
+        image: menuLogos.sidechains,
+      }, {
+        label: t('Wallet'),
+        id: 'wallet',
+        image: menuLogos.wallet,
+      }, {
+        label: t('Buy Lisk'),
+        id: 'butLisk',
+        image: menuLogos.buyLisk,
+      }, {
+        label: t('Sidechains'),
+        id: 'sidechains',
+        image: menuLogos.sidechains,
+      }, {
+        label: t('Search'),
+        id: 'search',
+        image: menuLogos.search,
+      },
+    ];
+    const filterTabs = getTabs(isDelegate, tabs);
+    return (
+      <div className={styles.sideBarWrapper}>
+        <img src={logo} className={styles.logo} />
+        <ToolboxTabs index={getIndex(history, tabs)}
+          theme={styles}
+          onChange={this.navigate.bind(this, history, filterTabs)}
+          disableAnimatedBottomBorder={true}
+          className={`${styles.tabs} main-tabs`}>
+          {filterTabs.map(({ label, image, id }, index) =>
             <Tab
               key={index}
               label={<TabTemplate label={label} img={image} />}
               className={styles.tab}
               id={id}
               disabled={isCurrent(history, index, tabs)} />)}
-        <Tab
-          key={tabs[tabs.length - 1].index}
-          className={`${styles.tab} ${styles.setting}`}
-          label={<Setting label={tabs[tabs.length - 1].label} />} />
-      </ToolboxTabs>
-    </div>
-  );
-};
+        </ToolboxTabs>
+        <div onClick={this.menuToggle.bind(this)} id="moreMenu" className={styles.setting}>
+          <FontIcon value='more' className={styles.readMoreIcon} />
+          <span className={styles.readMoreText}>{t('more')}</span>
+        </div>
+        <Drawer theme={styles}
+          className='drawer'
+          active={this.state.active}
+          onOverlayClick={this.menuToggle.bind(this)}>
+          <header className={styles.header}>
+            <img src={logo} className={styles.logo} />
+            <FontIcon value='close' className={styles.close} onClick={this.menuToggle.bind(this)} />
+          </header>
+          <ToolboxTabs index={getIndex(history, tabs)}
+            theme={styles}
+            onChange={this.navigate.bind(this, history, filterTabs)}
+            disableAnimatedBottomBorder={true}
+            className={`${styles.tabs} main-tabs`}>
+            {filterTabs.map(({ label, image, id }, index) =>
+              <Tab
+                key={index}
+                label={<TabTemplate label={label} img={image} />}
+                id={id}
+                disabled={isCurrent(history, index, tabs)} />)}
+          </ToolboxTabs>
+        </Drawer>
+      </div>
+    );
+  }
+}
 
 export default Tabs;
