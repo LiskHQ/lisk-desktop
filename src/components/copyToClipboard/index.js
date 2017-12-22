2ee1114f0689f265a9607509365d2839420e22a1
yasharAyari <yashar@lightcurve.io>
2017-12-22 16:55:59 +0100

    Replace svg file with fontIcon in copyToClipboard

--- src/components/copyToClipboard/index.js
+++ src/components/copyToClipboard/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { CopyToClipboard as ReactCopyToClipboard } from 'react-copy-to-clipboard';
-import copyIcon from './../../assets/images/icons/copy.svg';
+import { FontIcon } from '../fontIcon';
 
 class CopyToClipboard extends React.Component {
   render() {
     return (
       <ReactCopyToClipboard text={this.props.value} >
         <span className={this.props.className}>
-          <img src={copyIcon} />&nbsp;
+          <FontIcon value='copy-to-clipboard'></FontIcon>&nbsp;
           <span className='copy-title'>{this.props.text ? this.props.text : this.props.value}</span>
         </span>
       </ReactCopyToClipboard>
