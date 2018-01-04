8b245b884550b52ba5badff5df60cc5f3be08fea
Vit Stanislav <vit@lightcurve.io>
2018-01-04 16:10:27 +0100

    Implement mobile send slide in

--- src/components/menuBar/menuBar.js
+++ src/components/menuBar/menuBar.js
@@ -4,15 +4,8 @@ import { FontIcon } from 'react-toolbox/lib/font_icon';
 
 import styles from './menuBar.css';
 
-const MenuBar = ({ t, history }) => (
+const MenuBar = ({ t }) => (
   <section className={styles.menuBar}>
-    { history.location.pathname.indexOf('transactions') === 6 ?
-      <span>
-        <span className={styles.menuItem}> {t('Send')} </span>
-        <span className={styles.menuItem}> {t('Request')} </span>
-      </span> :
-      null
-    }
     <span className={`${styles.menuItem} ${styles.menuButton}`}>
       {t('Menu')} <FontIcon value='menu' />
     </span>
---
+++ src/components/send/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { FontIcon } from '../fontIcon';
+import Box from '../box';
+import MultiStep from './../multiStep';
+import ResultBox from '../resultBox';
+import SendWritable from '../sendWritable';
+import SendReadable from './../sendReadable';
+import styles from './styles.css';
+
+class Send extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sendIsActive: false,
+    };
+  }
+
+  setSendIsActive(sendIsActive) {
+    console.log('setSendIsActive', sendIsActive);
+    this.setState({ sendIsActive });
+  }
+
+  render() {
+    return (
+      <Box className={`${styles.send} ${this.state.sendIsActive ? styles.isActive : ''}`}>
+        <span className={`${styles.mobileMenu} ${this.state.sendIsActive ? styles.isHidden : ''}`}>
+          <span className={styles.mobileMenuItem}
+            onClick={this.setSendIsActive.bind(this, true)}>
+            {('Send')}
+          </span>
+        </span>
+        <span className={styles.mobileClose} onClick={this.setSendIsActive.bind(this, false)}>
+          {('Close')} <FontIcon value='close' />
+        </span>
+        <MultiStep>
+          <SendWritable/>
+          <SendReadable />
+          <ResultBox />
+        </MultiStep>
+      </Box>
+    );
+  }
+}
+
+export default Send;
+
