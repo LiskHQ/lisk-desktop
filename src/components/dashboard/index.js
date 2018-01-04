a61f46d7168380960851d793279b56284fd8003b
Vit Stanislav <vit@lightcurve.io>
2018-01-04 11:35:35 +0100

    Implement latest activity in dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { translate } from 'react-i18next';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
-import Transactions from './../transactions';
+import React from 'react';
+
+import { FontIcon } from '../fontIcon';
+import Box from '../box';
+import MultiStep from './../multiStep';
+import TransactionList from './../transactions/transactionList';
 import SendWritable from '../sendWritable';
 import SendReadable from './../sendReadable';
 import ResultBox from '../resultBox';
-import MultiStep from './../multiStep';
-import Box from '../box';
 import styles from './styles.css';
 
-const Dashboard = () => (
+const Dashboard = ({ t, transactions }) => (
   <div className={`${grid.row} ${styles.wrapper}`} >
     <div className={`${grid['col-md-8']} ${grid['col-xs-12']}`}>
       <Box className={`${styles.graph}`}>
       </Box>
       <Box className={`${styles.latestActivity}`}>
-        <Transactions></Transactions>
+        <header>
+          <h2 className={styles.title}>
+            {t('Latest activity')}
+            <Link to='/main/transactions' className={styles.seeAllLink}>
+              {t('See all transactions')}
+              <FontIcon value='arrow-right' />
+            </Link>
+          </h2>
+        </header>
+        <TransactionList {...{ transactions, t }} loadMore={() => {}} nextStep={() => {}} />
       </Box>
     </div>
     <div className={`${grid['col-md-4']} ${styles.sendWrapper}`}>
@@ -29,4 +43,8 @@ const Dashboard = () => (
   </div>
 );
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  transactions: [...state.transactions.pending, ...state.transactions.confirmed].slice(0, 5),
+});
+
+export default connect(mapStateToProps)(translate()(Dashboard));
