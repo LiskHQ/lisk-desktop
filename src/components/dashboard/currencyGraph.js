9fbd7c98c5edd18647dc9e8c5180384d0d981f45
Vit Stanislav <vit@lightcurve.io>
2018-01-11 18:07:42 +0100

    Create a mocked explorer api to use in dashboard currencyGraph

--- src/components/dashboard/currencyGraph.js
+++ src/components/dashboard/currencyGraph.js
@@ -1,7 +1,9 @@
 import { Line as LineChart } from 'react-chartjs-2';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
+import moment from 'moment';
 import React from 'react';
+import explorerApi from '../../utils/api/explorer';
 
 import styles from './currencyGraph.css';
 
@@ -50,16 +52,8 @@ const chartOptions = {
         return `BTC ${tooltipItem[0].yLabel}`;
       },
       label(tooltipItem) {
-        return tooltipItem.xLabel;
+        return moment(tooltipItem.xLabel).format('DD MMM           h:mm:ss');
       },
-    /*
-      afterLabel(tooltipItem, data) {
-        const dataset = data.datasets[0];
-        const percent = Math.round((dataset.data[tooltipItem.index] /
-          dataset._meta[0].total) * 100);
-        return `(${percent}%)`;
-      },
-    */
     },
     backgroundColor: 'rgba(255, 255, 255, 0.9)',
     titleFontSize: 18,
@@ -68,14 +62,14 @@ const chartOptions = {
     bodyFontSize: 12,
     displayColors: false,
     xPadding: 16,
-    yPadding: 28,
+    yPadding: 18,
+    titleFontFamily: 'gilroy',
+    titleSpacing: 12,
+    titleMarginBottom: 12,
     cornerRadius: 0,
     caretSize: 15,
   },
 };
-
-
-// const canvasGradient = CanvasGradient.addColorStop();addColorStop
 
 const chartData = (canvas) => {
   const ctx = canvas.getContext('2d');
@@ -85,28 +79,7 @@ const chartData = (canvas) => {
   gradient.addColorStop(1, '#93F4FE');
   return {
     datasets: [{
-      data: [{
-        x: new Date('2017-12-17T03:00:00'),
-        y: 0.007,
-      }, {
-        x: new Date('2017-12-17T04:00:00'),
-        y: 0.008,
-      }, {
-        x: new Date('2017-12-17T05:00:00'),
-        y: 0.005,
-      }, {
-        x: new Date('2017-12-17T06:00:00'),
-        y: 0.010,
-      }, {
-        x: new Date('2017-12-17T07:00:00'),
-        y: 0.012,
-      }, {
-        x: new Date('2017-12-17T08:00:00'),
-        y: 0.009,
-      }, {
-        x: new Date('2017-12-17T09:00:00'),
-        y: 0.011,
-      }],
+      data: explorerApi.getCurrencyGrapData(),
       backgroundColor: gradient,
       borderColor: gradient,
       borderWidth: 0,
