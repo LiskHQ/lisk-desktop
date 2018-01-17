da419399e65940f102a11414dea5e47488f5dbc9
yasharAyari <yashar@lightcurve.io>
2018-01-17 11:20:00 +0100

    Add close status to menuBar component

--- src/components/menuBar/menuBar.js
+++ src/components/menuBar/menuBar.js
@@ -4,13 +4,38 @@ import { FontIcon } from 'react-toolbox/lib/font_icon';
 
 import styles from './menuBar.css';
 
-const MenuBar = ({ t }) => (
-  <section className={styles.menuBar}>
-    <span className={`${styles.menuItem} ${styles.menuButton}`}>
-      {t('Menu')} <FontIcon value='menu' />
-    </span>
-  </section>
-);
+class MenuBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showMenu: false,
+    };
+  }
+  toggleMenu() {
+    this.props.menu();
+    this.setState({
+      showMenu: !this.state.showMenu,
+    });
+  }
+  render() {
+    const { t } = this.props;
+    const menuClass = this.state.showMenu ? styles.openMenu : '';
+    return (
+      <section className={`${styles.menuBar}  ${menuClass}`}>
+        {!this.state.showMenu ?
+          <span className={`${styles.menuItem} ${styles.menuButton}`}
+            onClick={this.toggleMenu.bind(this)}>
+            {t('Menu')}<FontIcon value='menu' />
+          </span>
+          : <span className={`${styles.menuItem} ${styles.menuButton}`}
+            onClick={this.toggleMenu.bind(this)}>
+            {t('Close')} <FontIcon value='close' />
+          </span>
+        }
+      </section>
+    );
+  }
+}
 
 export default MenuBar;
 
