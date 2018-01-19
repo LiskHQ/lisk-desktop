f20ecbc12f03d2281c0f7a2ef9bf052b3583ddd0
Vit Stanislav <vit@lightcurve.io>
2018-01-19 19:09:06 +0100

    Implement empty transactions list

--- src/components/transactions/transactionOverview.js
+++ src/components/transactions/transactionOverview.js
@@ -6,6 +6,7 @@ import LiskAmount from '../liskAmount';
 import Box from '../box';
 import styles from './transactions.css';
 import txFilters from './../../constants/transactionFilters';
+import cubeImage from '../../assets/images/dark-blue-cube.svg';
 
 class Transactions extends React.Component {
   constructor(props) {
@@ -74,29 +75,39 @@ class Transactions extends React.Component {
             <CopyToClipboard value={this.props.address} className={`${styles.address}`} copyClassName={styles.copy} />
           </div>
         </header>
-
-        <ul className={styles.list}>
-          {filters.map((filter, i) => (
-            <li key={i} className={`transaction-filter-item ${filter.className} ${styles.item} ${this.isActiveFilter(filter.value) ? styles.active : ''}`}
-              onClick={() => { this.props.transactionsFilterSet({ filter: filter.value }); }}>
-              {filter.name}
-            </li>
-          ))}
-        </ul>
-        <TransactionList
-          address={this.props.address}
-          transactions={this.props.transactions}
-          loadMore={this.loadMore.bind(this)}
-          nextStep={this.props.nextStep}
-          t={this.props.t}/>
-        {
-          // the whole transactions box should be scrollable on XS
-          // otherwise only the transaction list should be scrollable
-          // (see transactionList.js)
-          this.isSmallScreen()
-            ? <Waypoint bottomOffset='-80%' key={this.props.transactions.length}
-              onEnter={() => { this.loadMore(); }}></Waypoint>
-            : null
+        {this.props.transactions.length === 0 &&
+          (!this.props.activeFilter ||
+            this.props.activeFilter === txFilters.all) ?
+          <div className={styles.emptyTransactions}>
+            <img src={cubeImage} />
+            <h2>{this.props.t('No activity yet')}</h2>
+            <p>{this.props.t('The Wallet will show your recent transactions.')}</p>
+          </div> :
+          <div>
+            <ul className={styles.list}>
+              {filters.map((filter, i) => (
+                <li key={i} className={`transaction-filter-item ${filter.className} ${styles.item} ${this.isActiveFilter(filter.value) ? styles.active : ''}`}
+                  onClick={() => { this.props.transactionsFilterSet({ filter: filter.value }); }}>
+                  {filter.name}
+                </li>
+              ))}
+            </ul>
+            <TransactionList
+              address={this.props.address}
+              transactions={this.props.transactions}
+              loadMore={this.loadMore.bind(this)}
+              nextStep={this.props.nextStep}
+              t={this.props.t}/>
+            {
+              // the whole transactions box should be scrollable on XS
+              // otherwise only the transaction list should be scrollable
+              // (see transactionList.js)
+              this.isSmallScreen()
+                ? <Waypoint bottomOffset='-80%' key={this.props.transactions.length}
+                  onEnter={() => { this.loadMore(); }}></Waypoint>
+                : null
+            }
+          </div>
         }
       </Box>
     );
