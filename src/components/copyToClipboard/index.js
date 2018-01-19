2da7aa9c1916200992d7d0e6fc3804736ba8e609
Gina Contrino <gina@lightcurve.io>
2018-01-19 11:58:34 +0100

    make copy component clickable

--- src/components/copyToClipboard/index.js
+++ src/components/copyToClipboard/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { CopyToClipboard as ReactCopyToClipboard } from 'react-copy-to-clipboard';
 import { FontIcon } from '../fontIcon';
+import styles from './copy.css';
 
 class CopyToClipboard extends React.Component {
   render() {
     return (
       <ReactCopyToClipboard text={this.props.value} >
-        <span className={this.props.className}>
+        <span className={`${this.props.className} ${styles.clickable}`}>
           <span className='copy-title'>{this.props.text ? this.props.text : this.props.value}</span>
           <FontIcon value='copy-to-clipboard' className={this.props.copyClassName}></FontIcon>&nbsp;
         </span>
