bd9e73a09f3acac372ebd5e059e9e74611016e0f
Vit Stanislav <vit@lightcurve.io>
2018-01-19 20:01:56 +0100

    Make coverage check limits more strict

--- karma.conf.js
+++ karma.conf.js
@@ -43,10 +43,10 @@ module.exports = function (config) {
       ],
       check: {
         global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80,
+          statements: 90,
+          branches: 85,
+          functions: 90,
+          lines: 90,
         },
         each: {
           statements: 80,
