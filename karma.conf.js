07811b616d1942ffe0efde20d8130ec33c8ef227
reyraa <ali@lightcurve.io>
2018-01-24 11:06:32 +0100

    Set Karma threshold to a reasonable value

--- karma.conf.js
+++ karma.conf.js
@@ -43,10 +43,10 @@ module.exports = function (config) {
       ],
       check: {
         global: {
-          statements: 90,
-          branches: 85,
-          functions: 90,
-          lines: 90,
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
         },
         each: {
           statements: 80,
