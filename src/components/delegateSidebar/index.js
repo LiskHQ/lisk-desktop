9a2dc34cd5d8ecfa405035f5acd6ed2876d3d61c
yasharAyari <yashar@lightcurve.io>
2018-02-02 20:18:44 +0100

    Add  ResultBox to DelegateSidebar component

--- src/components/delegateSidebar/index.js
+++ src/components/delegateSidebar/index.js
@@ -5,13 +5,15 @@ import ConfirmVotes from '../confirmVotes';
 import VotesPreview from '../votesPreview';
 import PassphraseSteps from './../passphraseSteps';
 import styles from './delegateSidebar.css';
+import ResultBox from '../resultBox';
 
 const DelegateSidebar = ({ votes }) => (
-  <Box className='confirm-votes' finalCallback={() => {}}>
-    <MultiStep className={styles.wrapper}>
+  <Box className='confirm-votes'>
+    <MultiStep className={styles.wrapper} finalCallback={() => true}>
       <VotesPreview votes={votes} />
       <PassphraseSteps />
       <ConfirmVotes />
+      <ResultBox />
     </MultiStep>
   </Box>
 );
