a481e32ca2ec4680f9082c52e39b76a01189a0b9
Gina Contrino <gina@lightcurve.io>
2018-02-02 10:17:24 +0100

    Fix routes and make header with searchbar responsive

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -22,52 +22,56 @@ const Header = (props) => {
 
   return (
     <header className={`${grid.row} ${styles.wrapper} mainHeader`}>
-      <div className={`${grid['col-lg-6']} ${grid['col-xs-12']}`}>
+      <div className={`${grid['col-lg-6']} ${grid['col-md-6']} ${grid['col-xs-12']} ${styles.noPadding}`}>
         {shouldShowSearchBar() ? <SearchBar /> : <Account />}
       </div>
-      <div className={`${grid['col-lg-6']} ${grid['col-xs-12']}`}>
-        <img src={logo} className={styles.logo} />
-        <PrivateWrapper>
-          <div>
-            <div className={`account ${styles.account}`}>
-              <div className={styles.information} align="right">
-                <div className={styles.balance}>
-                  <LiskAmount val={props.account.balance}/>
-                  <small> LSK</small>
-                </div>
-                <div className={`${styles.address} account-information-address`}>{props.account.address}</div>
-                {props.autoLog ? <div className={styles.timer}>
-                  {((!props.account.expireTime || props.account.expireTime === 0)) ?
-                    <span><FontIcon value='locked' className={styles.lock}/> {props.t('Account locked!')}</span> : <div>
-                      <FontIcon value='unlocked' className={styles.lock}/> {props.t('Address timeout in')} <i> </i>
-                      <Countdown
-                        date={props.account.expireTime}
-                        renderer={CountDownTemplate}
-                        onComplete={() => props.removePassphrase()}
-                      />
-                    </div>}
-                </div>
-                  : <div className={styles.timer}>
-                    {props.account.passphrase ? '' : <span>
-                      <FontIcon value='locked' className={styles.lock}/> {props.t('Account locked!')}
-                    </span>
-                    }
-                  </div>
-                }
-              </div>
-              <RelativeLink to='saved-accounts' className={styles.avatar}>
-                <AccountVisual address={props.account.address} size={69}/>
-              </RelativeLink>
-              <div className={styles.menu}>
-                <figure className={styles.iconCircle}>
-                  <RelativeLink className={`${styles.link} saved-accounts`}
-                    to='saved-accounts'><FontIcon value='more'/></RelativeLink>
-                </figure>
-              </div>
-            </div>
+      <div className={`${grid['col-lg-6']} ${grid['col-md-6']} ${grid['col-xs-12']} ${styles.noPadding}`}>
+        <div className={`${grid.row} ${grid['between-xs']}`}>
+          <div className={`${grid['col-lg-1']}`}>
+            <img src={logo} className={`${styles.logo}`} />
           </div>
-        </PrivateWrapper>
-        {shouldShowActionButton() && <Link className={styles.actionButton} to='/'><ActionButton>{props.t('Use Lisk-App')}</ActionButton></Link>}
+          <div className={`${grid['col-lg-11']}`}>
+            <PrivateWrapper>
+              <div className={`account ${styles.account}`}>
+                <div className={styles.information} align="right">
+                  <div className={styles.balance}>
+                    <LiskAmount val={props.account.balance}/>
+                    <small> LSK</small>
+                  </div>
+                  <div className={`${styles.address} account-information-address`}>{props.account.address}</div>
+                  {props.autoLog ? <div className={styles.timer}>
+                    {((!props.account.expireTime || props.account.expireTime === 0)) ?
+                      <span><FontIcon value='locked' className={styles.lock}/> {props.t('Account locked!')}</span> : <div>
+                        <FontIcon value='unlocked' className={styles.lock}/> {props.t('Address timeout in')} <i> </i>
+                        <Countdown
+                          date={props.account.expireTime}
+                          renderer={CountDownTemplate}
+                          onComplete={() => props.removePassphrase()}
+                        />
+                      </div>}
+                  </div>
+                    : <div className={styles.timer}>
+                      {props.account.passphrase ? '' : <span>
+                        <FontIcon value='locked' className={styles.lock}/> {props.t('Account locked!')}
+                      </span>
+                      }
+                    </div>
+                  }
+                </div>
+                <RelativeLink to='saved-accounts' className={styles.avatar}>
+                  <AccountVisual address={props.account.address} size={69}/>
+                </RelativeLink>
+                <div className={styles.menu}>
+                  <figure className={styles.iconCircle}>
+                    <RelativeLink className={`${styles.link} saved-accounts`}
+                      to='saved-accounts'><FontIcon value='more'/></RelativeLink>
+                  </figure>
+                </div>
+              </div>
+            </PrivateWrapper>
+            {shouldShowActionButton() && <Link className={styles.actionButton} to='/'><ActionButton>{props.t('Use Lisk-App')}</ActionButton></Link>}
+          </div>
+        </div>
       </div>
     </header>
   );
