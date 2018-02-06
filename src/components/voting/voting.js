10787c32bafdcdd8c4e607cba95db6d2a0ca200a
yasharAyari <yashar@lightcurve.io>
2018-02-06 18:25:33 +0100

    Add toggleSummery to Voting component

--- src/components/voting/voting.js
+++ src/components/voting/voting.js
@@ -4,15 +4,35 @@ import styles from './voting.css';
 import DelegateSidebar from '../delegateSidebar';
 import DelegateList from '../delegateList';
 
-const Voting = ({ votes }) => (
-  <div className={`${grid.row} ${styles.wrapper}`} >
-    <aside className={`${grid['col-md-4']}`}>
-      <DelegateSidebar votes={votes} />
-    </aside>
-    <section className={`${grid['col-sm-12']} ${grid['col-md-8']}`}>
-      <DelegateList />
-    </section>
-  </div>
-);
+class Voting extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showChangeSummery: false,
+    };
+  }
+
+  toggleSummery() {
+    console.log(this.state.showChangeSummery);
+    this.setState({
+      showChangeSummery: !this.state.showChangeSummery,
+    });
+  }
+
+  render() {
+    const { votes } = this.props;
+    return (
+      <div className={`${grid.row} ${styles.wrapper}`} >
+        <aside className={`${grid['col-md-4']}`}>
+          {this.state.showChangeSummery}
+          <DelegateSidebar votes={votes} updateList={this.toggleSummery.bind(this)} />
+        </aside>
+        <section className={`${grid['col-sm-12']} ${grid['col-md-8']}`}>
+          <DelegateList showChangeSummery={this.state.showChangeSummery} />
+        </section>
+      </div>
+    );
+  }
+}
 
 export default Voting;
