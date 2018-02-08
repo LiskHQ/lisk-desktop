a021fd5ea685935cc6f313c91feefc5d93ae6682
yasharAyari <yashar@lightcurve.io>
2018-02-08 10:21:45 +0100

    Add an input value to toggleSummery method

--- src/components/voting/voting.js
+++ src/components/voting/voting.js
@@ -12,10 +12,12 @@ class Voting extends React.Component {
     };
   }
 
-  toggleSummery() {
-    this.setState({
-      showChangeSummery: !this.state.showChangeSummery,
-    });
+  toggleSummery(value) {
+    if (value !== this.state.showChangeSummery) {
+      this.setState({
+        showChangeSummery: value,
+      });
+    }
   }
 
   render() {
@@ -24,7 +26,7 @@ class Voting extends React.Component {
       <div className={`${grid.row} ${styles.wrapper}`} >
         <aside className={`${grid['col-md-4']}`}>
           {this.state.showChangeSummery}
-          <DelegateSidebar votes={votes} updateList={this.toggleSummery.bind(this)} />
+          <DelegateSidebar votes={votes} updateList={(value) => { this.toggleSummery(value); }} />
         </aside>
         <section className={`${grid['col-sm-12']} ${grid['col-md-8']}`}>
           <DelegateList showChangeSummery={this.state.showChangeSummery} />
