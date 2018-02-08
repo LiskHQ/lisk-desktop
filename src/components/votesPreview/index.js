9509c4f1841e5829b4f38dc1a54ab3d1aa715736
yasharAyari <yashar@lightcurve.io>
2018-02-08 10:20:40 +0100

    Use componentDidMount in VotesPreview component to fix a bug

--- src/components/votesPreview/index.js
+++ src/components/votesPreview/index.js
@@ -8,68 +8,73 @@ import { FontIcon } from '../fontIcon';
 import { Button } from '../toolbox/buttons/button';
 import { getTotalVotesCount, getVoteList, getUnvoteList } from './../../utils/voting';
 
-const VotesPreview = ({ votes, t, nextStep, updateList }) => {
-  const { maxCountOfVotes, maxCountOfVotesInOneTurn } = votingConst;
-  const voteList = getVoteList(votes);
-  const unvoteList = getUnvoteList(votes);
-  const totalVotesCount = getTotalVotesCount(votes);
-  const totalNewVotesCount = voteList.length + unvoteList.length;
-  const selectionClass = totalNewVotesCount > maxCountOfVotesInOneTurn ? styles.red : '';
-  const totalClass = totalVotesCount > 101 ? styles.red : '';
+class VotesPreview extends React.Component {
+  componentDidMount() {
+    this.props.updateList(false);
+  }
+  render() {
+    const { votes, t, nextStep, updateList } = this.props;
+    const { maxCountOfVotes, maxCountOfVotesInOneTurn } = votingConst;
+    const voteList = getVoteList(votes);
+    const unvoteList = getUnvoteList(votes);
+    const totalVotesCount = getTotalVotesCount(votes);
+    const totalNewVotesCount = voteList.length + unvoteList.length;
+    const selectionClass = totalNewVotesCount > maxCountOfVotesInOneTurn ? styles.red : '';
+    const totalClass = totalVotesCount > 101 ? styles.red : '';
+    const createPercentage = (count, total) => Math.ceil((count / total) * 100);
 
-  const createPercentage = (count, total) => Math.ceil((count / total) * 100);
-
-  return (<Fragment>
-    <section className={`${styles.wrapper} votes-preview`}>
-      <header>
-        <h3>{t('Votes')}</h3>
-        <a target='_blank' href='http://lisk.io' rel='noopener noreferrer'>
-          {t('Learn how Voting works')} <FontIcon>arrow-right</FontIcon>
-        </a>
-      </header>
-      <section>
-        <div className={`${styles.progressWrapper} ${selectionClass} selection-wrapper`}>
-          <CircularProgressbar
-            className={styles.progress}
-            percentage={createPercentage(totalNewVotesCount, maxCountOfVotesInOneTurn)}
-            textForPercentage={() => ''}/>
-          <article className='selection'>
-            <span>{t('Selection')} </span>
-            <h4>{totalNewVotesCount}</h4>
-            <span>/ {maxCountOfVotesInOneTurn}</span>
-          </article>
-        </div>
-        <div className={`${styles.progressWrapper} ${totalClass} ${styles.totalWrapper} total-wrapper`}>
-          <CircularProgressbar
-            className={styles.progress}
-            percentage={createPercentage(totalVotesCount, maxCountOfVotes)}
-            textForPercentage={() => ''}/>
-          <article className='total'>
-            <span>{t('Total')} </span>
-            <h4>{totalVotesCount}</h4>
-            <span>/ {maxCountOfVotes}</span>
-          </article>
-        </div>
+    return (<Fragment>
+      <section className={`${styles.wrapper} votes-preview`}>
+        <header>
+          <h2>{t('Votes')}</h2>
+          <a target='_blank' href='http://lisk.io' rel='noopener noreferrer'>
+            {t('Learn how Voting works')} <FontIcon>arrow-right</FontIcon>
+          </a>
+        </header>
+        <section>
+          <div className={`${styles.progressWrapper} ${selectionClass} selection-wrapper`}>
+            <CircularProgressbar
+              className={styles.progress}
+              percentage={createPercentage(totalNewVotesCount, maxCountOfVotesInOneTurn)}
+              textForPercentage={() => ''}/>
+            <article className='selection'>
+              <span>{t('Selection')} </span>
+              <h4>{totalNewVotesCount}</h4>
+              <span>/ {maxCountOfVotesInOneTurn}</span>
+            </article>
+          </div>
+          <div className={`${styles.progressWrapper} ${totalClass} ${styles.totalWrapper} total-wrapper`}>
+            <CircularProgressbar
+              className={styles.progress}
+              percentage={createPercentage(totalVotesCount, maxCountOfVotes)}
+              textForPercentage={() => ''}/>
+            <article className='total'>
+              <span>{t('Total')} </span>
+              <h4>{totalVotesCount}</h4>
+              <span>/ {maxCountOfVotes}</span>
+            </article>
+          </div>
+        </section>
+        <Button label={t('Next')}
+          className={`${styles.button} next`}
+          type='button'
+          onClick={() => { updateList(true); nextStep({}); }}
+          disabled={(totalNewVotesCount === 0 ||
+            totalNewVotesCount > maxCountOfVotesInOneTurn ||
+            totalVotesCount > 101)} />
       </section>
-      <Button label={t('Next')}
-        className={`${styles.button} next`}
-        type='button'
-        onClick={() => { updateList(); nextStep({}); }}
-        disabled={(totalNewVotesCount === 0 ||
-          totalNewVotesCount > maxCountOfVotesInOneTurn ||
-          totalVotesCount > 101)} />
-    </section>
-    <GradientSVG
-      id='grad'
-      rotation={0}
-      startColor='#BFF9FF'
-      endColor='#6693FF' />
-    <GradientSVG
-      id='danger'
-      rotation={0}
-      startColor='#FF6236'
-      endColor='#C80039' />
-  </Fragment>);
-};
+      <GradientSVG
+        id='grad'
+        rotation={0}
+        startColor='#BFF9FF'
+        endColor='#6693FF' />
+      <GradientSVG
+        id='danger'
+        rotation={0}
+        startColor='#FF6236'
+        endColor='#C80039' />
+    </Fragment>);
+  }
+}
 
 export default translate()(VotesPreview);
