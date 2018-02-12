4b85b8eaf630f336dd52f171120b4775972fcf14
reyraa <ali@lightcurve.io>
2018-02-12 16:13:47 +0100

    Pass layover class from the parent component(Voting)

--- src/components/delegateSidebar/index.js
+++ src/components/delegateSidebar/index.js
@@ -7,41 +7,19 @@ import PassphraseSteps from './../passphraseSteps';
 import styles from './delegateSidebar.css';
 import ResultBox from '../resultBox';
 
-class DelegateSidebar extends React.Component {
-  constructor() {
-    super();
-    this.state = { isLayover: false };
-  }
-
-  updateList(value) {
-    this.props.updateList(value);
-  }
-
-  setLayoverStatus(isLayover) {
-    if (isLayover) {
-      document.getElementById('votingComponent').classList.add('hasLayover');
-    } else {
-      document.getElementById('votingComponent').classList.remove('hasLayover');
-    }
-    this.setState({ isLayover });
-  }
-
-  render() {
-    return (
-      <Box className={`confirm-votes ${styles.box} ${this.state.isLayover ? styles.layover : ''}`}>
-        <MultiStep className={styles.wrapper} finalCallback={() => true}>
-          <VotesPreview votes={this.props.votes}
-            updateList={(value) => { this.updateList.call(this, value); }}
-            onMount={this.setLayoverStatus.bind(this)}/>
-          <PassphraseSteps onMount={this.setLayoverStatus.bind(this)} />
-          <ConfirmVotes
-            updateList={(value) => { this.updateList.call(this, value); }}
-            onMount={this.setLayoverStatus.bind(this)}/>
-          <ResultBox onMount={this.setLayoverStatus.bind(this)}/>
-        </MultiStep>
-      </Box>
-    );
-  }
-}
+const DelegateSidebar = props => (
+  <Box className={`confirm-votes ${styles.box}`}>
+    <MultiStep className={styles.wrapper} finalCallback={() => true}>
+      <VotesPreview votes={props.votes}
+        updateList={(value) => { props.updateList(value); }}
+        onMount={props.setLayover}/>
+      <PassphraseSteps onMount={props.setLayover} />
+      <ConfirmVotes
+        updateList={(value) => { props.updateList(value); }}
+        onMount={props.setLayover}/>
+      <ResultBox onMount={props.setLayover}/>
+    </MultiStep>
+  </Box>
+);
 
 export default DelegateSidebar;
--- src/components/voting/voting.js
+++ src/components/voting/voting.js
@@ -20,17 +20,24 @@ class Voting extends React.Component {
     }
   }
 
+  setLayover(isLayover) {
+    if (isLayover && this.root) {
+      this.root.classList.add(styles.hasLayover);
+    } else if (!isLayover && this.root) {
+      this.root.classList.remove(styles.hasLayover);
+    }
+  }
+
   render() {
-    const { votes } = this.props;
-    const { showChangeSummery } = this.state;
     return (
-      <div id='votingComponent' className={`${grid.row} ${styles.wrapper}`}>
+      <div className={`${grid.row} ${styles.wrapper}`} ref={(el) => { this.root = el; }}>
         <aside className={grid['col-md-4']}>
-          {showChangeSummery}
-          <DelegateSidebar votes={votes} updateList={(value) => { this.toggleSummery(value); }} />
+          <DelegateSidebar votes={this.props.votes}
+            setLayover={this.setLayover.bind(this)}
+            updateList={(value) => { this.toggleSummery(value); }} />
         </aside>
         <section className={`${grid['col-sm-12']} ${grid['col-md-8']}`}>
-          <DelegateList showChangeSummery={showChangeSummery} />
+          <DelegateList showChangeSummery={this.state.showChangeSummery} />
         </section>
       </div>
     );
