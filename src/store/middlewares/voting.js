c63d536d03d35733a0b1a02c5e5534a7407dbe53
Gina Contrino <gina@lightcurve.io>
2018-02-15 10:51:41 +0100

    Remove voting error toasts

--- src/store/middlewares/voting.js
+++ src/store/middlewares/voting.js
@@ -1,11 +1,6 @@
-import i18next from 'i18next';
-
-import { errorToastDisplayed } from '../../actions/toaster';
 import { getDelegate } from '../../utils/api/delegate';
 import { voteLookupStatusUpdated, voteToggled } from '../../actions/voting';
 import actionTypes from '../../constants/actions';
-import votingConst from '../../constants/voting';
-import { getTotalVotesCount } from './../../utils/voting';
 
 const updateLookupStatus = (store, list, username) => {
   store.dispatch(voteLookupStatusUpdated({
@@ -56,34 +51,9 @@ const lookupDelegatesFromUrl = (store, action) => {
   }
 };
 
-const checkVoteLimits = (store, action) => {
-  const { votes } = store.getState().voting;
-  const currentVote = votes[action.data.username] || { unconfirmed: true, confirmed: false };
-
-  const newVoteCount = Object.keys(votes).filter(
-    key => votes[key].confirmed !== votes[key].unconfirmed).length;
-  if (newVoteCount === votingConst.maxCountOfVotesInOneTurn + 1 &&
-        currentVote.unconfirmed !== currentVote.confirmed) {
-    const label = i18next.t('Maximum of {{n}} votes in one transaction exceeded.', { n: votingConst.maxCountOfVotesInOneTurn });
-    const newAction = errorToastDisplayed({ label });
-    store.dispatch(newAction);
-  }
-
-  const voteCount = getTotalVotesCount(votes);
-  if (voteCount === votingConst.maxCountOfVotes + 1 &&
-        currentVote.unconfirmed !== currentVote.confirmed) {
-    const label = i18next.t('Maximum of {{n}} votes exceeded.', { n: votingConst.maxCountOfVotes });
-    const newAction = errorToastDisplayed({ label });
-    store.dispatch(newAction);
-  }
-};
-
 const votingMiddleware = store => next => (action) => {
   next(action);
   switch (action.type) {
-    case actionTypes.voteToggled:
-      checkVoteLimits(store, action);
-      break;
     case actionTypes.votesAdded:
       lookupDelegatesFromUrl(store, action);
       break;
