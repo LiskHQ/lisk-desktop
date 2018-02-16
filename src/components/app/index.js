6b2946fd6dcfce1db74a9bdf2d2c7a61ab02a663
reyraa <ali@lightcurve.io>
2018-02-16 09:30:55 +0100

    Add markAsLoaded method to App to remove the spalsh screen

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -14,7 +14,6 @@ import Voting from '../voting';
 import Forging from '../forging';
 import SingleTransaction from './../singleTransaction';
 import styles from './app.css';
-import BackgroundMaker from '../backgroundMaker';
 import Dialog from '../dialog';
 import Toaster from '../toaster';
 import MainMenu from '../mainMenu';
@@ -25,48 +24,55 @@ import offlineStyle from '../offlineWrapper/offlineWrapper.css';
 import AccountVisualDemo from '../accountVisual/demo';
 import routes from '../../constants/routes';
 
-const App = () => (
-  <OfflineWrapper>
-    <BackgroundMaker />
-    <main className={`${styles.bodyWrapper}`}>
-      <MainMenu />
-      <section>
-        <div className={styles.mainBox}>
-          <Header />
-          <Switch>
-            <PrivateRoutes path='/main' render={ ({ match }) => (
-              <main className={offlineStyle.disableWhenOffline}>
-                <Switch>
-                  <Route path={`${match.url}/account-visual-demo/:dialog?`} component={AccountVisualDemo} />
-                  <Route path={`${match.url}/dashboard/:dialog?`} component={Dashboard} />
-                  <Route path={`${match.url}${routes.wallet.short}/:dialog?`} component={TransactionDashboard} />
-                  <Route path={`${match.url}/voting/:dialog?`} component={Voting} />
-                  <Route path={`${match.url}/sidechains/:dialog?`} component={Sidechains} />
-                  <Route path={`${match.url}/forging/:dialog?`} component={Forging} />
-                  <Route path='*' component={NotFound} />
-                </Switch>
-              </main>
-            )} />
-            <Route path='/explorer' render={ ({ match }) => (
-              <main>
-                <Route path={`${match.url}${routes.search.short}/:dialog?`} component={Search} />
-                <Route path={`${match.url}${routes.searchResult.short}/:query?`} component={SearchResult} />
-                <Route path={`${match.url}${routes.account.short}/:address?`} component={AccountTransactions} />
-                <Route path={`${match.url}${routes.transaction.short}/:id`} component={SingleTransaction} />
-              </main>
-            )} />
-            <Route exact path={routes.register.url} component={Register} />
-            <Route path={`${routes.addAccount.url}:dialog?`} component={Login} />
-            <Route exact path={routes.login.url} component={Login} />
-            <Route path='*' component={NotFound} />
-          </Switch>
-        </div>
-        <Dialog />
-        <Toaster />
-        <LoadingBar />
-      </section>
-    </main>
-  </OfflineWrapper>
-);
+class App extends React.Component {
+  markAsLoaded() {
+    this.main.classList.add(styles.loaded, 'appLoaded');
+  }
+
+  render() {
+    return (
+      <OfflineWrapper>
+        <main className={`${styles.bodyWrapper}`} ref={(el) => { this.main = el; }}>
+          <MainMenu />
+          <section>
+            <div className={styles.mainBox}>
+              <Header />
+              <Switch>
+                <PrivateRoutes path='/main' render={ ({ match }) => (
+                  <main className={offlineStyle.disableWhenOffline}>
+                    <Switch>
+                      <Route path={`${match.url}/account-visual-demo/:dialog?`} component={AccountVisualDemo} />
+                      <Route path={`${match.url}/dashboard/:dialog?`} component={Dashboard} />
+                      <Route path={`${match.url}${routes.wallet.short}/:dialog?`} component={TransactionDashboard} />
+                      <Route path={`${match.url}/voting/:dialog?`} component={Voting} />
+                      <Route path={`${match.url}/sidechains/:dialog?`} component={Sidechains} />
+                      <Route path={`${match.url}/forging/:dialog?`} component={Forging} />
+                      <Route path='*' component={NotFound} />
+                    </Switch>
+                  </main>
+                )} />
+                <Route path='/explorer' render={ ({ match }) => (
+                  <main>
+                    <Route path={`${match.url}${routes.search.short}/:dialog?`} component={Search} />
+                    <Route path={`${match.url}${routes.searchResult.short}/:query?`} component={SearchResult} />
+                    <Route path={`${match.url}${routes.account.short}/:address?`} component={AccountTransactions} />
+                    <Route path={`${match.url}${routes.transaction.short}/:id`} component={SingleTransaction} />
+                  </main>
+                )} />
+                <Route exact path={routes.register.url} component={Register} />
+                <Route path={`${routes.addAccount.url}:dialog?`} component={Login} />
+                <Route exact path={routes.login.url} component={Login} />
+                <Route path='*' component={NotFound} />
+              </Switch>
+            </div>
+            <Dialog />
+            <Toaster />
+            <LoadingBar markAsLoaded={this.markAsLoaded.bind(this)} />
+          </section>
+        </main>
+      </OfflineWrapper>
+    );
+  }
+}
 
 export default App;
