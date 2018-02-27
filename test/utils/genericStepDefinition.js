4bea22439d1c1b0ef2c6676fa51941a3604645d4
yasharAyari <yashar@lightcurve.io>
2018-02-27 12:22:41 +0100

    Move utils class to genericStepDefinition.js

---
+++ test/utils/genericStepDefinition.js
@@ -0,0 +1,44 @@
+
+import { expect } from 'chai';
+
+export default class GenericStepDefinition {
+  constructor(input) {
+    this.wrapper = input;
+  }
+  /**
+   * simulate click on a dom query
+   * @param {String} query - dom query that we need to simulate clink on it 
+   */
+  clickOnElement(query) {
+    this.wrapper.find(query).simulate('click');
+  }
+  /**
+   * check that dom query entry is disable or enable
+   * if value of status is equal to 'not' query shouldn't be disabled
+   * @param {String} query - dom query that we need to check that is disable or not
+   * @param {String} status - possible values are 'not' and ''
+   */
+  checkDisableInput(query, status = '') {
+    if (status === 'not') {
+      expect(this.wrapper.find(query)).to.not.be.disabled();
+    } else {
+      expect(this.wrapper.find(query)).to.be.disabled();
+    }
+  }
+  /**
+   * 
+   * @param {String} query - dom query that we need to check length of that
+   * @param {Integer} length 
+   */
+  haveLengthOf(query, length) {
+    expect(this.wrapper).to.have.exactly(length).descendants(query);
+  }
+  /**
+   * 
+   * @param {String} query - dom query that we need to check text of that
+   * @param {String} text - expect text of the dom query entry
+   */
+  haveTextOf(query, text) {
+    expect(this.wrapper.find(query)).to.have.text(text);
+  }
+}
--- test/utils/mountHelpers.js
+++ test/utils/mountHelpers.js
@@ -2,7 +2,6 @@ import { spy } from 'sinon';
 import { mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import PropTypes from 'prop-types';
-import { expect } from 'chai';
 
 import i18n from '../../src/i18n';
 
@@ -35,31 +34,3 @@ export const mountWithContext = (component, { storeState = {}, location = {} }) 
   return mount(component, options);
 };
 
-
-export class Utils {
-  constructor(input) {
-    this.wrapper = input;
-  }
-
-  clickOnElement(query) {
-    this.wrapper.find(query).simulate('click');
-  }
-  goToConfirmation() {
-    expect(this.wrapper.find('button.confirm')).to.be.not.present();
-    this.wrapper.find('button.next').simulate('click');
-    expect(this.wrapper.find('button.confirm')).to.be.present();
-  }
-  checkDisableInput(query, status = '') {
-    if (status === 'not') {
-      expect(this.wrapper.find(query)).to.not.be.disabled();
-    } else {
-      expect(this.wrapper.find(query)).to.be.disabled();
-    }
-  }
-  haveLengthOf(query, length) {
-    expect(this.wrapper).to.have.exactly(length).descendants(query);
-  }
-  haveTextOf(query, text) {
-    expect(this.wrapper.find(query)).to.have.text(text);
-  }
-}
