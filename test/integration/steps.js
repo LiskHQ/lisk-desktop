03eb86d5e7dd4ec3eacd80ecdef5b36b84283e76
reyraa <ali@lightcurve.io>
2018-02-28 10:17:06 +0100

    Create some generic steps

--- test/integration/steps.js
+++ test/integration/steps.js
@@ -10,3 +10,12 @@ export const containsMessage = (wrapper, elementName, message) => {
   const selector = `.${elementName.replace(/ /g, '-')}`;
   expect(wrapper.find(selector).first().text()).to.contain(message);
 };
+
+export const fillInputField = (wrapper, value, field) => {
+  wrapper.find(`.${field} input`).first().simulate('change', { target: { value } });
+};
+
+export const selectOptionItem = (wrapper, optionIndex, field) => {
+  const selector = `.${field.replace(/ /g, '-')} ul li`;
+  wrapper.find(selector).at(parseInt(optionIndex, 10) - 1).simulate('click');
+};
