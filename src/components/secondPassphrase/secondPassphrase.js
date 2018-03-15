1e2c08fceacd231dbccd52da8d031eab69c70a18
yasharAyari <yashar@lightcurve.io>
2018-03-15 17:49:57 +0100

    Compelete SecondPassphrase component

--- src/components/secondPassphrase/secondPassphrase.js
+++ src/components/secondPassphrase/secondPassphrase.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import Fees from '../../constants/fees';
-import Authenticate from '../authenticate';
+// import Fees from '../../constants/fees';
 import MultiStep from '../multiStep';
-import Info from '../passphrase/info';
-import Create from '../passphrase/create';
+// import Info from '../passphrase/info';
+import CreateSecond from '../passphrase/createSecond';
 import Safekeeping from '../passphrase/safekeeping';
 import Confirm from '../passphrase/confirm';
+import Box from '../box';
+import styles from './secondPassphrase.css';
 
 const SecondPassphrase = ({
-  passphrase, account, peers, registerSecondPassphrase, closeDialog, t,
+  account, peers, registerSecondPassphrase, t,
 }) => {
-  const onPassphraseRegister = (secondPassphrase) => {
+  const onPassphraseRegister = (secondPassphrase, passphrase) => {
     registerSecondPassphrase({
       activePeer: peers.data,
       secondPassphrase,
+      passphrase,
       account,
     });
   };
 
-  const useCaseNote = t('your second passphrase will be required for all transactions sent from this account');
-  const securityNote = t('Losing access to this passphrase will mean no funds can be sent from this account.');
+  // const useCaseNote = 
+  // t('your second passphrase will be required for all transactions sent from this account');
+  // const securityNote = 
+  // t('Losing access to this passphrase will mean no funds can be sent from this account.');
 
   return (
-    typeof passphrase === 'string' && passphrase.length > 0 ?
-      <MultiStep showNav={false} finalCallback={onPassphraseRegister}>
-        <Info title='Info' t={t} icon='bookmark_border' fee={Fees.setSecondPassphrase}
-          useCaseNote={useCaseNote} securityNote={securityNote} backButtonFn={closeDialog} />
-        <Create title='Create' t={t} icon='vpn_key' />
-        <Safekeeping title='Safekeeping' t={t} icon='done' />
-        <Confirm title='Confirm' t={t} confirmButton='Register' icon='launch' />
-      </MultiStep> :
-      <Authenticate nextAction={t('set second passphrase')} />);
+    <Box className={`${styles.hasPaddingTop} ${styles.register}`}>
+      <MultiStep
+        showNav={true}
+        finalCallback={onPassphraseRegister}
+        backButtonLabel={t('Back')}>
+        <CreateSecond title='Create' t={t} icon='add' />
+        <Safekeeping title='Safekeeping' t={t} icon='checkmark' />
+        <Confirm title='Confirm' t={t} confirmButton='Register'
+          icon='login' secondPassConfirmation={true} />
+      </MultiStep>
+    </Box>);
 };
 
 export default SecondPassphrase;
