e3c6eb4cf306a6ee32334d925cdf43dc1ee01b5c
Gina Contrino <gina@lightcurve.io>
2018-03-29 15:12:38 +0200

    Improve reference to onboarding component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -39,9 +39,7 @@ class App extends React.Component {
   render() {
     return (
       <OfflineWrapper>
-        <Onboarding appLoaded={this.appLoaded} ref={(el) => {
-          if (el) { this.onboarding = el.getWrappedInstance().getWrappedInstance(); }
-        }} />
+        <Onboarding appLoaded={this.appLoaded} onRef={(ref) => { this.onboarding = ref; }}/>
         <main className={`${styles.bodyWrapper}`} ref={(el) => { this.main = el; }}>
           <MainMenu startOnboarding={this.startOnboarding.bind(this)}/>
           <Route path={routes.accounts.path} component={SavedAccounts} />
