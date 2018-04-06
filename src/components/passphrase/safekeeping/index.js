9fc58c3520f2bb9f94ab13149e49b790874f5fe3
yasharAyari <yashar@lightcurve.io>
2018-04-06 12:14:00 +0200

    Fix a bug in safekeeping component

--- src/components/passphrase/safekeeping/index.js
+++ src/components/passphrase/safekeeping/index.js
@@ -10,6 +10,7 @@ class SafeKeeping extends React.Component {
     super();
     this.state = {
       step: '',
+      isSafe: false,
     };
   }
 
@@ -31,7 +32,7 @@ class SafeKeeping extends React.Component {
   }
 
   done() {
-    this.setState({ step: 'done-step' });
+    this.setState({ step: 'done-step', isSafe: true });
     setTimeout(() => {
       this.props.nextStep({ passphrase: this.props.passphrase });
     }, 400);
@@ -110,6 +111,7 @@ class SafeKeeping extends React.Component {
                 onClick: () => prevStep({ jump: 2 }),
               }}
               primaryButton={{
+                disabled: this.state.isSafe,
                 label: t('Yes! It\'s safe'),
                 className: 'next-button yes-its-safe-button',
                 onClick: this.done.bind(this),
