4b958d1293e39606e404b508f23206f3114f1335
michaeltomasik <shugo12345@gmail.com>
2018-04-12 08:19:02 +0200

    :white_check_mark: Fix failing test

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,14 +1,14 @@
 export const isPathCorrect = (location, explorerRoutes) => {
-  const locationElements = location.pathname.split('/').slice(2);
+  const locationElements = location.pathname.split('/').slice(2).filter(x => x);
   const isValid = explorerRoutes.find((route) => {
     const fullRouteName = route.path + (route.pathSuffix || '');
     const routeElements = fullRouteName.split('/').slice(1);
     return locationElements[0] === routeElements[0]
-    && locationElements[locationElements.length - 1] !== ''
+    && (locationElements[locationElements.length - 1] !== '')
     && ((locationElements.length === 2 && !!route.pathSuffix)
       || (locationElements.length < 2 && !route.pathSuffix));
   });
-  return isValid;
+  return isValid ? location.pathname : false;
 };
 
 export default isPathCorrect;
--- src/utils/app.test.js
+++ src/utils/app.test.js
@@ -8,23 +8,24 @@ describe('App Util', () => {
     const explorerRoutes = allRoutes.filter(routeObj =>
       routeObj.pathPrefix && routeObj.pathPrefix === routes.explorer.path);
 
-    const invalidURLs = ['/explorer/search/123', '/explorer/search/',
+    const invalidURLs = ['/explorer/search/123',
       '/explorer/accounts/16313739661670634666L/hi'];
-    for (let i = 0; i >= invalidURLs.length; i++) {
-      const data = isPathCorrect(invalidURLs[i], explorerRoutes);
-      expect(data).to.be.equal(undefined);
+    for (let i = 0; i < invalidURLs.length; i++) {
+      const data = isPathCorrect({ pathname: invalidURLs[i] }, explorerRoutes);
+      expect(data).to.be.equal(false);
     }
   });
 
-  it('should detach wrong urls', () => {
+  it('should detach right urls', () => {
     const allRoutes = Object.values(routes);
     const explorerRoutes = allRoutes.filter(routeObj =>
       routeObj.pathPrefix && routeObj.pathPrefix === routes.explorer.path);
 
-    const invalidURLs = ['/explorer/search', '/explorer/accounts/16313739661670634666L'];
-    for (let i = 0; i >= invalidURLs.length; i++) {
-      const data = isPathCorrect(invalidURLs[i], explorerRoutes);
-      expect(data).to.not.be.equal(undefined);
+    const invalidURLs = ['/explorer/search', '/explorer/search/',
+      '/explorer/accounts/16313739661670634666L'];
+    for (let i = 0; i < invalidURLs.length; i++) {
+      const data = isPathCorrect({ pathname: invalidURLs[i] }, explorerRoutes);
+      expect(data).to.be.equal(invalidURLs[i]);
     }
   });
 });
