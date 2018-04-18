28f291ef286b0ecb9fa4dfd0c69c6bff5f956b76
Pablo Molina <pablo@lightcurve.io>
2018-04-18 15:32:47 +0200

    :white_check_mark: registerDelegate, integration and unit above covereage limits

--- src/components/registerDelegate/registerDelegate.test.js
+++ src/components/registerDelegate/registerDelegate.test.js
@@ -1,70 +1,61 @@
 import React from 'react';
 import { expect } from 'chai';
-// import { mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { spy } from 'sinon';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
-import { BrowserRouter as Router } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Router } from 'react-router-dom';
 import configureMockStore from 'redux-mock-store';
-
-import { mountWithContext } from './../../../test/utils/mountHelpers';
-import i18n from '../../i18n'; // initialized i18next instance
-import RegisterDelegateHOC from './index';
+import i18n from '../../i18n';
+import RegisterDelegate from './registerDelegate';
 
 let store;
 
-const history = {
-  location: {
-    pathname: '/dashboard',
-  },
-  goBack: spy(),
+const account = {
+  balance: 11000,
 };
-const props = {
-  closeDialog: () => {},
-  history,
-  t: key => key,
+const delegate = {};
+const browserHistory = {
+  location: {
+    pathname: '/regiser-delegate',
+  },
+  listen: () => {},
+  goBack: spy(),
 };
 
 describe('RegisterDelegate', () => {
   let wrapper;
 
   const peers = { data: {} };
-  const account = {};
-  const delegate = {};
   store = configureMockStore([])({
     peers,
     account,
     delegate,
     activePeerSet: () => {},
-    history,
   });
+
+  const props = {
+    account,
+    delegate,
+    closeDialog: () => {},
+    history,
+    t: key => key,
+  };
 
   beforeEach(() => {
-    const options = {
-      context: { store, history, i18n },
-      childContextTypes: {
-        store: PropTypes.object.isRequired,
-        history: PropTypes.object.isRequired,
-        i18n: PropTypes.object.isRequired,
-      },
-    };
-
-    wrapper = mountWithContext(<Provider store={store}>
-      <Router>
-        <I18nextProvider i18n={ i18n }>
-          <RegisterDelegateHOC {...props} />
-        </I18nextProvider>
-      </Router>
-    </Provider>, options);
+    wrapper = mount(
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <I18nextProvider i18n={ i18n }>
+            <RegisterDelegate {...props} />
+          </I18nextProvider>
+        </Router>
+      </Provider>);
   });
 
-  // TODO: handle this in integration
-  it.skip('allows to go back to previous screen', () => {
+  it('allows to go back to previous screen', () => {
     const backButton = wrapper.find('.multistep-back');
-    wrapper.setProps({ history });
-    wrapper.update();
     backButton.first().simulate('click');
-    expect(history.goBack).to.have.been.calledWith();
+    expect(browserHistory.goBack).to.have.been.calledWith();
   });
 });
