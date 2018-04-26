3f749846df55448d884107ecef64a6da724023cf
reyraa <ali@lightcurve.io>
2018-04-26 10:37:14 +0200

    Create dedicated component for vote items

---
+++ src/components/votingListView/voteRow.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import styles from './votingListView.css';
+
+class VotingRow extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  shouldComponentUpdate({ data }) {
+    const oldData = this.props.data;
+    return (!oldData && !!data) ||
+      (!!oldData && !data) ||
+      ((!!oldData && !!data) &&
+      (oldData.unconfirmed !== data.unconfirmed));
+  }
+
+  render() {
+    const { data, className, username } = this.props;
+    const voteType = data.unconfirmed ? styles.upVoteRow : styles.downVoteRow;
+
+    return (<ul className={`delegate-row selected-row ${styles.row} ${grid.row} ${className} ${voteType}`}>
+      <li className={`${grid['col-md-1']} ${grid['col-xs-2']}`}>{data.rank}</li>
+      <li className={`${grid['col-md-3']} ${grid['col-xs-5']} ${styles.username}`}>{username}</li>
+      <li className={`${grid['col-md-5']}`}>{data.address}</li>
+      <li className={`${grid['col-md-2']} ${grid['col-xs-3']} ${styles.productivity}`}>{data.productivity} %</li>
+    </ul>
+    );
+  }
+}
+
+export default VotingRow;
