e83c87e60f10d094feb610fade7f4c4c183d1f2e
reyraa <ali@lightcurve.io>
2018-04-26 10:38:39 +0200

    Export presentational list titles to listLabels Component

---
+++ src/components/votingListView/listLabels.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ListLabels = ({ styles, grid, status, t }) =>
+  (<ul className={`${styles.tableHead} ${grid.row}`}>
+    {
+      !status ?
+        <li className={`${grid['col-md-1']}  ${grid['col-xs-2']} ${styles.leftText}`}>
+          {t('Vote', { context: 'verb' })}
+        </li> : null
+    }
+    <li className={`${grid['col-md-1']} ${grid['col-xs-2']}`}>{t('Rank')}</li>
+    <li className={`${grid['col-md-3']} ${grid['col-xs-5']}`}>{t('Name')}</li>
+    <li className={`${grid['col-md-5']}`}>{t('Lisk ID')}</li>
+    <li className={`${grid['col-md-2']} ${grid['col-xs-3']} ${styles.productivity}`}>{t('Productivity')}</li>
+  </ul>);
+
+export default ListLabels;
