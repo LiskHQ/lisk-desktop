f039afb87eda53049f009013d8448d277fe9ea2f
michaeltomasik <shugo12345@gmail.com>
2018-05-02 09:28:16 +0200

    :seedling: Add filter voters&votes

--- src/components/transactions/delegateStatistics.js
+++ src/components/transactions/delegateStatistics.js
@@ -12,11 +12,13 @@ class DelegateStatistics extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showVotesNumber: 35,
       showVotersNumber: 35,
-      loadAll: {
-        votes: false,
-        voters: false,
-      },
+      loadAllVotes: false,
+      votersFilterQuery: '',
+      votesFilterQuery: '',
+      votersSize: props.voters.length,
+      votesSize: props.votes.length,
     };
   }
 
@@ -24,14 +26,21 @@ class DelegateStatistics extends React.Component {
     this.setState({ showVotersNumber: this.state.showVotersNumber + 125 });
   }
 
-  showAll(dataName) {
-    this.setState({ loadAll: { ...this.state.loadAll, [dataName]: true } });
+  showAll() {
+    this.setState({ loadAllVotes: true });
+  }
+
+  // putting <span>•</span> inbetween array objects
+  putDotsInbetween(data) { // eslint-disable-line class-methods-use-this
+    return data && data
+      .reduce((arr, val) => [...arr, val, <span className={styles.dot} key={`span-${new Date().valueOf() * Math.random()}`}>• </span>], [])
+      .slice(0, -1);
   }
 
   getInterspersed(dataName) {
     let data = this.props[dataName];
     // data = this.props[dataName] && !this.state.loadAll[dataName] ? data.slice(0, 35) : data;
-    data = this.props[dataName] && this.state.loadAll[dataName] ? this.props[dataName] : data;
+    // data = this.props[dataName] && this.state.loadAll[dataName] ? this.props[dataName] : data;
     data = data ? data.map((user, key) => (
       <Link className={`${styles.addressLink} ${styles.clickable} voter-address`}
         to={`${routes.explorer.path}${routes.accounts.path}/${user.address}`}
@@ -39,17 +48,43 @@ class DelegateStatistics extends React.Component {
         {`${user.username || user.address} `}
       </Link>
     )) : [];
-    // putting <span>•</span> inbetween array objects
-    const intersperse = data && data
-      .reduce((arr, val) => [...arr, val, <span className={styles.dot} key={`span-${new Date().valueOf() * Math.random()}`}>• </span>], [])
-      .slice(0, -1);
-    return intersperse;
+
+    return this.putDotsInbetween(data);
+  }
+
+  search(filterName, e) {
+    const { value } = e.target;
+    this.setState({
+      [filterName]: value,
+    });
   }
 
   render() {
-    const { delegate } = this.props;
-    const votesInterspered = this.getInterspersed('votes');
-    const votersInterspered = this.getInterspersed('voters');
+    const { delegate, t } = this.props;
+    let votesInterspered = this.getInterspersed('votes');
+    let votersInterspered = this.getInterspersed('voters');
+
+    if (this.state.votesFilterQuery !== '') {
+      votesInterspered = votesInterspered.filter((votes) => {
+        const name = votes.props.children.trim();
+        return name.includes(this.state.votesFilterQuery);
+      });
+      votesInterspered = this.putDotsInbetween(votesInterspered);
+    }
+
+    if (this.state.votersFilterQuery !== '') {
+      votersInterspered = votersInterspered.filter((voters) => {
+        const name = voters.props.children.trim();
+        return name.includes(this.state.votersFilterQuery);
+      });
+      votersInterspered = this.putDotsInbetween(votersInterspered);
+    }
+
+    if (votesInterspered) {
+      votesInterspered = this.state.loadAllVotes ? votesInterspered :
+        votesInterspered.slice(0, this.state.showVotesNumber);
+    }
+
     const missed = this.props.t('missed');
     return (
       <div className={`${styles.details}`}>
@@ -81,23 +116,44 @@ class DelegateStatistics extends React.Component {
         </div>
         <div className={`transactions-detail-view ${grid.row} ${grid['between-md']} ${grid['between-sm']}`}>
           <div className={`${grid['col-xs-12']} ${grid['col-sm-12']} ${grid['col-md-12']}`}>
-            <div className={styles.label}>{this.props.t('Votes of this accont')}</div>
-            <div className={styles.value}>{votesInterspered}</div>
-            {!this.state.loadAll.votes ?
-              <div onClick={() => { this.showAll('votes'); }}>Show All</div> : ''
+            <div className={styles.label}>
+              {this.props.t('Votes of this accont')}
+              {`(${this.state.votesSize})`}
+              <input
+                type='text'
+                name='queryVotes'
+                value={this.state.votesFilterQuery}
+                onChange={this.search.bind(this, 'votesFilterQuery')}
+                placeholder={t('Filter votes')}/>
+            </div>
+            <div className={styles.value}>
+              {votesInterspered}
+            </div>
+            {!this.state.loadAllVotes ?
+              <div onClick={() => { this.showAll(); }}>Show All</div> : ''
             }
           </div>
         </div>
         <div className={`transactions-detail-view ${grid.row} ${grid['between-md']} ${grid['between-sm']}`}>
           <div className={`${grid['col-xs-12']} ${grid['col-sm-12']} ${grid['col-md-12']}`}>
-            <div className={styles.label}>{this.props.t('Who voted to this delegate')}</div>
+            <div className={styles.label}>
+              {this.props.t('Who voted to this delegate')}
+              {`(${this.state.votersSize})`}
+              <input
+                type='text'
+                name='queryVoters'
+                value={this.state.votersFilterQuery}
+                onChange={this.search.bind(this, 'votersFilterQuery')}
+                placeholder={t('Filter voters')}/>
+            </div>
             <div className={styles.value}>
-              {votersInterspered && votersInterspered.slice(0, this.state.showVotersNumber)}
+              {votersInterspered && votersInterspered
+                .slice(0, this.state.showVotersNumber)}
             </div>
           </div>
         </div>
         <Waypoint
-          bottomOffset='-60%'
+          bottomOffset='-20%'
           key='delegate-statistics'
           onEnter={() => {
             this.loadMore();
