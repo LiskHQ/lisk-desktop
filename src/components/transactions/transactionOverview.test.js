04ed06c9ce1b835e170650d3ae2361c8db44e2b5
Pablo Molina <pablo@lightcurve.io>
2018-05-09 12:19:44 +0200

    :white_check_mark: fix transactionOverview tests

--- src/components/transactions/transactionOverview.test.js
+++ src/components/transactions/transactionOverview.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { mountWithContext } from './../../../test/utils/mountHelpers';
 import TransactionOverview from './transactionOverview';
+import store from '../../store';
+import accounts from './../../../test/constants/accounts';
 
 describe('TransactionOverview', () => {
   it('should render Waypoint on smallScreen', () => {
@@ -9,10 +11,24 @@ describe('TransactionOverview', () => {
     const props = {
       t: () => {},
       loading: [],
-      transactions: [],
-      peers: {},
+      peers: {
+        data: {},
+      },
+      address: accounts.genesis.address,
+      loadTransactions: () => {},
     };
-    const wrapper = shallow(<TransactionOverview {...props} />);
+    store.getState = () => ({
+      peers: { status: {}, options: {}, data: {} },
+      transactions: {
+        confirmed: [],
+      },
+      account: {
+        address: accounts.genesis.address,
+      },
+      search: {},
+    });
+    const wrapper = mountWithContext(<TransactionOverview {...props} store={store} />,
+      { storeState: store });
     expect(wrapper).to.have.descendants('Waypoint');
   });
 });
