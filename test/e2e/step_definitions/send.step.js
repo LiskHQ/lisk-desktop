24f237bb20d9ea8e8c4daa39b51ca5c92353513c
michaeltomasik <shugo12345@gmail.com>
2018-05-16 10:51:53 +0200

    :recycle: Change to more readabel test name for converter match price

--- test/e2e/step_definitions/send.step.js
+++ test/e2e/step_definitions/send.step.js
@@ -3,7 +3,7 @@ const { defineSupportCode } = require('cucumber');
 const { waitForElemAndMatchItsText } = require('../support/util.js');
 
 defineSupportCode(({ Then }) => {
-  Then('I should see "{elementName}" element with text matching validator format', (elementName, callback) => {
+  Then('I should see "{elementName}" element with text matching coverter price', (elementName, callback) => {
     const regexp = /^~ \d{0,100}(\.\d{0,2}){0,1}$/;
     const selectorClass = `.${elementName.replace(/ /g, '-')}`;
     waitForElemAndMatchItsText(selectorClass, regexp, callback);
