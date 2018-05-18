0cf2e55eb09a68d326e0368d398ffd2e690c9802
Pablo Molina <pablo@lightcurve.io>
2018-05-18 11:33:20 +0200

    :white_check_mark: increase tests for transactionOverview

--- src/components/transactions/transactionOverview.test.js
+++ src/components/transactions/transactionOverview.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { expect } from 'chai';
+import { spy } from 'sinon';
 import { mountWithContext } from './../../../test/utils/mountHelpers';
 import TransactionOverview from './transactionOverview';
 import store from '../../store';
 import accounts from './../../../test/constants/accounts';
 
-describe('TransactionOverview', () => {
+describe.only('TransactionOverview', () => {
   let wrapper;
   let props;
+  let onInitSpy;
+  let onLoadMoreSpy;
+  let onFilterSetSpy;
 
   beforeEach(() => {
     window.innerWidth = 200;
@@ -17,8 +21,12 @@ describe('TransactionOverview', () => {
       peers: {
         data: {},
       },
+      transactions: [{ id: '13395546734664987127' }],
+      count: 1000,
       address: accounts.genesis.address,
-      loadTransactions: () => {},
+      onInit: () => {},
+      onLoadMore: () => {},
+      onFilterSet: () => {},
     };
     store.getState = () => ({
       peers: { status: {}, options: {}, data: {} },
@@ -30,6 +38,9 @@ describe('TransactionOverview', () => {
       },
       search: {},
     });
+    onInitSpy = spy(props, 'onInit');
+    onLoadMoreSpy = spy(props, 'onLoadMore');
+    onFilterSetSpy = spy(props, 'onFilterSet');
     wrapper = mountWithContext(<TransactionOverview {...props} store={store} />,
       { storeState: store });
   });
@@ -37,4 +48,26 @@ describe('TransactionOverview', () => {
   it('should render Waypoint on smallScreen', () => {
     expect(wrapper).to.have.descendants('Waypoint');
   });
+
+  it('should show empty state when no transactions', () => {
+    wrapper.setProps({ transactions: [] });
+    wrapper.update();
+    expect(wrapper.find('EmptyState')).to.be.present();
+  });
+
+  /* eslint-disable no-unused-expressions */
+  it('should call onInit on constructor call', () => {
+    expect(onInitSpy).to.have.been.calledOnce;
+  });
+
+  it('should call onLoadMore when Waypoint reached', () => {
+    wrapper.find('Waypoint').props().onEnter();
+    expect(onLoadMoreSpy).to.have.been.calledOnce;
+  });
+
+  it('should call onFilterSet when filtering transations', () => {
+    wrapper.find('.transaction-filter-item').first().simulate('click');
+    expect(onFilterSetSpy).to.have.been.calledOnce;
+  });
+  /* eslint-enable no-unused-expressions */
 });
