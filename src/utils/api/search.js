0ca152ab06f9ba9f67355dc0f9cf5def06a0e0f0
Pablo Molina <pablo@lightcurve.io>
2018-05-28 17:39:15 +0200

    :recycle: adapt search api to only respond with needed keys

--- src/utils/api/search.js
+++ src/utils/api/search.js
@@ -1,23 +1,39 @@
 import { requestToActivePeer } from './peers';
 import regex from './../../utils/regex';
 
+const reducers = {
+  addresses: ['address', 'balance'],
+  delegates: ['username', 'rank', 'address'],
+  transactions: ['id', 'height'],
+};
+const reduceResponseProps = (response, key, reducerKeys) => {
+  const reduced = response.map(result =>
+    Object.keys(result)
+      .filter(objKey => reducerKeys[key].includes(objKey))
+      .reduce((obj, objKey) => {
+        obj[objKey] = result[objKey];
+        return obj;
+      }, {}));
+  return reduced;
+};
+
 export const searchAddresses = ({ activePeer, search }) => new Promise((resolve, reject) =>
   requestToActivePeer(activePeer, 'accounts', {
     address: search,
-  }).then(response => resolve({ addresses: [response.account] }))
+  }).then(response => resolve({ addresses: reduceResponseProps([response.account], 'addresses', reducers) }))
     .catch(() => reject({ addresses: [] })));
 
 export const searchDelegates = ({ activePeer, search }) => new Promise((resolve, reject) =>
   requestToActivePeer(activePeer, 'delegates/search', {
     q: search,
     orderBy: 'username:asc',
-  }).then(response => resolve({ delegates: response.delegates }))
+  }).then(response => resolve({ delegates: reduceResponseProps(response.delegates, 'delegates', reducers) }))
     .catch(() => reject({ delegates: [] })));
 
 export const searchTransactions = ({ activePeer, search }) => new Promise((resolve, reject) =>
   requestToActivePeer(activePeer, 'transactions/get', {
     id: search,
-  }).then(response => resolve({ transactions: [response.transaction] }))
+  }).then(response => resolve({ transactions: reduceResponseProps([response.transaction], 'transactions', reducers) }))
     .catch(() => reject({ transactions: [] })));
 
 export const getSearches = (search) => {
--- src/utils/api/search.test.js
+++ src/utils/api/search.test.js
@@ -6,11 +6,23 @@ import * as peersAPI from './peers';
 describe('Utils: Search', () => {
   let peersAPIStub;
 
-  const accountsResponse = { account: { address: '1337L' } };
+  const accountsResponse = { account: { address: '1337L', balance: 1110, someOtherProperty: 'other property' } };
+  const accountExpectedResponse = { account: { address: '1337L', balance: 1110 } };
   const accountsUrlParams = { address: '1337L' };
   const accountsUrlParamsTxMatch = { address: '1337' };
 
-  const delegatesResponse = { delegates: [{ username: '1337', rank: 18 }, { username: '1337l', rank: 19 }] };
+  const delegatesResponse = {
+    delegates: [
+      { username: '1337', rank: 18, address: '123456', someOtherProperty: 'other property' },
+      { username: '1337l', rank: 19, address: '123456', someOtherProperty: 'other property' },
+    ],
+  };
+  const delegatesExpectedResponse = {
+    delegates: [
+      { username: '1337', rank: 18, address: '123456' },
+      { username: '1337l', rank: 19, address: '123456' },
+    ],
+  };
   const delegatesUrlParams = {
     q: '1337L',
     orderBy: 'username:asc',
@@ -20,7 +32,8 @@ describe('Utils: Search', () => {
     orderBy: 'username:asc',
   };
 
-  const transactionsResponse = { transaction: { id: '1337' } };
+  const transactionsResponse = { transaction: { id: '1337', height: 99, someOtherProperty: 'other property' } };
+  const transactionsExpectedResponse = { transaction: { id: '1337', height: 99 } };
   const transactionsUrlParams = {
     id: '1337L',
   };
@@ -47,20 +60,20 @@ describe('Utils: Search', () => {
 
   it('should search {addresses,delegates} when only address pattern matched', () =>
     expect(searchAll({ search: '1337L' })).to.eventually.deep.equal([
-      { addresses: [accountsResponse.account] },
-      { delegates: delegatesResponse.delegates },
+      { addresses: [accountExpectedResponse.account] },
+      { delegates: delegatesExpectedResponse.delegates },
     ]));
 
   it('should search {transactions,delegates} when only transaction pattern matched', () =>
     expect(searchAll({ search: '1337' })).to.eventually.deep.equal([
-      { transactions: [transactionsResponse.transaction] },
-      { delegates: delegatesResponse.delegates },
+      { transactions: [transactionsExpectedResponse.transaction] },
+      { delegates: delegatesExpectedResponse.delegates },
     ]));
 
   it('should still search for {addresses} when failing {delegates} request', () => {
     peersAPIStub.withArgs(undefined, 'delegates/search', delegatesUrlParams).returnsPromise().rejects({ success: false });
     return expect(searchAll({ search: '1337L' })).to.eventually.deep.equal([
-      { addresses: [accountsResponse.account] },
+      { addresses: [accountExpectedResponse.account] },
       { delegates: [] },
     ]);
   });
@@ -69,7 +82,7 @@ describe('Utils: Search', () => {
     peersAPIStub.withArgs(undefined, 'accounts', accountsUrlParams).returnsPromise().rejects({ success: false });
     return expect(searchAll({ search: '1337L' })).to.eventually.deep.equal([
       { addresses: [] },
-      { delegates: delegatesResponse.delegates },
+      { delegates: delegatesExpectedResponse.delegates },
     ]);
   });
 
@@ -77,7 +90,7 @@ describe('Utils: Search', () => {
     peersAPIStub.withArgs(undefined, 'transactions/get', transactionsUrlParamsTxMatch).returnsPromise().rejects({ success: false });
     return expect(searchAll({ search: '1337' })).to.eventually.deep.equal([
       { transactions: [] },
-      { delegates: delegatesResponse.delegates },
+      { delegates: delegatesExpectedResponse.delegates },
     ]);
   });
 });
