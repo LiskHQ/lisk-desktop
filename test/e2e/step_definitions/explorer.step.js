1f59896aaab3a2fb7c09cdd4ddf215586619482e
Pablo Molina <pablo@lightcurve.io>
2018-06-15 11:31:32 +0200

    :white_check_mark: fix e2e explorer feature

--- test/e2e/step_definitions/explorer.step.js
+++ test/e2e/step_definitions/explorer.step.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { defineSupportCode } = require('cucumber');
-const { waitForElemAndCheckItsText, waitForElemAndSendKeys } = require('../support/util.js');
+const { waitForElemAndCheckItsText, waitForElem } = require('../support/util.js');
 
 defineSupportCode(({ Then }) => {
   Then('I should see ID "{text}" in transaction header', (text, callback) => {
@@ -8,9 +8,13 @@ defineSupportCode(({ Then }) => {
     waitForElemAndCheckItsText(selectorClass, text, callback);
   });
 
-  Then('I hit enter key in "{element}" input', (element, callback) => {
-    const selectorClass = `.${element.replace(/ /g, '-')}`;
-    waitForElemAndSendKeys(selectorClass, protractor.Key.ENTER, callback);
+  Then('I hit "{key}" key in "{element}" input', (key, element, callback) => {
+    const selectorClass = `.${element.replace(/ /g, '-')} input`;
+    browser.sleep(500);
+    waitForElem(selectorClass).then((elem) => {
+      elem.sendKeys('', protractor.Key[key])
+        .then(callback)
+        .catch(callback);
+    }).catch(callback);
   });
 });
-
