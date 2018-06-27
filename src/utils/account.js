f482bfbbc6159d8fde1501869bbfbeb5139374e9
yasharAyari <yashar@lightcurve.io>
2018-06-27 12:08:01 +0200

    Update account utils to work with 'list-elements'

--- src/utils/account.js
+++ src/utils/account.js
@@ -1,18 +1,17 @@
-import Lisk from 'lisk-js';
+import Lisk from 'lisk-elements';
 
 export const extractPublicKey = passphrase =>
-  Lisk.crypto.getKeys(passphrase).publicKey;
+  Lisk.cryptography.getKeys(passphrase).publicKey;
 
 /**
-* @param {String} data - passphrase or public key
-*/
+ * @param {String} data - passphrase or public key
+ */
 export const extractAddress = (data) => {
   if (!data) {
     return false;
   }
   if (data.indexOf(' ') < 0) {
-    return Lisk.crypto.getAddress(data);
+    return Lisk.cryptography.getAddressFromPublicKey(data);
   }
-  const { publicKey } = Lisk.crypto.getKeys(data);
-  return Lisk.crypto.getAddress(publicKey);
+  return Lisk.cryptography.getAddressFromPassphrase(data);
 };
