a405609d53b770ec190795f8bde8883b9e5b59b3
Vit Stanislav <vit@lightcurve.io>
2018-07-06 13:47:38 +0200

    Revert "Replace the Lisk logo with beta-labeled logo"
    This reverts commit f7771662c561e479eaec9697edecf699f04d1984.

--- src/components/mainMenu/mainMenu.js
+++ src/components/mainMenu/mainMenu.js
@@ -4,7 +4,7 @@ import { Tab, Tabs as ToolboxTabs } from 'react-toolbox/lib/tabs';
 import Drawer from 'react-toolbox/lib/drawer';
 import MenuBar from '../menuBar';
 import styles from './mainMenu.css';
-import logo from '../../assets/images/logo-beta.svg';
+import logo from '../../assets/images/Lisk-Logo.svg';
 import * as menuLogos from '../../assets/images/main-menu-icons/*.svg'; //eslint-disable-line
 import { FontIcon } from '../fontIcon';
 import routes from '../../constants/routes';
