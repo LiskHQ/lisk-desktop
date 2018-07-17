77e896b2ac03b48d1503116e79718ee5234fafad
michaeltomasik <shugo12345@gmail.com>
2018-07-17 14:35:34 +0200

    :recycle: Ignore all unit and e2e tests

--- src/tests.js
+++ src/tests.js
@@ -6,6 +6,7 @@ import sinon from 'sinon';
 import sinonStubPromise from 'sinon-stub-promise';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { unitTestsDict, integrationTestsDict, electronTestsDict } from './testFiles';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -15,11 +16,20 @@ chai.use(chaiAsPromised);
 sinonStubPromise(sinon);
 
 window.localStorage.getItem = () => JSON.stringify([]);
-
-// load all tests into one bundle
 const testsContext = require.context('.', true, /\.test\.js$/);
-testsContext.keys().forEach(testsContext);
+testsContext
+  .keys()
+  .filter(url => unitTestsDict.indexOf(url) === -1)
+  .forEach(testsContext);
+
 const integrationContext = require.context('../test/integration/', true, /\.test\.js$/);
-integrationContext.keys().forEach(integrationContext);
+integrationContext
+  .keys()
+  .filter(url => integrationTestsDict.indexOf(url) === -1)
+  .forEach(integrationContext);
+
 const electronTestsContext = require.context('../app', true, /\.test\.js$/);
-electronTestsContext.keys().forEach(electronTestsContext);
+electronTestsContext
+  .keys()
+  .filter(url => electronTestsDict.indexOf(url) === -1)
+  .forEach(electronTestsContext);
