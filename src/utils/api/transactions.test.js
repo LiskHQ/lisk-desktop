009dd2ffdbe5672ed89c1c8f2e7fa66ef5e516a7
Vit Stanislav <vit@lightcurve.io>
2018-07-23 17:04:33 +0200

    :white_check_mark: Fix api utils unit tests

--- src/utils/api/transactions.test.js
+++ src/utils/api/transactions.test.js
@@ -1,24 +1,36 @@
 import { expect } from 'chai';
+import { stub } from 'sinon';
 import { send, getTransactions, unconfirmedTransactions } from './transactions';
 
 describe('Utils: Transactions API', () => {
+  const activePeer = {
+    transactions: {
+      get: stub().returnsPromise(),
+      broadcast: stub().returnsPromise(),
+    },
+    node: {
+      getTransactions: stub().returnsPromise(),
+    },
+  };
+
+  // TODO: fix these tests for assert more than just a promise is returned
   describe('send', () => {
     it('should return a promise', () => {
-      const promise = send();
+      const promise = send(activePeer);
       expect(typeof promise.then).to.be.equal('function');
     });
   });
 
   describe('transactions', () => {
     it('should return a promise', () => {
-      const promise = getTransactions({ activePeer: {} });
+      const promise = getTransactions({ activePeer });
       expect(typeof promise.then).to.be.equal('function');
     });
   });
 
   describe('unconfirmedTransactions', () => {
     it('should return a promise', () => {
-      const promise = unconfirmedTransactions();
+      const promise = unconfirmedTransactions(activePeer);
       expect(typeof promise.then).to.be.equal('function');
     });
   });
