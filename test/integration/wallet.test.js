88c8baca8c7ff17600db486ffa74021df5b43309
Pablo Molina <pablo@lightcurve.io>
2018-07-25 11:28:18 +0200

    :white_check_mark: don't match recipientId for sent stub so that can be reused along scenarios

--- test/integration/wallet.test.js
+++ test/integration/wallet.test.js
@@ -193,7 +193,7 @@ describe('@integration: Wallet', () => {
 
       sendTransactionsStub.withArgs(
         match.any,
-        '537318935439898807L',
+        match.any,
         match.any,
         match.any,
         match.any,
@@ -276,7 +276,7 @@ describe('@integration: Wallet', () => {
       step(`Then I should see text ${successMessage} in "result box message" element`, () => helper.haveTextOf('.result-box-message', successMessage));
     });
 
-    describe.only('Scenario: should allow to send LSK from unlocked account with 2nd passphrase', () => {
+    describe('Scenario: should allow to send LSK from unlocked account with 2nd passphrase', () => {
       const { secondPassphrase } = accounts['second passphrase account'];
       step('Given I\'m on "wallet" as "second passphrase account"', () => setupStep('second passphrase account'));
       step('And I fill in "1" to "amount" field', () => { helper.fillInputField('1', 'amount'); });
