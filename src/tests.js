abd9b6f0a78288bfc4af6ad7dd07738380eff3fc
michaeltomasik <shugo12345@gmail.com>
2018-07-27 14:31:09 +0200

    :revert: Revert unit and integration tests from test file

--- src/tests.js
+++ src/tests.js
@@ -6,7 +6,6 @@ import sinon from 'sinon';
 import sinonStubPromise from 'sinon-stub-promise';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { unitTestsDict, integrationTestsDict } from './testFiles';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -17,16 +16,10 @@ sinonStubPromise(sinon);
 /* istanbul ignore next */
 window.localStorage.getItem = () => JSON.stringify([]);
 const testsContext = require.context('.', true, /\.test\.js$/);
-testsContext
-  .keys()
-  .filter(url => unitTestsDict.indexOf(url) === -1)
-  .forEach(testsContext);
+testsContext.keys().forEach(testsContext);
 
 const integrationContext = require.context('../test/integration/', true, /\.test\.js$/);
-integrationContext
-  .keys()
-  .filter(url => integrationTestsDict.indexOf(url) === -1)
-  .forEach(integrationContext);
+integrationContext.keys().forEach(integrationContext);
 
 const electronTestsContext = require.context('../app', true, /\.test\.js$/);
 electronTestsContext.keys().forEach(electronTestsContext);
