59615f36238ea6565075751aa874a017ba7b671b
Pablo Molina <pablo@lightcurve.io>
2018-07-27 17:41:05 +0200

    :recycle: revert enabling coverage thresholds

--- src/tests.js
+++ src/tests.js
@@ -6,6 +6,7 @@ import sinon from 'sinon';
 import sinonStubPromise from 'sinon-stub-promise';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { unitTestsDict, integrationTestsDict } from './testFiles';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -16,10 +17,16 @@ sinonStubPromise(sinon);
 /* istanbul ignore next */
 window.localStorage.getItem = () => JSON.stringify([]);
 const testsContext = require.context('.', true, /\.test\.js$/);
-testsContext.keys().forEach(testsContext);
+testsContext
+  .keys()
+  .filter(url => unitTestsDict.indexOf(url) === -1)
+  .forEach(testsContext);
 
 const integrationContext = require.context('../test/integration/', true, /\.test\.js$/);
-integrationContext.keys().forEach(integrationContext);
+integrationContext
+  .keys()
+  .filter(url => integrationTestsDict.indexOf(url) === -1)
+  .forEach(integrationContext);
 
 const electronTestsContext = require.context('../app', true, /\.test\.js$/);
 electronTestsContext.keys().forEach(electronTestsContext);
