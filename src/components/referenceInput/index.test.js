22925cce304026b58a29325b2b25eb608466392c
michaeltomasik <shugo12345@gmail.com>
2018-07-27 17:23:14 +0200

    :recycle: Fix test thresholds coverage

---
+++ src/components/referenceInput/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { spy } from 'sinon';
+import ReferenceInput from './';
+
+describe('ReferenceInput', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      handleChange: spy(),
+      className: 'test',
+      address: '123L',
+      label: 'test',
+    };
+    wrapper = mount(<ReferenceInput {...props} />);
+  });
+
+  it('should render Input', () => {
+    expect(wrapper.find('Input')).to.have.lengthOf(1);
+  });
+
+  it('should call onChange', () => {
+    wrapper.find('Input').first().props().onChange('test');
+    expect(props.handleChange).to.have.been.calledWith('test');
+  });
+});
--- src/utils/api/transactions.test.js
+++ src/utils/api/transactions.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { stub } from 'sinon';
 import { send, getTransactions, unconfirmedTransactions } from './transactions';
+import txFilters from './../../constants/transactionFilters';
 
 describe('Utils: Transactions API', () => {
   const activePeer = {
@@ -25,6 +26,22 @@ describe('Utils: Transactions API', () => {
     it('should return a promise', () => {
       const promise = getTransactions({ activePeer });
       expect(typeof promise.then).to.be.equal('function');
+    });
+
+    it('should call transactions.get for incoming promise', () => {
+      getTransactions({ activePeer, address: '123L', filter: txFilters.incoming });
+
+      expect(activePeer.transactions.get).to.have.been.calledWith({
+        limit: 20, offset: 0, recipientId: '123L', sort: 'timestamp:desc',
+      });
+    });
+
+    it('should call transactions.get for outgoing promise', () => {
+      getTransactions({ activePeer, address: '123L', filter: txFilters.outgoing });
+
+      expect(activePeer.transactions.get).to.have.been.calledWith({
+        limit: 20, offset: 0, senderId: '123L', sort: 'timestamp:desc',
+      });
     });
   });
 
