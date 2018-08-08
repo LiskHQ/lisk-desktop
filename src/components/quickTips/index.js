522a87da6a9d57c65f1905642b081a78c6c1374f
michaeltomasik <shugo12345@gmail.com>
2018-08-08 10:45:27 +0200

    :recycle: Add TransitionWrapper to quickTip

--- src/components/quickTips/index.js
+++ src/components/quickTips/index.js
@@ -3,6 +3,7 @@ import { translate } from 'react-i18next';
 import Box from '../box';
 import quickTips from '../../constants/quickTips';
 import { FontIcon } from '../fontIcon';
+import TransitionWrapper from '../toolbox/transitionWrapper';
 
 import styles from './index.css';
 
@@ -30,25 +31,36 @@ class QuickTips extends React.Component {
 
   render() {
     const { currentIndex } = this.state;
-    const currentSlide = quickTips(this.props.t)[currentIndex];
+    const slides = quickTips(this.props.t);
+    const currentSlide = slides[currentIndex];
 
     return (
       <Box className={`quickTips ${styles.quickTips}`}>
-        <div className={styles.title}>{
-          currentSlide.title
-        }</div>
+        {slides.map(slide =>
+          <TransitionWrapper current={currentSlide.title} step={slide.title} key={`transition-title-${slide.title}`}>
+            <div className={styles.title}>{currentSlide.title}</div>
+          </TransitionWrapper>)
+        }
         <div className={styles.quickTipBody}>
-          <div className={`${styles.descriptionRow}`}>
-            <div className={`${styles.description}`}>
-              {currentSlide.description.map((desc, key) =>
-                <span key={`desc-${key}`}>{desc}<br/><br/></span>)}
-            </div>
-            <div className={`${styles.picture}`}><img src={currentSlide.picture} /></div>
-          </div>
+          {slides.map(slide =>
+            <TransitionWrapper current={currentSlide.title} step={slide.title} key={`transition-body-${slide.title}`}>
+              <div className={`${styles.descriptionRow}`}>
+                <div className={`${styles.description}`}>
+                  {currentSlide.description.map((desc, key) =>
+                    <span key={`desc-${key}`}>{desc}<br/><br/></span>)}
+                </div>
+                <div className={`${styles.picture}`}><img src={currentSlide.picture} /></div>
+              </div>
+            </TransitionWrapper>)
+          }
           <div className={styles.footer}>
-            <a href={currentSlide.goTo.link} className={styles.goTo}>
-              {currentSlide.goTo.title}<FontIcon value='arrow-right'/>
-            </a>
+            {slides.map(slide =>
+              <TransitionWrapper current={currentSlide.title} step={slide.title} key={`transition-goto-${slide.title}`}>
+                <a href={currentSlide.goTo.link} className={styles.goTo}>
+                  {currentSlide.goTo.title}<FontIcon value='arrow-right'/>
+                </a>
+              </TransitionWrapper>)
+            }
             <div className={styles.steps}>
               <div
                 onClick={() => { this.previousStep(); }}
