feeecdc6f1969e1a057c35aa610d517bc50b61d5
michaeltomasik <shugo12345@gmail.com>
2018-08-14 10:47:33 +0200

    :seedling: Add ned network switcher design

--- src/components/converter/converter.js
+++ src/components/converter/converter.js
@@ -16,19 +16,10 @@ class Converter extends React.Component {
         USD: '0',
         EUR: '0',
       },
-      // 0 index is active one
-      currencies: ['USD', 'EUR'],
     };
     this.fee = fees.send;
 
     this.updateData();
-  }
-
-  componentDidMount() {
-    const { settings } = this.props;
-    if (settings.currency) {
-      this.selectActive(settings.currency);
-    }
   }
 
   updateData() {
@@ -38,38 +29,13 @@ class Converter extends React.Component {
       this.setState({ error });
     });
   }
-  /*
-   * It swaping clicked currency with active currency on index 0
-   */
-  selectActive(currency) {
-    const currencyIndex = this.state.currencies.indexOf(currency);
-    if (currencyIndex !== 0) {
-      const currencies = this.state.currencies;
-
-      currencies.push(currencies.shift(currencies[currencyIndex]));
-      this.setState({ currencies });
-      this.props.settingsUpdated({ currency });
-    }
-  }
 
   render() {
-    const { LSK, currencies } = this.state;
+    const { LSK } = this.state;
 
     let price = !!this.props.error && Number.isNaN(this.props.value) ?
-      (0).toFixed(2) : (this.props.value * LSK[currencies[0]]).toFixed(2);
+      (0).toFixed(2) : (this.props.value * LSK[this.props.settings.currency]).toFixed(2);
     price = price > converter.maxLSKSupply || price === 'NaN' || price < 0 ? (0).toFixed(2) : price;
-
-    const currenciesObejects = currencies.map((currency, key) => (
-      <div
-        key={`${currency}-${key}`}
-        className={`${styles.convertElem} converted-currency`}
-        // eslint-disable-next-line
-        onClick={() => { this.selectActive(currency); }}>{currency}</div>
-    ));
-    // putting <div>|</div> inbetween array objects
-    const intersperse = currenciesObejects
-      .reduce((a, v, key) => [...a, v, <div key={key}>|</div>], []) // eslint-disable-line
-      .slice(0, -1);
     return (
       <Input
         label={this.props.label}
@@ -79,15 +45,19 @@ class Converter extends React.Component {
         theme={styles}
         onChange={this.props.onChange} >
         <div className={styles.convertorWrapper}>
-          {this.props.value !== '' && this.state.LSK[currencies[0]] ?
+          {this.props.value !== '' && this.state.LSK[this.props.settings.currency] ?
             <div className={this.props.error ? `${styles.convertorErr} convertorErr` : `${styles.convertor} convertor`}>
-              <div className={`${styles.convertElem} converted-price`}>~ {price}</div>
-              {intersperse}
+              <div className={`${styles.convertElem} converted-price`}>{`${this.props.t('ca.')} ${price} ${this.props.settings.currency}`}</div>
             </div>
             : <div></div>
           }
         </div>
-        { this.props.isRequesting ? null : <div className={styles.fee}> {this.props.t('Fee: {{fee}} LSK', { fee: fromRawLsk(this.fee) })} </div> }
+        { this.props.isRequesting || this.props.error ? null :
+          <div className={styles.fee}>{this.props.t('Additional fee: {{fee}} LSK', { fee: fromRawLsk(this.fee) })}
+          {`, ${this.props.t('ca. {{price}} {{currency}}', {
+            currency: this.props.settings.currency,
+            price: (1 * LSK[this.props.settings.currency]).toFixed(1),
+          })}`}</div> }
       </Input>
     );
   }
