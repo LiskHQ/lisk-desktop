596351322a8d50a95f44251590c1982954c56540
michaeltomasik <shugo12345@gmail.com>
2018-09-03 01:57:50 +0200

    :seedling: add bookmark functionality

--- src/components/bookmark/bookmark.js
+++ src/components/bookmark/bookmark.js
@@ -21,41 +21,47 @@ class Bookmark extends React.Component {
       followedAccounts, handleChange, className, label, address,
     } = this.props;
 
+    const filteredFollowedAccounts = followedAccounts
+      .filter(account => account.title.toLowerCase()
+        .includes(address.value.toLowerCase()));
+
     return (
-      <div className={styles.bookmark}>
-        <Input
-          className={`${className} ${styles.bookmarkInput}`}
-          label={label}
-          error={''}
-          value={address.value}
-          onFocus={() => this.setState({ show: true })}
-          onBlur={() => this.setState({ show: false })}
-          onChange={(val) => { handleChange(val); }}
-          >
-        </Input>
-        { this.state.show ?
-          <ul className={`${styles.resultList} ${this.state.show ? styles.show : ''}`}>
-            {
-              followedAccounts
-                .filter(account => account.title.toLowerCase()
-                  .includes(address.value.toLowerCase()))
-                .map((account, index) =>
-                  <li
-                    onClick={() => { handleChange(account.address); }}
-                    key={`followed-${index}`}
-                    className={styles.followedRow}>
-                    <AccountVisual
-                      className={styles.accountVisual}
-                      address={account.address}
-                      size={43}
-                    />
-                    <div>
-                      <div>{account.title}</div>
-                      <div>{account.address}</div>
-                    </div>
-                  </li>)
-            }
-          </ul> : null }
+      <div className={styles.scale}>
+        <div className={this.state.show ? styles.bookmark : ''}>
+          <Input
+            className={`${className} ${styles.bookmarkInput}`}
+            label={label}
+            error={''}
+            value={address.value}
+            onFocus={() => this.setState({ show: true })}
+            onBlur={() => this.setState({ show: false })}
+            onChange={(val) => { handleChange(val); }}
+            >
+          </Input>
+          { this.state.show ?
+            <ul className={`${filteredFollowedAccounts.length > 0 ? styles.resultList : ''} ${this.state.show ? styles.show : ''}`}>
+              {
+                filteredFollowedAccounts
+                  .map((account, index) =>
+                    <li
+                      onMouseDown={() => {
+                        handleChange(account.address);
+                      }}
+                      key={`followed-${index}`}
+                      className={styles.followedRow}>
+                      <AccountVisual
+                        className={styles.accountVisual}
+                        address={account.address}
+                        size={35}
+                      />
+                      <div className={styles.text}>
+                        <div className={styles.title}>{account.title}</div>
+                        <div className={styles.address}>{account.address}</div>
+                      </div>
+                    </li>)
+              }
+            </ul> : null }
+        </div>
       </div>
     );
   }
