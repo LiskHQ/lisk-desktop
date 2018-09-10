439008ea673f1b49acf2aa157484b101d4f1aaee
Pablo Molina <pablo@lightcurve.io>
2018-09-10 16:07:23 +0200

    :recycle: fix network showing based on new specs

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -27,8 +27,11 @@ const Account = ({ peers, t, showNetworkIndicator }) => {
     <FontIcon className={`${styles.network} online`} value={iconMap[iconCode]} /> :
     <FontIcon className='offline' value='error' />;
 
-  return ((showNetworkIndicator && peers.data &&
-      peers.options.code !== networks.mainnet.code) ?
+  const shouldShowNetworkIndicator = ((peers.data &&
+    peers.options.code !== networks.mainnet.code) ||
+    (peers.data && peers.options.code === networks.mainnet.code && showNetworkIndicator));
+
+  return (shouldShowNetworkIndicator ?
     <section className={styles.peer}>
       <div className={`${styles.title} ${`${styles[`${iconMap[iconCode]}Title`]}`} inner primary peer-network ${iconMap[iconCode]}-title`}>
         <span id="accountStatus" className={`${styles.status} network-status`}>
