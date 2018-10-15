a4e1d9106deea0cab73d0232651401ec990d3838
michaeltomasik <shugo12345@gmail.com>
2018-10-15 16:05:16 +0200

    :recycle: Add better comment for peers middleware & refactor function

--- src/store/middlewares/peers.js
+++ src/store/middlewares/peers.js
@@ -2,17 +2,20 @@ import { activePeerSet, activePeerUpdate } from '../../actions/peers';
 import actionTypes from '../../constants/actions';
 import networks from './../../constants/networks';
 import getNetwork from './../../utils/getNetwork';
-import { shouldAutoLogIn } from './../../utils/login';
+import { shouldAutoLogIn, getAutoLogInData } from './../../utils/login';
 
 const peersMiddleware = store => next => (action) => {
   next(action);
 
   const network = Object.assign({}, getNetwork(networks.mainnet.code));
+  const autologinData = getAutoLogInData();
 
   switch (action.type) {
     case actionTypes.storeCreated:
-      // It changes network back to mainnet when we want to autologin for explorer account
-      if (!shouldAutoLogIn) {
+      // It stops activePeer to be overridden to mainnet
+      // when we want to autologin for explorer account
+      // https://github.com/LiskHQ/lisk-hub/issues/1339
+      if (!shouldAutoLogIn(autologinData)) {
         store.dispatch(activePeerSet({ network }));
       }
       store.dispatch(activePeerUpdate({ online: true }));
