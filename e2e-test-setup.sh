9bc4def4f7cab184e520b41eda580ce2ae4ef92c
andreiklimenok <andrei.klimenok@lightcurve.io>
2018-10-17 16:22:56 +0200

    update blockchain snapshot applying script

--- e2e-test-setup.sh
+++ e2e-test-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  
+#!/bin/bash
 # Purpose of this script is to clean lisk database and create some tranactions
 
 if [ -z "$1" ]
@@ -7,16 +7,12 @@ if [ -z "$1" ]
     exit 1
 fi
 
-if [ ! -f blockchain_explorer.db.gz ]; then
-  wget https://downloads.lisk.io/lisk-explorer/dev/blockchain_explorer.db.gz
-fi
-
 pwd=`pwd`
 cd $1
 pm2 stop app.js
-dropdb lisk_test
-createdb lisk_test
-gunzip -fcq "$pwd/blockchain_explorer.db.gz" | psql -d lisk_test
+dropdb lisk_dev
+createdb lisk_dev
+gunzip -fcq "$pwd/blockchain.db.gz" | psql -d lisk_dev
 pm2 start app.js
 sleep 5
 cd $pwd
