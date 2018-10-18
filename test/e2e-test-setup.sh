ed7f0adcbb85810922373638882c906ad0abdd3c
andreiklimenok <andrei.klimenok@lightcurve.io>
2018-10-18 15:57:37 +0200

    Move snapshot related files to test folder

--- test/e2e-test-setup.sh
+++ test/e2e-test-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Purpose of this script is to clean lisk database and create some tranactions
+# Purpose of this script is to clean lisk database and create some trasnactions
 
 if [ -z "$1" ]
   then
@@ -12,8 +12,9 @@ cd $1
 pm2 stop app.js
 dropdb lisk_dev
 createdb lisk_dev
-gunzip -fcq "$pwd/blockchain.db.gz" | psql -d lisk_dev
+gunzip -fcq "$pwd/test/blockchain.db.gz" | psql -d lisk_dev
 pm2 start app.js
+
 sleep 5
 cd $pwd
 
