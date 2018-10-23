308aaed0791b2d743c8ff39d8accb24782bf0de1
michaeltomasik <shugo12345@gmail.com>
2018-10-23 10:41:49 +0200

    :seedling: Added Image to unlockWallet ledger

--- src/components/ledger/unlockWallet.js
+++ src/components/ledger/unlockWallet.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { translate } from 'react-i18next';
 import { ActionButton, Button } from './../toolbox/buttons/button';
 
+import ledgerNano from '../../assets/images/ledger-nano-s.png';
 import styles from './unlockWallet.css';
 
 const UnlockWallet = ({ handleOnClick, cancelLedgerLogin, t }) => (
   <div>
     <h1 className={styles.title}>{t('Open the Lisk App')}</h1>
+    <div className={styles.image}><img src={ledgerNano} width={300} height={80} /></div>
     <div className={styles.description}>{t('Open the Lisk App with your Ledger Nano S')}</div>
     <div className={styles.buttonWrapper}>
       <Button className={styles.button} onClick={() => { cancelLedgerLogin(); }} >{t('Cancel')}</Button>
