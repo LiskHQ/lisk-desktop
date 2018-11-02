07a23674c2da6b61105615a8e257140ecc1a206e
Benjamin Maggi <benjamin.maggi@lightcurve.io>
2018-11-02 13:33:32 +0100

    :white_check_mark: added test for action creators in secondPassphrase

--- src/actions/secondPassphrase.js
+++ src/actions/secondPassphrase.js
@@ -4,7 +4,6 @@ export const secondPassphraseRegisteredFailure = data => ({
   type: actionTypes.secondPassphraseRegisteredFailure,
   text: data.text,
 });
-
 export const secondPassphraseRegisteredFailureReset = () => ({
   type: actionTypes.secondPassphraseRegisteredFailureReset,
 });
