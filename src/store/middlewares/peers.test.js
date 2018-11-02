5e4f36ac7ddc5eafecb2379c3604e59eacef16c8
Benjamin Maggi <benjamin.maggi@lightcurve.io>
2018-11-02 10:25:16 +0100

    :white_check_mark: added tests for no selected network

--- src/store/middlewares/peers.test.js
+++ src/store/middlewares/peers.test.js
@@ -3,16 +3,21 @@ import { spy, stub } from 'sinon';
 import middleware from './peers';
 import actionTypes from '../../constants/actions';
 import { activePeerSet } from '../../actions/peers';
+import * as loginUtils from './../../utils/login';
 
 describe('Peer middleware', () => {
   let store;
   let next;
   let localStorageStub;
+  let loginUtilsStub;
+  let getAutoLogInDataStub;
 
   beforeEach(() => {
     next = spy();
     store = stub();
     localStorageStub = stub(localStorage, 'get');
+    loginUtilsStub = stub(loginUtils, 'findMatchingLoginNetwork');
+    getAutoLogInDataStub = stub(loginUtils, 'getAutoLogInData');
 
     store.getState = () => ({
       peers: {
@@ -25,11 +30,15 @@ describe('Peer middleware', () => {
 
   afterEach(() => {
     localStorageStub.restore();
+    loginUtilsStub.restore();
+    getAutoLogInDataStub.restore();
   });
 
   it('should just pass action along', () => {
     localStorageStub.returns(JSON.stringify([]));
-
+    getAutoLogInDataStub.returns({
+      liskCoreUrl: 'https://testnet.lisk.io',
+    });
     const sampleAction = {
       type: 'SAMPLE_TYPE',
       data: 'SAMPLE_DATA',
@@ -38,9 +47,34 @@ describe('Peer middleware', () => {
     expect(next).to.have.been.calledWith(sampleAction);
   });
 
+  it('should get the correct network', () => {
+    getAutoLogInDataStub.returns({
+      liskCoreUrl: 'https://testnet.lisk.io',
+    });
+    loginUtilsStub.returns();
+    const storeCreated = { type: actionTypes.storeCreated };
+    localStorageStub.returns(JSON.stringify([]));
+    middleware(store)(next)(storeCreated);
+    expect(store.dispatch).to.have.been.calledWith();
+  });
+
+  it('should auto login without shouldAutoLogIn', () => {
+    getAutoLogInDataStub.returns({
+      liskCoreUrl: undefined,
+    });
+    loginUtilsStub.returns();
+    const storeCreated = { type: actionTypes.storeCreated };
+    localStorageStub.returns(JSON.stringify([]));
+    middleware(store)(next)(storeCreated);
+    expect(store.dispatch).to.have.been.calledWith();
+  });
+
   it('should dispatch activePeerSet if there are no saved accounts', () => {
     const storeCreated = { type: actionTypes.storeCreated };
     localStorageStub.returns(JSON.stringify([]));
+    getAutoLogInDataStub.returns({
+      liskCoreUrl: 'https://testnet.lisk.io',
+    });
     middleware(store)(next)(storeCreated);
     expect(store.dispatch).to.have.been.calledWith();
   });
@@ -48,6 +82,9 @@ describe('Peer middleware', () => {
   it('should not dispatch activePeerSet if there are saved accounts', () => {
     const storeCreated = { type: actionTypes.storeCreated };
     localStorageStub.returns(JSON.stringify([{}, {}]));
+    getAutoLogInDataStub.returns({
+      liskCoreUrl: 'https://testnet.lisk.io',
+    });
     middleware(store)(next)(storeCreated);
     expect(store.dispatch).to.not.have.been.calledWith(activePeerSet());
   });
