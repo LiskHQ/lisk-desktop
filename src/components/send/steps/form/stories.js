f57f2d864b324f5b64cd24ccba4d87d973ab882e
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2018-11-05 15:44:35 +0100

    :recycle: Fix component import path in src/components/send/steps/form/stories.js file

--- src/components/send/steps/form/stories.js
+++ src/components/send/steps/form/stories.js
@@ -3,22 +3,22 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { storiesOf } from '@storybook/react';
 
-import Send from './send';
+import Form from './form';
 
 import accounts from '../../../../../test/constants/accounts';
 import store from '../../../../store';
 
 const account = accounts.genesis;
 
-storiesOf('Send', module)
+storiesOf('Form', module)
   .addDecorator(getStory => (
     <Provider store={store}>
       {getStory()}
     </Provider>
   ))
   .add('pre-filled recipient and amount', () => (
-    <Send recipient='11004588490103196952L' amount='100' account={account} activePeer={{}} />
+    <Form recipient='11004588490103196952L' amount='100' account={account} activePeer={{}} />
   ))
   .add('without pre-filles', () => (
-    <Send account={account} activePeer={{}} />
+    <Form account={account} activePeer={{}} />
   ));
