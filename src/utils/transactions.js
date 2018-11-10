619ef893a92afa821be64425f1092ab3a704ed84
michaeltomasik <shugo12345@gmail.com>
2018-11-10 16:18:30 +0100

    :reycle: Fix e2e test

--- src/utils/transactions.js
+++ src/utils/transactions.js
@@ -3,5 +3,5 @@ export default (pendingTransactions, confirmedTransactions) =>
   [...pendingTransactions, ...confirmedTransactions]
     .filter((transactionA, index, self) =>
       index === self.findIndex(transactionB => (
-        transactionB.id === transactionA.id && transactionB.confirmations
+        transactionB.id === transactionA.id
       )));
