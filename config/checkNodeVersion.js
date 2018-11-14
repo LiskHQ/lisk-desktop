9451d853315e622297bd136daac86b674c267343
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2018-11-14 12:30:07 +0100

    :house: Add engines property to package.json to check the node version and avoid problems

---
+++ config/checkNodeVersion.js
@@ -0,0 +1,12 @@
+const semver = require('semver');
+const { engines } = require('../package');
+
+const version = engines.node;
+const currentVersion = process.version;
+
+if (!semver.satisfies(currentVersion, version)) {
+  console.error(`\n\tCurrent node version ${currentVersion} \n\tRequired node version ${version}...\n`);
+  throw new Error('Invalid current node version');
+} else {
+  console.info('\n\tThe current node version satisfy the required version...\n\n');
+}
