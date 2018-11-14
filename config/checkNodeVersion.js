2346a3da360e2e8210a84a2635db7f485e53d649
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2018-11-14 14:47:44 +0100

    :house: Fix config/chackNodeVersion.js file eslint errors

--- config/checkNodeVersion.js
+++ config/checkNodeVersion.js
@@ -5,8 +5,8 @@ const version = engines.node;
 const currentVersion = process.version;
 
 if (!semver.satisfies(currentVersion, version)) {
-  console.error(`\n\tCurrent node version ${currentVersion} \n\tRequired node version ${version}...\n`);
+  console.error(`\n\tCurrent node version ${currentVersion} \n\tRequired node version ${version}...\n`); // eslint-disable-line no-console
   throw new Error('Invalid current node version');
 } else {
-  console.info('\n\tThe current node version satisfy the required version...\n\n');
+  console.info('\n\tThe current node version satisfy the required version...\n\n'); // eslint-disable-line no-console
 }
