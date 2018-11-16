f814d4cbbc372ddf7b72dbfc5d8e45afb22e2255
Vit Stanislav <vit@lightcurve.io>
2018-11-16 16:15:49 +0100

    :white_check_mark: Add missing className to fix e2e tests

--- src/components/transactions/transactionDetailView/transactionDetailDateField.js
+++ src/components/transactions/transactionDetailView/transactionDetailDateField.js
@@ -5,6 +5,7 @@ import TransactionDetailViewField from './transactionDetailViewField';
 const DateField = props => (
   <TransactionDetailViewField
     label={props.t('Date')}
+    className={'tx-date'}
     value={ props.transaction.timestamp ?
       <span>
         <DateFromTimestamp
