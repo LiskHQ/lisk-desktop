1ba12f9e25c97fbd40e148aba199bf6434618d9c
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2018-11-21 14:03:36 +0100

    :nail_care: Update css after review and update test file to reach the coverage

--- src/components/toolbox/buttons/button.test.js
+++ src/components/toolbox/buttons/button.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import { Button } from './button';
+import { Button, SecondaryLightButton, ActionButton } from './button';
 
 describe('Button wrapper', () => {
   it('Creates only one instance of the button', () => {
     const wrappedButton = mount(<Button />, {});
+    expect(wrappedButton.find('Button').length).to.equal(1);
+  });
+
+  it('Creates only one instance of the SecondaryLightButton', () => {
+    const wrappedButton = mount(<SecondaryLightButton />, {});
+    expect(wrappedButton.find('Button').length).to.equal(1);
+  });
+
+  it('Creates only one instance of the ActionButton', () => {
+    const wrappedButton = mount(<ActionButton />, {});
     expect(wrappedButton.find('Button').length).to.equal(1);
   });
 });
