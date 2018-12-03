ff2483d82929805c281831bead39ce57397de8ca
michaeltomasik <shugo12345@gmail.com>
2018-12-03 18:05:28 +0100

    :seedling: Add ledger voting feature

--- src/components/resultBox/resultBox.js
+++ src/components/resultBox/resultBox.js
@@ -66,7 +66,7 @@ class ResultBox extends React.Component {
               {this.props.t('Add to followed accounts')}
             </Button> : null
           }
-          {!this.props.success && this.props.account.hwInfo ?
+          {!this.props.success && this.props.account && this.props.account.hwInfo ?
             <Button className={`add-follwed-account-button ${styles.addFollowedAccountButton}`}
               onClick={() => {
                 this.props.transactionFailedClear();
