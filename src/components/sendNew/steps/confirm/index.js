26760f3a716d67d09e11fa4b88343c936d66adbe
Renato Massao Yonamine <renato.massao@lightcurve.io>
2018-12-07 16:33:55 +0100

    :recycle: Remove unit test of sendNew steps confirmHOC

--- src/components/sendNew/steps/confirm/index.js
+++ src/components/sendNew/steps/confirm/index.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 
@@ -11,8 +12,8 @@ const mapStateToProps = state => ({
   followedAccounts: state.followedAccounts ? state.followedAccounts.accounts : [],
 });
 
-const mapDispatchToProps = dispatch => ({
-  sent: data => dispatch(sent(data)),
-});
+const mapDispatchToProps = {
+  sent,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(translate()(Confirm));
