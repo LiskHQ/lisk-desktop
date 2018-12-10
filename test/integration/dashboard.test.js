c7666287f2c344ceb8ab8695f44e69644a782b15
andreiklimenok <andrei.klimenok@lightcurve.io>
2018-12-10 18:40:15 +0100

    :white_check_mark: undelete integration test files

---
+++ test/integration/dashboard.test.js
@@ -0,0 +1,191 @@
+// TODO: delete file after coverage is fulfilled
+import thunk from 'redux-thunk';
+import { step } from 'mocha-steps';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+import { stub, match, spy } from 'sinon';
+
+import * as accountAPI from '../../src/utils/api/account';
+import * as delegateAPI from '../../src/utils/api/delegate';
+import * as liskServiceAPI from '../../src/utils/api/liskService';
+import * as transactionsAPI from '../../src/utils/api/transactions';
+import { prepareStore, renderWithRouter } from '../utils/applicationInit';
+import accountReducer from '../../src/store/reducers/account';
+import followedAccountsReducer from '../../src/store/reducers/followedAccounts';
+import transactionsReducer from '../../src/store/reducers/transactions';
+import searchReducer from '../../src/store/reducers/search';
+import peersReducer from '../../src/store/reducers/peers';
+import loadingReducer from '../../src/store/reducers/loading';
+import settingsReducer from '../../src/store/reducers/settings';
+import liskServiceReducer from '../../src/store/reducers/liskService';
+import accountMiddleware from '../../src/store/middlewares/account';
+import followedAccountsMiddleware from '../../src/store/middlewares/followedAccounts';
+import peerMiddleware from '../../src/store/middlewares/peers';
+import { accountLoggedIn } from '../../src/actions/account';
+import { liskAPIClientSet } from '../../src/actions/peers';
+import networks from './../../src/constants/networks';
+import txTypes from './../../src/constants/transactionTypes';
+import routes from '../../src/constants/routes';
+import getNetwork from './../../src/utils/getNetwork';
+import Dashboard from '../../src/components/dashboard';
+import CurrencyGraph from '../../src/components/dashboard/currencyGraph';
+import accounts from '../constants/accounts';
+import GenericStepDefinition from '../utils/genericStepDefinition';
+import EmptyState from '../../src/components/emptyState';
+import TransactionRow from '../../src/components/transactions/transactionRow';
+import QuickTips from '../../src/components/quickTips';
+import NewsFeed from '../../src/components/newsFeed';
+
+describe('@integration: Dashboard', () => {
+  let store;
+  let wrapper;
+  let getTransactionsStub;
+  let accountAPIStub;
+  let delegateAPIStub;
+  let liskServiceAPIStub;
+  let helper;
+  let sendTransactionsStub;
+
+  const history = { push: spy(), location: { search: '' } };
+
+  class Helper extends GenericStepDefinition {
+    clickOnTransaction() {
+      this.wrapper.find('TransactionRow').at(0).simulate('click');
+      expect(history.push).to.have.been.calledWith(`${routes.wallet.path}?id=10385202636`);
+    }
+  }
+
+  const generateTransactions = (n) => {
+    const transactions = [];
+    for (let i = 0; i <= n; i++) {
+      const transactionExample = {
+        id: 10385202636 + i,
+        senderId: 'sample_address',
+        receiverId: 'some_address',
+        type: txTypes.send,
+        confirmations: 1,
+      };
+      transactions.push(transactionExample);
+    }
+    return transactions;
+  };
+
+  beforeEach(() => {
+    getTransactionsStub = stub(transactionsAPI, 'getTransactions');
+    liskServiceAPIStub = stub(liskServiceAPI, 'getCurrencyGraphData');
+    accountAPIStub = stub(accountAPI, 'getAccount');
+    delegateAPIStub = stub(delegateAPI, 'getDelegate');
+    sendTransactionsStub = stub(transactionsAPI, 'send');
+
+    sendTransactionsStub.withArgs(
+      match.defined,
+      match.defined,
+      match.defined,
+      match.defined,
+      match.defined,
+      '',
+    ).returnsPromise().resolves({ data: [] });
+    // transactionsFilterSet do pass filter
+    getTransactionsStub.withArgs(match.any)
+      .returnsPromise().resolves({ data: generateTransactions(25), meta: { count: 1000 } });
+
+    // rest of accounts send request
+    sendTransactionsStub.withArgs(
+      match.defined,
+      match.defined,
+      match.defined,
+      match.defined,
+      null,
+      '',
+    ).returnsPromise().resolves({ data: [] });
+  });
+
+  afterEach(() => {
+    getTransactionsStub.restore();
+    liskServiceAPIStub.restore();
+    accountAPIStub.restore();
+    delegateAPIStub.restore();
+    sendTransactionsStub.restore();
+  });
+
+  const setupStep = (accountType, options = { isLocked: false, isLoggedIn: true }) => {
+    store = prepareStore({
+      account: accountReducer,
+      transactions: transactionsReducer,
+      peers: peersReducer,
+      loading: loadingReducer,
+      liskService: liskServiceReducer,
+      search: searchReducer,
+      settings: settingsReducer,
+      followedAccounts: followedAccountsReducer,
+    }, [
+      thunk,
+      accountMiddleware,
+      peerMiddleware,
+      followedAccountsMiddleware,
+    ]);
+
+    const passphrase = options.isLocked ? undefined : accounts[accountType].passphrase;
+    const account = {
+      ...accounts[accountType],
+      delegate: {},
+      multisignatures: [],
+      u_multisignatures: [],
+      unconfirmedBalance: '0',
+      passphrase,
+    };
+    accountAPIStub.withArgs(match.any).returnsPromise().resolves({ data: [...account] });
+    store.dispatch(liskAPIClientSet({ network: getNetwork(networks.mainnet.code) }));
+    delegateAPIStub.withArgs(match.any).returnsPromise()
+      .resolves({ delegate: { ...accounts['delegate candidate'] } });
+
+    liskServiceAPIStub.withArgs(match.any).returnsPromise()
+      .resolves({
+        body: {
+          candles: [{ timestamp: 111111111 }, { timestamp: 11111111112 }],
+        },
+      });
+
+    if (options.isLoggedIn) {
+      store.dispatch(accountLoggedIn(account));
+    }
+
+    wrapper = mount(renderWithRouter(Dashboard, store, { history }));
+    helper = new Helper(wrapper, store);
+  };
+
+  describe('QuickTips', () => {
+    describe('Scenario: should display QuickTips on Dashboard even loggedIn', () => {
+      step('Given I\'m on not Logged in', () => setupStep('genesis', { isLoggedIn: false }));
+      step('Then I should see 1 instance of "quickTips"', () => helper.shouldSeeCountInstancesOf(1, NewsFeed));
+    });
+
+    describe('Scenario: should display QuickTips on Dashboard even logout', () => {
+      step('Given I\'m on not Logged in', () => setupStep('genesis'));
+      step('Then I should see 1 instance of "quickTips"', () => helper.shouldSeeCountInstancesOf(1, NewsFeed));
+    });
+  });
+
+  describe('Transactions', () => {
+    describe('Scenario: should allow to view transactions', () => {
+      step('Given I\'m on "wallet" as "genesis" account', () => setupStep('genesis'));
+      step('Then I should see 5 rows', () => helper.shouldSeeCountInstancesOf(5, 'TransactionRow'));
+      step('Then I click on one of the transactions and expect to get directed to its details', () => helper.clickOnTransaction());
+    });
+
+    describe('Scenario: should not display Transactions', () => {
+      step('Given I\'m on "wallet" as "genesis" account not Logged in', () => setupStep('genesis', { isLoggedIn: false }));
+      step('Then I should see 0 instances of "send box"', () => helper.shouldSeeCountInstancesOf(0, TransactionRow));
+      step('Then I should see 1 instance of "quickTips"', () => helper.shouldSeeCountInstancesOf(1, QuickTips));
+    });
+  });
+
+  describe('Currency Graph', () => {
+    describe('Scenario: displays the currency graph', () => {
+      step('Given I\'m on "wallet" as "genesis" account', () => setupStep('genesis'));
+      step('Then I should see the currency graph', () => helper.shouldSeeCountInstancesOf(1, CurrencyGraph));
+      step('When I click on "step"', () => helper.clickOnElement('.step'));
+      step('Then I should still see the currency graph', () => helper.shouldSeeCountInstancesOf(0, EmptyState));
+    });
+  });
+});
---
+++ test/integration/transactionID.test.js
@@ -0,0 +1,122 @@
+// TODO: delete file after coverage is fulfilled
+import thunk from 'redux-thunk';
+import { step } from 'mocha-steps';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { stub, match } from 'sinon';
+
+import * as accountAPI from '../../src/utils/api/account';
+import * as transactionsAPI from '../../src/utils/api/transactions';
+import { renderWithRouter, prepareStore } from '../utils/applicationInit';
+import accountReducer from '../../src/store/reducers/account';
+import transactionReducer from '../../src/store/reducers/transaction';
+import peersReducer from '../../src/store/reducers/peers';
+import votingReducer from '../../src/store/reducers/voting';
+import accountMiddleware from '../../src/store/middlewares/account';
+import votingMiddleware from '../../src/store/middlewares/voting';
+import { liskAPIClientSet } from '../../src/actions/peers';
+import networks from './../../src/constants/networks';
+import getNetwork from './../../src/utils/getNetwork';
+import { accountLoggedIn } from '../../src/actions/account';
+import SingleTransaction from './../../src/components/singleTransaction';
+import accounts from '../constants/accounts';
+import GenericStepDefinition from '../utils/genericStepDefinition';
+
+class Helper extends GenericStepDefinition {
+  checkTxDetails() {
+    expect(this.wrapper).to.have.descendants('.transaction-id');
+    expect(this.wrapper.find('.transaction-id').first()).to.include.text('123456789');
+    expect(this.wrapper).to.have.descendants('.sender-address');
+    expect(this.wrapper.find('.sender-address').first()).to.include.text('123l');
+  }
+}
+
+describe('@integration: Single Transaction', () => {
+  let store;
+  let helper;
+  let wrapper;
+  let accountAPIStub;
+  let getTransactionsAPIStub;
+  let getSingleTransactionAPIStub;
+
+  beforeEach(() => {
+    getTransactionsAPIStub = stub(transactionsAPI, 'getTransactions');
+    getSingleTransactionAPIStub = stub(transactionsAPI, 'getSingleTransaction');
+    accountAPIStub = stub(accountAPI, 'getAccount');
+
+    getTransactionsAPIStub
+      .returnsPromise()
+      .resolves({
+        data: [{
+          id: '123456789',
+          senderId: '123l',
+          recipientId: '456l',
+          asset: {
+            votes: { added: [], deleted: [] },
+          },
+        }],
+      });
+
+    getSingleTransactionAPIStub
+      .returnsPromise()
+      .resolves({
+        data: [{
+          id: '123456789',
+          senderId: '123l',
+          recipientId: '456l',
+          asset: {},
+        }],
+      });
+  });
+
+  afterEach(() => {
+    getTransactionsAPIStub.restore();
+    getSingleTransactionAPIStub.restore();
+    accountAPIStub.restore();
+  });
+
+  const setupStep = ({ accountType, id }) => {
+    const transactionMock = {
+      id,
+      senderId: '123L',
+    };
+    store = prepareStore({
+      account: accountReducer,
+      transaction: transactionReducer,
+      peers: peersReducer,
+      voting: votingReducer,
+    }, [
+      thunk,
+      accountMiddleware,
+      votingMiddleware,
+    ]);
+
+    const account = {
+      ...accounts[accountType],
+      delegate: {},
+      multisignatures: [],
+      u_multisignatures: [],
+      unconfirmedBalance: '0',
+    };
+
+    accountAPIStub.withArgs(match.any).returnsPromise().resolves({ ...account });
+    store.dispatch(liskAPIClientSet({ network: getNetwork(networks.mainnet.code) }));
+    accountAPIStub.withArgs(match.any).returnsPromise().resolves({ ...account });
+    if (accountType) { store.dispatch(accountLoggedIn(account)); }
+    wrapper = mount(renderWithRouter(
+      SingleTransaction, store,
+      { match: { params: { id } }, transaction: transactionMock, pendingTransactions: [] },
+    ));
+    helper = new Helper(wrapper, store);
+  };
+
+  describe('Scenario: should allow to view transactions of any account', () => {
+    step('Given I\'m on "transactions/123456789" as "genesis" account', () => setupStep({ accountType: 'genesis', id: '123456789' }));
+    step('Then I should see the transaction details of 123456789', () => helper.checkTxDetails());
+  });
+
+  describe('Scenario: should allow to view transactions of any account without login', () => {
+    step('Given I\'m on "transactions/123456789" as "genesis" account', () => setupStep({ id: '123456789' }));
+    step('Then I should see the transaction details of 123456789', () => helper.checkTxDetails());
+  });
+});
