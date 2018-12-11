cae3028c267a236f16cae94db1b4aeb5b36df02b
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2018-12-11 11:35:52 +0100

    :seedling: Adding Matomo (Piwik) to the project

--- src/components/customRoute/customRoute.js
+++ src/components/customRoute/customRoute.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import ErrorBoundary from '../errorBoundary';
 import offlineStyle from '../offlineWrapper/offlineWrapper.css';
+import Piwik from '../../utils/piwik';
+import piwikConfig from '../../constants/piwik';
+
+const piwik = new Piwik({
+  url: piwikConfig.URL,
+  siteId: piwikConfig.SITE_ID,
+  trackErrors: true,
+});
 
 const CustomRoute = ({
   path, component, isPrivate, exact,
@@ -9,6 +17,10 @@ const CustomRoute = ({
 }) => {
   const { pathname, search } = rest.history.location;
   const fullPath = pathPrefix + path + pathSuffix;
+
+  piwik.connectToHistory(rest.history);
+  Piwik.push([piwikConfig.TRACK_PAGE_VIEW]);
+
   return ((isPrivate && isAuthenticated) || !isPrivate ?
     <main className={isPrivate ? offlineStyle.disableWhenOffline : null}>
       <ErrorBoundary errorMessage={t('An error occoured while rendering this page')}>
