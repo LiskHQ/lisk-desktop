c552508e9371c675a3504dca649229af6ac5c714
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2018-12-12 14:56:14 +0100

    :white_check_mark: Add piwik unit test

--- src/components/customRoute/customRoute.js
+++ src/components/customRoute/customRoute.js
@@ -4,9 +4,6 @@ import ErrorBoundary from '../errorBoundary';
 import offlineStyle from '../offlineWrapper/offlineWrapper.css';
 import Piwik from '../../utils/piwik';
 
-const piwik = Piwik.initPiwik();
-Piwik.setPiwikParameters();
-
 const CustomRoute = ({
   path, component, isPrivate, exact,
   isAuthenticated, pathSuffix = '', pathPrefix = '', t, ...rest
@@ -14,7 +11,7 @@ const CustomRoute = ({
   const { pathname, search } = rest.history.location;
   const fullPath = pathPrefix + path + pathSuffix;
 
-  piwik.connectToHistory(rest.history);
+  Piwik.connectPiwikWithHistory(rest.history);
 
   return ((isPrivate && isAuthenticated) || !isPrivate ?
     <main className={isPrivate ? offlineStyle.disableWhenOffline : null}>
--- src/utils/piwik.js
+++ src/utils/piwik.js
@@ -1,20 +1,52 @@
 import ReactPiwik from 'react-piwik';
 import piwikOptions from '../constants/piwik';
+import localJSONStorage from './localJSONStorage';
 
-const piwik = new ReactPiwik({
-  url: piwikOptions.URL,
-  siteId: piwikOptions.SITE_ID,
-  trackErrors: true,
-});
+let piwik = false;
+let piwikSetAlready = false;
 
-const initPiwik = () => piwik;
 const setPiwikParameters = () => {
+  ReactPiwik.push([piwikOptions.REQUIRE_CONSENT]);
   ReactPiwik.push([piwikOptions.ENABLE_HEART_BEAT_TIMER, 30]);
   ReactPiwik.push([piwikOptions.TRACK_PAGE_VIEW]);
   ReactPiwik.push([piwikOptions.TRACK_ALL_CONTENT_IMPRESSIONS]);
 };
 
+const initPiwik = () => {
+  piwik = new ReactPiwik({
+    url: piwikOptions.URL,
+    siteId: piwikOptions.SITE_ID,
+    trackErrors: true,
+  });
+
+  setPiwikParameters();
+  return piwik;
+};
+
+const checkIfPiwikIsEnable = () => {
+  const isPiwikSet = localJSONStorage.get('statistics', false);
+
+  if (isPiwikSet && !piwik && !piwikSetAlready) {
+    initPiwik();
+    piwikSetAlready = true;
+  }
+};
+
+const disabledPiwikTracking = () => {
+  ReactPiwik.push([piwikOptions.FORGET_CONSENT_GIVEN]);
+};
+
+const connectPiwikWithHistory = (history) => {
+  checkIfPiwikIsEnable();
+
+  if (piwik) {
+    piwik.connectToHistory(history);
+  }
+
+  return piwik;
+};
+
 export default {
-  initPiwik,
-  setPiwikParameters,
+  connectPiwikWithHistory,
+  disabledPiwikTracking,
 };
