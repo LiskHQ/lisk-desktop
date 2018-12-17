f2a5bacbb9a3bf9855637b76d9cec942207731b8
Renato Massao Yonamine <renato.massao@lightcurve.io>
2018-12-17 17:41:49 +0100

    :recycle: Add initial structure to test components with new layout

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -11,9 +11,12 @@ import Header from '../header';
 import Dialog from '../dialog';
 import NotFound from '../notFound';
 
+import NewHeader from '../header/v2/header';
+
 import routes from '../../constants/routes';
 // eslint-disable-next-line import/no-named-as-default
 import Onboarding from '../onboarding';
+import Splashscreen from '../splashscreen/splashscreen';
 
 class App extends React.Component {
   constructor() {
@@ -22,8 +25,6 @@ class App extends React.Component {
   }
 
   markAsLoaded() {
-    this.main.classList.add(styles.loaded);
-    this.main.classList.add('appLoaded');
     this.setState({ loaded: true });
   }
 
@@ -50,61 +51,77 @@ class App extends React.Component {
       <OfflineWrapper>
         <Onboarding appLoaded={this.state.loaded} />
         <Dialog />
-        <main className={`${styles.bodyWrapper}`} ref={(el) => { this.main = el; }}>
-          <MainMenu />
-          <section>
-            <div className={styles.mainBox}>
-              <Header />
-              <div id='onboardingAnchor'></div>
-              <Switch>
-                {this.state.loaded ?
-                  <Route path={routes.explorer.path} component={ ({ location }) => (
-                    isPathCorrect(location, explorerRoutes) ? (
-                      <div>
-                        {explorerRoutes.map((route, key) => (
-                          <CustomRoute
-                            pathPrefix={route.pathPrefix}
-                            path={route.path}
-                            pathSuffix={route.pathSuffix}
-                            component={route.component}
-                            isPrivate={route.isPrivate}
-                            exact={true}
-                            key={key} />
-                        ))}
-                      </div>
-                    ) : <Route path='*' component={NotFound} />
-                  )} />
-                  : null
-                }
-                {this.state.loaded ?
-                  defaultRoutes.map((route, key) => (
-                    <CustomRoute
-                      path={route.path}
-                      pathSuffix={route.pathSuffix}
-                      component={route.component}
-                      isPrivate={route.isPrivate}
-                      exact={route.exact}
-                      key={key} />
-                  ))
-                  : null
-                }
+        <Switch>
+          <Route path={'/splashscreen'} component={() => (
+            <main className={this.state.loaded ?
+              `${styles.newWrapper} ${styles.loaded} appLoaded` :
+              `${styles.newWrapper}`
+            } ref={(el) => { this.main = el; }}>
+              <NewHeader />
+              <Splashscreen />
+            </main>
+          )} />
+          <Route component={() => (
+            <main className={this.state.loaded ?
+              `${styles.bodyWrapper} ${styles.loaded} appLoaded` :
+              `${styles.bodyWrapper}`
+            } ref={(el) => { this.main = el; }}>
+            <MainMenu />
+            <section>
+              <div className={styles.mainBox}>
+                <Header />
+                <div id='onboardingAnchor'></div>
+                <Switch>
+                  {this.state.loaded ?
+                    <Route path={routes.explorer.path} component={ ({ location }) => (
+                      isPathCorrect(location, explorerRoutes) ? (
+                        <div>
+                          {explorerRoutes.map((route, key) => (
+                            <CustomRoute
+                              pathPrefix={route.pathPrefix}
+                              path={route.path}
+                              pathSuffix={route.pathSuffix}
+                              component={route.component}
+                              isPrivate={route.isPrivate}
+                              exact={true}
+                              key={key} />
+                          ))}
+                        </div>
+                      ) : <Route path='*' component={NotFound} />
+                    )} />
+                    : null
+                  }
+                  {this.state.loaded ?
+                    defaultRoutes.map((route, key) => (
+                      <CustomRoute
+                        path={route.path}
+                        pathSuffix={route.pathSuffix}
+                        component={route.component}
+                        isPrivate={route.isPrivate}
+                        exact={route.exact}
+                        key={key} />
+                    ))
+                    : null
+                  }
 
-                {
-                  routesOutsideMainWrapper.map((route, key) => (
-                    <CustomRoute
-                      path={routes[route].path}
-                      component={routes[route].component}
-                      isPrivate={false}
-                      exact={true}
-                      key={key} />
-                  ))
-                }
-                <Route path='*' component={NotFound} />
-              </Switch>
-            </div>
-          </section>
-          <Toaster />
-        </main>
+                  {
+                    routesOutsideMainWrapper.map((route, key) => (
+                      <CustomRoute
+                        path={routes[route].path}
+                        component={routes[route].component}
+                        isPrivate={false}
+                        exact={true}
+                        key={key} />
+                    ))
+                  }
+                  <Route path='*' component={NotFound} />
+                </Switch>
+              </div>
+            </section>
+            <Toaster />
+          </main>
+          )} />
+        </Switch>
         <LoadingBar markAsLoaded={this.markAsLoaded.bind(this)} />
       </OfflineWrapper>
     );
