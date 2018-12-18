9122bbe91f5329e8b7a8f1b34ecab129888b263e
Renato Massao Yonamine <renato.massao@lightcurve.io>
2018-12-18 15:25:08 +0100

    :recycle: Update new wrapper to work better with main container

--- src/components/header/v2/header.js
+++ src/components/header/v2/header.js
@@ -11,7 +11,7 @@ const Header = () => (
     </div>
     <div className=''>
       <Link className={styles.settingButton} to={routes.setting.path}>
-        Setting
+        Settings
       </Link>
     </div>
   </header>
