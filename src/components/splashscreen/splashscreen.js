e6a866801774cd4ff2530a64068ce635229078cd
Renato Massao Yonamine <renato.massao@lightcurve.io>
2018-12-18 15:46:05 +0100

    :recycle: Add i18next to the Splashscreen

--- src/components/splashscreen/splashscreen.js
+++ src/components/splashscreen/splashscreen.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import { translate } from 'react-i18next';
 import routes from '../../constants/routes';
 import { NewPrimaryButton, NewSecondaryButton } from '../toolbox/buttons/button';
 import styles from './splashscreen.css';
 
-const Splashscreen = () => (
+const Splashscreen = ({ t }) => (
   <div className={`${styles.splashscreen} ${grid.row}`}>
     <div className={`${styles.wrapper} ${grid['col-sm-6']}`}>
       <div className={`${grid['col-xs-10']} ${styles.titleHolder}`}>
-        <h1>Welcome to the Lisk Hub!</h1>
-        <p>Create an account or sign in to manage your LSK,
-        become a delegate or vote for another delegates.</p>
+        <h1>{t('Welcome to the Lisk Hub!')}</h1>
+        <p>{
+          t('Create an account or sign in to manage your LSK, become a delegate or vote for another delegates.')
+        }</p>
       </div>
       <Link className={styles.button} to={routes.login.path}>
-        <NewSecondaryButton>Sign in</NewSecondaryButton>
+        <NewSecondaryButton>{t('Sign in')}</NewSecondaryButton>
       </Link>
       <Link className={styles.button} to={routes.register.path}>
-        <NewPrimaryButton>Create an Account</NewPrimaryButton>
+        <NewPrimaryButton>{t('Create an Account')}</NewPrimaryButton>
       </Link>
       <span className={styles.separator}>
-        <span>or</span>
+        <span>{t('or')}</span>
       </span>
-      <Link className={styles.link} to={routes.dashboard.path}>Explore as Guest</Link>
+      <Link className={styles.link} to={routes.dashboard.path}>
+        {t('Explore as Guest')}
+      </Link>
     </div>
   </div>
 );
 
-export default Splashscreen;
+export default translate()(Splashscreen);
