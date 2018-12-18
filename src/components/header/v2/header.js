fe839611edc6f36745975c603e179855db143c5b
Renato Massao Yonamine <renato.massao@lightcurve.io>
2018-12-18 17:49:51 +0100

    :recycle: Add react-i18next to new header

--- src/components/header/v2/header.js
+++ src/components/header/v2/header.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { translate } from 'react-i18next';
 import { NewSecondaryButton } from '../../toolbox/buttons/button';
 import logo from '../../../assets/images/Lisk-Logo.svg';
 import routes from '../../../constants/routes';
 import styles from './header.css';
 
-const Header = () => (
+const Header = ({ t }) => (
   <header className={`${styles.wrapper} mainHeader`}>
     <div className={`${styles.logo}`}>
       <img src={logo} />
     </div>
-    <div className=''>
+    <div>
       <Link className={styles.settingButton} to={routes.setting.path}>
-        <NewSecondaryButton>Settings</NewSecondaryButton>
+        <NewSecondaryButton>{t('Settings')}</NewSecondaryButton>
       </Link>
     </div>
   </header>
 );
 
-export default Header;
+export default translate()(Header);
