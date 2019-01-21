ef1f07067544a75a84fb8cb0bed124d0cb6ef638
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-01-21 10:58:40 +0100

    :white_check_mark: Adjust registration.spec to new layout

--- test/cypress/e2e/registration.spec.js
+++ test/cypress/e2e/registration.spec.js
@@ -1,21 +1,16 @@
-import networks from '../../constants/networks';
 import urls from '../../constants/urls';
 import ss from '../../constants/selectors';
-import chooseNetwork from '../utils/chooseNetwork';
-import moveMouseRandomly from '../utils/moveMouseRandomly';
-import slideCheckbox from '../utils/slideCheckbox';
 
 const registerUI = function () {
-  moveMouseRandomly();
+  cy.get(ss.chooseAvatar).first().click();
   cy.get(ss.getPassphraseButton).click();
-  cy.get(ss.iUnderstandCheckbox).click();
-  slideCheckbox(ss.revealCheckbox);
-  cy.get(ss.passphraseTextarea).invoke('text').as('passphrase');
+  cy.get(ss.copyPassphrase).invoke('text').as('passphrase');
   cy.get(ss.itsSafeBtn).click();
-  cy.get(ss.passphraseWordHolder).each(($el) => {
+  cy.get(ss.passphraseWordConfirm).each(($el) => {
     if (this.passphrase.includes($el[0].textContent)) cy.wrap($el).click();
   });
-  cy.get(ss.getToDashboardBtn).click();
+  cy.get(ss.passphraseConfirmButton).click();
+  cy.get(ss.loginBtn).click();
 };
 
 describe('Registration', () => {
@@ -27,80 +22,15 @@ describe('Registration', () => {
   it(`Opens by url ${urls.register}`, () => {
     cy.visit(urls.register);
     cy.url().should('contain', 'register');
-    cy.get(ss.app).contains('Create your Lisk ID');
+    cy.get(ss.app).contains('Choose your Avatar');
   });
 
   /**
-   * Create Lisk ID for Mainnet by default ("Switch Network" is not set)
-   * @expect network status is not shown
+   * Create Lisk ID
+   * @expect Lisk ID is created
    */
-  it('Create Lisk ID for Mainnet by default ("Switch Network" is not set)', function () {
+  it('Create Lisk ID', function () {
     cy.visit(urls.register);
     registerUI.call(this);
-    cy.get(ss.networkStatus).should('not.exist');
-  });
-
-  /**
-   * Create Lisk ID for Mainnet by default ("Switch Network" is false)
-   * @expect network status is not shown
-   */
-  it('Create Lisk ID for Mainnet ("Switch Network" is false)', function () {
-    cy.addLocalStorage('settings', 'showNetwork', false);
-    cy.visit(urls.register);
-    registerUI.call(this);
-    cy.get(ss.networkStatus).should('not.exist');
-  });
-
-  /**
-   * Create Lisk ID for Mainnet
-   * @expect network status shows mainnet
-   */
-  it('Create Lisk ID for Mainnet', function () {
-    cy.addLocalStorage('settings', 'showNetwork', true);
-    cy.visit('/');
-    chooseNetwork('main');
-    cy.get(ss.createLiskIdBtn).click();
-    registerUI.call(this);
-    cy.get(ss.networkStatus).contains('Connected to mainnet');
-  });
-
-  /**
-   * Create Lisk ID for Testnet
-   * @expect network status shows testnet
-   */
-  it('Create Lisk ID for Testnet', function () {
-    cy.addLocalStorage('settings', 'showNetwork', true);
-    cy.visit('/');
-    chooseNetwork('test');
-    cy.get(ss.createLiskIdBtn).click();
-    registerUI.call(this);
-    cy.get(ss.networkStatus).contains('Connected to testnet');
-  });
-
-  /**
-   * Create Lisk ID for Devnet
-   * @expect network status shows devnet
-   */
-  it('Create Lisk ID for Devnet', function () {
-    cy.addLocalStorage('settings', 'showNetwork', true);
-    cy.visit('/');
-    chooseNetwork('dev');
-    cy.get(ss.createLiskIdBtn).click();
-    registerUI.call(this);
-    cy.get(ss.networkStatus).contains('Connected to devnet');
-    cy.get(ss.nodeAddress).contains(networks.devnet.node);
-  });
-
-  /**
-   * Try Create Lisk ID with invalid address entered
-   * @expect error shown
-   */
-  it('Create Lisk ID for invalid address', () => {
-    cy.addLocalStorage('settings', 'showNetwork', true);
-    cy.visit('/');
-    chooseNetwork('invalid');
-    cy.get(ss.createLiskIdBtn).click();
-    cy.get(ss.errorPopup).contains('Unable to connect to the node');
-    cy.get(ss.createLiskIdBtn);
   });
 });
