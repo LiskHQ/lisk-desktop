ddd001c68351c519b5d88aaf85cfbd1eac65a63d
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-01-21 10:34:31 +0100

    :recycle: Update path and add classNames to help with tests

--- src/components/loginV2/loginV2.js
+++ src/components/loginV2/loginV2.js
@@ -177,7 +177,7 @@ class LoginV2 extends React.Component {
               <p>
                 {t('New to Lisk?')}
                 <Link className={`${styles.link}`}
-                  to={routes.registration.path}>
+                  to={routes.registerV2.path}>
                   {t('Create an Account')}
                 </Link>
               </p>
@@ -188,7 +188,7 @@ class LoginV2 extends React.Component {
               <div className={`${styles.inputsHolder}`}>
                 <div className={`${styles.customNode} ${this.state.network === networks.customNode.code ? styles.showInput : ''}`}>
                   <h2 className={`${styles.inputLabel}`}>{t('Enter IP or domain address of the node')}</h2>
-                  <div className={`${styles.addressInput}`}>
+                  <div className={`${styles.addressInput} address`}>
                     <input
                       className={`${this.state.addressValidity ? 'error' : ''}`}
                       type="url"
@@ -238,6 +238,7 @@ class LoginV2 extends React.Component {
                 </Link>
                 <span className={`${styles.button} ${grid['col-xs-4']}`}>
                   <PrimaryButtonV2
+                    className={'login-button'}
                     type='submit'
                     disabled={(this.state.network === networks.customNode.code
                       && !!this.state.addressValidity)
