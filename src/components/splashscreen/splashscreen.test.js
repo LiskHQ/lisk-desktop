7be7a62766b6a9bb520c497248217030fce4383e
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-01-21 11:30:31 +0100

    :white_check_mark: Adjust test for connected splashscreen

--- src/components/splashscreen/splashscreen.test.js
+++ src/components/splashscreen/splashscreen.test.js
@@ -2,21 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
+import { spy } from 'sinon';
+import configureMockStore from 'redux-mock-store';
 import { MemoryRouter } from 'react-router-dom';
 import i18n from '../../i18n';
 import SplashScreen from './splashscreen';
+import routes from '../../constants/routes';
 
 describe('V2 SplashScreen', () => {
   let wrapper;
+
+  const peers = {
+    data: {},
+    options: {},
+  };
+
+  const history = {
+    location: {
+      pathname: '',
+      search: '',
+    },
+    push: spy(),
+    replace: spy(),
+  };
+
+  const account = {
+    isDelegate: false,
+    address: '16313739661670634666L',
+    username: 'lisk-hub',
+  };
+
+  const store = configureMockStore([])({
+    peers,
+    account,
+    settings: {},
+  });
+
   const options = {
-    context: { i18n },
+    context: { store, i18n, history },
     childContextTypes: {
+      store: PropTypes.object.isRequired,
       i18n: PropTypes.object.isRequired,
+      history: PropTypes.object.isRequired,
     },
   };
 
+  const props = {
+    peers,
+    account,
+    history,
+  };
+
   beforeEach(() => {
-    wrapper = mount(<MemoryRouter><SplashScreen /></MemoryRouter>, options);
+    wrapper = mount(<MemoryRouter><SplashScreen {...props} /></MemoryRouter>, options);
   });
 
   it('Should render all links, Sign in, Create an Account and Explre as Guest', () => {
@@ -24,5 +62,28 @@ describe('V2 SplashScreen', () => {
     expect(buttons.at(0).text()).to.equal('Sign in');
     expect(buttons.at(1).text()).to.equal('Create an Account');
     expect(wrapper.find('.link').at(0).text()).to.equal('Explore as a Guest');
+  });
+
+  describe('History management', () => {
+    it('calls this.props.history.replace(\'/dashboard\')', () => {
+      wrapper.setProps({
+        history,
+        children: React.cloneElement(wrapper.props().children, {
+          account: { address: 'dummy' },
+        }),
+      });
+      expect(props.history.replace).to.have.been.calledWith(`${routes.dashboard.path}`);
+    });
+
+    it('calls this.props.history.replace with referrer address', () => {
+      props.history.replace.reset();
+      history.location.search = `?referrer=${routes.delegates.path}`;
+      wrapper.setProps({
+        children: React.cloneElement(wrapper.props().children, {
+          history, account: { address: 'dummy' },
+        }),
+      });
+      expect(props.history.replace).to.have.been.calledWith(`${routes.delegates.path}`);
+    });
   });
 });
