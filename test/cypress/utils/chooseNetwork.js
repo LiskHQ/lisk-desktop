1a187f2cb394f4bee36f9a547f45fa3e81f69f1b
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-01-21 10:34:45 +0100

    :white_check_mark: Adjust test of login.spec

--- test/cypress/utils/chooseNetwork.js
+++ test/cypress/utils/chooseNetwork.js
@@ -2,26 +2,27 @@ import networks from '../../constants/networks';
 
 const ss = {
   networkDropdown: '.network',
+  networkOptions: '.network .options > *',
 };
 
 export default function chooseNetwork(network) {
   switch (network) {
     case 'main':
       cy.get(ss.networkDropdown).click();
-      cy.get('ul li').eq(1).click();
+      cy.get(ss.networkOptions).eq(0).click();
       break;
     case 'test':
       cy.get(ss.networkDropdown).click();
-      cy.get('ul li').eq(2).click();
+      cy.get(ss.networkOptions).eq(1).click();
       break;
     case 'dev':
       cy.get(ss.networkDropdown).click();
-      cy.get('ul li').eq(3).click();
+      cy.get(ss.networkOptions).eq(2).click();
       cy.get('.address input').type(networks.devnet.node);
       break;
     case 'invalid':
       cy.get(ss.networkDropdown).click();
-      cy.get('ul li').eq(3).click();
+      cy.get(ss.networkOptions).eq(2).click();
       cy.get('.address input').type('http://silk.road');
       break;
     default:
