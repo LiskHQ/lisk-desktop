2ae671403027280cb7d28ef5b6d73ab50d599636
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-01-23 12:48:27 +0100

    :nail_care: Update css of topBar for when user is logout

--- src/components/topBar/userAccount.js
+++ src/components/topBar/userAccount.js
@@ -7,56 +7,67 @@ import DropdownV2 from '../toolbox/dropdownV2/dropdownV2';
 import { dropdownLinks } from './constants';
 import styles from './userAccount.css';
 
-const UserAccount = props => (
-  <div className={`${styles.wrapper} user-account`}>
-    <div className={styles.information}>
-      <CopyToClipboard
-        value={props.account.address}
-        className={`${styles.copyAddress} account-information-address`}
-        copyClassName={styles.copy}
-      />
-      <div className={`${styles.balance} balance`}>
-        <LiskAmount val={props.account.balance} />
-        <small>{props.t(' LSK')}</small>
-      </div>
-    </div>
-    <div
-      className={`${styles.avatar} user-avatar`}
-      onClick={() => props.onDropdownToggle()}
-    >
-      <span
-        className={styles.onAvatar}
-        ref={node => props.setDropdownRef(node)}
-      >
-        <AccountVisual
-          address={props.account.address || ''}
-          size={40}
-        />
-      </span>
-
-      <DropdownV2 showDropdown={props.isDropdownEnable}>
-        <Link
-          id={dropdownLinks.settings.id}
-          to={dropdownLinks.settings.path}
-          className={styles.dropdownOption}
-        >
-          <img src={dropdownLinks.settings.icon} className={styles.defaultIcon} />
-          <img src={dropdownLinks.settings.icon_active} className={styles.activeIcon} />
-          <span>{props.t(dropdownLinks.settings.label)}</span>
-        </Link>
-
-        <span
-          className={`${styles.dropdownOption} logout`}
-          onClick={() => props.onLogout()}
-          >
-          <img src={dropdownLinks.logout.icon} className={styles.defaultIcon}/>
-          <img src={dropdownLinks.logout.icon_active} className={styles.activeIcon}/>
-          <span>{props.t(dropdownLinks.logout.label)}</span>
+const UserAccount = (props) => {
+  if (props.isUserLogout) {
+    return (
+      <div className={styles.signIn}>
+        <p>{props.t('Welcome back')}</p>
+        <span>
+          <Link to={'/'}>{props.t('Sign in')}</Link>
+          {props.t('for full access')}
         </span>
-      </DropdownV2>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${styles.wrapper} user-account`}>
+      <div className={styles.information}>
+        <CopyToClipboard
+          value={props.account.address}
+          className={`${styles.copyAddress} account-information-address`}
+          copyClassName={styles.copy}
+        />
+        <div className={`${styles.balance} balance`}>
+          <LiskAmount val={props.account.balance}/>
+          <span>{props.t(' LSK')}</span>
+        </div>
+      </div>
+      <div
+        className={styles.avatar}
+        onClick={() => props.onDropdownToggle()}
+      >
+        <span
+          className={styles.onAvatar}
+          ref={node => props.setDropdownRef(node)}
+        >
+          <AccountVisual
+            address={props.account.address || ''}
+            size={40}
+          />
+        </span>
+
+        <DropdownV2 showDropdown={props.isDropdownEnable}>
+          <Link
+            id={dropdownLinks.settings.id}
+            to={dropdownLinks.settings.path}
+            className={styles.dropdownOption}
+          >
+            <img src={dropdownLinks.settings.icon} />
+            <span>{props.t(dropdownLinks.settings.label)}</span>
+          </Link>
+
+          <span
+            className={`${styles.dropdownOption}`}
+            onClick={() => props.onLogout()}
+            >
+            <img src={dropdownLinks.logout.icon} id={dropdownLinks.logout.id} />
+            <span>{props.t(dropdownLinks.logout.label)}</span>
+          </span>
+        </DropdownV2>
+      </div>
+    </div>);
+};
 
 
 export default UserAccount;
