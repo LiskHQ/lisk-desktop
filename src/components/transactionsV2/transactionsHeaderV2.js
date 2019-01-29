a338ef8ff176e300fe8161c16e8a6c4443734460
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-01-29 17:07:26 +0100

    :recycle: Add Transaction info and amount style

--- src/components/transactionsV2/transactionsHeaderV2.js
+++ src/components/transactionsV2/transactionsHeaderV2.js
@@ -5,11 +5,11 @@ import styles from './transactionRowV2.css';
 
 const TransactionsHeaderV2 = ({ t }) => (
   <div className={`${grid.row} ${styles.row} ${styles.header}`} id="transactionsHeader">
-    <div className={`${grid['col-xs-6']} ${grid['col-sm-3']} transactions-header`}>{t('Transaction')}</div>
-    <div className={`${styles.hiddenXs} ${grid['col-sm-3']} transactions-header`}>{t('Details')}</div>
-    <div className={`${styles.hiddenXs} ${grid['col-sm-2']} transactions-header`}>{t('Date')}</div>
-    <div className={`${styles.hiddenXs} ${grid['col-sm-2']} transactions-header`}>{t('Transaction Fee')}</div>
-    <div className={`${grid['col-xs-6']} ${grid['col-sm-2']} transactions-header`}>{t('Amount (LSK)')}</div>
+    <div className={`${grid['col-xs-6']} ${grid['col-sm-4']} ${grid['col-lg-3']} transactions-header`}>{t('Transaction')}</div>
+    <div className={`${styles.hiddenXs} ${grid['col-sm-2']} ${grid['col-lg-3']} transactions-header`}>{t('Details')}</div>
+    <div className={`${styles.hiddenXs} ${grid['col-sm-2']} ${grid['col-lg-2']} transactions-header`}>{t('Date')}</div>
+    <div className={`${styles.hiddenXs} ${grid['col-sm-2']} ${grid['col-lg-2']} transactions-header`}>{t('Transaction Fee')}</div>
+    <div className={`${grid['col-xs-6']} ${grid['col-sm-2']} ${grid['col-lg-2']} transactions-header`}>{t('Amount (LSK)')}</div>
   </div>);
 
 export default translate()(TransactionsHeaderV2);
