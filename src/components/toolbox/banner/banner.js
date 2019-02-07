ef295b221cfb484b06a1a7d63fa7d81fb73eb607
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-02-07 15:15:07 +0100

    :recycle: Update banner with close button and adjust unit test

--- src/components/toolbox/banner/banner.js
+++ src/components/toolbox/banner/banner.js
@@ -2,13 +2,17 @@ import React from 'react';
 import styles from './banner.css';
 
 const Banner = ({
-  title, children, footer, className,
+  title, children, footer, className = '',
+  onClose,
 }) => (
   <section className={`${styles.banner} ${className}`}>
     <header>
       <h1 className={`${styles.title}`}>
         {title}
       </h1>
+      { onClose ? <span
+        onClick={onClose}
+        className={`${styles.closeBtn}`} /> : null}
     </header>
     <main className={`${styles.content}`}>
       { children }
