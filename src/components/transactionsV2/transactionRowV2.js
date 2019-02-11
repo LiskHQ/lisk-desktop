3241e311ea51564afb1350d08405d9f90a92a9e1
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-02-11 14:40:15 +0100

    :recycle: Add initial animation for pending transaction

--- src/components/transactionsV2/transactionRowV2.js
+++ src/components/transactionsV2/transactionRowV2.js
@@ -10,7 +10,6 @@ import LiskAmount from '../liskAmount';
 import { DateTimeFromTimestamp } from './../timestamp/index';
 
 class TransactionRowV2 extends React.Component {
-  // eslint-disable-next-line class-methods-use-this
   shouldComponentUpdate(nextProps) {
     return nextProps.value.id !== this.props.value.id || nextProps.value.confirmations <= 1000;
   }
@@ -18,8 +17,9 @@ class TransactionRowV2 extends React.Component {
   render() {
     const { props } = this;
     const onClick = props.onClick || (() => {});
+    const isPending = !props.value.confirmations;
     return (
-      <div className={`${grid.row} ${styles.row} transactions-row`} onClick={() => onClick(props)}>
+      <div className={`${grid.row} ${styles.row} ${isPending ? styles.pending : ''} transactions-row`} onClick={() => onClick(props)}>
         <div className={`${grid['col-xs-6']} ${grid['col-sm-4']} ${grid['col-lg-3']} transactions-cell`}>
           <TransactionTypeV2 {...props.value}
             followedAccounts={props.followedAccounts}
@@ -31,8 +31,10 @@ class TransactionRowV2 extends React.Component {
               transaction={props.value} />
           </div>
         <div className={`${styles.hiddenXs} ${grid['col-sm-2']} ${grid['col-lg-2']} transactions-cell`}>
-          {props.value.confirmations ? <DateTimeFromTimestamp time={props.value.timestamp} />
-            : <SpinnerV2 label={props.t('Pending...')} />}
+          <div className={`${styles.status}`}>
+            {isPending ? <SpinnerV2 label={props.t('Pending...')} />
+              : <DateTimeFromTimestamp time={props.value.timestamp} />}
+          </div>
         </div>
         <div className={`${styles.hiddenXs} ${grid['col-sm-1']} ${grid['col-lg-2']} transactions-cell`}>
           <LiskAmount val={props.value.fee}/> {props.t('LSK')}
