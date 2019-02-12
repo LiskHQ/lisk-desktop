f11a61151778d24ab19038b840c46aa1d0b2055b
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-02-12 15:13:44 +0100

    :recycle: Add initial structure of request LSK dropdown

---
+++ src/components/requestV2/requestV2.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import QRCode from 'qrcode.react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { translate } from 'react-i18next';
+import { PrimaryButtonV2 } from '../toolbox/buttons/button';
+import { InputV2 } from '../toolbox/inputsV2';
+import styles from './requestV2.css';
+
+class RequestV2 extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      showQRCode: false,
+      shareLink: `lisk://wallet/send?recipient=${props.address}`,
+      linkCopied: false,
+      fields: {
+        amount: {
+          error: false,
+          value: '',
+          feedback: '',
+        },
+        message: {
+          error: false,
+          value: '',
+          feedback: '',
+        },
+      },
+    };
+
+    this.toggleQRCode = this.toggleQRCode.bind(this);
+    this.onCopy = this.onCopy.bind(this);
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+    this.updateShareLink = this.updateShareLink.bind(this);
+  }
+
+  onCopy() {
+    clearTimeout(this.timeout);
+    setTimeout(() => this.setState({
+      linkCopied: false,
+    }), 3000);
+
+    this.setState({
+      linkCopied: true,
+    });
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  toggleQRCode() {
+    this.setState(prevState => ({
+      showQRCode: !prevState.showQRCode,
+    }));
+  }
+
+  // eslint-disable-next-line max-statements
+  handleFieldChange({ target }) {
+    const { t } = this.props;
+    const messageMaxLength = 64;
+    const error = target.name === 'amount'
+      ? /([^\d.])/g.test(target.value) || /(\.)(.*\1){1}/g.test(target.value)
+      : target.value.length > messageMaxLength;
+    let feedback = '';
+
+    if (target.name === 'amount') {
+      target.value = /^\./.test(target.value) ? `0${target.value}` : target.value;
+      feedback = error ? t('Please use only digits and dots') : feedback;
+    } else if (target.name === 'message' && target.value.length > 0) {
+      feedback = error
+        ? t('{{length}} extra characters', { length: target.value.length - messageMaxLength })
+        : t('{{length}} out of {{total}} characters left', {
+          length: messageMaxLength - target.value.length,
+          total: messageMaxLength,
+        });
+    }
+
+    const field = {
+      [target.name]: {
+        error,
+        value: target.value,
+        feedback,
+      },
+    };
+
+    const shareLink = this.updateShareLink(field);
+
+    this.setState(prevState => ({
+      shareLink,
+      fields: {
+        ...prevState.fields,
+        ...field,
+      },
+    }));
+  }
+
+  updateShareLink(newField) {
+    const fields = {
+      ...this.state.fields,
+      ...newField,
+    };
+    const { address } = this.props;
+    return Object.keys(fields).reduce((link, fieldName) => {
+      const field = fields[fieldName];
+      return field.value !== '' && !field.error
+        ? `${link}&${fieldName}=${encodeURI(field.value)}`
+        : link;
+    }, `lisk://wallet/send?recipient=${address}`);
+  }
+
+  // eslint-disable-next-line complexity
+  render() {
+    const { t } = this.props;
+    const { fields } = this.state;
+    return (
+      <div className={`${styles.container}`}>
+        <section className={`${styles.formSection}`}>
+          <span className={`${styles.label}`}>
+            {t('Use the sharing link to easily request any amount of LSK from Lisk Hub or Lisk Mobile users.')}
+          </span>
+          <label className={`${styles.fieldGroup}`}>
+            <span className={`${styles.fieldLabel}`}>{t('Amount (LSK)')}</span>
+            <InputV2
+              autoComplete={'no'}
+              onChange={this.handleFieldChange}
+              name='amount'
+              value={fields.amount.value}
+              placeholder={t('Requested amount')}
+              className={`${styles.input} ${fields.amount.error ? 'error' : ''}`} />
+            <span className={`${styles.feedback} ${fields.amount.error ? 'error' : ''} ${fields.amount.feedback ? styles.show : ''}`}>
+              {fields.amount.feedback}
+            </span>
+          </label>
+          <label className={`${styles.fieldGroup}`}>
+            <span className={`${styles.fieldLabel}`}>{t('Message (optional)')}</span>
+            <InputV2
+              autoComplete={'no'}
+              onChange={this.handleFieldChange}
+              name='message'
+              value={fields.message.value}
+              placeholder={t('Write message')}
+              className={`${styles.input} ${fields.message.error ? 'error' : ''}`} />
+            <span className={`${styles.feedback} ${fields.message.error ? 'error' : ''} ${fields.message.feedback ? styles.show : ''}`}>
+              {fields.message.feedback}
+            </span>
+          </label>
+          <label className={`${styles.fieldGroup}`}>
+            <span className={`${styles.fieldLabel}`}>{t('Sharing link')}</span>
+            <InputV2
+              name='shareLink'
+              value={this.state.shareLink}
+              className={`${styles.input} ${styles.input}`}
+              readOnly />
+          </label>
+          <footer className={`${styles.sectionFooter}`}>
+            <CopyToClipboard
+              onCopy={this.onCopy}
+              text={this.state.shareLink}>
+                <PrimaryButtonV2
+                  disabled={this.state.linkCopied || this.state.hasError}>
+                  {this.state.linkCopied
+                    ? t('Link copied')
+                    : t('Copy Link')
+                  }
+                </PrimaryButtonV2>
+            </CopyToClipboard>
+            <span className={`${styles.footerContent} ${this.state.showQRCode ? styles.hide : ''}`}>
+              {t('Got the Lisk Mobile App?')} <span
+                className={`${styles.footerActionable}`}
+                onClick={this.toggleQRCode}>{t('Show the QR code')}
+              </span>
+            </span>
+          </footer>
+        </section>
+        <section className={`${styles.qrSection} ${!this.state.showQRCode ? styles.hide : ''}`}>
+          <span className={`${styles.label}`}>
+            {t('Simply scan the QR code using the Lisk Mobile app or any other QR code reader')}
+          </span>
+          <div className={`${styles.qrCodeContainer}`}>
+            <QRCode value={this.state.shareLink} size={200} />
+          </div>
+          <footer className={`${styles.sectionFooter}`}>
+            <span
+              className={`${styles.footerContent} ${styles.footerActionable}`}
+              onClick={this.toggleQRCode}>{t('Hide the QR code')}</span>
+          </footer>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default translate()(RequestV2);
--- src/components/toolbox/dropdownV2/dropdownV2.js
+++ src/components/toolbox/dropdownV2/dropdownV2.js
@@ -3,15 +3,15 @@ import styles from './dropdownV2.css';
 
 const DropdownV2 = ({ children, showDropdown, className }) => (
   <div className={`${styles.dropdown} ${showDropdown && styles.show} ${className}`}>
-    <span className={`${styles.dropdownArrow}`}>
+    <span className={`${styles.dropdownArrow} dropdown-arrow`}>
       <svg stroke="inherit" fill="currentColor" viewBox="0 0 36 9">
         <path d="M2 9c9-2 11-7.5 16-7.5S27 7 34 9"/>
       </svg>
     </span>
     <div className={`${styles.optionsHolder} options`}>
-      { children && children.map((child, key) => (
+      { children && children.length > 1 ? children.map((child, key) => (
         React.cloneElement(child, { className: `${child.props.className} ${styles.option}`, key })
-      )) }
+      )) : children }
     </div>
   </div>
 );
