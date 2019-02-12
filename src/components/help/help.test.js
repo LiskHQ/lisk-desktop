97e45b24781fdc9602697c568dfd7f7f3e78bd88
andreiklimenok <andrei.klimenok@lightcurve.io>
2019-02-12 12:57:57 +0100

    :white_check_mark: help.test.js snapshot implementation

--- src/components/help/help.test.js
+++ src/components/help/help.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { spy, mock } from 'sinon';
 import Help from './help';
 import links from './../../constants/externalLinks';
 
@@ -9,33 +7,28 @@ describe('Help Page', () => {
   let wrapper;
   let props;
 
-  let windowOpenMock;
-
   beforeEach(() => {
     props = {
       account: {
         address: '123L',
       },
       t: key => key,
-      settingsUpdated: spy(),
+      settingsUpdated: jest.fn(),
     };
 
-    windowOpenMock = mock(window).expects('open').returns({ focus: spy() });
+    window.open = jest.fn();
+    window.open.mockReturnValue({ focus: jest.fn() });
     wrapper = shallow(<Help {...props} />);
   });
 
-  afterEach(() => {
-    windowOpenMock.restore();
-  });
-
   it('renders three help article sections with proper btn callbacks', () => {
-    expect(wrapper).to.have.exactly(3).descendants('.help-articles article');
+    expect(wrapper).toMatchSnapshot();
 
     wrapper.find('.help-onboarding').simulate('click');
-    expect(props.settingsUpdated).to.have.been.calledWith({ onBoarding: true });
+    expect(props.settingsUpdated).toHaveBeenCalledWith({ onBoarding: true });
 
     wrapper.find('.help-visit-center').simulate('click');
-    expect(windowOpenMock).to.have.been.calledWith(links.helpCenter, '_blank');
+    expect(window.open).toHaveBeenCalledWith(links.helpCenter, '_blank');
 
     const propsNotLoggedIn = {
       ...props,
@@ -43,6 +36,6 @@ describe('Help Page', () => {
     delete propsNotLoggedIn.account.address;
     wrapper.setProps(propsNotLoggedIn);
     wrapper.update();
-    expect(wrapper).to.have.exactly(0).descendants('.help-onboarding');
+    expect(wrapper).toMatchSnapshot();
   });
 });
