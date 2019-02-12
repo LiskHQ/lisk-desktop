750604b1c5538e0f4ec1e5b01a295ecafdfbd928
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-02-12 16:40:00 +0100

    :recycle: Add converter and adjust spacing on request dropdown

---
+++ src/components/converterV2/converterV2.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import converter from '../../constants/converter';
+import styles from './converterV2.css';
+
+class ConverterV2 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    props.getPriceTicker();
+  }
+
+  render() {
+    const {
+      settings, value, error, className = '',
+    } = this.props;
+    const { LSK } = this.props.priceTicker;
+
+    const currency = settings.currency || 'USD';
+
+    let price = !!error && Number.isNaN(value)
+      ? (0).toFixed(2) : (value * LSK[currency]).toFixed(2);
+
+    price = price > converter.maxLSKSupply || price === 'NaN' || price < 0 ? (0).toFixed(2) : price;
+
+    return (
+      <div className={`${styles.wrapper} ${className}`}>
+        {value !== '' && LSK[currency]
+          ? <span className={`${styles.price} converted-price`}>~{price} {currency}</span>
+          : null
+        }
+      </div>
+    );
+  }
+}
+
+export default ConverterV2;
