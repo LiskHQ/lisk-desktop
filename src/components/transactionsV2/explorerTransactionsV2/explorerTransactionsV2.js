d12c17baca3cf2b093b776db3cec5ad8dca57dea
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-02-19 11:02:16 +0100

    :recycle: Add initial structure of another account page

---
+++ src/components/transactionsV2/explorerTransactionsV2/explorerTransactionsV2.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import TransactionsOverviewV2 from '../transactionsOverviewV2';
+import txFilters from '../../../constants/transactionFilters';
+import WalletHeader from '../walletTransactionsV2/walletHeader';
+import routes from '../../../constants/routes';
+
+class ExplorerTransactions extends React.Component {
+  constructor() {
+    super();
+
+    this.onInit = this.onInit.bind(this);
+    this.onLoadMore = this.onLoadMore.bind(this);
+    this.onFilterSet = this.onFilterSet.bind(this);
+    this.onTransactionRowClick = this.onTransactionRowClick.bind(this);
+    this.searchMoreVoters = this.searchMoreVoters.bind(this);
+  }
+
+  onInit() {
+    this.props.searchAccount({
+      address: this.props.address,
+    });
+
+    this.props.searchTransactions({
+      address: this.props.address,
+      limit: 30,
+      filter: txFilters.all,
+    });
+
+    this.props.addFilter({
+      filterName: 'transactions',
+      value: txFilters.all,
+    });
+  }
+
+  searchMoreVoters(offset) {
+    this.props.searchMoreVoters({
+      address: this.props.address,
+      offset,
+    });
+  }
+
+  onLoadMore() {
+    this.props.searchMoreTransactions({
+      address: this.props.address,
+      limit: 30,
+      offset: this.props.offset,
+      filter: this.props.activeFilter,
+    });
+  }
+  /*
+    Transactions from tabs are filtered based on filter number
+    It applys to All, Incoming and Outgoing
+    for other tabs that are not using transactions there is no need to call API
+  */
+  onFilterSet(filter) {
+    if (filter <= 2) {
+      this.props.searchTransactions({
+        address: this.props.address,
+        limit: 30,
+        filter,
+      });
+    } else {
+      this.props.addFilter({
+        filterName: 'transactions',
+        value: filter,
+      });
+    }
+  }
+
+  onTransactionRowClick(props) {
+    const transactionPath = `${routes.transactions.pathPrefix}${routes.transactions.path}/${props.value.id}`;
+    this.props.history.push(transactionPath);
+  }
+
+  render() {
+    const overviewProps = {
+      ...this.props,
+      canLoadMore: this.props.transactions.length < this.props.count,
+      onInit: this.onInit,
+      onLoadMore: this.onLoadMore,
+      onFilterSet: this.onFilterSet,
+      onTransactionRowClick: this.onTransactionRowClick,
+      searchMoreVoters: this.searchMoreVoters,
+    };
+
+    return (
+      <React.Fragment>
+        <WalletHeader
+          followedAccounts={this.props.followedAccounts}
+          address={this.props.address}
+          match={this.props.match}
+          t={this.props.t}
+          account={this.props.account}
+        />
+        <TransactionsOverviewV2 {...overviewProps} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default ExplorerTransactions;
---
+++ src/components/transactionsV2/explorerTransactionsV2/index.js
@@ -0,0 +1,42 @@
+import { connect } from 'react-redux';
+import { translate } from 'react-i18next';
+import { withRouter } from 'react-router-dom';
+import { searchTransactions, searchMoreTransactions, searchAccount, searchMoreVoters } from '../../../actions/search';
+import actionTypes from '../../../constants/actions';
+import ExplorerTransactionsV2 from './explorerTransactionsV2';
+import txFilters from '../../../constants/transactionFilters';
+
+/* istanbul ignore next */
+const mapStateToProps = (state, ownProps) => ({
+  delegate: state.search.delegates[state.search.lastSearch],
+  transaction: state.transaction,
+  transactions: state.search.searchResults,
+  votes: state.search.votes[state.search.lastSearch],
+  voters: state.search.voters[state.search.lastSearch],
+  votersSize: state.search.votersSize &&
+    state.search.votersSize[state.search.lastSearch] ?
+    state.search.votersSize[state.search.lastSearch] : 0,
+  count: state.search.transactions[state.search.lastSearch] &&
+    (state.search.transactions[state.search.lastSearch].count || null),
+  offset: state.search.searchResults.length,
+  activeFilter: state.filters.transactions || txFilters.all,
+  isSearchInStore: state.search.transactions[ownProps.address] !== undefined,
+  loading: state.loading,
+  account: state.account,
+  followedAccounts: state.followedAccounts.accounts,
+});
+
+/* istanbul ignore next */
+const mapDispatchToProps = {
+  searchAccount,
+  searchTransactions,
+  searchMoreTransactions,
+  addFilter: data => ({ type: actionTypes.addFilter, data }),
+  searchUpdateLast: data => ({ data, type: actionTypes.searchUpdateLast }),
+  searchMoreVoters,
+};
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(translate()(ExplorerTransactionsV2)));
