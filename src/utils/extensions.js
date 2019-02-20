8f3bfebae9d2c3b7cb813bf9e4bc717d67d54758
michaeltomasik <shugo12345@gmail.com>
2019-02-20 13:41:14 +0100

    :seedling: Add compiling external components

--- src/utils/extensions.js
+++ src/utils/extensions.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import LiskHubExtensions from './liskHubExtensions';
+import * as Babel from 'babel-standalone';
 
 const loadRemoteComponent = url =>
   fetch(url)
@@ -13,14 +14,12 @@ const loadRemoteComponent = url =>
         // else throw `You can't use modules other than "react" in remote component.`
         if (name === 'LiskHubExtensions') return LiskHubExtensions
       }
-      // const transformedSource = transform(source, {
-      //   presets: ['react', 'es2015', 'stage-2']
-      // }).code
-      // console.log(transformedSource, 'transformedSource');
-      // eval(transformedSource)
-      eval(source);
-      // traverse(componentRegistry, this.componentName);
-      // return source;
+
+      const transformedSource = Babel.transform(source, {
+        presets: ['es2015', 'react', 'stage-2']
+      }).code
+
+      eval(transformedSource);
       return exports.__esModule ? exports.default : exports
     });
 
