51155ae0f196285a270f92484a8e1e365b2bf1f0
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-02-22 12:03:14 +0100

    :recycle: Add validation for date before first block

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,11 +1,21 @@
 import moment from 'moment';
 
 /**
+ * Considers time
  * @param {Number} value - Date value
  * @param {String} format - Format in which the date is provided e.g. MM.DD.YY
  * @returns {Number} - Timestamp from first block
  */
 export const getTimestampFromFirstBlock = (value, format) =>
   (moment(value, format).format('x') - moment(new Date(2016, 4, 24, 17, 0, 0, 0)).format('x')) / 1000;
+
+/**
+ * Withtou consider time
+ * @param {Number} value - Date value
+ * @param {String} format - Format in which the date is provided e.g. MM.DD.YY
+ * @returns {Number} - Timestamp from first block
+ */
+export const getDateTimestampFromFirstBlock = (value, format) =>
+  (moment(value, format).format('x') - moment(new Date(2016, 4, 24)).format('x')) / 1000;
 
 export default getTimestampFromFirstBlock;
