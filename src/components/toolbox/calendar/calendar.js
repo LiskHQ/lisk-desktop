91c8c6fe04ca029e7d8b4a190873fab953b31636
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-03-01 17:26:45 +0100

    :recycle: Refactored datepicker component

--- src/components/toolbox/calendar/calendar.js
+++ src/components/toolbox/calendar/calendar.js
@@ -2,127 +2,81 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import 'moment/min/locales';
+import YearView from './yearView';
+import MonthView from './monthView';
 import styles from './calendar.css';
 
 class Calendar extends React.Component {
   constructor(props) {
     super();
 
-    const locale = Array.isArray(props.locale) ? [...props.locale] : [props.locale];
-    moment.locale([...locale, 'en']);
-
+    const locale = Array.isArray(props.locale) ? [...props.locale, 'en'] : [props.locale, 'en'];
+    moment.locale(locale);
     const showingDate =
       (moment(props.date, props.dateFormat).isValid() && moment(props.date, props.dateFormat))
       || moment();
+
     this.state = {
       showingDate,
-      selectedDate: showingDate.clone(),
+      locale,
+      currentView: 'month',
     };
 
-    this.previousMonth = this.previousMonth.bind(this);
-    this.nextMonth = this.nextMonth.bind(this);
-    this.handleSelectDate = this.handleSelectDate.bind(this);
-    this.shouldSetShowing = this.shouldSetShowing.bind(this);
+    this.setShowingDate = this.setShowingDate.bind(this);
+    this.setCurrentView = this.setCurrentView.bind(this);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  generatePlaceholder(count, day) {
-    return [...Array(count)].map((_, d) => {
-      const result = <button key={d} disabled={true} className={styles.day}>{day.format('DD')}</button>;
-      day.add(1, 'days');
-      return result;
-    });
-  }
-
-  shouldSetShowing(nextProps) {
-    const { dateFormat } = nextProps;
-    const minDate = moment(nextProps.minDate, dateFormat);
-    const maxDate = moment(nextProps.maxDate, dateFormat);
-    const updateTo = (minDate.isValid() && this.state.showingDate < minDate && 'min')
-      || (maxDate.isValid() && this.state.showingDate > maxDate && 'max')
-      || false;
-    if (updateTo) {
-      const showingDate = moment(nextProps[`${updateTo}Date`], dateFormat);
-      this.setState({ showingDate });
+  shouldComponentUpdate(nextProps) {
+    const { dateFormat } = this.props;
+    const prevDate = moment(this.props.date, dateFormat);
+    const newDate = moment(nextProps.date, dateFormat);
+    if (prevDate.isValid() && newDate.isValid()
+      && newDate.format('MM.YYYY') !== prevDate.format('MM.YYYY')) {
+      this.setState({ showingDate: newDate });
       return false;
     }
     return true;
   }
 
-  shouldComponentUpdate(nextProps) {
-    const { dateFormat } = nextProps;
-    const selectedDate =
-      (moment(nextProps.date, dateFormat).isValid()
-        && moment(nextProps.date, dateFormat))
-      || this.state.selectedDate.clone();
+  setCurrentView(view) {
+    this.setState({ currentView: view });
+  }
 
-    if (selectedDate.format(dateFormat)
-      !== this.state.selectedDate.format(dateFormat)) {
-      this.setState({ selectedDate, showingDate: selectedDate.clone() });
+  setShowingDate(date) {
+    const showingDate = moment(date);
+    if (showingDate.isValid()) {
+      this.setState({ showingDate });
     }
-    return this.shouldSetShowing(nextProps);
-  }
-
-  previousMonth() {
-    const showingDate = this.state.showingDate.subtract(1, 'month');
-    this.setState({
-      showingDate,
-    });
-  }
-
-  nextMonth() {
-    const showingDate = this.state.showingDate.add(1, 'month');
-    this.setState({
-      showingDate,
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  handleSelectDate({ target }) {
-    const selectedDate = moment(target.value, this.props.dateFormat);
-    this.setState({ selectedDate });
-    this.props.onDateSelected(target.value);
   }
 
   render() {
-    const locale = Array.isArray(this.props.locale) ? [...this.props.locale] : [this.props.locale];
-    moment.locale([...locale, 'en']);
+    moment.locale(this.state.locale);
+    const { currentView } = this.state;
     const { dateFormat } = this.props;
-    const { showingDate, selectedDate } = this.state;
-    const day = moment(showingDate).startOf('month');
 
     return (
       <section className={styles.calendarWrapper}>
-        <header className={styles.calendarHeader}>
-          <span className={styles.navigationButton} onClick={this.previousMonth} />
-          <span className={styles.monthName}>{showingDate.format('MMMM')}</span>
-          <span className={styles.navigationButton} onClick={this.nextMonth} />
-        </header>
-        <div className={styles.contentWrapper}>
-          <div className={styles.monthHeader}>
-            {moment.weekdaysShort(true).map((weekday, key) =>
-              <div className={styles.weekday} key={key}>{weekday}</div>)}
-          </div>
-          <div className={styles.month}>
-          { this.generatePlaceholder(day.weekday(), day.subtract(day.weekday(), 'days')) }
-          { [...Array(day.daysInMonth())].map((_, d) => {
-            day.date(d + 1);
-            const minDate = moment(this.props.minDate, dateFormat);
-            const maxDate = moment(this.props.maxDate, dateFormat);
-            const selected = day.format(dateFormat) === selectedDate.format('DD.MM.YY');
-            const isDisabled = (minDate.isValid() && day < minDate)
-            || (maxDate.isValid() && day > maxDate) || false;
-            return <button key={d}
-              onClick={this.handleSelectDate}
-              value={day.format(dateFormat)}
-              disabled={isDisabled}
-              className={`${styles.day} ${selected ? styles.selected : ''}`}>
-                {day.format('D')}
-              </button>;
-          })}
-          { this.generatePlaceholder(6 - day.weekday(), day.add(1, 'days')) }
-          </div>
-        </div>
+        <YearView
+          isShown={currentView === 'year'}
+          setCurrentView={this.setCurrentView}
+          setShowingDate={this.setShowingDate}
+          selectedDate={this.props.date}
+          dateFormat={dateFormat}
+          minDate={this.props.minDate}
+          maxDate={this.props.maxDate}
+          locale={this.state.locale}
+          showingDate={this.state.showingDate} />
+        <MonthView
+          isShown={currentView === 'month'}
+          setCurrentView={this.setCurrentView}
+          onDateSelected={this.props.onDateSelected}
+          setShowingDate={this.setShowingDate}
+          selectedDate={this.props.date}
+          dateFormat={dateFormat}
+          minDate={this.props.minDate}
+          maxDate={this.props.maxDate}
+          locale={this.state.locale}
+          showingDate={this.state.showingDate} />
       </section>
     );
   }
---
+++ src/components/toolbox/calendar/yearView.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import moment from 'moment';
+import 'moment/min/locales';
+import { validations } from './calendarUtils';
+import styles from './calendar.css';
+
+class YearView extends Component {
+  constructor(props) {
+    super();
+
+    this.options = {
+      dateFormat: props.dateFormat,
+      locale: props.locale,
+      amount: 'year',
+    };
+
+    this.nextYear = this.nextYear.bind(this);
+    this.previousYear = this.previousYear.bind(this);
+    this.selectMonth = this.selectMonth.bind(this);
+  }
+
+  previousYear() {
+    if (validations.canGoToPrevious(this.props.showingDate, this.props.minDate, this.options)) {
+      const showingDate = moment(this.props.showingDate).subtract(1, 'year');
+      this.props.setShowingDate(showingDate);
+    }
+  }
+
+  nextYear() {
+    if (validations.canGoToNext(this.props.showingDate, this.props.maxDate, this.options)) {
+      const showingDate = moment(this.props.showingDate, this.props.dateFormat).add(1, 'year');
+      this.props.setShowingDate(showingDate);
+    }
+  }
+
+  selectMonth({ target }) {
+    const showingDate = moment(this.props.showingDate).month(target.value);
+    this.props.setCurrentView('month');
+    this.props.setShowingDate(showingDate);
+  }
+
+  render() {
+    const { locale, dateFormat, isShown } = this.props;
+    moment.locale(locale);
+    const showingDate = moment(this.props.showingDate, dateFormat);
+    const selectedDate = moment(this.props.selectedDate, dateFormat);
+
+    return (
+      <div className={`${!isShown ? styles.hidden : ''}`}>
+        <header className={styles.calendarHeader}>
+          <span className={styles.navigationButton} onClick={this.previousYear} />
+          <span className={styles.viewName}>{showingDate.format('YYYY')}</span>
+          <span className={styles.navigationButton} onClick={this.nextYear} />
+        </header>
+        <div className={styles.contentWrapper}>
+          <div className={styles.itemsContent}>
+            {moment.monthsShort(true).map((month, key) => {
+              const options = {
+                ...this.options,
+                amount: 'month',
+              };
+              const day = moment(showingDate).month(month);
+              const selected = selectedDate.isValid()
+                && day.format('MM.YYYY') === selectedDate.format('MM.YYYY');
+              const isDisabled = validations
+                .shouldBeDisabled(day, this.props.minDate, this.props.maxDate, options);
+              return (
+                <button key={key}
+                  disabled={isDisabled}
+                  value={month}
+                  onClick={this.selectMonth}
+                  className={`${styles.item} ${styles.monthItem} ${selected ? styles.selected : ''}`}>
+                    {month}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// YearView.propTypes
+
+export default YearView;
