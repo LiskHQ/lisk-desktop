920687ab8d1d34a7d4dc005cb3f8cd24ac554f74
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-03-13 11:36:48 +0100

    :recycle: Update topBar component to properly focus the menu item selected based on the path or page selected

--- src/components/topBar/menuItems.js
+++ src/components/topBar/menuItems.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './menuItems.css';
 
-const MenuItems = props => (
-  <div className={`${styles.wrapper} menu-items`}>
-    {
-      props.items && props.items.map(item =>
-        <NavLink
-          key={item.id}
-          to={item.path}
-          className={`${styles.item} ${(props.isUserLogout && item.id !== 'dashboard') ? styles.notActive : ''}`}
-          id={item.id}
-          activeClassName={styles.selected}
-        >
-          <img src={ props.location.pathname === item.path ? item.iconActive : item.icon } />
-          <span>{props.t(item.label)}</span>
-        </NavLink>)
-    }
-  </div>
-);
+const MenuItems = (props) => {
+  function isActive(menuItem) {
+    const { location } = props;
+    return location.pathname.includes(menuItem.path);
+  }
+
+  return (
+    <div className={`${styles.wrapper} menu-items`}>
+      {
+        props.items && props.items.map(item =>
+          <NavLink
+            key={item.id}
+            to={item.path}
+            className={`${styles.item} ${(props.isUserLogout && item.id !== 'dashboard') ? styles.notActive : ''}`}
+            id={item.id}
+            activeClassName={styles.selected}
+          >
+            <img src={ isActive(item) ? item.iconActive : item.icon } />
+            <span>{item.label}</span>
+          </NavLink>)
+      }
+    </div>
+  );
+};
 
 export default MenuItems;
