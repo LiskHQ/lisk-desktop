6757ff0799e5959c16059b01bc60de97a6b37a55
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-03-13 15:00:03 +0100

    :recycle: Add table header to votes tab

--- src/components/votes/VotesTab.js
+++ src/components/votes/VotesTab.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { translate } from 'react-i18next';
 import BoxV2 from '../boxV2';
-import TableHeader from './TableHeader';
+import VotesTableHeader from './VotesTableHeader';
+import styles from './votesTab.css';
 
 class VotesTab extends React.Component {
   constructor() {
@@ -15,12 +16,12 @@ class VotesTab extends React.Component {
   render() {
     const { t } = this.props;
     return (
-      <BoxV2>
+      <BoxV2 className={`${styles.wrapper}`}>
         <header>
         <h1>{t('Voted delegates')}</h1>
         </header>
         <main>
-          <TableHeader />
+          <VotesTableHeader />
         </main>
       </BoxV2>
     );
---
+++ src/components/votes/VotesTableHeader.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import { translate } from 'react-i18next';
+import TableRow from '../toolbox/table/tableRow';
+import Tooltip from '../toolbox/tooltip/tooltip';
+
+const VotesTableHeader = ({ t }) => (
+  <TableRow isHeader={true}>
+    <div className={`${grid['col-sm-1']} ${grid['col-lg-1']}`}>{t('Rank')}</div>
+    <div className={`${grid['col-sm-3']} ${grid['col-lg-6']}`}>{t('Delegate')}</div>
+    <div className={`${grid['col-sm-3']} ${grid['col-lg-2']}`}>
+      {t('Forged')}
+      <Tooltip className={'showOnBottom'}>
+        <p>{t('Amount of LSK that the delegate earned by forging blocks of transactions.')}</p>
+      </Tooltip>
+    </div>
+    <div className={`${grid['col-sm-2']} ${grid['col-lg-1']}`}>
+      {t('Productivity')}
+      <Tooltip className={'showOnBottom'}>
+        <p>{t('Percentage of successfully forged blocks of when the delegate should have forged a block of transactions.')}</p>
+      </Tooltip>
+    </div>
+    <div className={`${grid['col-sm-3']} ${grid['col-lg-2']}`}>
+      {t('Vote weight')}
+      <Tooltip className={'showOnBottom'}>
+        <p>{t('Sum of LSK balance of all accounts who voted for this delegate.')}</p>
+      </Tooltip>
+    </div>
+  </TableRow>
+);
+
+export default translate()(VotesTableHeader);
