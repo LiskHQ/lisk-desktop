016b6a7bc0c1610be8b03cb77c8d695fda53ebd4
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-03-13 11:02:30 +0100

    :white_check_mark: Update the snapshot testing of help component

--- src/components/help/help.test.js
+++ src/components/help/help.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Help from './help';
 import links from './../../constants/externalLinks';
 
 describe('Help Page', () => {
   let wrapper;
   let props;
+  let rendered;
 
   beforeEach(() => {
     props = {
@@ -19,10 +21,11 @@ describe('Help Page', () => {
     window.open = jest.fn();
     window.open.mockReturnValue({ focus: jest.fn() });
     wrapper = shallow(<Help {...props} />);
+    rendered = renderer.create(<Help {...props} />).toJSON();
   });
 
   it('renders three help article sections with proper btn callbacks', () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(rendered).toMatchSnapshot();
 
     wrapper.find('.help-onboarding').simulate('click');
     expect(props.settingsUpdated).toHaveBeenCalledWith({ onBoarding: true });
@@ -36,6 +39,6 @@ describe('Help Page', () => {
     delete propsNotLoggedIn.account.address;
     wrapper.setProps(propsNotLoggedIn);
     wrapper.update();
-    expect(wrapper).toMatchSnapshot();
+    expect(rendered).toMatchSnapshot();
   });
 });
