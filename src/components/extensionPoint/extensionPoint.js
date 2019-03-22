799d61c030f2e0da37ea9c518f76593e364c10a4
michaeltomasik <shugo12345@gmail.com>
2019-03-22 12:05:27 +0100

    :recycle: Fix boxV2 styles for extensions

--- src/components/extensionPoint/extensionPoint.js
+++ src/components/extensionPoint/extensionPoint.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import LiskHubExtensions from '../../utils/liskHubExtensions';
-import styles from './extensionPoint.css';
 
 export default class ExtensionPoint extends React.Component {
   componentDidCatch(error) {
@@ -32,33 +31,30 @@ export default class ExtensionPoint extends React.Component {
       { modules.map(({ moduleId }, i) => {
         const Component = LiskHubExtensions._modules[moduleId];
         if (Component) {
-          return <div
-            className={styles.wrapper}>
-            <Component
-              data = {{
-                latestBlocks: this.props.blocks && this.props.blocks.latestBlocks,
-                transactions: this.props.transactions,
-                time: this.props.test,
-                accountAddress: this.props.account && this.props.account.address,
-                search: this.props.search,
-              }}
-              actions={{
-                testExtensions,
-                loadTransactions,
-                sent,
-                loadTransaction,
-                transactionsFilterSet,
-                searchDelegate,
-                searchVotes,
-                searchVoters,
-                searchAccount,
-                searchTransactions,
-              }}
-              onClickHandle={() => { this.props.testExtensions(); }}
-              t={this.props.t}
-              identifier={this.props.identifier}
-              key={i} />
-          </div>;
+          return <Component
+            data = {{
+              latestBlocks: this.props.blocks && this.props.blocks.latestBlocks,
+              transactions: this.props.transactions,
+              time: this.props.test,
+              accountAddress: this.props.account && this.props.account.address,
+              search: this.props.search,
+            }}
+            actions={{
+              testExtensions,
+              loadTransactions,
+              sent,
+              loadTransaction,
+              transactionsFilterSet,
+              searchDelegate,
+              searchVotes,
+              searchVoters,
+              searchAccount,
+              searchTransactions,
+            }}
+            onClickHandle={() => { this.props.testExtensions(); }}
+            t={this.props.t}
+            identifier={this.props.identifier}
+            key={i} />;
         }
         // eslint-disable-next-line no-console
         console.error(new Error(`Invalid component in extension point ${this.props.identifier}`));
