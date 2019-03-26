bb0ba06d685070ee9c4abb9167144d208fa424dc
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-03-26 10:46:09 +0100

    :recycle: Adjust dropdown on login to final UI

--- src/components/toolbox/dropdownV2/dropdownV2.js
+++ src/components/toolbox/dropdownV2/dropdownV2.js
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './dropdownV2.css';
 
-const DropdownV2 = ({ children, showDropdown, className }) => (
+const DropdownV2 = ({
+  children, showDropdown, className, showArrow,
+}) => (
   <div className={`${styles.dropdown} ${showDropdown ? styles.show : ''} ${className}`}>
-    <span className={`${styles.dropdownArrow} dropdown-arrow`}>
+    {showArrow && <span className={`${styles.dropdownArrow} dropdown-arrow`}>
       <svg stroke="inherit" fill="currentColor" viewBox="0 0 36 9">
         <path d="M2 9c9-2 11-7.5 16-7.5S27 7 34 9"/>
       </svg>
-    </span>
-    <div className={`${styles.optionsHolder} options`}>
-      { children && children.length > 1 ? children.map((child, key) => (
-        React.cloneElement(child, { className: `${child.props.className} ${styles.option}`, key })
+    </span>}
+    <div className={`${styles.dropdownContent} dropdown-content`}>
+      { children && Array.isArray(children) ? children.map((child, key) => (
+        React.cloneElement(child, { className: `${child.props.className || ''} ${styles.option}`, key })
       )) : children }
     </div>
   </div>
@@ -22,13 +24,15 @@ DropdownV2.propTypes = {
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element),
   ]).isRequired,
-  showDropdown: PropTypes.bool.isRequired,
-  className: PropTypes.string.isRequired,
+  showDropdown: PropTypes.bool,
+  className: PropTypes.string,
+  showArrow: PropTypes.bool,
 };
 
 DropdownV2.defaultProps = {
   showDropdown: false,
   className: '',
+  showArrow: true,
 };
 
 export default DropdownV2;
