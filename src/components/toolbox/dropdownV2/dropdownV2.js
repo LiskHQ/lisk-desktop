ae021388a6564f7cdd35fcb1db86b25c2116008b
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-03-26 17:03:14 +0100

    :recycle: Update dropdown with e2e classes when it is selection list

--- src/components/toolbox/dropdownV2/dropdownV2.js
+++ src/components/toolbox/dropdownV2/dropdownV2.js
@@ -4,20 +4,23 @@ import styles from './dropdownV2.css';
 
 const DropdownV2 = ({
   children, showDropdown, className, showArrow,
-}) => (
-  <div className={`${styles.dropdown} ${showDropdown ? styles.show : ''} ${className}`}>
-    {showArrow && <span className={`${styles.dropdownArrow} dropdown-arrow`}>
-      <svg stroke="inherit" fill="currentColor" viewBox="0 0 36 9">
-        <path d="M2 9c9-2 11-7.5 16-7.5S27 7 34 9"/>
-      </svg>
-    </span>}
-    <div className={`${styles.dropdownContent} dropdown-content`}>
-      { children && Array.isArray(children) ? children.map((child, key) => (
-        React.cloneElement(child, { className: `${child.props.className || ''} ${styles.option}`, key })
-      )) : children }
+}) => {
+  const isSelectionList = children && Array.isArray(children);
+  return (
+    <div className={`${styles.dropdown} ${showDropdown ? styles.show : ''} ${className}`}>
+      {showArrow && <span className={`${styles.dropdownArrow} dropdown-arrow`}>
+        <svg stroke="inherit" fill="currentColor" viewBox="0 0 36 9">
+          <path d="M2 9c9-2 11-7.5 16-7.5S27 7 34 9"/>
+        </svg>
+      </span>}
+      <div className={`${styles.dropdownContent} dropdown-content ${isSelectionList ? 'options' : ''}`}>
+        { isSelectionList ? children.map((child, key) => (
+          React.cloneElement(child, { className: `${child.props.className || ''} ${styles.option}`, key })
+        )) : children }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 DropdownV2.propTypes = {
   children: PropTypes.oneOfType([
