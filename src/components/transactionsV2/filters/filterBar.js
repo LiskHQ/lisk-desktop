4a7d210e28e0cb4025c5020f72817bfc131e86d9
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-04-01 15:04:31 +0200

    :recycle: Adjust filterBar on small breakpoint

--- src/components/transactionsV2/filters/filterBar.js
+++ src/components/transactionsV2/filters/filterBar.js
@@ -8,43 +8,46 @@ const FilterBar = props => (
     <span className={styles.label}>
       {props.t('Filtered results: {{results}}', { results: props.results })}
     </span>
-    {Object.keys(props.customFilters).map((filter, index) => {
-      let label = props.customFilters[filter];
-      if (label === '') return null;
+    <div className={styles.labelsHolder}>
+      {Object.keys(props.customFilters).map((filter, index) => {
+        let label = props.customFilters[filter];
+        if (label === '') return null;
 
-      switch (filter) {
-        case 'dateFrom':
-        case 'dateTo': {
-          const prefix = filter === 'dateFrom' ? props.t('from') : props.t('till');
-          label = `${prefix} ${moment(label, 'DD.MM.YY').format('Do MMM YYYY')}`;
-          break;
+        switch (filter) {
+          case 'dateFrom':
+          case 'dateTo': {
+            const prefix = filter === 'dateFrom' ? props.t('from') : props.t('till');
+            label = `${prefix} ${moment(label, 'DD.MM.YY').format('Do MMM YYYY')}`;
+            break;
+          }
+          case 'amountFrom':
+          case 'amountTo': {
+            const prefix = filter === 'amountFrom' ? '>' : '<';
+            label = `${prefix} ${label} ${props.t('LSK')}`;
+            break;
+          }
+          default:
+            break;
         }
-        case 'amountFrom':
-        case 'amountTo': {
-          const prefix = filter === 'amountFrom' ? '>' : '<';
-          label = `${prefix} ${label} ${props.t('LSK')}`;
-          break;
-        }
-        default:
-          break;
+
+        return (
+          <div
+            className={`${styles.filter}`}
+            key={filter + index}>
+              <p className={styles.label}>{label}</p>
+              <span
+                className={styles.clearBtn}
+                onClick={() => props.clearFilter(filter)} />
+          </div>);
+        })
       }
-
-      return (
-        <div
-          className={`${styles.filter}`}
-          key={filter + index}>
-            <p className={styles.label}>{label}</p>
-            <span
-              className={styles.clearBtn}
-              onClick={() => props.clearFilter(filter)} />
-        </div>);
-      })
-    }
-    <SecondaryButtonV2
-      className={'extra-small'}
-      onClick={props.clearAllFilters}>
-      {props.t('Clear All filters')}
-    </SecondaryButtonV2>
-  </div>);
+      <SecondaryButtonV2
+        className={'extra-small'}
+        onClick={props.clearAllFilters}>
+        {props.t('Clear All filters')}
+      </SecondaryButtonV2>
+    </div>
+  </div>
+);
 
 export default FilterBar;
