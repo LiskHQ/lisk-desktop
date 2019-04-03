61efd8ec2bf3aa603355dd90de47dfb1f6caf9fc
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-04-03 09:06:48 +0200

    :recycle: Add storywrapper to storybook to add space around components

--- storybook/stories/buttons.stories.js
+++ storybook/stories/buttons.stories.js
@@ -2,31 +2,31 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-
+import StoryWrapper from '../components/StoryWrapper/StoryWrapper';
 import { PrimaryButtonV2, SecondaryButtonV2, DangerButtonV2 } from 'Components/toolbox/buttons/button';
 
 storiesOf('Button', module)
   .add('Primary Button', () => (
-    <React.Fragment>
+    <StoryWrapper>
       <h2>Enabled State</h2>
       <PrimaryButtonV2 onClick={action('clicked')}>Get from GitHub</PrimaryButtonV2>
       <h2>Disabled State</h2>
       <PrimaryButtonV2 disabled onClick={action('clicked')}>Get from GitHub</PrimaryButtonV2>
-    </React.Fragment>
+    </StoryWrapper>
   ))
   .add('Secondary Button', () =>
-    <React.Fragment>
+    <StoryWrapper>
       <h2>Enabled State</h2>
       <SecondaryButtonV2 onClick={action('clicked')}>Get from GitHub</SecondaryButtonV2>
       <h2>Disabled State</h2>
       <SecondaryButtonV2 disabled onClick={action('clicked')}>Get from GitHub</SecondaryButtonV2>
-    </React.Fragment>
+    </StoryWrapper>
   )
   .add('Danger Button', () =>
-    <React.Fragment>
+    <StoryWrapper>
       <h2>Enabled State</h2>
       <DangerButtonV2 onClick={action('clicked')}>Get from GitHub</DangerButtonV2>
       <h2>Disabled State</h2>
       <DangerButtonV2 disabled onClick={action('clicked')}>Get from GitHub</DangerButtonV2>
-    </React.Fragment>
+    </StoryWrapper>
   );
