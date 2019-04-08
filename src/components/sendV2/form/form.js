d39a6479c67abcff3227d1d48d6615a92bcd6b0e
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-04-08 18:35:40 +0200

    :recycle: Update send LSK form amount validator

--- src/components/sendV2/form/form.js
+++ src/components/sendV2/form/form.js
@@ -234,6 +234,7 @@ class Form extends React.Component {
   }
 
   validateAmountField(value) {
+    if (/^0.(0|[a-zA-z])*$/g.test(value)) return this.props.t('Provide a correct amount of LSK');
     if (/([^\d.])/g.test(value)) return this.props.t('Provide a correct amount of LSK');
     if ((/(\.)(.*\1){1}/g.test(value) || /\.$/.test(value)) || value === '0') return this.props.t('Invalid amount');
     if (parseFloat(this.getMaxAmount()) < value) return this.props.t('Provided amount is higher than your current balance.');
