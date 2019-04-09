0c60209ac8d6b0905468b1cd3dd87ccd635f8d0d
Vit Stanislav <vit@lightcurve.io>
2019-04-09 16:25:34 +0200

    :white_check_mark: Fix unit test coverage of search reducer

--- src/store/reducers/search.test.js
+++ src/store/reducers/search.test.js
@@ -214,7 +214,57 @@ describe('Reducer: search', () => {
     });
   });
 
-  it('should reduce more transactions loaded', () => {
+  it('should set transactions of an account to be the transactions loaded by the searchTransactions action', () => {
+    const state = {
+      transactions: {
+        '0L': {
+          transactions: [
+            {
+              id: '1',
+            },
+          ],
+        },
+      },
+    };
+
+    const action = {
+      type: actionTypes.searchTransactions,
+      data: {
+        address: '0L',
+        transactions: [
+          {
+            id: '2',
+          },
+        ],
+        count: 2,
+        filter: 0,
+      },
+    };
+
+    const changedState = search(state, action);
+    expect(changedState).toEqual({
+      transactions: {
+        '0L': {
+          address: '0L',
+          transactions: [
+            {
+              id: '2',
+            },
+          ],
+          count: 2,
+          filter: 0,
+        },
+      },
+      lastSearch: '0L',
+      searchResults: [
+        {
+          id: '2',
+        },
+      ],
+    });
+  });
+
+  it('should ammend transactions of an account to contain the transactions loaded by the searchMoreTransactions action', () => {
     const state = {
       transactions: {
         '0L': {
@@ -268,6 +318,21 @@ describe('Reducer: search', () => {
         },
       ],
     });
+  });
+
+  it('should return state if action.type is none of the above', () => {
+    const state = {
+      suggestions: {
+        delegates: mockData,
+        addresses: mockData,
+        transactions: mockData,
+      },
+    };
+    const action = {
+      type: 'UNKNOWN',
+    };
+    const changedState = search(state, action);
+    expect(changedState).toEqual(state);
   });
 });
 
