bbda3118ed104931fd751d09ba93442e9b899f47
Altay <altay.aydemir@lightcurve.io>
2019-04-12 16:07:14 +0200

    Update src/utils/api/btc/config.js to use service bridge API
    Co-Authored-By: massao <rmy.yonamine@gmail.com>

--- src/utils/api/btc/config.js
+++ src/utils/api/btc/config.js
@@ -2,7 +2,7 @@ import * as bitcoin from 'bitcoinjs-lib';
 
 const getBtcConfig = netCode => ({
   isTestnet: netCode !== 0,
-  url: netCode !== 0 ? 'http://localhost:7201' : 'https://blockchain.info',
+  url: netCode !== 0 ? 'https://ams1-bitcoin-test-001.ed148.net' : 'https://ams1-bitcoin-001.ed148.net'
   minerFeesURL: 'https://bitcoinfees.earn.com/api/v1/fees/recommended',
   network: netCode !== 0 ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
   derivationPath: netCode !== 0 ? "m/44'/1'/0'/0/0" : "m/44'/0'/0'/0/0",
