ad98c11ec601b6f13689ff599a50cdf39d094c56
Vit Stanislav <vit@lightcurve.io>
2019-04-24 13:32:20 +0200

    :seedling: Implement btc api client

--- src/utils/api/btc/network.js
+++ src/utils/api/btc/network.js
@@ -1,7 +1,20 @@
+import * as popsicle from 'popsicle';
+
+import networks from '../../../constants/networks';
 import getBtcConfig from './config';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getAPIClient = (network) => {
-  throw new Error('Not yet implemented', network);
+  // TODO update getBtcConfig to accept network name instead of code
+  const config = getBtcConfig(network.name === networks.mainnet.name ?
+    networks.mainnet.code :
+    networks.testnet.code);
+  return {
+    get: path => (
+      popsicle.get(`${config.url}/${path}`).use(popsicle.plugins.parse('json'))
+    ),
+    post: (path, body) => (
+      popsicle.post(`${config.url}/${path}`, { body: JSON.stringify(body) })
+    ),
+  };
 };
-
