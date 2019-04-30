ceabd5a0ec5fb387b306c9613e173c8d00ee6648
Vit Stanislav <vit@lightcurve.io>
2019-04-30 15:17:19 +0200

    :white_check_mark: Create unit tests for hwWalletLogin

---
+++ src/components/hwWalletLogin/hwWalletLogin.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { mount } from 'enzyme';
+import HwWalletLogin from './hwWalletLogin';
+
+describe('HwWalletLogin', () => {
+  let wrapper;
+  const props = {
+    settings: {
+      isHarwareWalletConnected: false,
+    },
+    t: key => key,
+  };
+
+  const history = {
+    location: {
+      pathname: '',
+      search: '',
+    },
+    createHref: jest.fn(),
+    push: jest.fn(),
+    replace: jest.fn(),
+  };
+
+  const options = {
+    context: {
+      history, router: { route: history, history },
+    },
+    childContextTypes: {
+      history: PropTypes.object.isRequired,
+      router: PropTypes.object.isRequired,
+    },
+    lifecycleExperimental: true,
+  };
+
+  it('should show "looking for device" at first and "SelectDevice" after device conneted', () => {
+    wrapper = mount(<HwWalletLogin {...props}/>, options);
+    wrapper.update();
+    // TODO figure out why the assertion below doesn't work
+    // expect(wrapper.text()).toContain('Looking for a device...');
+    wrapper.setProps({
+      ...props,
+      settings: { isHarwareWalletConnected: true },
+    });
+    wrapper.update();
+    // TODO figure out why the assertion below doesn't work
+    // expect(wrapper.text()).toContain('SelectDevice');
+  });
+});
