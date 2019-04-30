dcb83118a569428f65ec1ca4c93f5bf3b3722ebb
Vit Stanislav <vit@lightcurve.io>
2019-04-30 12:27:22 +0200

    :seedling: Implement the logic of hw wallet loading step

--- src/components/hwWalletLogin/loading.js
+++ src/components/hwWalletLogin/loading.js
@@ -5,18 +5,35 @@ import routes from '../../constants/routes';
 import styles from './loading.css';
 import svgIcons from '../../utils/svgIcons';
 
-const Loading = ({ t }) => (
-  <React.Fragment>
-    <h2>{t('Connect your Hardware Wallet')}</h2>
-    <p>{t('Lisk Hub currently supports Ledger Nano S and Trezor wallets')}</p>
-    <img src={svgIcons.iconLoader} className={styles.loadingIcon} />
-    <p>{t('Looking for a device…')}</p>
-    <Link to={routes.splashscreen.path}>
-      <TertiaryButtonV2>
-        {t('Go Back')}
-      </TertiaryButtonV2>
-    </Link>
-  </React.Fragment>
-);
+class Loading extends React.Component {
+  componentDidMount() {
+    this.goNextIfDeviceConnected();
+  }
+
+  componentDidUpdate() {
+    this.goNextIfDeviceConnected();
+  }
+
+  goNextIfDeviceConnected() {
+    if (this.props.devices.length > 0) {
+      this.props.nextStep();
+    }
+  }
+
+  render() {
+    const { t } = this.props;
+    return <React.Fragment>
+      <h2>{t('Connect your Hardware Wallet')}</h2>
+      <p>{t('Lisk Hub currently supports Ledger Nano S and Trezor wallets')}</p>
+      <img src={svgIcons.iconLoader} className={styles.loadingIcon} />
+      <p>{t('Looking for a device…')}</p>
+      <Link to={routes.splashscreen.path}>
+        <TertiaryButtonV2>
+          {t('Go Back')}
+        </TertiaryButtonV2>
+      </Link>
+    </React.Fragment>;
+  }
+}
 
 export default Loading;
