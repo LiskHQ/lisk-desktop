ac68c9136ba7b19da4fbaeb7e42efce1c0245747
Vit Stanislav <vit@lightcurve.io>
2019-05-03 10:42:36 +0200

    :seedling: Update BTC account utils to use network config

--- src/utils/api/btc/account.js
+++ src/utils/api/btc/account.js
@@ -1,11 +1,28 @@
 import * as bitcoin from 'bitcoinjs-lib';
 import Lisk from 'lisk-elements';
 import bip32 from 'bip32';
-import getBtcConfig from './config';
 import { getAPIClient } from './network';
 
-export const getAccount = ({ networkConfig, address }) => new Promise(async (resolve, reject) => {
-  await getAPIClient(networkConfig).get(`account/${address}`).then((response) => {
+export const getDerivedPathFromPassphrase = (passphrase, config) => {
+  const seed = Lisk.passphrase.Mnemonic.mnemonicToSeed(passphrase);
+  return bip32.fromSeed(seed, config.network).derivePath(config.derivationPath);
+};
+
+export const extractPublicKey = (passphrase, config) =>
+  getDerivedPathFromPassphrase(passphrase, config).publicKey;
+
+export const extractAddress = (passphrase, config) => {
+  const publicKey = extractPublicKey(passphrase, config);
+  const btc = bitcoin.payments.p2pkh({ pubkey: publicKey, network: config.network });
+  return btc.address;
+};
+
+export const getAccount = ({
+  networkConfig, address, passphrase,
+}) => new Promise(async (resolve, reject) => {
+  const apiClient = getAPIClient(networkConfig);
+  address = address || extractAddress(passphrase, apiClient.config);
+  await apiClient.get(`account/${address}`).then((response) => {
     resolve({
       address,
       balance: response.body.data.confirmed_balance,
@@ -13,19 +30,3 @@ export const getAccount = ({ networkConfig, address }) => new Promise(async (res
     });
   }).catch(reject);
 });
-
-export const getDerivedPathFromPassphrase = (passphrase, netCode = 1) => {
-  const config = getBtcConfig(netCode);
-  const seed = Lisk.passphrase.Mnemonic.mnemonicToSeed(passphrase);
-  return bip32.fromSeed(seed, config.network).derivePath(config.derivationPath);
-};
-
-export const extractPublicKey = (passphrase, netCode = 1) =>
-  getDerivedPathFromPassphrase(passphrase, netCode).publicKey;
-
-export const extractAddress = (passphrase, netCode = 1) => {
-  const config = getBtcConfig(netCode);
-  const publicKey = extractPublicKey(passphrase);
-  const btc = bitcoin.payments.p2pkh({ pubkey: publicKey, network: config.network });
-  return btc.address;
-};
