b47305debee918959e3b7bac29f9cb25218a8946
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-06 11:40:15 +0200

    :white_check_mark: Adjust unit test due to change on headerV2

--- src/components/hwWalletLogin/hwWalletLogin.test.js
+++ src/components/hwWalletLogin/hwWalletLogin.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import configureMockStore from 'redux-mock-store';
 import { mount } from 'enzyme';
 import * as hwWalletUtils from '../../utils/hwWallet';
 import HwWalletLogin from './hwWalletLogin';
@@ -8,10 +10,18 @@ jest.mock('../../utils/hwWallet');
 
 describe('HwWalletLogin', () => {
   let wrapper;
+  const store = configureMockStore([])({});
   const props = {
     devices: [],
     devicesListUpdated: jest.fn(),
     t: key => key,
+  };
+
+  const options = {
+    context: { store },
+    childContextTypes: {
+      store: PropTypes.object.isRequired,
+    },
   };
 
   it('Should render Loading component and call getDeviceList ', async () => {
@@ -21,7 +31,7 @@ describe('HwWalletLogin', () => {
       { deviceId: 3, openApp: true, model: 'Ledger' },
     ]);
 
-    wrapper = mount(<Router><HwWalletLogin {...props} /></Router>);
+    wrapper = mount(<Router><HwWalletLogin {...props} /></Router>, options);
     const devices = await hwWalletUtils.getDeviceList();
     expect(wrapper).toContainMatchingElement('Loading');
     expect(props.devicesListUpdated).toBeCalledWith(devices);
