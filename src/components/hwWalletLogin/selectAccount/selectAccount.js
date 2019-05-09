ed2dac0fa02c5c5ce12dfe7b886345fc1b699c32
Vit Stanislav <vit@lightcurve.io>
2019-05-09 14:33:12 +0200

    :bug: Add missing translation function to selectAccount

--- src/components/hwWalletLogin/selectAccount/selectAccount.js
+++ src/components/hwWalletLogin/selectAccount/selectAccount.js
@@ -39,12 +39,12 @@ class SelectAccount extends React.Component {
   }
 
   getNameFromAccount(address) {
-    const { settings } = this.props;
+    const { settings, t } = this.props;
     // istanbul ignore else
     if (Array.isArray(settings.hardwareAccounts)) {
       const storedAccount = settings.hardwareAccounts.filter(account =>
         account.address === address);
-      return storedAccount.length ? storedAccount[0].name : 'Unnamed account';
+      return storedAccount.length ? storedAccount[0].name : t('Unnamed account');
     }
 
     return 'Unnamed account';
