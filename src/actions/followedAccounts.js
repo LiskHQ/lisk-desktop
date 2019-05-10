d3484309506c25f02eee70f4521a5ba325d234aa
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-10 11:14:01 +0200

    :recycle: Update bookmark actions

--- src/actions/followedAccounts.js
+++ src/actions/followedAccounts.js
@@ -1,29 +1,16 @@
 import actionTypes from '../constants/actions';
-import { getAccount } from '../utils/api/lsk/account';
 
-export const followedAccountAdded = account => ({
-  data: account,
+export const followedAccountAdded = ({ account, token = 'LSK' }) => ({
+  data: { account, token },
   type: actionTypes.followedAccountAdded,
 });
 
-export const followedAccountUpdated = account => ({
-  data: account,
+export const followedAccountUpdated = ({ account, token = 'LSK' }) => ({
+  data: { account, token },
   type: actionTypes.followedAccountUpdated,
 });
 
-export const followedAccountRemoved = account => ({
-  data: account,
+export const followedAccountRemoved = ({ address, token = 'LSK' }) => ({
+  data: { address, token },
   type: actionTypes.followedAccountRemoved,
 });
-
-export const followedAccountFetchedAndUpdated = ({ account }) =>
-  (dispatch, getState) => {
-    const liskAPIClient = getState().peers.liskAPIClient;
-    getAccount(liskAPIClient, account.address).then((result) => {
-      if (result.balance !== account.balance || result.publicKey !== account.publicKey) {
-        account.balance = result.balance;
-        if (result.publicKey) account.publicKey = result.publicKey;
-        dispatch(followedAccountUpdated(account));
-      }
-    });
-  };
