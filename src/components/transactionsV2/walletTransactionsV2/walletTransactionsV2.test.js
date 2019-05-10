4dc942f7a1b2c91a95cf116279204cdab73e8745
Vit Stanislav <vit@lightcurve.io>
2019-05-10 12:19:37 +0200

    :white_check_mark: Prevent extra output from component tests

--- src/components/transactionsV2/walletTransactionsV2/walletTransactionsV2.test.js
+++ src/components/transactionsV2/walletTransactionsV2/walletTransactionsV2.test.js
@@ -133,7 +133,7 @@ describe('WalletTransactions V2 Component', () => {
           rewards: '140500000000',
           username: accounts.delegate.username,
           vote: '9876965713168313',
-          lastBlock: { timestamp: 0 },
+          lastBlock: 0,
           txDelegateRegister: { timestamp: 0 },
         },
       },
