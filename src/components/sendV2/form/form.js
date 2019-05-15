3c319c045335be67fb0f9d83fd765dfe030d6857
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-15 15:56:50 +0200

    :recycle: Add processing speed selector when BTC

--- src/components/sendV2/form/form.js
+++ src/components/sendV2/form/form.js
@@ -4,7 +4,6 @@ import ConverterV2 from '../../converterV2';
 import { PrimaryButtonV2 } from '../../toolbox/buttons/button';
 import { InputV2, AutoresizeTextarea } from '../../toolbox/inputsV2';
 import Bookmark from '../../bookmarkV2';
-import regex from '../../../utils/regex';
 import SpinnerV2 from '../../spinnerV2/spinnerV2';
 import svg from '../../../utils/svgIcons';
 import Tooltip from '../../toolbox/tooltip/tooltip';
@@ -15,7 +14,9 @@ import Feedback from '../../toolbox/feedback/feedback';
 import CircularProgress from '../../toolbox/circularProgress/circularProgress';
 import styles from './form.css';
 import Piwik from '../../../utils/piwik';
-import { flattenFollowedAccounts } from '../../../utils/followedAccounts';
+import { validateAddress } from '../../../utils/validators';
+import Selector from '../../toolbox/selector/selector';
+import { tokenMap } from '../../../constants/tokens';
 
 class Form extends React.Component {
   // eslint-disable-next-line max-statements
@@ -49,6 +50,7 @@ class Form extends React.Component {
           feedback: props.t('64 bytes left'),
           isActive: false,
         },
+        processingSpeed: {},
       },
     };
 
@@ -63,14 +65,30 @@ class Form extends React.Component {
     this.onSelectedAccount = this.onSelectedAccount.bind(this);
     this.validateAmountAndReference = this.validateAmountAndReference.bind(this);
     this.validateBookmark = this.validateBookmark.bind(this);
-    this.checkIfBoormakedAccount = this.checkIfBoormakedAccount.bind(this);
+    this.checkIfBookmarkedAccount = this.checkIfBookmarkedAccount.bind(this);
     this.setReferenceActive = this.setReferenceActive.bind(this);
+    this.selectProcessingSpeed = this.selectProcessingSpeed.bind(this);
   }
 
   componentDidMount() {
-    this.checkIfBoormakedAccount();
     if (!Object.entries(this.props.prevState).length) this.ifDataFromUrl();
-    if (Object.entries(this.props.prevState).length) this.ifDataFromPrevState();
+    else this.ifDataFromPrevState();
+    this.checkIfBookmarkedAccount();
+  }
+
+  componentDidUpdate() {
+    const { fields } = this.state;
+    const { dynamicFees } = this.props;
+    if (!Object.keys(fields.processingSpeed).length && dynamicFees.Low) {
+      this.setState({
+        fields: {
+          ...fields,
+          processingSpeed: {
+            value: dynamicFees.Low,
+          },
+        },
+      });
+    }
   }
 
   ifDataFromPrevState() {
@@ -108,9 +126,9 @@ class Form extends React.Component {
     }
   }
 
-  checkIfBoormakedAccount() {
-    const { fields } = this.props;
-    const followedAccounts = flattenFollowedAccounts(this.props.followedAccounts);
+  checkIfBookmarkedAccount() {
+    const { fields, token } = this.props;
+    const followedAccounts = this.props.followedAccounts[token];
     const account = followedAccounts.length
       ? followedAccounts.find(acc => acc.address === fields.recipient.address)
       : false;
@@ -132,19 +150,18 @@ class Form extends React.Component {
 
   // eslint-disable-next-line max-statements
   validateBookmark() {
-    const followedAccounts = flattenFollowedAccounts(this.props.followedAccounts);
+    const { token } = this.props;
     let recipient = this.state.fields.recipient;
     let isAccountValid = '';
     let isAddressValid = '';
+    const followedAccounts = this.props.followedAccounts[token];
 
     if (followedAccounts.length && recipient.value !== '') {
       isAccountValid = followedAccounts
         .find(account => (account.title.toLowerCase() === recipient.value.toLowerCase()) ||
           account.address.toLowerCase() === recipient.value.toLowerCase()) || false;
-      isAddressValid = regex.address.test(recipient.value);
-    } else {
-      isAddressValid = recipient.value.match(regex.address);
     }
+    isAddressValid = validateAddress(token, recipient.value) === 0;
 
     // istanbul ignore else
     if (!isAccountValid && !isAddressValid && recipient.value) {
@@ -232,7 +249,11 @@ class Form extends React.Component {
   }
 
   getMaxAmount() {
-    return fromRawLsk(Math.max(0, this.props.account.balance - fees.send));
+    const { token } = this.props;
+    const account = this.props.account.info[token];
+    return token === 'LSK'
+      ? fromRawLsk(Math.max(0, account.balance - fees.send))
+      : fromRawLsk(account.balance);
   }
 
   validateAmountField(value) {
@@ -240,7 +261,19 @@ class Form extends React.Component {
     if (/([^\d.])/g.test(value)) return this.props.t('Provide a correct amount of LSK');
     if ((/(\.)(.*\1){1}/g.test(value) || /\.$/.test(value)) || value === '0') return this.props.t('Invalid amount');
     if (parseFloat(this.getMaxAmount()) < value) return this.props.t('Provided amount is higher than your current balance.');
+
+    if (this.props.token !== tokenMap.LSK.key) this.props.dynamicFeesRetrieved();
     return false;
+  }
+
+  selectProcessingSpeed({ item }) {
+    const { fields } = this.state;
+    this.setState({
+      fields: {
+        ...fields,
+        processingSpeed: item,
+      },
+    });
   }
 
   // eslint-disable-next-line max-statements
@@ -318,6 +351,7 @@ class Form extends React.Component {
   // eslint-disable-next-line complexity
   render() {
     const { fields } = this.state;
+    const { t, token, dynamicFees } = this.props;
     const messageMaxLength = 64;
     const byteCount = encodeURI(fields.reference.value).split(/%..|./).length - 1;
     const isBtnDisabled =
@@ -327,32 +361,40 @@ class Form extends React.Component {
     return (
       <div className={`${styles.wrapper}`}>
         <header className={styles.header}>
-          <h1>{this.props.t('Send LSK')}</h1>
+          <h1>{
+            localStorage.getItem('btc')
+              ? t('Send Tokens')
+              : t('Send LSK')
+          }</h1>
         </header>
-
         <div className={styles.formSection}>
           <span className={`${styles.fieldGroup} recipient`}>
-            <span className={`${styles.fieldLabel}`}>{this.props.t('Recipient')}</span>
+            <span className={`${styles.fieldLabel}`}>{t('Recipient')}</span>
             <Bookmark
               validateBookmark={this.validateBookmark}
               followedAccounts={this.props.followedAccounts}
               onChange={this.onInputChange}
-              placeholder={this.props.t('Insert public address or a name')}
+              placeholder={t('Insert public address or a name')}
               recipient={fields.recipient}
               showSuggestions={fields.recipient.showSuggestions}
               onSelectedAccount={this.onSelectedAccount}
+              token={token}
             />
           </span>
 
           <label className={`${styles.fieldGroup}`}>
-            <span className={`${styles.fieldLabel}`}>{this.props.t('Amount')}</span>
+            <span className={`${styles.fieldLabel}`}>{t('Amount')}</span>
             <span className={`${styles.amountField} amount`}>
               <InputV2
                 autoComplete={'off'}
                 onChange={this.onAmountOrReferenceChange}
                 name='amount'
                 value={fields.amount.value}
-                placeholder={this.props.t('Amount LSK')}
+                placeholder={
+                  localStorage.getItem('btc')
+                    ? t('Insert the amount of transaction')
+                    : t('Amount LSK')
+                  }
                 className={`${styles.input} ${fields.amount.error ? 'error' : ''}`} />
               <ConverterV2
                 className={styles.converter}
@@ -373,80 +415,104 @@ class Form extends React.Component {
               {fields.amount.feedback}
             </Feedback>
 
-            <span className={styles.amountHint}>
-              {this.props.t('+ Transaction fee {{fee}} LSK', { fee: fromRawLsk(fees.send) })}
-              <Tooltip
-                className={'showOnTop'}
-                title={this.props.t('Transaction fee')}
-                footer={
-                  <a href={links.transactionFee}
-                    rel="noopener noreferrer"
-                    target="_blank">
-                      {this.props.t('Read More')}
-                  </a>
-                }
-              >
-                <p className={styles.tooltipText}>
-                {
-                  this.props.t(`Every transaction needs to be confirmed and forged into Lisks blockchain network. 
-                  Such operations require hardware resources and because of that there is a small fee for processing those.`)
-                }
-                </p>
-              </Tooltip>
-            </span>
+            { token !== 'BTC' ? (
+              <span className={styles.amountHint}>
+                {t('+ Transaction fee {{fee}} LSK', { fee: fromRawLsk(fees.send) })}
+                <Tooltip
+                  className={'showOnTop'}
+                  title={t('Transaction fee')}
+                  footer={
+                    <a href={links.transactionFee}
+                      rel="noopener noreferrer"
+                      target="_blank">
+                        {t('Read More')}
+                    </a>
+                  }
+                >
+                  <p className={styles.tooltipText}>
+                  {
+                    t(`Every transaction needs to be confirmed and forged into Lisks blockchain network. 
+                    Such operations require hardware resources and because of that there is a small fee for processing those.`)
+                  }
+                  </p>
+                </Tooltip>
+              </span>
+            ) : null }
           </label>
 
-          <label className={`${styles.fieldGroup} reference`}>
-            <span className={`${styles.fieldLabel}`}>{this.props.t('Message (optional)')}</span>
-            <span className={styles.referenceField}>
-              <AutoresizeTextarea
-                maxLength={100}
-                spellCheck={false}
-                onChange={this.onAmountOrReferenceChange}
-                onFocus={() => this.setReferenceActive(true)}
-                onBlur={() => this.setReferenceActive(false)}
-                name='reference'
-                value={fields.reference.value}
-                placeholder={this.props.t('Write message')}
-                className={`${styles.textarea} ${fields.reference.error ? 'error' : ''} message`} />
-              <CircularProgress max={64} value={byteCount} className={`${styles.byteCounter} ${fields.reference.isActive ? styles.show : styles.hide}`} />
-              <img
-                className={`${styles.status} ${styles.referenceStatus} ${fields.reference.isActive || !fields.reference.value ? styles.hide : styles.show}`}
-                src={ fields.reference.error ? svg.alert_icon : svg.ok_icon}
+          { token !== 'BTC' ? (
+            <label className={`${styles.fieldGroup} reference`}>
+              <span className={`${styles.fieldLabel}`}>{t('Message (optional)')}</span>
+              <span className={styles.referenceField}>
+                <AutoresizeTextarea
+                  maxLength={100}
+                  spellCheck={false}
+                  onChange={this.onAmountOrReferenceChange}
+                  onFocus={() => this.setReferenceActive(true)}
+                  onBlur={() => this.setReferenceActive(false)}
+                  name='reference'
+                  value={fields.reference.value}
+                  placeholder={t('Write message')}
+                  className={`${styles.textarea} ${fields.reference.error ? 'error' : ''} message`} />
+                <CircularProgress max={64} value={byteCount} className={`${styles.byteCounter} ${fields.reference.isActive ? styles.show : styles.hide}`} />
+                <img
+                  className={`${styles.status} ${styles.referenceStatus} ${fields.reference.isActive || !fields.reference.value ? styles.hide : styles.show}`}
+                  src={ fields.reference.error ? svg.alert_icon : svg.ok_icon}
+                />
+              </span>
+              <span className={`${styles.feedback} ${fields.reference.error || messageMaxLength - byteCount < 10 ? 'error' : ''} ${fields.reference.isActive || fields.reference.value ? styles.show : ''}`}>
+                {fields.reference.feedback}
+                <Tooltip
+                  className={'showOnTop'}
+                  title={t('Bytes counter')}
+                  footer={
+                    <a href={links.transactionFee}
+                      rel="noopener noreferrer"
+                      target="_blank">
+                        {t('Read More')}
+                    </a>
+                  }
+                >
+                  <p className={styles.tooltipText}>
+                  {
+                    t(`LISK Hub counts your message by bytes so keep in mind 
+                    that the length on your message may vary in different languages. 
+                    Different characters may consume different amount of bytes space.`)
+                  }
+                  </p>
+                </Tooltip>
+              </span>
+            </label>
+          ) : !!Object.keys(dynamicFees).length && (
+            <div className={`${styles.fieldGroup}`}>
+              <span className={`${styles.fieldLabel}`}>
+                {t('Processing Speed')}
+                <Tooltip>
+                  <p className={styles.tooltipText}>{
+                    t('Bitcoin transactions are made with some delay that depends on two parameters: the fee and the bitcoin network’s congestion. The higher the fee, the higher the processing speed.')
+                  }</p>
+                </Tooltip>
+              </span>
+              <Selector
+                onSelectorChange={this.selectProcessingSpeed}
+                name={'speedSelector'}
+                options={[
+                  { title: t('Low'), value: dynamicFees.Low },
+                  { title: t('High'), value: dynamicFees.High },
+                ]}
               />
-            </span>
-            <span className={`${styles.feedback} ${fields.reference.error || messageMaxLength - byteCount < 10 ? 'error' : ''} ${fields.reference.isActive || fields.reference.value ? styles.show : ''}`}>
-              {fields.reference.feedback}
-              <Tooltip
-                className={'showOnTop'}
-                title={this.props.t('Bytes counter')}
-                footer={
-                  <a href={links.transactionFee}
-                    rel="noopener noreferrer"
-                    target="_blank">
-                      {this.props.t('Read More')}
-                  </a>
-                }
-              >
-                <p className={styles.tooltipText}>
-                {
-                  this.props.t(`LISK Hub counts your message by bytes so keep in mind 
-                  that the length on your message may vary in different languages. 
-                  Different characters may consume different amount of bytes space.`)
-                }
-                </p>
-              </Tooltip>
-            </span>
-          </label>
+              <span>{t('Transaction fee:')} {fields.processingSpeed.value}</span>
+            </div>
+          )}
         </div>
 
-        <footer>
+        <footer className={`${styles.footer}`}>
           <PrimaryButtonV2
             className={`${styles.confirmButton} btn-submit send-next-button`}
             disabled={isBtnDisabled}
             onClick={this.onGoNext}
           >
-            {this.props.t('Go to Confirmation')}
+            {t('Go to Confirmation')}
           </PrimaryButtonV2>
         </footer>
       </div>
