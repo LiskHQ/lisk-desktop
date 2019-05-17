543eca92574896cc75048e5a3d184d6fd55c2bd9
Vit Stanislav <vit@lightcurve.io>
2019-05-17 15:06:44 +0200

    :seedling: Implement Register as a delegate link

--- src/components/votingV2/votingHeader.js
+++ src/components/votingV2/votingHeader.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import { SecondaryButtonV2, PrimaryButtonV2 } from '../toolbox/buttons/button';
+import routes from './../../constants/routes';
 
 class VotingHeader extends React.Component {
   render() {
@@ -14,9 +16,11 @@ class VotingHeader extends React.Component {
               <div>{t('My votes after confirmation')}</div>
             </span>
             <span>
-              <SecondaryButtonV2>
-                {t('Register as a Delegate')}
-              </SecondaryButtonV2>
+              <Link to={routes.registerDelegate.path} >
+                <SecondaryButtonV2 className='register-delegate'>
+                  {t('Register as a Delegate')}
+                </SecondaryButtonV2>
+              </Link>
               <PrimaryButtonV2>
                 {t('Start voting')}
               </PrimaryButtonV2>
