01143c9979e985fa22b9797ff1f673744ede4a37
Vit Stanislav <vit@lightcurve.io>
2019-05-17 15:52:11 +0200

    :seedling: Implement voting header statistics

--- src/components/votingV2/votingHeader.js
+++ src/components/votingV2/votingHeader.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import { SecondaryButtonV2, PrimaryButtonV2 } from '../toolbox/buttons/button';
+import Tooltip from '../toolbox/tooltip/tooltip';
 import routes from './../../constants/routes';
+import votingConst from '../../constants/voting';
+import {
+  getTotalVotesCount,
+  getVoteList,
+  getUnvoteList,
+} from './../../utils/voting';
 
 class VotingHeader extends React.Component {
   render() {
@@ -12,13 +19,47 @@ class VotingHeader extends React.Component {
       toggleVotingMode,
       votingModeEnabled,
     } = this.props;
+    const voteList = getVoteList(votes);
+    const unvoteList = getUnvoteList(votes);
+    const {
+      maxCountOfVotesInOneTurn,
+      maxCountOfVotes,
+      fee,
+    } = votingConst;
+    const totalActions = Math.ceil((
+      voteList.length + unvoteList.length
+    ) / maxCountOfVotesInOneTurn);
     return (
       <div className={`${grid.row} ${grid['between-xs']}`}>
         <div className={`${grid['col-sm-12']} ${grid['col-md-12']} `}>
           <div className={`${grid.row} ${grid['between-xs']}`}>
             <span>
-              <h2>{Object.keys(votes).length}/101</h2>
-              <div>{t('My votes after confirmation')}</div>
+              <span>
+                <h2>{getTotalVotesCount(votes)}/{maxCountOfVotes}</h2>
+                <div>{t('My votes after confirmation')}</div>
+              </span>
+              { votingModeEnabled ?
+              <span>
+               <h3>{voteList.length}</h3>
+               <span>{t('Added votes')}</span>
+              </span> :
+              null }
+              { unvoteList.length ?
+              <span>
+               <h3>{unvoteList.length}</h3>
+               <span>{t('Remove votes')}</span>
+              </span> :
+              null }
+              { votingModeEnabled ?
+              <span>
+               <h3>{totalActions}
+                 <Tooltip>
+                  <p>{t('Each time you add or remove a vote it is counted as an action. There\'s {{fee}} LSK fee per every 33 actions.', { fee })}</p>
+                 </Tooltip>
+               </h3>
+               <span>{t('Total actions (Total fee:')}<b>{fee * totalActions} LSK</b>)</span>
+              </span> :
+              null }
             </span>
             { votingModeEnabled ?
             <span>
