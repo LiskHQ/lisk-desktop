8c279ddbae215f7f62212f6f7a4278d9827d07e7
Vit Stanislav <vit@lightcurve.io>
2019-05-21 16:34:03 +0200

    :seedling: Implement layout of transactionSummary

--- src/components/transactionSummary/index.js
+++ src/components/transactionSummary/index.js
@@ -5,25 +5,27 @@ import styles from './transactionSummary.css';
 const TransactionSummary = ({
   title, children, confirmButton, cancelButton,
 }) => (
-  <div className={styles.wrapper}>
-    <header className='summary-header'>
-      <h1>{title}</h1>
-    </header>
-    <div className={styles.content}>
-     {children}
+  <div className={styles.container}>
+    <div className={styles.wrapper}>
+      <header className='summary-header'>
+        <h1>{title}</h1>
+      </header>
+      <div className={styles.content}>
+       {children}
+      </div>
+      <footer className='summary-footer'>
+        <PrimaryButtonV2
+          className={`${styles.confirmBtn} confirm-button`}
+          onClick={confirmButton.onClick}>
+          {confirmButton.label}
+        </PrimaryButtonV2>
+        <TertiaryButtonV2
+          className={`${styles.editBtn} cancel-button`}
+          onClick={cancelButton.onClick}>
+          {cancelButton.label}
+        </TertiaryButtonV2>
+      </footer>
     </div>
-    <footer className='summary-footer'>
-      <PrimaryButtonV2
-        className={`${styles.confirmBtn} confirm-button`}
-        onClick={confirmButton.onClick}>
-        {confirmButton.label}
-      </PrimaryButtonV2>
-      <TertiaryButtonV2
-        className={`${styles.editBtn} cancel-button`}
-        onClick={cancelButton.onClick}>
-        {cancelButton.label}
-      </TertiaryButtonV2>
-    </footer>
   </div>
 );
 
