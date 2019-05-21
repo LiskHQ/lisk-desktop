2c900a3a73a263992a2cb1ba46441effbfcfee04
Vit Stanislav <vit@lightcurve.io>
2019-05-21 16:52:11 +0200

    :seedling: Implement vote lists in votingSummary

--- src/components/votingSummary/votingSummary.js
+++ src/components/votingSummary/votingSummary.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import TransactionSummary from '../transactionSummary';
+import votingConst from '../../constants/voting';
 import {
+  getTotalVotesCount,
   getVoteList,
   getUnvoteList,
 } from '../../utils/voting';
 import routes from '../../constants/routes';
 
+import styles from './votingSummary.css';
+
 const VotingSummary = ({ t, votes, history }) => {
+  const {
+    maxCountOfVotes,
+    fee,
+  } = votingConst;
   const voteList = getVoteList(votes);
+  console.log(voteList);
   const unvoteList = getUnvoteList(votes);
   return (
     <TransactionSummary
@@ -24,22 +33,40 @@ const VotingSummary = ({ t, votes, history }) => {
       title={t('Voting summary')} >
       <section>
         <label>{t('Votes after confirmation')}</label>
-        <label> 7/101 </label>
+        <label>{getTotalVotesCount(votes)}/{maxCountOfVotes}</label>
       </section>
       <section>
         <label>{t('Transaction fee')}</label>
-        <label> 1 LSK </label>
+        <label> {fee} LSK </label>
       </section>
       {voteList.length > 0 ?
         <section>
           <label>{t('Added votes')} ({voteList.length})</label>
-          <label> TODO </label>
+          <label>
+            <div className={styles.votesContainer} >
+              {voteList.map(vote => (
+               <span key={vote} className={`${styles.voteTag} vote`}>
+                <span className={styles.rank}>#{votes[vote].rank}</span>
+                <span className={styles.username}>{vote}</span>
+               </span>
+              ))}
+            </div>
+          </label>
         </section> :
         null }
       {unvoteList.length > 0 ?
         <section>
           <label>{t('Removed votes')} ({unvoteList.length})</label>
-          <label> TODO </label>
+          <label>
+            <div className={styles.votesContainer} >
+              {unvoteList.map(vote => (
+               <span key={vote} className={`${styles.voteTag} vote`}>
+                <span className={styles.rank}>#{votes[vote].rank}</span>
+                <span className={styles.username}>{vote}</span>
+               </span>
+              ))}
+            </div>
+          </label>
         </section> :
         null }
     </TransactionSummary>
