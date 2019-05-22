662b2fc36892613b0a02caf801ec54d463dfcb5a
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-22 10:33:45 +0200

    :recycle: Improve illustrations loading

---
+++ src/utils/illustrations.js
@@ -0,0 +1,18 @@
+import welcomeLiskDelegates from '../assets/images/illustrations/illustration-welcome-to-lisk-delegates-dark.svg';
+import yourVoiceMatters from '../assets/images/illustrations/illustration-your-voice-matters-dark.svg';
+import getRewarded from '../assets/images/illustrations/illustration-get-rewarded-dark.svg';
+import expandYourKnowledge from '../assets/images/illustrations/illustration-expand-your-knowledge-dark.svg';
+
+export const delegateOnboarding = {
+  welcomeLiskDelegates,
+  yourVoiceMatters,
+  getRewarded,
+  expandYourKnowledge,
+};
+
+export default {
+  welcomeLiskDelegates,
+  yourVoiceMatters,
+  getRewarded,
+  expandYourKnowledge,
+};
