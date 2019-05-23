a7c6463c76a8aa782d87ce268c334910330546c5
Vit Stanislav <vit@lightcurve.io>
2019-05-23 15:36:26 +0200

    :seedling: Implement layout of voting submitted page

--- src/components/toolbox/illustration/index.js
+++ src/components/toolbox/illustration/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ledgerNanoLight from '../../../assets/images/illustrations/illustration-ledger-nano-light.svg';
 import trezorLight from '../../../assets/images/illustrations/illustration-trezor-confirm-light.svg';
+import transactionSuccess from '../../../assets/images/illustrations/transaction_success.svg';
+import transactionError from '../../../assets/images/illustrations/transaction_error.svg';
 
 const illustrations = {
   ledgerNanoLight,
   trezorLight,
+  transactionSuccess,
+  transactionError,
 };
 
 const Illustration = ({ name, className }) => (
--- src/components/votingV2/votingResult.js
+++ src/components/votingV2/votingResult.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { PrimaryButtonV2, TertiaryButtonV2 } from '../toolbox/buttons/button';
+import Illustration from '../toolbox/illustration';
 
-const VotingResult = () => (
+const VotingResult = ({
+  success, title, message, primaryButon, t,
+}) => (
   <div>
-    <h1>Voting Result - TODO</h1>
+    <Illustration name={success ? 'transactionSuccess' : 'transactionError'} />
+    <h1>{title}</h1>
+    <p>{message}</p>
+    <PrimaryButtonV2 onClick={primaryButon.onClick}>
+      {primaryButon.title}
+    </PrimaryButtonV2>
+    {!success ?
+      <React.Fragment>
+        <p>{t('Does the problem still persist?')}</p>
+        <TertiaryButtonV2>
+          {t('Report the error via E-Mail')}
+        </TertiaryButtonV2>
+      </React.Fragment> :
+    null}
   </div>
 );
 
