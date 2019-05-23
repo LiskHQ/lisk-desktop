17889d2eae7e65eb6d387e2afe3eece0f78165b7
Vit Stanislav <vit@lightcurve.io>
2019-05-23 15:58:29 +0200

    :seedling: Implement error mail link in votingResult

--- src/components/votingV2/votingResult.js
+++ src/components/votingV2/votingResult.js
@@ -4,8 +4,14 @@ import Illustration from '../toolbox/illustration';
 
 import styles from './votingResult.css';
 
+const getErrorReportMailto = (error) => {
+  const recipient = 'hubdev@lisk.io';
+  const subject = `User Reported Error - Lisk Hub - ${VERSION}`; // eslint-disable-line no-undef
+  return `mailto:${recipient}?&subject=${subject}&body=${error}`;
+};
+
 const VotingResult = ({
-  success, title, message, primaryButon, t,
+  success, title, message, primaryButon, t, error,
 }) => (
   <div className={styles.wrapper}>
     <Illustration name={success ? 'transactionSuccess' : 'transactionError'} />
@@ -17,9 +23,14 @@ const VotingResult = ({
     {!success ?
       <React.Fragment>
         <p>{t('Does the problem still persist?')}</p>
-        <TertiaryButtonV2>
-          {t('Report the error via E-Mail')}
-        </TertiaryButtonV2>
+        <a
+          href={getErrorReportMailto(error)}
+          target='_top'
+          rel='noopener noreferrer'>
+          <TertiaryButtonV2>
+            {t('Report the error via E-Mail')}
+          </TertiaryButtonV2>
+        </a>
       </React.Fragment> :
     null}
   </div>
