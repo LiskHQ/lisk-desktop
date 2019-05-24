ddb419fcc47b0c9b8a3ef95e1af1db00f803ce48
andreiklimenok <andrei.klimenok@lightcurve.io>
2019-05-24 18:06:45 +0200

    implement 404 page

--- src/components/notFound/index.js
+++ src/components/notFound/index.js
@@ -1,17 +1,25 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import { translate } from 'react-i18next';
-import Box from '../box';
 import styles from './notFound.css';
-import cubeImage from '../../assets/images/dark-blue-cube.svg';
+import { PrimaryButtonV2 } from '../toolbox/buttons/button';
+import Illustration from '../toolbox/illustration';
+import routes from '../../constants/routes';
 
-const NotFound = ({ t }) => (<section>
-  <Box>
-    <div className={styles.emptyTransactions}>
-      <img src={cubeImage} />
-      <h2 className='empty-message'>{t('Page not found.')}</h2>
-      <p>{t('Try using menu for navigation.')}</p>
+const NotFound = ({ t }) => (
+  <section className={styles.notFoundPage}>
+    <div className={styles.errorMessageContainer}>
+      <Illustration name={ 'pageNotFound' }/>
+      <h2 className='empty-message'>{t('Whoops, that page is gone.')}</h2>
+      <p>{t('Sorry, we couldn’t find the page you were looking for. ' +
+        'We suggest that you return to the main dashboard.')}</p>
+      <Link className={'go-to-dashboard-button'} to={routes.dashboard.path}>
+        <PrimaryButtonV2 className={styles.goToDashboardButton}>
+          {t('Go to Dashboard')}
+        </PrimaryButtonV2>
+      </Link>
+
     </div>
-  </Box>
 </section>);
 
 export default translate()(NotFound);
