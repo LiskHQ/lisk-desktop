daa47ac4a110a9b224dfde927154975230689c5b
Vit Stanislav <vit@lightcurve.io>
2019-05-27 09:26:41 +0200

    :seedling: Create tooltip demo

--- src/components/toolbox/demo.js
+++ src/components/toolbox/demo.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BoxV2Demo from '../boxV2/demo';
 import CheckBoxDemo from './checkBox/demo';
 import IllustrationDemo from './illustration/demo';
+import TooltipDemo from './tooltip/demo';
 
 const ToolboxDemo = () => (
   <React.Fragment>
     <BoxV2Demo/>
     <CheckBoxDemo/>
     <IllustrationDemo/>
+    <TooltipDemo/>
   </React.Fragment>
 );
 
---
+++ src/components/toolbox/tooltip/demo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Tooltip from './tooltip';
+import DemoRenderer from '../demoRenderer';
+
+const IllustrationDemo = () => (
+  <React.Fragment>
+    <h2>Tooltip</h2>
+    <DemoRenderer>
+      <Tooltip
+        title='Tooltip title'
+        className={'showOnTop'}
+        footer={<a>Learn more</a>} >
+        <p>
+          Tooltip content
+        </p>
+      </Tooltip>
+    </DemoRenderer>
+    <DemoRenderer>
+      <Tooltip
+        alwaysShow={true} >
+        <p>
+          Tooltip content
+        </p>
+      </Tooltip>
+    </DemoRenderer>
+    <DemoRenderer>
+      <Tooltip
+        title='Tooltip title'
+        className={'showOnBottom'}
+        content={<span>Something else than the icon</span>}
+        footer={<a>Learn more</a>} >
+        <p>
+          Tooltip content
+        </p>
+      </Tooltip>
+    </DemoRenderer>
+  </React.Fragment>
+);
+
+export default IllustrationDemo;
