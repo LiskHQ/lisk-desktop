4123f14f40134e64a1494fb800c670e9e516a853
Vit Stanislav <vit@lightcurve.io>
2019-05-27 10:44:59 +0200

    :seedling: Create icon component and demo for it

--- src/components/toolbox/demo.js
+++ src/components/toolbox/demo.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BoxV2Demo from '../boxV2/demo';
 import CheckBoxDemo from './checkBox/demo';
 import IllustrationDemo from './illustration/demo';
+import IconDemo from './icon/demo';
 import TooltipDemo from './tooltip/demo';
 
 const ToolboxDemo = () => (
   <React.Fragment>
     <BoxV2Demo/>
     <CheckBoxDemo/>
+    <IconDemo/>
     <IllustrationDemo/>
     <TooltipDemo/>
   </React.Fragment>
