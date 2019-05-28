6a0bbf385b204226a8b35a88d5f6c47d7baab14c
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-28 18:10:15 +0200

    :recycle: Refactor checkbox component

--- src/components/toolbox/checkBox/index.test.js
+++ src/components/toolbox/checkBox/index.test.js
@@ -6,22 +6,23 @@ describe('CheckBox', () => {
   const props = {
     added: true,
     removed: false,
-    onChange: () => {},
+    onChange: jest.fn(),
   };
 
   it('should render checkmark icon if props.checked is true', () => {
     const wrapper = mount(<CheckBox {...props} checked={true} />);
-    expect(wrapper.find('img').props()).toHaveProperty('src', 'test-file-stub');
+    expect(wrapper).toHaveClassName('checked');
   });
 
   it('should render with "accent" class if props.accent is true', () => {
     const wrapper = mount(<CheckBox {...props} accent={true} />);
-    expect(wrapper.find('.checked').hasClass('accent')).toEqual(true);
+    expect(wrapper).toHaveClassName('unchecked');
+    expect(wrapper).toContainExactlyOneMatchingElement('.accent');
   });
 
   it('should render with "removed" class if props.removed is true', () => {
     const wrapper = mount(<CheckBox {...props} removed={true} />);
-    expect(wrapper.find('span.unchecked').hasClass('removed')).toEqual(true);
+    expect(wrapper).toContainExactlyOneMatchingElement('.removed');
   });
 });
 
