671088be7eaecae33a224a0aa0b7309e27d3c5f5
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-31 11:16:05 +0200

    :recycle: Simplify signInTooltipWrapper to have display inline-block by default

--- src/components/signInTooltipWrapper/signInTooltipWrapper.js
+++ src/components/signInTooltipWrapper/signInTooltipWrapper.js
@@ -2,32 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from './../../constants/routes';
 import Tooltip from '../toolbox/tooltip/tooltip';
+import styles from './signInTooltipWrapper.css';
 
 const SignInTooltipWrapper = ({
   children, account, t, history,
 }) => {
   const { pathname, search } = history.location;
-  return <React.Fragment>
-    { account && account.address ?
-      children :
-      <span>
-        <Tooltip
-          className='showOnBottom'
-          content={
-            <span style={{ pointerEvents: 'none' }}>
-              {children}
-            </span>}
-          title={t('Please sign in')}
-          footer={
-            <Link to={`${routes.loginV2.path}?referrer=${pathname}${encodeURIComponent(search)}`}>
-              {t('Sign in')}
-            </Link>
-          }
-          >
-          <p>{t('In order to use this Lisk Hub feature you need to sign in to your Lisk account.')}</p>
-        </Tooltip>
-      </span>}
-    </React.Fragment>;
+  return account && account.address
+    ? children
+    : (
+      <Tooltip
+        className={`${styles.wrapper} showOnBottom`}
+        content={React.cloneElement(children, { className: `${children.props.className} ${styles.child}` })}
+        title={t('Please sign in')}
+        footer={
+          <Link to={`${routes.loginV2.path}?referrer=${pathname}${encodeURIComponent(search)}`}>
+            {t('Sign in')}
+          </Link>
+        }
+        >
+        <p>{t('In order to use this Lisk Hub feature you need to sign in to your Lisk account.')}</p>
+      </Tooltip>
+    );
 };
 
 export default SignInTooltipWrapper;
