6c8609ff2cc42cb28d9f357c8ec27edf35647650
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-05-31 09:29:46 +0200

    :white_check_mark: Add unit test for outsideClickHandler component

---
+++ src/components/toolbox/outsideClickHandler/index.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import OutsideClickHandler from './index';
+
+describe('Ouside Click Handler Compnent', () => {
+  const props = {
+    onOutsideClick: jest.fn(),
+  };
+  let eventsMap;
+
+  beforeEach(() => {
+    eventsMap = {};
+    document.addEventListener = jest.fn((event, cb) => {
+      eventsMap[event] = cb;
+    });
+    document.removeEventListener = jest.fn();
+  });
+
+  it('Should not add Event Listener and add afterwards if props disabled is set to false', () => {
+    const wrapper = mount(<OutsideClickHandler {...props} disabled={true}>
+      <div className={'child'}></div>
+    </OutsideClickHandler>);
+    expect(wrapper).toContainExactlyOneMatchingElement('.child');
+    expect(document.addEventListener).not.toBeCalled();
+    wrapper.setProps({ disabled: false });
+    expect(document.addEventListener).toBeCalled();
+    wrapper.unmount();
+    expect(document.removeEventListener).toBeCalled();
+  });
+
+  it('Should add Event Listener and remove after prop disabled is changed to true', () => {
+    const wrapper = mount(<OutsideClickHandler {...props} disabled={false}>
+      <span></span>
+    </OutsideClickHandler>);
+    expect(document.addEventListener).toBeCalled();
+    wrapper.setProps({ disabled: true });
+    expect(document.removeEventListener).toBeCalled();
+  });
+
+  it('Should call click outside if target is outside wrapper', () => {
+    const wrapper = mount(<OutsideClickHandler {...props} disabled={false}>
+      <div></div>
+    </OutsideClickHandler>);
+    expect(document.addEventListener).toBeCalled();
+    eventsMap.click({ target: document.createElement('span') });
+    expect(props.onOutsideClick).toBeCalledTimes(1);
+    wrapper.setProps({ disabled: true });
+    eventsMap.click({ target: document.createElement('span') });
+    expect(props.onOutsideClick).not.toBeCalledTimes(2);
+  });
+});
