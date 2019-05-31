3b0a30f07e5d1d7d853d3c86ee01559c4b3cc6ae
Vit Stanislav <vit@lightcurve.io>
2019-05-31 15:01:00 +0200

    :seedling: Implement calendar component demo

--- src/components/toolbox/calendar/demo.js
+++ src/components/toolbox/calendar/demo.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import moment from 'moment';
+import DemoRenderer from '../demoRenderer';
+import Calendar from './calendar';
+import { firstBlockTime } from '../../../constants/datetime';
 
+/* eslint-disable-next-line no-console */
+const onDateSelected = console.log;
+
+const dateFormat = ('DD.MM.YY');
 const CalendarDemo = () => (
   <div>
     <h2>Calendar</h2>
     <h3> TODO </h3>
+    <DemoRenderer>
+      <Calendar
+        onDateSelected={onDateSelected}
+        dateFormat={dateFormat}
+        minDate={moment(firstBlockTime).format(dateFormat)}
+        maxDate={moment().format(dateFormat)}
+        date={moment().format(dateFormat)} />
+    </DemoRenderer>
   </div>
 );
 
