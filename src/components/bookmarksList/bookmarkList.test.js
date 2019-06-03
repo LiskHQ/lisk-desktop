f0fdf812eb492c88bca13356af04bc99a59a4366
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-06-03 15:12:17 +0200

    :recycle: remove tabs from bookmarks component

--- src/components/bookmarksList/bookmarkList.test.js
+++ src/components/bookmarksList/bookmarkList.test.js
@@ -15,10 +15,6 @@ describe('BookmarksList', () => {
     },
     token: {
       active: tokenMap.LSK.key,
-      list: {
-        LSK: true,
-        BTC: true,
-      },
     },
     bookmarks: {
       LSK: [{
@@ -60,7 +56,6 @@ describe('BookmarksList', () => {
 
   it('should render properly', () => {
     expect(wrapper).toContainMatchingElement('.bookmarks-list');
-    expect(wrapper).toContainMatchingElement('Tabs');
     expect(wrapper).toContainMatchingElement('.bookmark-list-container');
     expect(wrapper).not.toContainMatchingElement('EmptyState');
   });
@@ -71,9 +66,23 @@ describe('BookmarksList', () => {
   });
 
   it('should render BTC bookmakrs ONLY', () => {
-    wrapper.find('.tab-bitcoin').at(0).simulate('click');
+    wrapper.setProps({
+      children: React.cloneElement(wrapper.props().children, {
+        token: {
+          active: 'BTC',
+        },
+      }),
+    });
+    wrapper.update();
     expect(wrapper).toContainMatchingElement('.bookmark-list-container');
     expect(wrapper).toContainMatchingElements(1, '.bookmark-list-row');
+  });
+
+  it('should redirect to a accounts page for selected bookmark', () => {
+    expect(wrapper).toContainMatchingElement('.bookmark-list-container');
+    expect(wrapper).toContainMatchingElements(5, '.bookmark-list-row');
+    wrapper.find('.bookmark-list-row').at(1).simulate('click');
+    expect(props.history.push).toBeCalled();
   });
 
   it('should render EmptyState', () => {
@@ -89,25 +98,4 @@ describe('BookmarksList', () => {
     expect(wrapper).not.toContainMatchingElement('.bookmark-list-row');
     expect(wrapper).toContainMatchingElement('EmptyState');
   });
-
-  // it('render properly when bookmard is selected', () => {
-  //   props.recipient.address = '12345L';
-  //   props.recipient.title = 'John Cena';
-  //   props.recipient.balance = '10';
-  //   wrapper = mount(<Bookmark {...props} />, options);
-  //   expect(wrapper).toContainMatchingElement('AccountVisual');
-  // });
-
-  // it('should select an account from the available list', () => {
-  //   props.showSuggestions = true;
-  //   props.recipient.value = 'L';
-  //   wrapper = mount(<Bookmark {...props} />, options);
-  //   expect(wrapper).toContainMatchingElement('.bookmark-list');
-  //   expect(wrapper).toContainMatchingElements(3, 'li');
-  //   wrapper.find('InputV2.input').simulate('keyDown', { keyCode: keyCodes.arrowDown });
-  //   wrapper.find('InputV2.input').simulate('keyDown', { keyCode: keyCodes.arrowUp });
-  //   wrapper.find('InputV2.input').simulate('keyDown', { keyCode: keyCodes.enter });
-  //   wrapper.find('.bookmark-list li').at(0).simulate('click');
-  //   expect(props.onSelectedAccount).toBeCalled();
-  // });
 });
--- src/components/bookmarksList/bookmarksList.js
+++ src/components/bookmarksList/bookmarksList.js
@@ -4,90 +4,56 @@ import { Link } from 'react-router-dom';
 import AccountVisual from '../accountVisual';
 import EmptyState from '../emptyStateV2';
 import Box from '../boxV2';
-import Tabs from '../toolbox/tabs';
 import { tokenMap } from '../../constants/tokens';
 import regex from '../../utils/regex';
 import svg from '../../utils/svgIcons';
 import { SecondaryButtonV2 } from '../toolbox/buttons/button';
+import routes from '../../constants/routes';
 import styles from './bookmarksList.css';
 
 class BookmarksList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeToken: props.token.active,
-    };
-
-    this.onTabChange = this.onTabChange.bind(this);
-  }
-
-  onTabChange({ value }) {
-    this.setState({ activeToken: value });
+  onBookmarkSelected(address) {
+    const { history } = this.props;
+    history.push(`${routes.explorer.path}/accounts/${address}`);
   }
 
   getBookmarkListBasedOnSelectedToken() {
-    const { bookmarks } = this.props;
-    const { activeToken } = this.state;
+    const { bookmarks, token } = this.props;
 
     const actualBookmarks = { ...bookmarks };
-    return actualBookmarks[activeToken].length > 5
-      ? actualBookmarks[activeToken].slice(0, 5)
-      : bookmarks[activeToken];
+    return actualBookmarks[token.active].length > 5
+      ? actualBookmarks[token.active].slice(0, 5)
+      : bookmarks[token.active];
   }
 
   displayAddressBasedOnSelectedToken(address) {
-    const { activeToken } = this.state;
-    return activeToken === tokenMap.LSK.key
+    const { token } = this.props;
+
+    return token.active === tokenMap.LSK.key
       ? address
       : address.replace(regex.btcAddressTrunk, '$1...$3');
   }
 
-  isMultipleCoinActive() {
-    const { token } = this.props;
-    return Object.values(token.list).filter(coin => coin === true).length > 1;
-  }
-
   render() {
-    const { t } = this.props;
-    const { activeToken } = this.state;
+    const { t, token } = this.props;
 
     const selectedBookmarks = this.getBookmarkListBasedOnSelectedToken();
-    const tabs = [
-      {
-        name: t('Lisk'),
-        value: 'LSK',
-        className: 'tab-lisk',
-      },
-      {
-        name: t('Bitcoin'),
-        value: 'BTC',
-        className: 'tab-bitcoin',
-      },
-    ];
 
     return (
       <Box className={` ${styles.box} bookmarks-list`}>
         <header>
           <h2>Bookmarks</h2>
-          {
-            this.isMultipleCoinActive()
-            ? <Tabs
-                active={activeToken}
-                onClick={this.onTabChange}
-                className={styles.tab}
-                tabs={tabs}
-              />
-            : null
-          }
         </header>
         <div className={`${styles.bookmarkList} bookmark-list-container`}>
         {
           selectedBookmarks.length
           ? selectedBookmarks.map(bookmark =>
-            <div key={bookmark.address} className={`${styles.row} bookmark-list-row`}>
+            <div
+              key={bookmark.address}
+              className={`${styles.row} bookmark-list-row`}
+              onClick={() => this.onBookmarkSelected(bookmark.address)}>
               {
-                activeToken === tokenMap.LSK.key
+                token.active === tokenMap.LSK.key
                 ? <AccountVisual address={bookmark.address} size={40}/>
                 : null
               }
