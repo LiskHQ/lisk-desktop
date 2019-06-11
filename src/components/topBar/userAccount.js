f26f6428aef9f24f5711b0c15557b3bb538fabaa
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-06-11 18:12:12 +0200

    :recycle: Adjust userAccount component

--- src/components/topBar/userAccount.js
+++ src/components/topBar/userAccount.js
@@ -1,75 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AccountVisual from '../accountVisual';
-import CopyToClipboard from '../copyToClipboard';
 import LiskAmount from '../liskAmount';
 import DropdownV2 from '../toolbox/dropdownV2/dropdownV2';
 import { dropdownLinks } from './constants';
 import styles from './userAccount.css';
 import OutsideClickHandler from '../toolbox/outsideClickHandler';
+import Icon from '../toolbox/icon';
 
 const UserAccount = (props) => {
   const dropdownOptions = dropdownLinks(props.t);
 
   return (
-    <div className={`${styles.wrapper} user-account`}>
-      <div className={styles.information}>
-        <CopyToClipboard
-          value={props.account.address}
-          className={`${styles.copyAddress} account-information-address`}
-          copyClassName={styles.copy}
-        />
-        <div className={`${styles.balance} balance`}>
-          {Object.keys(props.account.info).map(key => (
-            <React.Fragment key={key}>
-              &nbsp; &nbsp;
-              <LiskAmount val={props.account.info[key].balance}/>
-              &nbsp;
-              <small>{key}</small>
-            </React.Fragment>
-          ))}
+    <OutsideClickHandler
+      className={`${styles.wrapper} user-account`}
+      onClick={() => props.onDropdownToggle('avatar')}
+      onOutsideClick={() => props.onDropdownToggle('avatar')}
+      disabled={!props.isDropdownEnable}
+    >
+      <div className={styles.accountInfo} ref={props.setDropdownRef}>
+        <Icon name='btcIcon' />
+        <div>
+          <p>{props.t('{{token}} Wallet', { token: 'Bitcoin' })}</p>
+          <span>
+            <LiskAmount val={props.account.info.LSK.balance}/> BTC
+          </span>
         </div>
       </div>
-      <OutsideClickHandler
-        className={`${styles.avatar} user-avatar`}
-        onClick={() => props.onDropdownToggle('avatar')}
-        onOutsideClick={() => props.onDropdownToggle('avatar')}
-        disabled={!props.isDropdownEnable}
+      <DropdownV2
+        showArrow={false}
+        className={styles.dropdown}
+        showDropdown={props.isDropdownEnable}
       >
-        <span
-          className={styles.onAvatar}
-          data-name={'avatar'}
-          ref={props.setDropdownRef}
+        <Link
+          id={dropdownOptions.settings.id}
+          to={dropdownOptions.settings.path}
+          className={styles.dropdownOption}
         >
-          <AccountVisual
-            address={props.account.address || ''}
-            size={40}
-          />
+          <img src={dropdownOptions.settings.icon} className={styles.defaultIcon} />
+          <img src={dropdownOptions.settings.icon_active} className={styles.activeIcon} />
+          <span>{dropdownOptions.settings.label}</span>
+        </Link>
+
+        <span
+          className={`${styles.dropdownOption} logout`}
+          onClick={props.onLogout}
+        >
+          <img src={dropdownOptions.logout.icon} className={styles.defaultIcon}/>
+          <img src={dropdownOptions.logout.icon_active} className={styles.activeIcon}/>
+          <span>{dropdownOptions.logout.label}</span>
         </span>
-
-        <DropdownV2 showArrow={false}
-          className={styles.dropdown} showDropdown={props.isDropdownEnable}>
-          <Link
-            id={dropdownOptions.settings.id}
-            to={dropdownOptions.settings.path}
-            className={styles.dropdownOption}
-          >
-            <img src={dropdownOptions.settings.icon} className={styles.defaultIcon} />
-            <img src={dropdownOptions.settings.icon_active} className={styles.activeIcon} />
-            <span>{dropdownOptions.settings.label}</span>
-          </Link>
-
-          <span
-            className={`${styles.dropdownOption} logout`}
-            onClick={() => props.onLogout()}
-            >
-            <img src={dropdownOptions.logout.icon} className={styles.defaultIcon}/>
-            <img src={dropdownOptions.logout.icon_active} className={styles.activeIcon}/>
-            <span>{dropdownOptions.logout.label}</span>
-          </span>
-        </DropdownV2>
-      </OutsideClickHandler>
-    </div>
+      </DropdownV2>
+    </OutsideClickHandler>
   );
 };
 
