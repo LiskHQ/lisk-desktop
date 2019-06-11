1b27aa70f49ef60c76911bb51f89b58b2a848ec2
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-06-12 00:26:49 +0200

    :recycle: Update community feed component to use scroll

--- src/components/newsFeedV2/newsFeed.js
+++ src/components/newsFeedV2/newsFeed.js
@@ -3,25 +3,13 @@ import styles from './newsFeed.css';
 import News from './news';
 import Box from '../boxV2';
 import logo from '../../assets/images/Lisk-Logo.svg';
-import ShowMore from '../showMore';
 
 class NewsFeed extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMore: false,
-    };
-
-    props.getNewsFeed();
-  }
-
-  onShowMore() {
-    this.setState({ showMore: !this.state.showMore });
+  componentDidMount() {
+    this.props.getNewsFeed();
   }
 
   render() {
-    const onShowMore = this.state.showMore ? styles.showMore : '';
-
     const filteredNewsFeed =
       this.props.newsFeed.filter(feed => this.props.channels[feed.source]) || [];
 
@@ -31,36 +19,26 @@ class NewsFeed extends React.Component {
           <h1>{this.props.t('Community feed')}</h1>
         </header>
 
-        <div className={`${styles.container} ${onShowMore}`}>
-          {
-            <div>
-              {
-                filteredNewsFeed.length > 0 &&
-                filteredNewsFeed.map((news, index) =>
-                  <div className={styles.newsWrapper} key={`newsWrapper-${index}`}>
-                    <News
-                      t={this.props.t}
-                      {...news} />
-                  </div>)
-              }
-              {
-                this.props.showNewsFeedEmptyState && filteredNewsFeed.length === 0 &&
-                (<div className={`${styles.emptyNews} empty-news`}>
-                  {this.props.t('No newsfeed available')}
-                  <img className={styles.liskLogo} src={logo} />
+        <div className={`${styles.container}`}>
+          <div>
+            {
+              filteredNewsFeed.length > 0 &&
+              filteredNewsFeed.map((news, index) =>
+                <div className={styles.newsWrapper} key={`newsWrapper-${index}`}>
+                  <News
+                    t={this.props.t}
+                    {...news} />
                 </div>)
-              }
-            </div>
-          }
+            }
+            {
+              this.props.showNewsFeedEmptyState && filteredNewsFeed.length === 0 &&
+              <div className={`${styles.emptyNews} empty-news`}>
+                {this.props.t('No newsfeed available')}
+                <img className={styles.liskLogo} src={logo} />
+              </div>
+            }
+          </div>
         </div>
-        {
-          filteredNewsFeed.length > 3 && !this.state.showSettings &&
-          <ShowMore
-            className={`${styles.showMoreAlign} show-more`}
-            onClick={() => this.onShowMore()}
-            text={this.state.showMore ? this.props.t('Show Less') : this.props.t('Show More')}
-          />
-        }
       </Box>
     );
   }
