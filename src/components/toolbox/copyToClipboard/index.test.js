cba01e003e52fffe1d5f30b0c10a5a61c0f3cc6c
Vit Stanislav <vit@lightcurve.io>
2019-06-12 09:00:41 +0200

    :white_check_mark: Use jest instead of chai in copyToClipboard tests

--- src/components/toolbox/copyToClipboard/index.test.js
+++ src/components/toolbox/copyToClipboard/index.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { expect } from 'chai';
 import { mount } from 'enzyme';
 import PropTypes from 'prop-types';
-import sinon from 'sinon';
 import i18n from '../../../i18n';
 import CopyToClipboard from './index';
 
 describe('CopyToClipboard', () => {
   let wrapper;
-  let clock;
   const props = {
     text: 'test',
     className: 'className',
@@ -26,27 +23,20 @@ describe('CopyToClipboard', () => {
 
   beforeEach(() => {
     wrapper = mount(<CopyToClipboard {...props} />, options);
-    clock = sinon.useFakeTimers({
-      toFake: ['setTimeout', 'clearTimeout', 'Date', 'setInterval'],
-    });
-  });
-
-  afterEach(() => {
-    clock.restore();
   });
 
   it('should show "Copied!" on click', () => {
     wrapper.find('.default').simulate('click');
-    expect(wrapper.find('.copied')).to.have.text(copiedText);
+    expect(wrapper.find('.copied').text()).toEqual(copiedText);
   });
 
   it('should hide "Copied!" after 3000ms', () => {
     wrapper.find('.default').simulate('click');
-    clock.tick(2900);
-    expect(wrapper.find('.copied')).to.have.text(copiedText);
-    clock.tick(2000);
+    jest.advanceTimersByTime(2900);
+    expect(wrapper.find('.copied').text()).toEqual(copiedText);
+    jest.advanceTimersByTime(2000);
     wrapper.update();
-    expect(wrapper.find('.copied')).to.have.length(0);
-    expect(wrapper.find('.default')).to.have.text(`${props.text} `);
+    expect(wrapper.find('.copied')).toHaveLength(0);
+    expect(wrapper.find('.default').text()).toEqual(`${props.text} `);
   });
 });
