929fc30c7a247c9c07cfbabe26ada928f1606950
Vit Stanislav <vit@lightcurve.io>
2019-06-13 14:50:33 +0200

    :seedling: Define default props of Icon component

--- src/components/toolbox/icon/index.js
+++ src/components/toolbox/icon/index.js
@@ -1,7 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+
 import svgIcons from '../../../utils/svgIcons';
 
 const Icon = ({ name, className }) => (
   <img src={svgIcons[name]} className={className} />
 );
+
+Icon.propTypes = {
+  name: PropTypes.oneOf(Object.keys(svgIcons)).isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+Icon.defaultProps = {
+  className: '',
+};
+
 export default Icon;
