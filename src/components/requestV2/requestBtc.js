efb10a17264164b4774f1881f7bf021ebdb98273
Vit Stanislav <vit@lightcurve.io>
2019-06-13 10:23:33 +0200

    :seedling: Implement Request BTC component

---
+++ src/components/requestV2/requestBtc.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import RequestWrapper from './requestWrapper';
+
+import styles from './requestV2.css';
+
+const RequestBtc = ({
+  address, t,
+}) => (
+  <RequestWrapper copyLabel={t('Copy address')} copyValue={address} t={t}>
+    <span className={`${styles.label}`}>
+      {t('Copy the address or scan the QR code, to easily request BTC tokens from Lisk Hub or Lisk Mobile users.')}
+    </span>
+    <label className={`${styles.fieldGroup}`}>
+      <span className={`${styles.fieldLabel}`}>{t('BTC Address')}</span>
+      <span className={`${styles.amountField} address`}>
+        {address}
+      </span>
+    </label>
+  </RequestWrapper>
+);
+
+export default RequestBtc;
