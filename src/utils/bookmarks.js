1c4834d707cbd71aa78b47e3e98919904a0a303b
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-06-17 16:16:20 +0200

    :recycle: Make getBookmarksFromLocalStorage save it after normalizing the data

--- src/utils/bookmarks.js
+++ src/utils/bookmarks.js
@@ -1,20 +1,24 @@
 import localJSONStorage from './localJSONStorage';
-import { tokenKeys } from '../constants/tokens';
+import { tokenKeys, tokenMap } from '../constants/tokens';
 
 export const flattenBookmarks = accounts =>
   Object.keys(accounts).reduce((acc, token) =>
     [...acc, ...accounts[token]], []);
 
-export const getBookmarksFromLocalStorage = () => {
-  const bookmarksObj = tokenKeys.reduce((acc, token) => ({ ...acc, [token]: [] }), {});
-  const bookmarks = localJSONStorage.get('bookmarks', localJSONStorage.get('followedAccounts', bookmarksObj));
-  localStorage.removeItem('followedAccounts');
-  return Array.isArray(bookmarks)
-    ? { ...bookmarksObj, LSK: bookmarks }
-    : bookmarks;
-};
-
 export const setBookmarksInLocalStorage = data => localJSONStorage.set('bookmarks', data);
 
-export const getIndexOfBookmark = (accounts, { address, token = 'LSK' }) =>
+export const getBookmarksFromLocalStorage = () => {
+  const bookmarksObj = tokenKeys.reduce((acc, token) => ({ ...acc, [token]: [] }), {});
+  const localStorageBookmarks =
+    localJSONStorage.get('bookmarks', localJSONStorage.get('followedAccounts', bookmarksObj));
+  const bookmarks = Array.isArray(localStorageBookmarks)
+    ? { ...bookmarksObj, LSK: localStorageBookmarks }
+    : localStorageBookmarks;
+  localStorage.removeItem('followedAccounts');
+  setBookmarksInLocalStorage(bookmarks);
+
+  return bookmarks;
+};
+
+export const getIndexOfBookmark = (accounts, { address, token = tokenMap.LSK.key }) =>
   accounts[token].findIndex(account => (account.address === address));
