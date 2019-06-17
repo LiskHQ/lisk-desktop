325decbf9b4298acee535133122a95c0d4d3325a
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-06-17 16:53:30 +0200

    :recycle: Add select delegate nickname form

---
+++ src/components/registerDelegateV2/delegateRegistration.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import MultiStep from '../multiStep';
+import SelectName from './selectName';
+import styles from './delegateRegistration.css';
+
+class DelegateRegistration extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.submitDelegate = this.submitDelegate.bind(this);
+    this.goBack = this.goBack.bind(this);
+  }
+
+  submitDelegate({ delegateName, passphrase, secondPassphrase }) {
+    this.props.delegateRegistered({
+      account: this.props.account.info.LSK,
+      username: delegateName,
+      passphrase: passphrase.value,
+      secondPassphrase: secondPassphrase.value,
+    });
+  }
+
+  checkDelegateUsernameAvailable(username) {
+    this.props.delegatesFetched({ username });
+  }
+
+  goBack() {
+    this.props.history.goBack();
+  }
+
+  render() {
+    const { t } = this.props;
+
+    return (
+      <section className={`${styles.wrapper}`}>
+        <MultiStep
+          className={styles.multiStep}
+          prevPage={this.goBack}
+          finalCallback={this.submitDelegate}
+          backButtonLabel={t('Back')}>
+          <SelectName t={t} />
+          <SelectName t={t} />
+          <SelectName t={t} />
+        </MultiStep>
+      </section>
+    );
+  }
+}
+
+export default withRouter(DelegateRegistration);
