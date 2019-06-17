91d01f92ae23a13fcadb241e0ab331d022d286a7
Vit Stanislav <vit@lightcurve.io>
2019-06-17 12:20:01 +0200

    :seedling: Add "Forged LSK" column to delegates table
    :recycle: move column definition to one place

--- src/components/delegatesListView/delegateListV2.js
+++ src/components/delegatesListView/delegateListV2.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import DelegateRowV2 from './delegateRowV2';
 import ListLabelsV2 from './listLabelsV2';
 import { Button } from '../toolbox/buttons/button';
@@ -22,28 +23,43 @@ class DelegateListV2 extends React.Component {
       votes,
       firstTimeVotingActive,
       shouldLoadMore,
+      t,
     } = this.props;
+
     const shouldShowVoteColumn = votingModeEnabled || getTotalVotesCount(votes) > 0;
+    const columnClassNames = {
+      vote: `${grid['col-md-1']} ${grid['col-xs-1']}`,
+      rank: `${grid['col-md-1']} ${grid['col-xs-1']}`,
+      delegate: `${grid[shouldShowVoteColumn ? 'col-md-5' : 'col-md-6']} ${grid['col-xs-4']}`,
+      forged: `${grid['col-md-2']} ${grid['col-xs-2']}`,
+      productivity: `${grid['col-md-1']} ${grid['col-xs-2']}`,
+      voteWeight: `${grid['col-md-2']}`,
+    };
     return (<div>
       { this.state.didMount ? <div className={`${styles.results} delegate-list`}>
-        <ListLabelsV2 t={this.props.t} shouldShowVoteColumn={shouldShowVoteColumn} />
+        <ListLabelsV2
+          t={t}
+          columnClassNames={columnClassNames}
+          shouldShowVoteColumn={shouldShowVoteColumn}
+        />
         {
             this.props.list.map(item =>
               <DelegateRowV2 key={item.account.address} data={item}
-                t={this.props.t}
+                t={t}
                 className={this.props.safari}
                 voteToggled={this.props.voteToggled}
                 voteStatus={votes[item.username]}
                 votingModeEnabled={votingModeEnabled}
                 shouldShowVoteColumn={shouldShowVoteColumn}
                 shouldHightlightCheckbox={firstTimeVotingActive}
+                columnClassNames={columnClassNames}
               />)
         }
         {shouldLoadMore ? <Button
           className={`${styles.loadMore} loadMore`}
           type='button'
           onClick={() => this.props.loadMore()}>
-          {this.props.t('Load More')}
+          {t('Load More')}
         </Button> : null}
       </div> : null}
     </div>);
