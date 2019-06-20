57676188d012bd81c3aef095a1a29e8fac23b1a3
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-06-20 11:05:50 +0200

    :white_check_mark: Adjust unit test for animation component

--- src/components/toolbox/animation/index.test.js
+++ src/components/toolbox/animation/index.test.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Animation from './';
 
 describe('Animation component', () => {
   const props = {
-    name: '',
-    loop: true,
+    name: 'delegateCreated',
+    loop: false,
     autoplay: true,
     renderer: 'svg',
     className: 'test-classname',
+    events: {
+      complete: jest.fn(),
+      loopComplete: jest.fn(),
+      enterFrame: jest.fn(),
+    },
   };
 
   it('Should render animation container with className', () => {
-    const wrapper = shallow(<Animation {...props} name={'delegateCreated'} />);
-    expect(wrapper).toMatchElement(<div/>);
+    const wrapper = mount(<Animation {...props} />);
+    wrapper.setProps({ name: 'delegateCreated' });
     expect(wrapper).toHaveClassName(props.className);
+    wrapper.unmount();
+  });
+
+  it('Should render animation container without className', () => {
+    const wrapper = mount(<Animation {...props} />);
+    wrapper.setProps({
+      name: 'delegatePending',
+      loop: true,
+      className: '',
+    });
+    wrapper.update();
+    expect(wrapper).not.toHaveClassName(props.className);
+    wrapper.unmount();
   });
 });
