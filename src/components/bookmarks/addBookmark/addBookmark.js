66ba7083cd32fd3d82e9e979109a6861ac6acf2b
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-06-26 17:53:51 +0200

    :recycle: Finish initial logic of add new bookmark

--- src/components/bookmarks/addBookmark/addBookmark.js
+++ src/components/bookmarks/addBookmark/addBookmark.js
@@ -7,6 +7,9 @@ import { InputV2 } from '../../toolbox/inputsV2';
 import { PrimaryButtonV2 } from '../../toolbox/buttons/button';
 import Feedback from '../../toolbox/feedback/feedback';
 import styles from './addBookmark.css';
+import { getIndexOfBookmark } from '../../../utils/bookmarks';
+import { tokenMap } from '../../../constants/tokens';
+import routes from '../../../constants/routes';
 
 class AddBookmark extends React.Component {
   constructor(props) {
@@ -27,6 +30,7 @@ class AddBookmark extends React.Component {
         value: '',
         error: false,
         feedback: field.feedback || '',
+        readonly: false,
       },
     }), {});
 
@@ -38,11 +42,22 @@ class AddBookmark extends React.Component {
       address: this.onAddressChange.bind(this),
       label: this.onLabelChange.bind(this),
     };
+    this.handleAddBookmark = this.handleAddBookmark.bind(this);
   }
 
   componentDidUpdate(prevProps) {
-    const { token } = this.props;
+    const { token, accounts } = this.props;
     const { token: prevToken } = prevProps;
+    const { fields } = this.state;
+
+    const account = accounts[fields.address.value] || {};
+    if (account.delegate && account.delegate.username !== fields.label.value) {
+      this.updateField({
+        name: 'label',
+        data: { value: account.delegate.username, readonly: true },
+      });
+    }
+
     if (token.active === prevToken.active) {
       return true;
     }
@@ -86,9 +101,18 @@ class AddBookmark extends React.Component {
     return { feedback, error };
   }
 
+  checkDelegate(value) {
+    const { searchAccount } = this.props;
+    searchAccount({ address: value });
+  }
+
   onAddressChange({ target: { name, value } }) {
     const { token: { active } } = this.props;
     const { feedback, error } = this.validateAddress(active, value);
+    if (active === tokenMap.LSK.key && !error) {
+      this.checkDelegate(value);
+    }
+
     this.updateField({
       name,
       data: { error, value, feedback },
@@ -96,20 +120,41 @@ class AddBookmark extends React.Component {
   }
 
   validateAddress(token, value) {
-    const { network } = this.props;
+    const { network, bookmarks } = this.props;
     const netCode = network.name === networks.mainnet.name
       ? networks.mainnet.code
       : networks.testnet.code;
-    const error = validateAddress(token, value, netCode) === 1;
-    const feedback = error
-      ? 'Invalid address.'
-      : '';
-    return { error, feedback };
+    const isInvalid = validateAddress(token, value, netCode) === 1;
+    const alreadyBookmarked = !isInvalid
+      && getIndexOfBookmark(bookmarks, { address: value, token }) !== -1;
+    const feedback =
+      (isInvalid && 'Invalid address.')
+      || (alreadyBookmarked && 'Address already bookmarked.')
+      || '';
+    return { error: isInvalid || alreadyBookmarked, feedback };
+  }
+
+  handleAddBookmark(e) {
+    e.preventDefault();
+    const { token: { active }, bookmarkAdded, accounts } = this.props;
+    const { fields: { label, address } } = this.state;
+    const publicKey = active === tokenMap.LSK.key
+      ? accounts[address.value].publicKey
+      : null;
+    const account = {
+      title: label.value,
+      address: address.value,
+      publicKey,
+    };
+    bookmarkAdded({ token: active, account });
+    this.props.history.push(routes.bookmarks.path);
   }
 
   render() {
     const { t } = this.props;
     const { fields } = this.state;
+    const isDisabled = !!Object.keys(fields).find(field => fields[field].error || fields[field].value === '');
+
     return (
       <div className={styles.wrapper}>
         <div className={styles.content}>
@@ -133,6 +178,7 @@ class AddBookmark extends React.Component {
                     value={fields[field.name].value}
                     onChange={this.onInputChange[field.name]}
                     name={field.name}
+                    readOnly={field.readonly}
                   />
                   <Feedback
                     className={`${styles.feedback} ${fields[field.name].error ? styles.error : ''}`}
@@ -144,7 +190,10 @@ class AddBookmark extends React.Component {
                 </label>
               ))}
               <div className={styles.buttonHolder}>
-                <PrimaryButtonV2>
+                <PrimaryButtonV2
+                  disabled={isDisabled}
+                  onClick={this.handleAddBookmark}
+                >
                   {t('Add bookmark')}
                 </PrimaryButtonV2>
               </div>
--- src/components/bookmarks/addBookmark/index.js
+++ src/components/bookmarks/addBookmark/index.js
@@ -1,12 +1,20 @@
 /* istanbul ignore file */
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
+import { searchAccount } from '../../../actions/search';
+import { bookmarkAdded } from '../../../actions/bookmarks';
 import AddBookmark from './addBookmark';
 
 const mapStateToProps = state => ({
   bookmarks: state.bookmarks,
   token: state.settings.token,
   network: state.network,
+  accounts: state.search.accounts,
 });
 
-export default connect(mapStateToProps)(translate()(AddBookmark));
+const mapDispatchToProps = {
+  searchAccount,
+  bookmarkAdded,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(translate()(AddBookmark));
