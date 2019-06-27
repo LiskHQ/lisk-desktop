d7477a6dd14cd39f41da27e8f8622d2238870ff8
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-06-27 15:29:08 +0200

    :white_check_mark: Adjust inputV2 unit tests

--- src/components/toolbox/inputsV2/inputV2.test.js
+++ src/components/toolbox/inputsV2/inputV2.test.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { expect } from 'chai';
-import { spy } from 'sinon';
 import { mount } from 'enzyme';
 import InputV2 from './inputV2';
 
 describe('InputV2', () => {
   let wrapper;
   const props = {
-    className: '',
+    className: 'test',
     defaultValue: 'test',
-    onChange: spy(),
+    onChange: jest.fn(),
   };
 
   it('should render with passed props', () => {
     wrapper = mount(<InputV2 {...props} />);
-    expect(wrapper).to.have.className(props.className);
-    expect(wrapper.html()).to.be.contain(props.defaultValue);
+    expect(wrapper.find('input')).toHaveValue(props.defaultValue);
+    expect(wrapper.find('input')).toHaveClassName(props.className);
     wrapper.simulate('change', { target: { value: 'test' } });
-    expect(props.onChange).to.have.been.calledWith();
+    expect(props.onChange).toBeCalled();
   });
 
   it('should pass size as className ', () => {
@@ -26,6 +24,14 @@ describe('InputV2', () => {
       size: 'l',
     };
     wrapper = mount(<InputV2 {...propWithoutClassName} />);
-    expect(wrapper).to.have.className('l');
+    expect(wrapper.find('input')).toHaveClassName('l');
+  });
+
+  it('Should render with error class', () => {
+    const propWithError = {
+      error: true,
+    };
+    wrapper = mount(<InputV2 { ...propWithError } />);
+    expect(wrapper.find('input')).toHaveClassName('error');
   });
 });
