7dbaa14e5cb4d2e93c3b49d5232e16cfd1b96ba6
yasharAyari <yashar@lightcurve.io>
2019-06-28 15:25:00 +0200

    Implement the new design of the help center

--- src/components/help/help.js
+++ src/components/help/help.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
-import Box from '../box';
-import { FontIcon } from '../fontIcon';
-import { Button } from './../toolbox/buttons/button';
+import Box from '../boxV2';
+import { PrimaryButtonV2 as Button } from './../toolbox/buttons/button';
 import styles from './help.css';
 import links from './../../constants/externalLinks';
 import Piwik from '../../utils/piwik';
+import academyIcon from '../../assets/images/help/academy.svg';
+import helpCenterIcon from '../../assets/images/help/help-center.svg';
+import liskChatIcon from '../../assets/images/help/lisk-chat.svg';
+import Illustration from '../../assets/images/help/Illustration.svg';
+import arrowIcon from '../../assets/images/help/arrow.svg';
 
 /* eslint-disable class-methods-use-this */
 class Help extends React.Component {
@@ -19,91 +23,82 @@ class Help extends React.Component {
   }
 
   render() {
-    const fAQIcon = () => (<span className={styles.inlineIcon}><FontIcon>info</FontIcon><b>{this.props.t(' FAQ')}</b></span>);
-    const chatIcon = () => (<span className={styles.inlineIcon}><FontIcon>conversation</FontIcon><b>{this.props.t(' Chat')}</b></span>);
+    const ArrowIcon = () => <img src={arrowIcon} className={styles.listArrow} />;
     return (
       <Box className={styles.wrapper}>
-        <aside className={`${grid['col-sm-12']} ${grid['col-md-4']} ${styles.sideBar}`}>
-          <header>
-            <h2>{this.props.t('Help')}</h2>
-            <p>{this.props.t('Browse through our tutorials, check out the FAQ or connect with our knowledgeable community.')}</p>
+        <header>
+          <h2>{this.props.t('Help Center')}</h2>
+        </header>
+        <section className={`${grid.row} ${styles.topRow}`}>
+        <section className={`${grid['col-sm-12']} ${grid['col-md-12']} ${styles.topBox}`}>
+            <article>
+              <h3>{this.props.t('Don’t worry, we’re here to help')}</h3>
+              <p>
+                {this.props.t('Browse through our tutorials, check out the FAQ section, connect with our community or take a look at our ')}
+                <a
+                  onClick={this.checkTermsAndConditions}
+                  href={'https://lisk.io/terms-conditions'}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {this.props.t('Terms of Use')}
+                </a>
+              </p>
+            </article>
+            <img src={Illustration} />
+          </section>
+        </section>
 
-            <a
-              className={'terms-of-use'}
-              onClick={this.checkTermsAndConditions}
-              href={'https://lisk.io/terms-conditions'}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {this.props.t('Terms of Use')}
-              <FontIcon>arrow-right</FontIcon>
-            </a>
-          </header>
-        </aside>
-
-        <section className={`${grid['col-sm-12']} ${grid['col-md-8']} ${styles.helpSection} help-articles`}>
-          <article>
-            <h2>
-              <FontIcon className={styles.headerIcon}>logo-icon</FontIcon>
+        <section className={`${grid.row} ${styles.helpSection} help-articles`}>
+          <article className={`${grid['col-sm-12']} ${grid['col-md-4']} ${styles.articleContainer}`}>
+            <h4>
+              <img src={helpCenterIcon} className={styles.titleIcon} />
               {this.props.t('Help Center')}
-            </h2>
-            <div className={styles.articleContainer}>
-              <div>
-                <p>{this.props.t('Search for answers in our extensive ')}
-                  {fAQIcon()}
-                  {this.props.t(' or get in touch in via ')}
-                  {chatIcon()}.
-                </p>
-              </div>
-              <div>
-              <Button className='help-visit-center' onClick={() => this.visitHelpCenter() }>{this.props.t('Visit Help Center')}
-                <FontIcon>arrow-right</FontIcon>
-              </Button>
-              </div>
-            </div>
+            </h4>
+            <p className={styles.faqWrapper}>
+              {this.props.t('Perhaps your question has already been raised in our FAQ')}
+            </p>
+            <Button className='help-visit-center' onClick={() => this.visitHelpCenter() }>
+              {this.props.t('Visit Help Center')}
+            </Button>
           </article>
 
-          <article>
-            <h2>
-              <FontIcon className={styles.headerIcon}>academy</FontIcon>
+          <article className={`${grid['col-sm-12']} ${grid['col-md-4']} ${styles.articleContainer}`}>
+            <h4>
+              <img src={academyIcon} className={styles.titleIcon} />
               {this.props.t('Academy')}
-            </h2>
-            <div className={styles.articleContainer}>
-              <div>
-                <p>{this.props.t('Learn about blockchain with our comprehensive knowledge base.')}
-                </p>
-              </div>
+            </h4>
+              <p>{this.props.t('Learn about blockchain with our comprehensive knowledge base.')}
+              </p>
               <div>
                 <ul>
                   <li>
                     <a target='_blank' href={links.explainBlockchain} rel='noopener noreferrer'>
                       {this.props.t('Explain Blockchain Like I\'m 5')}
-                      <FontIcon>arrow-right</FontIcon>
+                      <ArrowIcon />
                     </a>
                   </li>
                   <li>
                     <a target='_blank' href={links.isBlockchainSecure} rel='noopener noreferrer'>
                       {this.props.t('Is Blockchain Secure?')}
-                      <FontIcon>arrow-right</FontIcon>
+                      <ArrowIcon />
                     </a>
                   </li>
                   <li>
                     <a target='_blank' href={links.seeMore} rel='noopener noreferrer'>
                       {this.props.t('See more')}
-                      <FontIcon>arrow-right</FontIcon>
+                      <ArrowIcon />
                     </a>
                   </li>
                 </ul>
               </div>
-            </div>
           </article>
 
-          <article>
-            <h2>
-              <FontIcon className={styles.headerIcon}>lisk-chat</FontIcon>
+          <article className={`${grid['col-sm-12']} ${grid['col-md-4']} ${styles.articleContainer}`}>
+            <h4>
+              <img src={liskChatIcon} className={styles.titleIcon} />
               {this.props.t('Lisk.Chat')}
-            </h2>
-            <div className={styles.articleContainer}>
+            </h4>
               <div>
                 <p>{this.props.t('Don’t be a stranger! Connect with our worldwide community.')}
                 </p>
@@ -113,18 +108,17 @@ class Help extends React.Component {
                   <li>
                     <a target='_blank' href={links.liskChat} rel='noopener noreferrer'>
                       {this.props.t('Visit Lisk.Chat')}
-                      <FontIcon>arrow-right</FontIcon>
+                      <ArrowIcon />
                     </a>
                   </li>
                   <li>
                     <a target='_blank' href={links.helpDeskChannel} rel='noopener noreferrer'>
                       {this.props.t('See #help-desk channel')}
-                      <FontIcon>arrow-right</FontIcon>
+                      <ArrowIcon />
                     </a>
                   </li>
                 </ul>
               </div>
-            </div>
           </article>
         </section>
       </Box>
