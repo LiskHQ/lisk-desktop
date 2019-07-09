b62ad12961e712fd1a8c6a31ef962c721d3cda3d
andreiklimenok <andrei.klimenok@lightcurve.io>
2019-07-09 13:32:25 +0200

    txTable_filtering feature

---
+++ test/cypress/features/txTable_filtering/txTable_filtering.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import networks from '../../../constants/networks';
+import ss from '../../../constants/selectors';
+import accounts from '../../../constants/accounts';
+
+Then(/^I click filter transactions$/, function () {
+  cy.get(ss.filterTransactionsBtn).click();
+});
+
+Then(/^I type date from ([^\s]+)$/, function (string) {
+  cy.get(ss.dateFromInputFilter).type(string);
+});
+
+Then(/^I type date to ([^\s]+)$/, function (string) {
+  cy.get(ss.dateToInputFilter).type(string);
+});
+
+Then(/^I type amount from ([^\s]+)$/, function (string) {
+  cy.get(ss.amountFromInputFilter).type(string);
+});
+
+Then(/^I type amount to ([^\s]+)$/, function (string) {
+  cy.get(ss.amountToInputFilter).type(string);
+});
+
+Then(/^I type message ([^\s]+)$/, function (string) {
+  cy.get(ss.messageInputFilter).type(string);
+});
+
+Then(/^I apply filters$/, function () {
+  cy.get(ss.applyFilters).click();
+});
+
+Then(/^I clear all filters$/, function () {
+  cy.get(ss.clearAllFiltersBtn).click();
+});
+
+Then(/^I should see (\d+) transactions in table$/, function (number) {
+  cy.get(ss.transactionRow).should('have.length', number);
+});
+
+Then(/^Clear filter containing ([^\s]+)$/, function (string) {
+  cy.get(ss.filter).contains('25').parent().find(ss.clearFilterBtn).click();
+});
+
+Then(/^I click filter incoming$/, function () {
+  cy.get(ss.filterIncoming).click().should('have.class', 'active');
+});
+
+Then(/^I click filter outgoing$/, function () {
+  cy.get(ss.filterOutgoing).click().should('have.class', 'active');
+});
+
+
+
