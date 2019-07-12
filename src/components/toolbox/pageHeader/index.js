7e3b90e055294224fbc0f0c344761f262391567f
Vit Stanislav <vit@lightcurve.io>
2019-07-12 11:18:10 +0200

    :white_check_mark: Add back wallet-header class used in e2e

--- src/components/toolbox/pageHeader/index.js
+++ src/components/toolbox/pageHeader/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './pageHeader.css';
 
 const PageHeader = ({
-  title, subtitle, children,
+  title, subtitle, children, className,
 }) => (
-  <header className={styles.wrapper}>
+  <header className={`${styles.wrapper} ${className}`} >
     <div>
       <h1>{title}</h1>
       <span className={styles.subtitle}>{subtitle}</span>
@@ -14,5 +14,9 @@ const PageHeader = ({
     </div>
   </header>
 );
+
+PageHeader.defaultProps = {
+  className: '',
+};
 
 export default PageHeader;
