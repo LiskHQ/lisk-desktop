d07a9b57fdd0d35216a96c719ff827e6c9c4e692
Vit Stanislav <vit@lightcurve.io>
2019-07-15 19:07:43 +0200

    :white_check_mark: Update unit test of transactionDetailViewV2

--- src/components/transactionsV2/transactionDetailViewV2/transactionDetailViewV2.test.js
+++ src/components/transactionsV2/transactionDetailViewV2/transactionDetailViewV2.test.js
@@ -30,7 +30,6 @@ describe('Transaction Detail View V2', () => {
 
     it('Should render transfer transaction with message', () => {
       wrapper = mount(<Router><TransactionDetailViewV2 {...props} /></Router>, options);
-      expect(wrapper).toContainMatchingElement('.summaryHeader');
       expect(wrapper).toContainMatchingElements(2, '.accountInfo');
       expect(wrapper.find('.accountInfo .sender-address').first().text()).toBe(transaction.senderId);
       expect(wrapper.find('.accountInfo .receiver-address').at(1).text()).toBe(transaction.recipientId);
@@ -60,7 +59,7 @@ describe('Transaction Detail View V2', () => {
     it('Should render register 2nd passphrase details', () => {
       wrapper = mount(<Router><TransactionDetailViewV2 {...props} /></Router>, options);
       expect(wrapper).toContainExactlyOneMatchingElement('.accountInfo');
-      expect(wrapper.find('.accountInfo .label').text()).toBe('Registrant');
+      expect(wrapper.find('.accountInfo .label').text()).toBe('Account');
     });
   });
 
@@ -81,7 +80,9 @@ describe('Transaction Detail View V2', () => {
     it('Should render register delegate details', () => {
       wrapper = mount(<Router><TransactionDetailViewV2 {...props} /></Router>, options);
       expect(wrapper).toContainExactlyOneMatchingElement('.accountInfo');
-      expect(wrapper.find('.summaryHeader p').text()).toBe(transaction.asset.delegate.username);
+      // commented out while waiting for answer to whether this should be kept in place
+      // https://projects.invisionapp.com/d/main#/console/17570736/368355792/comments/118391923
+      // expect(wrapper.find('.summaryHeader p').text()).toBe(transaction.asset.delegate.username);
     });
   });
 });
