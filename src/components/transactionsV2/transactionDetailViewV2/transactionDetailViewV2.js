ebf02601aba31faba9dd3f3ef49181b9764f0b5c
Vit Stanislav <vit@lightcurve.io>
2019-07-15 18:56:04 +0200

    :seedling: Adjust title of non-send transactionDetailViewV2

--- src/components/transactionsV2/transactionDetailViewV2/transactionDetailViewV2.js
+++ src/components/transactionsV2/transactionDetailViewV2/transactionDetailViewV2.js
@@ -1,33 +1,30 @@
-import React from 'react';
 import { translate } from 'react-i18next';
+import React from 'react';
 import AccountInfo from './accountInfo';
+import TransactionTypeFigure from
+  '../../transactions/typeFigure/TransactionTypeFigure';
 import TransactionVotes from './transactionVotes';
 import styles from './transactionDetailViewV2.css';
-import AmountV2 from '../amountV2';
 import transactionTypes from '../../../constants/transactionTypes';
 
 class TransactionDetailViewV2 extends React.Component {
   // eslint-disable-next-line complexity
   render() {
-    const { transaction, t, activeToken } = this.props;
-    let label = t('Sender');
+    const { transaction, t } = this.props;
+    let senderLabel = t('Sender');
     let title;
-    let value = transaction.amount;
     switch (transaction.type) {
       case transactionTypes.setSecondPassphrase:
-        title = t('2nd passphrase pegistration');
-        label = t('Account');
+        title = t('2nd passphrase registration');
+        senderLabel = t('Account');
         break;
       case transactionTypes.registerDelegate:
         title = t('Delegate registration');
-        label = t('Account');
-        value = transaction.asset
-          && transaction.asset.delegate
-          && transaction.asset.delegate.username;
+        senderLabel = t('Account');
         break;
       case transactionTypes.vote:
         title = t('Delegate vote');
-        label = t('Voter');
+        senderLabel = t('Voter');
         break;
       default:
         break;
@@ -37,26 +34,18 @@ class TransactionDetailViewV2 extends React.Component {
       <React.Fragment>
         {title ? (
           <div className={styles.summaryHeader}>
+            <TransactionTypeFigure
+              address={transaction.senderId}
+              transactionType={transaction.type}
+            />
             <h2>{title}</h2>
-            <p>
-              {transaction.type === transactionTypes.send ? (
-                <span className="tx-amount">
-                  <AmountV2
-                    className={styles.txAmount}
-                    token={activeToken}
-                    value={transaction}
-                  />
-                </span>
-              ) : value
-            }
-            </p>
           </div>
         ) : null}
         <div className={styles.detailsWrapper}>
           <AccountInfo
             address={transaction.senderId}
             addressClass="sender-address"
-            label={label}
+            label={senderLabel}
           />
         </div>
         {transaction.type === transactionTypes.send
@@ -70,24 +59,21 @@ class TransactionDetailViewV2 extends React.Component {
             </div>
           )
           : null
-          }
+        }
         { transaction.type === transactionTypes.send
-          || transaction.type === transactionTypes.vote ? (
-            <React.Fragment>
-              { transaction.type === transactionTypes.send
-            && (transaction.asset && transaction.asset.data) ? (
-              <div className={styles.detailsWrapper}>
-                <span className={styles.label}>{t('Message')}</span>
-                <div className={`${styles.message} tx-reference`}>
-                  {transaction.asset.data}
-                </div>
+          && (transaction.asset && transaction.asset.data) ? (
+            <div className={styles.detailsWrapper}>
+              <span className={styles.label}>{t('Message')}</span>
+              <div className={`${styles.message} tx-reference`}>
+                {transaction.asset.data}
               </div>
-                ) : null}
-              { transaction.type === transactionTypes.vote && transaction.votesName ? (
-                <TransactionVotes votes={transaction.votesName} />
-              ) : null}
-            </React.Fragment>
-          ) : null}
+            </div>
+          ) : null
+        }
+        { transaction.type === transactionTypes.vote && transaction.votesName ? (
+          <TransactionVotes votes={transaction.votesName} />
+        ) : null
+        }
       </React.Fragment>
     ) : null);
   }
