5eb52032e95fb4b2943280ae53f3e2eca139ddd9
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-07-26 14:58:08 +0200

    :recycle: Remove actions that are not used anymore

--- src/actions/liskService.js
+++ src/actions/liskService.js
@@ -1,24 +1,6 @@
 import actionTypes from '../constants/actions';
 import liskServiceApi from '../utils/api/lsk/liskService';
 
-
-// TODO Remove Since the currency graph is not used anymore
-export const addDataToCurrencyGraph = data => ({
-  type: actionTypes.addDataToCurrencyGraph,
-  data,
-});
-
-// TODO Remove Since the currency graph is not used anymore
-export const addErrorToCurrencyGraph = data => ({
-  type: actionTypes.addErrorToCurrencyGraph,
-  data,
-});
-
-// TODO Remove Since the currency graph is not used anymore
-export const clearDataOfCurrencyGraph = () => ({
-  type: actionTypes.clearDataOfCurrencyGraph,
-});
-
 // TODO Update to HOC to load newsFeed only when needed
 export const addNewsFeed = data => ({
   type: actionTypes.getNewsFeed,
@@ -31,22 +13,6 @@ export const showEmptyNewsFeedState = data => ({
   data,
 });
 
-// TODO Remove Since priceTicker was moved to service already
-export const addPriceTicker = data => ({
-  type: actionTypes.addPriceTicker,
-  data,
-});
-
-// TODO Remove Since the currency graph is not used anymore
-export const getCurrencyGraphData = step => (dispatch) => {
-  dispatch(clearDataOfCurrencyGraph());
-  liskServiceApi.getCurrencyGraphData(step).then((response) => {
-    dispatch(addDataToCurrencyGraph({ response, step }));
-  }).catch((error) => {
-    dispatch(addErrorToCurrencyGraph(error));
-  });
-};
-
 // TODO Update to HOC to load newsFeed only when needed
 export const getNewsFeed = () => (dispatch) => {
   liskServiceApi.getNewsFeed().then((newsFeed) => {
@@ -54,14 +20,5 @@ export const getNewsFeed = () => (dispatch) => {
   }).catch((error) => {
     dispatch(showEmptyNewsFeedState({ showNewsFeedEmptyState: true }));
     throw new Error(error);
-  });
-};
-
-// TODO Remove Since priceTicker was moved to service already
-export const getPriceTicker = () => (dispatch) => {
-  liskServiceApi.getPriceTicker().then((response) => {
-    dispatch(addPriceTicker({ ...response }));
-  }).catch((error) => {
-    dispatch(addPriceTicker({ error }));
   });
 };
