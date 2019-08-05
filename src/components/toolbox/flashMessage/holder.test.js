07bf2ac633f6edfecc33aa0203d983a23242522c
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-08-05 12:01:22 +0200

    :white_check_mark: Improve FlashMessageHolder unit test

--- src/components/toolbox/flashMessage/holder.test.js
+++ src/components/toolbox/flashMessage/holder.test.js
@@ -4,22 +4,37 @@ import FlashMessageHolder from './holder';
 import FlashMessage from './flashMessage';
 
 describe('Flash Message Holder', () => {
-  const wrapper = mount(<FlashMessageHolder />);
+  let wrapper;
+  const DummyMessage = (
+    <FlashMessage shouldShow>
+      Dummy text
+    </FlashMessage>
+  );
+
+  beforeEach(() => {
+    wrapper = mount(<FlashMessageHolder />);
+  });
 
   it('Should render FlashMessageHolder and add message when addMessage is called', () => {
     expect(wrapper).toBeEmptyRender();
-    expect(FlashMessageHolder.addMessage((
-      <FlashMessage shouldShow>
-        Dummy text
-      </FlashMessage>
-    ), 'testMessage')).toEqual(true);
+    expect(FlashMessageHolder.addMessage(DummyMessage, 'testMessage')).toEqual(true);
     wrapper.update();
-    expect(wrapper).toContainMatchingElements(1, 'FlashMessage');
+    expect(wrapper).toContainExactlyOneMatchingElement('FlashMessage');
   });
 
   it('Should dismiss message and remove from holder if closeBtn clicked', () => {
-    wrapper.find('.closeBtn').simulate('click');
+    FlashMessageHolder.addMessage(DummyMessage, 'testMessage2');
+    FlashMessageHolder.addMessage(DummyMessage, 'testMessage');
     wrapper.update();
-    expect(wrapper).not.toContainMatchingElement('FlashMessage');
+    wrapper.find('.closeBtn').at(0).simulate('click');
+    wrapper.update();
+    expect(wrapper).toContainExactlyOneMatchingElement('FlashMessage');
+  });
+
+  it('Should not render message with invalid React element', () => {
+    expect(wrapper).toBeEmptyRender();
+    expect(FlashMessageHolder.addMessage(jest.fn(), 'testMessage')).toBe(false);
+    wrapper.update();
+    expect(wrapper).toBeEmptyRender();
   });
 });
