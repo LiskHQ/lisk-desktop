d018924d454de10e7753b9c7511f040f66438dbd
Vit Stanislav <vit@lightcurve.io>
2019-08-06 17:18:19 +0200

    :recycle: Reformat singleTransaction.js

--- src/components/singleTransaction/singleTransaction.js
+++ src/components/singleTransaction/singleTransaction.js
@@ -61,103 +61,101 @@ class SingleTransaction extends React.Component {
 
     return (
       <div className={`${grid.row} ${grid['center-xs']} ${styles.container}`}>
-        { error
-          ? <NotFound />
-          : (
-            <React.Fragment>
-              { transaction.id ? (
-                <Box className={styles.wrapper}>
-                  <header className={`${styles.detailsHeader}`}>
-                    <h1>{t('Transaction details')}</h1>
-                    <CopyToClipboard
-                      value={this.getLinkToCopy()}
-                      text={t('Copy link')}
-                      Container={SecondaryButton}
-                      containerClassName="extra-small"
-                      copyClassName={styles.copyIcon}
-                    />
-                  </header>
-                  <main className={styles.mainContent}>
-                    <TransactionDetailView
-                      address={address}
-                      activeToken={activeToken}
-                      transaction={transaction}
-                    >
-                      <footer className={styles.detailsFooter}>
-                        <div>
-                          <div className={styles.value}>
-                            <span className={styles.label}>{t('Date')}</span>
-                            <span className={`${styles.date} tx-date`}>
-                              <DateTimeFromTimestamp
-                                fulltime
-                                className="date"
-                                time={transaction.timestamp}
-                                token={activeToken}
-                                showSeconds
-                              />
-                            </span>
-                          </div>
-                          <div className={`${styles.value}`}>
-                            <span className={styles.label}>
-                              {t('Confirmations')}
-                              <Tooltip className="showOnTop">
-                                <p>
-                                  { t('Confirmations refer to the number of blocks added to the {{token}} blockchain after a transaction has been submitted. The more confirmations registered, the more secure the transaction becomes.', { token: tokenMap[activeToken].label })}
-                                </p>
-                              </Tooltip>
-                            </span>
-                            <span className="tx-confirmation">
-                              {transaction.confirmations || 0}
-                            </span>
-                          </div>
+        { transaction.id ? (
+          <Box className={styles.wrapper}>
+            <header className={`${styles.detailsHeader}`}>
+              <h1>{t('Transaction details')}</h1>
+              <CopyToClipboard
+                value={this.getLinkToCopy()}
+                text={t('Copy link')}
+                Container={SecondaryButton}
+                containerClassName="extra-small"
+                copyClassName={styles.copyIcon}
+              />
+            </header>
+            <main className={styles.mainContent}>
+              <TransactionDetailView
+                address={address}
+                activeToken={activeToken}
+                transaction={transaction}
+              >
+                <footer className={styles.detailsFooter}>
+                  <div>
+                    <div className={styles.value}>
+                      <span className={styles.label}>{t('Date')}</span>
+                      <span className={`${styles.date} tx-date`}>
+                        <DateTimeFromTimestamp
+                          fulltime
+                          className="date"
+                          time={transaction.timestamp}
+                          token={activeToken}
+                          showSeconds
+                        />
+                      </span>
+                    </div>
+                    <div className={`${styles.value}`}>
+                      <span className={styles.label}>
+                        {t('Confirmations')}
+                        <Tooltip className="showOnTop">
+                          <p>
+                            { t('Confirmations refer to the number of blocks added to the {{token}} blockchain after a transaction has been submitted. The more confirmations registered, the more secure the transaction becomes.', { token: tokenMap[activeToken].label })}
+                          </p>
+                        </Tooltip>
+                      </span>
+                      <span className="tx-confirmation">
+                        {transaction.confirmations || 0}
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    <div className={`${styles.value}`}>
+                      <span className={styles.label}>
+                        {t('Transaction ID')}
+                      </span>
+                      <span className="transaction-id">
+                        <CopyToClipboard
+                          value={transaction.id}
+                          className="tx-id"
+                          containerClassName="extra-small copy-title"
+                          copyClassName={styles.copyIcon}
+                        />
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    { transaction.type === transactionTypes.send
+                      ? (
+                        <div className={styles.value}>
+                          <span className={styles.label}>
+                            {t('Amount')}
+                          </span>
+                          <span className="tx-amount">
+                            <LiskAmount val={transaction.amount} />
+                            {' '}
+                            {activeToken}
+                          </span>
                         </div>
-                        <div>
-                          <div className={`${styles.value}`}>
-                            <span className={styles.label}>
-                              {t('Transaction ID')}
-                            </span>
-                            <span className="transaction-id">
-                              <CopyToClipboard
-                                value={transaction.id}
-                                className="tx-id"
-                                containerClassName="extra-small copy-title"
-                                copyClassName={styles.copyIcon}
-                              />
-                            </span>
-                          </div>
-                        </div>
-                        <div>
-                          { transaction.type === transactionTypes.send
-                            ? (
-                              <div className={styles.value}>
-                                <span className={styles.label}>
-                                  {t('Amount')}
-                                </span>
-                                <span className="tx-amount">
-                                  <LiskAmount val={transaction.amount} />
-                                  {' '}
-                                  {activeToken}
-                                </span>
-                              </div>
-                            ) : null }
-                          <div className={styles.value}>
-                            <span className={styles.label}>
-                              {t('Transaction fee')}
-                            </span>
-                            <span className="tx-fee">
-                              <LiskAmount val={transaction.fee} />
-                              {' '}
-                              {activeToken}
-                            </span>
-                          </div>
-                        </div>
-                      </footer>
-                    </TransactionDetailView>
-                  </main>
-                </Box>
-              ) : null }
-            </React.Fragment>
-          )}
+                      ) : null }
+                    <div className={styles.value}>
+                      <span className={styles.label}>
+                        {t('Transaction fee')}
+                      </span>
+                      <span className="tx-fee">
+                        <LiskAmount val={transaction.fee} />
+                        {' '}
+                        {activeToken}
+                      </span>
+                    </div>
+                  </div>
+                </footer>
+              </TransactionDetailView>
+            </main>
+          </Box>
+        ) : (
+          <React.Fragment>
+            { error ? <NotFound /> : null}
+          </React.Fragment>
+        ) }
       </div>
     );
   }
