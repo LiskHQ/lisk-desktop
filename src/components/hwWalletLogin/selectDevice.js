58b5e015771d871c79e1b72b8a2634c74ea4baaa
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-08-12 15:53:50 +0200

    :recycle: Update copy of which wallets are supported

--- src/components/hwWalletLogin/selectDevice.js
+++ src/components/hwWalletLogin/selectDevice.js
@@ -37,7 +37,7 @@ class SelectDevice extends React.Component {
     return (
       <div>
         <h1>{t('Found several devices, choose the one you’d like to access')}</h1>
-        <p>{t('Lisk Hub currently supports Ledger Nano S and Trezor Model T wallets')}</p>
+        <p>{t('Lisk Hub currently supports Ledger Nano S, Ledger Nano X and Trezor Model T wallets')}</p>
 
         <div className={`${styles.deviceContainer} hw-container`}>
           {
