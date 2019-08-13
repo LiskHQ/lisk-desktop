088867ddb82d85434c2d8b265aa093bd62fa524d
Vit Stanislav <vit@lightcurve.io>
2019-08-13 10:24:04 +0200

    :seedling: Add new prop <Box.Row isClickable />

--- src/components/toolbox/box/row.js
+++ src/components/toolbox/box/row.js
@@ -2,17 +2,26 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './box.css';
 
-const Row = ({ children, className, ...rest }) => (
-  <section {...rest} className={`${styles.row} ${className}`}>{children}</section>
+const Row = ({
+  children, className, isClickable, ...rest
+}) => (
+  <section
+    {...rest}
+    className={`${styles.row} ${className} ${isClickable ? styles.clickableRow : ''}`}
+  >
+    {children}
+  </section>
 );
 
 Row.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  isClickable: PropTypes.bool,
 };
 
 Row.defaultProps = {
   className: '',
+  isClickable: false,
 };
 
 export default Row;
