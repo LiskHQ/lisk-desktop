7d5b0b46f3d5fa4d13176bbccd35e246b6a955b2
Vit Stanislav <vit@lightcurve.io>
2019-08-13 10:52:03 +0200

    :recycle: Update secondPassphrase registration to new Box

--- src/components/secondPassphrase/firstStep.js
+++ src/components/secondPassphrase/firstStep.js
@@ -1,7 +1,7 @@
 import { Trans } from 'react-i18next';
 import React from 'react';
 import { PrimaryButton, TertiaryButton } from '../toolbox/buttons/button';
-import Box from '../box';
+import Box from '../toolbox/box';
 import PassphraseBackup from '../passphraseBackup';
 import styles from './secondPassphrase.css';
 
@@ -9,29 +9,31 @@ const FirstStep = ({
   t, goBack, nextStep, account,
 }) => (
   <Box>
-    <header>
+    <Box.Header>
       <h2>{t('Register 2nd passphrase')}</h2>
-    </header>
-    <Trans>
-      <p className={styles.info}>
+    </Box.Header>
+    <Box.Content>
+      <Trans>
+        <p className={styles.info}>
         After registration, your second passphrase will be
-        <strong> required </strong>
+          <strong> required </strong>
         when
-        <strong> confirming every transaction </strong>
+          <strong> confirming every transaction </strong>
         and every vote.
         You are responsible for safekeeping your second passphrase.
         No one can restore it, not even Lisk.
         Once activated a second passphrase
-        <strong> can’t be turned off.</strong>
-      </p>
-    </Trans>
-    <PassphraseBackup
-      account={account}
-      t={t}
-      passphraseName={t('Second passphrase')}
-      paperWalletName="lisk_2nd_passphrase"
-    />
-    <footer className={`${styles.footer} summary-footer`}>
+          <strong> can’t be turned off.</strong>
+        </p>
+      </Trans>
+      <PassphraseBackup
+        account={account}
+        t={t}
+        passphraseName={t('Second passphrase')}
+        paperWalletName="lisk_2nd_passphrase"
+      />
+    </Box.Content>
+    <Box.Footer className="summary-footer">
       <PrimaryButton
         className={`${styles.confirmBtn} large go-to-confirmation`}
         onClick={nextStep}
@@ -44,7 +46,7 @@ const FirstStep = ({
       >
         {t('Go back')}
       </TertiaryButton>
-    </footer>
+    </Box.Footer>
   </Box>
 );
 
