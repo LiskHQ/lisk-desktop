356c80034da1e6b7500c4d2ce68b986f10a229a0
Vit Stanislav <vit@lightcurve.io>
2019-08-20 09:48:26 +0200

    :recycle: Split send form into token-specific sub-components

---
+++ src/components/send/form/formBtc.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FormBase from './formBase';
+
+export default function FormBtc(props) {
+  return (
+    <FormBase {...props} />
+  );
+}
---
+++ src/components/send/form/formLsk.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FormBase from './formBase';
+
+export default function FormLsk(props) {
+  return (
+    <FormBase {...props} />
+  );
+}
