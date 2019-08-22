994aefbfd92d9af4ec45b18ed120327193a6d895
Vit Stanislav <vit@lightcurve.io>
2019-08-22 11:23:50 +0200

    :recycle: Add state to autoSuggest demo

--- src/components/toolbox/autoSuggest/demo.js
+++ src/components/toolbox/autoSuggest/demo.js
@@ -2,32 +2,60 @@ import React from 'react';
 import AutoSuggest from '.';
 import DemoRenderer from '../demoRenderer';
 
-export default function AutoSuggestDemo() {
-  return (
-    <div>
-      <h2>AutoSuggest</h2>
-      <DemoRenderer>
-        <AutoSuggest
-          recipient={{
-            address: '124124L',
-            value: '',
-          }}
-          placeholder=""
-          token="LSK"
-          bookmarks={{
-            LSK: [{
-              title: '#1',
-              address: '214986124L',
-            }, {
-              title: '#2',
-              address: '7124124L',
-            }],
-          }}
-          validateBookmark={() => true}
-          onChange={() => {}}
-          onSelectedAccount={() => {}}
-        />
-      </DemoRenderer>
-    </div>
-  );
+export default class AutoSuggestDemo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      address: '',
+      value: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSelectedAccount = this.onSelectedAccount.bind(this);
+  }
+
+  onSelectedAccount(account) {
+    this.setState({
+      value: account.title,
+      address: account.address,
+    });
+  }
+
+  onChange({ target }) {
+    this.setState({
+      value: target.value,
+    });
+  }
+
+  render() {
+    const { value, address } = this.state;
+    return (
+      <div>
+        <h2>AutoSuggest</h2>
+        <DemoRenderer>
+          <AutoSuggest
+            recipient={{
+              address,
+              value,
+            }}
+            placeholder="Start typing..."
+            token="LSK"
+            bookmarks={{
+              LSK: [{
+                title: '#1',
+                address: '214986124L',
+              }, {
+                title: '#2',
+                address: '7124124L',
+              }],
+            }}
+            validateBookmark={() => true}
+            onChange={this.onChange}
+            onSelectedAccount={this.onSelectedAccount}
+          />
+        </DemoRenderer>
+      </div>
+    );
+  }
 }
