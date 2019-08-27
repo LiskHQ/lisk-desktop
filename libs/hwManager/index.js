6247052119703fe2c9264eca24cf6a3dc9528a5c
Renato Massao Yonamine <renato.massao@lightcurve.io>
2019-08-27 17:06:23 +0200

    :recycle: Improve hwManager lib

--- libs/hwManager/index.js
+++ libs/hwManager/index.js
@@ -1,95 +1,56 @@
 import manufactures from './manufactures';
 
-export const hwManager = () => ({
-  transports: {
-    ledger: null,
-    trezor: null,
-  },
+export class HwManager {
+  constructor() {
+    this.transports = {};
 
-  devices: [],
+    this.devices = [];
+  }
 
-  setTransport: ({ type = 'ledger', transport }) => { 
-    console.log('set', { type, transport });
-    console.log('set', this.transports);
-    this.transports[type] = transport;
-  },
-  getDevices: async (type = 'ledger') => { await this.transports[type].list(); },
-  listener: () => {
+  /**
+   * Set transport for specific type of wallet.
+   * @param {object} data -> Object with type and transport
+   * @param {string} data.name -> name of wallet brand. eg. ledger, trezor
+   * @param {any} data.transport -> Transport used to communicate with the wallets
+   */
+  setTransport({ name, transport }) {
+    // console.log('set', { type, transport });
+    this.transports[name] = transport;
+    // console.log('set', this.transports);
+  }
+
+  /**
+   * Remove a specific hwWallet from the manager
+   * @param {string} descriptor - Path of hWWallet that shoud be removed
+   */
+  removeDeviceWithPath(descriptor) {
+    const index = this.devices.findIndex(d => d.path === descriptor);
+    this.devices = [...this.devices.slice(0, index), ...this.devices.slice(index + 1)];
+  }
+
+  listener() {
     try {
-      this.transports.ledger.setListenDevicesPollingSkip(() => false);
-      this.transports.ledger.setListenDevicesDebounce(0);
       this.transports.ledger.listen({
-        next: ({ device, type }) => {
-          console.log({ device, type });
+        next: ({ type, ...args }) => {
+          // console.log({ args, type });
+          if (type === 'add') {
+            const device = {
+              id: Math.random() * 1e4 + 1,
+              path: args.descriptor,
+              ...args.deviceModel,
+            };
+            this.devices.push(device);
+          } else if (type === 'remove') {
+            const device = this.devices.findIndex(d => d.path === args.descriptor);
+            this.devices = [...this.devices.slice(0, device), ...this.devices.slice(device + 1)];
+          }
+          console.log(this.devices);
         },
       });
     } catch (e) {
       console.log({ e });
     }
-  },
-});
-
-const isDataValid = (data) => {
-  console.log('validating data => ', data);
-  return true;
-};
-
-const devicesListener = () => manufactures.devicesListener();
-
-const getConnectedDevices = () => manufactures.getConnectedDevices();
-
-const getDeviceById = ({ name, id }) => manufactures.getDeviceById(name, id);
-
-const getPublicKey = ({ name, id, data }) => {
-  if (isDataValid(data)) {
-    manufactures.getPublicKey({ name, id, data });
-  } else {
-    console.log('DATA INVALID');
   }
-};
+}
 
-const getAddress = ({ name, id, data }) => {
-  if (isDataValid(data)) {
-    manufactures.getAddress({ name, id, data });
-  } else {
-    console.log('DATA INVALID');
-  }
-};
-
-const getAccounts = ({ name, id, data }) => {
-  if (isDataValid(data)) {
-    manufactures.getAccounts({ name, id, data });
-  } else {
-    console.log('DATA INVALID');
-  }
-};
-
-const signInTransaction = ({ name, id, data }) => {
-  if (isDataValid(data)) {
-    manufactures.signInTransaction({ name, id, data });
-  } else {
-    console.log('DATA INVALID');
-  }
-};
-
-const signInMessage = ({ name, id, data }) => {
-  if (isDataValid(data)) {
-    manufactures.signInMessage({ name, id, data });
-  } else {
-    console.log('DATA INVALID');
-  }
-};
-
-const checkStatus = ({ name, id }) => manufactures.signInMessage({ name, id });
-
-export default {
-  checkStatus,
-  devicesListener,
-  getAccounts,
-  getAddress,
-  getConnectedDevices,
-  getDeviceById,
-  getPublicKey,
-  signInMessage,
-  signInTransaction,
-};
+export default new HwManager();
