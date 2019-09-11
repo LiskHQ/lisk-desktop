365e7d87175c0f6b552fe03abe875597cbc37b0b
Vit Stanislav <vit@lightcurve.io>
2019-09-11 17:10:40 +0200

    :seedling: Implement layout of verify message result

--- src/components/screens/verify-message/result.js
+++ src/components/screens/verify-message/result.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { PrimaryButton, TertiaryButton } from '../../toolbox/buttons/button';
+import Box from '../../toolbox/box';
 import Illustration from '../../toolbox/illustration';
 
 export default function Result({
@@ -10,10 +11,10 @@ export default function Result({
     <div>
       <h1>{isCorrect ? t('The signature is correct') : t('The signature is incorrect')}</h1>
       <Illustration name={isCorrect ? 'verifyMessageSuccess' : 'verifyMessageError'} />
-      <div>
+      <Box.Footer>
         <PrimaryButton onClick={finalCallback} className="go-to-dashboard">{t('Go to Dashboard')}</PrimaryButton>
         <TertiaryButton onClick={prevStep} className="go-back">{t('Go back')}</TertiaryButton>
-      </div>
+      </Box.Footer>
     </div>
   );
 }
