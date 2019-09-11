b8ca17d7f2cef3fdaeb489d7c561ea401f4ed75c
Vit Stanislav <vit@lightcurve.io>
2019-09-11 16:23:32 +0200

    :seedling: Add VerifyMessage illustrations

--- src/components/screens/verify-message/result.js
+++ src/components/screens/verify-message/result.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { PrimaryButton, TertiaryButton } from '../../toolbox/buttons/button';
+import Illustration from '../../toolbox/illustration';
 
 export default function Result({
   finalCallback, isCorrect, prevStep, t,
@@ -8,7 +9,7 @@ export default function Result({
   return (
     <div>
       <h1>{isCorrect ? t('The signature is correct') : t('The signature is incorrect')}</h1>
-      Result Illustration TODO
+      <Illustration name={isCorrect ? 'verifyMessageSuccess' : 'verifyMessageError'} />
       <div>
         <PrimaryButton onClick={finalCallback} className="go-to-dashboard">{t('Go to Dashboard')}</PrimaryButton>
         <TertiaryButton onClick={prevStep} className="go-back">{t('Go back')}</TertiaryButton>
