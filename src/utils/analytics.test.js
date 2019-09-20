1fb51fab95452588517e046b447fc18211b45460
Vit Stanislav <vit@lightcurve.io>
2019-09-20 16:47:12 +0200

    :white_check_mark: Create unit test for analytics alert after 7 days

--- src/utils/analytics.test.js
+++ src/utils/analytics.test.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import { mount } from 'enzyme';
 import PropTypes from 'prop-types';
+import React from 'react';
+import moment from 'moment';
+import { advanceBy, clear } from 'jest-date-mock';
+import { mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
-import i18n from '../i18n';
-import analyticsUtil from './analytics';
-import FlashMessageHolder from '../components/toolbox/flashMessage/holder';
 import DialogHolder from '../components/toolbox/dialog/holder';
+import FlashMessageHolder from '../components/toolbox/flashMessage/holder';
+import analyticsUtil from './analytics';
+import i18n from '../i18n';
 
 describe('Analytics Util', () => {
-  const props = {
-    settings: { statistics: false },
-    t: k => k,
-    settingsUpdated: jest.fn(),
-    toastDisplayed: jest.fn(),
-  };
-
-  const store = configureMockStore()({
-    settings: { statistics: false },
-    settingsUpdated: jest.fn(),
-    toastDisplayed: jest.fn(),
-  });
-
-  const options = {
-    context: { i18n, store },
-    childContextTypes: {
-      i18n: PropTypes.object.isRequired,
-      store: PropTypes.object.isRequired,
-    },
-  };
+  let props;
+  let store;
+  let options;
 
   beforeEach(() => {
+    props = {
+      settings: { statistics: false },
+      t: k => k,
+      settingsUpdated: jest.fn((data) => {
+        props.settings = {
+          ...props.settings,
+          ...data,
+        };
+      }),
+      toastDisplayed: jest.fn(),
+    };
+
+    store = configureMockStore()({
+      settings: { statistics: false },
+      settingsUpdated: props.settingsUpdated,
+      toastDisplayed: jest.fn(),
+    });
+
+    options = {
+      context: { i18n, store },
+      childContextTypes: {
+        i18n: PropTypes.object.isRequired,
+        store: PropTypes.object.isRequired,
+      },
+    };
+
     store.dispatch = jest.fn();
   });
 
   afterEach(() => {
     store.dispatch.mockRestore();
+    clear();
   });
 
   it('Should call FlashMessageHolder.addMessage', () => {
@@ -102,5 +114,32 @@ describe('Analytics Util', () => {
     wrapper.find('a.url-link').simulate('click');
     dialogWrapper.update();
     expect(dialogWrapper).toIncludeText('Anonymous Data Collection');
+  });
+
+  it('Should call FlashMessageHolder.addMessage 7 days after it was closed', () => {
+    const wrapper = mount(<FlashMessageHolder />, options);
+    const dialogWrapper = mount(<DialogHolder {...props} />, options);
+    expect(wrapper).toBeEmptyRender();
+    expect(dialogWrapper).toBeEmptyRender();
+    analyticsUtil.onTriggerPageLoaded({
+      settings: props.settings, settingsUpdated: props.settingsUpdated,
+    });
+    expect(props.settingsUpdated).toHaveBeenCalledWith({ statisticsRequest: true });
+    wrapper.update();
+    wrapper.find('a.url-link').simulate('click');
+    dialogWrapper.update();
+    expect(dialogWrapper).toIncludeText('Anonymous Data Collection');
+    dialogWrapper.find('button.cancel-button').simulate('click');
+    props.settings.statisticsFollowingDay = moment().format('YYYY-MM-DD');
+    wrapper.unmount();
+    wrapper.mount();
+
+    expect(wrapper).toBeEmptyRender();
+    analyticsUtil.checkIfAnalyticsShouldBeDisplayed({ settings: props.settings });
+    expect(wrapper).toBeEmptyRender();
+
+    advanceBy(8 * 24 * 60 * 60 * 1000);
+    analyticsUtil.checkIfAnalyticsShouldBeDisplayed({ settings: props.settings });
+    expect(wrapper).toIncludeText('Opt-in to sharing anonymous data in order to improve Lisk Hub.');
   });
 });
