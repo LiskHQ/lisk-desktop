f8db7cf6f13b5ddb875af346fd55d7a59d16514f
Vit Stanislav <vit@lightcurve.io>
2019-09-20 10:40:37 +0200

    :recycle: Migrate VerifyMessage to withTranslation

--- src/components/screens/verify-message/index.js
+++ src/components/screens/verify-message/index.js
@@ -1,10 +1,10 @@
 /* istanbul ignore file */
 import { compose } from 'redux';
-import { translate } from 'react-i18next';
+import { withTranslation } from 'react-i18next';
 import { withRouter } from 'react-router';
 import VerifyMessage from './verifyMessage';
 
 export default compose(
   withRouter,
-  translate(),
+  withTranslation(),
 )(VerifyMessage);
