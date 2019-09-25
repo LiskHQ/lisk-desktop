04cc217b6d0809fcfba35665a987559e5d3a9a09
Vit Stanislav <vit@lightcurve.io>
2019-09-25 17:41:54 +0200

    :bug: Fix i18n in electron app
    I forgot it in https://github.com/LiskHQ/lisk-hub/commit/e57a6f2c233c6b6683261ba73edd2bef530b6937

--- app/src/i18n.js
+++ app/src/i18n.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import i18n from 'i18next';
 import setup from '../../i18n/i18n-setup';
 
-const i18n = setup();
+setup();
 
 export default i18n;
