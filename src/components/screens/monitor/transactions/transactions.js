f5806c72049342140dc439c3faf7904985497bee
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-02 11:26:15 +0200

    ♻️ Make tx table render API response data

--- src/components/screens/monitor/transactions/transactions.js
+++ src/components/screens/monitor/transactions/transactions.js
@@ -3,16 +3,19 @@ import grid from 'flexboxgrid/dist/flexboxgrid.css';
 
 import TransactionsTable from '../../../shared/transactionsTable';
 import PageLayout from '../../../toolbox/pageLayout';
+import routes from '../../../../constants/routes';
 
 const Transactions = ({ t, transactions }) => {
   const columns = [
-    { header: 'ID', className: `${grid['col-md-2']} ${grid['col-xs-2']}` },
-    { header: 'Date', className: `${grid['col-md-2']} ${grid['col-xs-2']}` },
-    { header: 'Sender', className: `${grid['col-md-2']} ${grid['col-xs-2']}` },
-    { header: 'Recipient', className: `${grid['col-md-2']} ${grid['col-xs-2']}` },
-    { header: 'Amount', className: `${grid['col-md-2']} ${grid['col-xs-2']}` },
-    { header: 'Fee', className: `${grid['col-md-1']} ${grid['col-xs-1']}` },
-    { header: 'Status', className: `${grid['col-md-1']} ${grid['col-xs-1']}` },
+    {
+      header: 'ID', className: `${grid['col-md-2']} ${grid['col-xs-2']}`, key: 'id', isLink: true, pathPrefix: routes.transactions.path,
+    },
+    { header: 'Date', className: `${grid['col-md-2']} ${grid['col-xs-2']}`, key: 'timestamp' },
+    { header: 'Sender', className: `${grid['col-md-2']} ${grid['col-xs-2']}`, key: 'senderId' },
+    { header: 'Recipient', className: `${grid['col-md-2']} ${grid['col-xs-2']}`, key: 'recipientId' },
+    { header: 'Amount', className: `${grid['col-md-2']} ${grid['col-xs-2']}`, key: 'amount' },
+    { header: 'Fee', className: `${grid['col-md-1']} ${grid['col-xs-1']}`, key: 'fee' },
+    { header: 'Status', className: `${grid['col-md-1']} ${grid['col-xs-1']}`, key: 'type' },
   ];
 
   return (
--- src/components/shared/transactionsTable/index.js
+++ src/components/shared/transactionsTable/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import Box from '../../toolbox/box';
@@ -6,31 +7,46 @@ import TableRow from '../../toolbox/table/tableRow';
 
 const TransactionsTable = ({
   t, title, columns, transactions,
-}) => (
-  <Box width="full">
-    <Box.Header>
-      <h1>{t(title)}</h1>
-    </Box.Header>
-    <div>
-      {!!transactions.data.length && (
+}) => {
+  const renderCell = (column, transaction) => {
+    if (column.isLink) {
+      return (
+        <span key={column.key} className={column.className}>
+          <Link to={`${column.pathPrefix}/${transaction[column.key]}`}>
+            {transaction[column.key]}
+          </Link>
+        </span>
+      );
+    }
+    return (
+      <span key={column.key} className={column.className}>{transaction[column.key]}</span>
+    );
+  };
+
+
+  return (
+    <Box width="full">
+      <Box.Header>
+        <h1>{t(title)}</h1>
+      </Box.Header>
+      <div>
+        {!!transactions.data.data && transactions.data.data.length && (
         <React.Fragment>
           <TableRow isHeader>
             {columns.map(column => (
-              <div key={column.header} className={column.className}>{t(column.header)}</div>
+              <div key={column.key} className={column.className}>{t(column.header)}</div>
             ))}
           </TableRow>
-          {transactions.data.map(transaction => (
-            columns.map(column => (
-              <TableRow key={transaction.id} className={`${grid.row}`}>
-                <span className={column.className}>{column.header}</span>
-              </TableRow>
-            ))
+          {transactions.data.data.map(transaction => (
+            <TableRow key={transaction.id} className={`${grid.row}`}>
+              {columns.map(column => renderCell(column, transaction))}
+            </TableRow>
           ))}
         </React.Fragment>
-      )}
-    </div>
-    )
-  </Box>
-);
+        )}
+      </div>
+    </Box>
+  );
+};
 
 export default withTranslation()(TransactionsTable);
