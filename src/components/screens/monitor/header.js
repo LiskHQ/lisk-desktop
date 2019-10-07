c43b06dbe93af82b41e7d193006bfcf6bef57ba8
Vit Stanislav <vit@lightcurve.io>
2019-10-07 13:57:24 +0200

    :books: Note that monitor header will need change

--- src/components/screens/monitor/header.js
+++ src/components/screens/monitor/header.js
@@ -16,7 +16,7 @@ export const Header = ({ t, history }) => {
       />
       <Switcher
         options={[{
-          value: routes.dashboard.path,
+          value: routes.dashboard.path, // TODO change to monitorTransactions when available
           name: t('Transactions'),
           className: 'transactions',
         }, {
