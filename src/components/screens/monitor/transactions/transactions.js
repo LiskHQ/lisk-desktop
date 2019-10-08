d06fde9788f88af294d39cb1efbeb8f3f0b05d6f
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-08 17:19:46 +0200

    ♻️ Add filter fields to monitor transactions

--- src/components/screens/monitor/transactions/transactions.js
+++ src/components/screens/monitor/transactions/transactions.js
@@ -5,6 +5,40 @@ import TransactionsTable from '../../../shared/transactionsTable';
 import PageLayout from '../../../toolbox/pageLayout';
 
 const Transactions = ({ t, transactions }) => {
+  const filters = [{
+    label: t('Date'),
+    name: 'date',
+    type: 'date-range',
+  }, {
+    label: t('Amount Range'),
+    name: 'amount',
+    type: 'number-range',
+  }, {
+    label: t('Sender'),
+    name: 'sender',
+    type: 'text',
+  }, {
+    label: t('Message'),
+    placeholder: t('Your message'),
+    name: 'message',
+    type: 'text',
+  }, {
+    label: t('Recipient'),
+    placeholder: t('Address or Public key'),
+    name: 'message',
+    type: 'text',
+  }, {
+    label: t('Type'),
+    placeholder: t('Eg. 1,5'),
+    name: 'type',
+    type: 'text',
+  }, {
+    label: t('Height'),
+    placeholder: t('Eg. 10180477'),
+    name: 'height',
+    type: 'text',
+  }];
+
   const columns = [
     { header: t('Sender'), className: `${grid['col-xs-3']}`, key: 'senderId' },
     { header: t('Recipient'), className: `${grid['col-xs-3']}`, key: 'recipientId' },
@@ -20,7 +54,7 @@ const Transactions = ({ t, transactions }) => {
 
   return (
     <PageLayout>
-      <TransactionsTable isLoadMoreEnabled columns={columns} title={t('All transactions')} transactions={transactions} />
+      <TransactionsTable isLoadMoreEnabled filters={filters} isFilterDropdownExpandable columns={columns} title={t('All transactions')} transactions={transactions} />
     </PageLayout>
   );
 };
