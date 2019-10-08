380fab397f28627b960845abe89cd692b1feffc6
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-08 15:14:18 +0200

    ♻️ Add translation to header

--- src/components/screens/monitor/transactions/transactions.js
+++ src/components/screens/monitor/transactions/transactions.js
@@ -6,16 +6,16 @@ import PageLayout from '../../../toolbox/pageLayout';
 
 const Transactions = ({ t, transactions }) => {
   const columns = [
-    { header: 'Sender', className: `${grid['col-xs-3']}`, key: 'senderId' },
-    { header: 'Recipient', className: `${grid['col-xs-3']}`, key: 'recipientId' },
+    { header: t('Sender'), className: `${grid['col-xs-3']}`, key: 'senderId' },
+    { header: t('Recipient'), className: `${grid['col-xs-3']}`, key: 'recipientId' },
     {
-      header: 'Date', className: `${grid['col-xs-2']}`, key: 'timestamp', isSortingColumn: true, defaultSort: true,
+      header: t('Date'), className: `${grid['col-xs-2']}`, key: 'timestamp', isSortingColumn: true, defaultSort: true,
     },
     {
-      header: 'Amount', className: `${grid['col-xs-2']}`, key: 'amount', isSortingColumn: true,
+      header: t('Amount'), className: `${grid['col-xs-2']}`, key: 'amount', isSortingColumn: true,
     },
-    { header: 'Fee', className: `${grid['col-xs-1']}  hidden-m`, key: 'fee' },
-    { header: 'Status', className: `${grid['col-xs-1']}`, key: 'confirmations' },
+    { header: t('Fee'), className: `${grid['col-xs-1']}  hidden-m`, key: 'fee' },
+    { header: t('Status'), className: `${grid['col-xs-1']}`, key: 'confirmations' },
   ];
 
   return (
