59d181039bfd81d09f168baf1f32532ea29bf099
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-09 18:16:39 +0200

    ♻️Remove unnecessary param from API function

--- src/utils/api/lsk/liskService.js
+++ src/utils/api/lsk/liskService.js
@@ -63,13 +63,12 @@ const liskServiceApi = {
     path: `/api/v1/block/${id}`,
   }),
 
-  getLastTransactions: async ({ networkConfig }, searchParams) => liskServiceGet({
+  getLastTransactions: async ({ networkConfig }) => liskServiceGet({
     serverUrl: getServerUrl(networkConfig),
     path: '/api/v1/transactions/last',
     transformResponse: response => response.data,
     searchParams: {
       limit: 20,
-      ...searchParams,
     },
   }),
 };
