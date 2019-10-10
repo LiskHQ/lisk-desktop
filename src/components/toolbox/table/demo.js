840868f0660dca8efbaf7d073a3fb66dcdb1c599
Vit Stanislav <vit@lightcurve.io>
2019-10-10 10:59:38 +0200

    :seedling: Add sorting to toolbox Table component

--- src/components/toolbox/table/demo.js
+++ src/components/toolbox/table/demo.js
@@ -4,11 +4,16 @@ import CheckBox from '../checkBox';
 import DemoRenderer from '../demoRenderer';
 import Table from '.';
 
+/* eslint-disable-next-line no-console */
+const onSortChange = console.log;
+
 const TableDemo = () => (
   <div>
     <h2>Table</h2>
     <DemoRenderer>
       <Table
+        sort="id:asc"
+        onSortChange={onSortChange}
         data={[
           { id: 1, name: 'Row 1' },
           { id: 2, name: 'Row 2' },
@@ -18,9 +23,9 @@ const TableDemo = () => (
           /* eslint-disable react/display-name */
           className: grid['col-xs-1'], header: '', id: 'checkbox', getValue: () => <CheckBox />,
         }, {
-          className: grid['col-xs-2'], header: 'ID', id: 'id',
+          className: grid['col-xs-2'], header: 'ID', id: 'id', isSortable: true,
         }, {
-          className: grid['col-xs-6'], header: 'Name', id: 'name', getValue: row => <i>{row.name}</i>,
+          className: grid['col-xs-6'], header: 'Name', id: 'name', getValue: row => <i>{row.name}</i>, isSortable: true,
         }, {
           className: grid['col-xs-3'], header: 'Other', id: 'other', getValue: () => 'Something static',
           /* eslint-enable react/display-name */
--- src/components/toolbox/table/index.js
+++ src/components/toolbox/table/index.js
@@ -1,26 +1,66 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import TableRow from './tableRow';
+import styles from './table.css';
 
-const Table = ({ data, columns }) => (
-  <React.Fragment>
-    {!!data.length && (
-    <TableRow isHeader className={`${grid.row}`}>
-      {columns.map(({ className, header, id }) => (
-        <div className={className} key={id}>{header}</div>
-      ))}
-    </TableRow>
-    )}
-    {data.map(row => (
-      <TableRow key={row.id} className={`${grid.row}`}>
-        {columns.map(({ className, id, getValue }) => (
-          <span className={className} key={id}>
-            {typeof getValue === 'function' ? getValue(row) : row[id]}
-          </span>
+const Table = ({
+  data, columns, onSortChange, sort,
+}) => {
+  const onHeaderClick = ({ id, isSortable }) => {
+    if (isSortable) {
+      onSortChange(id);
+    }
+  };
+
+  const getSortClass = ({ id, isSortable }) => (
+    isSortable && ([
+      sort.includes(id) ? (
+        styles[sort.includes('asc') ? 'sortAsc' : 'sortDesc']
+      ) : styles.sortable,
+      'sort-by',
+      id,
+    ].join(' '))
+  );
+
+  return (
+    <React.Fragment>
+      {!!data.length && (
+      <TableRow isHeader className={grid.row}>
+        {columns.map(({
+          className, header, id, isSortable,
+        }) => (
+          <div key={id} className={className} onClick={() => onHeaderClick({ id, isSortable })}>
+            <span className={getSortClass({ id, isSortable })}>
+              {header}
+            </span>
+          </div>
         ))}
       </TableRow>
-    ))}
-  </React.Fragment>
-);
+      )}
+      {data.map(row => (
+        <TableRow key={row.id} className={`${grid.row}`}>
+          {columns.map(({ className, id, getValue }) => (
+            <span className={className} key={id}>
+              {typeof getValue === 'function' ? getValue(row) : row[id]}
+            </span>
+          ))}
+        </TableRow>
+      ))}
+    </React.Fragment>
+  );
+};
+
+Table.propTypes = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired,
+  onSortChange: PropTypes.func,
+  sort: PropTypes.string,
+};
+
+Table.defaultProps = {
+  onSortChange: () => null,
+  sort: '',
+};
 
 export default Table;
