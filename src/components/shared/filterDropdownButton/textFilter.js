a77055d961f3718e9aca586b871088dbe20f57c9
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-10 11:22:08 +0200

    ♻️ Restrict height field to numbers

--- src/components/shared/filterDropdownButton/textFilter.js
+++ src/components/shared/filterDropdownButton/textFilter.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Input } from '../../toolbox/inputs';
 
-const TextFilter = ({
-  filters, label, name, placeholder, updateCustomFilters, valueFormatter,
-}) => {
-  const onChange = ({ target }) => {
+class TextFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange({ target }) {
+    const { name, updateCustomFilters, valueFormatter } = this.props;
+
+    if (name === 'height') {
+      let value = /^\./.test(target.value) ? `0${target.value}` : target.value;
+      value = value.replace(/[^\d.]/g, '');
+
+      this.setState({ value });
+    } else {
+      this.setState({ value: target.value });
+    }
+
     updateCustomFilters({
       [name]: {
         value: valueFormatter(target.value),
@@ -12,19 +30,26 @@ const TextFilter = ({
         loading: false,
       },
     });
-  };
-  const inputProps = {
-    label, name, placeholder, onChange,
-  };
-  return (
-    <Input
-      {...inputProps}
-      value={filters[name]}
-      className={name}
-      size="xs"
-    />
-  );
-};
+  }
+
+  render() {
+    const {
+      label, name, placeholder,
+    } = this.props;
+    const inputProps = {
+      label, name, placeholder,
+    };
+    return (
+      <Input
+        {...inputProps}
+        onChange={this.onChange}
+        value={this.state.value}
+        className={name}
+        size="xs"
+      />
+    );
+  }
+}
 
 TextFilter.defaultProps = {
   valueFormatter: value => value,
