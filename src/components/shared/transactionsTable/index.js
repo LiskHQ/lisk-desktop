3fa709e9b3e3315b7fc51dfc016d0484d7f4c3f2
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-10 14:20:42 +0200

    ♻️ Apply toolbox table comp to TransactionsTable

--- src/components/shared/transactionsTable/index.js
+++ src/components/shared/transactionsTable/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import { DateTimeFromTimestamp } from '../../toolbox/timestamp';
@@ -9,110 +8,22 @@ import Icon from '../../toolbox/icon';
 import IconlessTooltip from '../iconlessTooltip';
 import LiskAmount from '../liskAmount';
 import regex from '../../../utils/regex';
-import routes from '../../../constants/routes';
 import Illustration from '../../toolbox/illustration';
 import styles from './transactionsTable.css';
-import TableRow from '../../toolbox/table/tableRow';
 import FilterDropdownButton from '../filterDropdownButton';
 import withResizeValues from '../../../utils/withResizeValues';
 import withFilters from '../../../utils/withFilters';
 import FilterBar from '../filterBar';
+import Table from '../../toolbox/table';
 
 class TransactionsTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       sortingColumn: props.columns.find(column => column.defaultSort).key,
-      ascendingSorting: true,
     };
 
-    this.changeSorting = this.changeSorting.bind(this);
-    this.renderCellContent = this.renderCellContent.bind(this);
-    this.renderTransactions = this.renderTransactions.bind(this);
     this.handleLoadMore = this.handleLoadMore.bind(this);
-  }
-
-
-  renderCellContent(column, transaction) {
-    const { t, isMediumViewPort } = this.props;
-
-    switch (column.key) {
-      case 'senderId':
-      case 'recipientId':
-        return (
-          <div className={`${styles.address}`}>
-            <AccountVisual address={transaction[column.key]} size={32} />
-            <span className={`${styles.addressValue}`}>
-              {isMediumViewPort
-                ? transaction[column.key].replace(regex.lskAddressTrunk, '$1...$3')
-                : transaction[column.key]}
-            </span>
-          </div>
-        );
-      case 'amount':
-        return (
-          <React.Fragment>
-            <LiskAmount val={transaction[column.key]} />
-&nbsp;
-            {t('LSK')}
-          </React.Fragment>
-        );
-      case 'fee':
-        return (
-          <IconlessTooltip
-            tooltipContent={<p>{`${t('Type')} ${transaction.type}`}</p>}
-            title={t('Transaction')}
-            className="showOnBottom"
-            tooltipClassName={styles.tooltip}
-          >
-            <div>
-              <LiskAmount val={transaction[column.key]} />
-&nbsp;
-              {t('LSK')}
-            </div>
-          </IconlessTooltip>
-        );
-      case 'timestamp':
-        return <DateTimeFromTimestamp time={transaction[column.key] * 1000} token="BTC" />;
-      case 'confirmations':
-        return (
-          <IconlessTooltip
-            tooltipContent={<p>{`${transaction.confirmations}/101 ${t('Confirmations')}`}</p>}
-            title={transaction.confirmations > 0 ? t('Confirmed') : t('Pending')}
-            className="showOnLeft"
-            tooltipClassName={styles.tooltip}
-          >
-            {transaction.confirmations > 0 ? <Icon name="approved" /> : <Icon name="pending" />}
-          </IconlessTooltip>
-        );
-      default:
-        return transaction[column.key];
-    }
-  }
-
-  // TODO add test when sorting is enabled
-  // istanbul ignore next
-  changeSorting(column) {
-    if (column.isSortingColumn) {
-    // Only changes the caret.
-    // TODO: Implementation of sorting functionality to be done in a separate ticket.
-      this.setState({
-        sortingColumn: column.key,
-        ascendingSorting:
-        column.key === this.state.sortingColumn ? !this.state.ascendingSorting : true,
-      });
-    }
-  }
-
-  // TODO add test when sorting is enabled
-  // istanbul ignore next
-  renderTransactions() {
-    const { sortingColumn } = this.state;
-
-    return this.props.transactions.data.sort((a, b) =>
-      (this.state.ascendingSorting
-        ? b[sortingColumn] - a[sortingColumn]
-        : a[sortingColumn] - b[sortingColumn]));
   }
 
   handleLoadMore() {
@@ -128,10 +39,9 @@ class TransactionsTable extends React.Component {
 
   render() {
     const {
-      title, transactions, columns, isLoadMoreEnabled, t, fields, filters,
-      emptyStateMessage, applyFilters, clearFilter, clearAllFilters,
+      title, transactions, isLoadMoreEnabled, t, fields, filters,
+      emptyStateMessage, applyFilters, clearFilter, clearAllFilters, isMediumViewPort,
     } = this.props;
-    const { ascendingSorting } = this.state;
 
     return (
       <Box width="full" isLoading={transactions.isLoading} className="transactions-box">
@@ -158,53 +68,82 @@ class TransactionsTable extends React.Component {
           )
           : (
             <React.Fragment>
-              {!!transactions.data.length && (
-              <React.Fragment>
-                <TableRow isHeader>
-                  {columns.map(column => (
-                    <div
-                      onClick={
-                    /* TODO add test when sorting is enabled */
-                    /* istanbul ignore next */
-                    () => this.changeSorting(column)
-                  }
-                      key={column.key}
-                      className={`${column.className} ${
-                        column.isSortingColumn ? styles.sortingColumn : ''
-                      }`}
-                    >
-                      {t(column.header)}
-                      {column.isSortingColumn && this.state.sortingColumn === column.key && (
-                      <div
-                        style={{ display: 'none' } /* TODO remove this line when enabling sorting */}
-                        className={[
-                          styles.arrow,
-                          // TODO add test when sorting is enabled
-                          // istanbul ignore next
-                          ascendingSorting ? styles.arrowUp : styles.arrowDown,
-                        ].join(' ')}
-                      />
-                      )}
-                    </div>
-                  ))}
-                </TableRow>
-                {transactions.data.map(transaction => (
-                  <Link
-                    key={transaction.id}
-                    to={`${routes.transactions.path}/${transaction.id}`}
-                    className={styles.rowLink}
-                  >
-                    <TableRow className={[grid.row, styles.tableRow, 'row'].join(' ')}>
-                      {columns.map(column => (
-                        <span key={column.key} className={column.className}>
-                          {this.renderCellContent(column, transaction)}
+              <Box.Content className={styles.content}>
+                <Table
+                  data={transactions.data}
+                  columns={[{
+                    header: t('Sender'),
+                    className: grid['col-xs-3'],
+                    getValue: transaction => (
+                      <div className={`${styles.address}`}>
+                        <AccountVisual address={transaction.senderId} size={32} />
+                        <span className={`${styles.addressValue}`}>
+                          {isMediumViewPort
+                            ? transaction.senderId.replace(regex.lskAddressTrunk, '$1...$3')
+                            : transaction.senderId}
                         </span>
-                      ))}
-                    </TableRow>
-                  </Link>
-                ))}
-              </React.Fragment>
-              )}
+                      </div>
+                    ),
+                  }, {
+                    header: t('Recipient'),
+                    className: grid['col-xs-3'],
+                    getValue: transaction => (
+                      <div className={`${styles.address}`}>
+                        <AccountVisual address={transaction.recipientId} size={32} />
+                        <span className={`${styles.addressValue}`}>
+                          {isMediumViewPort
+                            ? transaction.recipientId.replace(regex.lskAddressTrunk, '$1...$3')
+                            : transaction.recipientId}
+                        </span>
+                      </div>
+                    ),
+                  }, {
+                    header: t('Date'),
+                    className: grid['col-xs-2'],
+                    getValue: transaction => <DateTimeFromTimestamp time={transaction.timestamp * 1000} token="BTC" />,
+                  }, {
+                    header: t('Amount'),
+                    className: grid['col-xs-2'],
+                    getValue: transaction => (
+                      <React.Fragment>
+                        <LiskAmount val={transaction.timestamp} />
+            &nbsp;
+                        {t('LSK')}
+                      </React.Fragment>
+                    ),
+                  }, {
+                    header: t('Fee'),
+                    className: grid['col-xs-1'],
+                    getValue: transaction => (
+                      <IconlessTooltip
+                        tooltipContent={<p>{`${t('Type')} ${transaction.type}`}</p>}
+                        title={t('Transaction')}
+                        className="showOnBottom"
+                        tooltipClassName={styles.tooltip}
+                      >
+                        <div>
+                          <LiskAmount val={transaction.fee} />
+            &nbsp;
+                          {t('LSK')}
+                        </div>
+                      </IconlessTooltip>
+                    ),
+                  }, {
+                    header: t('Status'),
+                    className: grid['col-xs-1'],
+                    getValue: transaction => (
+                      <IconlessTooltip
+                        tooltipContent={<p>{`${transaction.confirmations}/101 ${t('Confirmations')}`}</p>}
+                        title={transaction.confirmations > 0 ? t('Confirmed') : t('Pending')}
+                        className="showOnLeft"
+                        tooltipClassName={styles.tooltip}
+                      >
+                        {transaction.confirmations > 0 ? <Icon name="approved" /> : <Icon name="pending" />}
+                      </IconlessTooltip>
+                    ),
+                  }]}
+                />
+              </Box.Content>
               {isLoadMoreEnabled && (
               <Box.FooterButton
                 className="load-more"
