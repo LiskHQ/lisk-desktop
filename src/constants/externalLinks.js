ce678c4d47a49cd2eeadd961c6c3ee963467ff87
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-10-14 17:14:00 +0200

    :recycle: Removed lisk chat link mentions

--- src/constants/externalLinks.js
+++ src/constants/externalLinks.js
@@ -1,7 +1,6 @@
 const links = {
   discord: 'https://discord.gg/CngsY6D',
   helpDeskChannel: 'https://lisk.chat/channel/help-desk',
-  liskChat: 'https://lisk.chat/home',
   privacyPolicy: 'https://lisk.io/privacy',
   trezorOneHelp: 'https://wiki.trezor.io/PIN',
 };
