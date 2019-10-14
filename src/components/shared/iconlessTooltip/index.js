02818a88f170d683686abaf61e9dee44cd9637bf
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-14 16:09:33 +0200

    ♻️ Increase tooltip offset is shown horizontally

--- src/components/shared/iconlessTooltip/index.js
+++ src/components/shared/iconlessTooltip/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Tooltip from '../../toolbox/tooltip/tooltip';
+import styles from './iconlessTooltip.css';
 
 const IconlessTooltip = ({
   children, title, tooltipContent, className, tooltipClassName,
-}) => (
-  <Tooltip
-    content={children}
-    title={title}
-    className={className}
-    tooltipClassName={tooltipClassName}
-  >
-    {tooltipContent}
-  </Tooltip>
-);
+}) => {
+  const tooltipClassNames = [
+    tooltipClassName,
+    className.includes('showOnLeft') && styles.showOnLeft,
+    className.includes('showOnRight') && styles.showOnRight,
+  ].join(' ');
+
+  return (
+    <Tooltip
+      content={children}
+      title={title}
+      className={className}
+      tooltipClassName={tooltipClassNames}
+    >
+      {tooltipContent}
+    </Tooltip>
+  );
+};
 
 export default IconlessTooltip;
