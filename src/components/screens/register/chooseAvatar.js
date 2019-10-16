dd5daeb920efa0ddce0707d41a2f898b3bd9dab3
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-16 11:32:06 +0200

    ♻️ Change header text of avatar selection

--- src/components/screens/register/chooseAvatar.js
+++ src/components/screens/register/chooseAvatar.js
@@ -67,7 +67,7 @@ class ChooseAvatar extends React.Component {
           <h1 className={styles.title}>
             {t('Choose your avatar')}
           </h1>
-          <p className={styles.text}>{t('Each avatar is a unique visual representation of your Lisk address.')}</p>
+          <p>{t('This avatar will then be linked to your new Lisk address')}</p>
         </div>
         <div
           ref={this.setWrapperRef}
