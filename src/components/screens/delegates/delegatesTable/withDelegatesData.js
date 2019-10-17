5fbfa631a0ee418208382324430c843ce1da55a4
Vit Stanislav <vit@lightcurve.io>
2019-10-17 08:30:05 +0200

    ♻️  Improve readability of withDelegatesData HOC

--- src/components/screens/delegates/delegatesTable/withDelegatesData.js
+++ src/components/screens/delegates/delegatesTable/withDelegatesData.js
@@ -2,93 +2,89 @@ import React from 'react';
 import { getPendingVotesList } from '../../../../utils/voting';
 import voteFilters from '../../../../constants/voteFilters';
 
-function withDelegatesData() {
-  return function (ChildComponent) {
-    class DelegatesContainer extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          isLoading: false,
-          filters: {
-            tab: voteFilters.all,
-            search: '',
-          },
-        };
+const withDelegatesData = () => (ChildComponent) => {
+  class DelegatesContainer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoading: false,
+        filters: {
+          tab: voteFilters.all,
+          search: '',
+        },
+      };
 
-        this.loadDelegates = this.loadDelegates.bind(this);
-        this.loadDelegatesFinished = this.loadDelegatesFinished.bind(this);
-        this.applyFilters = this.applyFilters.bind(this);
+      this.loadDelegates = this.loadDelegates.bind(this);
+      this.loadDelegatesFinished = this.loadDelegatesFinished.bind(this);
+      this.applyFilters = this.applyFilters.bind(this);
+    }
+
+    componentDidMount() {
+      const { account: { serverPublicKey, address }, votes, loadVotes } = this.props;
+      if (serverPublicKey && getPendingVotesList(votes).length === 0) {
+        loadVotes({ address });
       }
+      this.loadDelegates({});
+    }
 
-      componentDidMount() {
-        const { account, votes } = this.props;
-        if (account.serverPublicKey && getPendingVotesList(votes).length === 0) {
-          this.props.loadVotes({
-            address: account.address,
-          });
-        }
-        this.loadDelegates({});
-      }
+    getDelegatesData() {
+      const tabFilters = {
+        [voteFilters.all]: () => true,
+        [voteFilters.voted]: ({ voteStatus }) => voteStatus && voteStatus.confirmed,
+        [voteFilters.notVoted]: ({ voteStatus }) => !voteStatus || !voteStatus.confirmed,
+      };
+      return this.props.delegates.map(delegate => ({
+        ...delegate,
+        voteStatus: this.props.votes[delegate.username],
+      })).filter(tabFilters[this.state.filters.tab]);
+    }
 
-      getDelegatesData() {
-        const tabFilters = {
-          [voteFilters.all]: () => true,
-          [voteFilters.voted]: ({ voteStatus }) => voteStatus && voteStatus.confirmed,
-          [voteFilters.notVoted]: ({ voteStatus }) => !voteStatus || !voteStatus.confirmed,
-        };
-        return this.props.delegates.map(d => ({
-          ...d,
-          voteStatus: this.props.votes[d.username],
-        })).filter(tabFilters[this.state.filters.tab]);
-      }
-
-      loadDelegates({ q = '', offset = 0 }) {
-        this.setState({ isLoading: true }, () => {
-          this.props.loadDelegates({
-            offset,
-            q,
-            refresh: offset === 0,
-            callback: this.loadDelegatesFinished,
-          });
+    loadDelegates({ q = '', offset = 0 }) {
+      this.setState({ isLoading: true }, () => {
+        this.props.loadDelegates({
+          offset,
+          q,
+          refresh: offset === 0,
+          callback: this.loadDelegatesFinished,
         });
-      }
+      });
+    }
 
-      loadDelegatesFinished() {
-        if (this.state.isLoading) {
-          this.setState({ isLoading: false });
-        }
-      }
-
-      applyFilters(filters) {
-        this.setState({
-          filters: {
-            ...this.state.filters,
-            ...filters,
-          },
-        }, () => {
-          this.loadDelegates({ q: this.state.filters.search });
-        });
-      }
-
-      render() {
-        return (
-          <ChildComponent {...{
-            ...this.props,
-            delegates: {
-              data: this.getDelegatesData(),
-              loadData: this.loadDelegates,
-              isLoading: this.state.isLoading,
-            },
-            applyFilters: this.applyFilters,
-            filters: this.state.filters,
-          }}
-          />
-        );
+    loadDelegatesFinished() {
+      if (this.state.isLoading) {
+        this.setState({ isLoading: false });
       }
     }
 
-    return DelegatesContainer;
-  };
-}
+    applyFilters(filters) {
+      this.setState({
+        filters: {
+          ...this.state.filters,
+          ...filters,
+        },
+      }, () => {
+        this.loadDelegates({ q: this.state.filters.search });
+      });
+    }
+
+    render() {
+      return (
+        <ChildComponent {...{
+          ...this.props,
+          delegates: {
+            data: this.getDelegatesData(),
+            loadData: this.loadDelegates,
+            isLoading: this.state.isLoading,
+          },
+          applyFilters: this.applyFilters,
+          filters: this.state.filters,
+        }}
+        />
+      );
+    }
+  }
+
+  return DelegatesContainer;
+};
 
 export default withDelegatesData;
