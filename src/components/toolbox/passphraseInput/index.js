01636be2644014e8321c59b58011240bbb540b6e
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-10-18 16:36:28 +0200

    :nail_care: Update passphrase input css file

--- src/components/toolbox/passphraseInput/index.js
+++ src/components/toolbox/passphraseInput/index.js
@@ -153,7 +153,7 @@ class passphraseInput extends React.Component {
           >
             {
               [...Array(inputsLength)].map((x, i) => (
-                <span key={i}>
+                <span key={i} className={styles.inputContainer}>
                   <span className={[
                     styles.inputNumber,
                     partialPassphraseError[i] ? styles.inputNumberError : '',
