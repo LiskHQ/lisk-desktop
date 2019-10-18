b503d532f9ff2643f8967699c072e990179b2938
Vit Stanislav <vit@lightcurve.io>
2019-10-18 15:40:11 +0200

    ✅ Move mount to setup function in monitor/delegates.test.js

--- src/components/screens/monitor/delegates/delegates.test.js
+++ src/components/screens/monitor/delegates/delegates.test.js
@@ -14,9 +14,11 @@ jest.mock('../../../../constants/monitor', () => ({ DEFAULT_LIMIT: 8 }));
 describe('Delegates monitor page', () => {
   let props;
   let delegatesWithData;
+  let wrapper;
   const name = '1234';
+  const setup = properties => mount(<Delegates {...properties} />);
 
-  const switchTab = (wrapper, tab) => {
+  const switchTab = (tab) => {
     wrapper.find(`.tab.${tab}`).simulate('click');
     wrapper.setProps({
       filters: {
@@ -47,30 +49,27 @@ describe('Delegates monitor page', () => {
       isLoading: false,
       data: delegatesApiResponse,
     };
+    wrapper = setup(props);
   });
 
   it('renders a page with header', () => {
-    const wrapper = mount(<Delegates {...props} />);
     expect(wrapper.find('Box header')).toIncludeText('Active delegates');
   });
 
   it('renders table with delegates', () => {
-    const wrapper = mount(<Delegates {...props} />);
     expect(wrapper.find('.delegate-row')).toHaveLength(0);
     wrapper.setProps({ delegates: delegatesWithData });
     expect(wrapper.find('.delegate-row').hostNodes()).toHaveLength(delegatesApiResponse.length);
   });
 
   it('allows to switch to "Standby delegates" tab', () => {
-    const wrapper = mount(<Delegates {...{ ...props }} />);
     expect(wrapper.find('.tab.standby')).not.toHaveClassName('active');
-    switchTab(wrapper, 'standby');
+    switchTab('standby');
     expect(wrapper.find('.tab.standby')).toHaveClassName('active');
   });
 
   it('shows error if API failed', () => {
     const error = 'Loading failed';
-    const wrapper = mount(<Delegates {...props} />);
     wrapper.setProps({
       delegates: {
         ...props.delegates,
@@ -82,7 +81,6 @@ describe('Delegates monitor page', () => {
   });
 
   it('allows to filter delegates by name and clear the filter', () => {
-    const wrapper = mount(<Delegates {...props} />);
     wrapper.find('input.filter-by-name').simulate('change', { target: { value: name } });
     expect(props.applyFilters).toHaveBeenCalledWith({ search: name, tab: props.filters.tab });
     wrapper.find('input.filter-by-name').simulate('change', { target: { value: '' } });
@@ -90,15 +88,15 @@ describe('Delegates monitor page', () => {
   });
 
   it('cannot load more active delegates', () => {
-    const wrapper = mount(<Delegates {...{ ...props, delegates: delegatesWithData }} />);
+    wrapper = setup({ ...props, delegates: delegatesWithData });
     expect(wrapper.find('.tab.active')).toHaveClassName('active');
     expect(wrapper.find('button.loadMore')).toHaveLength(0);
   });
 
   it('allows to load more standby delegates', () => {
     const tab = 'standby';
-    const wrapper = mount(<Delegates {...{ ...props, delegates: delegatesWithData }} />);
-    switchTab(wrapper, tab);
+    wrapper = setup({ ...props, delegates: delegatesWithData });
+    switchTab(tab);
     wrapper.find('button.loadMore').simulate('click');
     expect(props.delegates.loadData).toHaveBeenCalledWith({
       offset: delegatesApiResponse.length, tab,
