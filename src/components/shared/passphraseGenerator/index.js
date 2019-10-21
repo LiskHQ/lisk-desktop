16354f50ab2e0df565701a77741f7a8e98f983fd
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-21 12:05:51 +0200

    ♻️ Add logic for passphrase confirmation

--- src/components/shared/passphraseGenerator/index.js
+++ src/components/shared/passphraseGenerator/index.js
@@ -3,26 +3,72 @@ import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import styles from './passphraseGenerator.css';
 
 class PassphraseGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fieldSelected: undefined,
+      displayOptions: undefined,
+      chosenWords: {},
+    };
+  }
+
+  getStyle(i) {
+    const { missingWords, isConfirmation } = this.props;
+    const { fieldSelected, chosenWords } = this.state;
+
+    if (!missingWords) return styles.default;
+    if (chosenWords[i]) return styles.selected;
+    if (missingWords.includes(i) && fieldSelected === i) return styles.emptyInputFocused;
+    if (missingWords.includes(i) && fieldSelected !== i) return styles.emptyInput;
+    if (isConfirmation) return styles.disabled;
+    return styles.default;
+  }
+
+  handleClick(index) {
+    this.setState({ fieldSelected: index, displayOptions: true });
+  }
+
+  renderMissingValue(i) {
+    return this.state.chosenWords[i] || '_________';
+  }
+
   render() {
-    const { values } = this.props;
+    const {
+      values, options, missingWords,
+    } = this.props;
+
+    const { fieldSelected } = this.state;
+
     return (
-      <div className={styles.container}>
-        <div className={`${styles.inputsRow} ${grid.row} passphrase`}>
-          {values.slice(0, 6).map((value, i) => (
-            <React.Fragment key={i}>
-              <span className={`${grid['col-xs-2']} ${styles.inputValue}`}>{value}</span>
-              <span className={styles.whitespace}>&nbsp;</span>
-            </React.Fragment>
-          ))}
+      <div>
+        <div className={styles.passphraseContainer}>
+          <div className={`${styles.inputsRow} ${grid.row} passphrase`}>
+            {values.map((value, i) => (
+              <div onClick={() => this.handleClick(i)} className={`${grid['col-xs-2']} ${styles.inputContainer}`} key={i}>
+                <span className={`${styles.inputValue} ${this.getStyle(i)}`}>{missingWords && missingWords.includes(i) ? this.renderMissingValue(i) : value}</span>
+                <span className={styles.whitespace}>&nbsp;</span>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className={`${styles.inputsRow} ${grid.row} passphrase`}>
-          {values.slice(6, 12).map((value, i) => (
-            <React.Fragment key={i}>
-              <span className={`${grid['col-xs-2']} ${styles.inputValue}`}>{value}</span>
-              <span className={styles.whitespace}>&nbsp;</span>
-            </React.Fragment>
-          ))}
-        </div>
+        {fieldSelected && (
+          <div className={styles.optionsContainer}>
+            {options[fieldSelected].map((option, i) => (
+              <div
+                onClick={() => this.setState({
+                  ...this.state,
+                  chosenWords: {
+                    ...this.state.chosenWords,
+                    [fieldSelected]: option,
+                  },
+                })}
+                key={i}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
