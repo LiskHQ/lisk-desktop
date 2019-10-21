7d3b936e44783a35e05c2664c0dc99d785576ae4
Sergio Colado <sergio.colado@lightcurve.io>
2019-10-21 08:51:26 +0200

    🐛 Allow to copy the passphrase manually

--- src/components/shared/passphraseGenerator/index.js
+++ src/components/shared/passphraseGenerator/index.js
@@ -9,12 +9,18 @@ class PassphraseGenerator extends React.Component {
       <div className={styles.container}>
         <div className={`${styles.inputsRow} ${grid.row} passphrase`}>
           {values.slice(0, 6).map((value, i) => (
-            <span className={`${grid['col-xs-2']} ${styles.inputValue}`} key={i}>{value}</span>
+            <React.Fragment key={i}>
+              <span className={`${grid['col-xs-2']} ${styles.inputValue}`}>{value}</span>
+              <span className={styles.whitespace}>&nbsp;</span>
+            </React.Fragment>
           ))}
         </div>
         <div className={`${styles.inputsRow} ${grid.row} passphrase`}>
           {values.slice(6, 12).map((value, i) => (
-            <span className={`${grid['col-xs-2']} ${styles.inputValue}`} key={i}>{value}</span>
+            <React.Fragment key={i}>
+              <span className={`${grid['col-xs-2']} ${styles.inputValue}`}>{value}</span>
+              <span className={styles.whitespace}>&nbsp;</span>
+            </React.Fragment>
           ))}
         </div>
       </div>
