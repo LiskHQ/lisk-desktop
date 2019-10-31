738a587a4bc49d5c23efa0a6034f5d96c58ef965
Osvaldo Vega <osvaldo.vega@lightcurve.io>
2019-10-31 15:36:35 +0100

    :white_check_mark: Update SearchBar E2E test

--- test/constants/selectors.js
+++ test/constants/selectors.js
@@ -202,7 +202,7 @@ const ss = {
   searchAccountRowTitle: '.account-title',
   searchTransactionRowId: '.transaction-id',
   searchDelegatesRow: '.delegates-row',
-  searchMessage: '.search-bar .feedback',
+  searchMessage: '.search-bar-feedback',
   searchDelegetesResults: '.delegates-content',
   searchAccountResults: '.account-content',
   searchTransactionRow: '.search-transaction-row',
