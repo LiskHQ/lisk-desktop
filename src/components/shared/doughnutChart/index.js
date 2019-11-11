c3c2076e5ba20e1bbf079bccfe1f49c9d1f13bd7
Sergio Colado <sergio.colado@lightcurve.io>
2019-11-11 12:19:55 +0100

    ♻️Create legend

--- src/components/shared/doughnutChart/index.js
+++ src/components/shared/doughnutChart/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const DoughnutChart = props => (
-  <Doughnut {...props} />
-);
+const DoughnutChart = (props) => {
+  const legend = {
+    display: true,
+    position: 'left',
+    align: 'center',
+    labels: {
+      fontSize: 13,
+      boxWidth: 3,
+      usePointStyle: true,
+    },
+  };
+
+  return (
+    <Doughnut legend={legend} {...props} />
+  );
+};
 
 export default DoughnutChart;
