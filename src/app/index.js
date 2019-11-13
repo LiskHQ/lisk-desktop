9d2aafb6f7ec1c3d6fe4d7a88a048ddccb80c628
yasharAyari <yashar@lightcurve.io>
2019-11-13 17:39:17 +0100

    Add ThemeContext.Provider to app component

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@
 // so it's covered by e2e tests.
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import styles from './app.css';
 import Toaster from '../components/shared/toaster';
 import LoadingBar from '../components/shared/loadingBar';
@@ -15,6 +16,7 @@ import routes from '../constants/routes';
 import Header from '../components/shared/header/header';
 import FlashMessageHolder from '../components/toolbox/flashMessage/holder';
 import DialogHolder from '../components/toolbox/dialog/holder';
+import ThemeContext from '../contexts/theme';
 
 class App extends React.Component {
   constructor() {
@@ -31,7 +33,8 @@ class App extends React.Component {
   }
 
   render() {
-    const { location, history } = this.props;
+    const { location, history, settings } = this.props;
+    const theme = settings.DarkMode ? 'dark' : 'light';
     const allRoutes = Object.values(routes);
     const mainClassNames = [
       styles.bodyWrapper,
@@ -40,52 +43,57 @@ class App extends React.Component {
     const routeObj = Object.values(routes).find(r => r.path === location.pathname) || {};
 
     return (
-      <OfflineWrapper>
-        <DialogHolder />
-        <Header
-          isSigninFlow={routeObj.isSigninFlow}
-          location={location}
-        />
-        <main
-          className={mainClassNames}
-          ref={(el) => { this.main = el; }}
-        >
-          <section>
-            <FlashMessageHolder />
-            <InitializationMessage history={history} />
-            <div className={`${styles.mainContent} ${!routeObj.isSigninFlow ? styles.mainBox : ''}`}>
-              <Switch>
-                {this.state.loaded && allRoutes.map(route => (
-                  route.isSigninFlow
-                    ? (
-                      <Route
-                        path={route.path}
-                        key={route.path}
-                        component={route.component}
-                        exact={route.exact}
-                      />
-                    ) : (
-                      <CustomRoute
-                        path={route.path}
-                        pathSuffix={route.pathSuffix}
-                        component={route.component}
-                        isPrivate={route.isPrivate}
-                        exact={route.exact}
-                        forbiddenTokens={route.forbiddenTokens}
-                        key={route.path}
-                      />
-                    )
-                ))}
-                <Route path="*" component={NotFound} />
-              </Switch>
-            </div>
-          </section>
-          <Toaster />
-        </main>
-        <LoadingBar markAsLoaded={this.markAsLoaded.bind(this)} />
-      </OfflineWrapper>
+      <ThemeContext.Provider value={theme}>
+        <OfflineWrapper>
+          <DialogHolder />
+          <Header
+            isSigninFlow={routeObj.isSigninFlow}
+            location={location}
+          />
+          <main
+            className={mainClassNames}
+            ref={(el) => { this.main = el; }}
+          >
+            <section>
+              <FlashMessageHolder />
+              <InitializationMessage history={history} />
+              <div className={`${styles.mainContent} ${!routeObj.isSigninFlow ? styles.mainBox : ''}`}>
+                <Switch>
+                  {this.state.loaded && allRoutes.map(route => (
+                    route.isSigninFlow
+                      ? (
+                        <Route
+                          path={route.path}
+                          key={route.path}
+                          component={route.component}
+                          exact={route.exact}
+                        />
+                      ) : (
+                        <CustomRoute
+                          path={route.path}
+                          pathSuffix={route.pathSuffix}
+                          component={route.component}
+                          isPrivate={route.isPrivate}
+                          exact={route.exact}
+                          forbiddenTokens={route.forbiddenTokens}
+                          key={route.path}
+                        />
+                      )
+                  ))}
+                  <Route path="*" component={NotFound} />
+                </Switch>
+              </div>
+            </section>
+            <Toaster />
+          </main>
+          <LoadingBar markAsLoaded={this.markAsLoaded.bind(this)} />
+        </OfflineWrapper>
+      </ThemeContext.Provider>
     );
   }
 }
 
-export default withRouter(App);
+const mapStateToProps = state => ({
+  settings: state.settings,
+});
+export default withRouter(connect(mapStateToProps)(App));
