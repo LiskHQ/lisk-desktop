62795db7b7ea9fa31ae2d38964e6c7bc4a081e8f
yasharAyari <yashar@lightcurve.io>
2019-11-14 17:48:05 +0100

    Use react hooks in withTheme HOC

--- src/utils/withTheme.js
+++ src/utils/withTheme.js
@@ -1,28 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ThemeContext from '../contexts/theme';
 
-export default (WrappedComponent, noWrapper) => {
-  const WithTheme = props => (
-    <ThemeContext.Consumer>
-      {
-        theme => (
-          !noWrapper ? (
-            <div data-theme={theme}>
-              <WrappedComponent
-                {...props}
-                theme={theme}
-              />
-            </div>
-          ) : (
-            <WrappedComponent
-              {...props}
-              theme={theme}
-              data-theme={theme}
-            />
-          )
-        )
-    }
-    </ThemeContext.Consumer>
-  );
-  return WithTheme;
-};
+export default function (WrappedComponent) {
+  return function (props) {
+    const theme = useContext(ThemeContext);
+    return (
+      <div data-theme={theme}>
+        <WrappedComponent
+          {...props}
+          theme={theme}
+        />
+      </div>
+    );
+  };
+}
