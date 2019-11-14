f53afad47b61749095170ade56dfc22e563fb2fd
Vit Stanislav <vit@lightcurve.io>
2019-11-14 16:20:38 +0100

    📚 Enable showing and hiding dialog in storybook

--- src/components/toolbox/dialog/dialog.stories.js
+++ src/components/toolbox/dialog/dialog.stories.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
+import { PrimaryButton } from '../buttons/button';
 import StoryWrapper from '../../../../.storybook/components/StoryWrapper/StoryWrapper';
 import Dialog from './dialog';
+import DialogHolder from './holder';
 
 storiesOf('Toolbox', module)
-  .add('Dialog', () => (
-    <StoryWrapper>
-      <h3>Dialog</h3>
-      <Dialog hasClose>
-        <Dialog.Title>Dummy Title</Dialog.Title>
-        <Dialog.Description>dsa</Dialog.Description>
-        <Dialog.Options align="center">sdvs</Dialog.Options>
-      </Dialog>
-    </StoryWrapper>
-  ));
+  .add('Dialog', () => {
+    const showDialog = () => {
+      DialogHolder.showDialog(
+        <Dialog hasClose>
+          <Dialog.Title>Dummy Title</Dialog.Title>
+          <Dialog.Description>dsa</Dialog.Description>
+          <Dialog.Options align="center">sdvs</Dialog.Options>
+        </Dialog>,
+      );
+    };
+    useEffect(showDialog, []);
+    return (
+      <StoryWrapper>
+        <h3>Dialog</h3>
+        <DialogHolder />
+        <PrimaryButton onClick={showDialog}>Show dialog</PrimaryButton>
+      </StoryWrapper>
+    );
+  });
