394afd71e22f2d2ad98c4b209c407e2eca279f19
reyraa <ali@lightcurve.io>
2019-12-02 11:14:02 +0100

    Add dark illustration

--- src/components/toolbox/illustration/index.js
+++ src/components/toolbox/illustration/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import welcomeLiskDelegates from '../../../assets/images/illustrations/illustration-welcome-to-lisk-delegates-dark.svg';
 import yourVoiceMatters from '../../../assets/images/illustrations/illustration-your-voice-matters-dark.svg';
 import getRewarded from '../../../assets/images/illustrations/illustration-get-rewarded-dark.svg';
@@ -13,7 +14,9 @@ import votingError from '../../../assets/images/illustrations/voting-failed.svg'
 import secondPassphraseSuccess from '../../../assets/images/illustrations/2nd-passphrase-submitted.svg';
 import secondPassphraseError from '../../../assets/images/illustrations/2nd-passphrase-failed.svg';
 import pageNotFound from '../../../assets/images/illustrations/illustration-page-not-found.svg';
+import pageNotFoundDark from '../../../assets/images/illustrations/illustration-page-not-found-dark.svg';
 import errorBoundaryPage from '../../../assets/images/illustrations/illustration-error-boundary-page.svg';
+import errorBoundaryPageDark from '../../../assets/images/illustrations/illustration-error-boundary-page-dark.svg';
 import hubReadyToGo from '../../../assets/images/illustrations/hub-ready-togo.svg';
 import builtAroundCommunity from '../../../assets/images/illustrations/built-around-community.svg';
 import sendLSKTokens from '../../../assets/images/illustrations/send-lsk-tokens.svg';
@@ -22,6 +25,7 @@ import emptyBookmarkFiler from '../../../assets/images/illustrations/empty-bookm
 import emptyBookmarksList from '../../../assets/images/illustrations/empty-bookmarks-list.svg';
 import helpCenter from '../../../assets/images/illustrations/help-center.svg';
 import emptyWallet from '../../../assets/images/illustrations/empty-wallet.svg';
+import emptyWalletDark from '../../../assets/images/illustrations/empty-wallet-dark.svg';
 import diveIntoDetails from '../../../assets/images/illustrations/diveIntoDetails.svg';
 import manageYourLSK from '../../../assets/images/illustrations/manageYourLSK.svg';
 import verifyMessageError from '../../../assets/images/illustrations/verify-message-error.svg';
@@ -56,11 +60,16 @@ export const illustrations = {
   verifyMessageError,
   verifyMessageSuccess,
   registrationSuccess,
+  emptyWalletDark,
+  pageNotFoundDark,
+  errorBoundaryPageDark,
 };
 
-const Illustration = ({ name, className }) => (
-  <img src={illustrations[name]} className={className} />
-);
+const Illustration = ({ name, className, noTheme }) => {
+  const darkMode = useSelector(state => state.settings.DarkMode);
+  const themed = darkMode && !noTheme && illustrations[`${name}Dark`] ? `${name}Dark` : name;
+  return <img src={illustrations[themed]} className={className} />;
+};
 
 Illustration.propTypes = {
   name: PropTypes.oneOf(Object.keys(illustrations)),
