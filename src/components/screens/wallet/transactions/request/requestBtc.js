787ebfedff39ad2e349696b146bd26d190e15193
reyraa <ali@lightcurve.io>
2019-12-02 16:01:29 +0100

    Fix the background colors of request forms

--- src/components/screens/wallet/transactions/request/requestBtc.js
+++ src/components/screens/wallet/transactions/request/requestBtc.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AutoresizeTextarea } from '../../../../toolbox/inputs';
 import RequestWrapper from './requestWrapper';
 import styles from './request.css';
 
@@ -11,9 +12,12 @@ const RequestBtc = ({
     </span>
     <label className={`${styles.fieldGroup}`}>
       <span className={`${styles.fieldLabel}`}>{t('BTC Address')}</span>
-      <span className={`${styles.amountField} address`}>
-        {address}
-      </span>
+      <AutoresizeTextarea
+        name="shareLink"
+        value={address}
+        className={`${styles.textarea} ${styles.sharingLink} request-link`}
+        readOnly
+      />
     </label>
   </RequestWrapper>
 );
