ee43c1eed7352956fb624b916ddec48ac735db59
Vit Stanislav <vit@lightcurve.io>
2019-12-03 11:40:58 +0100

    🔥 Remove unnecessary Provider from transactionsOverviewHeader.test.js

--- src/components/screens/wallet/transactions/transactionsOverviewHeader/transactionsOverviewHeader.test.js
+++ src/components/screens/wallet/transactions/transactionsOverviewHeader/transactionsOverviewHeader.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
 import TransactionHeader from './transactionsOverviewHeader';
 import accounts from '../../../../../../test/constants/accounts';
 import routes from '../../../../../constants/routes';
-
-const fakeStore = configureStore();
 
 describe('Transactions Overview Header', () => {
   let wrapper;
@@ -25,14 +21,8 @@ describe('Transactions Overview Header', () => {
   };
 
   describe('Current user wallet', () => {
-    const store = fakeStore({
-      settings: {
-        darkMode: false,
-      },
-    });
-
     beforeEach(() => {
-      wrapper = mount(<Provider store={store}><TransactionHeader {...props} /></Provider>);
+      wrapper = mount(<TransactionHeader {...props} />);
     });
 
     it('Should render header and Request and Send LSK buttons', () => {
