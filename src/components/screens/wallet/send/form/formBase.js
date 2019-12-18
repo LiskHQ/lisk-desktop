175029e1f15d7ccda4a9f46570c106d367ae5c7e
Vit Stanislav <vit@lightcurve.io>
2019-12-18 15:08:25 +0100

    ♻️  Create and use send/form/useCommonFields.js hook

--- src/components/screens/wallet/send/form/formBase.js
+++ src/components/screens/wallet/send/form/formBase.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { PrimaryButton } from '../../../../toolbox/buttons/button';
-import { parseSearchParams } from '../../../../../utils/searchParams';
 import AmountField from './amountField';
 import BookmarkAutoSuggest from './bookmarkAutoSuggest';
 import Box from '../../../../toolbox/box';
@@ -9,35 +8,26 @@ import BoxFooter from '../../../../toolbox/box/footer';
 import BoxHeader from '../../../../toolbox/box/header';
 import Piwik from '../../../../../utils/piwik';
 import styles from './form.css';
-import useAmountField from './useAmountField';
-import useRecipientField from './useRecipientField';
+import useCommonFields from './useCommonFields';
 
 const FormBase = ({
   t, token, children, extraFields, fee, networkConfig, getMaxAmount,
   bookmarks, history, nextStep, prevState, onInputChange,
 }) => {
   const {
-    recipient: initialRecipient = '',
-    amount: initialAmount,
-  } = parseSearchParams(history.location.search);
-
-  const [amount, setAmountField] = useAmountField(
-    prevState && prevState.fields ? prevState.fields.amount.value : initialAmount,
-    getMaxAmount,
-  );
-  const [recipient, setRecipientField] = useRecipientField(
-    prevState && prevState.fields ? prevState.fields.recipient.value : initialRecipient,
-  );
+    fields,
+    fieldUpdateFunctions: { setAmountField, setRecipientField },
+  } = useCommonFields(prevState, history, getMaxAmount);
 
   const handleRecipientChange = (name, value) => {
     onInputChange({ target: { name, value: value.value } }, value);
     setRecipientField({
-      ...recipient,
+      ...fields.recipient,
       ...value,
     });
   };
 
-  const allFields = { ...extraFields, amount, recipient };
+  const allFields = { ...extraFields, ...fields };
 
   const onGoNext = () => {
     Piwik.trackingEvent('Send_Form', 'button', 'Next step');
@@ -50,8 +40,8 @@ const FormBase = ({
     setAmountField(value);
   };
 
-  const isSubmitButtonDisabled = !!(amount.isLoading
-      || Object.values({ amount, recipient }).find(({ value }) => value === '')
+  const isSubmitButtonDisabled = !!(fields.amount.isLoading
+      || Object.values(fields).find(({ value }) => value === '')
       || Object.values(allFields).find(({ error }) => error)
   );
   return (
@@ -65,7 +55,7 @@ const FormBase = ({
           <BookmarkAutoSuggest
             bookmarks={bookmarks[token]}
             networkConfig={networkConfig}
-            recipient={recipient}
+            recipient={fields.recipient}
             t={t}
             token={token}
             updateField={handleRecipientChange}
@@ -73,7 +63,7 @@ const FormBase = ({
         </span>
         <AmountField
           t={t}
-          amount={amount}
+          amount={fields.amount}
           extraFields={extraFields}
           fee={fee}
           getMaxAmount={getMaxAmount}
@@ -96,6 +86,7 @@ const FormBase = ({
 
 FormBase.defaultProps = {
   extraFields: {},
+  prevState: {},
   onInputChange: () => {},
 };
 
