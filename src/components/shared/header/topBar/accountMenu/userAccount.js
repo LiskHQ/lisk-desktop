67b36808135e84c714aad1dab927b46a10a915a4
Vit Stanislav <vit@lightcurve.io>
2020-01-03 09:01:05 +0100

    🎨 Fix hover area size of Sign in/out menu item

--- src/components/shared/header/topBar/accountMenu/userAccount.js
+++ src/components/shared/header/topBar/accountMenu/userAccount.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import styles from './userAccount.css';
-import Icon from '../../../../toolbox/icon';
-import { tokenKeys } from '../../../../../constants/tokens';
-import routes from '../../../../../constants/routes';
-import feedbackLinks from '../../../../../constants/feedbackLinks';
-import externalLinks from '../../../../../constants/externalLinks';
-import DropdownButton from '../../../../toolbox/dropdownButton';
 import { SecondaryButton } from '../../../../toolbox/buttons/button';
-import AccountInfo from './accountInfo';
 import { loginType } from '../../../../../constants/hwConstants';
+import { tokenKeys } from '../../../../../constants/tokens';
+import AccountInfo from './accountInfo';
+import DropdownButton from '../../../../toolbox/dropdownButton';
+import Icon from '../../../../toolbox/icon';
 import MenuItem from './menuItem';
+import Separator from '../../../../toolbox/dropdown/separator';
+import externalLinks from '../../../../../constants/externalLinks';
+import feedbackLinks from '../../../../../constants/feedbackLinks';
+import routes from '../../../../../constants/routes';
+import styles from './userAccount.css';
 
 class UserAccount extends React.Component {
   constructor(props) {
@@ -160,11 +161,12 @@ class UserAccount extends React.Component {
               />
             ) : null
         }
+        <Separator />
         {
           isUserLogout
             ? (
               <MenuItem
-                className={`${styles.signInOut} signIn`}
+                className="signIn"
                 name="signIn"
                 title={t('Sign in')}
                 to={routes.login.path}
@@ -173,7 +175,7 @@ class UserAccount extends React.Component {
             )
             : (
               <MenuItem
-                className={`${styles.signInOut} logout`}
+                className="logout"
                 name="logout"
                 title={t('Sign out')}
                 onClick={this.handleLogout}
