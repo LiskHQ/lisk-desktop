42df0155ea525a2bc2535f111ff06407caec25d3
reyraa <ali@lightcurve.io>
2020-01-10 12:51:32 +0100

    Use the new table on the delegates page in monior section

--- src/components/screens/monitor/delegates/delegates.js
+++ src/components/screens/monitor/delegates/delegates.js
@@ -1,17 +1,30 @@
-import { withTranslation } from 'react-i18next';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { withTranslation } from 'react-i18next';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
-import { DEFAULT_LIMIT } from '../../../../constants/monitor';
-import { formatAmountBasedOnLocale } from '../../../../utils/formattedNumber';
-import AccountVisualWithAddress from '../../../shared/accountVisualWithAddress';
-import DelegatesTable from '../../../shared/delegatesTable';
 import MonitorHeader from '../header';
-import Tooltip from '../../../toolbox/tooltip/tooltip';
-import routes from '../../../../constants/routes';
 import Overview from './overview';
-import styles from './delegates.css';
+import { DEFAULT_LIMIT } from '../../../../constants/monitor';
+import routes from '../../../../constants/routes';
 import { forgingDataDisplayed, forgingDataConcealed } from '../../../../actions/blocks';
+import { Input } from '../../../toolbox/inputs';
+import Box from '../../../toolbox/box';
+import BoxHeader from '../../../toolbox/box/header';
+import BoxContent from '../../../toolbox/box/content';
+import BoxTabs from '../../../toolbox/tabs';
+import Table from '../../../toolbox/list';
+import Tooltip from '../../../toolbox/tooltip/tooltip';
+import AccountVisualWithAddress from '../../../shared/accountVisualWithAddress';
+import { formatAmountBasedOnLocale } from '../../../../utils/formattedNumber';
+import styles from './delegates.css';
+
+const statuses = {
+  forging: 'Forging',
+  awaitingSlot: 'Awaiting slot',
+  notForging: 'Not forging',
+  missedBlock: 'Missed block',
+};
 
 const getForgingTime = (data) => {
   if (!data || data.time === -1) return '-';
@@ -25,102 +38,145 @@ const getForgingTime = (data) => {
   return `${minutes}${seconds} ago`;
 };
 
+const DelegateRow = React.memo(({ data, className, forgingTime }) => (
+  <Link
+    key={data.id}
+    className={`${grid.row} ${className}`}
+    to={`${routes.blocks.path}/${data.id}`}
+  >
+    <span className={grid['col-md-1']}>
+      {`#${data.rank}`}
+    </span>
+    <span className={grid['col-md-2']}>
+      {data.username}
+    </span>
+    <span className={data.rank > 101 ? `${grid['col-xs-5']} ${grid['col-md-6']}` : grid['col-md-3']}>
+      <AccountVisualWithAddress address={data.address} />
+    </span>
+    {
+      data.rank < 101 ? (
+        <Fragment>
+          <span className={grid['col-md-2']}>
+            {getForgingTime(forgingTime)}
+          </span>
+          <span className={`${grid['col-xs-2']} ${grid['col-md-1']}`}>
+            <Tooltip
+              title={forgingTime
+                ? statuses[forgingTime.status]
+                : statuses.notForging}
+              className="showOnBottom"
+              size="s"
+              content={(
+                <div className={`${styles.status} ${
+                  styles[forgingTime
+                    ? forgingTime.status
+                    : 'notForging']}`
+                  }
+                />
+              )}
+              footer={(
+                <p>{getForgingTime(forgingTime, data)}</p>
+              )}
+            >
+              <p className={styles.statusToolip}>
+                {data.lastBlock && `Last block forged ${data.lastBlock}`}
+              </p>
+            </Tooltip>
+          </span>
+        </Fragment>
+      ) : null
+    }
+    <span className={`${grid['col-xs-2']} ${grid['col-md-2']} ${grid['col-lg-2']}`}>
+      {`${formatAmountBasedOnLocale({ value: data.productivity })} %`}
+    </span>
+    <span className={`${grid['col-xs-2']} ${grid['col-md-1']}`}>
+      {data.productivity}
+    </span>
+  </Link>
+));
+
+const header = (activeTab, changeSort) => ([
+  {
+    title: 'Rank',
+    classList: grid['col-md-1'],
+    sort: () => changeSort('rank'),
+  },
+  {
+    title: 'Name',
+    classList: grid['col-md-2'],
+  },
+  {
+    title: 'Address',
+    classList: activeTab === 'active'
+      ? grid['col-md-3']
+      : `${grid['col-xs-5']} ${grid['col-md-6']}`,
+  },
+  {
+    title: 'Forging time',
+    classList: activeTab === 'active' ? grid['col-md-2'] : 'hidden',
+    tooltip: {
+      title: 'Forging time',
+      message: 'Time until next forging slot of a delegate.',
+    },
+  },
+  {
+    title: 'Status',
+    classList: activeTab === 'active'
+      ? `${grid['col-xs-2']} ${grid['col-md-1']} ${styles.statusTitle}`
+      : 'hidden',
+    tooltip: {
+      title: 'Status',
+      message: 'Current status of a delegate: forging, not forging, awaiting slot or missed block.',
+    },
+  },
+  {
+    title: 'Productivity',
+    classList: `${grid['col-xs-2']} ${grid['col-md-2']} ${grid['col-lg-2']}`,
+    tooltip: {
+      title: 'Productivity',
+      message: 'Percentage of successfully forged blocks in relation to all blocks (forged and missed).',
+    },
+  },
+  {
+    title: 'Approval',
+    classList: `${grid['col-xs-2']} ${grid['col-md-1']} ${styles.approvalTitle}`,
+    tooltip: {
+      title: 'Approval',
+      message: 'Percentage of total supply voting for a delegate.',
+    },
+  },
+]);
+
 // eslint-disable-next-line max-statements
-const Delegates = ({
-  applyFilters,
-  changeSort,
+const DelegatesTable = ({
   chartActiveAndStandbyData,
   chartRegisteredDelegatesData,
+  standByDelegates,
+  applyFilters,
+  changeSort,
   delegates,
   filters,
-  isMediumViewPort,
   sort,
-  standByDelegates,
   t,
 }) => {
   const [activeTab, setActiveTab] = useState('active');
   const dispatch = useDispatch();
-  const statuses = {
-    forging: t('Forging'),
-    awaitingSlot: t('Awaiting slot'),
-    notForging: t('Not forging'),
-    missedBlock: t('Missed block'),
+  const handleLoadMore = () => {
+    delegates.loadData(Object.keys(filters).reduce((acc, key) => ({
+      ...acc,
+      ...(filters[key] && { [key]: filters[key] }),
+    }), {
+      offset: delegates.data.length,
+    }));
   };
   const forgingTimes = useSelector(state => state.blocks.forgingTimes);
 
-  const columns = [
-    {
-      id: 'rank',
-      isSortable: activeTab === 'active',
-    },
-    {
-      id: 'username',
-      header: ('Name'),
-      className: grid['col-xs-2'],
-    },
-    {
-      id: 'address',
-      header: t('Address'),
-      /* eslint-disable-next-line react/display-name */
-      getValue: ({ address }) => <AccountVisualWithAddress {...{ address, isMediumViewPort }} />,
-      className: (activeTab === 'active'
-        ? `${grid['col-xs-3']} ${grid['col-md-3']}`
-        : `${grid['col-xs-5']} ${grid['col-md-6']}`
-      ),
-    },
-    ...(activeTab === 'active' ? [{
-      id: 'forgingTime',
-      header: t('Forging time'),
-      headerTooltip: t('Time until next forging slot of a delegate.'),
-      /* eslint-disable-next-line react/display-name */
-      getValue: data => getForgingTime(forgingTimes[data.publicKey]),
-      className: `hidden-m ${grid['col-md-2']}`,
-    },
-    {
-      id: 'status',
-      header: t('Status'),
-      headerTooltip: t('Current status of a delegate: forging, not forging, awaiting slot or missed block.'),
-      /* eslint-disable-next-line react/display-name */
-      getValue: data => (
-        <Tooltip
-          title={forgingTimes[data.publicKey]
-            ? statuses[forgingTimes[data.publicKey].status]
-            : statuses.notForging}
-          className="showOnBottom"
-          size="s"
-          content={(
-            <div className={`${styles.status} ${
-              styles[forgingTimes[data.publicKey]
-                ? forgingTimes[data.publicKey].status
-                : 'notForging']}`
-              }
-            />
-          )}
-          footer={(
-            <p>{getForgingTime(forgingTimes[data.publicKey], data)}</p>
-          )}
-        >
-          <p className={styles.statusToolip}>
-            {data.lastBlock && t('Last block forged @{{height}}', data.lastBlock)}
-          </p>
-        </Tooltip>
-      ),
-      className: `${grid['col-xs-2']} ${grid['col-md-1']} ${styles.statusTitle}`,
-    },
-    ] : []),
-    {
-      id: 'productivity',
-      isSortable: activeTab === 'active',
-    },
-    {
-      id: 'approval',
-      header: t('Approval'),
-      headerTooltip: t('Percentage of total supply voting for a delegate.'),
-      /* eslint-disable-next-line react/display-name */
-      getValue: ({ approval }) => <strong>{`${formatAmountBasedOnLocale({ value: approval })} %`}</strong>,
-      className: `${grid['col-xs-2']} ${grid['col-md-1']} ${styles.approvalTitle}`,
-    },
-  ];
+  const handleFilter = ({ target: { value } }) => {
+    applyFilters({
+      ...filters,
+      search: value,
+    });
+  };
 
   const tabs = {
     tabs: [
@@ -141,8 +197,6 @@ const Delegates = ({
   const canLoadMore = activeTab === 'active'
     ? false
     : !!standByDelegates.data.length && standByDelegates.data.length % DEFAULT_LIMIT === 0;
-
-  const getRowLink = delegate => `${routes.accounts.pathPrefix}${routes.accounts.path}/${delegate.address}`;
 
   delegates = activeTab === 'active'
     ? {
@@ -168,20 +222,37 @@ const Delegates = ({
         delegatesForgedLabels={Object.values(statuses)}
         t={t}
       />
-      <DelegatesTable {...{
-        columns,
-        delegates,
-        tabs,
-        filters,
-        applyFilters,
-        canLoadMore,
-        getRowLink,
-        onSortChange: changeSort,
-        sort,
-      }}
-      />
+      <Box main isLoading={delegates.isLoading}>
+        <BoxHeader className="delegates-table">
+          {tabs.tabs.length === 1
+            ? <h2>{tabs.tabs[0].name}</h2>
+            : <BoxTabs {...tabs} />
+          }
+          <span>
+            <Input
+              onChange={handleFilter}
+              value={filters.search}
+              className="filter-by-name"
+              size="xs"
+              placeholder={t('Filter by name...')}
+            />
+          </span>
+        </BoxHeader>
+        <BoxContent className={styles.content}>
+          <Table
+            data={delegates.data}
+            isLoading={delegates.isLoading}
+            row={props =>
+              <DelegateRow {...props} forgingTime={forgingTimes[props.data.publicKey]} />}
+            loadData={handleLoadMore}
+            header={header(activeTab, changeSort)}
+            currentSort={sort}
+            loadMoreButton={canLoadMore}
+          />
+        </BoxContent>
+      </Box>
     </div>
   );
 };
 
-export default withTranslation()(Delegates);
+export default withTranslation()(DelegatesTable);
