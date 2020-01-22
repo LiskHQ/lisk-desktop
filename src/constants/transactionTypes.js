c28c942753d10124111d2a111764bfbb7e23e18c
reyraa <ali@lightcurve.io>
2020-01-22 11:24:14 +0100

    Unify the transactions types cofnig

--- src/constants/transactionTypes.js
+++ src/constants/transactionTypes.js
@@ -1,22 +1,63 @@
-export default {
-  send: 0,
-  setSecondPassphrase: 1,
-  registerDelegate: 2,
-  vote: 3,
+import store from '../store';
+
+const ApiTxTypes = {
+  '2.x': [0, 1, 2, 3, 4],
+  '3.x': [8, 9, 10, 11, 12],
 };
 
-export const transactionNames = t => ({
-  0: t('Send'),
-  1: t('Second passphrase registration'),
-  2: t('Delegate registration'),
-  3: t('Delegate vote'),
-  4: t('Multisignature creation'),
-  5: t('Blockchain application registration'),
-  6: t('Send Lisk to blockchain application'),
-  7: t('Send Lisk from blockchain application'),
-});
-
-export const createTransactionType = {
-  transaction: 'transfer',
-  delegate_registration: 'registerDelegate',
+const transactionTypes = (t = str => str) => {
+  const ApiVersion = store.getState().network.apiVersion;
+  return {
+    send: {
+      code: ApiTxTypes[ApiVersion][0],
+      title: t('Send'),
+      key: 'transfer',
+    },
+    setSecondPassphrase: {
+      code: ApiTxTypes[ApiVersion][1],
+      title: t('Second passphrase registration'),
+      key: 'secondPassphrase',
+    },
+    registerDelegate: {
+      code: ApiTxTypes[ApiVersion][2],
+      title: t('Delegate registration'),
+      key: 'registerDelegate',
+    },
+    vote: {
+      code: ApiTxTypes[ApiVersion][3],
+      title: t('Delegate vote'),
+      key: 'vote',
+    },
+    createMultiSig: {
+      code: ApiTxTypes[ApiVersion][4],
+      title: t('Multisignature creation'),
+      key: 'createMultiSig',
+    },
+  };
 };
+
+/**
+ * To get the transaction config for a given transaction code.
+ *
+ * @param {Number} transaction type code
+ * @returns {Object} Returns the transaction config if the key is valid, else it return null
+ */
+transactionTypes.getByCode = (code) => {
+  const types = transactionTypes();
+  const key = Object.keys(types).filter(type => types[type].code === code);
+  return key.length ? types[key] : null;
+};
+
+/**
+ * To get the list of values for a given key
+ * For example it returns [0, 1, 2, 3, 4] if called with 'code' against Core 3.x
+ *
+ * @param {String} transaction type code
+ * @returns {Array} Returns the list of values for a given key
+ */
+transactionTypes.getListOf = (key) => {
+  const types = transactionTypes();
+  return Object.keys(types).map(type => types[type][key]);
+};
+
+export default transactionTypes;
