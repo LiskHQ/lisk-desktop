91f2401c532683875c6f5813456ab980ff6361da
yasharAyari <yashar@lightcurve.io>
2020-01-24 16:38:23 +0100

    Fix a bug in lisk-client util

--- src/utils/api/lisk-client.js
+++ src/utils/api/lisk-client.js
@@ -3,6 +3,10 @@ import Lisk3x from '@liskhq/lisk-client-Edge';
 import store from '../../store/index';
 
 export default function () {
-  const { apiVersion } = store.getState().network.networks.LSK;
+  const networkConfig = store.getState().network.networks.LSK;
+  let apiVersion = '2';
+  if (networkConfig) {
+    apiVersion = networkConfig.apiVersion;
+  }
   return apiVersion === '3' ? Lisk3x : Lisk2x;
 }
