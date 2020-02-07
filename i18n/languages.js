90a53039a9e5d7c8ec4e40bb998b4bae9035223c
Lemii <peter.spaargaren@gmail.com>
2020-02-07 12:41:28 +0100

    feat: add dutch localization

--- i18n/languages.js
+++ i18n/languages.js
@@ -1,4 +1,3 @@
-
 const t = key => key; // This is hack to get the names in common.json
 // we have to call t(...) again in src/components/setting/setting.js
 
@@ -11,5 +10,10 @@ const languages = {
     name: t('German'),
     common: require('./locales/de/common.json'),
   },
+  nl: {
+    name: t('Dutch'),
+    common: require('./locales/nl/common.json'),
+  },
 };
+
 export default languages;
