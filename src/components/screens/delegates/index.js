d1fa0a1ffa2ef12e6b9e2c9197338c295a66d181
reyraa <ali@lightcurve.io>
2020-02-12 15:36:45 +0100

    Simplify the component by removing the extra HOC

--- src/components/screens/delegates/index.js
+++ src/components/screens/delegates/index.js
@@ -1,23 +1,75 @@
-// istanbul ignore file
+import React, { useState } from 'react';
+import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import { withTranslation } from 'react-i18next';
-import { getVotes, getDelegates } from '../../../utils/api/delegates';
-import withData from '../../../utils/withData';
-import DelegatesList from './delegates';
+import { useDispatch } from 'react-redux';
+import styles from './delegates.css';
+import DelegatesTable from './table';
+import Header from './header';
+import Onboarding from '../../toolbox/onboarding/onboarding';
+import { clearVotes } from '../../../actions/voting';
 
-const apis = {
-  votes: {
-    apiUtil: getVotes,
-    defaultData: {},
-    transformResponse: response => response.data.votes,
-  },
-  delegates: {
-    apiUtil: getDelegates,
-    defaultData: [],
-    transformResponse: (response, oldData) => ({
-      ...oldData,
-      ...response.data.reduce((acc, item) => ({ ...acc, [item.username]: item }), {}),
-    }),
-  },
+const getOnboardingSlides = t => (
+  [{
+    title: t('Welcome to Lisk Delegates!'),
+    content: t('Lisk\'s blockchain is based on the Delegated Proof of Stake (DPoS) consensus algorithm, in which 101 delegates are voted in by token holders to secure the network.'),
+    illustration: 'welcomeLiskDelegates',
+  }, {
+    title: t('Your voice matters'),
+    content: t('Voting for delegates gives Lisk users the opportunity to choose who they trust to secure the network and validate the transactions that are sent on it.'),
+    illustration: 'yourVoiceMatters',
+  }, {
+    title: t('Casting a vote'),
+    content: t('We encourage community members to research individual delegate contributions to the Lisk ecosystem before voting. There are several community created sites which can assist with the process.'),
+    illustration: 'getRewarded',
+  }, {
+    title: t('Expand your knowledge'),
+    content: t('Want to know more? We’ve got you covered. Read more about Lisk’s consensus algorithm and its benefits in the Lisk Academy.'),
+    illustration: 'expandYourKnowledge',
+  }]
+);
+
+const Delegates = ({
+  t,
+}) => {
+  const [votingMode, setVotingMode] = useState(false);
+  const [onBoardingToggled, setOnBoardingToggled] = useState(false);
+  const dispatch = useDispatch();
+  // eslint-disable-next-line prefer-const
+  let wrapper = React.createRef();
+
+  const toggleVotingMode = () => {
+    if (votingMode) {
+      dispatch(clearVotes());
+    }
+    setVotingMode(!votingMode);
+  };
+
+  const onBoardingDiscarded = () => {
+    setOnBoardingToggled(!onBoardingToggled);
+  };
+
+  return (
+    <div className={`${grid.row} ${styles.wrapper}`} ref={wrapper}>
+      <Onboarding
+        slides={getOnboardingSlides(t)}
+        finalCallback={toggleVotingMode}
+        onDiscard={onBoardingDiscarded}
+        actionButtonLabel={t('Start voting')}
+        name="delegateOnboarding"
+      />
+      <Header
+        t={t}
+        votingModeEnabled={votingMode}
+        toggleVotingMode={toggleVotingMode}
+        onBoardingDiscarded={onBoardingDiscarded}
+      />
+      <section className={`${grid['col-sm-12']} ${grid['col-md-12']} ${styles.votingBox} ${styles.votes}`}>
+        <DelegatesTable
+          votingModeEnabled={votingMode}
+        />
+      </section>
+    </div>
+  );
 };
 
-export default withData(apis)(withTranslation()(DelegatesList));
+export default withTranslation()(Delegates);
