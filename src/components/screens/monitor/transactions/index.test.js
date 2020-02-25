e342c872ecd4c916f6c6c938d41cbd42cb57e765
reyraa <ali@lightcurve.io>
2020-02-25 14:59:16 +0100

    Remove an obsolete test

--- src/components/screens/monitor/transactions/index.test.js
+++ src/components/screens/monitor/transactions/index.test.js
@@ -8,15 +8,6 @@ jest.mock('../../../../constants/monitor', () => ({ DEFAULT_LIMIT: 4 }));
 const fakeStore = configureStore();
 
 describe('Transactions monitor page', () => {
-  it('should render NotAvailable when using Custom Node', () => {
-    const store = fakeStore({
-      network: {
-        name: 'Custom Node',
-      },
-    });
-    const wrapper = mount(<Provider store={store}><TransactionsMonitor /></Provider>);
-    expect(wrapper.find('NotAvailable')).toHaveLength(1);
-  });
   it('should render Transactions when using testnet', () => {
     const store = fakeStore({
       network: {
