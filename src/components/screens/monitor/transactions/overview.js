73a5d0d301c0d64d4cb0108beb23ab1dea288c62
reyraa <ali@lightcurve.io>
2020-03-06 15:10:32 +0100

    Fromat dates in bar chart

--- src/components/screens/monitor/transactions/overview.js
+++ src/components/screens/monitor/transactions/overview.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { compose } from 'redux';
 import { withTranslation } from 'react-i18next';
+import moment from 'moment';
 import withData from '../../../../utils/withData';
 import liskService from '../../../../utils/api/lsk/liskService';
 import Box from '../../../toolbox/box';
@@ -66,13 +67,23 @@ const formatNumberRange = (range) => {
   return `${kFormatter(start)} < ${kFormatter(end)}`;
 };
 
+const formatDates = (date, period) => {
+  if (period === 'week') {
+    return moment(date).format('ddd');
+  }
+  if (period === 'month') {
+    return moment(date).format('MMM');
+  }
+  return date.replace(/^\d{4}-/, '');
+};
+
 const Overview = ({ t, txStats }) => {
+  const [activeTab, setActiveTab] = useState('week');
   const distributionByType = txStats.data.distributionByType;
   const distributionByAmount = txStats.data.distributionByAmount;
   const txCountList = txStats.data.timeline.map(item => item.transactionCount);
   const txVolumeList = txStats.data.timeline.map(item => fromRawLsk(item.volume));
-  const txDateList = txStats.data.timeline.map(item => item.date.replace(/^\d{4}-/, ''));
-  const [activeTab, setActiveTab] = useState('week');
+  const txDateList = txStats.data.timeline.map(item => formatDates(item.date, activeTab));
 
   const changeTab = (tab) => {
     setActiveTab(tab.value);
