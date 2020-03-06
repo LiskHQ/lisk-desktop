b64f40f2f9ec686b968a924ed3e6500448db9347
reyraa <ali@lightcurve.io>
2020-03-06 13:37:20 +0100

    Diversify chart colors

--- src/constants/chartConstants.js
+++ src/constants/chartConstants.js
@@ -8,7 +8,7 @@ export const chartStyles = {
   slateGray: '#70778b',
   maastrichtBlue: '#0c152e',
   ultramarineBlue: '#4070f4',
-  darkBlue: '#19224d',
+  darkBlue: '#254898',
   ufoGreen: '#2bd67b',
   linthPink: '#f8d2d2',
   mystic: '#edf0f5',
