99c942789c27e4479b2f1b64a08e1d5add8f5fc9
reyraa <ali@lightcurve.io>
2020-03-13 08:20:30 +0100

    Put Network button on the first place

--- src/components/screens/monitor/header.js
+++ src/components/screens/monitor/header.js
@@ -17,6 +17,11 @@ export const Header = ({ t, history }) => {
       <Switcher
         options={[
           {
+            value: routes.monitorNetwork.path,
+            name: t('Network'),
+            className: 'network',
+          },
+          {
             value: routes.monitorTransactions.path,
             name: t('Transactions'),
             className: 'transactions',
@@ -35,11 +40,6 @@ export const Header = ({ t, history }) => {
             value: routes.monitorAccounts.path,
             name: t('Accounts'),
             className: 'accounts',
-          },
-          {
-            value: routes.monitorNetwork.path,
-            name: t('Network'),
-            className: 'network',
           },
         ]}
         active={history.location.pathname}
