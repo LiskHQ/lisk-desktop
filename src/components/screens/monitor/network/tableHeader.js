200df9564605e5cb068f20db4af23bcdeafce943
reyraa <ali@lightcurve.io>
2020-03-13 08:19:10 +0100

    Fix table header on small screens

--- src/components/screens/monitor/network/tableHeader.js
+++ src/components/screens/monitor/network/tableHeader.js
@@ -1,21 +1,22 @@
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import styles from './network.css';
 
 export default (changeSort, t) => ([
   {
     title: t('IP address'),
-    classList: grid['col-md-3'],
+    classList: grid['col-xs-3'],
   },
   {
     title: t('WS'),
-    classList: grid['col-md-2'],
+    classList: grid['col-xs-2'],
   },
   {
     title: t('Country'),
-    classList: grid['col-md-2'],
+    classList: grid['col-xs-2'],
   },
   {
     title: t('Version'),
-    classList: grid['col-md-2'],
+    classList: grid['col-xs-2'],
     sort: {
       fn: changeSort,
       key: 'version',
@@ -23,7 +24,7 @@ export default (changeSort, t) => ([
   },
   {
     title: t('Height'),
-    classList: grid['col-md-3'],
+    classList: `${grid['col-xs-3']} ${styles.height}`,
     sort: {
       fn: changeSort,
       key: 'height',
