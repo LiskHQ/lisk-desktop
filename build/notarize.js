f3965448b7aff566911036144d51d6427233d682
reyraa <ali@lightcurve.io>
2020-03-19 15:30:01 +0100

    Add notarization script

---
+++ build/notarize.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName === 'darwin') {
+    const appName = context.packager.appInfo.productFilename;
+
+    await notarize({
+      appBundleId: 'com.yourcompany.yourAppId',
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASS,
+    });
+  }
+
+};
