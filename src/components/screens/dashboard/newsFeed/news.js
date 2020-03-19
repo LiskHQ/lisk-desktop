f82a501757df6f004685d741e1e7cd26da8898df
yasharAyari <yashar@lightcurve.io>
2020-03-19 17:05:35 +0100

    Remove formatDate method in News component to be aligned with the news design

--- src/components/screens/dashboard/newsFeed/news.js
+++ src/components/screens/dashboard/newsFeed/news.js
@@ -1,51 +1,28 @@
 import React from 'react';
 import moment from 'moment';
-import i18n from '../../../../i18n';
 import Icon from '../../../toolbox/icon';
 import styles from './news.css';
 
-class News extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      readMore: false,
-    };
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  formatDate(t, timestamp) {
-    const hours = Math.floor(Math.abs(new Date() - new Date(timestamp)) / 36e5);
-    if (hours <= 1) return t('Just now');
-    if (hours > 1 && hours <= 24) return t('{{hours}}h ago', { hours });
-    if (hours > 24 && hours <= 48) return t('Yesterday');
-    moment.locale(i18n.language);
-    return moment(timestamp).format('DD MMM YYYY');
-  }
-
-  render() {
-    const {
-      source, content, timestamp, url, t,
-    } = this.props;
-    const date = this.formatDate(t, timestamp);
-
-    // Makes first letter capital
-    const sourceName = source.charAt(0).toUpperCase() + source.substr(1);
-    return (
-      <a href={url} target="_blank" rel="noopener noreferrer" className={`${styles.news} news-item`}>
-        <div className={styles.header}>
-          <Icon name="newsFeedAvatar" />
-          <div>
-            <span className={styles.title}>{sourceName}</span>
-            <span className={styles.subtitle}>{date}</span>
-          </div>
+const News = ({
+  content, timestamp, url, title, author,
+}) => {
+  const date = moment.unix(timestamp).format('DD MMM YYYY');
+  // Makes first letter capital
+  const newsTitle = title.length === 0 ? author : title;
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className={`${styles.news} news-item`}>
+      <div className={styles.header}>
+        <Icon name="newsFeedAvatar" />
+        <div>
+          <span className={styles.title}>{newsTitle}</span>
+          <span className={styles.subtitle}>{date}</span>
         </div>
-        <div className={styles.description}>
-          {url ? content.replace(url, '') : url}
-        </div>
-      </a>
-    );
-  }
-}
+      </div>
+      <div className={styles.description}>
+        {url ? content.replace(url, '') : url}
+      </div>
+    </a>
+  );
+};
 
 export default News;
