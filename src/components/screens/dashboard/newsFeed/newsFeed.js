d59c9bf3f6bb5ef9c11a88b44a5cdcc81c91bf50
yasharAyari <yashar@lightcurve.io>
2020-03-19 17:06:32 +0100

    Refactor NewsFeed component to use React Hooks

--- src/components/screens/dashboard/newsFeed/newsFeed.js
+++ src/components/screens/dashboard/newsFeed/newsFeed.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import styles from './newsFeed.css';
 import News from './news';
 import Box from '../../../toolbox/box';
@@ -8,47 +9,53 @@ import BoxContent from '../../../toolbox/box/content';
 import BoxEmptyState from '../../../toolbox/box/emptyState';
 import Icon from '../../../toolbox/icon';
 
-class NewsFeed extends React.Component {
-  render() {
-    const {
-      channels,
-      newsFeed,
-      t,
-    } = this.props;
+const NewsFeed = (props) => {
+  const {
+    newsFeed,
+    t,
+  } = props;
+  const filteredNewsFeed = newsFeed.data || [];
+  const serviceUrl = useSelector(state => state.network.serviceUrl);
 
-    const filteredNewsFeed = newsFeed.data.filter(feed => channels[feed.source]) || [];
+  useEffect(
+    () => {
+      if (newsFeed && (newsFeed.error.length === 0 && newsFeed.data.length === 0)) {
+        newsFeed.loadData();
+      }
+    },
+    [serviceUrl],
+  );
 
-    return (
-      <Box className="newsFeed-box">
-        <BoxHeader>
-          <h1>{t('Community feed')}</h1>
-        </BoxHeader>
-        <BoxContent className={styles.container}>
-          {
-              filteredNewsFeed.length
-                ? filteredNewsFeed.map(news => (
-                  <BoxRow isClickable key={news.sourceId} className={styles.row}>
-                    <News
-                      t={t}
-                      {...news}
-                    />
-                  </BoxRow>
-                ))
-                : null
-            }
-          {
-              newsFeed.error && (
-                <BoxEmptyState className="empty-news">
-                  <Icon name="noTweetsIcon" />
-                  <h1>{t('No available tweets')}</h1>
-                  <p>{t('At this moment there is a connection problem with the tweets feed')}</p>
-                </BoxEmptyState>
-              )
-            }
-        </BoxContent>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box className="newsFeed-box">
+      <BoxHeader>
+        <h1>{t('News feed')}</h1>
+      </BoxHeader>
+      <BoxContent className={styles.container}>
+        {
+            filteredNewsFeed.length > 0
+              ? filteredNewsFeed.map(news => (
+                <BoxRow isClickable key={news.sourceId} className={styles.row}>
+                  <News
+                    t={t}
+                    {...news}
+                  />
+                </BoxRow>
+              ))
+              : null
+          }
+        {
+            newsFeed.error && (
+              <BoxEmptyState className="empty-news">
+                <Icon name="noTweetsIcon" />
+                <h1>{t('No available tweets')}</h1>
+                <p>{t('At this moment there is a connection problem with the tweets feed')}</p>
+              </BoxEmptyState>
+            )
+          }
+      </BoxContent>
+    </Box>
+  );
+};
 
 export default NewsFeed;
