a4a2534708c53539dab6803e9050b153a4c7a3ac
reyraa <ali@lightcurve.io>
2020-03-20 12:40:30 +0100

    Fix the bundle id

--- build/notarize.js
+++ build/notarize.js
@@ -8,7 +8,7 @@ exports.default = async function notarizing(context) {
     const appName = context.packager.appInfo.productFilename;
 
     await notarize({
-      appBundleId: 'com.yourcompany.yourAppId',
+      appBundleId: 'io.lisk.desktop',
       appPath: `${appOutDir}/${appName}.app`,
       appleId: process.env.APPLEID,
       appleIdPassword: process.env.APPLEIDPASS,
