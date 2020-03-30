959c4b17b9b17581e9fde74305f5336b1413348f
yasharAyari <yashar@lightcurve.io>
2020-03-30 18:57:43 +0200

    Write some unit tests for twitterParser component

--- src/components/screens/dashboard/newsFeed/twitterParser.js
+++ src/components/screens/dashboard/newsFeed/twitterParser.js
@@ -5,12 +5,22 @@ const TweetParser = ({ children }) => {
   const REGEX_USER = /\B(@[a-zA-Z0-9_]+)/g; // regex for @users
   const REGEX_HASHTAG = /\B(#[Ã¡-ÃºÃ-ÃÃ¤-Ã¼Ã-Ãa-zA-Z0-9_]+)/g; // regex for #hashtags
   const textWithHashtag = reactStringReplace(children, REGEX_HASHTAG, (hashtag, i) => (
-    <a key={hashtag + i} href={`https://twitter.com/hashtag/${hashtag.slice(1)}`} target="_blank">
+    <a
+      key={hashtag + i}
+      href={`https://twitter.com/hashtag/${hashtag.slice(1)}`}
+      target="_blank"
+      className="hashtag"
+    >
       {hashtag}
     </a>
   ));
   const textWithUsers = reactStringReplace(textWithHashtag, REGEX_USER, (user, i) => (
-    <a key={user + i} href={`https://twitter.com/${user.slice(1)}`} target="_blank">
+    <a
+      key={user + i}
+      href={`https://twitter.com/${user.slice(1)}`}
+      target="_blank"
+      className="user"
+    >
       {user}
     </a>
   ));
