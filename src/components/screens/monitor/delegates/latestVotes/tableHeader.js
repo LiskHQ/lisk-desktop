f2f97451186a032e5592b4587d86d5d8b0814f90
reyraa <ali@lightcurve.io>
2020-03-31 07:52:46 +0200

    Fix the table elements

--- src/components/screens/monitor/delegates/latestVotes/tableHeader.js
+++ src/components/screens/monitor/delegates/latestVotes/tableHeader.js
@@ -2,11 +2,23 @@ import grid from 'flexboxgrid/dist/flexboxgrid.css';
 
 export default t => ([
   {
-    title: t('Rank'),
-    classList: grid['col-md-1'],
+    title: t('Sender'),
+    classList: grid['col-md-3'],
   },
   {
-    title: t('Name'),
+    title: t('Date'),
     classList: grid['col-md-2'],
+  },
+  {
+    title: t('Current balance'),
+    classList: grid['col-md-2'],
+  },
+  {
+    title: t('Round'),
+    classList: grid['col-md-2'],
+  },
+  {
+    title: t('Votes'),
+    classList: grid['col-md-3'],
   },
 ]);
--- src/components/screens/monitor/delegates/latestVotes/voteRow.js
+++ src/components/screens/monitor/delegates/latestVotes/voteRow.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import routes from '../../../../../constants/routes';
+import { DateTimeFromTimestamp } from '../../../../toolbox/timestamp';
+import LiskAmount from '../../../../shared/liskAmount';
+import AccountVisualWithAddress from '../../../../shared/accountVisualWithAddress';
 
 const VoteRow = ({
   data, className,
@@ -10,11 +13,25 @@ const VoteRow = ({
     className={`${grid.row} ${className} vote-row`}
     to={`${routes.transactions.path}/${data.id}`}
   >
-    <span className={grid['col-md-1']}>
-      no
+    <span className={grid['col-md-3']}>
+      <AccountVisualWithAddress
+        address={data.senderId}
+        transactionSubject="senderId"
+        transactionType={3}
+        showBookmarkedAddress
+      />
     </span>
     <span className={grid['col-md-2']}>
-      ba ah
+      <DateTimeFromTimestamp time={data.timestamp * 1000} token="BTC" />
+    </span>
+    <span className={grid['col-md-2']}>
+      <LiskAmount val={data.balance} token="LSK" />
+    </span>
+    <span className={grid['col-md-2']}>
+      <span>{data.height}</span>
+    </span>
+    <span className={grid['col-md-3']}>
+      <span>votes here</span>
     </span>
   </Link>
 );
