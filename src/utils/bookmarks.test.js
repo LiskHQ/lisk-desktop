1ed4e3b3fbf708ae7e37e06b73ece125a5171151
reyraa <ali@lightcurve.io>
2020-04-06 08:44:16 +0200

    Update bookmark utilities

--- src/utils/bookmarks.test.js
+++ src/utils/bookmarks.test.js
@@ -1,13 +1,10 @@
 import { expect } from 'chai';
 import { stub } from 'sinon';
 import {
-  getBookmarksFromLocalStorage,
-  setBookmarksInLocalStorage,
   getIndexOfBookmark,
 } from './bookmarks';
 
 describe('Bookmarks', () => {
-  const bookmarksObj = { LSK: [], BTC: [] };
   const accounts = {
     LSK: [{
       address: '1234L',
@@ -29,36 +26,6 @@ describe('Bookmarks', () => {
   afterEach(() => {
     localStorage.getItem.restore();
     localStorage.setItem.restore();
-  });
-
-  describe('getBookmarksFromLocalStorage', () => {
-    it('returns { LSK: [], BTC: [] } if localStorage.getItem(\'bookmarks\') returns undefined', () => {
-      expect(getBookmarksFromLocalStorage()).to.deep.equal(bookmarksObj);
-    });
-
-    it('returns { LSK: [], BTC: [] } if localStorage.getItem(\'bookmarks\') returns invalid JSON string', () => {
-      localStorage.getItem.returns('{]');
-      expect(getBookmarksFromLocalStorage()).to.deep.equal(bookmarksObj);
-      localStorage.getItem.returns('{}');
-      expect(getBookmarksFromLocalStorage()).to.deep.equal({});
-    });
-
-    it('returns { LSK: [], BTC: [] } if localStorage.getItem(\'bookmarks\') returns JSON encoded array with invalid data', () => {
-      localStorage.getItem.returns('[]');
-      expect(getBookmarksFromLocalStorage()).to.deep.equal(bookmarksObj);
-    });
-
-    it('returns object parsed from json in localStorage.getItem(\'bookmarks\')', () => {
-      localStorage.getItem.returns(JSON.stringify(accounts));
-      expect(getBookmarksFromLocalStorage()).to.deep.equal(accounts);
-    });
-  });
-
-  describe('setBookmarksInLocalStorage', () => {
-    it('sets accounts in localStorage with appended passed account and also returns it', () => {
-      setBookmarksInLocalStorage(accounts);
-      expect(localStorage.setItem).to.have.been.calledWith('bookmarks', JSON.stringify(accounts));
-    });
   });
 
   describe('getIndexOfBookmark', () => {
