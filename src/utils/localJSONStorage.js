794823fcbee5bd7b2f8047ad4d9a91c7a461e9c2
reyraa <ali@lightcurve.io>
2020-04-08 08:29:03 +0200

    Use window.localStorage as development fallback

--- src/utils/localJSONStorage.js
+++ src/utils/localJSONStorage.js
@@ -2,21 +2,27 @@ export const setInStorage = (key, value) => {
   const { ipc } = window;
   if (ipc) {
     ipc.send('storeConfig', { key, value });
-    // window.localStorage.setItem(key, JSON.stringify(value));
+  } else {
+    window.localStorage.setItem(key, JSON.stringify(value));
   }
 };
 
 export const getFromStorage = (key, backup, cb) => {
   let info = null;
-  try {
-    const { ipc } = window;
+  const { ipc } = window;
+  if (ipc) {
     ipc.on('configRetrieved', (action, data) => {
       info = data[key];
       cb(info);
     });
     ipc.send('retrieveConfig');
-  } catch (e) {
-    cb(backup);
+  } else {
+    try {
+      const value = JSON.parse(window.localStorage.getItem(key));
+      cb(value);
+    } catch (e) {
+      cb(backup);
+    }
   }
 };
 
