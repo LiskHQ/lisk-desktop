71569ddc241af865b6c0edede888470c251c72a5
reyraa <ali@lightcurve.io>
2020-04-08 12:47:41 +0200

    Remove redundant modules imported

--- test/cypress/features/dashboard/dashboard.js
+++ test/cypress/features/dashboard/dashboard.js
@@ -1,10 +1,7 @@
 /* eslint-disable */
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-import networks from '../../../constants/networks';
-import ss from '../../../constants/selectors';
 import accounts from '../../../constants/accounts';
-import urls from '../../../constants/urls';
 
 Given(/^I have a bookmark saved$/, function () {
-  window.localStorage.setItem('bookmarks', `[{"title":"Alice","address":"${accounts.genesis.address}","balance":101}]`);
+  window.localStorage.setItem('bookmarks', `{"LSK":[{"title":"Alice","address":"${accounts.genesis.address}","balance":101}],"BTC":[]}`);
 });
