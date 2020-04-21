64b8de8d84087b0c757031b1c100954689fe8dbc
reyraa <ali@lightcurve.io>
2020-04-21 08:25:27 +0200

    Add filters and tabs

---
+++ src/components/screens/wallet/transactions/filterDropdown.js
@@ -0,0 +1,30 @@
+import { withTranslation } from 'react-i18next';
+import React from 'react';
+import FilterDropdownButton from '../../../shared/filterDropdownButton';
+
+const FilterDropdown = ({ t, filters, applyFilters }) => {
+  const fields = [
+    {
+      label: t('Date Range'),
+      name: 'date',
+      type: 'date-range',
+    },
+    {
+      label: t('Amount Range'),
+      name: 'amount',
+      type: 'number-range',
+    },
+    {
+      label: t('Message'),
+      placeholder: t('Write message'),
+      name: 'message',
+      type: 'text',
+    },
+  ];
+
+  const props = { fields, filters, applyFilters };
+
+  return <FilterDropdownButton {...props} />;
+};
+
+export default withTranslation()(FilterDropdown);
--- src/components/screens/wallet/transactions/index.js
+++ src/components/screens/wallet/transactions/index.js
@@ -1,27 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Box from '../../../toolbox/box';
 import BoxHeader from '../../../toolbox/box/header';
 import BoxContent from '../../../toolbox/box/content';
+import txFilters from '../../../../constants/transactionFilters';
+import BoxTabs from '../../../toolbox/tabs';
 import Table from '../../../toolbox/table';
 import styles from './transactions.css';
 import header from './tableHeader';
+import FilterBar from '../../../shared/filterBar';
+import withFilters from '../../../../utils/withFilters';
 import TransactionRow from './transactionRow';
+import FilterDropdown from './filterDropdown';
+
+const tabsData = (t, activeToken) => {
+  const all = {
+    name: t('All transactions'),
+    value: txFilters.all,
+    className: 'filter-all',
+  };
+  const filtered = [
+    {
+      name: t('Incoming transactions'),
+      value: txFilters.incoming,
+      className: 'filter-in',
+    },
+    {
+      name: t('Outgoing transactions'),
+      value: txFilters.outgoing,
+      className: 'filter-out',
+    },
+  ];
+
+  if (activeToken === 'LSK') return [all, ...filtered];
+  return [all];
+};
+
+const Tabs = ({ t, onTabChange }) => {
+  const activeToken = useSelector(state => state.settings.token.active);
+  const tabs = tabsData(t, activeToken);
+  const [active, setActive] = useState(tabs[0].value);
+
+  return (
+    <BoxTabs
+      tabs={tabs}
+      active={active}
+      onClick={({ value }) => {
+        setActive(value);
+        onTabChange({ tab: value });
+      }}
+    />
+  );
+};
 
 const Transactions = ({
   transactions,
+  filters,
+  applyFilters,
+  clearFilter,
+  clearAllFilters,
   t,
 }) => {
   /* istanbul ignore next */
   const handleLoadMore = () => {
     transactions.loadData({ offset: transactions.data.length });
   };
-  const canLoadMore = transactions.meta ? transactions.meta.count === 30 : false;
+
+  const canLoadMore = transactions.meta
+    ? transactions.meta.count > transactions.data.length
+    : false;
+
+  const formatters = {
+    dateFrom: value => `${t('From')}: ${value}`,
+    dateTo: value => `${t('To')}: ${value}`,
+    amountFrom: value => `> ${value}`,
+    amountTo: value => `< ${value}`,
+    message: value => `${t('Message')}: ${value}`,
+  };
 
   return (
     <Box main isLoading={transactions.isLoading} className="transactions-box">
       <BoxHeader>
-        <h1>{t('All transactions')}</h1>
+        <Tabs t={t} onTabChange={applyFilters} />
+        <FilterDropdown filters={filters} applyFilters={applyFilters} />
       </BoxHeader>
+      <FilterBar {...{
+        clearFilter, clearAllFilters, filters, formatters, t,
+      }}
+      />
       <BoxContent className={styles.content}>
         <Table
           data={transactions.data}
@@ -31,10 +97,20 @@ const Transactions = ({
           header={header(t)}
           error={transactions.error}
           canLoadMore={canLoadMore}
+          additionalRowProps={{ t }}
         />
       </BoxContent>
     </Box>
   );
 };
 
-export default Transactions;
+const defaultFilters = {
+  dateFrom: '',
+  dateTo: '',
+  amountFrom: '',
+  amountTo: '',
+  message: '',
+};
+const defaultSort = 'timestamp:desc';
+
+export default withFilters('transactions', defaultFilters, defaultSort)(Transactions);
