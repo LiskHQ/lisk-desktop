5621cf32fdb6dd0c7be74246937995ed70115ca1
yasharAyari <yashar@lightcurve.io>
2020-04-22 12:22:41 +0200

    Move transactionAmount component to the shared component folder

---
+++ src/components/shared/transactionAmount/index.js
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import LiskAmount from '../liskAmount';
+import DiscreetMode from '../discreetMode';
+import styles from './transactionAmount.css';
+import transactionTypes from '../../../constants/transactionTypes';
+
+const TransactionAmount = ({
+  sender, recipient, type, token, roundTo, host, amount,
+}) => {
+  const isIncoming = host === recipient && sender !== recipient;
+  return (
+    <div className={`${styles.wrapper} transaction-amount`}>
+      { type === transactionTypes().send.code
+        ? (
+          <DiscreetMode shouldEvaluateForOtherAccounts>
+            <span className={isIncoming ? styles.receive : ''}>
+              {isIncoming ? '' : '- '}
+              <LiskAmount val={amount} roundTo={roundTo} token={token} />
+            </span>
+          </DiscreetMode>
+        )
+        : '-'
+      }
+    </div>
+  );
+};
+
+TransactionAmount.propTypes = {
+  host: PropTypes.string,
+  sender: PropTypes.string.isRequired,
+  recipient: PropTypes.string,
+  token: PropTypes.string.isRequired,
+  type: PropTypes.number.isRequired,
+  amount: PropTypes.string,
+  roundTo: PropTypes.number,
+};
+
+TransactionAmount.defaultProps = {
+  roundTo: 8,
+};
+
+export default TransactionAmount;
