03e5ee507cd6f7045361477bacff6fc4b100418f
reyraa <ali@lightcurve.io>
2020-04-23 10:53:56 +0200

    Use Redux store to access data

--- src/components/screens/wallet/index.js
+++ src/components/screens/wallet/index.js
@@ -1,14 +1,11 @@
 /* istanbul ignore file */
-import React from 'react';
-import { compose } from 'redux';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { withTranslation } from 'react-i18next';
-import withData from '../../../utils/withData';
 import Overview from './overview';
 import Header from './header';
 import Transactions from './transactions';
-import { getAccount } from '../../../utils/api/account';
-import { getTransactions } from '../../../utils/api/transactions';
+import { getTransactions } from '../../../actions/transactions';
 import txFilters from '../../../constants/transactionFilters';
 
 const filterNames = ['message', 'dateFrom', 'dateTo', 'amountFrom', 'amountTo', 'direction'];
@@ -29,11 +26,31 @@ const transformParams = params => Object.keys(params)
   }, { filters: {} });
 
 
-const Wallet = ({ transactions, t, match }) => {
+const Wallet = ({ t, match }) => {
+  const dispatch = useDispatch();
   const account = useSelector(state => state.account);
   const activeToken = useSelector(state => state.settings.token.active);
   const bookmarks = useSelector(state => state.bookmarks);
   const { discreetMode } = useSelector(state => state.settings);
+  const { confirmed, pending } = useSelector(state => state.transactions);
+  const transactions = {
+    data: confirmed,
+    loadData: (params) => {
+      const modified = transformParams(params);
+      modified.address = account.info[activeToken].address;
+      dispatch(getTransactions(modified));
+    },
+    isLoading: false,
+    error: false,
+  };
+
+  useEffect(() => {
+    transactions.loadData({
+      offset: 0,
+      limit: 30,
+      direction: txFilters.all,
+    });
+  }, []);
 
   return (
     <section>
@@ -41,62 +58,26 @@ const Wallet = ({ transactions, t, match }) => {
         bookmarks={bookmarks}
         address={account.info[activeToken].address}
         match={match}
-        account={account}
+        publicKey={account.info[activeToken].publicKey || ''}
         activeToken={activeToken}
         t={t}
       />
       <Overview
         t={t}
-        account={account.info[activeToken]}
+        address={account.info[activeToken].address}
+        balance={account.info[activeToken].balance || 0}
         activeToken={activeToken}
-        transactions={transactions.data}
+        transactions={confirmed}
         discreetMode={discreetMode}
       />
       <Transactions
         transactions={transactions}
-        activeToken={activeToken}
         host={account.info[activeToken].address}
+        activeToken={activeToken}
         t={t}
       />
     </section>
   );
 };
 
-const apis = {
-  detailAccount: {
-    apiUtil: (liskAPIClient, params) => getAccount({ liskAPIClient, ...params }),
-    autoload: true,
-    getApiParams: state => ({
-      token: state.settings.token.active,
-      address: state.account.info[state.settings.token.active].address,
-      networkConfig: state.network,
-    }),
-  },
-  transactions: {
-    apiUtil: (apiClient, params) => getTransactions(transformParams(params)),
-    autoload: true,
-    getApiParams: state => ({
-      token: state.settings.token.active,
-      address: state.account.info[state.settings.token.active].address,
-      networkConfig: state.network,
-    }),
-    defaultData: [],
-    defaultUrlSearchParams: {
-      filters: {
-        direction: txFilters.all,
-      },
-    },
-    transformResponse: (response, oldData, urlSearchParams) => (
-      urlSearchParams.offset
-        ? [...oldData, ...response.data]
-        : response.data
-    ),
-  },
-};
-
-const ComposedWallet = compose(
-  withData(apis),
-  withTranslation(),
-)(Wallet);
-
-export default ComposedWallet;
+export default withTranslation()(Wallet);
