1fe7b0fe00511fa5a0d72cb40a0d5aac87ab7a53
reyraa <ali@lightcurve.io>
2020-05-06 07:54:40 +0200

    Use the new data row components

--- src/components/screens/transactionDetails/transactions.js
+++ src/components/screens/transactionDetails/transactions.js
@@ -1,22 +1,19 @@
 import { withTranslation } from 'react-i18next';
 import React from 'react';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
-import { DateTimeFromTimestamp } from '../../toolbox/timestamp';
 import { SecondaryButton } from '../../toolbox/buttons/button';
-import { tokenMap } from '../../../constants/tokens';
 import Box from '../../toolbox/box';
 import BoxHeader from '../../toolbox/box/header';
 import BoxContent from '../../toolbox/box/content';
-import BoxRow from '../../toolbox/box/row';
 import CopyToClipboard from '../../toolbox/copyToClipboard';
-import DiscreetMode from '../../shared/discreetMode';
-import LiskAmount from '../../shared/liskAmount';
 import NotFound from '../../shared/notFound';
-import Tooltip from '../../toolbox/tooltip/tooltip';
-import TransactionDetailView from './transactionDetailView/transactionDetailView';
+import TransactionVotes from './transactionVotes';
 import routes from '../../../constants/routes';
+import {
+  DateAndConfirmation, FeeAndAmount, TransactionId,
+  Sender, Recipient, Message, Illustration,
+} from './dataRows';
 import styles from './transactions.css';
-import transactionTypes from '../../../constants/transactionTypes';
 
 function addVotesWithDelegateNames(transaction, delegates, t) {
   const getVotesStartingWith = sign => (
@@ -65,7 +62,7 @@ class Transactions extends React.Component {
 
   render() {
     const {
-      t, activeToken, address, delegates, netCode,
+      t, activeToken, delegates, netCode,
     } = this.props;
     const transaction = addVotesWithDelegateNames(this.props.transaction.data, delegates.data, t);
     const { error, isLoading } = this.props.transaction;
@@ -86,85 +83,33 @@ class Transactions extends React.Component {
               />
             </BoxHeader>
             <BoxContent className={styles.mainContent}>
-              <TransactionDetailView
-                netCode={netCode}
-                address={address}
-                activeToken={activeToken}
+              <Illustration transaction={transaction} />
+              <Sender
                 transaction={transaction}
-              >
-                <BoxRow>
-                  <div className={styles.value}>
-                    <span className={styles.label}>{t('Date')}</span>
-                    <span className={`${styles.date} tx-date`}>
-                      <DateTimeFromTimestamp
-                        fulltime
-                        className="date"
-                        time={transaction.timestamp}
-                        token={activeToken}
-                        showSeconds
-                      />
-                    </span>
-                  </div>
-                  <div className={`${styles.value}`}>
-                    <span className={styles.label}>
-                      {t('Confirmations')}
-                      <Tooltip className="showOnTop">
-                        <p>
-                          { t('Confirmations refer to the number of blocks added to the {{token}} blockchain after a transaction has been submitted. The more confirmations registered, the more secure the transaction becomes.', { token: tokenMap[activeToken].label })}
-                        </p>
-                      </Tooltip>
-                    </span>
-                    <span className="tx-confirmation">
-                      {transaction.confirmations || 0}
-                    </span>
-                  </div>
-                </BoxRow>
-                <BoxRow>
-                  <div className={`${styles.value}`}>
-                    <span className={styles.label}>
-                      {t('Transaction ID')}
-                    </span>
-                    <span className="transaction-id">
-                      <CopyToClipboard
-                        value={transaction.id}
-                        className="tx-id"
-                        containerProps={{
-                          size: 'xs',
-                          className: 'copy-title',
-                        }}
-                        copyClassName={styles.copyIcon}
-                      />
-                    </span>
-                  </div>
-                </BoxRow>
-                <BoxRow>
-                  { transaction.type === transactionTypes().send.code
-                    ? (
-                      <div className={styles.value}>
-                        <span className={styles.label}>
-                          {t('Amount')}
-                        </span>
-                        <DiscreetMode addresses={addresses} shouldEvaluateForOtherAccounts>
-                          <span className="tx-amount">
-                            <LiskAmount val={transaction.amount} />
-                            {' '}
-                            {activeToken}
-                          </span>
-                        </DiscreetMode>
-                      </div>
-                    ) : null }
-                  <div className={styles.value}>
-                    <span className={styles.label}>
-                      {t('Transaction fee')}
-                    </span>
-                    <span className="tx-fee">
-                      <LiskAmount val={transaction.fee} />
-                      {' '}
-                      {activeToken}
-                    </span>
-                  </div>
-                </BoxRow>
-              </TransactionDetailView>
+                activeToken={activeToken}
+                netCode={netCode}
+              />
+              <Recipient
+                transaction={transaction}
+                activeToken={activeToken}
+                netCode={netCode}
+                t={t}
+              />
+              <DateAndConfirmation
+                transaction={transaction}
+                activeToken={activeToken}
+                addresses={addresses}
+                t={t}
+              />
+              <TransactionId t={t} id={transaction.id} />
+              <FeeAndAmount
+                transaction={transaction}
+                activeToken={activeToken}
+                addresses={addresses}
+                t={t}
+              />
+              <Message activeToken={activeToken} transaction={transaction} t={t} />
+              <TransactionVotes transaction={transaction} t={t} />
             </BoxContent>
           </Box>
         ) : (
