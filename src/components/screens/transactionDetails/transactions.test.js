69cf6faa935607e8e22dceeedbdad1c7813445f5
reyraa <ali@lightcurve.io>
2020-05-06 07:54:55 +0200

    Improve unit test coverage

--- src/components/screens/transactionDetails/transactions.test.js
+++ src/components/screens/transactionDetails/transactions.test.js
@@ -7,32 +7,35 @@ import fees from '../../../constants/fees';
 import transactionTypes from '../../../constants/transactionTypes';
 
 describe('Single Transaction Component', () => {
-  let wrapper;
   const transaction = {
-    senderId: accounts.genesis.address,
-    recipientId: accounts.delegate.address,
-    amount: 100000,
-    asset: {
-      data: 'Transaction message',
+    data: {
+      senderId: accounts.genesis.address,
+      recipientId: accounts.delegate.address,
+      amount: 100000,
+      asset: {
+        data: 'Transaction message',
+      },
+      confirmation: 1,
+      type: 0,
+      id: 123,
+      fee: fees.send,
+      timestamp: Date.now(),
     },
-    confirmation: 1,
-    type: 0,
-    id: 123,
-    fee: fees.send,
-    timestamp: Date.now(),
   };
   const voteTransaction = {
-    type: transactionTypes().vote.code,
-    amount: '0',
-    fee: Lisk.transaction.constants.VOTE_FEE.toString(),
-    senderId: accounts.genesis.address,
-    recipientId: accounts.delegate.address,
-    timestamp: Lisk.transaction.utils.getTimeFromBlockchainEpoch() - 100,
-    asset: {
-      votes: [
-        accounts.delegate.publicKey,
-        accounts.delegate_candidate.publicKey,
-      ].map(publicKey => `+${publicKey}`),
+    data: {
+      type: transactionTypes().vote.code,
+      amount: '0',
+      fee: Lisk.transaction.constants.VOTE_FEE.toString(),
+      senderId: accounts.genesis.address,
+      recipientId: accounts.delegate.address,
+      timestamp: Lisk.transaction.utils.getTimeFromBlockchainEpoch() - 100,
+      asset: {
+        votes: [
+          accounts.delegate.publicKey,
+          accounts.delegate_candidate.publicKey,
+        ].map(publicKey => `+${publicKey}`),
+      },
     },
   };
 
@@ -58,23 +61,14 @@ describe('Single Transaction Component', () => {
   };
 
   describe('Transfer transactions', () => {
-    beforeEach(() => {
-      wrapper = mount(<Transactions {...props} />);
-    });
-
-    it('Should render transaction details after transaciton loaded', () => {
-      wrapper.setProps({
-        ...props,
-        transaction: {
-          data: transaction,
-          isLoading: false,
-        },
-      });
+    it('Should render transaction details after transaction loaded', () => {
+      const wrapper = mount(<Transactions {...props} transaction={transaction} />);
       expect(wrapper.find('header h1')).toHaveText('Transaction details');
-      expect(wrapper.find('.transaction-id .copy-title').first().text().trim()).toBe(`${transaction.id}`);
+      expect(wrapper.find('.transaction-id .copy-title').first().text().trim()).toBe(`${transaction.data.id}`);
     });
 
     it('Should redirect to dashboard if activeToken changes', () => {
+      const wrapper = mount(<Transactions {...props} />);
       wrapper.setProps({
         ...props,
         activeToken: 'BTC',
@@ -83,12 +77,10 @@ describe('Single Transaction Component', () => {
     });
 
     it('Should load delegate names after vote transaction loading finished', () => {
+      const wrapper = mount(<Transactions {...props} />);
       wrapper.setProps({
         ...props,
-        transaction: {
-          data: voteTransaction,
-          isLoading: false,
-        },
+        transaction: voteTransaction,
       });
       expect(props.delegates.loadData).toHaveBeenCalledWith({
         publicKey: accounts.delegate.publicKey,
@@ -97,11 +89,114 @@ describe('Single Transaction Component', () => {
         publicKey: accounts.delegate_candidate.publicKey,
       });
     });
+
+    it('Should render transfer transaction with message (LSK)', () => {
+      const wrapper = mount(<Transactions {...props} transaction={transaction} />);
+      expect(wrapper).toContainMatchingElements(2, '.accountInfo');
+      expect(wrapper.find('.accountInfo .sender-address').text()).toBe(transaction.data.senderId);
+      expect(wrapper.find('.accountInfo .receiver-address').text()).toBe(transaction.data.recipientId);
+      expect(wrapper).toContainExactlyOneMatchingElement('.tx-reference');
+    });
+
+    it('Should not render transfer transaction with message (BTC)', () => {
+      const wrapper = mount(<Transactions {...props} activeToken="BTC" />);
+      expect(wrapper).not.toContain('.tx-reference');
+    });
+
+    it('Should show the delegate name if the sender is a Lisk delegate', () => {
+      const delegateTransaction = {
+        data: {
+          type: 3,
+          senderId: accounts.genesis.address,
+          recipientId: '',
+          amount: 0,
+          id: 123,
+          asset: {
+            delegate: { username: 'genesis' },
+            votes: [
+              accounts.delegate.publicKey,
+              accounts.delegate_candidate.publicKey,
+            ].map(publicKey => `+${publicKey}`),
+          },
+        },
+      };
+      const wrapper = mount(
+        <Transactions
+          {...props}
+          activeToken="BTC"
+          transaction={delegateTransaction}
+        />,
+      );
+      expect(wrapper).not.toContain('genesis');
+    });
+  });
+
+  describe('Delegate vote transaction', () => {
+    it('Should render delegate vote details', () => {
+      const wrapper = mount(
+        <Transactions
+          {...props}
+          transaction={{
+            data: {
+              type: 3,
+              senderId: accounts.genesis.address,
+              recipientId: '',
+              amount: 0,
+              id: 123,
+            },
+          }}
+        />,
+      );
+      expect(wrapper).toContainExactlyOneMatchingElement('.accountInfo');
+      expect(wrapper.find('.accountInfo .label').text()).toBe('Voter');
+    });
+  });
+
+  describe('2nd Passphrase transaction', () => {
+    it('Should render register 2nd passphrase details', () => {
+      const wrapper = mount(
+        <Transactions
+          {...props}
+          transaction={{
+            data: {
+              type: 1,
+              senderId: accounts.genesis.address,
+              recipientId: '',
+              amount: 0,
+              id: 123,
+            },
+          }}
+        />,
+      );
+      expect(wrapper).toContainExactlyOneMatchingElement('.accountInfo');
+      expect(wrapper.find('.accountInfo .label').text()).toBe('Account');
+    });
+  });
+
+  describe('Register delegate transaction', () => {
+    it('Should render register delegate details', () => {
+      const wrapper = mount(
+        <Transactions
+          {...props}
+          transaction={{
+            data: {
+              type: 2,
+              senderId: accounts.delegate.address,
+              recipientId: '',
+              amount: 0,
+              asset: { delegate: accounts.delegate },
+              id: 123,
+            },
+          }}
+        />,
+      );
+      expect(wrapper).toContainExactlyOneMatchingElement('.accountInfo');
+    });
   });
 
   describe('No results', () => {
     it('Should render no result screen', () => {
-      wrapper = mount(<Transactions {...{
+      const wrapper = mount(<Transactions {...{
         ...props,
         transaction: {
           error: 'INVALID_REQUEST_PARAMETER',
