ba252fb1fe3b8250104f1f08c7a1b8831a14b0ed
reyraa <ali@lightcurve.io>
2020-05-06 13:47:03 +0200

    Update unit tests to match props

--- src/components/screens/transactionDetails/transactionVotes.test.js
+++ src/components/screens/transactionDetails/transactionVotes.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import * as reactRedux from 'react-redux';
 import TransactionVotes from './transactionVotes';
+import accounts from '../../../../test/constants/accounts';
 
 const store = {
   network: {
@@ -13,47 +14,46 @@ const store = {
 describe('Transaction Votes', () => {
   let wrapper;
   const props = {
-    votes: {
-      added: [{ username: 'delegate1', rank: '1', account: { address: '123L' } }],
-      deleted: [{ username: 'delegate2', rank: '2', account: { address: '12345L' } }],
+    transaction: {
+      type: 3,
+      asset: {
+        votes: [accounts.delegate_candidate, accounts.delegate]
+          .map((item, i) => `${i > 0 ? '+' : '-'}${item.publicKey}`),
+      },
     },
     t: v => v,
+    delegates: {
+      data: {},
+      loadData: jest.fn(),
+    },
   };
   reactRedux.useSelector = jest.fn().mockImplementation(filter => filter(store));
 
   it('Should render with added and deleted Votes', () => {
     wrapper = mount(<TransactionVotes {...props} />);
     expect(wrapper).toContainMatchingElements(2, '.votesContainer');
-    expect(wrapper.find('.rank').first().text()).toEqual(`#${props.votes.added[0].rank}`);
-    expect(wrapper.find('.username').first().text()).toEqual(props.votes.added[0].username);
+    expect(wrapper.find('.rank').first().text()).toEqual('#-');
+    expect(wrapper.find('.username').first().text()).toEqual('Loading...');
   });
 
-  it('Should only render added votes and sort by rank', () => {
-    const addedProps = {
+  it('Should fetch and render delegate names and ranks', () => {
+    wrapper = mount(<TransactionVotes {...props} />);
+    wrapper.setProps({
       ...props,
-      votes: {
-        added: [
-          { username: 'delegate2', rank: '2', account: { address: '12345L' } },
-          { username: 'delegate1', rank: '1', account: { address: '123L' } },
-        ],
+      delegates: {
+        data: {
+          [accounts.delegate_candidate.publicKey]: {
+            ...accounts.delegate_candidate,
+            account: { address: accounts.delegate_candidate.address },
+          },
+          [accounts.delegate.publicKey]: {
+            ...accounts.delegate,
+            account: { address: accounts.delegate.address },
+          },
+        },
+        loadData: jest.fn(),
       },
-    };
-    wrapper = mount(<TransactionVotes {...addedProps} />);
-    expect(wrapper).toContainMatchingElements(1, '.votesContainer.added');
-    expect(wrapper.find('.rank').at(1).text()).toBe(`#${addedProps.votes.added[1].rank}`);
-  });
-
-  it('Should only render removed votes', () => {
-    const deleteProps = {
-      ...props,
-      votes: {
-        deleted: [
-          { username: 'delegate2', rank: '2', account: { address: '12345L' } },
-          { username: 'delegate1', rank: '1', account: { address: '123L' } },
-        ],
-      },
-    };
-    wrapper = mount(<TransactionVotes {...deleteProps} />);
+    });
     expect(wrapper).toContainMatchingElements(1, '.votesContainer.deleted');
   });
 });
