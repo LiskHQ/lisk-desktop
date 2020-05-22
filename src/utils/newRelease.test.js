27248aad24e5c6c0d4767821ceb67498132b3de4
reyraa <ali@lightcurve.io>
2020-05-22 13:02:30 +0200

    Increase unit test coverage

--- src/utils/newRelease.test.js
+++ src/utils/newRelease.test.js
@@ -52,4 +52,19 @@ describe('new release util', () => {
     dialogWrapper.update();
     expect(dialogWrapper).toIncludeText('dummy text');
   });
+
+  it('Should initiate the update process if clicked on updateNow', () => {
+    const spy = jest.spyOn(FlashMessageHolder, 'deleteMessage');
+    const wrapper = mount(<FlashMessageHolder />);
+    const dialogWrapper = mount(<DialogHolder />);
+    const version = '1.20.1';
+    const releaseNotes = '<h4>dummy text</h4><h3>Fixed bugs</h3>';
+    callbacks['update:available']({}, { version, releaseNotes });
+    wrapper.update();
+    wrapper.find('button').at(0).simulate('click');
+    jest.runAllTimers();
+    dialogWrapper.update();
+    expect(ipc.send).toHaveBeenCalledWith('update:started');
+    expect(spy).toHaveBeenCalledWith('NewRelease');
+  });
 });
