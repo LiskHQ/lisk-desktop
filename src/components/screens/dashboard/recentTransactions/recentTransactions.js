248e69058fa155142509ed0e18b45a6ccd515c53
reyraa <ali@lightcurve.io>
2020-05-25 15:56:21 +0200

    Create reccent transactios list using function components

--- src/components/screens/dashboard/recentTransactions/recentTransactions.js
+++ src/components/screens/dashboard/recentTransactions/recentTransactions.js
@@ -1,82 +1,85 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { withTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import routes from '../../../../constants/routes';
+import { SecondaryButton } from '../../../toolbox/buttons/button';
+import { getActiveTokenAccount } from '../../../../utils/account';
 import { tokenMap } from '../../../../constants/tokens';
 import Box from '../../../toolbox/box';
 import BoxHeader from '../../../toolbox/box/header';
+import BoxContent from '../../../toolbox/box/content';
 import BoxEmptyState from '../../../toolbox/box/emptyState';
 import Icon from '../../../toolbox/icon';
-import TransactionList from './transactionList';
 import styles from './recentTransactions.css';
-import txFilters from '../../../../constants/transactionFilters';
+import Table from '../../../toolbox/table';
+import header from './tableHeader';
+import TransactionRow from './transactionRow';
 
-class RecentTransactions extends Component {
-  constructor(props) {
-    super(props);
+export const NoTransactions = withTranslation()(({ t }) => {
+  const activeToken = useSelector(state => tokenMap[state.settings.token.active]);
+  return (
+    <BoxEmptyState>
+      <Icon name="iconEmptyRecentTransactions" />
+      <h1>{t('No Transactions Yet')}</h1>
+      <p>{t('A great way to start is to top up your account with some {{value}}.', { value: activeToken.key })}</p>
+    </BoxEmptyState>
+  );
+});
 
-    if (!props.transactions.length && props.account.address) {
-      props.getTransactions({
-        address: props.account.address,
-        filters: {
-          direction: txFilters.all,
-        },
-      });
+export const NotSignedIn = withTranslation()(({ t }) => (
+  <BoxEmptyState>
+    <Icon name="iconEmptyRecentTransactions" />
+    <h1>{t('Sign in to view recent transactions')}</h1>
+    <p>{t('In order to see your recent transactions you need to sign in.')}</p>
+  </BoxEmptyState>
+));
+
+const RecentTransactions = ({ className, t, transactions }) => {
+  const account = useSelector(state => getActiveTokenAccount(state));
+  const [isLoaded, setLoaded] = useState(!!transactions.data.length);
+  // const bookmarks = useSelector(state => state.bookmarks);
+  const settings = useSelector(state => state.settings);
+  const activeToken = tokenMap[settings.token.active];
+
+  useEffect(() => {
+    if (account.passphrase && !isLoaded && !transactions.data.length) {
+      setLoaded(true);
+      transactions.loadData();
     }
-  }
+  }, [account]);
 
-  render() {
-    const {
-      account,
-      bookmarks,
-      className,
-      isLoggedIn,
-      settings,
-      t,
-      transactions,
-    } = this.props;
-    const activeToken = tokenMap[settings.token.active];
+  console.log(transactions.data.length, transactions.isLoading);
 
-    return (
-      <Box className={`${styles.box} ${className}`}>
-        <BoxHeader>
-          <h2 className={styles.title}>{t('Recent {{value}} transactions', { value: activeToken.label })}</h2>
-        </BoxHeader>
-        {
-          isLoggedIn && transactions.length
-            ? (
-              <TransactionList
-                account={account}
-                activeToken={activeToken.key}
-                bookmarks={bookmarks}
-                transactions={transactions}
-                t={t}
-              />
-            )
-            : null
-        }
-        {
-          isLoggedIn && !transactions.length
-            ? (
-              <BoxEmptyState>
-                <Icon name="iconEmptyRecentTransactions" />
-                <h1>{t('No Transactions Yet')}</h1>
-                <p>{t('A great way to start is to top up your account with some {{value}}.', { value: activeToken.key })}</p>
-              </BoxEmptyState>
-            )
-            : null
-        }
-        {
-          !isLoggedIn
-            ? (
-              <BoxEmptyState>
-                <Icon name="iconEmptyRecentTransactions" />
-                <h1>{t('Sign in to view recent transactions')}</h1>
-                <p>{t('In order to see your recent transactions you need to sign in.')}</p>
-              </BoxEmptyState>
-            )
-            : null
-        }
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      isLoading={transactions.isLoading}
+      className={`${styles.box} ${className}`}
+    >
+      <BoxHeader>
+        <h2 className={styles.title}>{t('Recent {{value}} transactions', { value: activeToken.label })}</h2>
+      </BoxHeader>
+      <BoxContent className={styles.content}>
+        <Table
+          data={transactions.data}
+          isLoading={transactions.isLoading}
+          row={TransactionRow}
+          header={header(t)}
+          error={transactions.error}
+          canLoadMore={false}
+          additionalRowProps={{ t, activeToken: activeToken.key, host: account.address }}
+          emptyState={account.passphrase ? NoTransactions : NotSignedIn}
+        />
+        <div className={styles.viewAll}>
+          <Link to={routes.wallet.path} className="view-all">
+            <SecondaryButton size="s">
+              {t('View All')}
+            </SecondaryButton>
+          </Link>
+        </div>
+      </BoxContent>
+    </Box>
+  );
+};
 
 export default RecentTransactions;
