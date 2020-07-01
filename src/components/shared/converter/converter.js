a4f92b3128eb6f3c78623821c4c1445ab7a1274f
reyraa <ali@lightcurve.io>
2020-07-01 15:41:04 +0200

    Remove redundant conditions

--- src/components/shared/converter/converter.js
+++ src/components/shared/converter/converter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import numeral from 'numeral';
 import 'numeral/locales';
-import converter from '../../../constants/converter';
 import styles from './converter.css';
 import i18n from '../../../i18n';
 
@@ -16,25 +15,20 @@ class Converter extends React.Component {
     const {
       settings, value, error, className = '', token, priceTicker,
     } = this.props;
-    const currencies = priceTicker[token];
-
-    const currency = settings.currency || 'USD';
+    const ratio = priceTicker[token][settings.currency];
+    const numericValue = numeral(value).value();
 
     numeral.locale(i18n.language);
 
-    let price = error || Number.isNaN(numeral(value).value())
-      ? 0
-      : (numeral(value).value() * currencies[currency]);
-
-    price = price > converter.maxLSKSupply || Number.isNaN(price) || price < 0 ? 0 : price;
-    price = numeral(price).format('0,0.00');
+    let price = error || Number.isNaN(numericValue) ? 0 : (numericValue * ratio);
+    price = Number.isNaN(price) || price < 0 ? 0 : numeral(price).format('0,0.00');
 
     return (
       <div className={`${styles.wrapper} ${className}`}>
-        {value !== '' && currencies[currency]
+        {value !== '' && ratio
           ? (
             <span className={`${styles.price} converted-price`}>
-              {`~${price} ${currency}`}
+              {`~${price} ${settings.currency}`}
             </span>
           )
           : null
