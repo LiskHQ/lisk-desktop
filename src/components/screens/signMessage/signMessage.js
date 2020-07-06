e48959fd867b77bf0f7afcdaebb303127fc64659
reyraa <ali@lightcurve.io>
2020-07-06 11:07:49 +0200

    Add dialog HOC

--- src/components/screens/signMessage/signMessage.js
+++ src/components/screens/signMessage/signMessage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ConfirmMessage from './confirmMessage';
 import MultiStep from '../../shared/multiStep';
 import SignMessageInput from './signMessageInput';
+import Dialog from '../../toolbox/dialog/dialog';
 import styles from './signMessage.css';
 
 const SignMessage = ({ account, t, history }) => (
-  <section className={styles.wrapper}>
+  <Dialog hasClose className={styles.wrapper}>
     <MultiStep>
       <SignMessageInput t={t} history={history} />
       <ConfirmMessage t={t} account={account} />
     </MultiStep>
-  </section>
+  </Dialog>
 );
 
 export default SignMessage;
