4d64d2753e827687960eafd24e0770ac5d9cbd45
reyraa <ali@lightcurve.io>
2020-07-12 08:41:39 +0200

    Adapt unit tests

--- src/components/screens/send/form/form.test.js
+++ src/components/screens/send/form/form.test.js
@@ -54,7 +54,9 @@ describe('Form', () => {
         push: jest.fn(),
       },
       nextStep: jest.fn(),
-      initialValue: {},
+      initialValue: {
+        recipient: bookmarks.LSK[0].title,
+      },
     };
 
     wrapper = mount(<Form {...props} />, options);
