26a6eaa3cff80bd6d7560c4d86ec5548fa118148
UsamaHameed <usamatech2@gmail.com>
2020-07-15 16:39:02 +0200

    add search param tests and make search param parsing better

--- src/utils/searchParams.js
+++ src/utils/searchParams.js
@@ -1,11 +1,18 @@
 
+/**
+ * returns parsed query params from a url
+ * @param {String} search the search string
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const parseSearchParams = (search) => {
   const searchParams = new URLSearchParams(search);
   const parsedParams = {};
   // eslint-disable-next-line no-restricted-syntax
   for (const [key, value] of searchParams.entries()) {
-    parsedParams[key] = value;
+    const values = value.split(',');
+    if (values.length > 1) {
+      parsedParams[key] = values;
+    } else { parsedParams[key] = value; }
   }
   return parsedParams;
 };
