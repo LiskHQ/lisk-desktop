e8c682c518d213f16fc24023bbfb7caa46351bc2
UsamaHameed <usamatech2@gmail.com>
2020-07-16 16:44:04 +0200

    implement a draft component activation through search params

--- src/components/shared/customRoute/index.js
+++ src/components/shared/customRoute/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
+
 import ErrorBoundary from '../errorBoundary';
 import offlineStyle from '../offlineWrapper/offlineWrapper.css';
 import Piwik from '../../../utils/piwik';
-import routes from '../../../constants/routes';
+import routes, { modals } from '../../../constants/routes';
+import { parseSearchParams } from '../../../utils/searchParams';
+import DialogHolder from '../../toolbox/dialog/holder';
+
+const Content = ({
+  t, isPrivate, pathPrefix, path, pathSuffix, component, exact,
+}) => (
+  <main className={`${isPrivate ? offlineStyle.disableWhenOffline : ''} offlineWrapper`}>
+    <ErrorBoundary errorMessage={t('An error occoured while rendering this page')}>
+      <Route
+        path={`${pathPrefix}${path}${pathSuffix}`}
+        exact={exact}
+        key={path}
+        component={component}
+      />
+    </ErrorBoundary>
+  </main>
+);
 
 // eslint-disable-next-line max-statements
 const CustomRoute = ({
@@ -24,6 +42,8 @@ const CustomRoute = ({
   const networkIsSet = useSelector(state => !!state.network.name && !!state.network.serviceUrl);
   const { search = '' } = history.location;
 
+  const { modal: modalQuery, tab: tabQuery } = parseSearchParams(history.location.search);
+
   if (!networkIsSet) return null;
   Piwik.tracking(history, settings);
 
@@ -39,17 +59,33 @@ const CustomRoute = ({
     );
   }
 
+  if (modalQuery) {
+    const modal = modals[modalQuery];
+    const Component = modal.component;
+    const page = history.location.pathname;
+
+    if (modal.allowedOnlyOnPages) {
+      if (modal.allowedOnlyOnPages.includes(page)) {
+        DialogHolder.showDialog(<Component t={t} />, modalQuery);
+      }
+    } else {
+      DialogHolder.showDialog(<Component t={t} />, modalQuery);
+    }
+  }
+
+  if (tabQuery) {
+    // save the tab in the state and implement tab activation  in the tables etc through the state
+  }
   return (
-    <main className={`${isPrivate ? offlineStyle.disableWhenOffline : ''} offlineWrapper`}>
-      <ErrorBoundary errorMessage={t('An error occoured while rendering this page')}>
-        <Route
-          path={`${pathPrefix}${path}${pathSuffix}`}
-          exact={exact}
-          key={path}
-          component={component}
-        />
-      </ErrorBoundary>
-    </main>
+    <Content
+      t={t}
+      isPrivate={isPrivate}
+      component={component}
+      exact={exact}
+      path={path}
+      pathPrefix={pathPrefix}
+      pathSuffix={pathSuffix}
+    />
   );
 };
 
