8dd159c7dca545738f066fde74ead7802172db31
reyraa <ali@lightcurve.io>
2020-07-16 14:25:37 +0200

    ADd loading to transaction list

--- src/components/screens/wallet/index.js
+++ src/components/screens/wallet/index.js
@@ -13,6 +13,7 @@ import Transactions from './transactions';
 import { isEmpty } from '../../../utils/helpers';
 import Send from '../send';
 import DialogHolder from '../../toolbox/dialog/holder';
+import actionTypes from '../../../constants/actions';
 
 const filterNames = ['message', 'dateFrom', 'dateTo', 'amountFrom', 'amountTo', 'direction'];
 /**
@@ -31,12 +32,21 @@ const transformParams = params => Object.keys(params)
     return acc;
   }, { filters: {} });
 
-
+/*
+ * @todo This component should not load transactions.
+ * We should move this logic to the tx tab component.
+ * Since we will remove the tabs with the next release
+ * We can move this logic then.
+ */
+// eslint-disable-next-line max-statements
 const Wallet = ({ t, history }) => {
   const dispatch = useDispatch();
   const account = useSelector(state => state.account);
   const activeToken = useSelector(state => state.settings.token.active);
   const { discreetMode } = useSelector(state => state.settings);
+  const isLoading = useSelector(
+    state => state.loading.indexOf(actionTypes.getTransactions) > -1,
+  );
   const { confirmed, pending, count } = useSelector(state => state.transactions);
   const transactions = {
     data: confirmed,
@@ -45,7 +55,7 @@ const Wallet = ({ t, history }) => {
       modified.address = account.info[activeToken].address;
       dispatch(getTransactions(modified));
     },
-    isLoading: false,
+    isLoading,
     error: false,
     meta: {
       count,
@@ -95,7 +105,7 @@ const Wallet = ({ t, history }) => {
           <VotesTab
             history={history}
             address={account.info[activeToken].address}
-            tabName={t('Voting')}
+            tabName={t('Votes')}
           />
         ) : null}
         {account.info[activeToken].delegate
