8a2fd7936b4dd09a3ac8fc5966b0de62a44eadf9
UsamaHameed <usamatech2@gmail.com>
2020-07-16 16:01:12 +0200

    add more search param utils

--- src/utils/searchParams.js
+++ src/utils/searchParams.js
@@ -16,3 +16,39 @@ export const parseSearchParams = (search) => {
   }
   return parsedParams;
 };
+
+/**
+ * returns parsed query params from a url
+ * @param {object} params the parsed searchParams object
+ */
+// eslint-disable-next-line import/prefer-default-export
+export const strigifySearchParams = (params) => {
+  const result = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of Object.keys(params)) {
+    const value = params[key];
+    result.push(`${key}=${value}`);
+  }
+
+  let stringifiedResult = result.join('&');
+  if (result.length > 0) {
+    stringifiedResult = `?${stringifiedResult}`;
+  }
+
+  return stringifiedResult;
+};
+
+/**
+ * returns parsed query params from a url
+ * @param {String} search the search string
+ * @param {String} key the key of the param to append
+ * @param {String | Number} value the value of the param to append
+ */
+// eslint-disable-next-line import/prefer-default-export
+export const appendSearchParams = (search, key, value) => {
+  const searchParams = parseSearchParams(search);
+  searchParams[key] = value;
+
+  return strigifySearchParams(searchParams);
+};
