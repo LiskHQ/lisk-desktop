3dea5e82fd8ba8029a79c06e582a19208860e3a7
UsamaHameed <usamatech2@gmail.com>
2020-07-21 17:38:39 +0200

    add validation checks to the DialogHolder

--- src/components/toolbox/dialog/holder.js
+++ src/components/toolbox/dialog/holder.js
@@ -1,16 +1,35 @@
 import React, {
   useState, useRef, useMemo,
 } from 'react';
+import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router';
 import styles from './dialog.css';
 import { modals } from '../../../constants/routes';
 import { parseSearchParams, removeSearchParamFromUrl } from '../../../utils/searchParams';
 
+// eslint-disable-next-line max-statements
 const DialogHolder = ({ history }) => {
   const modalName = useMemo(() => {
     const { modal = '' } = parseSearchParams(history.location.search);
     return modals[modal] ? modal : undefined;
   }, [history.location.search]);
+
+  if (!modalName) {
+    return null;
+  }
+
+  const settings = useSelector(state => state.settings);
+  const networkIsSet = useSelector(state => !!state.network.name && !!state.network.serviceUrl);
+  const isAuthenticated = useSelector(state =>
+    (state.account.info && state.account.info[settings.token.active]));
+
+  if (!networkIsSet || modals[modalName].forbiddenTokens.includes(settings.token.active)) {
+    return null;
+  }
+
+  if (modals[modalName].isPrivate && !isAuthenticated) {
+    return null;
+  }
 
   const backdropRef = useRef();
   const [dismissed, setDismissed] = useState(true);
@@ -26,6 +45,7 @@ const DialogHolder = ({ history }) => {
       setDismissed(false);
     }
   };
+
 
   const ModalComponent = useMemo(() => {
     if (modalName) {
