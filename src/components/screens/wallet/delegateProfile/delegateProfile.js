4bd22d40558d6a1f828c7aa537601868f7f0848d
reyraa <ali@lightcurve.io>
2020-07-23 23:17:16 +0200

    Use vote and date components. Use isolated async data

--- src/components/screens/wallet/delegateProfile/delegateProfile.js
+++ src/components/screens/wallet/delegateProfile/delegateProfile.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import moment from 'moment';
+import React, { useEffect } from 'react';
 import { withTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import Box from '../../../toolbox/box';
 import BoxHeader from '../../../toolbox/box/header';
 import BoxContent from '../../../toolbox/box/content';
+import { DateTimeFromTimestamp } from '../../../toolbox/timestamp';
 import VoteWeight from '../../../shared/voteWeight';
 import { formatAmountBasedOnLocale } from '../../../../utils/formattedNumber';
-import { getUnixTimestampFromValue } from '../../../../utils/datetime';
 import { tokenMap } from '../../../../constants/tokens';
 import LiskAmount from '../../../shared/liskAmount';
-import i18n from '../../../../i18n';
 import styles from './delegateProfile.css';
 
 const delegateProfile = ({
-  delegate, account, t, nextForgers,
+  delegate, lastBlock, txDelegateRegister, address, nextForgers, t = str => str,
 }) => {
   const { apiVersion } = useSelector(state => state.network.networks.LSK);
-  moment.locale(i18n.language);
-  delegate = {
-    ...account.delegate,
-    ...delegate.data,
-  };
-  const timeFromLastBlock = delegate.lastBlock !== '-'
-    ? moment(getUnixTimestampFromValue(delegate.lastBlock)).format(t('DD MMM YY, HH:mm'))
-    : '-';
-  const delegateSince = delegate.txDelegateRegister
-    ? getUnixTimestampFromValue(delegate.txDelegateRegister.timestamp)
-    : '-';
-  const isActive = nextForgers.data.filter(item => (item.username === delegate.username)).length;
+  useEffect(() => {
+    delegate.loadData();
+    txDelegateRegister.loadData();
+  }, [address]);
+
+  useEffect(() => {
+    if (delegate.data.username) {
+      lastBlock.loadData({
+        generatorPublicKey: delegate.data.account.publicKey,
+        limit: 1,
+      });
+    }
+  }, [delegate.data.username]);
+
+  const isActive = nextForgers.data.filter(item =>
+    (item.username === delegate.data.username)).length;
 
   return (
     <Box>
@@ -43,7 +44,7 @@ const delegateProfile = ({
               {t('Rank')}
             </span>
             <span className={`${styles.rank} rank`}>
-              {`#${delegate.rank}`}
+              {`#${delegate.data.rank}`}
             </span>
           </li>
           <li className="status">
@@ -56,88 +57,57 @@ const delegateProfile = ({
           </li>
           <li className="delegate-since">
             <span className={styles.label}>{t('Delegate since')}</span>
-            {moment(delegateSince).format(t('DD MMM YYYY'))}
+            {
+              txDelegateRegister.data ? (
+                <DateTimeFromTimestamp
+                  fulltime
+                  className="date"
+                  time={txDelegateRegister.data}
+                  token="LSK"
+                />
+              ) : '-'
+            }
           </li>
           <li className="vote">
             <span className={styles.label}>{t('Vote weight')}</span>
-            <VoteWeight data={delegate} />
+            <VoteWeight data={delegate.data} />
           </li>
           <li className="approval">
             <span className={styles.label}>{t('Approval')}</span>
-            {`${formatAmountBasedOnLocale({ value: delegate.approval })}%`}
+            {`${formatAmountBasedOnLocale({ value: delegate.data.approval })}%`}
           </li>
           <li className="productivity">
             <span className={styles.label}>{t('Productivity')}</span>
-            {`${formatAmountBasedOnLocale({ value: delegate.productivity })}%` }
+            {`${formatAmountBasedOnLocale({ value: delegate.data.productivity })}%` }
           </li>
           <li className="blocks">
             <span className={styles.label}>{t('Blocks forged')}</span>
-            {`${delegate.producedBlocks} (${delegate.missedBlocks})`}
+            {`${delegate.data.producedBlocks} (${delegate.data.missedBlocks})`}
           </li>
           <li className="forged">
             <span className={styles.label}>{t('LSK forged')}</span>
             <span>
-              <LiskAmount val={delegate.rewards} />
+              <LiskAmount val={delegate.data.rewards} />
               {` ${tokenMap.LSK.key}`}
             </span>
           </li>
           <li className="last-forged">
             <span className={styles.label}>{t('Last Forged Block')}</span>
-            {timeFromLastBlock}
+            {
+              typeof lastBlock.data === 'number' ? (
+                <DateTimeFromTimestamp
+                  fulltime
+                  className="date"
+                  time={lastBlock.data}
+                  token="LSK"
+                />
+              ) : '-'
+            }
           </li>
         </ul>
       </BoxContent>
     </Box>
   );
-};
-
-delegateProfile.propTypes = {
-  delegate: PropTypes.shape({
-    data: PropTypes.shape({
-      account: PropTypes.shape({
-        publicKey: PropTypes.string.isRequired,
-      }),
-      approval: PropTypes.number,
-      missedBlocks: PropTypes.number,
-      producedBlocks: PropTypes.number,
-      productivity: PropTypes.number,
-      rank: PropTypes.number,
-      rewards: PropTypes.string,
-      username: PropTypes.string,
-      vote: PropTypes.string,
-      lastBlock: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
-      ]),
-      txDelegateRegister: PropTypes.shape({
-        timestamp: PropTypes.number.isRequired,
-      }),
-    }).isRequired,
-  }).isRequired,
-  account: PropTypes.shape({
-    publicKey: PropTypes.string.isRequired,
-    address: PropTypes.string.isRequired,
-  }).isRequired,
-};
-
-delegateProfile.defaultProps = {
-  delegate: {
-    data: {
-      account: {
-        publicKey: '',
-      },
-      approval: 0,
-      missedBlocks: 0,
-      producedBlocks: 0,
-      productivity: 0,
-      rank: 0,
-      rewards: '',
-      username: '',
-      vote: '',
-      lastBlock: '-',
-      txDelegateRegister: { timestamp: 0 },
-    },
-  },
 };
 
 export default withTranslation()(delegateProfile);
