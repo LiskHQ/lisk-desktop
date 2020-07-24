569dc41110d721beee45959256da149635505829
reyraa <ali@lightcurve.io>
2020-07-24 12:17:33 +0200

    Fix tooltip storybook

--- src/components/toolbox/tooltip/tooltip.stories.js
+++ src/components/toolbox/tooltip/tooltip.stories.js
@@ -7,15 +7,15 @@ storiesOf('Toolbox', module)
   .add('Tooltip', () => (
     <StoryWrapper>
       <h3>Normal state</h3>
-      <Tooltip title="Tooltip title">
+      <Tooltip title="Tooltip title" position="right">
         <p>Tooltip content</p>
       </Tooltip>
       <h3>Simple tooltip</h3>
-      <Tooltip alwaysShow>
+      <Tooltip alwaysShow position="right">
         <p>Tooltip content</p>
       </Tooltip>
       <h3>Expanded tooltip</h3>
-      <Tooltip alwaysShow title="Tooltip title" content={<span>Something else than the icon</span>} footer={<a>Read more</a>}>
+      <Tooltip alwaysShow title="Tooltip title" position="right" content={<span>Something else than the icon</span>} footer={<a>Read more</a>}>
         <p>Tooltip content</p>
       </Tooltip>
     </StoryWrapper>
