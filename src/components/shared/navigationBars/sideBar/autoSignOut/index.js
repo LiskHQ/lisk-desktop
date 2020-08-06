b685b98d4dcff3def9f10ce85a75ef45794fe21c
reyraa <ali@lightcurve.io>
2020-08-06 16:48:03 +0200

    Remove obsolete branches

--- src/components/shared/navigationBars/sideBar/autoSignOut/index.js
+++ src/components/shared/navigationBars/sideBar/autoSignOut/index.js
@@ -8,10 +8,9 @@ const TimeOutToast = ({ t, history, ...props }) => {
   const minutes = parseInt(props.minutes, 10);
   const seconds = parseInt(props.seconds, 10);
 
-  const renderToast = minutes === 0 && (
-    seconds === 1 && 'complete'
-  );
+  const renderToast = minutes === 0 && seconds === 1;
 
+  /* istanbul ignore next */
   return (
     renderToast && toast(
       <div className={styles.toastText}>{t('Session timeout')}</div>, {
