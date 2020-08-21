b1204d6088c9fe4949c4d549d02ed801c3d23929
reyraa <ali@lightcurve.io>
2020-08-21 15:10:28 +0200

    Move fee calculation to API utilities

--- src/components/screens/send/form/useDynamicFeeCalculation.js
+++ src/components/screens/send/form/useDynamicFeeCalculation.js
@@ -1,54 +1,18 @@
 import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import usePromise from 'react-use-promise';
-import { fromRawLsk, toRawLsk } from '../../../../utils/lsk';
 import {
-  getTransactionFeeFromUnspentOutputs,
-  getUnspentTransactionOutputs,
-} from '../../../../utils/api/btc/transactions';
+  getDynamicFee,
+} from '../../../../utils/api/transactions';
 
-// @todo account for LSK dynamic fees
-const useDynamicFeeCalculation = (account, dynamicFeePerByte) => {
-  const {
-    settings: { token: { active: token } },
-    network: networkConfig,
-  } = useSelector(state => state);
-  const { t } = useTranslation();
-  const [unspentTransactionOutputs = []] = usePromise(
-    () => getUnspentTransactionOutputs(account.info[token].address, networkConfig),
-    [account.info[token].address],
+const useDynamicFeeCalculation = async (dynamicFeePerByte, txData) => {
+  const network = useSelector(state => state.network);
+  const token = useSelector(state => state.settings.token.active);
+  const account = useSelector(state => state.account[token]);
+
+  const fee = await getDynamicFee(account, network, txData, dynamicFeePerByte);
+  const maxAmount = await getDynamicFee(
+    account, network, { ...txData, amount: account.balance }, dynamicFeePerByte,
   );
-
-  const getDynamicFee = (txAmount) => {
-    const value = getTransactionFeeFromUnspentOutputs({
-      unspentTransactionOutputs,
-      satoshiValue: toRawLsk(txAmount),
-      dynamicFeePerByte,
-    });
-    const feedback = txAmount === ''
-      ? '-'
-      : `${(value ? '' : t('Invalid amount'))}`;
-    return {
-      value,
-      error: !!feedback,
-      feedback,
-    };
-  };
-
-  const getMaxAmount = () => {
-    const unspentTxOutsTotal = unspentTransactionOutputs.reduce((total, tx) => {
-      total += tx.value;
-      return total;
-    }, 0) /* fallback before unspentTransactionOutputs are loaded */
-    || /* istanbul ignore next */ account.info[token].balance;
-
-    return fromRawLsk(Math.max(
-      0,
-      unspentTxOutsTotal - getDynamicFee(fromRawLsk(unspentTxOutsTotal)).value,
-    ));
-  };
-
-  return [getDynamicFee, getMaxAmount];
+  return [fee, maxAmount];
 };
 
 export default useDynamicFeeCalculation;
