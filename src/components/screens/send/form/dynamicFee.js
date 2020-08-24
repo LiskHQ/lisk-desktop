8c33a9329d6e8d3f7d05b76379700b767307bc0b
iris salcedo <iris.salcedo@lightcurve.io>
2020-08-24 16:54:50 +0200

    Handle input focus and blur status

--- src/components/screens/send/form/dynamicFee.js
+++ src/components/screens/send/form/dynamicFee.js
@@ -3,6 +3,7 @@ import { withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import styles from './form.css';
 import Input from '../../../toolbox/inputs/input';
+import Icon from '../../../toolbox/icon';
 
 const DynamicFee = ({
   t,
@@ -10,18 +11,33 @@ const DynamicFee = ({
   initialFeeIndex = 1,
   priorities,
 }) => {
-  const [selectedPriority, setSelectedPriority] = useState(1);
-  const [fee, setFee] = useState(priorities[initialFeeIndex] ? priorities[initialFeeIndex].fee : 0);
+  const [selectedPriority, setSelectedPriority] = useState(initialFeeIndex);
+  const [customFee, setCustomFee] = useState(0.1);
+  const [showEditIcon, setShowEditIcon] = useState(false);
+  const isCustom = priorities[selectedPriority] && !priorities[selectedPriority].fee;
+  const currentFee = !isCustom && priorities[selectedPriority]
+    ? priorities[selectedPriority].fee : customFee;
 
   const onClickPriority = (e) => {
     e.preventDefault();
     setSelectedPriority(parseInt(e.target.value, 10));
-    setFee(priorities[e.target.value].fee);
+    setShowEditIcon(false);
   };
 
   const onInputChange = (e) => {
     e.preventDefault();
-    setFee(e.target.value);
+    setCustomFee(e.target.value);
+  };
+
+  const onInputBlur = (e) => {
+    e.preventDefault();
+    setCustomFee(e.target.value);
+    setShowEditIcon(true);
+  };
+
+  const onClickCustomEdit = (e) => {
+    e.preventDefault();
+    setShowEditIcon(false);
   };
 
   return (
@@ -43,18 +59,21 @@ const DynamicFee = ({
       </div>
       <div className={`${styles.col}`}>
         <span className={`${styles.fieldLabel}`}>{t('Transaction fee')}</span>
-        {priorities[selectedPriority] && !priorities[selectedPriority].fee
-          ? (
-            <Input
-              type="text"
-              size="m"
-              defaultValue={priorities[0] ? priorities[0].fee : 0.1}
-              onChange={onInputChange}
-              placeholder="size s"
-            />
-          )
-          : <span className={styles.fee}>{`${fee} ${token}`}</span>
-        }
+        {isCustom && !showEditIcon ? (
+          <Input
+            autoFocus
+            type="text"
+            size="m"
+            defaultValue={customFee}
+            onChange={onInputChange}
+            onBlur={onInputBlur}
+          />
+        ) : (
+          <span className={styles.fee}>
+            {`${currentFee} ${token}`}
+            {isCustom && showEditIcon && <Icon name="edit" onClick={onClickCustomEdit} />}
+          </span>
+        )}
       </div>
     </div>
   );
