55a9d7ae08686cd7c1082169c6f6922a68a53458
iris salcedo <iris.salcedo@lightcurve.io>
2020-09-07 18:00:39 +0200

    Add unit test

--- src/components/toolbox/demo.test.js
+++ src/components/toolbox/demo.test.js
@@ -1,9 +1,15 @@
-import { mountWithRouter } from '../../utils/testHelpers';
+import React from 'react';
+import { mount } from 'enzyme';
 import ToolboxDemo from './demo';
 
 describe('ToolboxDemo', () => {
   it('should render', () => {
-    const wrapper = mountWithRouter(ToolboxDemo);
+    const wrapper = mount(<ToolboxDemo />);
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('Should render clickable tab', () => {
+    const wrapper = mount(<ToolboxDemo />);
+    wrapper.find('li[data-value="opt_2"]').first().simulate('click');
   });
 });
--- src/components/toolbox/switcher/demo.js
+++ src/components/toolbox/switcher/demo.js
@@ -1,29 +1,30 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import Switcher from '.';
 import DemoRenderer from '../demoRenderer';
-
-/* eslint-disable-next-line no-console */
-const onClick = console.log;
 
 const SwitcherDemo = () => (
   <React.Fragment>
     <h2>Switcher</h2>
-    <DemoRenderer>
-      <Switcher
-        onClick={onClick}
-        options={[{
-          name: 'Option 1',
-          value: 'opt_1',
-        }, {
-          name: 'Option 2',
-          value: 'opt_2',
-        }, {
-          name: 'Option 3',
-          value: 'opt_3',
-        }]}
-        active="opt_1"
-      />
-    </DemoRenderer>
+    <MemoryRouter
+      initialEntries={[{}]}
+    >
+      <DemoRenderer>
+        <Switcher
+          options={[{
+            name: 'Option 1',
+            value: 'opt_1',
+          }, {
+            name: 'Option 2',
+            value: 'opt_2',
+          }, {
+            name: 'Option 3',
+            value: 'opt_3',
+          }]}
+          active="opt_1"
+        />
+      </DemoRenderer>
+    </MemoryRouter>
   </React.Fragment>
 );
 
