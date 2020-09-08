707e6a907a2ca5077603747ffe9833a5f56c48eb
iris salcedo <iris.salcedo@lightcurve.io>
2020-09-08 16:51:11 +0200

    Update selector

--- test/constants/selectors.js
+++ test/constants/selectors.js
@@ -50,7 +50,7 @@ const ss = {
   requestLink: '.request-link',
   backButton: '.back',
   walletTab: 'li[data-value=\'Wallet\']',
-  votesTab: 'li[data-value=\'Votes\']',
+  votesTab: 'li[data-value=\'votes\']',
   delegateStatisticsTab: '.delegate-statistics ',
   votedAddress: '.votes .voter-address',
   voteRow: '.vote-row',
