3de1570298db4915565f259798efea50615aa1ce
reyraa <ali@lightcurve.io>
2020-09-21 22:00:38 +0200

    Update mock votes

--- test/constants/votes.js
+++ test/constants/votes.js
@@ -1,32 +1,16 @@
 export default [
   {
-    address: '1478505779553195737L',
-    publicKey: '5c4af5cb0c1c92df2ed4feeb9751e54e951f9d3f77196511f13e636cf6064e74',
-    balance: '2515148513',
-    username: 'genesis_11',
-  },
-  {
-    address: '12254605294831056546L',
-    publicKey: '141b16ac8d5bd150f16b1caa08f689057ca4c4434445e56661831f4e671b7c0a',
-    balance: '3015148513',
-    username: 'genesis_2',
-  },
-  {
-    address: '8273455169423958419L',
-    publicKey: '9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f',
-    balance: '2515148513',
-    username: 'genesis_1',
-  },
-  {
-    address: '16807489144327319524L',
-    publicKey: '82174ee408161186e650427032f4cfb2496f429b4157da78888cbcea39c387fc',
-    balance: '4015148513',
-    username: 'genesis_32',
-  },
-  {
-    address: '2581762640681118072L',
-    publicKey: '01389197bbaf1afb0acd47bbfeabb34aca80fb372a8f694a1c0716b3398db746',
-    balance: '2515148513',
-    username: 'genesis_51',
+    amount: '1000000000000',
+    delegateAddress: '5059876081639179984L',
+    delegate: {
+      username: 'genesis_1',
+      totalVotesReceived: '1000000000000',
+      delegate: {
+        isBanned: false,
+        pomHeights: [478, 555],
+        lastForgedHeight: 0,
+        consecutiveMissedBlocks: 0,
+      },
+    },
   },
 ];
