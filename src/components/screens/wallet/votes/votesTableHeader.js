c7327f238e4041a8c8c9b60a1d082b5f4e53a733
reyraa <ali@lightcurve.io>
2020-09-23 14:59:24 +0200

    update vote styles

--- src/components/screens/wallet/votes/votesTableHeader.js
+++ src/components/screens/wallet/votes/votesTableHeader.js
@@ -3,25 +3,12 @@ import styles from './votes.css';
 
 export default t => ([
   {
-    title: t('Rank'),
-    classList: 'hidden',
-  },
-  {
     title: t('Delegate'),
-    classList: `${grid['col-sm-3']} ${grid['col-lg-6']}`,
-  },
-  {
-    title: t('Forged'),
-    classList: grid['col-sm-2'],
-    tooltip: {
-      title: t('Forged'),
-      message: t('Sum of all LSK awarded to a delegate for each block successfully generated on the blockchain.'),
-      position: 'bottom',
-    },
+    classList: `${grid['col-sm-4']} ${grid['col-lg-3']}`,
   },
   {
     title: t('Productivity'),
-    classList: `${grid['col-sm-2']} ${grid['col-lg-2']}`,
+    classList: grid['col-sm-2'],
     tooltip: {
       title: t('Productivity'),
       message: t('% of successfully forged blocks in relation to total blocks that were available for this particular delegate to forge'),
@@ -29,11 +16,24 @@ export default t => ([
     },
   },
   {
-    title: t('Vote weight'),
-    classList: `${grid['col-sm-4']} ${grid['col-lg-2']} ${styles.lastHeading}`,
+    title: t('Rank'),
+    classList: grid['col-sm-1'],
+  },
+  {
+    title: t('Delegate weight'),
+    classList: `${grid['col-sm-3']} ${grid['col-lg-2']}`,
     tooltip: {
-      title: t('Productivity'),
-      message: t('Sum of LSK in all accounts who have voted for this delegate.'),
+      title: t('Delegate weight'),
+      message: t('The total amount of all votes a delegate has received.'),
+      position: 'bottom',
+    },
+  },
+  {
+    title: t('Vote amount'),
+    classList: `${grid['col-sm-2']} ${grid['col-lg-4']} ${styles.lastHeading}`,
+    tooltip: {
+      title: t('Vote amount'),
+      message: t('The amount of LSK blocked for voting.'),
       position: 'left',
     },
   },
