b8aee1db10deac582bed157f015c5040d32e8945
reyraa <ali@lightcurve.io>
2020-09-23 14:59:11 +0200

    Fetch vote list and extra data

--- src/components/screens/wallet/votes/index.js
+++ src/components/screens/wallet/votes/index.js
@@ -1,6 +1,7 @@
 // istanbul ignore file
 import { withTranslation } from 'react-i18next';
-import { getVotes, getDelegates } from '../../../../utils/api/delegates';
+import { getVotes } from '../../../../utils/api/delegates';
+import { getAccounts } from '../../../../utils/api/lsk/liskService';
 import withData from '../../../../utils/withData';
 import Votes from './votes';
 
@@ -8,15 +9,18 @@ const apis = {
   votes: {
     apiUtil: getVotes,
     defaultData: [],
+    autoload: false,
     transformResponse: response => response.data.votes,
   },
-  delegates: {
-    apiUtil: getDelegates,
+  accounts: {
+    apiUtil: getAccounts,
+    autoload: false,
     defaultData: {},
-    transformResponse: (response, oldData) => ({
-      ...oldData,
-      ...response.data.reduce((acc, item) => ({ ...acc, [item.username]: item }), {}),
-    }),
+    transformResponse: response =>
+      response.data.reduce((dict, account) => {
+        dict[account.address] = account;
+        return dict;
+      }, {}),
   },
 };
 
