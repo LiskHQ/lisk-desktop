a7b129d433bed484e40d8012c7505209c7b4fe4f
UsamaHameed <usamatech2@gmail.com>
2020-09-29 11:04:30 +0200

    add initial structure

---
+++ src/components/screens/votingQueue/index.js
@@ -0,0 +1,3 @@
+import VotingQueue from './votingQueue';
+
+export default VotingQueue;
---
+++ src/components/screens/votingQueue/votingQueue.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const VotingQueue = () => {
+  console.log('mounted');
+  return (
+    <section>
+        Hello
+    </section>
+  );
+};
+
+export default VotingQueue;
