f7036a889aa46763dbcf16ac300f6e835219ccee
UsamaHameed <usamatech2@gmail.com>
2020-10-02 09:46:41 +0200

    add initial structure to the result component

--- src/components/screens/votingQueue/result/index.js
+++ src/components/screens/votingQueue/result/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import Box from '../../../toolbox/box';
+import BoxContent from '../../../toolbox/box/content';
+import ToggleIcon from '../toggleIcon';
 
-const Result = () => 'askjdaskjhd';
+const Result = ({
+  t = s => s,
+}) => (
+  <section>
+    <Box>
+      <ToggleIcon />
+      <header>
+        <h1>{t('Voting Confirmation')}</h1>
+      </header>
+
+    </Box>
+  </section>
+);
 
 export default Result;
