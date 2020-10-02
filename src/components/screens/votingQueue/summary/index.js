dfc09ca07ec808b2bd5fcc257cfb1f958b93a042
UsamaHameed <usamatech2@gmail.com>
2020-10-02 09:46:34 +0200

    add initial structure to the summary component

--- src/components/screens/votingQueue/summary/index.js
+++ src/components/screens/votingQueue/summary/index.js
@@ -1,5 +1,63 @@
 import React from 'react';
 
-const Summary = () => 'fdshergs';
+import Box from '../../../toolbox/box';
+import BoxContent from '../../../toolbox/box/content';
+import BoxFooter from '../../../toolbox/box/footer';
+import { PrimaryButton, SecondaryButton } from '../../../toolbox/buttons';
+import ToggleIcon from '../toggleIcon';
+
+import styles from './styles.css';
+
+const Summary = ({
+  t = s => s, removedVotes = [], editedVotes = [], addedVotes = [], fee = 100,
+}) => (
+  <section>
+    <Box className={styles.container}>
+      <ToggleIcon />
+      <header>
+        <h1>{t('Voting Summary')}</h1>
+      </header>
+      <BoxContent>
+        <div>
+          <span>{t('Added votes')}</span>
+          <div>
+            {addedVotes.map((vote, i) => <span key={i}>{vote}</span>)}
+          </div>
+        </div>
+        <div>
+          <span>{t('Changed votes')}</span>
+          <div>
+            {editedVotes.map((vote, i) => <span key={i}>{vote}</span>)}
+          </div>
+        </div>
+        <div>
+          <span>{t('Removed votes')}</span>
+          <div>
+            {removedVotes.map((vote, i) => <span key={i}>{vote}</span>)}
+          </div>
+        </div>
+      </BoxContent>
+      <BoxFooter className={styles.footer}>
+        <div>
+          <div className={styles.footerColumn}>
+            <span>{t('Total votes after confirmation')}</span>
+            <span>0/10</span>
+          </div>
+          <div className={styles.footerColumn}>
+            <span>{t('Transaction fee')}</span>
+            <span>{fee}</span>
+          </div>
+
+        </div>
+        <div>
+          <SecondaryButton>Edit</SecondaryButton>
+          <PrimaryButton size="l">
+            {t('Confirm')}
+          </PrimaryButton>
+        </div>
+      </BoxFooter>
+    </Box>
+  </section>
+);
 
 export default Summary;
