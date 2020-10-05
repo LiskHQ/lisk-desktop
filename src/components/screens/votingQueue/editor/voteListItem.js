c24377859e40b7c7ee076b001b2d51a0554a9c36
reyraa <ali@lightcurve.io>
2020-10-05 16:33:36 +0200

    Add validation to vote amount

--- src/components/screens/votingQueue/editor/voteListItem.js
+++ src/components/screens/votingQueue/editor/voteListItem.js
@@ -1,28 +1,42 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import AccountVisual from '../../../toolbox/accountVisual';
 import Box from '../../../toolbox/box';
 import { SecondaryButton, TertiaryButton } from '../../../toolbox/buttons';
 import Icon from '../../../toolbox/icon';
-import { Input } from '../../../toolbox/inputs';
 import LiskAmount from '../../../shared/liskAmount';
 import { tokenMap } from '../../../../constants/tokens';
-
+import useVoteAmountField from '../../editVote/useVoteAmountField';
+import { voteEdited } from '../../../../actions/voting';
+import { toRawLsk } from '../../../../utils/lsk';
+import AmountField from '../../../shared/amountField';
 import styles from './editor.css';
 
 const ComponentState = Object.freeze({ editing: 1, notEditing: 2 });
 const token = tokenMap.LSK.key;
 
 const VoteListItem = ({
-  t = s => s, address, username, confirmed, unconfirmed, setVoteAmount, deleteVote,
+  t = s => s, address, username, confirmed, unconfirmed,
 }) => {
   const [state, setState] = useState(ComponentState.notEditing);
-  const [inputValue, setInputValue] = useState();
+  const dispatch = useDispatch();
+  const [voteAmount, setVoteAmount] = useVoteAmountField(unconfirmed || confirmed);
 
   const handleFormSubmission = (e) => {
     e.preventDefault();
-    setVoteAmount(inputValue);
+    dispatch(voteEdited([{
+      address,
+      amount: toRawLsk(voteAmount.value),
+    }]));
     setState(ComponentState.notEditing);
+  };
+
+  const discard = () => {
+    dispatch(voteEdited([{
+      address,
+      amount: confirmed,
+    }]));
   };
 
   const changeToEditingMode = () => {
@@ -32,12 +46,6 @@ const VoteListItem = ({
   const changeToNotEditingMode = () => {
     setState(ComponentState.notEditing);
   };
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const inputError = false;
 
   return (
     <Box className={styles.voteItemContainer}>
@@ -61,7 +69,7 @@ const VoteListItem = ({
               <span onClick={changeToEditingMode}>
                 <Icon name="edit" className={styles.editIcon} />
               </span>
-              <span onClick={deleteVote}>
+              <span onClick={discard}>
                 <Icon name="deleteIcon" className={styles.editIcon} />
               </span>
             </div>
@@ -72,11 +80,12 @@ const VoteListItem = ({
             className={`${styles.editVoteForm} ${styles.centerContent}`}
             onSubmit={handleFormSubmission}
           >
-            <Input
-              size="m"
-              value={inputValue}
-              onChange={handleInputChange}
-              error={inputError}
+            <AmountField
+              amount={voteAmount}
+              setAmountField={setVoteAmount}
+              title={t('Vote amount (LSK)')}
+              inputPlaceHolder={t('Vote amount')}
+              name="vote"
               className={styles.editAmountInput}
             />
             <div className={styles.formButtonsContainer}>
