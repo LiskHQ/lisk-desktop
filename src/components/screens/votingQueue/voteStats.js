e9dd83015c5de72e86169637ec642c70e0615d75
UsamaHameed <usamatech2@gmail.com>
2020-10-05 11:15:36 +0200

    use correct colors for the VoteStats

--- src/components/screens/votingQueue/voteStats.js
+++ src/components/screens/votingQueue/voteStats.js
@@ -7,9 +7,18 @@ const VoteStats = ({
 }) => (
   <header className={styles.header}>
     <span className={styles.heading}>{heading}</span>
-    <span className={styles.voteStats}>{`${added} ${t('added')}`}</span>
-    <span className={styles.voteStats}>{`${edited} ${t('edited')}`}</span>
-    <span className={styles.voteStats}>{`${removed} ${t('removed')}`}</span>
+    <span className={styles.voteStats}>
+      <span className={styles.addedNumeral}>{`${added} `}</span>
+      <span>{`${t('added')}`}</span>
+    </span>
+    <span className={styles.voteStats}>
+      <span className={styles.editedNumeral}>{`${edited} `}</span>
+      <span>{`${t('edited')}`}</span>
+    </span>
+    <span className={styles.voteStats}>
+      <span>{`${removed} `}</span>
+      <span>{`${t('removed')}`}</span>
+    </span>
   </header>
 );
 
