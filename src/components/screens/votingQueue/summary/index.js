ea84bcf2898a9d4acb2a1c257adf9325f595762b
UsamaHameed <usamatech2@gmail.com>
2020-10-06 11:45:37 +0200

    cleanup and pass data around correctly

--- src/components/screens/votingQueue/summary/index.js
+++ src/components/screens/votingQueue/summary/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withTranslation } from 'react-i18next';
 
 import LiskAmount from '../../../shared/liskAmount';
 import Box from '../../../toolbox/box';
@@ -10,71 +11,77 @@ import VoteStats from '../voteStats';
 
 import styles from './styles.css';
 
-const vote1 = { rank: '123', username: 'haha', amount: '10000' };
-const votes = new Array(4).fill(vote1);
-
-const ItemList = ({ items }) => (
-  <div className={styles.voteItems}>
-    {items.map((item, i) => (
-      <span key={i} className={styles.voteItem}>
-        <span className={styles.rankText}>{`#${item.rank} `}</span>
-        <span>
-          {`${item.username} - `}
-          <LiskAmount val={item.amount} />
+const ItemList = ({ items, heading }) => (
+  <div className={styles.contentItem}>
+    <span className={styles.contentHeading}>{heading}</span>
+    <div className={styles.voteItems}>
+      {Object.keys(items).map((address, i) => (
+        <span key={i} className={styles.voteItem}>
+          <span className={styles.rankText}>{`#${address} `}</span>
+          <span>
+            {Object.values(items[address]).length === 2
+              ? (
+                <>
+                  <LiskAmount val={1} />
+                  <LiskAmount val={2} />
+                </>
+              )
+              : <LiskAmount val={Object.values(items[address])[0]} />
+            }
+          </span>
         </span>
-      </span>
-    ))}
+      ))}
+    </div>
+  </div>
+);
+
+const InfoColumn = ({ title, children }) => (
+  <div className={styles.infoColumn}>
+    <span className={styles.infoTitle}>{title}</span>
+    <span className={styles.infoValue}>
+      {children}
+    </span>
   </div>
 );
 
 const Summary = ({
-  t = s => s, removedVotes = votes, editedVotes = votes, addedVotes = votes,
-  fee = 100, prevStep, nextStep,
-}) => (
-  <section>
-    <Box className={styles.container}>
-      <ToggleIcon isNotHeader />
-      <VoteStats
-        t={t}
-        heading={t('Voting Summary')}
-        added={addedVotes.length}
-        edited={editedVotes.length}
-        removed={removedVotes.length}
-      />
-      <BoxContent className={styles.content}>
-        <div className={styles.contentItem}>
-          <span className={styles.contentHeading}>{t('Added votes')}</span>
-          <ItemList items={addedVotes} />
-        </div>
-        <div className={styles.contentItem}>
-          <span className={styles.contentHeading}>{t('Changed votes')}</span>
-          <ItemList items={editedVotes} />
-        </div>
-        <div className={styles.contentItem}>
-          <span className={styles.contentHeading}>{t('Removed votes')}</span>
-          <ItemList items={removedVotes} />
-        </div>
-        <div className={styles.infoContainer}>
-          <div className={styles.infoColumn}>
-            <span className={styles.infoTitle}>{t('Total votes after confirmation')}</span>
-            <span className={styles.infoValue}>
-              {`${addedVotes.length + editedVotes.length}/10`}
-            </span>
-          </div>
-          <div className={styles.infoColumn}>
-            <span className={styles.infoTitle}>{t('Transaction fee')}</span>
-            <span className={styles.infoValue}>{fee}</span>
-          </div>
-        </div>
-      </BoxContent>
-      <BoxFooter className={styles.footer} direction="horizontal">
-        <SecondaryButton onClick={prevStep}>Edit</SecondaryButton>
-        <PrimaryButton size="l" onClick={nextStep}>
-          {t('Confirm')}
-        </PrimaryButton>
-      </BoxFooter>
-    </Box>
-  </section>
-);
+  t, removed, edited, added, fee, prevStep, nextStep,
+}) => {
+  const addedLength = Object.keys(added).length;
+  const editedLength = Object.keys(edited).length;
+  const removedLength = Object.keys(removed).length;
 
-export default Summary;
+  return (
+    <section>
+      <Box className={styles.container}>
+        <ToggleIcon isNotHeader />
+        <VoteStats
+          t={t}
+          heading={t('Voting Summary')}
+          added={addedLength}
+          edited={editedLength}
+          removed={removedLength}
+        />
+        <BoxContent className={styles.content}>
+          {addedLength ? <ItemList heading={t('Added votes')} items={added} /> : null}
+          {editedLength ? <ItemList heading={t('Changed votes')} items={edited} /> : null}
+          {removedLength ? <ItemList heading={t('Removed votes')} items={removed} /> : null}
+          <div className={styles.infoContainer}>
+            <InfoColumn title={t('Total votes after confirmation')}>{`${addedLength + editedLength}/10`}</InfoColumn>
+            <InfoColumn title={t('Transaction fee')}>
+              <LiskAmount val={fee} />
+            </InfoColumn>
+          </div>
+        </BoxContent>
+        <BoxFooter className={styles.footer} direction="horizontal">
+          <SecondaryButton onClick={prevStep}>Edit</SecondaryButton>
+          <PrimaryButton size="l" onClick={nextStep}>
+            {t('Confirm')}
+          </PrimaryButton>
+        </BoxFooter>
+      </Box>
+    </section>
+  );
+};
+
+export default withTranslation()(Summary);
