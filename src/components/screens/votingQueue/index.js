f3df94cc8083a9bb5dfde56acfad5efa9235f572
reyraa <ali@lightcurve.io>
2020-10-09 10:12:24 +0200

    Dispatch action to edit votes by query params

--- src/components/screens/votingQueue/index.js
+++ src/components/screens/votingQueue/index.js
@@ -1,9 +1,11 @@
 /* istanbul ignore file */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import MultiStep from '../../shared/multiStep';
 import { removeSearchParamsFromUrl } from '../../../utils/searchParams';
+import processLaunchProtocol from './urlProcessor';
 
 import Editor from './editor';
 import Summary from './summary';
@@ -11,9 +13,14 @@ import Result from './result';
 import styles from './styles.css';
 
 const VotingQueue = ({ history }) => {
+  const dispatch = useDispatch();
   const closeModal = () => {
     removeSearchParamsFromUrl(history, ['modal'], true);
   };
+
+  useEffect(() => {
+    dispatch(processLaunchProtocol(history.location.search));
+  }, []);
 
   return (
     <MultiStep
