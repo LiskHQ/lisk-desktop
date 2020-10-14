020e9e08c525c53ce347a07fb7e44fd1bdd9286d
iris salcedo <iris.salcedo@lightcurve.io>
2020-10-14 09:31:27 +0200

    Remove wait time

--- test/cypress/features/voting/voting.js
+++ test/cypress/features/voting/voting.js
@@ -4,5 +4,4 @@ import ss from '../../../constants/selectors';
 
 Then(/^I should see unlocking balance (.*?)$/, function (amount) {
   cy.get(`${ss.unlockingBalance}`).eq(0).contains(amount);
-  cy.wait(1000);
 });
