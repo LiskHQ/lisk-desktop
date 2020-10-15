4f749e915ee33812a91b7fad07e8b45379c9eb05
iris salcedo <iris.salcedo@lightcurve.io>
2020-10-15 11:12:58 +0200

    Define HOC and style component

--- src/components/screens/multiSignature/summary/summary.js
+++ src/components/screens/multiSignature/summary/summary.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withTranslation } from 'react-i18next';
 import LiskAmount from '../../../shared/liskAmount';
 import Box from '../../../toolbox/box';
 import BoxContent from '../../../toolbox/box/content';
@@ -9,23 +8,41 @@ import Piwik from '../../../../utils/piwik';
 import { PrimaryButton, SecondaryButton } from '../../../toolbox/buttons';
 import AccountVisual from '../../../toolbox/accountVisual';
 import { tokenMap } from '../../../../constants/tokens';
-
+import regex from '../../../../utils/regex';
 import styles from './styles.css';
 
-const Members = ({ members, t }) => (
-  <div>
-    <p>{t('Members')}</p>
-    {members.map((member, i) => (
-      <div key={`registerMultiSignature-members-list-${i}`}>
-        {`${i + 1}.`}
-        <AccountVisual address={member.address} />
-        {member.name || member.address}
-        {member.publicKey}
-        {`${member.mandatory}`}
-      </div>
-    ))}
+const Member = ({ member, i, t }) => (
+  <div className={styles.memberInfo}>
+    <p className={styles.memberInfoIndex}>{`${i + 1}.`}</p>
+    <AccountVisual address={member.address} />
+    <div className={styles.memberDetails}>
+      <p className={styles.memberTitle}>
+        {member.name || member.address.replace(regex.lskAddressTrunk, '$1...$3')}
+        <span>{`(${member.mandatory ? t('Mandatory') : t('Optional')})`}</span>
+      </p>
+      <p className={styles.memberKey}>{member.publicKey.replace(regex.publicKeyTrunk, '$1...$3')}</p>
+    </div>
   </div>
 );
+
+const Members = ({ members, t }) => {
+  const sliceIndex = Math.round(members.length / 2);
+  const leftColumn = members.slice(0, sliceIndex);
+  const rightColumn = members.slice(sliceIndex, members.length);
+  return (
+    <div className={styles.membersContainer}>
+      <p>{t('Members')}</p>
+      <div>
+        {leftColumn.map((member, i) =>
+          <Member member={member} i={i} key={`registerMultiSignature-members-list-${i}`} t={t} />)}
+      </div>
+      <div>
+        {rightColumn.map((member, i) =>
+          <Member member={member} i={i + sliceIndex} key={`registerMultiSignature-members-list-${i + sliceIndex}`} t={t} />)}
+      </div>
+    </div>
+  );
+};
 
 const InfoColumn = ({ title, children }) => (
   <div className={styles.infoColumn}>
@@ -42,22 +59,21 @@ const Summary = ({
     {
       name: 'Wilson Geidt', address: '8195226425328336181L', publicKey: '8155694652104526882', mandatory: true,
     },
-    { address: '6195226421328336181L', publicKey: '06549eb906e7e96379f063abreyi32j31bce', mandatory: false },
-    { address: '4827364921328336181L', publicKey: '06549eb906e7e96379f063abreyi32j31bce', mandatory: false },
+    { address: '6195226421328336181L', publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a', mandatory: false },
+    { address: '4827364921328336181L', publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a', mandatory: false },
   ],
   fee = 15000000, // rawLSK
   requiredSignatures = 2,
   // account,
+  // network,
   prevStep,
   nextStep,
+  transactionCreatedSuccess,
+  transactionCreatedError,
 }) => {
-  // const account = useSelector(state => getActiveTokenAccount(state));
-  // const dispatch = useDispatch();
-  // const network = useSelector(state => state.network);
-
   const submitTransaction = () => {
     Piwik.trackingEvent('MultiSig_SubmitTransaction', 'button', 'Sign');
-    // onst txData = {
+    // const txData = {
     //  nonce: account.nonce,
     //  fee: `${fee}`,
     //  network,
@@ -66,19 +82,21 @@ const Summary = ({
     // const [error, tx] = await to(
     //   create(txData, transactionTypes().unlockToken.key),
     // );
-    const tx = { id: 1 };
+    const [error, tx] = [false, { id: 1 }];
 
-    // dispatch({
-    //   type: actionTypes.transactionCreatedSuccess,
-    //   data: tx,
-    // });
-    nextStep({ transactionInfo: tx });
+    if (!error) {
+      transactionCreatedSuccess(tx);
+      nextStep({ transactionInfo: tx });
+    } else {
+      transactionCreatedError(tx);
+      nextStep({ transactionInfo: tx });
+    }
   };
 
   return (
     <section>
       <Box className={styles.container}>
-        <BoxHeader className={styles.header}>
+        <BoxHeader>
           <h1>{t('Register multisignature account')}</h1>
         </BoxHeader>
         <BoxContent className={styles.content}>
@@ -101,4 +119,4 @@ const Summary = ({
   );
 };
 
-export default withTranslation()(Summary);
+export default Summary;
