ef9709759ec196641cfe9d6c7a676519aa94f6fc
UsamaHameed <usamatech2@gmail.com>
2020-10-19 12:32:35 +0200

    add inputWithDropdown test

--- src/components/toolbox/inputs/inputWithDropdown.js
+++ src/components/toolbox/inputs/inputWithDropdown.js
@@ -8,14 +8,15 @@ import styles from './input.css';
 const InputWithDropdown = ({
   className, value, onChange, children, buttonLabel, ButtonComponent,
 }) => (
-  <div className={`${styles.inputWithDropdown} ${className}`}>
+  <div className={`${styles.inputWithDropdown} ${className} input-with-dropdown-container`}>
     <Input
       value={value}
       onChange={onChange}
       size="m"
-      className={styles.inputDropdownInput}
+      className={`${styles.inputDropdownInput} input-with-dropdown-input`}
     />
     <DropdownButton
+      className="input-with-dropdown-dropdown"
       buttonClassName={styles.inputDropdownButton}
       buttonLabel={buttonLabel}
       size="s"
