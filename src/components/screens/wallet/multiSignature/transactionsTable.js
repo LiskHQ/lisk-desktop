9d5b7fe724c9eaf1ec25ac71ae34884acb19dc22
iris salcedo <iris.salcedo@lightcurve.io>
2020-10-21 15:13:58 +0200

    Remove filter button

--- src/components/screens/wallet/multiSignature/transactionsTable.js
+++ src/components/screens/wallet/multiSignature/transactionsTable.js
@@ -3,7 +3,6 @@ import Table from '../../../toolbox/table';
 import Box from '../../../toolbox/box';
 import BoxHeader from '../../../toolbox/box/header';
 import BoxContent from '../../../toolbox/box/content';
-import FilterDropdownButton from '../../../shared/filterDropdownButton';
 import TransactionRow from './transactionRow';
 import header from './transactionsTableHeader';
 import styles from './multiSignature.css';
@@ -12,16 +11,6 @@ const TransactionsTable = ({ transactions, t, host }) => (
   <Box>
     <BoxHeader>
       <h2>{t('Pending multisignature transactions')}</h2>
-      <FilterDropdownButton
-        t={t}
-        filters={{ amountTo: '' }}
-        applyFilters={() => {}}
-        fields={[{
-          label: t('Amount Range'),
-          name: 'amount',
-          type: 'number-range',
-        }]}
-      />
     </BoxHeader>
     <BoxContent className={`${styles.content} transactions-table`}>
       <Table
