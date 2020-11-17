a46027fd3f09882fbe1be8c6157bee618bab1080
UsamaHameed <usamatech2@gmail.com>
2020-11-17 08:39:38 +0100

    create the VoteItem component

---
+++ src/components/shared/voteItem/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../../constants/routes';
+
+import { tokenMap } from '../../../constants/tokens';
+import Icon from '../../toolbox/icon';
+import LiskAmount from '../liskAmount';
+
+import styles from './styles.css';
+
+const token = tokenMap.LSK.key;
+
+/**
+ * Componenet to display address/delegate username along with vote amount
+ * @param vote object containing either or both the confirmed and unconfirmed
+ * vote amount values
+ * @param address the address to redirect to, also used as primary text if
+ * primaryText is not defined
+ * @param primaryText text to use instead of the address e.g. delegate username
+ */
+const VoteItem = ({ vote, address, primaryText }) => {
+  const accountPath = routes.account.path;
+  return (
+    <span className={styles.container}>
+      <Link
+        to={`${accountPath}?address=${address}`}
+        className="vote-item-address"
+      >
+        <span className={styles.primaryText}>{primaryText || address}</span>
+      </Link>
+      <span>
+        {Object.values(vote).length === 2
+          ? (
+            <>
+              <LiskAmount val={vote.confirmed} token={token} />
+              <span className={styles.arrowIcon}><Icon name="arrowRightTailed" /></span>
+              <LiskAmount val={vote.unconfirmed} token={token} />
+            </>
+          )
+          : <LiskAmount val={Object.values(vote)[0]} token={token} />
+          }
+      </span>
+    </span>
+  );
+};
+
+export default VoteItem;
