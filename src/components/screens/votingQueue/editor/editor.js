252ae13a5c834e71178e7f06929ac91e79d7ef34
UsamaHameed <usamatech2@gmail.com>
2020-11-19 14:41:14 +0100

    create tx for only those votes that have changed

--- src/components/screens/votingQueue/editor/editor.js
+++ src/components/screens/votingQueue/editor/editor.js
@@ -49,10 +49,12 @@ const header = t => ([
  * @returns {Array} Array of votes as Lisk Element expects
  */
 const normalizeVotesForTx = votes =>
-  Object.keys(votes).map(delegateAddress => ({
-    delegateAddress,
-    amount: (votes[delegateAddress].unconfirmed - votes[delegateAddress].confirmed).toString(),
-  }));
+  Object.keys(votes)
+    .filter(address => votes[address].confirmed !== votes[address].unconfirmed)
+    .map(delegateAddress => ({
+      delegateAddress,
+      amount: (votes[delegateAddress].unconfirmed - votes[delegateAddress].confirmed).toString(),
+    }));
 
 /**
  * Validates given votes against the following criteria:
