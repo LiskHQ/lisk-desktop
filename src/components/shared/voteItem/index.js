afe84f7a4df1c1998803012815a24aafc09a4bf1
reyraa <ali@lightcurve.io>
2020-11-20 12:56:38 +0100

    Add trcucation option and adapt to dark mode

--- src/components/shared/voteItem/index.js
+++ src/components/shared/voteItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../../../constants/routes';
 
+import { truncateAddress } from '../../../utils/account';
 import { tokenMap } from '../../../constants/tokens';
 import Icon from '../../toolbox/icon';
 import LiskAmount from '../liskAmount';
@@ -11,28 +12,34 @@ import styles from './styles.css';
 const token = tokenMap.LSK.key;
 
 /**
- * Componenet to display address/delegate username along with vote amount
- * @param vote object containing either or both the confirmed and unconfirmed
+ * Displays address/delegate username along with vote amount
+ *
+ * @param {Object} vote object containing either or both the confirmed and unconfirmed
  * vote amount values
- * @param address the address to redirect to, also used as primary text if
+ * @param {String} address the address to redirect to, also used as primary text if
  * primaryText is not defined
- * @param primaryText text to use instead of the address e.g. delegate username
+ * @param {String} primaryText text to use instead of the address e.g. delegate username
+ * @param {Boolean} truncate text to use instead of the address e.g. delegate username
  */
-const VoteItem = ({ vote, address, primaryText }) => {
+const VoteItem = ({
+  vote, address, primaryText, truncate,
+}) => {
   const accountPath = routes.account.path;
   return (
     <span className={`${styles.container} vote-item-address`}>
       <Link
         to={`${accountPath}?address=${address}`}
       >
-        <span className={styles.primaryText}>{primaryText || address}</span>
+        <span className={styles.primaryText}>
+          {primaryText || (truncate ? truncateAddress(address) : address)}
+        </span>
       </Link>
-      <span>
+      <span className={styles.value}>
         {Object.values(vote).length === 2
           ? (
             <>
               <LiskAmount val={vote.confirmed} token={token} />
-              <span className={styles.arrowIcon}><Icon name="arrowRightTailed" /></span>
+              <span className={styles.arrowIcon}>➞</span>
               <LiskAmount val={vote.unconfirmed} token={token} />
             </>
           )
