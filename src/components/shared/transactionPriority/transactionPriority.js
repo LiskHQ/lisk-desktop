a3eba65c3675fa404563fc28da882fab2d3b3a87
iris salcedo <iris.salcedo@lightcurve.io>
2020-11-26 15:22:07 +0100

    Add missing semicolon

--- src/components/shared/transactionPriority/transactionPriority.js
+++ src/components/shared/transactionPriority/transactionPriority.js
@@ -139,7 +139,7 @@ const TransactionPriority = ({
               >
                 {priority.title}
               </button>
-            )
+            );
           })}
         </div>
       </div>
