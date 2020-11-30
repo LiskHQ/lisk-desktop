d1280ec745b5cbd9ca17525efa15b990ed54887c
reyraa <ali@lightcurve.io>
2020-11-30 11:18:58 +0100

    Added comments

---
+++ src/utils/api/ws.js
@@ -0,0 +1,5 @@
+// handles the call
+// handles parameters
+// documents the function signature
+// handles errors
+// should handle 2 Service APIs -> accept base URL defaulting to the new API, accepts optional base URL
