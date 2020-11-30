54392e616c502fa9e54269947ef7857efcddfb1f
reyraa <ali@lightcurve.io>
2020-11-30 11:07:04 +0100

    Use mapper function to export TXs methods

--- src/utils/api/transaction/index.js
+++ src/utils/api/transaction/index.js
@@ -1,16 +1,5 @@
 import * as lsk from './lsk';
 import * as btc from './btc';
-import { tokenMap } from '../../../constants/tokens';
+import functionMapper from '../functionMapper';
 
-const resourceMap = {
-  [tokenMap.LSK.key]: lsk,
-  [tokenMap.BTC.key]: btc,
-};
-
-export const getTransaction = (data, token) => resourceMap[token].getTransaction(data);
-
-export const getTransactions = (data, token) => resourceMap[token].getTransactions(data);
-
-export const getRegisteredDelegates = data => resourceMap.LSK.getTransactions(data);
-
-export const getTransactionStats = data => resourceMap.LSK.getTransactionStats(data);
+export default functionMapper(lsk, btc);
