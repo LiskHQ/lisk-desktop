e82d0e4aa4ce459cae8b107f589db19ec37afc08
UsamaHameed <usamatech2@gmail.com>
2020-12-02 13:27:33 +0100

    add initial getAccount function

--- src/utils/api/account/lsk.js
+++ src/utils/api/account/lsk.js
@@ -1,5 +1,60 @@
-export const getAccount = data => new Promise(resolve =>
-  resolve({ endpoint: 'getAccount', token: 'LSK', data }));
+import http from '../http';
+import ws from '../ws';
 
-export const getAccounts = data => new Promise(resolve =>
-  resolve({ endpoint: 'getAccounts', token: 'LSK', data }));
+const DEFAULT_PROTOCOL = 'ws';
+const httpPrefix = '/api/v1';
+
+const httpPaths = {
+  accounts: `${httpPrefix}/accounts}`,
+  account: `${httpPrefix}/account}`,
+};
+
+const wsMethods = {
+  accounts: 'get.accounts',
+  account: 'get.accounts',
+};
+
+const selectParams = ({
+  address, username, publicKey, passphrase,
+}) => {
+  const selectedParams = {};
+  if (address) {
+    selectedParams.address = address;
+  } else if (publicKey) {
+    selectedParams.publicKey = publicKey;
+  } else if (username) {
+    selectedParams.username = username;
+  } else if (passphrase) {
+    // @@todo add a unit test that checks that fetch is never called with the passphrase
+    // getAddressFromPassphrase...
+    selectedParams.address = '';
+  }
+
+  return selectedParams;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const getAccount = async ({
+  protocol = DEFAULT_PROTOCOL, network, address, publicKey, username, passphrase,
+}) => {
+  const path = httpPaths.account;
+  const method = 'GET';
+  const params = selectParams({
+    address, passphrase, publicKey, username,
+  });
+
+  if (protocol === 'http') {
+    const response = await http({
+      path, network, method, params,
+    });
+    // @todo handle errors?
+    return response;
+  }
+
+  const response = await ws({
+    baseUrl: 'some url',
+    requests: [{ method: wsMethods.account, params }],
+  });
+  // @todo handle errors?
+  return response;
+};
