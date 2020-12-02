3d190f1136aeb1d55a440c225692ee87adbc7e72
reyraa <ali@lightcurve.io>
2020-12-02 12:32:20 +0100

    Add getTransactionStats

--- src/utils/api/transaction/lsk.js
+++ src/utils/api/transaction/lsk.js
@@ -6,7 +6,8 @@ import http from '../http';
  * @param {Object} data
  * @param {String} data.params - Id of the transaction
  * @param {String?} data.baseUrl - Lisk Service API url to override the
- * existing ServiceUrl on the network param
+ * existing ServiceUrl on the network param. We may use this to retrieve
+ * the details of an archived transaction.
  * @param {Object} data.network - Network setting from Redux store
  * @returns {Object} Transaction details
  */
@@ -23,5 +24,18 @@ export const getTransactions = data => new Promise(resolve =>
 export const getRegisteredDelegates = data => new Promise(resolve =>
   resolve({ endpoint: 'getRegisteredDelegates', token: 'LSK', data }));
 
-export const getTransactionStats = data => new Promise(resolve =>
-  resolve({ endpoint: 'getTransactionStats', token: 'LSK', data }));
+/**
+ * Retrieves the overall statistics of network transactions.
+ *
+ * @param {Object} data
+ * @param {Object} data.params
+ * @param {String} data.params.period - An option of 'day' or 'month'
+ * @param {Number} data.params.limit - The number of results
+ * @param {Object} data.network - Network setting from Redux store
+ * @returns {Object} Network transactions statistics
+ */
+export const getTransactionStats = data => http({
+  path: `transactions/statistics/${data.params.period}`,
+  params: { limit: data.limit },
+  network: data.network,
+});
