882bd6a6dc8619b1607feac2f2e49edca9482829
reyraa <ali@lightcurve.io>
2020-12-08 14:11:21 +0100

    Improve readability

--- src/utils/api/account/lsk.js
+++ src/utils/api/account/lsk.js
@@ -1,5 +1,8 @@
 import http from '../http';
 import ws from '../ws';
+import { isEmpty } from '../../helpers';
+import { extractAddress } from '../../account';
+import regex from '../../regex';
 
 const httpPrefix = '/api/v1';
 
@@ -13,71 +16,110 @@ const wsMethods = {
   account: 'get.accounts',
 };
 
-
-// eslint-disable-next-line import/prefer-default-export
-export const getAccount = async ({
-  network, address, publicKey, username, passphrase,
-}) => {
-  const selectParams = (params) => {
-    const selectedParams = {};
-    if (address) {
-      selectedParams.address = params.address;
-    } else if (publicKey) {
-      selectedParams.publicKey = params.publicKey;
-    } else if (username) {
-      selectedParams.username = params.username;
-    } else if (passphrase) {
-      // @@todo add a unit test that checks that fetch is never called with the passphrase
-      // getAddressFromPassphrase...
-      selectedParams.address = '';
-    }
-
-    return selectedParams;
-  };
-
-  const params = selectParams({
-    address, passphrase, publicKey, username,
-  });
-
-  const path = httpPaths.account;
-  const response = await http({ path, network, params });
-  // @todo handle errors?
-  return response;
+/**
+ * Prioritizes the API params and if passed, converts
+ * publicKey and passphrase to address.
+ *
+ * @param {Object} params
+ * @param {String?} params.username Valid delegate username
+ * @param {String?} params.address Valid Lisk Address
+ * @param {String?} params.passphrase Valid Mnemonic passphrase
+ * @param {String?} params.publicKey Valid Lisk PublicKey
+ *
+ * @returns {Object} Params containing either address or username
+ */
+const getAccountParams = (params) => {
+  if (!params || isEmpty(params)) return {};
+  // Pick username, cause the address is not obtainable from the username
+  if (params.username) return { username: params.username };
+  // If you have the address, you don't need anything else
+  if (params.address) return { address: params.address };
+  // convert other params to address
+  if (params.publicKey || params.passphrase) {
+    const address = extractAddress(params.publicKey || params.passphrase);
+    return { address };
+  }
+  // if none of the above, ignore the params
+  return {};
 };
 
+/**
+ * Retrieves details of an account with given params
+ *
+ * @param {Object} data
+ * @param {Object} data.network The network config from the Redux store
+ * @param {String?} data.baseUrl Custom API URL
+ * @param {Object} data.params
+ * @param {String?} data.params.username Valid delegate username
+ * @param {String?} data.params.address Valid Lisk Address
+ * @param {String?} data.params.passphrase Valid Mnemonic passphrase
+ * @param {String?} data.params.publicKey Valid Lisk PublicKey
+ *
+ * @returns {Promise}
+ */
+export const getAccount = async ({
+  network, params, baseUrl,
+}) => http({
+  path: httpPaths.account,
+  network,
+  params: getAccountParams(params),
+  baseUrl,
+});
+
+const txFilters = {
+  limit: { key: 'limit', test: num => (typeof num === 'number') },
+  offset: { key: 'offset', test: num => (typeof num === 'number' && num > 0) },
+  sort: {
+    key: 'sort',
+    test: str => ['amount:asc', 'amount:desc', 'timestamp:asc', 'timestamp:desc'].includes(str),
+  },
+};
+
+/**
+ * Retrieves the list of accounts with given params
+ *
+ * @param {Object} data
+ * @param {Object} data.network The network config from the Redux store
+ * @param {String?} data.baseUrl Custom API URL
+ * @param {Object} data.params
+ * @param {String?} data.params.username Valid delegate username
+ * @param {String?} data.params.address Valid Lisk Address
+ * @param {String?} data.params.passphrase Valid Mnemonic passphrase
+ * @param {String?} data.params.publicKey Valid Lisk PublicKey
+ *
+ * @returns {Promise}
+ */
 export const getAccounts = async ({
-  network, addressList, offset, limit, top,
+  network,
+  params,
+  baseUrl,
 }) => {
-  const selectParams = (params) => {
-    const selectedParams = {};
-    if (addressList) {
-      selectedParams.addressList = params.addressList;
-    } else {
-      selectedParams.offset = params.offset;
-      selectedParams.limit = params.limit;
-      selectedParams.top = params.top;
-    }
+  // Use websock to retrieve accounts with a given array of addresses
+  if (Array.isArray(params.addressList) && params.addressList.length) {
+    const requests = params.addressList
+      .filter(address => regex.address.test(address))
+      .map(address => ({
+        method: wsMethods.accounts,
+        params: { address },
+      }));
 
-    return selectedParams;
-  };
-
-  const params = selectParams({
-    addressList, offset, limit, top,
-  });
-
-  const protocol = params.addressList ? 'ws' : 'http';
-
-  if (protocol === 'http') {
-    const path = httpPaths.accounts;
-    const response = await http({ path, network, params });
-    // @todo handle errors?
-    return response;
+    return ws({
+      network,
+      requests,
+    });
   }
 
-  const response = await ws({
-    baseUrl: network.serviceUrl,
-    requests: [{ method: wsMethods.accounts, params }],
+  // Use HTTP to retrieve accounts with given sorting and pagination parameters
+  const normParams = {};
+  Object.keys(params).forEach((key) => {
+    if (txFilters[key].test(params[key])) {
+      normParams[txFilters[key].key] = params[key];
+    }
   });
-  // @todo handle errors?
-  return response;
+
+  return http({
+    network,
+    baseUrl,
+    params: normParams,
+  });
 };
