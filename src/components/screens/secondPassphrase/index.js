3398d868bcaaaadd5a1f0a30cdc0335722084a80
reyraa <ali@lightcurve.io>
2020-12-15 00:14:58 +0100

    Remove second passphrase actions and props

--- src/components/screens/secondPassphrase/index.js
+++ src/components/screens/secondPassphrase/index.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
 import { withRouter } from 'react-router';
 import { getActiveTokenAccount } from '../../../utils/account';
-import { secondPassphraseRegistered } from '../../../actions/account';
 import SecondPassphrase from './secondPassphrase';
 
 /**
@@ -13,11 +12,6 @@ const mapStateToProps = state => ({
   account: getActiveTokenAccount(state),
 });
 
-const mapDispatchToProps = {
-  secondPassphraseRegistered,
-};
-
 export default withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps,
 )(withTranslation()(SecondPassphrase)));
