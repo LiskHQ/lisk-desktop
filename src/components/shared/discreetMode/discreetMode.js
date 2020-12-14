a3082707e61502c9035265dd447149bbdf6981bd
reyraa <ali@lightcurve.io>
2020-12-14 12:41:27 +0100

    Update API import statements

--- src/components/shared/discreetMode/discreetMode.js
+++ src/components/shared/discreetMode/discreetMode.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import routes from '../../../constants/routes';
 import styles from './discreetMode.css';
-import { getTokenFromAddress } from '../../../utils/api/transactions';
+import { getTokenFromAddress } from '../../../utils/api/transaction';
 import { selectSearchParamValue } from '../../../utils/searchParams';
 
 class DiscreetMode extends Component {
