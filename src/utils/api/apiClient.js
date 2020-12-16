2b030e82645f582aa820f223ab1a4cc76e099e5d
UsamaHameed <usamatech2@gmail.com>
2020-12-16 15:40:35 +0100

    use network names from contants

--- src/utils/api/apiClient.js
+++ src/utils/api/apiClient.js
@@ -1,21 +1,20 @@
 import { APIClient } from '@liskhq/lisk-client';
 import networks from '../../constants/networks';
 
-const networkNameMap = {
-  mainnet: 'Mainnet',
-  testnet: 'Testnet',
-  betanet: 'Betanet',
-};
-
 // eslint-disable-next-line import/prefer-default-export
 export const getApiClient = (networkName) => {
-  if (networkName === networkNameMap.mainnet) {
-    return APIClient.createMainnetAPIClient();
-  } if (networkName === networkNameMap.testnet) {
-    return APIClient.createMainnetAPIClient();
-  } if (networkName === networkNameMap.betanet) {
+  if (networkName === networks.mainnet.name) {
     return APIClient.createMainnetAPIClient();
   }
+  if (networkName === networks.testnet.name) {
+    return APIClient.createMainnetAPIClient();
+  }
+
+  // the networks object does not contain any betanet info
+  // if (networkName === networks.betanet.name) {
+  //   return APIClient.createMainnetAPIClient();
+  // }
+
   // custom node
   const client = new APIClient([networks.customNode.address]);
   return client;
