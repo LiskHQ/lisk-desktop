08b7b002a9e56d5995292f1698892beb942154c7
reyraa <ali@lightcurve.io>
2020-12-31 12:45:53 +0100

    Implement BTC search API methods

--- src/utils/api/search/btc.js
+++ src/utils/api/search/btc.js
@@ -1,3 +1,35 @@
+
+import { getAccount } from '../account/btc';
+import { getTransaction } from '../transaction/btc';
+import regex from '../../regex';
+
+const rejectOnly = () => new Promise((_, reject) =>
+  reject(Error('Nothing found.')));
+
+/**
+ * Defines the entity corresponding the given query
+ * and uses other API methods to fetch the data
+ *
+ * @param {Object} data
+ * @param {Object} data.network - Network config from the Redux store
+ * @param {Object} data.params
+ * @param {String} data.params.query - Search query
+ * @param {String} data.baseUrl - custom BaseUrl used for fetching archived data
+ * @returns {Promise}
+ */
 // eslint-disable-next-line import/prefer-default-export
-export const search = data => new Promise(resolve =>
-  resolve({ endpoint: 'search', token: 'BTC', data }));
+export const search = ({ network, params }) => {
+  if (regex.btcAddress.test(params.query)) {
+    return getAccount({
+      network,
+      params: { address: params.query },
+    }).then(res => ({ data: { addresses: [res] }, meta: res.meta }));
+  }
+  if (regex.btcTransactionId.test(params.query)) {
+    return getTransaction({
+      network,
+      params: { id: params.query },
+    }).then(res => ({ data: { transaction: res.data }, meta: res.meta }));
+  }
+  return rejectOnly;
+};
