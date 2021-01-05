544de1e6f454f9c6758ce39414d636654e2025b3
iris salcedo <iris.salcedo@lightcurve.io>
2021-01-05 10:20:01 +0100

    Remove parameters

--- src/actions/service.js
+++ src/actions/service.js
@@ -5,7 +5,7 @@ import { getPrices } from '../utils/api/market';
 
 // eslint-disable-next-line import/prefer-default-export
 export const pricesRetrieved = () => (dispatch, getState) => {
-  const { settings: { token }, network } = getState();
+  const { settings: { token } } = getState();
   const activeToken = token.active;
   const tickerReducer = (acc, key) => ({
     ...acc,
@@ -15,7 +15,7 @@ export const pricesRetrieved = () => (dispatch, getState) => {
     },
   });
 
-  getPrices(network, activeToken)
+  getPrices()
     .then((priceTicker) => {
       const priceTickerReduced = priceTicker.reduce(tickerReducer, {});
       dispatch({
