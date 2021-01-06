90ad602a2e46fc26dde62602fbdfdb0c0c88e914
reyraa <ali@lightcurve.io>
2021-01-06 19:44:10 +0100

    Move token selector button to dedicated file

---
+++ src/components/shared/navigationBars/topBar/tokenSelector.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import routes from '../../../../constants/routes';
+import styles from './topBar.css';
+import Icon from '../../../toolbox/icon';
+import { settingsUpdated } from '../../../../actions/settings';
+import Tooltip from '../../../toolbox/tooltip/tooltip';
+import { tokenMap } from '../../../../constants/tokens';
+
+const TokenSelector = ({ token, history, t }) => {
+  const dispatch = useDispatch();
+  const activeToken = useSelector(state => state.settings.token.active);
+
+  const activateToken = () => {
+    if (activeToken !== token) {
+      dispatch(settingsUpdated({ token: { active: token } }));
+      const { location, push } = history;
+      if (location.pathname !== routes.wallet.path) {
+        push(routes.wallet.path);
+      }
+    }
+  };
+
+  return (
+    <Tooltip
+      className={styles.tooltipWrapper}
+      size="maxContent"
+      position="bottom"
+      content={(
+        <Icon
+          name={`${token.toLowerCase()}Icon`}
+          className={`${styles.toggle} token-selector-${token} ${activeToken === token ? '' : styles.disabled}`}
+          onClick={activateToken}
+        />
+      )}
+    >
+      <p>{t(`${token === tokenMap.LSK.key ? 'Lisk' : 'Bitcoin'} wallet`)}</p>
+    </Tooltip>
+  );
+};
+
+export default TokenSelector;
