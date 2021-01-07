1c10f992a17396fc5a2cd9f14e075f5d18e8d77d
reyraa <ali@lightcurve.io>
2021-01-07 12:35:56 +0100

    Move tickerReducer outside action

--- src/actions/service.js
+++ src/actions/service.js
@@ -3,17 +3,18 @@ import { toast } from 'react-toastify';
 import actionTypes from '../constants/actions';
 import { getPrices } from '../utils/api/market';
 
+const tickerReducer = (acc, key) => ({
+  ...acc,
+  [key.from]: {
+    ...acc[key.from],
+    [key.to]: key.rate,
+  },
+});
+
 // eslint-disable-next-line import/prefer-default-export
 export const pricesRetrieved = () => (dispatch, getState) => {
   const { settings: { token } } = getState();
   const activeToken = token.active;
-  const tickerReducer = (acc, key) => ({
-    ...acc,
-    [key.from]: {
-      ...acc[key.from],
-      [key.to]: key.rate,
-    },
-  });
 
   getPrices()
     .then(({ data }) => {
