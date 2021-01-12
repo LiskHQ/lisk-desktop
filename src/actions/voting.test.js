d1d057217457fefed56f002f8ebac63e9864df47
reyraa <ali@lightcurve.io>
2021-01-12 15:00:35 +0100

    Update unit test

--- src/actions/voting.test.js
+++ src/actions/voting.test.js
@@ -10,7 +10,7 @@ import networks from '../constants/networks';
 import * as TransactionApi from '../utils/api/transaction';
 import * as delegateApi from '../utils/api/delegate';
 import sampleVotes from '../../test/constants/votes';
-import { loginType } from '../constants/loginTypes';
+import loginTypes from '../constants/loginTypes';
 
 jest.mock('../utils/api/transaction', () => ({
   create: jest.fn(),
@@ -30,7 +30,7 @@ describe('actions: voting', () => {
       },
     },
     account: {
-      loginType: loginType.normal.code,
+      loginType: loginTypes.passphrase.code,
       info: {
         LSK: {
           address: '123L',
