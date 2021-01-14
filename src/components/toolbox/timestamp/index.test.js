38062d01e1784d6c22235a2be2248e65154418d7
reyraa <ali@lightcurve.io>
2021-01-14 15:35:33 +0100

    Make mock available for all blocks

--- src/components/toolbox/timestamp/index.test.js
+++ src/components/toolbox/timestamp/index.test.js
@@ -5,10 +5,14 @@ import { Time, DateFromTimestamp, TimeFromTimestamp } from './index';
 describe('components/toolbox/timestamp', () => {
   const inputValue = 1499983200;
 
+  jest.spyOn(global.Date, 'now').mockImplementation(() => Date.UTC(2017, 1, 15));
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   describe('<Time label={1499983200} />', () => {
     it('renders "5 months" if today is 2017-01-15', () => {
-      jest.spyOn(global.Date, 'now').mockImplementation(() =>
-        Date.UTC(2017, 1, 15));
 
       const wrapper = mount(<Time label={inputValue} />);
       expect(wrapper).toHaveText('5 months');
