7b35dc2c220db74b968c29304ce8f606a860ff05
UsamaHameed <usamatech2@gmail.com>
2021-01-28 17:39:04 +0100

    add watchlist middleware

---
+++ src/store/middlewares/watchList.test.js
@@ -0,0 +1,31 @@
+import middleware from './watchList';
+import actionTypes from '../../constants/actions';
+import { setInStorage } from '../../utils/localJSONStorage';
+
+jest.mock('../../utils/localJSONStorage', () => ({ setInStorage: jest.fn() }));
+
+describe('Middleware: WatchList', () => {
+  const next = jest.fn();
+  const watchList = ['1L'];
+  const store = {
+    dispatch: jest.fn(),
+    getState: () => ({
+      watchList,
+    }),
+  };
+
+  it('should pass the action', () => {
+    const action = { type: 'ANY_ACTION' };
+    middleware(store)(next)(action);
+
+    expect(next).toBeCalledWith(action);
+  });
+
+  it('should update localStorage with current watchlist', () => {
+    const actions = [{ type: actionTypes.addedToWatchList, data: { address: '2L' } },
+      { type: actionTypes.removedFromWatchList, data: { address: '3L' } }];
+    actions.map(action => middleware(store)(next)(action));
+
+    expect(setInStorage).toHaveBeenCalledTimes(actions.length);
+  });
+});
