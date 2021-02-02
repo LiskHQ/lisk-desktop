0c540fb8a5362437bb1e21081348456a0cadfec0
UsamaHameed <usamatech2@gmail.com>
2021-02-02 15:16:19 +0100

    fix device disconnect modal

--- src/store/middlewares/hwManager.js
+++ src/store/middlewares/hwManager.js
@@ -1,37 +1,14 @@
 // istanbul ignore file
-import React from 'react';
 import { toast } from 'react-toastify';
-import { PrimaryButton } from '../../components/toolbox/buttons';
 import { accountLoggedOut, login } from '../../actions/account';
 import {
   getDeviceList,
   getPublicKey,
 } from '../../../libs/hwManager/communication';
 import { subscribeToDeviceConnected, subscribeToDeviceDisconnected } from '../../utils/hwManager';
-import Dialog from '../../components/toolbox/dialog/dialog';
-import DialogHolder from '../../components/toolbox/dialog/holder';
 import actionTypes from '../../constants/actions';
-import i18n from '../../i18n';
-
-/**
- * TODO this can move later to a different file in case we need.
- * deviceDisconnectDialog - Function - Renders the dialog when user is SignedIn and remove
- * hw device connection, the dialog will logout the session.
- * @param {string} model - HW Device model
- */
-const deviceDisconnectDialog = model => DialogHolder.showDialog(
-  <Dialog>
-    <Dialog.Title>{i18n.t('You are disconnected')}</Dialog.Title>
-    <Dialog.Description>
-      {i18n.t('There is no connection to the {{model}}. Please check the cables if it happened by accident.', { model })}
-    </Dialog.Description>
-    <Dialog.Options align="center">
-      <PrimaryButton>
-        {i18n.t('Ok')}
-      </PrimaryButton>
-    </Dialog.Options>
-  </Dialog>,
-);
+import { addSearchParamsToUrl } from '../../utils/searchParams';
+import history from '../../history';
 
 async function autoLogInIfNecessary(store) {
   // not tested as it is just a development helper
@@ -84,7 +61,7 @@ const hwWalletMiddleware = store => next => (action) => {
         && account.hwInfo.deviceId
         && account.hwInfo.deviceModel === response.model
       ) {
-        deviceDisconnectDialog(response.model);
+        addSearchParamsToUrl(history, { modal: 'deviceDisconnectDialog', model: response.model });
         store.dispatch(accountLoggedOut());
       }
 
