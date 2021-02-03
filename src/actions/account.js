f0f3aae48461ae8bca06d4447fc7e6a473cd320b
reyraa <ali@lightcurve.io>
2021-02-03 09:51:44 +0100

    Remove obsolete and unused actions

--- src/actions/account.js
+++ src/actions/account.js
@@ -5,30 +5,6 @@ import { getConnectionErrorMessage } from '../utils/getNetwork';
 import loginTypes from '../constants/loginTypes';
 import { networkStatusUpdated } from './network';
 import actionTypes from '../constants/actions';
-import { tokenMap } from '../constants/tokens';
-
-/**
- * Trigger this action to remove passphrase from account object
- *
- * @param {Object} data - account data
- * @returns {Object} - Action object
- */
-export const removePassphrase = data => ({
-  data,
-  type: actionTypes.removePassphrase,
-});
-
-/**
- * Trigger this action to update the account object
- * while already logged in
- *
- * @param {Object} data - account data
- * @returns {Object} - Action object
- */
-export const accountUpdated = data => ({
-  data,
-  type: actionTypes.accountUpdated,
-});
 
 /**
  * Trigger this action to log out of the account
@@ -47,18 +23,6 @@ export const accountLoggedOut = () => ({
 export const timerReset = date => ({
   type: actionTypes.timerReset,
   data: date,
-});
-
-/**
- * Trigger this action to login to an account
- * The login middleware triggers this action
- *
- * @param {Object} data - account data
- * @returns {Object} - Action object
- */
-export const accountLoggedIn = data => ({
-  type: actionTypes.accountLoggedIn,
-  data,
 });
 
 export const accountLoading = () => ({
