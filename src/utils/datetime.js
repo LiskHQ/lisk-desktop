ee902a2a5f96cdd982f5746cc8483585bd593668
iris salcedo <iris.salcedo@lightcurve.io>
2021-02-05 11:33:03 +0100

    Add documentation

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -49,6 +49,11 @@ export const convertUnixSecondsToLiskEpochSeconds = timestamp => (
   moment(timestamp * 1000).unix() - moment(firstBlockTime).unix()
 );
 
+/**
+ * Converts a date in DD-MM-YYYY format to timestamp
+ * @param {String} date - Date in DD-MM-YYYY format
+ * @returns {Number} - Unix timestamp
+ */
 export const transformStringDateToUnixTimestamp = date => new Date(moment(date, 'DD-MM-YYYY').format('MM/DD/YYYY')).valueOf() / 1000;
 
 export default {
