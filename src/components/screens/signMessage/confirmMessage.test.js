d0c57185835df671c5749cdb0cd4603c1511a501
UsamaHameed <usamatech2@gmail.com>
2021-02-17 14:41:09 +0100

    fix import

--- src/components/screens/signMessage/confirmMessage.test.js
+++ src/components/screens/signMessage/confirmMessage.test.js
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 import ConfirmMessage from './confirmMessage';
 import accounts from '../../../../test/constants/accounts';
-import { loginType } from '../../../constants/hwConstants';
+import loginType from '../../../constants/loginTypes';
 import * as hwManager from '../../../utils/hwManager';
 
 jest.mock('../../../utils/hwManager');
