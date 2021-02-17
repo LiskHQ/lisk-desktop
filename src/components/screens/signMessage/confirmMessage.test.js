aaf325c8c0d0a8df3f49eb77e1df95235a3b5009
Iris Salcedo <iris.salcedo@lightcurve.io>
2021-02-17 15:29:18 +0100

    Revert "Update loginType"

--- src/components/screens/signMessage/confirmMessage.test.js
+++ src/components/screens/signMessage/confirmMessage.test.js
@@ -18,7 +18,7 @@ const updateWrapperAsync = async wrapper => new Promise((resolve) => {
 describe('Confirm Message Component', () => {
   const accountWithPassphrase = {
     ...accounts.genesis,
-    loginType: loginType.passphrase,
+    loginType: loginType.passphrase.code,
   };
   const accountWithHW = {
     ...accounts.genesis,
