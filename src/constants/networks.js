2a11ba26c170a309f323c417b16762f6bc9d9b9e
UsamaHameed <usamatech2@gmail.com>
2021-03-09 16:04:09 +0100

    make api/network/lsk more in line to sdk v5 and lisk-service

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -4,41 +4,20 @@ export const networkKeys = {
   customNode: 'customNode',
 };
 
+const initialSupply = 10000000000000000;
+
 const networks = {
   [networkKeys.mainNet]: {
     label: 'Mainnet',
-    code: 0,
-    nodes: [
-      'https://hub21.lisk.io',
-      'https://hub22.lisk.io',
-      'https://hub23.lisk.io',
-      'https://hub24.lisk.io',
-      'https://hub25.lisk.io',
-      'https://hub26.lisk.io',
-      'https://hub27.lisk.io',
-      'https://hub28.lisk.io',
-      'https://hub31.lisk.io',
-      'https://hub32.lisk.io',
-      'https://hub33.lisk.io',
-      'https://hub34.lisk.io',
-      'https://hub35.lisk.io',
-      'https://hub36.lisk.io',
-      'https://hub37.lisk.io',
-      'https://hub38.lisk.io',
-    ],
-    initialSupply: 10000000000000000,
+    serviceUrl: '', // @todo add service url for mainnet when defined
   },
   [networkKeys.testNet]: {
     label: 'Testnet',
-    code: 1,
-    nodes: ['https://testnet.lisk.io'],
-    initialSupply: 10000000000000000,
+    serviceUrl: '', // @todo add service url for testnet when defined
   },
   [networkKeys.customNode]: {
     label: 'Custom Node',
-    code: 2,
-    nodes: ['http://localhost:4000'],
-    initialSupply: 10000000000000000,
+    serviceUrl: 'localhost:9901', // a default value. would usually be the one entered by the user
   },
 };
 
