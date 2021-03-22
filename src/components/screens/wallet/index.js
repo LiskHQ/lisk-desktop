f5c01497076a9514224b25202ebcb250c42d1b9f
UsamaHameed <usamatech2@gmail.com>
2021-03-22 16:32:21 +0100

    use selectors

--- src/components/screens/wallet/index.js
+++ src/components/screens/wallet/index.js
@@ -1,25 +1,28 @@
 /* istanbul ignore file */
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { withTranslation } from 'react-i18next';
 import { parseSearchParams, addSearchParamsToUrl } from '@utils/searchParams';
 import { transactionsRetrieved } from '@actions';
 import { isEmpty } from '@utils/helpers';
+import {
+  selectAccount,
+  selectActiveToken,
+  selectSettings,
+  selectTransactions,
+} from '@store/selectors';
 import Overview from './overview';
 import TabsContainer from '../../toolbox/tabsContainer/tabsContainer';
 import DelegateTab from './delegateProfile';
 import VotesTab from './votes';
 import Transactions from './transactions';
-import {
-  useAccount, useActiveToken, useSettings, useTransactions,
-} from '../../../hooks/selectors';
 
 const Wallet = ({ t, history }) => {
   const dispatch = useDispatch();
-  const account = useAccount();
-  const activeToken = useActiveToken();
-  const { discreetMode } = useSettings();
-  const { confirmed, pending } = useTransactions();
+  const account = useSelector(selectAccount);
+  const activeToken = useSelector(selectActiveToken);
+  const { discreetMode } = useSelector(selectSettings);
+  const { confirmed, pending } = useSelector(selectTransactions);
 
   useEffect(() => {
     if (!confirmed.length && account.info && !isEmpty(account.info)) {
