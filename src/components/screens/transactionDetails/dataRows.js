4b4e908775afd632b4925bc4074dcf241e148e93
UsamaHameed <usamatech2@gmail.com>
2021-03-23 20:44:00 +0100

    fix transaction details modal

--- src/components/screens/transactionDetails/dataRows.js
+++ src/components/screens/transactionDetails/dataRows.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { tokenMap, MODULE_ASSETS_NAME_ID_MAP } from '@constants';
 import { getTxAmount } from '@utils/api/transaction';
-import { getModuleAssetSenderLabel } from '@utils/moduleAssets';
+import { getModuleAssetSenderLabel, getModuleAssetTitle } from '@utils/moduleAssets';
 import CopyToClipboard from '../../toolbox/copyToClipboard';
 import TransactionTypeFigure from '../../shared/transactionTypeFigure';
 import AccountInfo from './accountInfo';
@@ -29,15 +29,16 @@ const getTxAsset = (tx) => {
 export const Illustration = ({
   transaction,
 }) => {
-  const TypeInfo = MODULE_ASSETS_NAME_ID_MAP[transaction.moduleAssetId];
-  if (transaction.title === MODULE_ASSETS_NAME_ID_MAP.transfer) return null;
+  if (transaction.moduleAssetId === MODULE_ASSETS_NAME_ID_MAP.transfer) {
+    return null;
+  }
   return (
     <BoxRow className={styles.summaryHeader}>
       <TransactionTypeFigure
         address={transaction.senderId}
-        transactionType={transaction.title}
+        moduleAssetId={transaction.moduleAssetId}
       />
-      <h2 className="tx-header">{TypeInfo.title}</h2>
+      <h2 className="tx-header">{getModuleAssetTitle()[transaction.moduleAssetId]}</h2>
     </BoxRow>
   );
 };
@@ -45,7 +46,7 @@ export const Illustration = ({
 export const Sender = ({
   transaction, activeToken, network,
 }) => {
-  const { senderLabel } = getModuleAssetSenderLabel[transaction.moduleAssetId];
+  const senderLabel = getModuleAssetSenderLabel()[transaction.moduleAssetId];
 
   return (
     <BoxRow className={styles.detailsWrapper}>
@@ -53,7 +54,7 @@ export const Sender = ({
         name={getDelegateName(transaction, activeToken)}
         token={activeToken}
         network={network}
-        address={transaction.senderId}
+        address={transaction.sender.address}
         addressClass="sender-address"
         label={senderLabel}
       />
@@ -64,13 +65,15 @@ export const Sender = ({
 export const Recipient = ({
   activeToken, network, transaction, t,
 }) => {
-  if (transaction.type !== MODULE_ASSETS_NAME_ID_MAP.transfer) return null;
+  if (transaction.moduleAssetId !== MODULE_ASSETS_NAME_ID_MAP.transfer) {
+    return null;
+  }
   return (
     <BoxRow className={styles.detailsWrapper}>
       <AccountInfo
         token={activeToken}
         network={network}
-        address={transaction.recipientId}
+        address={transaction.asset.recipientAddress}
         addressClass="receiver-address"
         label={t('Recipient')}
       />
@@ -112,7 +115,8 @@ export const AmountAndDate = ({
 
   return (
     <BoxRow>
-      { transaction.title === 'transfer' || transaction.title === 'unlockToken'
+      {transaction.moduleAssetId === MODULE_ASSETS_NAME_ID_MAP.transfer
+        || transaction.moduleAssetId === MODULE_ASSETS_NAME_ID_MAP.unlockToken
         ? (
           <div className={styles.value}>
             <span className={styles.label}>
@@ -133,7 +137,7 @@ export const AmountAndDate = ({
           <DateTimeFromTimestamp
             fulltime
             className="date"
-            time={transaction.timestamp}
+            time={transaction.block.timestamp}
             token={activeToken}
             showSeconds
           />
@@ -208,8 +212,13 @@ export const DateAndConfirmation = ({
 export const Message = ({
   activeToken, transaction, t,
 }) => {
-  if (transaction.title !== 'transfer'
-    || activeToken !== tokenMap.LSK.key) return null;
+  if (
+    transaction.moduleAssetId !== MODULE_ASSETS_NAME_ID_MAP.transfer
+    || activeToken !== tokenMap.LSK.key
+  ) {
+    return null;
+  }
+
   return (
     <BoxRow className={styles.message}>
       <div className={`${styles.value}`}>
@@ -225,7 +234,9 @@ export const Message = ({
 export const DelegateUsername = ({
   transaction, t,
 }) => {
-  if (transaction.title !== 'registerDelegate') return null;
+  if (transaction.moduleAssetId !== MODULE_ASSETS_NAME_ID_MAP.registerDelegate) {
+    return null;
+  }
 
   return (
     <BoxRow className={styles.message}>
