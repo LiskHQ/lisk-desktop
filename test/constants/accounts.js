c4b37b976d95c5a0fe643a4f1de8b34c528b4243
UsamaHameed <usamatech2@gmail.com>
2021-03-25 14:19:10 +0100

    update devnet account addresses

--- test/constants/accounts.js
+++ test/constants/accounts.js
@@ -4,7 +4,7 @@ const accounts = {
     summary: {
       publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a',
       serverPublicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a',
-      address: '5059876081639179984L',
+      address: 'lsks6uckwnap7s72ov3edddwgxab5e89t6uy8gjt6',
       balance: '9897000000000000',
     },
     token: { balance: '9897000000000000' },
@@ -18,7 +18,7 @@ const accounts = {
     summary: {
       publicKey: '86499879448d1b0215d59cbf078836e3d7d9d2782d56a2274a568761bff36f19',
       serverPublicKey: '86499879448d1b0215d59cbf078836e3d7d9d2782d56a2274a568761bff36f19',
-      address: '537318935439898807L',
+      address: 'lskehj8am9afxdz8arztqajy52acnoubkzvmo9cjy',
       balance: 1000e8,
     },
     token: { balance: 1000e8 },
@@ -32,7 +32,7 @@ const accounts = {
     summary: {
       publicKey: '197cf311f678406bc72a8edfdc3dffe6f59f49c4550a860e4b68fb20382211d0',
       serverPublicKey: '197cf311f678406bc72a8edfdc3dffe6f59f49c4550a860e4b68fb20382211d0',
-      address: '5932438298200837883L',
+      address: 'lskgonvfdxt3m6mm7jaeojrj5fnxx7vwmkxq72v79',
       balance: 0,
     },
     token: { balance: 0 },
@@ -46,7 +46,7 @@ const accounts = {
     summary: {
       publicKey: '35c6b25520fc868b56c83fed6e1c89bb350fb7994a5da0bcea7a4f621f948c7f',
       serverPublicKey: '35c6b25520fc868b56c83fed6e1c89bb350fb7994a5da0bcea7a4f621f948c7f',
-      address: '544792633152563672L',
+      address: 'lsksckkjs2c8dnu7vhcku825cp62ed6eyxd8pbt6p',
     },
     token: { },
     sequence: { },
@@ -59,7 +59,7 @@ const accounts = {
     summary: {
       publicKey: 'ecf6a5cc0b7168c7948ccfaa652cce8a41256bdac1be62eb52f68cde2fb69f2d',
       serverPublicKey: 'ecf6a5cc0b7168c7948ccfaa652cce8a41256bdac1be62eb52f68cde2fb69f2d',
-      address: '4264113712245538326L',
+      address: 'lskvagp38bkmuetwazdww84bvkzc2g58u2aodn24z',
     },
     token: { },
     sequence: { },
@@ -73,7 +73,7 @@ const accounts = {
     summary: {
       publicKey: 'c5e64031407c3ca8d526bf7404f7c78ab60ea0792e90393a73b3b06a8c8841d4',
       serverPublicKey: 'c5e64031407c3ca8d526bf7404f7c78ab60ea0792e90393a73b3b06a8c8841d4',
-      address: '16422276087748907680L',
+      address: 'lskkx46qy3wkwtfbh4gzbq57tx6dzfexu5yufwh5b',
     },
     token: { },
     sequence: { },
@@ -88,7 +88,7 @@ const accounts = {
       serverPublicKey: 'fab9d261ea050b9e326d7e11587eccc343a20e64e29d8781b50fd06683cacc88',
       secondPassphrase: 'forest around decrease farm vanish permit hotel clay senior matter endorse domain',
       secondPublicKey: 'ec057d8816b18b83a2baac387eebf8af707f8fb565c963476a0e4533e8481eaf',
-      address: '1155682438012955434L',
+      address: 'lskm4qfwau9ajpyx3m59axfederx5ne9qb6grbuy3',
       balance: 1000e8,
     },
     token: { balance: 1000e8 },
@@ -101,7 +101,7 @@ const accounts = {
     passphrase: 'traffic woman skull forest nerve luggage traffic wrestle ensure organ moon century',
     summary: {
       publicKey: 'a1fa251b368939ed2aa8c620e955cb4537c06a351fa50e928ec21e89372e7494',
-      address: '94495548317450502L',
+      address: 'lskzazqvqytehnucqos7atc5knsp872dhetz9rchn',
       balance: 0,
     },
     token: { balance: 0 },
@@ -113,7 +113,7 @@ const accounts = {
   testnet_guy: {
     passphrase: 'call scene goat common morning immune oxygen reunion skirt amazing current hire',
     summary: {
-      address: '9819477579273755847L',
+      address: 'lsk2h73o3bqa4v2u3ehn6c5e787ky38q8wte538mn',
       balance: 200000000,
     },
     token: { balance: 200000000 },
