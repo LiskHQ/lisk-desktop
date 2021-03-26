c3641c67d1fc3a927b4275ccd2d94d88eea0bed1
reyraa <ali@lightcurve.io>
2021-03-26 13:54:21 +0100

    Import MODULE_ASSETS_NAME_ID_MAP

--- src/utils/api/transaction/lsk.js
+++ src/utils/api/transaction/lsk.js
@@ -2,7 +2,11 @@
 import { transactions } from '@liskhq/lisk-client';
 
 import {
-  tokenMap, MODULE_ASSETS, minFeePerByte, DEFAULT_NUMBER_OF_SIGNATURES, DEFAULT_SIGNATURE_BYTE_SIZE,
+  tokenMap,
+  MODULE_ASSETS_NAME_ID_MAP,
+  minFeePerByte,
+  DEFAULT_NUMBER_OF_SIGNATURES,
+  DEFAULT_SIGNATURE_BYTE_SIZE,
 } from '@constants';
 import { selectSchema } from '@utils/moduleAssets';
 import { extractAddress } from '@utils/account';
