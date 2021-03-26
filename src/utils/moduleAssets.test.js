dd55a23fea750a007e831304f15b93e926acd85a
iris salcedo <iris.salcedo@lightcurve.io>
2021-03-26 17:19:59 +0100

    Add selectSchema test

--- src/utils/moduleAssets.test.js
+++ src/utils/moduleAssets.test.js
@@ -1,5 +1,8 @@
 import { MODULE_ASSETS_NAME_ID_MAP } from '@constants';
-import { getModuleAssetSenderLabels } from './moduleAssets';
+import { getModuleAssetSenderLabels, selectSchema } from './moduleAssets';
+import http from './api/http';
+
+jest.mock('./api/http');
 
 describe('Utils: moduleAssets', () => {
   describe('getModuleAssetSenderLabels', () => {
@@ -11,6 +14,21 @@ describe('Utils: moduleAssets', () => {
       expect(label).toBeDefined();
       expect(typeof label).toBe('string');
       expect(t).toHaveBeenCalled();
+    });
+  });
+
+  describe('selectSchema', () => {
+    beforeEach(() => {
+      http.mockClear();
+    });
+
+    it('', async () => {
+      const expectedSchema = { id: 'id' };
+      http.mockImplementation(() => Promise.resolve({ data: [expectedSchema] }));
+      const schema = await selectSchema('2:0');
+      expect(schema).toEqual(expectedSchema);
+      await selectSchema('2:0');
+      expect(http).toHaveBeenCalledTimes(1);
     });
   });
 });
