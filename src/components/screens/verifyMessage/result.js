9125f6e1e2df77200cc46b6978aa50089575eaef
reyraa <ali@lightcurve.io>
2021-03-31 13:41:43 +0200

    Convert to buffer before verifying

--- src/components/screens/verifyMessage/result.js
+++ src/components/screens/verifyMessage/result.js
@@ -12,7 +12,11 @@ export default function Result({
 }) {
   let isCorrect = false;
   try {
-    isCorrect = cryptography.verifyMessageWithPublicKey(inputs);
+    isCorrect = cryptography.verifyMessageWithPublicKey({
+      publicKey: Buffer.from(inputs.publicKey, 'hex'),
+      signature: Buffer.from(inputs.signature, 'hex'),
+      message: inputs.message,
+    });
   } catch (e) {
     isCorrect = false;
   }
