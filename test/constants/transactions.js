6f953f9d145f847a6094ffc8bcc85b7a18783783
iris salcedo <iris.salcedo@lightcurve.io>
2021-04-01 12:50:56 +0200

    Update test transaction constant

--- test/constants/transactions.js
+++ test/constants/transactions.js
@@ -1,20 +1,26 @@
 const transaction = index => ({
+  //height: 9381199 + index,
+  //blockId: `35335302495521273${index}`,
+  //title: 'transfer',
+  //amount: `${index}00000000`,
   id: `1402835196409030464${index}`,
-  height: 9381199 + index,
-  blockId: `35335302495521273${index}`,
   type: 0,
-  title: 'transfer',
-  timestamp: 106359314,
-  senderPublicKey: 'f4852b270f76dc8b49bfa88de5906e81d3b001d23852f0e74ba60cac7180a184',
-  recipientPublicKey: '',
-  senderId: `60766716343473650${index}L`,
-  recipientId: `162819183031167495${index}L`,
-  amount: `${index}00000000`,
+  moduleAssetId: '2:0',
   fee: '10000000',
+  isPending: false,
+  sender: { address: 'lskdwsyfmcko6mcd357446yatromr9vzgu7eb8y11' },
+  asset: {
+    amount: '200',
+    recipientAddress: 'lskdwsyfmcko6mcd357446yatromr9vzgu7eb8y22',
+    data: 'test',
+  },
+  block: {
+    timestamp: 106359314,
+    height: 9381199 + index,
+  },
+  confirmations: 98 + index,
   signature: 'bbd659c908a609e27b491aeb429038fa8638b7eaed357043e5fbd463658caf7e9777b8b53f3d5e3ef4b6280e55d264b30162319eee24c3844c7c65974200ed00',
   signatures: [],
-  asset: {},
-  confirmations: 98 + index,
 });
 
 const transactions = Array(30).fill(1).map((item, index) => transaction(index));
