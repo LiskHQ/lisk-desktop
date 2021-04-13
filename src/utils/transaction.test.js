34d32c9da0187588dc81b4a0dd0cc8cf7f58d551
UsamaHameed <usamatech2@gmail.com>
2021-04-13 14:35:48 +0200

    add transfer transaction test

--- src/utils/transaction.test.js
+++ src/utils/transaction.test.js
@@ -1,5 +1,7 @@
 import { MODULE_ASSETS_NAME_ID_MAP } from '@constants';
-import { getTxAmount } from './transaction';
+import { getTxAmount, transformTransaction } from './transaction';
+import accounts from '../../test/constants/accounts';
+import { splitModuleAndAssetIds } from '@utils/moduleAssets';
 
 describe('API: LSK Transactions', () => {
   describe('getTxAmount', () => {
@@ -48,6 +50,44 @@ describe('API: LSK Transactions', () => {
       };
 
       expect(getTxAmount(tx)).toEqual(200000000);
+    });
+  });
+
+  describe.only('transformTransaction', () => {
+    const [moduleID, assetID] = splitModuleAndAssetIds(MODULE_ASSETS_NAME_ID_MAP.transfer);
+    const binaryAddress = 'd04699e57c4a3846c988f3c15306796f8eae5c1c';
+
+    it('should a transfer transaction with type signature of lisk service', () => {
+      const tx = {
+        moduleID,
+        assetID,
+        fee: 0.1,
+        nonce: 1,
+        id: Buffer.from('123', 'hex'),
+        senderPublicKey: accounts.genesis.summary.publicKey,
+        asset: { amount: 100000000, recipientAddress: binaryAddress, data: '' },
+      };
+
+      const expectedTransaction = {
+        id: '12',
+        moduleAssetId: '2:0',
+        fee: '0.1',
+        nonce: '1',
+        sender: {
+          publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a',
+          address: 'lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt',
+        },
+        signatures: undefined,
+        asset: {
+          recipient: {
+            address: 'lskzzzz3xu4xpzz6x2zzuzzbvzpz2zzrvz3zzxuzz3mzozzox24z2zzuzzzzzvuzz3z577dz7',
+          },
+          amount: '100000000',
+          data: '',
+        },
+      };
+
+      expect(transformTransaction(tx)).toMatchObject(expectedTransaction);
     });
   });
 });
