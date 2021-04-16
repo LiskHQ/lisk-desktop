c1deba434599271ef3666ddb62984ba398898a56
iris salcedo <iris.salcedo@lightcurve.io>
2021-04-16 13:36:24 +0200

    Style typos

--- src/components/screens/reclaimBalance/reclaim.js
+++ src/components/screens/reclaimBalance/reclaim.js
@@ -2,39 +2,46 @@ import React from 'react';
 import { withTranslation } from 'react-i18next';
 import CopyToClipboard from '@toolbox/copyToClipboard';
 import Tooltip from '@toolbox/tooltip/tooltip';
+import Icon from '@toolbox/icon';
 import AccountVisualWithAddress from '@shared/accountVisualWithAddress';
+import styles from './reclaim.css';
 
 const Reclaim = ({ t }) => (
-  <>
+  <div className={styles.container}>
     <h4>{t('Update to your new account')}</h4>
     <p>{t('Your tokens and passphrase are safe. We kindly ask you to transfer your balance to the new account.')}</p>
-    <section>
-      <div>
+    <section className={styles.box}>
+      <div className={styles.accountContainer}>
         <div>
           <h5>{t('Old account')}</h5>
-          <AccountVisualWithAddress address="5726759782318848681L" />
-          <CopyToClipboard />
+          <div className={styles.addressContainer}>
+            <AccountVisualWithAddress address="5726759782318848681L" />
+            <CopyToClipboard />
+          </div>
           <p>{t('Balance')}: 1000LSK</p>
         </div>
+        <Icon name="arrowRightActive" />
         <div>
           <h5>{t('New account')}</h5>
-          <AccountVisualWithAddress address="lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt" />
-          <CopyToClipboard />
+          <div className={styles.addressContainer}>
+            <AccountVisualWithAddress address="lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt" />
+            <CopyToClipboard />
+          </div>
           <p>{t('Balance')}: 0LSK</p>
         </div>
       </div>
       <div>
-        <p>{t('You will be able to:')}</p>
+        <h5 className={styles.listHeading}>{t('You will be able to:')}</h5>
         <ul>
-          <li>{t('Use your old passphrase ')}</li>
-          <li>{t('Access your old address and transaction history')}</li>
+          <li className={styles.listItem}>{t('Use your old passphrase ')}</li>
+          <li className={styles.listItem}>{t('Access your old address and transaction history')}</li>
         </ul>
       </div>
     </section>
-    <section>
-      <h5>{t('All you need to do:')}</h5>
+    <section className={styles.box}>
+      <h5 className={styles.listHeading}>{t('All you need to do:')}</h5>
       <ul>
-        <li>
+        <li className={styles.listItem}>
           <p>
             {t('Deposit at least 0.01 LSK to your new account')}
             <Tooltip position="right" size="m">
@@ -47,7 +54,7 @@ const Reclaim = ({ t }) => (
             </span>
           </p>
         </li>
-        <li>
+        <li className={styles.listItem}>
           <p>
             {t('Send a reclaim transaction')}
             <span>
@@ -57,7 +64,7 @@ const Reclaim = ({ t }) => (
         </li>
       </ul>
     </section>
-  </>
+  </div>
 );
 
 export default withTranslation()(Reclaim);
