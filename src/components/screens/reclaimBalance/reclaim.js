b573f09846c5574b13b14dad481bcc71925ddd35
iris salcedo <iris.salcedo@lightcurve.io>
2021-04-16 16:21:22 +0200

    Style list

--- src/components/screens/reclaimBalance/reclaim.js
+++ src/components/screens/reclaimBalance/reclaim.js
@@ -1,70 +1,110 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import CopyToClipboard from '@toolbox/copyToClipboard';
 import Tooltip from '@toolbox/tooltip/tooltip';
 import Icon from '@toolbox/icon';
+import { PrimaryButton } from '@toolbox/buttons';
+import DialogLink from '@toolbox/dialog/link';
 import AccountVisualWithAddress from '@shared/accountVisualWithAddress';
+import { getActiveTokenAccount } from '@utils/account';
 import styles from './reclaim.css';
 
-const Reclaim = ({ t }) => (
-  <div className={styles.container}>
-    <h4>{t('Update to your new account')}</h4>
-    <p>{t('Your tokens and passphrase are safe. We kindly ask you to transfer your balance to the new account.')}</p>
-    <section className={styles.box}>
-      <div className={styles.accountContainer}>
-        <div>
-          <h5>{t('Old account')}</h5>
-          <div className={styles.addressContainer}>
-            <AccountVisualWithAddress address="5726759782318848681L" />
-            <CopyToClipboard />
+const Reclaim = ({ t }) => {
+  const account = useSelector(state => getActiveTokenAccount(state));
+  // const hasEnoughtBalance = false;
+  const hasEnoughtBalance = parseInt(account.token?.balance, 10) > 1000000;
+
+  return (
+    <div className={styles.container}>
+      <h4>{t('Update to your new account')}</h4>
+      <p>{t('Your tokens and passphrase are safe. We kindly ask you to transfer your balance to the new account.')}</p>
+      <section className={styles.box}>
+        <div className={styles.accountContainer}>
+          <div>
+            <h5>{t('Old account')}</h5>
+            <div className={styles.addressContainer}>
+              <AccountVisualWithAddress address="5726759782318848681L" />
+              <CopyToClipboard />
+            </div>
+            <p>{t('Balance')}: 1000LSK</p>
           </div>
-          <p>{t('Balance')}: 1000LSK</p>
-        </div>
-        <Icon name="arrowRightActive" />
-        <div>
-          <h5>{t('New account')}</h5>
-          <div className={styles.addressContainer}>
-            <AccountVisualWithAddress address="lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt" />
-            <CopyToClipboard />
+          <Icon name="arrowRightActive" />
+          <div>
+            <h5>{t('New account')}</h5>
+            <div className={styles.addressContainer}>
+              <AccountVisualWithAddress address="lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt" />
+              <CopyToClipboard />
+            </div>
+            <p>{t('Balance')}: 0LSK</p>
           </div>
-          <p>{t('Balance')}: 0LSK</p>
         </div>
-      </div>
-      <div>
-        <h5 className={styles.listHeading}>{t('You will be able to:')}</h5>
-        <ul>
-          <li className={styles.listItem}>{t('Use your old passphrase ')}</li>
-          <li className={styles.listItem}>{t('Access your old address and transaction history')}</li>
+        <div>
+          <h5 className={styles.listHeading}>{t('You will be able to:')}</h5>
+          <ul className={styles.list}>
+            <li>{t('Use your old passphrase ')}</li>
+            <li>{t('Access your old address and transaction history')}</li>
+          </ul>
+        </div>
+      </section>
+      <section className={styles.box}>
+        <h5 className={styles.listHeading}>{t('All you need to do:')}</h5>
+        <ul className={styles.list}>
+          <li className={`${styles.step} ${hasEnoughtBalance ? styles.tick : styles.green}`}>
+            <p>
+              {t('Deposit at least 0.01 LSK to your new account')}
+              <Tooltip position="right" size="m">
+                <>
+                  <p>
+                    {t('Since you want to reclaim your LSK on the new blockchain, you need to pay the fee from your new account.')}
+                  </p>
+                  <br />
+                  <p>
+                    {t('Hence your LSK in your old account can not be used to pay the fee. Read more')}
+                  </p>
+                  <br />
+                  <p className={styles.link}>
+                    {t('Read more')}
+                  </p>
+                </>
+              </Tooltip>
+              <br />
+              {!hasEnoughtBalance && (
+                <>
+                  <span>
+                    {t('An initial one-time transfer fee will be deducted from the new account.')}
+                  </span>
+                  <br />
+                  <span>
+                    {t('Please use ')}
+                    <span className={styles.link}>{t('external services')}</span>
+                    {t(' to deposit LSK.')}
+                  </span>
+                </>
+              )}
+            </p>
+          </li>
+          <li className={`${styles.step} ${hasEnoughtBalance && styles.green}`}>
+            <p>
+              {t('Send a reclaim transaction')}
+              <br />
+              <span>
+                {t('Once you have enough tokens on your new account you will be able to send a transaction.')}
+              </span>
+            </p>
+          </li>
         </ul>
-      </div>
-    </section>
-    <section className={styles.box}>
-      <h5 className={styles.listHeading}>{t('All you need to do:')}</h5>
-      <ul>
-        <li className={styles.listItem}>
-          <p>
-            {t('Deposit at least 0.01 LSK to your new account')}
-            <Tooltip position="right" size="m">
-              <p>
-                {t('Since you want to reclaim your LSK on the new blockchain, you need to pay the fee from your new account. Hence your LSK in your old account can not be used to pay the fee. Read more')}
-              </p>
-            </Tooltip>
-            <span>
-              {t('An initial one-time transfer fee will be deducted from the new account. Please use external services to deposit LSK.')}
-            </span>
-          </p>
-        </li>
-        <li className={styles.listItem}>
-          <p>
-            {t('Send a reclaim transaction')}
-            <span>
-              {t('Once you have enough tokens on your new account you will be able to send a transaction.')}
-            </span>
-          </p>
-        </li>
-      </ul>
-    </section>
-  </div>
-);
+      </section>
+      <DialogLink component="balanceReclaim">
+        <PrimaryButton
+          className={styles.button}
+          disabled={!hasEnoughtBalance}
+        >
+            {t('Continue')}
+        </PrimaryButton>
+      </DialogLink>
+    </div>
+  );
+};
 
 export default withTranslation()(Reclaim);
