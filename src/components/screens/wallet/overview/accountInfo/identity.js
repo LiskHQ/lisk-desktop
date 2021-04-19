cc50a2c23255c9ca3f6a8b20f99bfb918d8066e1
reyraa <ali@lightcurve.io>
2021-04-20 01:02:36 +0200

    Break identifier into smaller components and add the legacy address

--- src/components/screens/wallet/overview/accountInfo/identity.js
+++ src/components/screens/wallet/overview/accountInfo/identity.js
@@ -1,7 +1,96 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { truncateAddress } from '@utils/account';
+import Tooltip from '@toolbox/tooltip/tooltip';
+import CopyToClipboard from '@toolbox/copyToClipboard';
 import styles from './accountInfo.css';
+
+const Legacy = ({ legacyAddress }) => (
+  <div className={styles.legacy}>
+    <span className={`${styles.text} ${styles.label}`}>Old address</span>
+    <CopyToClipboard
+      text={legacyAddress}
+      className={styles.text}
+    >
+      {legacyAddress}
+    </CopyToClipboard>
+  </div>
+);
+
+const WithTitle = ({
+  newAddress,
+  legacyAddress,
+  bookmark,
+  username = '',
+  t = str => str,
+  onClickFn,
+}) => (
+  <div className={styles.identity}>
+    <span className={`${styles.primary} ${styles.text} ${styles.noSelect} account-primary`}>
+      {username || bookmark.title}
+    </span>
+    <Tooltip
+      className={`${styles.tooltip} ${styles.secondary} ${styles.truncated}`}
+      title={t('Click to see full and old addresses')}
+      content={(
+        <span
+          className={`${styles.text} ${styles.noSelect} account-secondary-truncated`}
+          onClick={onClickFn}
+        >
+          {truncateAddress(newAddress)}
+        </span>
+      )}
+    />
+    <Tooltip
+      className={`${styles.tooltip} ${styles.secondaryTooltip} ${styles.full}`}
+      title={t('Click to hide full and old addresses')}
+      content={(
+        <span
+          className={`${styles.text} ${styles.noSelect} account-secondary-full`}
+          onClick={onClickFn}
+        >
+          {newAddress}
+        </span>
+      )}
+    />
+    <Legacy legacyAddress={legacyAddress} />
+  </div>
+);
+
+const WithOutTitle = ({
+  newAddress,
+  legacyAddress,
+  t = str => str,
+  onClickFn,
+}) => (
+  <div className={styles.identity}>
+    <Tooltip
+      className={`${styles.tooltip} ${styles.primary} ${styles.truncated}`}
+      title={t('Click to see full and old addresses')}
+      content={(
+        <span
+          className={`${styles.text} ${styles.primary} ${styles.noSelect} account-primary-truncated`}
+          onClick={onClickFn}
+        >
+          {truncateAddress(newAddress)}
+        </span>
+      )}
+    />
+    <Tooltip
+      className={`${styles.tooltip} ${styles.primaryTooltip} ${styles.full}`}
+      title={t('Click to hide full and old addresses')}
+      content={(
+        <span
+          className={`${styles.text} ${styles.primary} ${styles.noSelect} delegate-primary-full`}
+          onClick={onClickFn}
+        >
+          {newAddress}
+        </span>
+      )}
+    />
+    <Legacy legacyAddress={legacyAddress} />
+  </div>
+);
 
 /**
  *
@@ -10,45 +99,14 @@ import styles from './accountInfo.css';
  * but toggles expanded on user click
  *
  */
-const Identity = ({
-  address,
-  bookmark,
-  username = '',
-}) => {
-  if (!address) return null;
-  const truncatedAddress = truncateAddress(address);
-  const [isTruncated, setTruncationState] = useState(true);
-  const onClick = () => setTruncationState(!isTruncated);
-
-  return (
-    <div className={styles.text}>
-      {
-        username || !!bookmark
-          ? (
-            <>
-              <span className={`${styles.primary} account-primary`}>
-                {username || bookmark.title}
-              </span>
-              <span
-                className={`${styles.secondary} ${styles.noSelect} delegate-secondary`}
-                onClick={onClick}
-              >
-                {isTruncated ? truncatedAddress : address}
-              </span>
-            </>
-          )
-          : (
-            <span
-              className={`${styles.primary} ${styles.noSelect} account-primary`}
-              onClick={onClick}
-            >
-              {isTruncated ? truncatedAddress : address}
-            </span>
-          )
-      }
-    </div>
-  );
+const Identity = (props) => {
+  if (!props.newAddress) {
+    return null;
+  }
+  if (props.username || !!props.bookmark) {
+    return <WithTitle {...props} />;
+  }
+  return <WithOutTitle {...props} />;
 };
-
 
 export default Identity;
