042de36df844e0fd817545832f6fd0cbd64ed1ba
reyraa <ali@lightcurve.io>
2021-04-22 16:41:14 +0200

    Use Number instead of parseInt

--- src/components/shared/accountMigration/index.js
+++ src/components/shared/accountMigration/index.js
@@ -34,7 +34,7 @@ const AccountMigration = ({ t, account, showBalance }) => (
       {showBalance && (
         <p>
           <span>{`${t('Balance')}: `}</span>
-          <LiskAmount val={parseInt(account.token?.balance, 10)} token={token} />
+          <LiskAmount val={Number(account.token?.balance)} token={token} />
         </p>
       )}
     </div>
