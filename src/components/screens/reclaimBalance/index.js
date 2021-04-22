2dd082d5afc079e835e3a6c72849756ef50d81b4
iris salcedo <iris.salcedo@lightcurve.io>
2021-04-22 16:47:37 +0200

    Disable keyboard navigation

--- src/components/screens/reclaimBalance/index.js
+++ src/components/screens/reclaimBalance/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withTranslation } from 'react-i18next';
 import MultiStep from '@shared/multiStep';
 import Introduction from './introduction';
 import Reclaim from './reclaim';
 import styles from './index.css';
 
-const ReclaimBalance = ({ t }) => (
-  <MultiStep className={styles.multiStep}>
-    <Introduction t={t} />
-    <Reclaim t={t} />
-  </MultiStep>
-);
+const disableKeyboardNavigation = (e) => {
+  if (e.ctrlKey || e.metaKey) {
+    if (e.keyCode === 37 || e.keyCode === 39) {
+      e.preventDefault();
+    }
+  }
+};
+
+const ReclaimBalance = ({ t }) => {
+  useEffect(() => {
+    document.addEventListener('keydown', disableKeyboardNavigation);
+    return () => {
+      document.removeEventListener('keydown', disableKeyboardNavigation);
+    };
+  }, []);
+
+  return (
+    <MultiStep className={styles.multiStep}>
+      <Introduction t={t} />
+      <Reclaim t={t} />
+    </MultiStep>
+  );
+};
 
 export default withTranslation()(ReclaimBalance);
