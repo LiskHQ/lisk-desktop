58b7a9c085c699624690c431e62210b983712ea2
reyraa <ali@lightcurve.io>
2021-04-29 23:46:40 +0200

    Use components from dataColumns.js

--- src/components/screens/monitor/delegates/delegatesTable/delegateRow.js
+++ src/components/screens/monitor/delegates/delegatesTable/delegateRow.js
@@ -1,150 +1,35 @@
-/* eslint-disable no-nested-ternary */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import grid from 'flexboxgrid/dist/flexboxgrid.css';
 
 import { useDispatch } from 'react-redux';
 import { routes } from '@constants';
-import { truncateAddress } from '@utils/account';
 import { addedToWatchList, removedFromWatchList } from '@actions';
-import Tooltip from '@toolbox/tooltip/tooltip';
-import Icon from '@toolbox/icon';
-import AccountVisual from '@toolbox/accountVisual';
 import styles from '../delegates.css';
-import DelegateWeight from './delegateWeight';
-import { getStatusClass, getDelegateWeightClass, getRoundStateClass } from './tableHeader';
+import {
+  DelegateWeight,
+  DelegateDetails,
+  RoundStatus,
+  DelegateStatus,
+  ForgingTime,
+} from './dataColumns';
 
-const roundStatus = {
-  forging: 'Forging',
-  awaitingSlot: 'Awaiting slot',
-  missedBlock: 'Missed block',
-};
-
-const icons = {
-  forging: 'delegateForged',
-  awaitingSlot: 'delegateAwaiting',
-  missedBlock: 'delegateMissed',
-};
-
-const delegateStatus = {
-  active: 'Active',
-  standby: 'Standby',
-  banned: 'Banned',
-  punished: 'Punished',
-  nonEligible: 'Non-eligible to forge',
-};
-
-const getForgingTime = (data) => {
-  if (!data || data.time === undefined) return '-';
-  if (data.time === 0) return 'now';
-  const { time } = data;
-  const absTime = Math.abs(time);
+const getForgingTime = (time) => {
+  if (!time) return '-';
+  const diff = time - Math.floor((new Date()).getTime() / 1000);
+  if (Math.abs(diff) < 9) return 'now';
+  const absTime = Math.abs(diff);
   const minutes = absTime / 60 >= 1 ? `${Math.floor(absTime / 60)}m ` : '';
   const seconds = absTime % 60 >= 1 ? `${absTime % 60}s` : '';
-  if (data.time > 0) {
+  if (diff > 0) {
     return `in ${minutes}${seconds}`;
   }
   return `${minutes}${seconds} ago`;
 };
 
-// eslint-disable-next-line complexity
-const DelegateDetails = ({
-  t, watched = false, data, activeTab, removeFromWatchList, addToWatchList,
-}) => {
-  const showEyeIcon = activeTab === 'active' || activeTab === 'standby' || activeTab === 'watched';
-  return (
-    <div className={styles.delegateColumn}>
-      <Tooltip
-        tooltipClassName={styles.tooltipContainer}
-        className={styles.eyeIconTooltip}
-        position="bottom right"
-        size="s"
-        content={(
-          <span
-            className={`
-              ${styles.eyeIcon} ${!showEyeIcon ? 'hidden' : ''} ${watched && showEyeIcon ? styles.watchedDelegate : ''}
-            `}
-            onClick={watched ? removeFromWatchList : addToWatchList}
-          >
-            <Icon name={watched ? 'eyeActive' : 'eyeInactive'} />
-          </span>
-        )}
-      >
-        <p className={styles.watchedTooltip}>
-          {watched ? t('Remove from watched') : t('Add to watched')}
-        </p>
-      </Tooltip>
-
-      <div className={`${styles.delegateDetails}`}>
-        <AccountVisual address={data.address} />
-        <div>
-          <p className={styles.delegateName}>
-            {data.username}
-          </p>
-          <p className={styles.delegateAddress}>{truncateAddress(data.address)}</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const RoundStatus = ({ data, t, formattedForgingTime }) => (
-  <>
-    <Tooltip
-      title={data.forgingTime
-        ? t(roundStatus[data.forgingTime.status])
-        : t(roundStatus.missedBlock)}
-      position="left"
-      size="maxContent"
-      content={(
-        <Icon
-          className={styles.statusIcon}
-          name={
-            data.forgingTime
-              ? icons[data.forgingTime.status]
-              : icons.notForging
-          }
-        />
-      )}
-      footer={(
-        <p>{formattedForgingTime}</p>
-      )}
-    >
-      <p className={styles.statusToolip}>
-        {data.lastBlock && `Last block forged ${data.lastBlock}`}
-      </p>
-    </Tooltip>
-    {data.isBanned && (
-      <Tooltip
-        position="left"
-        size="maxContent"
-        content={<Icon className={styles.statusIcon} name="delegateWarning" />}
-        footer={(
-          <p>{formattedForgingTime}</p>
-        )}
-      >
-        <p>
-          {t('This delegate will be punished in upcoming rounds')}
-        </p>
-      </Tooltip>
-    )}
-  </>
-);
-
-const DelegateStatus = ({ activeTab, data }) => {
-  const status = data.totalVotesReceived < 1e11 ? 'nonEligible' : data.status;
-  return (
-    <span className={getStatusClass(activeTab)}>
-      <span className={`${styles.delegateStatus} ${styles[status]}`}>{delegateStatus[status]}</span>
-    </span>
-  );
-};
-
-// eslint-disable-next-line complexity
 const DelegateRow = ({
   data, className, t, activeTab, watchList, setActiveTab,
 }) => {
-  const formattedForgingTime = data.forgingTime && getForgingTime(data.forgingTime);
+  const formattedForgingTime = getForgingTime(data.nextForgingTime);
   const dispatch = useDispatch();
 
   const isWatched = watchList.find(address => address === data.address);
@@ -169,34 +54,19 @@ const DelegateRow = ({
       className={`${className} delegate-row ${styles.tableRow}`}
       to={`${routes.account.path}?address=${data.address}`}
     >
-      <span className={grid['col-xs-5']}>
-        <DelegateDetails
-          t={t}
-          data={data}
-          watched={isWatched}
-          activeTab={activeTab}
-          addToWatchList={addToWatchList}
-          removeFromWatchList={removeFromWatchList}
-        />
-      </span>
-      <span className={getDelegateWeightClass(activeTab)}>
-        <DelegateWeight value={data.totalVotesReceived} />
-      </span>
+      <DelegateDetails
+        t={t}
+        data={data}
+        watched={isWatched}
+        activeTab={activeTab}
+        addToWatchList={addToWatchList}
+        removeFromWatchList={removeFromWatchList}
+      />
+      <DelegateWeight value={data.totalVotesReceived} activeTab={activeTab} />
       {(activeTab === 'active' || activeTab === 'watched') && (
         <>
-          <span className={`
-            ${activeTab === 'active'
-            ? `${grid['col-xs-3']}`
-            : activeTab === 'watched' ? `${grid['col-xs-2']}`
-              : 'hidden'}
-            ${styles.noEllipsis}
-            `}
-          >
-            {formattedForgingTime}
-          </span>
-          <span className={`${getRoundStateClass(activeTab)} ${styles.noEllipsis} ${styles.statusIconsContainer}`}>
-            <RoundStatus data={data} t={t} formattedForgingTime={formattedForgingTime} />
-          </span>
+          <ForgingTime activeTab={activeTab} time={formattedForgingTime} />
+          <RoundStatus data={data} t={t} time={formattedForgingTime} activeTab={activeTab} />
         </>
       )}
       {(activeTab !== 'active') && <DelegateStatus data={data} activeTab={activeTab} />}
