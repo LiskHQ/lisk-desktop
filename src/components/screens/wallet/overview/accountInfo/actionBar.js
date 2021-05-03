49338dbc97dccc2c207875427c8c7c857ed1f261
Iris Salcedo <iris.salcedo@lightcurve.io>
2021-05-03 16:34:20 +0200

    Get and read isDelegate (#3522)

--- src/components/screens/wallet/overview/accountInfo/actionBar.js
+++ src/components/screens/wallet/overview/accountInfo/actionBar.js
@@ -77,11 +77,11 @@ const ActionBar = ({
                 data={username ? {
                   formAddress: address,
                   label: account.dpos?.delegate?.username,
-                  isDelegate: account.isDelegate,
+                  isDelegate: account.summary?.isDelegate,
                 } : {
                   formAddress: address,
                   label: bookmark ? bookmark.title : '',
-                  isDelegate: account.isDelegate,
+                  isDelegate: account.summary?.isDelegate,
                 }}
               >
                 <BookmarkIcon bookmark={bookmark} />
