14a526f77e958894d823df7ac752f94e975d8f33
reyraa <ali@lightcurve.io>
2021-05-06 01:11:21 +0200

    Update unit tests

--- src/utils/api/transaction/lsk.test.js
+++ src/utils/api/transaction/lsk.test.js
@@ -285,7 +285,7 @@ describe('API: LSK Transactions', () => {
         title: MODULE_ASSETS_NAME_ID_MAP.unlockToken,
         moduleAssetId: MODULE_ASSETS_NAME_ID_MAP.unlockToken,
         asset: {
-          unlockingObjects: [
+          unlockObjects: [
             {
               amount: '100000000',
             },
