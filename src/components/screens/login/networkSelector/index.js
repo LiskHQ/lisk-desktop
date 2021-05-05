6ff748aa160af4f5376c2d9bb02d8bb693ca9133
reyraa <ali@lightcurve.io>
2021-05-05 23:34:51 +0200

    Read selected network from the stored settings

--- src/components/screens/login/networkSelector/index.js
+++ src/components/screens/login/networkSelector/index.js
@@ -2,29 +2,25 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { networks, tokenMap } from '@constants';
+import { tokenMap } from '@constants';
 import { networkSelected, settingsUpdated } from '@actions';
 import NetworkSelectorComp from './networkSelector';
 
 const NetworkSelector = (props) => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
-  const selectedNetworkName = useSelector(state => state.network.name);
-  const selectedNetwork = useSelector(
-    state => state.network.networks[state.settings.token.active || tokenMap.LSK.key],
-  );
-  const selectedAddress = useSelector(
-    state => ((state.network.networks[state.settings.token.active || tokenMap.LSK.key]
-    && state.network.networks[state.settings.token.active || tokenMap.LSK.key].nodeUrl)
-    || (state.settings.network && state.settings.network.name === networks.customNode.name && state.settings.network.address)) || '',
-  );
+  const network = useSelector(state => state.network);
+  const settings = useSelector(state => state.settings);
+  const selectedNetworkName = network.name || settings.network.name;
+  const networkConfig = network.networks[settings.token.active || tokenMap.LSK.key];
+  const selectedAddress = networkConfig?.serviceUrl || settings.network?.address;
 
   return (
     <NetworkSelectorComp
       t={t}
       selectedNetworkName={selectedNetworkName}
-      selectedNetwork={selectedNetwork}
-      selectedAddress={selectedAddress}
+      selectedNetwork={networkConfig}
+      selectedAddress={selectedNetworkName === 'customNode' ? selectedAddress : ''}
       networkSelected={params => dispatch(networkSelected(params))}
       settingsUpdated={params => dispatch(settingsUpdated(params))}
       {...props}
