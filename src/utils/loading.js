5b0cff013bbb481d3c0faacd4f00c8cce439fe1a
UsamaHameed <usamatech2@gmail.com>
2021-05-06 15:57:59 +0200

    remove unused file

--- src/utils/loading.js
+++
@@ -1,6 +0,0 @@
-import { loadingStarted as loadingStartedAction, loadingFinished as loadingFinishedAction } from '@actions';
-import store from '@store';
-
-export const loadingStarted = data => store.dispatch(loadingStartedAction(data));
-
-export const loadingFinished = data => store.dispatch(loadingFinishedAction(data));
