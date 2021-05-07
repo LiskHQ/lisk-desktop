403e6b46a8c4cb94fc7e9b6ddcc8907940ee15ff
reyraa <ali@lightcurve.io>
2021-05-08 00:22:24 +0200

    Replace specific data with higher order keys

--- src/components/screens/login/networkSelector/index.js
+++ src/components/screens/login/networkSelector/index.js
@@ -1,27 +1,18 @@
 /* istanbul ignore file */
 import { connect } from 'react-redux';
 import NetworkSelector from './networkSelector';
-import { networkSet } from '../../../../actions/network';
+import { networkSet, networkStatusUpdated } from '../../../../actions/network';
 import { settingsUpdated } from '../../../../actions/settings';
-import networks from '../../../../constants/networks';
-import { tokenMap } from '../../../../constants/tokens';
-import { getAPIClient } from '../../../../utils/api/network';
 
-// eslint-disable-next-line complexity
 const mapStateToProps = state => ({
   network: state.network,
-  selectedNetwork: (state.network.networks.LSK && state.network.networks.LSK.code)
-    || networks.mainnet.code,
-  address: ((state.network.networks[state.settings.token.active || tokenMap.LSK.key]
-    && state.network.networks[state.settings.token.active || tokenMap.LSK.key].nodeUrl)
-    || (state.settings.network && state.settings.network.name === networks.customNode.name
-      && state.settings.network.address)) || '',
-  liskAPIClient: getAPIClient(tokenMap.LSK.key, state.network),
+  settings: state.settings,
 });
 
 const mapDispatchToProps = {
   networkSet,
   settingsUpdated,
+  networkStatusUpdated,
 };
 
 export default connect(
