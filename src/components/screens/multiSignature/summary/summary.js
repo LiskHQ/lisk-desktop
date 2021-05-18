d56d95985999a0cffdff8b0b1cc6120d3fe36988
UsamaHameed <usamatech2@gmail.com>
2021-05-18 12:36:24 +0200

    cleanup multisignature.summary

--- src/components/screens/multiSignature/summary/summary.js
+++ src/components/screens/multiSignature/summary/summary.js
@@ -1,43 +1,45 @@
 import React from 'react';
 
+import { MODULE_ASSETS_NAME_ID_MAP, tokenMap } from '@constants';
+import to from 'await-to-js';
+import { createMultiSignatureTransaction } from '@api/transaction';
 import Box from '../../../toolbox/box';
 import BoxContent from '../../../toolbox/box/content';
 import BoxFooter from '../../../toolbox/box/footer';
-import Piwik from '../../../../utils/piwik';
 import { PrimaryButton, SecondaryButton } from '../../../toolbox/buttons';
 import MultiSignatureReview from '../../../shared/multiSignatureReview';
-
 import ProgressBar from '../progressBar';
 import styles from './styles.css';
 
+const moduleAssetId = MODULE_ASSETS_NAME_ID_MAP.registerMultisignatureGroup;
+
 const Summary = ({
   t,
-  members = [
-    {
-      accountId: '8195226425328336181L', publicKey: '8155694652104526882', accountRole: 'mandatory',
-    },
-    { accountId: '6195226421328336181L', publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a', accountRole: 'optional' },
-    { accountId: '4827364921328336181L', publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a', accountRole: 'optional' },
-  ],
-  fee = 15000000, // rawLSK
-  requiredSignatures = 2,
+  members,
+  fee,
+  account,
+  network,
+  mandatoryKeys,
+  optionalKeys,
+  numberOfSignatures,
   prevStep,
   nextStep,
   transactionCreatedSuccess,
   transactionCreatedError,
 }) => {
-  const submitTransaction = () => {
-    Piwik.trackingEvent('MultiSig_SubmitTransaction', 'button', 'Sign');
-    // const txData = {
-    //  nonce: account.nonce,
-    //  fee: `${fee}`,
-    //  network,
-    // };
-
-    // const [error, tx] = await to(
-    //   create(txData, transactionTypes().unlockToken.key),
-    // );
-    const [error, tx] = [false, { id: 1 }];
+  const submitTransaction = async () => {
+    const [error, tx] = await to(
+      createMultiSignatureTransaction({
+        network,
+        mandatoryKeys,
+        optionalKeys,
+        numberOfSignatures,
+        moduleAssetId,
+        fee,
+        passphrase: account.passphrase,
+        senderPublicKey: account.senderPublicKey,
+      }, tokenMap.LSK.key),
+    );
 
     if (!error) {
       transactionCreatedSuccess(tx);
@@ -60,7 +62,7 @@ const Summary = ({
             t={t}
             members={members}
             fee={fee}
-            requiredSignatures={requiredSignatures}
+            numberOfSignatures={numberOfSignatures}
           />
         </BoxContent>
         <BoxFooter className={styles.footer} direction="horizontal">
