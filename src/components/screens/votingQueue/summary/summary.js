da3f9f3a5e044a27ba16567bf351a56e58b1a053
iris salcedo <iris.salcedo@lightcurve.io>
2021-05-18 11:32:39 +0200

    Remove unused import

--- src/components/screens/votingQueue/summary/summary.js
+++ src/components/screens/votingQueue/summary/summary.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import Piwik from '@utils/piwik';
 import TransactionInfo from '@shared/transactionInfo';
