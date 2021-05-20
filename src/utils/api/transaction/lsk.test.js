6a1a5b073dca3bc7af987174cbfd77dd44f95e7a
reyraa <ali@lightcurve.io>
2021-05-20 14:51:36 +0200

    Fix block API param

--- src/utils/api/transaction/lsk.test.js
+++ src/utils/api/transaction/lsk.test.js
@@ -146,7 +146,7 @@ describe('API: LSK Transactions', () => {
 
       expect(http).toHaveBeenCalledWith({
         path: '/api/v2/transactions',
-        params: { block: sampleId },
+        params: { blockId: sampleId },
         network,
         baseUrl: undefined,
       });
