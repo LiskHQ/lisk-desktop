b64250893a8c20d9caae6e6165120909874652c5
UsamaHameed <usamatech2@gmail.com>
2021-05-28 13:26:07 +0200

    refactor the reviewSign component

--- src/components/screens/signMultiSignTransaction/reviewSign/index.js
+++ src/components/screens/signMultiSignTransaction/reviewSign/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { getActiveTokenAccount } from '@utils/account';
 import ReviewSignComp from './reviewSign';
 
 const ReviewSign = (props) => {
   const { t } = useTranslation();
   const networkIdentifier = useSelector(state => state.network.networks.LSK.networkIdentifier);
-  const host = useSelector(state => state.account);
+  const account = useSelector(getActiveTokenAccount);
 
   return (
     <ReviewSignComp
       t={t}
       {...props}
       networkIdentifier={networkIdentifier}
-      host={host}
+      account={account}
     />
   );
 };
--- src/components/screens/signMultiSignTransaction/reviewSign/reviewSign.js
+++ src/components/screens/signMultiSignTransaction/reviewSign/reviewSign.js
@@ -8,69 +8,37 @@ import { PrimaryButton, SecondaryButton } from '../../../toolbox/buttons';
 import MultiSignatureReview from '../../../shared/multiSignatureReview';
 import ProgressBar from '../progressBar';
 import styles from '../styles.css';
-
-const convertKeyToMemberData = accountRole => publicKey => ({
-  accountId: extractAddressFromPublicKey(publicKey),
-  publicKey,
-  accountRole,
-});
+import TransactionDetails from '../../transactionDetails/transactionDetails';
 
 const ReviewSign = ({
   t,
   transaction,
   networkIdentifier,
-  host,
+  account,
   prevStep,
   nextStep,
-}) => {
-  const mandatoryMembers = transaction.asset.mandatoryKeys.map(convertKeyToMemberData('mandatory'));
-  const optionalMembers = transaction.asset.optionalKeys.map(convertKeyToMemberData('optional'));
-  const members = [...mandatoryMembers, ...optionalMembers];
-  const fee = parseInt(transaction.fee, 10);
-  const requiredSignatures = transaction.asset.numberOfSignatures;
-
-  const submitTransaction = () => {
-    try {
-      const signedTx = transactions.signMultiSignatureTransaction({
-        transaction,
-        passphrase: host.passphrase,
-        networkIdentifier,
-        keys: {
-          mandatoryKeys: transaction.asset.mandatoryKeys,
-          optionalKeys: transaction.asset.optionalKeys,
-        },
-      });
-      nextStep({ transactionInfo: signedTx });
-    } catch (error) {
-      nextStep({ error });
-    }
-  };
-
-  return (
-    <section>
-      <Box className={styles.boxContainer}>
-        <header>
-          <h1>{t('Sign multisignature transaction')}</h1>
-          <p>{t('If you have received a multisignature transaction that requires your signature, use this tool to review and sign it.')}</p>
-        </header>
-        <BoxContent>
-          <ProgressBar current={2} />
-          <MultiSignatureReview
-            t={t}
-            members={members}
-            fee={fee}
-            requiredSignatures={requiredSignatures}
-          />
-        </BoxContent>
-        <BoxFooter className={styles.footer} direction="horizontal">
-          <SecondaryButton className="go-back" onClick={prevStep}>{t('Edit')}</SecondaryButton>
-          <PrimaryButton className="confirm" size="l" onClick={submitTransaction}>
-            {t('Sign')}
-          </PrimaryButton>
-        </BoxFooter>
-      </Box>
-    </section>
-  );
-};
-
+}) => (
+  <section>
+    <Box className={styles.boxContainer}>
+      <header>
+        <h1>{t('Sign multisignature transaction')}</h1>
+        <p>{t('If you have received a multisignature transaction that requires your signature, use this tool to review and sign it.')}</p>
+      </header>
+      <BoxContent>
+        <ProgressBar current={2} />
+        <TransactionDetails
+          t={t}
+          activeToken="LSK"
+          schema={`${transaction.moduleAssetId}-preview`}
+          account={account}
+          transaction={{
+            data: transaction,
+            error: false,
+            isLoading: false,
+          }}
+        />
+      </BoxContent>
+    </Box>
+  </section>
+);
 export default ReviewSign;
