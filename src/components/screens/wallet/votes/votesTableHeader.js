16c2a111dd573f7d65607bb61513ec2d42fb94dd
reyraa <ali@lightcurve.io>
2021-06-11 10:13:53 +0200

    Remove rank and productivity

--- src/components/screens/wallet/votes/votesTableHeader.js
+++ src/components/screens/wallet/votes/votesTableHeader.js
@@ -4,24 +4,11 @@ import styles from './votes.css';
 export default t => ([
   {
     title: t('Delegate'),
-    classList: grid['col-sm-3'],
-  },
-  {
-    title: t('Productivity'),
-    classList: grid['col-sm-2'],
-    tooltip: {
-      title: t('Productivity'),
-      message: t('% of successfully forged blocks in relation to total blocks that were available for this particular delegate to forge'),
-      position: 'bottom',
-    },
-  },
-  {
-    title: t('Rank'),
-    classList: grid['col-sm-2'],
+    classList: grid['col-sm-5'],
   },
   {
     title: t('Delegate weight'),
-    classList: `${grid['col-sm-2']} ${grid['col-lg-2']}`,
+    classList: grid['col-sm-3'],
     tooltip: {
       title: t('Delegate weight'),
       message: t('The total amount of all votes a delegate has received.'),
@@ -30,7 +17,7 @@ export default t => ([
   },
   {
     title: t('Vote amount'),
-    classList: `${grid['col-sm-2']} ${grid['col-lg-2']} ${styles.flexRightAlign}`,
+    classList: `${grid['col-sm-3']} ${styles.flexRightAlign}`,
     tooltip: {
       title: t('Vote amount'),
       message: t('The amount of LSK blocked for voting.'),
