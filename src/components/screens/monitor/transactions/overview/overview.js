0b2e4dee15352a7d551538d4f8cb77bc52525aad
Oliver Beddows <oliver@lightcurve.io>
2021-06-19 14:33:36 +0200

    :pencil: Improve transaction statistics tooltips

--- src/components/screens/monitor/transactions/overview/overview.js
+++ src/components/screens/monitor/transactions/overview/overview.js
@@ -243,13 +243,13 @@ const Overview = ({ t, txStats }) => {
               <h5 className={`${styles.legend} ${styles.volume}`}>
                 <span>{t('Volume')}</span>
                 <Tooltip className={styles.tooltip} position="left">
-                  <p>{t('The aggregated LSK volume transferred on the given time period.')}</p>
+                  <p>{t('The aggregated LSK volume transferred over the selected time period.')}</p>
                 </Tooltip>
               </h5>
               <h5 className={`${styles.legend} ${styles.number}`}>
                 <span>{t('Number')}</span>
                 <Tooltip className={styles.tooltip} position="left">
-                  <p>{t('The number of transactions submitted on the given time period.')}</p>
+                  <p>{t('The number of transactions submitted over the selected time period.')}</p>
                 </Tooltip>
               </h5>
             </aside>
