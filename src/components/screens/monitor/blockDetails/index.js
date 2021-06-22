1e6dad529b4e7a861989a9bcf15b191838db5d44
reyraa <ali@lightcurve.io>
2021-06-22 11:17:54 +0200

    Add support for height query param

--- src/components/screens/monitor/blockDetails/index.js
+++ src/components/screens/monitor/blockDetails/index.js
@@ -12,6 +12,7 @@ import BlockDetails from './blockDetails';
 
 const mapStateToProps = (state, ownProps) => ({
   id: selectSearchParamValue(ownProps.history.location.search, 'id'),
+  height: selectSearchParamValue(ownProps.history.location.search, 'height'),
   currentHeight: state.blocks.latestBlocks.length ? state.blocks.latestBlocks[0].height : 0,
 });
 const ComposedBlockDetails = compose(
@@ -20,13 +21,13 @@ const ComposedBlockDetails = compose(
   withData({
     blockDetails: {
       apiUtil: (network, params) => getBlock({ network, params }),
-      getApiParams: (state, ownProps) => ({ blockId: ownProps.id }),
+      getApiParams: (_, ownProps) => ({ blockId: ownProps.id, height: ownProps.height }),
       transformResponse: response => (response.data && response.data[0]),
     },
     blockTransactions: {
       apiUtil: (network, params) => getTransactions({ network, params }, tokenMap.LSK.key),
       defaultData: [],
-      getApiParams: (state, ownProps) => ({ blockId: ownProps.id }),
+      getApiParams: (_, ownProps) => ({ blockId: ownProps.id }),
       transformResponse: (response, oldData, urlSearchParams) => (
         urlSearchParams.offset
           ? [...oldData, ...response.data.filter(block =>
