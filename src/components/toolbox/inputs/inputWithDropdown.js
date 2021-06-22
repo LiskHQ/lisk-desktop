1aa64b65ff8ddec55bc2ef6d8051feb4d0fc6380
reyraa <ali@lightcurve.io>
2021-06-22 13:19:47 +0200

    Pass placeholder to input

--- src/components/toolbox/inputs/inputWithDropdown.js
+++ src/components/toolbox/inputs/inputWithDropdown.js
@@ -6,11 +6,12 @@ import DropdownButton from '../dropdownButton';
 import styles from './input.css';
 
 const InputWithDropdown = ({
-  className, value, onChange, children, buttonLabel, ButtonComponent,
+  className, value, onChange, children, buttonLabel, ButtonComponent, placeholder,
 }) => (
   <div className={`${styles.inputWithDropdown} ${className} input-with-dropdown-container`}>
     <Input
       value={value}
+      placeholder={placeholder}
       onChange={onChange}
       size="m"
       className={`${styles.inputDropdownInput} input-with-dropdown-input`}
