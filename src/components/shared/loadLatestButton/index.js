87484d132f101e906447c359d94e8337cf19d70e
reyraa <ali@lightcurve.io>
2021-06-24 11:47:30 +0200

    Pass the component to Redux connect

--- src/components/shared/loadLatestButton/index.js
+++ src/components/shared/loadLatestButton/index.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux';
+import LoadLatestButton from './loadLatestButton';
 
 const mapStateToProps = state => ({
   latestBlocks: state.blocks.latestBlocks,
 });
 
-export default connect(mapStateToProps);
+export default connect(mapStateToProps)(LoadLatestButton);
