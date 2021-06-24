60419b2bfad521a72e3741564c4d2ad0d1c5a48c
reyraa <ali@lightcurve.io>
2021-06-24 10:40:45 +0200

    Update unit tests

--- src/components/shared/loadLatestButton/loadLatestButton.test.js
+++ src/components/shared/loadLatestButton/loadLatestButton.test.js
@@ -1,66 +1,42 @@
 import { act } from 'react-dom/test-utils';
 import React from 'react';
 import { mount } from 'enzyme';
-import io from 'socket.io-client';
-import { subscribeConnections } from '@api/ws';
-import LoadLatestButton from '.';
+import LoadLatestButton from './loadLatestButton';
 
-jest.mock('socket.io-client');
-
-const on = (ev, callback) => {
-  setTimeout(() => {
-    callback(ev);
-  }, 1);
-};
 const close = jest.fn();
-io.mockImplementation(() => ({ on, close }));
 
 describe('LoadLatestButton', () => {
   const props = {
     onClick: jest.fn(),
-    event: 'test.event',
+    entity: 'block',
     children: 'Test load button',
-  };
-  const render = () => {
-    const wrapper = mount(<LoadLatestButton {...props} />);
-    return wrapper;
+    latestBlocks: [],
   };
 
   it('renders empty by default', () => {
-    const wrapper = render();
+    const wrapper = mount(<LoadLatestButton {...props} />);
     expect(wrapper).toBeEmptyRender();
   });
 
-  it('shows button on websocket event and hides the button on click', () => {
-    jest.useFakeTimers();
-
-    const wrapper = render();
+  it('shows button when there is a new block', () => {
+    const wrapper = mount(<LoadLatestButton {...props} />);
     expect(wrapper).toBeEmptyRender();
-    act(() => {
-      jest.runOnlyPendingTimers();
+    wrapper.setProps({
+      latestBlocks: [
+        { height: 11111112 },
+      ],
     });
-    wrapper.update();
+    act(() => {
+      wrapper.update();
+    });
     expect(wrapper).toContainExactlyOneMatchingElement('button');
     expect(wrapper).toHaveText(props.children);
 
-    wrapper.find('button').simulate('click');
+    wrapper.find('button').at(0).simulate('click');
     expect(props.onClick).toHaveBeenCalledWith();
     act(() => {
-      jest.runOnlyPendingTimers();
+      wrapper.update();
     });
     expect(wrapper).toBeEmptyRender();
-    expect(subscribeConnections[props.event]).toBeDefined();
-  });
-
-  it('clears the timeout before unmounting', () => {
-    jest.useFakeTimers();
-    const wrapper = render();
-    expect(subscribeConnections[props.event]).toBeDefined();
-    expect(close).toHaveBeenCalledTimes(0);
-
-    wrapper.unmount();
-    expect(clearTimeout).toHaveBeenCalledTimes(1);
-    expect(close).toHaveBeenCalledTimes(1);
-    expect(subscribeConnections[props.event]).not.toBeDefined();
   });
 });
