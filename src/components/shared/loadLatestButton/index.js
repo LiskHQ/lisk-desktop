340caad08b1f1df015477772d411cc558ec8b217
reyraa <ali@lightcurve.io>
2021-06-24 10:04:11 +0200

    Use latest blocks to display the button

--- src/components/shared/loadLatestButton/index.js
+++ src/components/shared/loadLatestButton/index.js
@@ -1,33 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import { PrimaryButton } from '@toolbox/buttons';
 import Icon from '@toolbox/icon';
-import useServiceSocketUpdates from '../../../hooks/useServiceSocketUpdates';
 import styles from './loadLatestButton.css';
 
-const LoadLatestButton = ({ children, onClick, event }) => {
-  const [isUpdateAvailable, hideUpdateButton] = useServiceSocketUpdates(event);
-  const [hasUpdatedRecently, didUpdateRecently] = useState(false);
+const shouldShow = {
+  block: (updateHeight, latestBlocks) => (
+    latestBlocks.length > 0
+    && latestBlocks[0].height > updateHeight
+  ),
+  transaction: () => (updateHeight, latestBlocks) => (
+    latestBlocks.length > 0
+    && latestBlocks[0].height > updateHeight
+    && latestBlocks[0].numberOfTransactions > 0
+  ),
+};
+
+const LoadLatestButton = ({ children, onClick, entity }) => {
+  const { latestBlocks } = useSelector(state => state.blocks);
+  const [updateHeight, setUpdateHeight] = useState(
+    latestBlocks.length ? latestBlocks[0].height : 0,
+  );
 
   const handleClick = () => {
-    hideUpdateButton();
-    const timer = setTimeout(() => {
-      didUpdateRecently(false);
-    }, 10000);
-    didUpdateRecently(timer);
+    setUpdateHeight(latestBlocks[0].height);
     onClick();
   };
 
-  // willUnmount
-  useEffect(() => () => clearTimeout(hasUpdatedRecently), []);
-
-  return (isUpdateAvailable && hasUpdatedRecently === false
+  return shouldShow[entity](latestBlocks, updateHeight)
     ? (
       <PrimaryButton onClick={handleClick} className={styles.button}>
         <Icon name="arrowUpCircle" className={styles.icon} />
         {children}
       </PrimaryButton>
     )
-    : null);
+    : null;
+};
+LoadLatestButton.propTypes = {
+  entity: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default LoadLatestButton;
