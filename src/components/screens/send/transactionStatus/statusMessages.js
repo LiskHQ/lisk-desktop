27ae3598bc08bf53d853a5417ea85b8352421bc5
reyraa <ali@lightcurve.io>
2021-06-29 09:46:32 +0200

    Update titles

--- src/components/screens/send/transactionStatus/statusMessages.js
+++ src/components/screens/send/transactionStatus/statusMessages.js
@@ -1,16 +1,20 @@
 /* istanbul ignore file */
 const statusMessages = t => ({
+  pending: {
+    title: t('Submitting the transaction'),
+    message: t('Your transaction is being submitted to the blockchain.'),
+  },
   success: {
     title: t('Transaction submitted'),
-    paragraph: t('Your transaction has been submitted and will be confirmed in a few moments.'),
+    message: t('Your transaction has been submitted and will be confirmed in a few moments.'),
   },
   error: {
     title: t('Transaction failed'),
-    paragraph: t('Oops, it looks like something went wrong. Please try again.'),
+    message: t('Oops, it looks like something went wrong. Please try again.'),
   },
   hw: {
     title: t('Transaction aborted on device'),
-    paragraph: t('You have cancelled the transaction on your hardware wallet.'),
+    message: t('You have cancelled the transaction on your hardware wallet.'),
   },
 });
 
