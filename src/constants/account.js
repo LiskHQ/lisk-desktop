f80dc23b022a6c7434f1e7ba106cf7984c514701
iris salcedo <iris.salcedo@lightcurve.io>
2021-06-29 12:46:59 +0200

    Update times

--- src/constants/account.js
+++ src/constants/account.js
@@ -1,8 +1,8 @@
 export const balanceNeededForInitialization = 2e7;
 export const balanceNeededForReclaim = 1e6;
 const account = {
-  lockDuration: 10000, // lock duration time is 10 minutes in milliSecond
-  warnLockDuration: 5000,
+  lockDuration: 600000, // lock duration time is 10 minutes in milliSecond
+  warnLockDuration: 480000, // warning time is 8 minutes in milliSecond
 };
 
 export default account;
