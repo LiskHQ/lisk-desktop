2c3067d4033f9d0ff2644915e015389ddccf9b49
UsamaHameed <usamatech2@gmail.com>
2021-07-05 16:29:22 +0200

    add steps to valdiate the pasted transaction

--- src/components/screens/signMultiSignTransaction/importData/importData.js
+++ src/components/screens/signMultiSignTransaction/importData/importData.js
@@ -1,11 +1,14 @@
 /* eslint-disable complexity */
 import React, { useState, useEffect } from 'react';
-import { transformTransaction } from '@utils/transaction';
+import { transformTransaction, createTransactionObject, flattenTransaction } from '@utils/transaction';
+import { joinModuleAndAssetIds } from '@utils/moduleAssets';
 import Box from '@toolbox/box';
 import BoxContent from '@toolbox/box/content';
 import BoxFooter from '@toolbox/box/footer';
 import { PrimaryButton } from '@toolbox/buttons';
 import Feedback from '@toolbox/feedback/feedback';
+import { validateTransaction } from '@liskhq/lisk-transactions';
+import { moduleAssetSchemas } from '@constants';
 import ProgressBar from '../progressBar';
 import styles from './styles.css';
 
@@ -23,12 +26,27 @@ const ImportData = ({ t, nextStep }) => {
     }
   };
 
+  // eslint-disable-next-line max-statements
   const validateAndSetTransaction = (input) => {
     try {
       const parsedInput = JSON.parse(input);
       setTransaction(parsedInput);
+      const moduleAssetId = joinModuleAndAssetIds({
+        moduleID: parsedInput.moduleID,
+        assetID: parsedInput.assetID,
+      });
+
+      const schema = moduleAssetSchemas[moduleAssetId];
+      const tx1 = transformTransaction(parsedInput);
+      const flat = flattenTransaction(tx1);
+      const transactionObejct = createTransactionObject(flat, moduleAssetId);
+      const err = validateTransaction(schema, transactionObejct);
+      if (err) {
+        throw Error('Unknown transaction');
+      }
       setError(undefined);
     } catch (e) {
+      console.log(e);
       setTransaction(undefined);
       setError('Invalid transaction');
     }
@@ -45,8 +63,6 @@ const ImportData = ({ t, nextStep }) => {
       validateAndSetTransaction(target.result);
     };
   }, []);
-
-  console.log(transaction);
 
   return (
     <section>
