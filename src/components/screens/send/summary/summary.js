3d28aef795d8e9cc872ec2cbf045d05d1329c82b
reyraa <ali@lightcurve.io>
2021-08-03 12:12:05 +0200

    Convert Send summary to function component

--- src/components/screens/send/summary/summary.js
+++ src/components/screens/send/summary/summary.js
@@ -1,37 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { loginTypes, MODULE_ASSETS_NAME_ID_MAP } from '@constants';
 import { toRawLsk, fromRawLsk } from '@utils/lsk';
 import { isEmpty } from '@utils/helpers';
+import { selectNetworkIdentifier } from '@store/selectors';
 import Piwik from '@utils/piwik';
 import TransactionSummary from '@shared/transactionSummary';
 import TransactionInfo from '@shared/transactionInfo';
+import { signTransaction, transformTransaction } from '@utils/transaction';
+import { useSelector } from 'react-redux';
 
-class Summary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.prevStep = this.prevStep.bind(this);
-    this.submitTransaction = this.submitTransaction.bind(this);
-  }
+const Summary = ({
+  resetTransactionResult,
+  transactionCreated,
+  isInitialization,
+  transactions,
+  prevStep,
+  nextStep,
+  account,
+  fields,
+  token,
+  t,
+}) => {
+  const networkIdentifier = useSelector(selectNetworkIdentifier);
+  const [secondPass, setSecondPass] = useState('');
+  const [doubleSignedTx, setDoubleSignedTx] = useState(null);
 
-  componentDidMount() {
-    const { fields } = this.props;
-
-    this.props.transactionCreated({
+  useEffect(() => {
+    transactionCreated({
       amount: `${toRawLsk(fields.amount.value)}`,
       data: fields.reference ? fields.reference.value : '',
       recipientAddress: fields.recipient.address,
       fee: toRawLsk(parseFloat(fields.fee.value)),
     });
-  }
+  }, []);
 
-  submitTransaction(fn) {
-    const {
-      account,
-      fields,
-      nextStep,
-      transactions,
-    } = this.props;
+  useEffect(() => {
+    if (secondPass) {
+      const [signedTx, err] = signTransaction(
+        transformTransaction(transactions.signedTransaction),
+        secondPass,
+        networkIdentifier,
+        { data: account },
+        false,
+      );
+      console.log('signedTx', signedTx);
+      if (!err) {
+        setDoubleSignedTx(signedTx);
+      }
+    }
+  }, [secondPass]);
 
+  const submitTransaction = (fn) => {
     if (!account.summary.isMultisignature) {
       Piwik.trackingEvent('Send_SubmitTransaction', 'button', 'Next step');
       if (account.loginType !== loginTypes.passphrase.code
@@ -54,57 +73,54 @@ class Summary extends React.Component {
         });
       }
     } else {
-      fn(transactions.signedTransaction);
+      fn(doubleSignedTx || transactions.signedTransaction);
     }
-  }
+  };
 
-  prevStep() {
+  const goBack = () => {
     Piwik.trackingEvent('Send_Summary', 'button', 'Previous step');
-    this.props.resetTransactionResult();
-    this.props.prevStep({ fields: this.props.fields });
-  }
+    resetTransactionResult();
+    prevStep({ fields });
+  };
 
-  render() {
-    const {
-      fields, t, token, account, isInitialization, transactions,
-    } = this.props;
-    const transaction = transactions.signedTransaction;
-    const amount = transaction?.asset?.amount
-      ? fromRawLsk(transaction?.asset?.amount) : fields.amount.value;
+  const transaction = transactions.signedTransaction;
+  const amount = transaction?.asset?.amount
+    ? fromRawLsk(transaction?.asset?.amount) : fields.amount.value;
 
-    return (
-      <TransactionSummary
-        title={t('Transaction summary')}
-        t={t}
-        account={account}
-        confirmButton={{
-          label: isInitialization ? t('Send') : t('Send {{amount}} {{token}}', { amount, token }),
-          onClick: this.submitTransaction,
-        }}
-        cancelButton={{
-          label: t('Edit transaction'),
-          onClick: this.prevStep,
-        }}
-        showCancelButton={!isInitialization}
-        fee={!account.summary.isMultisignature && fields.fee.value}
+  return (
+    <TransactionSummary
+      title={t('Transaction summary')}
+      t={t}
+      account={account}
+      confirmButton={{
+        label: isInitialization ? t('Send') : t('Send {{amount}} {{token}}', { amount, token }),
+        onClick: submitTransaction,
+      }}
+      cancelButton={{
+        label: t('Edit transaction'),
+        onClick: goBack,
+      }}
+      showCancelButton={!isInitialization}
+      fee={!account.summary.isMultisignature && fields.fee.value}
+      token={token}
+      createTransaction={submitTransaction}
+      keys={account.keys}
+      setSecondPass={setSecondPass}
+    >
+      <TransactionInfo
+        fields={fields}
         token={token}
-        createTransaction={this.submitTransaction}
-      >
-        <TransactionInfo
-          fields={fields}
-          token={token}
-          moduleAssetId={MODULE_ASSETS_NAME_ID_MAP.transfer}
-          transaction={
-            !isEmpty(transaction)
-              ? transaction
-              : { asset: { amount: toRawLsk(fields.amount.value) } }
-          }
-          account={account}
-          isMultisignature={account.summary.isMultisignature}
-        />
-      </TransactionSummary>
-    );
-  }
-}
+        moduleAssetId={MODULE_ASSETS_NAME_ID_MAP.transfer}
+        transaction={
+          !isEmpty(transaction)
+            ? transaction
+            : { asset: { amount: toRawLsk(fields.amount.value) } }
+        }
+        account={account}
+        isMultisignature={account.summary.isMultisignature}
+      />
+    </TransactionSummary>
+  );
+};
 
 export default Summary;
