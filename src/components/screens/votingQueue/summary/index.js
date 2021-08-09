2732f0c52c739d303c3b50e6cb6dc755bcc92084
iris salcedo <iris.salcedo@lightcurve.io>
2021-08-10 01:33:25 +0200

    Add vote tx

--- src/components/screens/votingQueue/summary/index.js
+++ src/components/screens/votingQueue/summary/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { votesSubmitted } from '@actions';
+import { votesSubmitted, transactionDoubleSigned } from '@actions';
 import { getActiveTokenAccount } from '@utils/account';
 import SummaryComponent from './summary';
 
@@ -18,6 +18,9 @@ const Summary = (props) => {
       t={t}
       account={account}
       transactions={transactions}
+      transactionDoubleSigned={(params) => {
+        dispatch(transactionDoubleSigned(params));
+      }}
       votesSubmitted={(params) => {
         dispatch(votesSubmitted(params));
       }}
