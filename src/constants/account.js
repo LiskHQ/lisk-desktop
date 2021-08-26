a6561f6afb693d33c4d7d47cc988a898c87affcb
iris salcedo <iris.salcedo@lightcurve.io>
2021-08-26 16:19:44 +0200

    Increase balance required for reclaim

--- src/constants/account.js
+++ src/constants/account.js
@@ -1,5 +1,5 @@
 export const balanceNeededForInitialization = 2e7;
-export const balanceNeededForReclaim = 1e6;
+export const balanceNeededForReclaim = 5e6;
 const account = {
   lockDuration: 600000, // lock duration time is 10 minutes in milliSecond
   warnLockDuration: 480000, // warning time is 8 minutes in milliSecond
