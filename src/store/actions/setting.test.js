a2306bfcb3dbdcee4ba3a803e67eff174a4a8ca7
Ikemefuna Obioha <ikemoobioha@gmail.com>
2021-09-08 10:13:08 +0100

    Fixed settings action test

--- src/store/actions/setting.test.js
+++ src/store/actions/setting.test.js
@@ -2,9 +2,18 @@ import { actionTypes } from '@constants';
 import {
   settingsReset,
   settingsUpdated,
+  settingsRetrieved,
 } from './settings';
+import { getFromStorage } from '../../utils/localJSONStorage';
 
 describe('actions: setting', () => {
+  const dispatch = jest.fn();
+  const settings = { autoLog: false };
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('settingsUpdated', () => {
     it('should create an action to update settings', () => {
       const data = { autoLog: false };
@@ -25,4 +34,21 @@ describe('actions: setting', () => {
       expect(settingsReset()).toEqual(expectedAction);
     });
   });
+
+  describe('settingsRetrieved', () => {
+    it('should create an action to retrieve settings', () => {
+      const initialState = { autoLog: true }
+
+      window.localStorage.getItem = jest.fn(() => JSON.stringify(settings))
+      getFromStorage('settings', initialState, async (data) => {
+        const expectedAction = {
+          data,
+          type: actionTypes.settingsRetrieved,
+        };
+
+        await settingsRetrieved(data)(dispatch)
+        expect(dispatch).toHaveBeenCalledWith(expectedAction);
+      })
+    })
+  })
 });
