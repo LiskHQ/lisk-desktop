02f12b5847793ffc14021484409874ac9ba09877
Gad Jacobs <gadexofficial@gmail.com>
2021-10-14 03:13:58 +0100

    :zap: Add locked amount value to dashboard

--- src/components/shared/walletDetails/walletDetails.js
+++ src/components/shared/walletDetails/walletDetails.js
@@ -10,14 +10,16 @@ import BoxRow from '@toolbox/box/row';
 import Icon from '@toolbox/icon';
 import LiskAmount from '../liskAmount';
 import DiscreetMode from '../discreetMode';
+import LockedBalanceLink from '../../screens/wallet/overview/balanceInfo/unlocking';
 import styles from './walletDetails.css';
 
 const WalletDetails = ({
   t, account, settings, className,
 }) => {
   const dispatch = useDispatch();
-  const tokens = Object.entries(account.info || {})
-    .filter(([key, info]) => settings.token.list[key] && info);
+  const tokens = Object.entries(account.info || {}).filter(
+    ([key, info]) => settings.token.list[key] && info,
+  );
 
   return (
     <Box className={`${styles.box} ${className}`}>
@@ -25,29 +27,31 @@ const WalletDetails = ({
         <h1>{t('Wallet details')}</h1>
       </BoxHeader>
       <BoxContent className={`${styles.container} coin-container`}>
-        {
-        tokens.map(([token, info]) => (
-          <BoxRow key={`${info.address}-${token}`} className={`${styles.row} coin-row`}>
+        {tokens.map(([token, info]) => (
+          <BoxRow
+            key={`${info.address}-${token}`}
+            className={`${styles.row} coin-row`}
+          >
             <Link
               to={routes.wallet.path}
-              onClick={() => dispatch(settingsUpdated({ token: { active: token } }))}
+              onClick={() =>
+                dispatch(settingsUpdated({ token: { active: token } }))}
             >
               <Icon name={token === tokenMap.BTC.key ? 'btcIcon' : 'lskIcon'} />
               <div className={styles.details}>
-                <span>{t('{{token}} balance', { token: tokenMap[token].label })}</span>
+                <span>
+                  {t('{{token}} balance', { token: tokenMap[token].label })}
+                </span>
                 <DiscreetMode>
                   <span className={styles.amounts}>
-                    <LiskAmount
-                      val={info.summary?.balance}
-                      token={token}
-                    />
+                    <LiskAmount val={info.summary?.balance} token={token} />
                   </span>
+                  {token === tokenMap.LSK.key ? <LockedBalanceLink /> : null}
                 </DiscreetMode>
               </div>
             </Link>
           </BoxRow>
-        ))
-      }
+        ))}
       </BoxContent>
     </Box>
   );
