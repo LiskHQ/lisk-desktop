242f3b10f98cc6ddf24c2bba95dd28cd47356bdd
Gad Jacobs <gadexofficial@gmail.com>
2021-10-25 05:31:11 +0100

    Update add vote warning text

--- src/components/shared/warnPunishedDelegate/voteWarning.js
+++ src/components/shared/warnPunishedDelegate/voteWarning.js
@@ -7,9 +7,8 @@ const VoteWarning = ({ t, daysLeft }) => (
   <div className={styles.voteContainer}>
     <Icon name="warningYellow" />
     {t(
-      'Caution! You are about to vote for the punished delegate, this will result in your LSK tokens being locked for a period of {{lockedTime}} days. In addition, please note that your vote will not be counted until the {{daysLeft}} day period has expired.',
+      'Caution! You are about to vote for the punished delegate, this will result in your LSK tokens being locked for a period of {{daysLeft}} days. In addition, please note that your vote will not be counted until the {{daysLeft}} day period has expired.',
       {
-        lockedTime: 'XX',
         daysLeft,
       },
     )}
