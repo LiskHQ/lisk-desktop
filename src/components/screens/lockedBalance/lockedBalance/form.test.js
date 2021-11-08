6d4e78b9391cf4259f0be6761be8028d5a8a8ada
reyraa <ali@lightcurve.io>
2021-11-08 11:40:02 +0100

    Update lockedBalance unit tests

---
+++ src/components/screens/lockedBalance/lockedBalance/form.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import signedTransaction from '../../../../../test/fixtures/signedTx.json';
+import Form from './form';
+
+describe('Unlock LSK form', () => {
+  let wrapper;
+
+  const props = {
+    t: str => str,
+    nextStep: jest.fn(),
+    data: {},
+    signedTransaction: {},
+    txSignatureError: null,
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    wrapper = mount(<Form {...props} />);
+  });
+
+  it('calls nextStep when the transactions is successfully signed', async () => {
+    wrapper.setProps({ signedTransaction });
+    act(() => { wrapper.update(); });
+    expect(props.nextStep).toBeCalledWith(
+      expect.objectContaining({ transactionInfo: expect.any(Object) }),
+    );
+  });
+
+  it('calls nextStep when the transactions is successfully signed', async () => {
+    wrapper.setProps({ txSignatureError: { message: 'some error' } });
+    act(() => { wrapper.update(); });
+    expect(props.nextStep).toBeCalledWith(
+      expect.objectContaining({ fee: undefined }),
+    );
+  });
+});
