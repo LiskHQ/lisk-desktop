0502a760464737b65314293bf2114f6c65239223
Ali Haghighatkhah <ali@lightcurve.io>
2021-11-11 09:17:36 +0100

    Break down the summary component

--- src/components/screens/reclaimBalance/modal/summary/index.js
+++ src/components/screens/reclaimBalance/modal/summary/index.js
@@ -1,106 +1,22 @@
-import React from 'react';
-import to from 'await-to-js';
-import { useSelector } from 'react-redux';
-import { selectAccount } from '@store/selectors';
-import { create, computeTransactionId } from '@api/transaction';
-import { signTransactionByHW } from '@utils/hwManager';
-import { tokenMap, MODULE_ASSETS_NAME_ID_MAP, loginTypes } from '@constants';
-import { useTransactionFeeCalculation, useTransactionPriority } from '@shared/transactionPriority';
-import TransactionSummary from '@shared/transactionSummary';
-import TransactionInfo from '@shared/transactionInfo';
-import { toRawLsk } from '@utils/lsk';
-import styles from './summary.css';
+// istanbul ignore file
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withTranslation } from 'react-i18next';
+import { balanceReclaimed } from '@actions/account';
 
-const moduleAssetId = MODULE_ASSETS_NAME_ID_MAP.reclaimLSK;
+import Summary from './summary';
 
-const Summary = ({
-  nextStep,
-  t,
-}) => {
-  const account = useSelector(selectAccount);
-  const network = useSelector(state => state.network);
-  const senderPublicKey = account.info.LSK.summary.publicKey;
-  const [
-    // eslint-disable-next-line no-unused-vars
-    selectedPriority, _, priorityOptions,
-  ] = useTransactionPriority(tokenMap.LSK.key);
+const mapStateToProps = state => ({
+  account: state.account,
+  signedTransaction: state.transactions.signedTransaction,
+  txSignatureError: state.transactions.txSignatureError,
+});
 
-  const { minFee } = useTransactionFeeCalculation({
-    selectedPriority,
-    token: tokenMap.LSK.key,
-    account: account.info.LSK,
-    priorityOptions,
-    transaction: {
-      moduleAssetId,
-      nonce: account.info.LSK.sequence.nonce,
-      senderPublicKey,
-      amount: account.info.LSK.legacy.balance,
-    },
-  });
-
-  // eslint-disable-next-line max-statements
-  const onSubmit = async () => {
-    const isHwSigning = account.loginType !== loginTypes.passphrase.code;
-    const data = {
-      network,
-      account: account.info.LSK,
-      transactionObject: {
-        moduleAssetId,
-        fee: toRawLsk(minFee.value),
-        amount: account.info.LSK.legacy.balance,
-        keys: { numberOfSignatures: 0 },
-      },
-      isHwSigning,
-    };
-
-    let [error, tx] = await to(
-      create(data, tokenMap.LSK.key),
-    );
-
-    if (error) {
-      nextStep({ transactionError: error, balance: account.info.LSK.legacy?.balance });
-    }
-
-    if (isHwSigning) {
-      // tx contain txObject and txBytes that needs to be signed by HW
-      [error, tx] = await to(signTransactionByHW(
-        account,
-        tx.networkIdentifier,
-        tx.transactionObject,
-        tx.transactionBytes,
-      ));
-
-      if (!error && tx) {
-        tx.id = computeTransactionId({ transaction: tx });
-      }
-    }
-
-    if (!error) {
-      nextStep({ transactionInfo: tx, balance: account.info.LSK.legacy?.balance });
-    } else {
-      nextStep({ transactionError: error, balance: account.info.LSK.legacy?.balance });
-    }
-  };
-
-  const onConfirmAction = {
-    label: t('Continue'),
-    onClick: onSubmit,
-  };
-
-  return (
-    <TransactionSummary
-      title={t('Transaction summary')}
-      t={t}
-      account={account.info.LSK}
-      confirmButton={onConfirmAction}
-      showCancelButton={false}
-      fee={minFee.value}
-      token={tokenMap.LSK.key}
-      classNames={styles.summaryContainer}
-    >
-      <TransactionInfo account={account} moduleAssetId={MODULE_ASSETS_NAME_ID_MAP.reclaimLSK} />
-    </TransactionSummary>
-  );
+const mapDispatchToProps = {
+  balanceReclaimed,
 };
 
-export default Summary;
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withTranslation(),
+)(Summary);
---
+++ src/components/screens/reclaimBalance/modal/summary/summary.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react';
+import { tokenMap, MODULE_ASSETS_NAME_ID_MAP } from '@constants';
+import { isEmpty } from '@utils/helpers';
+import { useTransactionFeeCalculation, useTransactionPriority } from '@shared/transactionPriority';
+import TransactionSummary from '@shared/transactionSummary';
+import TransactionInfo from '@shared/transactionInfo';
+import styles from './summary.css';
+
+const Summary = ({
+  signedTransaction,
+  txSignatureError,
+  balanceReclaimed,
+  nextStep,
+  account,
+  t,
+}) => {
+  const [
+    selectedPriority,, priorityOptions,
+  ] = useTransactionPriority(tokenMap.LSK.key);
+
+  const { minFee } = useTransactionFeeCalculation({
+    selectedPriority,
+    token: tokenMap.LSK.key,
+    account: account.info.LSK,
+    priorityOptions,
+    transaction: {
+      moduleAssetId: MODULE_ASSETS_NAME_ID_MAP.reclaimLSK,
+      nonce: account.info.LSK.sequence.nonce,
+      senderPublicKey: account.info.LSK.summary.publicKey,
+      amount: account.info.LSK.legacy.balance,
+    },
+  });
+
+  // eslint-disable-next-line max-statements
+  const onSubmit = () => {
+    balanceReclaimed({ fee: minFee });
+  };
+
+  useEffect(() => {
+    // success
+    if (!isEmpty(signedTransaction)) {
+      nextStep({
+        transactionInfo: signedTransaction,
+        balance: account.info.LSK.legacy?.balance,
+      });
+    }
+  }, [signedTransaction]);
+
+  useEffect(() => {
+    // error
+    if (txSignatureError) {
+      nextStep({
+        transactionError: txSignatureError,
+        balance: account.info.LSK.legacy?.balance,
+      });
+    }
+  }, [txSignatureError]);
+
+  return (
+    <TransactionSummary
+      title={t('Transaction summary')}
+      t={t}
+      account={account.info.LSK}
+      confirmButton={{
+        label: t('Continue'),
+        onClick: onSubmit,
+      }}
+      showCancelButton={false}
+      fee={minFee.value}
+      token={tokenMap.LSK.key}
+      classNames={styles.summaryContainer}
+    >
+      <TransactionInfo account={account} moduleAssetId={MODULE_ASSETS_NAME_ID_MAP.reclaimLSK} />
+    </TransactionSummary>
+  );
+};
+
+export default Summary;
