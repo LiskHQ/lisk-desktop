d5ad21b79299328d7c6dfe0cde9bd8eb6a14a8ec
Ali Haghighatkhah <ali@lightcurve.io>
2021-11-17 16:18:23 +0100

    Use redux component for cleaner code

--- src/components/screens/signMultiSignTransaction/reviewSign/index.js
+++ src/components/screens/signMultiSignTransaction/reviewSign/index.js
@@ -1,28 +1,11 @@
-import React from 'react';
 import { compose } from 'redux';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { getActiveTokenAccount } from '@utils/account';
+import { multisigTransactionSigned } from '@actions';
 import { getAccount } from '@api/account';
 import withData from '@utils/withData';
 import { withRouter } from 'react-router';
-import ReviewSignComp from './reviewSign';
-
-const ReviewSign = (props) => {
-  const { t } = useTranslation();
-  const network = useSelector(state => state.network);
-  const account = useSelector(getActiveTokenAccount);
-
-  return (
-    <ReviewSignComp
-      t={t}
-      {...props}
-      networkIdentifier={network.networks.LSK.networkIdentifier}
-      account={account}
-      network={network}
-    />
-  );
-};
+import ReviewSign from './reviewSign';
 
 const apis = {
   senderAccount: {
@@ -37,7 +20,23 @@ const apis = {
   },
 };
 
+const mapStateToProps = state => ({
+  account: {
+    ...state.account.info.LSK,
+    passphrase: state.passphrase,
+    hwInfo: state.hwInfo,
+  },
+  network: state.network,
+  networkIdentifier: state.network.networks.LSK.networkIdentifier,
+});
+
+const dispatchToProps = {
+  multisigTransactionSigned,
+};
+
 export default compose(
+  connect(mapStateToProps, dispatchToProps),
   withRouter,
   withData(apis),
+  useTranslation(),
 )(ReviewSign);
