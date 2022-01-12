2509cd82c95589a1d748a611d645e07d388ff411
iris salcedo <iris.salcedo@lightcurve.io>
2022-01-12 13:58:44 +0100

    Remove fake test

--- app/src/modules/autoUpdater.test.js
+++ app/src/modules/autoUpdater.test.js
@@ -49,13 +49,6 @@ describe('autoUpdater', () => {
 
   beforeEach(() => {
     const quitAndInstall = spy();
-    callbacks = {
-      clickDialogButton: (buttonIndex) => {
-        if (buttonIndex === 0) {
-          quitAndInstall();
-        }
-      },
-    };
     params = {
       autoUpdater: {
         checkForUpdates: spy(),
@@ -136,22 +129,6 @@ describe('autoUpdater', () => {
     dialogSpy.resetHistory();
     callbacks['update-not-available']({ version });
     expect(dialogSpy).to.have.not.been.calledWith();
-  });
-
-  it('should install update once downloaded and "Restart now" button pressed', () => {
-    autoUpdater(params);
-    callbacks['update-downloaded']({ version });
-    callbacks.clickDialogButton(0);
-
-    expect(params.autoUpdater.quitAndInstall).to.have.been.calledWithExactly();
-  });
-
-  it('should not install update when "Later" was pressed', () => {
-    autoUpdater(params);
-    callbacks['update-downloaded']({ releaseNotes, version });
-    callbacks.clickDialogButton(1);
-
-    expect(params.autoUpdater.quitAndInstall).to.not.have.been.calledWith();
   });
 
   it('should Send information to renderer when update is available', () => {
