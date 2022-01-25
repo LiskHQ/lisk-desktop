1e08cb0db96a362793f4e636d5d656e5bacce4dd
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-01-25 10:48:42 +0100

    Added e2e tests for transaction filters

--- test/cypress/features/transaction/transaction.js
+++ test/cypress/features/transaction/transaction.js
@@ -6,6 +6,6 @@ Given(/^Given I am on Transactions page$/, function () {
 	cy.visit('/transactions');
 })
 
-Then(/^I should see more than ([^\s]+) transactions$/, function (trnxNumber) {
+Then(/^I should see more than ([^\s]+) transactions in table$/, function (trnxNumber) {
   cy.get(ss.transactionRow).should('have.length.greaterThan', Number(trnxNumber));
 });
