75bb174f007ca4761f03d92b07767c50ff743f64
iris salcedo <iris.salcedo@lightcurve.io>
2022-02-02 10:41:15 +0100

    Fix newReleaseDialog screen

--- src/components/shared/newReleaseDialog/index.js
+++ src/components/shared/newReleaseDialog/index.js
@@ -1,11 +1,43 @@
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { withTranslation } from 'react-i18next';
-import NewReleaseDialog from './newReleaseDialog';
+import Dialog from '@toolbox/dialog/dialog';
+import FlashMessageHolder from '@toolbox/flashMessage/holder';
+import { PrimaryButton, SecondaryButton } from '@toolbox/buttons';
+import styles from './newReleaseDialog.css';
 
-const mapStateToProps = state => ({
-  ipc: state.appUpdates.ipc,
-  releaseNotes: state.appUpdates.releaseNotes,
-  version: state.appUpdates.version,
-});
+const NewReleaseDialog = ({ t }) => {
+  const { version, releaseNotes, ipc } = useSelector(state => state.appUpdates);
+  const handleClick = () => {
+    FlashMessageHolder.deleteMessage('NewRelease');
+    ipc.send('update:started');
+  };
 
-export default connect(mapStateToProps)(withTranslation()(NewReleaseDialog));
+  // return !!ipc && (
+  return (
+    <Dialog hasClose>
+      <div className={styles.wrapper}>
+        <Dialog.Title>
+          {t('Lisk {{version}} is here!', { version })}
+        </Dialog.Title>
+        <Dialog.Description>
+          <p>{t('Would you like to download it now?')}</p>
+        </Dialog.Description>
+        <h3>{t('Release notes')}</h3>
+        <div className={styles.releaseNotes}>
+          {releaseNotes}
+        </div>
+      </div>
+      <Dialog.Options align="center">
+        <SecondaryButton onClick={() => FlashMessageHolder.deleteMessage('NewRelease')}>
+          {t('Remind me later')}
+        </SecondaryButton>
+        <PrimaryButton onClick={handleClick}>
+          {t('Install update')}
+        </PrimaryButton>
+      </Dialog.Options>
+    </Dialog>
+  );
+};
+
+export default withTranslation()(NewReleaseDialog);
