8495012c286bb98d8825e4c6f6bbac74d724a55b
iris salcedo <iris.salcedo@lightcurve.io>
2022-02-02 17:37:00 +0100

    Update unit tests

--- src/hooks/useIpc.test.js
+++ src/hooks/useIpc.test.js
@@ -21,9 +21,13 @@ describe('useIpc', () => {
     on: jest.fn((event, callback) => { callbacks[event] = callback; }),
     send: jest.fn(),
   };
+  const version = '1.20.1';
+  const releaseNotes = '<h4>dummy text</h4><h3>Fixed bugs</h3>';
 
   beforeEach(() => {
     ipc.send.mockClear();
+    mockHistory.push.mockClear();
+    mockDispatch.mockClear();
     window.ipc = ipc;
   });
 
@@ -41,12 +45,11 @@ describe('useIpc', () => {
     expect(result.current).toBe(undefined);
   });
 
-  it('Should call FlashMessageHolder.addMessage when ipc receives update:available', () => {
+  it('Should call render FlashMessage correctly and dispatch appUpdateAvailable when ipc receives update:available', () => {
     renderHook(() => useIpc(mockHistory));
+    const spy = jest.spyOn(FlashMessageHolder, 'addMessage');
     const wrapper = mountWithRouter(FlashMessageHolder);
     const dialogWrapper = mountWithRouter(DialogHolder);
-    const version = '1.20.1';
-    const releaseNotes = '<h4>dummy text</h4><h3>Fixed bugs</h3>';
 
     expect(wrapper).toBeEmptyRender();
     expect(dialogWrapper).toBeEmptyRender();
@@ -54,25 +57,36 @@ describe('useIpc', () => {
 
     callbacks['update:available']({}, { version, releaseNotes });
     wrapper.update();
-    expect(wrapper).toIncludeText('dummy text');
+
+    expect(spy).toHaveBeenCalledWith(expect.anything(), 'NewRelease');
+    expect(wrapper).toIncludeText(`Lisk ${version} is out. dummy text`);
+    expect(wrapper.find('.read-more').at(0)).toHaveText('Read more');
+    expect(wrapper.find('.update-now').at(0)).toHaveText('Update now');
     expect(mockDispatch).toHaveBeenCalledWith(appUpdateAvailable({
       releaseNotes,
       version,
       remindMeLater: expect.any(Function),
       updateNow: expect.any(Function),
     }));
+  });
+
+  it('Should call to open modal when readMore is clicked', () => {
+    renderHook(() => useIpc(mockHistory));
+    const wrapper = mountWithRouter(FlashMessageHolder);
+
+    callbacks['update:available']({}, { version, releaseNotes });
+    wrapper.update();
 
     wrapper.find('button.read-more').simulate('click');
     expect(mockHistory.push).toBeCalledWith('?modal=newRelease');
   });
 
-  it('Should initiate the update process if clicked on updateNow', () => {
+  it('Should call FlashMessageHolder.deleteMessage and send update:started through ipc on updateNow click', () => {
     renderHook(() => useIpc(mockHistory));
     const spy = jest.spyOn(FlashMessageHolder, 'deleteMessage');
     const wrapper = mountWithRouter(FlashMessageHolder);
     const dialogWrapper = mountWithRouter(DialogHolder);
-    const version = '1.20.1';
-    const releaseNotes = '<h4>dummy text</h4><h3>Fixed bugs</h3>';
+
     callbacks['update:available']({}, { version, releaseNotes });
     wrapper.update();
     wrapper.find('button.update-now').simulate('click');
@@ -81,5 +95,18 @@ describe('useIpc', () => {
 
     expect(ipc.send).toHaveBeenCalledWith('update:started');
     expect(spy).toHaveBeenCalledWith('NewRelease');
+  });
+
+  it('Should call FlashMessageHolder.deleteMessage and remove modal when remindMeLater is triggered', () => {
+    renderHook(() => useIpc(mockHistory));
+    const spy = jest.spyOn(FlashMessageHolder, 'deleteMessage');
+    const wrapper = mountWithRouter(FlashMessageHolder);
+
+    callbacks['update:available']({}, { version, releaseNotes });
+    wrapper.update();
+    mockDispatch.mock.calls[0][0].data.remindMeLater();
+
+    expect(spy).toHaveBeenCalledWith('NewRelease');
+    expect(mockHistory.push).toBeCalledWith('');
   });
 });
