3d951fe1b42de77c6677b8129786e013b8209897
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-02-04 17:01:10 +0100

    Fixed transaction details for modal test

--- test/cypress/features/transaction/transaction.js
+++ test/cypress/features/transaction/transaction.js
@@ -9,7 +9,7 @@ When(/^I select ([^\s]+) in ([^\s]+) field$/, function (value, field) {
 
 Then(/^I should be on transaction details modal$/, function () {
   cy.location().should((location) => {
-    const hasAddress = /\?modal=transactionDetails&transactionId=a1c5521f466ae5476d3908cc8d562444d45adf4ac3af57e77f1f9359999ab9ca/.test(location.href);
+    const hasAddress = /\?modal=transactionDetails&transactionId=f3f4755b31eae903c8cf1e35f123e907fd8ed8e2b9feeae39bedd5c495326d62/.test(location.href);
     expect(hasAddress).true;
   });
 });
