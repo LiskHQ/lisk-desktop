808a6059bb0755e8f4b906fdd4e1c53c9ec63596
eniolam1000752 <eniolam1000752@gmail.com>
2022-02-13 11:14:30 +0100

    implemented unit test

---
+++ test/fixtures/transactions_data.js
@@ -0,0 +1,175 @@
+export default [
+  {
+    id: '6dc372dfcbfd2b1782d4d1964a35908f71e3878c69b5bbe28e5c69973671c8ee',
+    moduleAssetId: '2:0',
+    moduleAssetName: 'token:transfer',
+    fee: '143000',
+    height: 414,
+    nonce: '134',
+    block: {
+      id: '408c63b500eac768140ef7a0dacd1638726c783c1cefa52a42189ef0fa46a1c1',
+      height: 414,
+      timestamp: 1629456796,
+    },
+    sender: {
+      address: 'lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt',
+      publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a',
+    },
+    signatures: [
+      '6ab80c376f207f1a1b11d4cef3fbae00fb301d67d15c306ddafc62aaf6c9e5e5ae5c76d940f2ac6eab5d513f6a7f0da2d13b1f6674c367dc047237416bfe8a07',
+    ],
+    asset: {
+      amount: '2000000000',
+      data: '',
+      recipient: {
+        address: 'lsk769d7rrnxtvme2egazc8uc8d77uzwnmgzqyp53',
+        publicKey: 'd517f9d9ac10a61b57d1959b88f8b5c6e8824d27a5349ec7ece44c4a027c41cb',
+      },
+    },
+    isPending: false,
+  },
+  {
+    id: '827028a55e5a28f96437bf87531c61255b737ceb3407989dfd41985ed6d5453b',
+    moduleAssetId: '5:0',
+    moduleAssetName: 'dpos:registerDelegate',
+    fee: '1000268000',
+    height: 329,
+    nonce: '2',
+    block: {
+      id: '886c179abf3c5c3c31a9216b31efcd28b683dfff646224e2d8214f40e1fddd0d',
+      height: 329,
+      timestamp: 1629113716,
+    },
+    sender: {
+      address: 'lskatchyvyh9y3tz3mmpu9z3kptw56c6qcw4k7fks',
+      publicKey: '2f3dfb37326c6c42af28d04e6b6a76865430ade3d26969f1b86e3987b500bbaf',
+      username: 'multisig_delegateacc',
+    },
+    signatures: [
+      '9105041cb8dc0d20def295cfbf1fa10e242e121ca82de5d63d5ae457e1a6a81c8ae92d2ca445b369bc73aec9838f10f779b0f5788220713a5e7f0bfb103ace0c',
+      'c3ea76f6aeefe0214c021ec82ae5e5cfaa762f2ed7d5110c8d723f62ef73e2ad862768ed6bab13a53d0126b895085075327019a207d7edba0ad692a1ae74b909',
+      '9b568bbe264dfb775f6d55a58592c077daa30f6d6abb1e914def734119aa709fe6c08ffe2fdd44375645d377e754dce17c755e2940751a11e786e8198e01ad06',
+    ],
+    asset: {
+      username: 'multisig_delegateacc',
+    },
+    isPending: false,
+  },
+  {
+    id: 'ecf56cf27f113893f325f147813bda0f8d5077ea59e8871f668289ccb8f40fa6',
+    moduleAssetId: '4:0',
+    moduleAssetName: 'keys:registerMultisignatureGroup',
+    fee: '414000',
+    height: 315,
+    nonce: '0',
+    block: {
+      id: '36a54aa380c39e7a28063d08133c7e2fd43907793e148b5cee885a88059506ba',
+      height: 315,
+      timestamp: 1629113576,
+    },
+    sender: {
+      address: 'lskatchyvyh9y3tz3mmpu9z3kptw56c6qcw4k7fks',
+      publicKey: '2f3dfb37326c6c42af28d04e6b6a76865430ade3d26969f1b86e3987b500bbaf',
+      username: 'multisig_delegateacc',
+    },
+    signatures: [
+      'b7ec0e305d7f8f8ca533eda1576f0c39e6ab256979b2e10a1ca2dd1db89228b4bac0dd63a469852ceafea3b8ed8c38e327aab420694c35f1324f19fc37db0702',
+      'b7ec0e305d7f8f8ca533eda1576f0c39e6ab256979b2e10a1ca2dd1db89228b4bac0dd63a469852ceafea3b8ed8c38e327aab420694c35f1324f19fc37db0702',
+      'bbfca9940762ac1df1c4208249cbe1a40676e8babde9e11fb829044d0c8730e9b76317a4ea824e470d5a075b67bbb25e2d90cbc0aad0ab3fbe74e81148a72d02',
+      'c14f37f0f0dd8b40a973f0f809623c9ccdf279de76cfb4e7e129bd58fdd4230f1e540467918ea8c0cd3f5a6e18f123666b3d97a6e89521353350acde4e49fe02',
+    ],
+    asset: {
+      numberOfSignatures: 3,
+      mandatoryKeys: [
+        '2f3dfb37326c6c42af28d04e6b6a76865430ade3d26969f1b86e3987b500bbaf',
+        '520bd47952f98ba475bba6aafa926f730f8ddbcb4c362a826be1ac6618be3d3e',
+      ],
+      optionalKeys: [
+        'd1e267fd2cfdba3ee1f657c75a88f3d41aae3ca98d82286a21e4be12aaa6a388',
+      ],
+    },
+    isPending: false,
+  },
+  {
+    id: 'a1c5521f466ae5476d3908cc8d562444d45adf4ac3af57e77f1f9359999ab9ca',
+    moduleAssetId: '5:1',
+    moduleAssetName: 'dpos:voteDelegate',
+    fee: '144000',
+    height: 454,
+    nonce: '137',
+    block: {
+      id: '4491496ec38cbb7d91da81411d896abb100de030f233448ab878429a01ac005b',
+      height: 454,
+      timestamp: 1641422866,
+    },
+    sender: {
+      address: 'lskdxc4ta5j43jp9ro3f8zqbxta9fn6jwzjucw7yt',
+      publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a',
+    },
+    signatures: [
+      '4edc354c9d40d1e9280db9744e584a7058ac6a779a73a647e3388cbf05e6351c592c111390d209ae6e87746167a3f58779192065c9e6650c237bf6466d1bd503',
+    ],
+    asset: {
+      votes: [
+        {
+          delegateAddress: 'lsk9gf9y56k8emm4byye44c92av2fzd65om95kxp8',
+          amount: '-30000000000',
+        },
+      ],
+    },
+    isPending: false,
+  },
+  {
+    id: '1f1aa24b49859d825cc5ace36b5554a1ee65556852b0c1f5c043853c697a2651',
+    moduleAssetId: '5:3',
+    moduleAssetName: 'dpos:reportDelegateMisbehavior',
+    fee: '10000000',
+    height: 15334905,
+    nonce: '6',
+    block: {
+      id: '70762049c0270c28a92b22193da91c93049981722f14697163ce650ccfced65d',
+      height: 15334905,
+      timestamp: 1641018190,
+    },
+    sender: {
+      address: 'lskhtpeyg2944gcjwubrc7vwp52qoshtu4fs9wmfn',
+      publicKey: 'd1d951c46376e108bab5e7a9f1dce725bb33f9823442a4a34841b6774bbb5b2f',
+    },
+    signatures: [
+      '6fe09dfdb98bbcc6da9043a4c6153c7276c99c23c29f759b84f1d8c3e71d84ca1487ac0aabe78edb07e424ac1caff2ccf0e1f18c47b7d21797dbe9b44344690c',
+    ],
+    asset: {
+      header1: {
+        version: 2,
+        timestamp: 1641017150,
+        height: 15329877,
+        previousBlockID: 'e1cf8688c454ac350c941c47d54466d22c226c1d028f58d821afc2761cac5330',
+        transactionRoot: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+        generatorPublicKey: '8100bb532f85183cbf6e99f9fcc5893f45536fbc9dbf7a80cfe1c1e165203ec7',
+        reward: '25000000',
+        asset: {
+          maxHeightPreviouslyForged: 15329877,
+          maxHeightPrevoted: 15329809,
+          seedReveal: '9df2fee500047174fe19c4454d32c0e2',
+        },
+        signature: 'fcab8714c33e635e8c5827c054383488c4873472a0b73657d7d8af39f4291324850b76897082f3e477ffc01975e019431945cd1bc079274e10ed9ada5c7a2405',
+      },
+      header2: {
+        version: 2,
+        timestamp: 1641018180,
+        height: 15329877,
+        previousBlockID: 'e1cf8688c454ac350c941c47d54466d22c226c1d028f58d821afc2761cac5330',
+        transactionRoot: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+        generatorPublicKey: '8100bb532f85183cbf6e99f9fcc5893f45536fbc9dbf7a80cfe1c1e165203ec7',
+        reward: '25000000',
+        asset: {
+          maxHeightPreviouslyForged: 15329877,
+          maxHeightPrevoted: 15329809,
+          seedReveal: '9df2fee500047174fe19c4454d32c0e2',
+        },
+        signature: 'b749358492ce7ef8ae351b5d3c63fb798af049569a8e410f621be113d80b190a4204e87cfbc29f5188713731f50220622674474382cf384cfe6b68d6dc14c90c',
+      },
+    },
+    isPending: false,
+  },
+];
