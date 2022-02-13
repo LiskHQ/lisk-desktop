e977bf39e5d57e0d8bcf0f401552cf949fe41e3d
eniolam1000752 <eniolam1000752@gmail.com>
2022-02-13 11:14:30 +0100

    implemented unit test

--- src/components/shared/transactionsTable/index.js
+++ src/components/shared/transactionsTable/index.js
@@ -14,6 +14,7 @@ import styles from './transactionsTable.css';
 import TransactionRow from './transactionRow';
 import header from './tableHeader';
 
+const blackListTypes = ['4:0', '5:0', '5:1', '5:3'];
 const TransactionsTable = ({
   title,
   transactions,
@@ -31,7 +32,6 @@ const TransactionsTable = ({
 }) => {
   const [innerFields, setInnerFields] = useState(fields);
 
-  const blackListTypes = ['4:0', '5:0', '5:1', '5:3'];
   const currentBlockHeight = useSelector(selectCurrentBlockHeight);
   const handleLoadMore = () => {
     // filter the blanks out
