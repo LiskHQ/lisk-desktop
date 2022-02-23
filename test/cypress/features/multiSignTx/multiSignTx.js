d1daf5507405e6260c124813e7a50649dc1d9d7d
iris salcedo <iris.salcedo@lightcurve.io>
2022-02-23 11:49:22 +0100

    Assert file correctly

--- test/cypress/features/multiSignTx/multiSignTx.js
+++ test/cypress/features/multiSignTx/multiSignTx.js
@@ -92,5 +92,7 @@ Then(/^I should have the transaction ([^\s]+) in the clipboard$/, function (tx) 
 });
 
 Then(/^([^\s]+) should have been downloaded correctly$/, function (tx) {
-  cy.readFile(`cypress/downloads/tx-${multiSignatureTxs[tx].id}.json`).should('eq', multiSignatureTxs[tx]);
+  cy.readFile(`cypress/downloads/tx-${JSON.parse(multiSignatureTxs[tx]).id}.json`).then(json => {
+    expect(JSON.stringify(json)).to.eq(multiSignatureTxs[tx]);
+  });
 });
