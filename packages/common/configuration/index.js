72e10c4c0061b37f8e19da017c8d3ccb0714c4fa
iris salcedo <iris.salcedo@lightcurve.io>
2022-03-29 15:59:39 +0200

    Move keyCodes, loginTypes and monitor

--- packages/common/configuration/index.js
+++ packages/common/configuration/index.js
@@ -1,6 +1,3 @@
-export { default as loginTypes } from './loginTypes';
-export { default as keyCodes } from './keyCodes';
-export { DEFAULT_LIMIT } from './monitor';
 export { default as piwikOptions } from './piwik';
 export { default as actionTypes } from './actionTypes';
 export { chartStyles, colorPalette, colorPaletteDark } from './chart';
--- packages/views/configuration/index.js
+++ packages/views/configuration/index.js
@@ -1,1 +1,4 @@
+export { default as loginTypes } from './loginTypes';
+export { default as keyCodes } from './keyCodes';
+export { DEFAULT_LIMIT } from './monitor';
 export * from './toasts';
