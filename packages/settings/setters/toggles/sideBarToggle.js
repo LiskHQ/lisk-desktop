df119a43d149e5f88855f5ef6122706190646dc1
Gad Jacobs <gadexofficial@gmail.com>
2022-04-06 13:56:34 +0100

    Fix indentation issues

--- packages/settings/setters/toggles/sideBarToggle.js
+++ packages/settings/setters/toggles/sideBarToggle.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import settingConstants from "@settings/configuration/settingConstants";
-import Toggle from "./toggle";
+import React from 'react';
+import settingConstants from '@settings/configuration/settingConstants';
+import Toggle from './toggle';
 
-function SideBarToggle({ t }){
-    return ( 
-        <Toggle
-            setting={settingConstants.keys.sideBarExpanded}
-            icons={['toggleSidebarActive', 'toggleSidebar']}
-            tips={[t('Collapse sidebar'), t('Expand sidebar')]}
-        />
-    )
+function SideBarToggle({ t }) {
+  return (
+    <Toggle
+      setting={settingConstants.keys.sideBarExpanded}
+      icons={['toggleSidebarActive', 'toggleSidebar']}
+      tips={[t('Collapse sidebar'), t('Expand sidebar')]}
+    />
+  );
 }
 
-export default SideBarToggle
+export default SideBarToggle;
