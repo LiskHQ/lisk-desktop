99831bf6668f7a5a2cf4744fb7e507814e873884
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-04-06 12:01:57 +0100

    Refactored component to functional component and updated test

--- packages/views/basics/autoSuggest/index.js
+++ packages/views/basics/autoSuggest/index.js
@@ -1,195 +1,180 @@
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 import { keyCodes } from '@views/configuration';
 import { Input } from '../inputs';
 import styles from './autoSuggest.css';
 
-class AutoSuggest extends React.Component {
-  constructor(props) {
-    super(props);
+const AutoSuggest = ({
+  items,
+  selectedItem,
+  matchProps,
+  onSelectItem,
+  onChange,
+  onChangeDelayed,
+  placeholder,
+  renderItem,
+  renderIcon,
+  className,
+}) => {
+  const [dropdownIndex, setDropdownIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFocused, setIsFocused] = useState(true);
+  let loaderTimeout = null;
+  const listContainerRef = useRef(null);
 
-    this.state = {
-      dropdownIndex: 0,
-      isLoading: false,
-      isFocused: true,
-    };
+  const getFilterList = () => {
+    if (selectedItem.value === "") return items;
 
-    this.loaderTimeout = null;
-    this.listContainerRef = null;
-    this.input = null;
-
-    this.bindAll();
-  }
-
-  bindAll() {
-    this.onHandleKeyPress = this.onHandleKeyPress.bind(this);
-    this.getFilterList = this.getFilterList.bind(this);
-    this.onKeyPressDownOrUp = this.onKeyPressDownOrUp.bind(this);
-    this.onKeyPressEnter = this.onKeyPressEnter.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.onSelectItem = this.onSelectItem.bind(this);
-    this.resetListIndex = this.resetListIndex.bind(this);
-    this.handleUpdateIndex = this.handleUpdateIndex.bind(this);
-  }
-
-  getFilterList() {
-    const { items, selectedItem, matchProps } = this.props;
-
-    if (selectedItem.value === '') return items;
-
-    return items.filter(item => (
-      matchProps.find(prop => (
+    return items.filter((item) =>
+      matchProps.find((prop) =>
         item[prop].toLowerCase().includes(selectedItem.value.toLowerCase())
-      ))
-    ));
-  }
+      )
+    );
+  };
 
-  resetListIndex() {
-    this.setState({ dropdownIndex: 0 });
-  }
+  const resetListIndex = () => {
+    setDropdownIndex(0);
+  };
 
-  onSelectItem(item) {
-    this.setState({ isLoading: false });
-    this.resetListIndex();
-    this.props.onSelectItem(item);
-  }
+  const onItemSelect = (item) => {
+    setIsLoading(false);
+    resetListIndex();
+    onSelectItem(item);
+  };
 
-  onKeyPressDownOrUp(action) {
+  const onKeyPressDownOrUp = (action) => {
     const rowHeight = 44;
-    const filteredItemsLength = this.getFilterList().length;
+    const filteredItemsLength = getFilterList().length;
 
-    this.setState(({ dropdownIndex }) => {
+    setDropdownIndex((dropdownIndex) => {
       // istanbul ignore else
-      if (action === 'down' && dropdownIndex < filteredItemsLength - 1) {
+      if (action === "down" && dropdownIndex < filteredItemsLength - 1) {
         if (dropdownIndex + 1 >= 4) {
           // eslint-disable-next-line operator-assignment
-          this.listContainerRef.scrollTop = this.listContainerRef.scrollTop + rowHeight;
+          listContainerRef.current.scrollTop = listContainerRef.current.scrollTop + rowHeight;
         }
         dropdownIndex += 1;
       }
 
       // istanbul ignore else
-      if (action === 'up' && dropdownIndex > 0) {
-        this.listContainerRef.scrollTop = this.listContainerRef.scrollTop > 0
-        && (dropdownIndex - 1) * rowHeight < this.listContainerRef.scrollTop
-          ? this.listContainerRef.scrollTop - rowHeight
-          : this.listContainerRef.scrollTop;
+      if (action === "up" && dropdownIndex > 0) {
+        listContainerRef.current.scrollTop =
+          listContainerRef.current.scrollTop > 0 &&
+          (dropdownIndex - 1) * rowHeight < listContainerRef.current.scrollTop
+            ? listContainerRef.current.scrollTop - rowHeight
+            : listContainerRef.current.scrollTop;
         dropdownIndex -= 1;
       }
-      return { dropdownIndex };
+      return dropdownIndex;
     });
-  }
+  };
 
-  handleUpdateIndex(dropdownIndex) {
-    this.setState({ dropdownIndex });
-  }
+  const handleUpdateIndex = (dropdownIndex) => {
+    setDropdownIndex(dropdownIndex);
+  };
 
-  onKeyPressEnter() {
-    const { dropdownIndex } = this.state;
-    const item = this.getFilterList()[dropdownIndex];
-    this.onSelectItem(item);
-  }
+  const onKeyPressEnter = () => {
+    const item = getFilterList()[dropdownIndex];
+    onItemSelect(item);
+  };
 
-  onHandleKeyPress(e) {
+  const onHandleKeyPress = (e) => {
     // istanbul ignore else
-    if (this.getFilterList().length) {
+    if (getFilterList().length) {
       switch (e.keyCode) {
         case keyCodes.arrowDown:
-          this.onKeyPressDownOrUp('down');
+          onKeyPressDownOrUp("down");
           break;
         case keyCodes.arrowUp:
-          this.onKeyPressDownOrUp('up');
+          onKeyPressDownOrUp("up");
           break;
         case keyCodes.enter:
-          this.onKeyPressEnter();
+          onKeyPressEnter();
           break;
         // istanbul ignore next
         default:
           break;
       }
     }
-  }
+  };
 
-  onChange(e) {
-    clearTimeout(this.loaderTimeout);
-    this.setState({ isLoading: true });
-    this.loaderTimeout = setTimeout(() => {
+  const onChangeEv = (e) => {
+    clearTimeout(loaderTimeout);
+    setIsLoading(true);
+    loaderTimeout = setTimeout(() => {
       // istanbul ignore else
-      if (this.getFilterList().length >= 0) this.setState({ isLoading: false });
-      this.props.onChangeDelayed();
+      if (getFilterList().length >= 0) setIsLoading(false);
+      onChangeDelayed();
     }, 300);
 
-    if (e?.target?.value === '') this.resetListIndex();
-    this.props.onChange(e);
-  }
+    if (e?.target?.value === "") resetListIndex();
+    onChange(e);
+  };
 
-  onBlur = () => {
-    this.setState({ isFocused: false });
-  }
+  const onBlur = () => {
+    setIsFocused(false);
+  };
 
-  onFocus = () => {
-    this.setState({ isFocused: true });
-  }
+  const onFocus = () => {
+    setIsFocused(true);
+  };
 
-  isOnError = () => {
-    const { isFocused } = this.state;
-    const { selectedItem, items } = this.props;
-    const bookmarksList = this.getFilterList();
+  const isOnError = () => {
+    const bookmarksList = getFilterList();
 
-    if ((!items.length && selectedItem.error) || (!isFocused && selectedItem.error)) return true;
+    if (
+      (!items.length && selectedItem.error) ||
+      (!isFocused && selectedItem.error)
+    )
+      return true;
     if (items.length && !bookmarksList.length && selectedItem.error) {
       return true;
     }
     return false;
-  }
+  };
 
-  render() {
-    const {
-      selectedItem,
-      placeholder,
-      renderItem,
-      renderIcon,
-      className,
-    } = this.props;
-    const { dropdownIndex, isLoading } = this.state;
-
-    return (
-      <>
-        <span className={`${styles.inputWrapper} ${className}`}>
-          <Input
-            autoComplete="off"
-            className={`${styles.input} ${this.isOnError() ? 'error' : ''} ${className} bookmark`}
-            name={className}
-            value={selectedItem.selected ? selectedItem.title : selectedItem.value}
-            placeholder={placeholder}
-            onKeyDown={this.onHandleKeyPress}
-            onChange={this.onChange}
-            feedback={selectedItem.feedback}
-            isLoading={isLoading && selectedItem.value}
-            status={this.isOnError() ? 'error' : 'ok'}
-            icon={renderIcon(selectedItem)}
-            onBlur={this.onBlur}
-            onFocus={this.onFocus}
-          />
-          <ul className={`${styles.suggestionList} bookmark-list`} ref={(node) => { this.listContainerRef = node; }}>
-            { this.getFilterList()
-              .map((item, index) => (
-                <li
-                  key={index}
-                  onMouseEnter={() => this.handleUpdateIndex(index)}
-                  onClick={() => this.onSelectItem(item)}
-                  onKeyPress={this.onHandleKeyPress}
-                  className={`${dropdownIndex === index ? styles.active : ''}`}
-                >
-                  {renderItem(item)}
-                </li>
-              )) }
-          </ul>
-        </span>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <span className={`${styles.inputWrapper} ${className}`}>
+        <Input
+          autoComplete="off"
+          className={`${styles.input} ${
+            isOnError() ? "error" : ""
+          } ${className} bookmark`}
+          name={className}
+          value={
+            selectedItem.selected ? selectedItem.title : selectedItem.value
+          }
+          placeholder={placeholder}
+          onKeyDown={onHandleKeyPress}
+          onChange={onChangeEv}
+          feedback={selectedItem.feedback}
+          isLoading={isLoading && selectedItem.value}
+          status={isOnError() ? "error" : "ok"}
+          icon={renderIcon(selectedItem)}
+          onBlur={onBlur}
+          onFocus={onFocus}
+        />
+        <ul
+          className={`${styles.suggestionList} bookmark-list`}
+          ref={listContainerRef}
+        >
+          {getFilterList().map((item, index) => (
+            <li
+              key={index}
+              onMouseEnter={() => handleUpdateIndex(index)}
+              onClick={() => onItemSelect(item)}
+              onKeyPress={onHandleKeyPress}
+              className={`${dropdownIndex === index ? styles.active : ""}`}
+            >
+              {renderItem(item)}
+            </li>
+          ))}
+        </ul>
+      </span>
+    </>
+  );
+};
 
 AutoSuggest.propTypes = {
   renderItem: PropTypes.func,
