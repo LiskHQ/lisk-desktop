450d95aace2df855375afd0edd78a8ab8dcbc201
eniolam1000752 <eniolam1000752@gmail.com>
2022-04-11 15:12:17 +0100

    changed test/cypress/screenshots/ -> tests/cypress/screenshots/

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -10,10 +10,16 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+const webpack = require('@cypress/webpack-preprocessor')
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = (on) => {
+  const options = {
+    webpackOptions: require('../../../setup/config/webpack.config.js'),
+    watchOptions: {},
+  };
+
+  on('file:preprocessor', webpack(options));
   on('file:preprocessor', cucumber());
 
   on('task', {
