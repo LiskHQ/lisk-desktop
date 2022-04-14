50bfcbf841e9b73fa4c14bbedeb79094946e0d24
eniolam1000752 <eniolam1000752@gmail.com>
2022-04-14 13:04:30 +0100

    fixed pr comemnts on adding function signature

--- packages/wallet/manager/signMessage.js
+++ packages/wallet/manager/signMessage.js
@@ -1,6 +1,12 @@
 import { cryptography } from '@liskhq/lisk-client';
 import { signMessageByHW } from '@wallet/utilities/hwManager';// eslint-disable-line
 
+/**
+ * Signs a string with the users private key
+ * @param {string} message - string value to be signed
+ * @param {Object} account - Account of the user 
+ * @returns {string} result - A signed value of the message
+ */
 export const signUsingPrivateKey = ({ message, account }) => {
   const msgBytes = cryptography.digestMessage(message);
   const signedMessage = cryptography.signDataWithPrivateKey(msgBytes, Buffer.from(account.summary.privateKey, 'hex'));
@@ -12,6 +18,12 @@ export const signUsingPrivateKey = ({ message, account }) => {
   return result;
 };
 
+/**
+ * Signs a string using hardware wallet
+ * @param {string} message - string value to be signed
+ * @param {Object} account - Account of the user 
+ * @returns {string} result - A signed value of the message
+ */
 export const signUsingHW = async ({ message, account }) => {
   let signedMessage = await signMessageByHW({
     account,
