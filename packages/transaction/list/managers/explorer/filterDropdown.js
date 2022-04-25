d290373b585449746fa3be2adaf3571911cc15cd
Ali Haghighatkhah <ali@lightcurve.io>
2022-04-25 12:55:03 +0200

    Create explorer manager

---
+++ packages/transaction/list/managers/explorer/filterDropdown.js
@@ -0,0 +1,24 @@
+import { withTranslation } from 'react-i18next';
+import React from 'react';
+import FilterDropdownButton from '@shared/filterDropdownButton';
+
+const FilterDropdown = ({ t, filters, applyFilters }) => {
+  const fields = [
+    {
+      label: t('Date range'),
+      name: 'date',
+      type: 'date-range',
+    },
+    {
+      label: t('Amount range'),
+      name: 'amount',
+      type: 'number-range',
+    },
+  ];
+
+  const props = { fields, filters, applyFilters };
+
+  return <FilterDropdownButton {...props} />;
+};
+
+export default withTranslation()(FilterDropdown);
