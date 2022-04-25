8389bf13125abc053f153694bcbe78a998563914
Ali Haghighatkhah <ali@lightcurve.io>
2022-04-25 12:56:01 +0200

    Create recentTransactions manager

---
+++ packages/transaction/list/managers/recentTransactions/index.js
@@ -0,0 +1,22 @@
+// istanbul ignore file
+import { withTranslation } from 'react-i18next';
+import { getTransactions } from '@transaction/utilities/api';
+import withData from '@common/utilities/withData';
+import RecentTransaction from './recentTransactions';
+
+export default withData({
+  transactions: {
+    apiUtil: (network, { token, ...params }) => getTransactions({ network, params }, token),
+    getApiParams: (state) => {
+      const token = state.settings.token.active;
+      const address = state.account.info ? state.account.info[token].summary.address : '';
+      return {
+        token,
+        address,
+        network: state.network,
+      };
+    },
+    defaultData: [],
+    transformResponse: response => response.data.splice(0, 5),
+  },
+})(withTranslation()(RecentTransaction));
---
+++ packages/transaction/list/managers/recentTransactions/tableHeader.js
@@ -0,0 +1,14 @@
+import grid from 'flexboxgrid/dist/flexboxgrid.css';
+
+export default t => (
+  [
+    {
+      title: t('Transaction'),
+      classList: grid['col-xs-8'],
+    },
+    {
+      title: t('Amount'),
+      classList: grid['col-xs-4'],
+    },
+  ]
+);
