ae4b1b663a5ac9e4cd3eaba637a5ef2dd2c07421
Ali Haghighatkhah <ali@lightcurve.io>
2022-04-26 10:17:27 +0200

    Strip tx figure from address/title

--- packages/transaction/detail/info/transactionTypeFigure/index.js
+++ packages/transaction/detail/info/transactionTypeFigure/index.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { MODULE_ASSETS_NAME_ID_MAP, MODULE_ASSETS_MAP } from '@transaction/configuration/moduleAssets';
-import { tokenMap } from '@token/configuration/tokens';
-import { validateAddress } from '@common/utilities/validators';
-import AccountVisual from '@wallet/detail/info/accountVisual';
 import Icon from '@basics/icon';
+import TransactionAddress from '@wallet/detail/info/accountVisual/transactionAddress';
 import styles from './transactionTypeFigure.css';
 
 const TransactionTypeFigure = ({
-  moduleAssetId, address, avatarSize = 40, className = '', icon, iconOnly,
+  moduleAssetId, className = '', address,
 }) => {
-  const renderAvatar = () => {
-    if (validateAddress(tokenMap.LSK.key, address) === 0) {
-      return <AccountVisual address={address} size={avatarSize} />;
-    }
+  if (moduleAssetId === MODULE_ASSETS_NAME_ID_MAP.transfer) {
     return null;
-  };
-
+  }
   return (
     <div className={`${styles.wrapper} ${className} transaction-image`}>
-      { icon ? <Icon name={icon} className={styles.inOutIcon} /> : null }
-      {
-        moduleAssetId === MODULE_ASSETS_NAME_ID_MAP.transfer && !iconOnly
-          ? renderAvatar()
-          : (
-            <Icon
-              name={MODULE_ASSETS_MAP[moduleAssetId]?.icon ?? 'txDefault'}
-              className={styles.transactionIcon}
-            />
-          )
-      }
+      <Icon
+        name={MODULE_ASSETS_MAP[moduleAssetId]?.icon ?? 'txDefault'}
+        className={styles.transactionIcon}
+      />
+      <span>
+        <TransactionAddress
+          address={address}
+          bookmarks={{ LSK: [] }}
+          t={str => str}
+          token={'LSK'}
+          moduleAssetId={moduleAssetId}
+        />
+      </span>
     </div>
   );
 };
