90ad6dd802e2ee4b158ca7ba5a911f475cc962a1
Gad Jacobs <gadexofficial@gmail.com>
2022-04-28 06:03:44 +0100

    Remove duplicate util files

--- packages/block/utilities/api/index.js
+++
@@ -1,5 +0,0 @@
-export { getBlock } from './getBlock';
-export { getBlocks } from './getBlocks';
-export { getBlockProps } from './getBlockProps';
-export { blockSubscribe } from './blockSubscribe';
-export { blockUnsubscribe } from './blockUnsubscribe';
