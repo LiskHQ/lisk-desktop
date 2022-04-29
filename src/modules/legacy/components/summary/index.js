11453bd0990d6068094daaff1bc9f2a0c8d44fcc
Ali Haghighatkhah <ali@lightcurve.io>
2022-04-29 08:30:38 +0200

    Create the reclaim tx summary step component

---
+++ src/modules/legacy/components/summary/index.js
@@ -0,0 +1,21 @@
+// istanbul ignore file
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withTranslation } from 'react-i18next';
+import { balanceReclaimed } from '@wallet/store/action';
+
+import Summary from './summary';
+
+const mapStateToProps = state => ({
+  account: state.wallet,
+  network: state.network,
+});
+
+const dispatchToProps = {
+  balanceReclaimed,
+};
+
+export default compose(
+  connect(mapStateToProps, dispatchToProps),
+  withTranslation(),
+)(Summary);
