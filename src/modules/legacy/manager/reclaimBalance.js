fbdcebcf57450fee0c5709f1287d786d2bdcbeb5
Ali Haghighatkhah <ali@lightcurve.io>
2022-05-02 10:55:54 +0200

    Fix remaining lint issues

--- src/modules/legacy/manager/reclaimBalance.js
+++ src/modules/legacy/manager/reclaimBalance.js
@@ -3,9 +3,10 @@ import { withTranslation } from 'react-i18next';
 import MultiStep from 'src/modules/common/components/OldMultiStep';
 import Introduction from '../components/introduction';
 import Reclaim from '../components/reclaim';
+import styles from '../components/introduction/introduction.css';
 
 const ReclaimBalance = ({ t }) => (
-  <MultiStep>
+  <MultiStep className={styles.multiStep}>
     <Introduction t={t} />
     <Reclaim t={t} />
   </MultiStep>
