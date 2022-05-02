ad381e71feb4e13e2a59bafe173b1cf3efda0a6b
Masoud <masoud.s.geo@gmail.com>
2022-05-02 18:38:42 +0200

    refactor: move transaction file

--- src/modules/transaction/utils/index.js
+++ src/modules/transaction/utils/index.js
@@ -2,3 +2,4 @@ export { default as normalizeVotesForTx } from './normalizeVotesForTx';
 export * from './moduleAssets';
 export * from './transaction';
 export * from './transactionDetailsHelper';
+export * from './removeDuplicateTransactions';
---
+++ src/modules/transaction/utils/removeDuplicateTransactions.js
@@ -0,0 +1,5 @@
+export const removeDuplicateTransactions = (pendingTransactions, confirmedTransactions) =>
+  [...pendingTransactions, ...confirmedTransactions]
+    .filter((transactionA, index, self) => (
+      index === self.findIndex(transactionB => (transactionB.id === transactionA.id))
+    ));
