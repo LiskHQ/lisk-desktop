e140e18b24fa9577374847134e71d73c2852d10e
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-05-05 15:38:48 +0100

    Fixed failing icon test

--- src/theme/Icon/index.test.js
+++ src/theme/Icon/index.test.js
@@ -7,12 +7,12 @@ import Icon from './index';
 describe('Icon', () => {
   it('should render an image with given name andcustom property', () => {
     const wrapper = mount(<Icon name="user" customprop="customValue" />);
-    expect(wrapper.find('img').props().src).to.be.equal('test-file-stub');
+    expect(wrapper.find('img').props().src).to.deep.equal({});
     expect(wrapper.find('img').props().customprop).to.be.equal('customValue');
   });
 
   it('should render dark icons if available', () => {
     const wrapper = mount(<ThemeContext.Provider value="dark"><Icon name="fileOutline" /></ThemeContext.Provider>);
-    expect(wrapper.find('img').props().src).to.be.equal('test-file-stub');
+    expect(wrapper.find('img').props().src).to.deep.equal({});
   });
 });
