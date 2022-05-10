7a4798d58d0f4dec96fbb4f6cb96064f7b031860
Ali Haghighatkhah <ali@lightcurve.io>
2022-05-10 10:37:11 +0200

    Add unit test for Map

--- src/modules/network/components/map/index.test.js
+++ src/modules/network/components/map/index.test.js
@@ -1,102 +1,43 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import L from 'leaflet';
 import peers from '@tests/constants/peers';
 import Map from './index';
 
-const LeafletMock = jest.genMockFromModule('leaflet');
+const mockAddLayer = jest.fn();
+const mockAddAttribution = jest.fn();
+const networkMap = {
+  addLayer: mockAddLayer,
+  attributionControl: {
+    addAttribution: mockAddAttribution,
+  },
+};
 
-class Map1 extends LeafletMock.Map {
-  constructor(id, options = {}) {
-    super();
-    Object.assign(this, L.Evented.prototype);
+const mockAddTo = jest.fn();
+const mockSetView = jest.fn().mockReturnValue(networkMap);
 
-    this.options = { ...L.Map.prototype.options, ...options };
-    this._container = id;
-
-    if (options.bounds) {
-      this.fitBounds(options.bounds, options.boundsOptions);
-    }
-
-    if (options.maxBounds) {
-      this.setMaxBounds(options.maxBounds);
-    }
-
-    if (options.center && options.zoom !== undefined) {
-      this.setView(L.latLng(options.center), options.zoom);
-    }
-  }
-
-  _limitZoom(zoom) {
-    const min = this.getMinZoom();
-    const max = this.getMaxZoom();
-    return Math.max(min, Math.min(max, zoom));
-  }
-
-  _resetView(center, zoom) {
-    this._initialCenter = center;
-    this._zoom = zoom;
-  }
-
-  fitBounds(bounds, options) {
-    this._bounds = bounds;
-    this._boundsOptions = options;
-  }
-
-  getBounds() {
-    return this._bounds;
-  }
-
-  getCenter() {
-    return this._initialCenter;
-  }
-
-  getMaxZoom() {
-    return this.options.maxZoom === undefined ? Infinity : this.options.maxZoom;
-  }
-
-  getMinZoom() {
-    return this.options.minZoom === undefined ? 0 : this.options.minZoom;
-  }
-
-  getZoom() {
-    return this._zoom;
-  }
-
-  setMaxBounds(bounds) {
-    bounds = L.latLngBounds(bounds);
-    this.options.maxBounds = bounds;
-    return this;
-  }
-
-  setView(center, zoom) {
-    zoom = zoom === undefined ? this.getZoom() : zoom;
-    this._resetView(L.latLng(center), this._limitZoom(zoom));
-    return this;
-  }
-
-  setZoom(zoom) {
-    return this.setView(this.getCenter(), zoom);
-  }
-}
+jest.mock('leaflet', () => ({
+  map: () => ({
+    setView: mockSetView,
+  }),
+  tileLayer: () => ({
+    addTo: mockAddTo,
+  }),
+  icon: jest.fn(),
+  divIcon: jest.fn(),
+  setView: jest.fn(),
+  marker: jest.fn(),
+  markerClusterGroup: () => ({
+    addLayer: mockAddLayer,
+  }),
+}));
+jest.mock('leaflet.markercluster/dist/leaflet.markercluster', () => ({}));
 
 describe('Map view', () => {
-  jest.mock('leaflet', () => ({
-    map: (id, options) => new Map1(id, options),
-    tileLayer: jest.fn(),
-  }));
-
   it('Renders the map correctly', () => {
-    // @todo Fix failing test with Map component
-    const div = global.document.createElement('div');
-    global.document.body.appendChild(div);
-
-    const wrapper = mount(<Map peers={peers} />, { attachTo: div });
-
-    // map called, tiles added, layer added with peer list, attributionControl called
-    expect(wrapper).toBeVisible();
-    expect(L.map).toHaveBeenCalled();
-    expect(L.tileLayer).toHaveBeenCalled();
-    expect(L.addLayer).toHaveBeenCalledWith();
+    mount(<Map peers={peers} />);
+    expect(mockSetView).toBeCalled();
+    expect(mockAddTo).toBeCalledWith(networkMap);
+    expect(mockAddLayer).toHaveBeenCalledTimes(peers.length + 1);
+    expect(mockAddAttribution).toBeCalled();
   });
 });
