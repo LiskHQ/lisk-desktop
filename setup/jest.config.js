561c04a1f3ed5d258603a2c55141f31f8224fb6d
eniolam1000752 <eniolam1000752@gmail.com>
2022-05-10 09:43:19 +0100

    chore: reverted collectCoverage

--- setup/jest.config.js
+++ setup/jest.config.js
@@ -52,7 +52,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/__mocks__/fileMock.js',
   },
-  collectCoverage: false,
+  collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage/jest',
   collectCoverageFrom: ['packages/**/*.js', 'setup/**/*.js', 'app/src/**/*.js'],
   coveragePathIgnorePatterns: [
