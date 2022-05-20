bbe0aa0bd780141f8ad1680ee753bedd9a6d53c0
eniolam1000752 <eniolam1000752@gmail.com>
2022-05-20 11:02:23 +0100

    created mocks and hook for account

--- src/modules/account/components/AccountSelect/AccountSelect.js
+++ src/modules/account/components/AccountSelect/AccountSelect.js
@@ -1,52 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Box from 'src/theme/box';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import { OutlineButton } from 'src/theme/buttons';
 import Icon from 'src/theme/Icon';
-import Table from 'src/theme/table';
 import WalletVisual from '@wallet/components/walletVisual';
+import { useAccounts } from '../../hooks/useAccounts';
 import styles from './AccountSelect.css';
 
-function AccountRow({ data: { address, username } }) {
+function AccountRow({
+  account, onSelect, onRemove, showRemove,
+}) {
+  const { metadata: { name, address } } = account;
+
   return (
-    <button className={styles.accountWraper}>
+    <button className={styles.accountWraper} onClick={() => onSelect(account)}>
       <WalletVisual address={address} size={40} />
       <div align="left">
         <b className={`${styles.addressValue}`}>
-          {username}
+          {name}
         </b>
         <p className={`${styles.addressValue}`}>
           {address}
         </p>
       </div>
+      {showRemove && (
+      <button onClick={() => onRemove(account)}>
+        <Icon name="deleteIcon" />
+      </button>
+      )}
     </button>
   );
 }
 
-const AccountSelect = () => {
+const AccountSelect = ({ onSelect, onAddAccount, onRemoveAccount }) => {
   const { t } = useTranslation();
-
-  const accounts = [
-    {
-      address: 'lskckzngagcs4d5gvsgxmgnabyfyj8pz266gv8s8t',
-      publicKey: '3ff32442bb6da7d60c1b7752b24e6467813c9b698e0f278d48c43580da972',
-      username: 'test user',
-    },
-    {
-      address: 'lsk74ar23k2zk3mpsnryxbxf5yf9ystudqmj4oj6e',
-      publicKey: '3ff32442bb6da7d60c1b7752b24e6467813c9b698e0f278d48c43580da972',
-      username: 'test user',
-    },
-    {
-      address: 'lskckzngagcs4d5gvsgxmgnabyfyj8pz266gv8s8t',
-      publicKey: '3ff32442bb6da7d60c1b7752b24e6467813c9b698e0f278d48c43580da972',
-      username: 'test user',
-    },
-  ];
-
-  const onAddAccount = () => {};
-  const onRemoveAccount = () => {};
+  const [accounts] = useAccounts();
+  const [showRemove, setShowRemove] = useState();
 
   return (
     <>
@@ -56,35 +46,48 @@ const AccountSelect = () => {
         >
           <div className={styles.wrapper}>
             <div className={styles.headerWrapper}>
-              <h1>{t('Manage accounts')}</h1>
+              <h1>{t(showRemove ? 'Choose account' : 'Manage accounts')}</h1>
             </div>
             <Box className={styles.accountListWrapper}>
-              <Table
-                data={accounts}
-                header={[]}
-                isLoading={false}
-                row={AccountRow}
-                canLoadMore={false}
-                additionalRowProps={{}}
-                emptyState={<span>no account</span>}
-              />
+              {
+               accounts.map((account) => (
+                 <AccountRow
+                   key={account.uuid}
+                   account={account}
+                   onSelect={onSelect}
+                   showRemove={showRemove}
+                   onRemove={onRemoveAccount}
+                 />
+               ))
+             }
             </Box>
-            <OutlineButton
-              className={`${styles.button} ${styles.addAccountBtn}`}
-              onAddAccount={onAddAccount}
-            >
-              <Icon name="personIcon" />
-              {' '}
-              Add another account
-            </OutlineButton>
-            <OutlineButton
-              className={styles.button}
-              onRemoveAccount={onRemoveAccount}
-            >
-              <Icon name="deleteIcon" />
-              {' '}
-              Remove an account
-            </OutlineButton>
+            { showRemove ? (
+              <OutlineButton
+                className={`${styles.button} ${styles.addAccountBtn}`}
+                onClick={() => setShowRemove(false)}
+              >
+                {t('Done')}
+              </OutlineButton>
+            ) : (
+              <>
+                <OutlineButton
+                  className={`${styles.button} ${styles.addAccountBtn}`}
+                  onClick={onAddAccount}
+                >
+                  <Icon name="personIcon" />
+                  {t('Add another account')}
+                </OutlineButton>
+                <OutlineButton
+                  className={styles.button}
+                  onClick={() => {
+                    setShowRemove(true);
+                  }}
+                >
+                  <Icon name="deleteIcon" />
+                  {t('Remove an account')}
+                </OutlineButton>
+              </>
+            )}
           </div>
         </div>
       </div>
---
+++ src/modules/account/hooks/useAccounts.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+
+const mockAccounts = [
+  {
+    crypto: {
+      kdf: 'argon2id',
+      kdfparams: {
+        parallelism: 4,
+        iterations: 1,
+        memory: 2048,
+        salt: '30fc0301d36fcdc7bd8204e19a0043fc',
+      },
+      cipher: 'aes-256-gcm',
+      cipherparams: {
+        iv: '281d21872c2d303e59850ce4',
+        tag: '2458479edf6aea5c748021ae296e467d',
+      },
+      ciphertext: '44fdb2b132d353a5c65f04e5e3afdd531f63abc45444ffd4cdbc7dedc45f899bf5b7478947d57319ea8c620e13480def8a518cc05e46bdddc8ef7c8cfc21a3bd',
+    },
+    metadata: {
+      name: 'my lisk account',
+      description: 'ed25519 key pair',
+      pubkey: 'c6bae83af23540096ac58d5121b00f33be6f02f05df785766725acdd5d48be9d',
+      path: "m/44'/134'/0'",
+      address: 'lsk74ar23k2zk3mpsnryxbxf5yf9ystudqmj4oj6e',
+      creationTime: '',
+      derivedFromUUID: 'fa3e4ceb-10dc-41ad-810e-17bf51ed93aa',
+    },
+    uuid: 'ef52c117-d7cc-4246-bc9d-4dd506bef82f',
+    version: 1,
+  },
+  {
+    crypto: {
+      kdf: 'argon2id',
+      kdfparams: {
+        parallelism: 4,
+        iterations: 1,
+        memory: 2048,
+        salt: '30fc0301d36fcdc7bd8204e19a0043fc',
+      },
+      cipher: 'aes-256-gcm',
+      cipherparams: {
+        iv: '281d21872c2d303e59850ce4',
+        tag: '2458479edf6aea5c748021ae296e467d',
+      },
+      ciphertext: '44fdb2b132d353a5c65f04e5e3afdd531f63abc45444ffd4cdbc7dedc45f899bf5b7478947d57319ea8c620e13480def8a518cc05e46bdddc8ef7c8cfc21a3bd',
+    },
+    metadata: {
+      name: 'test account',
+      description: 'ed25519 key pair',
+      pubkey: 'c6bae83af23540096ac58d5121b00f33be6f02f05df785766725acdd5d48be9d',
+      path: "m/44'/134'/0'",
+      address: 'lsktzb4j7e3knk4mkxckdr3y69gtu2nwmsb3hjbkg',
+      creationTime: '',
+      derivedFromUUID: 'fa3e4ceb-10dc-41ad-810e-17bf51ed93aa',
+    },
+    uuid: 'ef52c117-d7cc-4246-bc9d-4dd506bef82f',
+    version: 1,
+  },
+];
+
+// eslint-disable-next-line
+export function useAccounts(){
+  const [accounts, setAccount] = useState(mockAccounts);
+
+  return [accounts, setAccount];
+}
