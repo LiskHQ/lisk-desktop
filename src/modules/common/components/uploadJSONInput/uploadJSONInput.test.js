9e197daee76ec78649b9fd202c521bde6d6232a4
Gad Jacobs <gadexofficial@gmail.com>
2022-05-20 12:23:25 +0100

    Add unit tests

---
+++ src/modules/common/components/uploadJSONInput/uploadJSONInput.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import UploadJSONInput from './index';
+
+describe('Upload JSON input component', () => {
+  let wrapper;
+  const props = {
+    label: '',
+    value: null,
+    error: '',
+    onChange: jest.fn(),
+  };
+
+  let inputField;
+
+  beforeEach(() => {
+    wrapper = mount(<UploadJSONInput {...props} />);
+    inputField = wrapper.find('.tx-sign-input').first();
+  });
+
+  it('renders properly', () => {
+    expect(wrapper).toContainMatchingElement('.fileInputLabel');
+    expect(wrapper).toContainMatchingElement('.fileInputBtn');
+    expect(wrapper).toContainMatchingElement('.clickableFileInput');
+    expect(wrapper).toContainMatchingElement('.textAreaContainer');
+  });
+
+  it('should paste if JSON valid', () => {
+    const JSONObject = {};
+    const clipboardData = {
+      getData: () => JSON.stringify(JSONObject),
+    };
+
+    inputField.simulate('paste', { clipboardData });
+    expect(props.onChange).toBeCalledWith(expect.objectContaining(JSONObject));
+  });
+
+  it('should show error', () => {
+    
+  })
+});
