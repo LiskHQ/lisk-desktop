6961f61a0d7979922fc6b7f377071ccc74bda068
eniolam1000752 <eniolam1000752@gmail.com>
2022-05-23 17:01:18 +0100

    added extra unit test for account row

--- src/modules/account/components/AccountRow/AccountRow.test.js
+++ src/modules/account/components/AccountRow/AccountRow.test.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import mockSavedAccounts from '@tests/fixtures/accounts';
 import AccountRow from './AccountRow';
 
 jest.mock('react-i18next');
 
+let container = null;
 const props = {
   account: mockSavedAccounts[0],
+  showRemove: true,
 };
 
 beforeEach(() => {
-  render(<AccountRow {...props} />);
+  container = render(<AccountRow {...props} />);
 });
 
-describe('Select Account Form', () => {
+describe('Select Account Row', () => {
   it('Should render account list', async () => {
-    expect(screen.getByText(mockSavedAccounts[0].metadata.address)).toBeTruthy();
-    expect(screen.getByText(mockSavedAccounts[0].metadata.name)).toBeTruthy();
+    expect(container.getByText(mockSavedAccounts[0].metadata.address)).toBeTruthy();
+    expect(container.getByText(mockSavedAccounts[0].metadata.name)).toBeTruthy();
+  });
+
+  it('Should  list', async () => {
+    container.rerender(<AccountRow {...props} showRemove />);
+    expect(container.queryByTestId('delete-icon')).toBeTruthy();
   });
 });
