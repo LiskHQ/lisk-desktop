c0d943b296cc03b5daec3738c099eba6be7c934b
Gad Jacobs <gadexofficial@gmail.com>
2022-05-30 05:16:54 +0100

    Added unit tests for setPasswordSuccess component

--- src/modules/auth/components/setPasswordSuccess/setPasswordSuccess.js
+++ src/modules/auth/components/setPasswordSuccess/setPasswordSuccess.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { PrimaryButton, TertiaryButton } from 'src/theme/buttons';
-import { downloadJSON } from '@transaction/utils';
+import { downloadJSON, transactionToJSON } from '@transaction/utils';
 import Box from 'src/theme/box';
 import BoxContent from 'src/theme/box/content';
 import Icon from 'src/theme/Icon';
 import styles from './setPasswordSuccess.css';
 
-function SetPasswordSuccess({ onClose, backupJSON }) {
+function SetPasswordSuccess({ onClose, encryptedPhrase }) {
   const { t } = useTranslation();
 
   const onDownload = () => {
-    downloadJSON(backupJSON, 'encrypted_secret_recovery_phrase');
+    const encryptedJSON = JSON.parse(transactionToJSON(encryptedPhrase));
+    downloadJSON(encryptedJSON, 'encrypted_secret_recovery_phrase');
   };
+
+  const onContinue = () => onClose();
 
   return (
     <Box className={styles.container}>
       <BoxContent className={styles.content}>
         <h1>{t("Perfect! You're all set")}</h1>
         <p className={styles.subheader}>
-          {t('You can now download your encrypted secret recovery phrase and use it to add your account on other devices.')}
+          {t(
+            'You can now download your encrypted secret recovery phrase and use it to add your account on other devices.',
+          )}
         </p>
         <div className={styles.downloadLisk}>
           <Icon name="fileOutline" />
@@ -32,12 +37,18 @@ function SetPasswordSuccess({ onClose, backupJSON }) {
             {t('Download')}
           </TertiaryButton>
         </div>
-        <PrimaryButton className={styles.button} onClick={onClose}>
+        <PrimaryButton className={styles.continueButton} onClick={onContinue}>
           {t('Continue to Dashboard')}
         </PrimaryButton>
       </BoxContent>
     </Box>
   );
 }
+
+SetPasswordSuccess.defaultProps = {
+  encryptedPhrase: {
+    error: 'no encrypted backup found',
+  },
+};
 
 export default SetPasswordSuccess;
