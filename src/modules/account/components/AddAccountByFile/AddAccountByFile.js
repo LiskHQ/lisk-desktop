8c4fd9b9fa00fa21d7b4c9d8b6670838bc709f4a
iris salcedo <iris.salcedo@lightcurve.io>
2022-05-31 15:16:05 +0200

    Add RestoreAccountForm

--- src/modules/account/components/AddAccountByFile/AddAccountByFile.js
+++ src/modules/account/components/AddAccountByFile/AddAccountByFile.js
@@ -1,6 +1,18 @@
 /* eslint-disable max-lines */
 import React from 'react';
+import { withRouter } from 'react-router';
+import RestoreAccountForm from 'src/modules/auth/components/restoreAccountForm';
+import styles from './AddAccountByFile.css';
 
-const AddAccountByPassFile = () => <>TODO</>;
+const AddAccountByPassFile = ({ history }) => {
+  const onSubmit = () => {
+  };
 
-export default AddAccountByPassFile;
+  return (
+    <div className={styles.container}>
+      <RestoreAccountForm onSubmit={onSubmit} onBack={history.goBack} />
+    </div>
+  );
+};
+
+export default withRouter(AddAccountByPassFile);
