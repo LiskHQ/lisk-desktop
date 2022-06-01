3b52a6ea004653d4dfbb59b04c0d66bf4e0b1eaf
Ali Haghighatkhah <ali@lightcurve.io>
2022-06-01 16:40:45 +0200

    Fix the alignment of vote details

--- src/modules/dpos/validator/components/latestVotes/tableHeader.js
+++ src/modules/dpos/validator/components/latestVotes/tableHeader.js
@@ -1,5 +1,4 @@
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
-import gridVisibility from 'flexboxgrid-helpers/dist/flexboxgrid-helpers.min.css';
 
 export default t => ([
   {
@@ -8,14 +7,10 @@ export default t => ([
   },
   {
     title: t('Date'),
-    classList: grid['col-xs-3'],
-  },
-  {
-    title: t('Round'),
-    classList: `${grid['col-lg-2']} ${gridVisibility['hidden-md']} ${gridVisibility['hidden-sm']} ${gridVisibility['hidden-xs']}`,
+    classList: grid['col-xs-4'],
   },
   {
     title: t('Votes'),
-    classList: `${grid['col-xs-5']} ${grid['col-lg-3']}`,
+    classList: grid['col-xs-4'],
   },
 ]);
