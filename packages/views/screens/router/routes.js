ecf90c35fef7e14dec8495705f41789bb44dd840
masoud soroush <masoud.s.geo@gmail.com>
2022-06-03 10:44:50 +0200

    chore: change account init route

--- packages/views/screens/router/routes.js
+++ packages/views/screens/router/routes.js
@@ -11,7 +11,7 @@ export default {
     exact: true,
   },
   addAccountOptions: {
-    path: '/account/add/options',
+    path: '/account',
     isPrivate: false,
     forbiddenTokens: [],
     exact: true,
