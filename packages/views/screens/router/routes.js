f3c8f734bc2a85b657a2bb99973de5f272dae134
Gad Jacobs <gadexofficial@gmail.com>
2022-06-06 04:15:00 +0100

    Update backup recovery phrase  route

--- packages/views/screens/router/routes.js
+++ packages/views/screens/router/routes.js
@@ -104,10 +104,9 @@ export default {
     forbiddenTokens: [],
   },
   backupRecoveryPhraseFlow: {
-    path: 'account/backup-recovery-phrase',
+    path: '/account/backup-recovery-phrase',
     isPrivate: true,
     forbiddenTokens: [],
-    exact: true,
   },
 };
 
