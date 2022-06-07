186c42ca8217f840b508692df6f97466dd9861fe
masoud soroush <masoud.s.geo@gmail.com>
2022-06-07 22:39:41 +0200

    feat: add useAccounts test

---
+++ src/modules/account/hooks/useAccounts.test.js
@@ -0,0 +1,44 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import mockSavedAccounts from '@tests/fixtures/accounts';
+import actionTypes from '@account/store/actionTypes';
+import { useAccounts } from './useAccounts';
+
+const mockDispatch = jest.fn();
+const accountStateObject = { [mockSavedAccounts[0].metadata.address]: mockSavedAccounts[0] };
+const mockState = {
+  account: {
+    list: accountStateObject,
+  },
+};
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn().mockImplementation((fn) => fn(mockState)),
+  useDispatch: () => mockDispatch,
+}));
+
+describe('useAccount hook', () => {
+  const { result } = renderHook(() => useAccounts());
+  it('setAccount Should not trigger on mounting', async () => {
+    expect(mockDispatch).toHaveBeenCalledTimes(0);
+  });
+
+  it('setAccount should dispatch an action', async () => {
+    const { setAccount } = result.current;
+    const expectedAction = {
+      type: actionTypes.addAccount,
+      accountSchema: mockSavedAccounts[0],
+    };
+    act(() => {
+      setAccount(mockSavedAccounts[0]);
+    });
+    expect(mockDispatch).toHaveBeenCalledTimes(1);
+    expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+  });
+
+  it('accounts should return and convert state as an array', async () => {
+    const { accounts } = result.current;
+    const expectArrayAccounts = [
+      mockSavedAccounts[0],
+    ];
+    expect(accounts).toMatchObject(expectArrayAccounts);
+  });
+});
