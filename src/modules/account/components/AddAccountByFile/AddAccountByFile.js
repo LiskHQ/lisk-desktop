b43d3ba26df51d9f1d1b18e65d32f04797d047ab
eniolam1000752 <eniolam1000752@gmail.com>
2022-06-07 06:08:07 +0100

    fixed dark theme css issue

--- src/modules/account/components/AddAccountByFile/AddAccountByFile.js
+++ src/modules/account/components/AddAccountByFile/AddAccountByFile.js
@@ -2,17 +2,18 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import routes from '@screens/router/routes';
-import RestoreAccountForm from 'src/modules/auth/components/restoreAccountForm';
+import RestoreAccountForm from 'src/modules/auth/components/RestoreAccountForm';
 import EnterPasswordForm from 'src/modules/auth/components/EnterPasswordForm';
 import MultiStep from 'src/modules/common/components/OldMultiStep';
 import styles from './AddAccountByFile.css';
 import { useCurrentAccount } from '../../hooks';
 
-const AddAccountByPassFile = ({ history }) => {
+const AddAccountByPassFile = ({ history, login }) => {
   const [, setCurrentAccount] = useCurrentAccount();
 
-  const onEnterPasswordSuccess = ({ accountSchema }) => {
+  const onEnterPasswordSuccess = ({ accountSchema, recoveryPhrase }) => {
     setCurrentAccount(accountSchema);
+    login(recoveryPhrase); // Todo: this login method is depricated
     history.push(routes.dashboard.path);
   };
 
