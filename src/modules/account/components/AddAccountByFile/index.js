f6fa30751304ce69c4a845b56cef528b74fea650
eniolam1000752 <eniolam1000752@gmail.com>
2022-06-07 06:08:07 +0100

    fixed dark theme css issue

--- src/modules/account/components/AddAccountByFile/index.js
+++ src/modules/account/components/AddAccountByFile/index.js
@@ -1,1 +1,13 @@
-export { default } from './AddAccountByFile';
+/* istanbul ignore file */
+import { connect } from 'react-redux';
+import { login } from '@auth/store/action';
+import AddAccountByFile from './AddAccountByFile';
+
+const mapDispatchToProps = {
+  login,
+};
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(AddAccountByFile);
