917dedb5041c00fb3ae86b9e8465f00cc8a6ba1b
iris salcedo <iris.salcedo@lightcurve.io>
2022-06-13 15:19:24 +0200

    Iterate accounts list

--- src/modules/account/components/SwitchAccount/SwitchAccount.js
+++ src/modules/account/components/SwitchAccount/SwitchAccount.js
@@ -4,32 +4,28 @@ import { Link } from 'react-router-dom';
 import WalletVisual from '@wallet/components/walletVisual';
 import { useAccounts } from '@account/hooks/useAccounts';
 import routes from '@screens/router/routes';
+import history from 'src/utils/history';
+import { removeSearchParamsFromUrl } from 'src/utils/searchParams';
 import Box from 'src/theme/box';
 import BoxContent from 'src/theme/box/content';
 import Icon from 'src/theme/Icon';
 import styles from './SwitchAccount.css';
 
-const AccountList = ({ accounts }) => (
+const AccountList = ({ accounts, onAccountClick }) => (
   <div className={styles.accountList}>
-    <div
-      className={styles.account}
-      onClick={() => {
-        // TODO close modal, refetch data and update redux
-      }}
-    >
-      <WalletVisual address="lskm555k7nhhw954rw4pqy5q9wn28n3cec94fmp4n" />
-      <div>
-        <p className={styles.accountName}>Lisker</p>
-        <p className={styles.accountAddress}>lskm555k7nhhw954rw4pqy5q9wn28n3cec94fmp4n</p>
+    {accounts.map(account => (
+      <div
+        key={`switch-account-lisk-${account.metadata.address}`}
+        className={styles.account}
+        onClick={onAccountClick}
+      >
+        <WalletVisual address={account.metadata.address} />
+        <div>
+          <p className={styles.accountName}>{account.metadata.name}</p>
+          <p className={styles.accountAddress}>{account.metadata.address}</p>
+        </div>
       </div>
-    </div>
-    <div className={styles.account}>
-      <WalletVisual address="lskw7488a9nqy6m3zkg68x6ynsp6ohg4y7wazs3mw" />
-      <div>
-        <p className={styles.accountName}>bob</p>
-        <p className={styles.accountAddress}>lskw7488a9nqy6m3zkg68x6ynsp6ohg4y7wazs3mw</p>
-      </div>
-    </div>
+    ))}
   </div>
 );
 
@@ -37,11 +33,15 @@ const SwitchAccount = () => {
   const { t } = useTranslation();
   const [accounts] = useAccounts();
 
+  const onAccountClick = () => {
+    // TODO close modal, refetch data and update redux
+  };
+
   return (
     <Box className={styles.container}>
       <BoxContent className={styles.content}>
         <h1>{t('Switch account')}</h1>
-        <AccountList accounts={accounts} />
+        <AccountList accounts={accounts} onAccountClick={onAccountClick} />
         <Link
           className={styles.addAccountLink}
           to={routes.addAccount.path}
