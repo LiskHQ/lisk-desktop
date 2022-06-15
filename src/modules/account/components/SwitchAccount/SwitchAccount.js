7c7a50d024b1bb3e9ef4dffa1fdb9d491ed24c97
iris salcedo <iris.salcedo@lightcurve.io>
2022-06-15 09:51:18 +0200

    Remove unnecessary changes

--- src/modules/account/components/SwitchAccount/SwitchAccount.js
+++ src/modules/account/components/SwitchAccount/SwitchAccount.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import routes from '@screens/router/routes';
-import { login } from '@auth/store/action';
-import history from 'src/utils/history';
-import { removeSearchParamsFromUrl } from 'src/utils/searchParams';
 import ManageAccounts from '../ManageAccounts';
 
 const SwitchAccount = () => {
-  const dispatch = useDispatch();
   const { t } = useTranslation();
 
-  const onSelectAccount = (account) => {
-    removeSearchParamsFromUrl(history, ['modal']);
-    dispatch(login({ publicKey: account.metadata.pubkey })); // Todo this login method is deprecated
-  };
-
-  const onAddAccount = () => {
-    history.push(routes.addAccountOptions.path);
-  };
-
   return (
-    <ManageAccounts
-      title={t('Switch account')}
-      onSelectAccount={onSelectAccount}
-      onAddAccount={onAddAccount}
-    />
+    <ManageAccounts title={t('Switch account')}/>
   );
 };
 
--- src/modules/account/components/SwitchAccount/SwitchAccount.test.js
+++ src/modules/account/components/SwitchAccount/SwitchAccount.test.js
@@ -1,9 +1,6 @@
 import { useDispatch } from 'react-redux';
 import mockSavedAccounts from '@tests/fixtures/accounts';
-import routes from '@screens/router/routes';
-import { removeSearchParamsFromUrl } from 'src/utils/searchParams';
 import { mountWithProps } from 'src/utils/testHelpers';
-import history from 'src/utils/history';
 import { OutlineButton } from 'src/theme/buttons';
 import AccountRow from '../AccountRow';
 import SwitchAccount from './SwitchAccount';
@@ -30,16 +27,5 @@ describe('Switch account', () => {
     expect(wrapper.find(AccountRow)).toHaveLength(mockSavedAccounts.length);
     expect(wrapper.find(AccountRow).first()).toHaveText(`${mockSavedAccounts[0].metadata.name}${mockSavedAccounts[0].metadata.address}`);
     expect(wrapper.find(OutlineButton)).toHaveText('Add another account');
-  });
-
-  it('Should work properly when clicking on clicking add account', () => {
-    wrapper.find(OutlineButton).first().simulate('click');
-    expect(history.push).toHaveBeenCalledWith(routes.addAccountOptions.path);
-  });
-
-  it('Should work properly when clicking on clicking select account', () => {
-    wrapper.find(AccountRow).first().simulate('click');
-    expect(removeSearchParamsFromUrl).toHaveBeenCalledWith(history, ['modal']);
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
   });
 });
