0820eb915f8eee27b43cc86b7ac5e28d8478e248
iris salcedo <iris.salcedo@lightcurve.io>
2022-06-15 10:09:13 +0200

    Reduce unit test

--- src/modules/account/components/SwitchAccount/SwitchAccount.test.js
+++ src/modules/account/components/SwitchAccount/SwitchAccount.test.js
@@ -1,31 +1,20 @@
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { mount } from 'enzyme';
 import mockSavedAccounts from '@tests/fixtures/accounts';
-import { mountWithProps } from 'src/utils/testHelpers';
 import { OutlineButton } from 'src/theme/buttons';
 import AccountRow from '../AccountRow';
 import SwitchAccount from './SwitchAccount';
 
-jest.mock('src/utils/history');
-
 jest.mock('@account/hooks/useAccounts', () => ({
   useAccounts: jest.fn().mockReturnValue([mockSavedAccounts]),
 }));
-jest.mock('src/utils/searchParams', () => ({
-  removeSearchParamsFromUrl: jest.fn(),
-}));
-
-const mockDispatch = jest.fn();
-useDispatch.mockReturnValue(mockDispatch);
 
 describe('Switch account', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = mountWithProps(SwitchAccount, {});
-  });
-
   it('Should render properly', () => {
+    const wrapper = mount(<SwitchAccount />);
+
     expect(wrapper.find(AccountRow)).toHaveLength(mockSavedAccounts.length);
     expect(wrapper.find(AccountRow).first()).toHaveText(`${mockSavedAccounts[0].metadata.name}${mockSavedAccounts[0].metadata.address}`);
-    expect(wrapper.find(OutlineButton)).toHaveText('Add another account');
+    expect(wrapper.find(OutlineButton).first()).toHaveText('Add another account');
   });
 });
