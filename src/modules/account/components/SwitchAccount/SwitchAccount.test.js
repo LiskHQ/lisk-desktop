2929f01b9f9ed1852057b342b64692d50103ff97
eniolam1000752 <eniolam1000752@gmail.com>
2022-06-19 13:53:30 +0100

    fixed failing unit test

--- src/modules/account/components/SwitchAccount/SwitchAccount.test.js
+++ src/modules/account/components/SwitchAccount/SwitchAccount.test.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { mount } from 'enzyme';
 import mockSavedAccounts from '@tests/fixtures/accounts';
 import { OutlineButton } from 'src/theme/buttons';
+import { mountWithRouter } from 'src/utils/testHelpers';
 import AccountRow from '../AccountRow';
 import SwitchAccount from './SwitchAccount';
 
@@ -9,9 +8,19 @@ jest.mock('@account/hooks/useAccounts', () => ({
   useAccounts: jest.fn().mockReturnValue([mockSavedAccounts]),
 }));
 
+jest.mock('react-i18next');
+jest.mock('@account/hooks', () => ({
+  useAccounts: jest.fn(() => ({
+    accounts: mockSavedAccounts,
+  })),
+  useCurrentAccount: jest.fn(() => (
+    [mockSavedAccounts[0], jest.fn()]
+  )),
+}));
+
 describe('Switch account', () => {
   it('Should render properly', () => {
-    const wrapper = mount(<SwitchAccount />);
+    const wrapper = mountWithRouter(SwitchAccount);
 
     expect(wrapper.find(AccountRow)).toHaveLength(mockSavedAccounts.length);
     expect(wrapper.find(AccountRow).first()).toHaveText(`${mockSavedAccounts[0].metadata.name}${mockSavedAccounts[0].metadata.address}`);
