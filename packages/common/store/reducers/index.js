cb4eb552a6d100a7544240aa088400f0dec90853
masoud soroush <masoud.s.geo@gmail.com>
2022-06-20 10:47:05 +0200

    chore: move loading reducer

--- packages/common/store/reducers/index.js
+++ packages/common/store/reducers/index.js
@@ -9,5 +9,5 @@ export { default as appUpdates } from '@update/store/reducers/appUpdates';
 export { default as voting } from '@dpos/validator/store/reducers/voting';
 export { default as watchList } from '@dpos/validator/store/reducers/watchList';
 export { default as service } from './service';
-export { default as loading } from './loading';
+export { default as loading } from 'src/modules/common/store/reducers/loading';
 export { account } from 'src/modules/account/store/reducer';
