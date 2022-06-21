6b92f0e9d9ee75e1b1be91171250036648451d37
eniolam1000752 <eniolam1000752@gmail.com>
2022-06-21 17:28:38 +0100

    fixed issue in rootReducer.test.js

--- src/redux/rootReducer.test.js
+++ src/redux/rootReducer.test.js
@@ -1,5 +1,9 @@
 import * as reducers from './rootReducer';
 
+jest.mock('src/redux/store', () => ({
+  storage: {},
+}));
+
 describe('Reducer: Initiate reducers', () => {
   it('should initiate the reducers with any first action', () => {
     Object.keys(reducers).forEach((reducer) => {
