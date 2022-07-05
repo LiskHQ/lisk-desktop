e3dd18ee9135d455c00270b74ea181c1ab0d48c8
iris salcedo <iris.salcedo@lightcurve.io>
2022-07-05 15:44:25 +0200

    Use InfoBanner component

--- src/modules/blockchainApplication/manage/components/BlockchainApplications/index.js
+++ src/modules/blockchainApplication/manage/components/BlockchainApplications/index.js
@@ -1,23 +1,37 @@
 /* istanbul ignore file */
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import InfoBanner from 'src/modules/common/components/infoBanner/infoBanner';
 import BlockchainApplicationStatistics from '../../../explore/components/BlockchainApplicationStatistics';
 import styles from './blockchainApplications.css';
 
-const InfoBanner = () => <p className={styles.banner}>banner</p>;
+// TODO use correct component when #4355 ticket it completed
 const BlockchainApplicationsList = () => <span>list</span>;
 
-const BlockchainApplications = ({ apps, statistics }) => (
-  <div className={styles.wrapper}>
-    <InfoBanner />
-    <div>
-      <div className={styles.content}>
-        <BlockchainApplicationsList apps={apps} />
-      </div>
-      <div className={styles.sideBar}>
-        <BlockchainApplicationStatistics statistics={statistics} />
+const BlockchainApplications = ({ apps, statistics }) => {
+  const { t } = useTranslation();
+  return (
+    <div className={styles.wrapper}>
+      <InfoBanner
+        className={styles.banner}
+        t={t}
+        name="blockchainApplicationsPageBanner"
+        infoLabel={t('New')}
+        infoMessage={t('Explore decentralized applications')}
+        infoDescription={t('Description')}
+        infoLink="https://lisk.io"
+        show
+      />
+      <div>
+        <div className={styles.content}>
+          <BlockchainApplicationsList apps={apps} />
+        </div>
+        <div className={styles.sideBar}>
+          <BlockchainApplicationStatistics statistics={statistics} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BlockchainApplications;
