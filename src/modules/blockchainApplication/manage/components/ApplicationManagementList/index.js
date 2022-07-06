8ae5c10bead7e4c3a9f414349f3c45d7ce2fed63
iris salcedo <iris.salcedo@lightcurve.io>
2022-07-06 15:16:13 +0200

    Add useCurrentApplication

--- src/modules/blockchainApplication/manage/components/ApplicationManagementList/index.js
+++ src/modules/blockchainApplication/manage/components/ApplicationManagementList/index.js
@@ -1,34 +1,68 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useCurrentApplication } from '@blockchainApplication/manage/hooks/useCurrentApplication.js';
 import Box from 'src/theme/box';
 import BoxHeader from 'src/theme/box/header';
 import BoxContent from 'src/theme/box/content';
 import Footer from 'src/theme/box/footer';
+import { TertiaryButton } from 'src/theme/buttons';
+import Icon from 'src/theme/Icon';
 import styles from './applicationManagementList.css';
 
-const ApplicationListItem = ({ key, app }) => (
-  <div key={key}>
+const ApplicationListItem = ({
+  key, app, isCurrentApplication, deleteApp, setApplication,
+}) => (
+  <div
+    key={key}
+    onClick={() => {
+      setApplication(app.chainId);
+    }}
+  >
+    <div className={styles.appAvatar} />
     {app.name}
+    {isCurrentApplication ? (
+      <Icon name="checkmark" />
+    ) : (
+      <TertiaryButton
+        onClick={() => { deleteApp(app.chainId); }}
+        size="m"
+      >
+        <Icon name="remove" />
+      </TertiaryButton>
+    )}
   </div>
 );
 
-const ApplicationList = ({ apps }) => (
-  <div>
-    {apps.map((app, index) => <ApplicationListItem app={app} key={`ApplicationListItem-${index}`} />)}
-  </div>
-);
+const ApplicationList = ({ apps }) => {
+  const [currentApplication, setApplication] = useCurrentApplication();
+
+  return (
+    <div>
+      {apps.map((app) => (
+        <ApplicationListItem
+          key={`ApplicationListItem-${app.chainId}`}
+          app={app}
+          isCurrentApplication={currentApplication.chainId === app.chainId}
+          setApplication={setApplication}
+        />
+      ))}
+    </div>
+  );
+};
 
 const ApplicationManagementList = () => {
   const { t } = useTranslation();
-
   return (
     <Box className={styles.wrapper}>
       <BoxHeader>
         <h1>{t('Applications')}</h1>
       </BoxHeader>
       <BoxContent className={styles.chartBox}>
-        <ApplicationList apps={[{ name: 'LISK' }, { name: 'Enevti' }, { name: 'DoEdu' }]} />
+        <ApplicationList apps={[
+          { name: 'LISK' }, { name: 'Enevti' }, { name: 'DoEdu' },
+        ]}
+        />
       </BoxContent>
       <Footer>
         <Link>
