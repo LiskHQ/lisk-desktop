01ea65c2f5fe724a45f1565beb63f6ecc37e02de
iris salcedo <iris.salcedo@lightcurve.io>
2022-07-06 13:49:08 +0200

    Add box layout

--- src/modules/blockchainApplication/manage/components/ApplicationManagementList/index.js
+++ src/modules/blockchainApplication/manage/components/ApplicationManagementList/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import Box from 'src/theme/box';
+import BoxHeader from 'src/theme/box/header';
+import BoxContent from 'src/theme/box/content';
+import Footer from 'src/theme/box/footer';
 import styles from './applicationManagementList.css';
 
-const ApplicationManagementList = () => (
-  <div className={styles.wrapper}>
-    TODO
+const ApplicationListItem = ({ key, app }) => (
+  <div key={key}>
+    {app.name}
   </div>
 );
+
+const ApplicationList = ({ apps }) => (
+  <div>
+    {apps.map((app, index) => <ApplicationListItem app={app} key={`ApplicationListItem-${index}`} />)}
+  </div>
+);
+
+const ApplicationManagementList = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box className={styles.wrapper}>
+      <BoxHeader>
+        <h1>{t('Applications')}</h1>
+      </BoxHeader>
+      <BoxContent className={styles.chartBox}>
+        <ApplicationList apps={[{ name: 'LISK' }, { name: 'Enevti' }, { name: 'DoEdu' }]} />
+      </BoxContent>
+      <Footer>
+        <Link>
+          {t('Add application')}
+        </Link>
+      </Footer>
+    </Box>
+  );
+};
 
 export default ApplicationManagementList;
