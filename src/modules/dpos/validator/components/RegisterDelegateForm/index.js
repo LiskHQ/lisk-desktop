0b50c4928ef672e888cf0e91ef7002f8bec91fd3
Ali Haghighatkhah <ali@lightcurve.io>
2022-07-06 17:18:09 +0200

    Add new fields to tx form

--- src/modules/dpos/validator/components/RegisterDelegateForm/index.js
+++ src/modules/dpos/validator/components/RegisterDelegateForm/index.js
@@ -1,82 +1,51 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { regex } from 'src/const/regex';
 import { MODULE_ASSETS_NAME_ID_MAP } from '@transaction/configuration/moduleAssets';
-import { getDelegate } from '@dpos/validator/api';
 import BoxHeader from 'src/theme/box/header';
 import BoxContent from 'src/theme/box/content';
 import { Input } from 'src/theme';
-import Tooltip from 'src/theme/Tooltip';
 import TxComposer from '@transaction/components/TxComposer';
+import useDelegateName from '../../hooks/useDelegateName';
+import useDelegateKey from '../../hooks/useDelegateKey';
+import InputLabel from './InputLabel';
 import styles from './form.css';
 
-const validateUsername = (query, t) => {
-  if (query.length === 0) {
-    return t('Username can not be empty.');
-  }
-  if (query.length < 2) {
-    return t('Username is too short.');
-  }
-  if (query.length > 20) {
-    return t('Username is too long.');
-  }
-  const hasInvalidChars = query.replace(regex.delegateSpecialChars, '');
-  if (hasInvalidChars) {
-    return t(`Invalid character ${hasInvalidChars.trim()}`);
-  }
-  return '';
-};
-
-// eslint-disable-next-line max-statements
 const RegisterDelegateForm = ({
   nextStep,
   prevState,
 }) => {
-  const timeout = useRef();
   const { t } = useTranslation();
-  const network = useSelector(state => state.network);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [username, setUsername] = useState(prevState?.rawTx?.asset.username ?? '');
+  const [name, setName] = useDelegateName(prevState?.rawTx?.asset.username);
+  const [generatorPublicKey, blsPublicKey, proofOfPossession, setKey] = useDelegateKey(
+    prevState?.rawTx?.asset.generatorPublicKey,
+    prevState?.rawTx?.asset.blsPublicKey,
+    prevState?.rawTx?.asset.proofOfPossession,
+  );
 
   const onConfirm = (rawTx) => {
     nextStep({ rawTx });
   };
 
-  const checkUsername = () => {
-    clearTimeout(timeout.current);
-
-    timeout.current = setTimeout(() => {
-      setLoading(true);
-      getDelegate({ network, params: { username } })
-        .then((response) => {
-          setLoading(false);
-          if (response.data.length) {
-            setError(t('"{{username}}" is already taken.', { username }));
-          }
-        })
-        .catch(() => setLoading(false));
-    }, 1000);
-  };
-
   const onChangeUsername = ({ target: { value } }) => {
-    setError(validateUsername(value, t));
-    setUsername(value);
+    setName({
+      ...name,
+      value,
+    });
   };
 
-  useEffect(() => {
-    if (!error && username) {
-      checkUsername();
-    }
-  }, [username]);
+  const onKeyChange = ({ target }) => {
+    setKey(target.name, target.value);
+  };
 
   const transaction = {
     moduleAssetId: MODULE_ASSETS_NAME_ID_MAP.registerDelegate,
     asset: {
-      username,
+      username: name.value,
+      blsPublicKey,
+      generatorPublicKey,
+      proofOfPossession,
     },
-    isValid: !error && username.length > 0 && !loading,
+    isValid: !(name.error || name.loading),
   };
 
   return (
@@ -90,39 +59,68 @@ const RegisterDelegateForm = ({
             <h2>{t('Register delegate')}</h2>
           </BoxHeader>
           <BoxContent className={`${styles.container} select-name-container`}>
-            <p className={`${styles.description} select-name-text-description`}>
-              {t(
-                'Register as a delegate to assign a username and allow votes to be locked to your account.',
-              )}
-            </p>
-            <p className={`${styles.description} select-name-text-description`}>
-              {t(
-                'Depending on the number of votes locked to your account (delegate weight), your account can become eligible to forge new blocks on the Lisk blockchain. With every new round (103 blocks), the top 101 active delegates and 2 randomly selected standby delegates each become eligible to forge a new block. For each block forged and accepted by the Lisk network, a delegate receives a new block reward and the transaction fees collected from each sender. The minimum required delegate weight to become eligible is 1000 LSK.',
-              )}
-            </p>
-            <label className={styles.usernameLabel}>
-              {t('Your username')}
-              <Tooltip position="right">
-                <p>
-                  {t(
-                    'Max. 20 characters, a-z, 0-1, no special characters except !@$_.',
-                  )}
-                </p>
-              </Tooltip>
-            </label>
+            <InputLabel field="name" />
             <div className={styles.inputContainer}>
               <Input
                 data-name="delegate-username"
                 autoComplete="off"
                 onChange={onChangeUsername}
                 name="delegate-username"
-                value={username}
+                value={name.value}
                 placeholder={t('e.g. peter_pan')}
-                className={`${styles.inputUsername} select-name-input`}
-                error={error}
-                isLoading={loading}
-                status={error ? 'error' : 'ok'}
-                feedback={error}
+                className={`${styles.input} select-name-input`}
+                error={name.error && name.value.length}
+                isLoading={name.loading}
+                status={name.error && name.value.length ? 'error' : 'ok'}
+                feedback={name.message}
+              />
+            </div>
+
+            <InputLabel field="generatorPublicKey" />
+            <div className={styles.inputContainer}>
+              <Input
+                data-name="generator-publicKey"
+                autoComplete="off"
+                onChange={onKeyChange}
+                name="generatorPublicKey"
+                value={generatorPublicKey.value}
+                placeholder={t('A string value')}
+                className={`${styles.input} generator-publicKey-input`}
+                error={generatorPublicKey.error}
+                status={generatorPublicKey.error ? 'error' : 'ok'}
+                feedback={generatorPublicKey.message}
+              />
+            </div>
+
+            <InputLabel field="blsPublicKey" />
+            <div className={styles.inputContainer}>
+              <Input
+                data-name="bls-key"
+                autoComplete="off"
+                onChange={onKeyChange}
+                name="blsPublicKey"
+                value={blsPublicKey.value}
+                placeholder={t('A string value')}
+                className={`${styles.input} bls-key-input`}
+                error={blsPublicKey.error}
+                status={blsPublicKey.error ? 'error' : 'ok'}
+                feedback={blsPublicKey.message}
+              />
+            </div>
+
+            <InputLabel field="proofOfPossession" />
+            <div className={styles.inputContainer}>
+              <Input
+                data-name="pop"
+                autoComplete="off"
+                onChange={onKeyChange}
+                name="proofOfPossession"
+                value={proofOfPossession.value}
+                placeholder={t('A string value')}
+                className={`${styles.input} pop-input`}
+                error={proofOfPossession.error}
+                status={proofOfPossession.error ? 'error' : 'ok'}
+                feedback={proofOfPossession.message}
               />
             </div>
           </BoxContent>
