40695e3198ba548ab92397034cc6caad5cbed21d
eniolam1000752 <eniolam1000752@gmail.com>
2022-08-01 13:45:58 +0100

    wip: unit tested MessageField

--- src/modules/wallet/components/MessageField/MessageField.test.js
+++ src/modules/wallet/components/MessageField/MessageField.test.js
@@ -3,16 +3,41 @@ import { fireEvent, screen, render } from '@testing-library/react';
 import MessageField from './MessageField';
 
 describe('MessageField', () => {
+  let wrapper;
   const props = {
+    onChange: jest.fn(),
+    value: 'test message',
+    maxMessageLength: 10,
+    label: 'label',
+    placeholder: 'placeholder',
   };
 
   beforeEach(() => {
     jest.clearAllMocks();
-    render(<MessageField {...props} />, props);
+    wrapper = render(<MessageField {...props} />);
   });
 
-  it('should render properly', () => {});
-  it('should be collapsed', () => {});
-  it('should not be collapsed', () => {});
-  it('should show the number of bytes left', () => {});
+  it('should be in shrink mode when its mounted', () => {
+    expect(screen.queryByText('Add message (Optional)')).toBeTruthy();
+    expect(screen.queryByText(props.label)).toBeFalsy();
+    expect(screen.queryByText('Remove')).toBeFalsy();
+    expect(screen.queryByAltText('removeBlueIcon')).toBeFalsy();
+  });
+
+  it('should be collapsed', () => {
+    fireEvent.click(screen.queryByText('Add message (Optional)'));
+
+    expect(screen.queryByText('Add message (Optional)')).toBeFalsy();
+    expect(screen.queryByText(props.label)).toBeTruthy();
+    expect(screen.queryByText('Remove')).toBeTruthy();
+    expect(screen.queryByAltText('removeBlueIcon')).toBeTruthy();
+  });
+
+  it('should show the byte counter', () => {
+    props.feedback = '1 byte left';
+    wrapper.rerender(<MessageField {...props} />);
+
+    fireEvent.click(screen.queryByText('Add message (Optional)'));
+    expect(screen.getByTestId('feedback').textContent).toContain(props.feedback);
+  });
 });
