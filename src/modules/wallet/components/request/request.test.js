fbb2683734023d6de37aed8031868d91d18654e8
eniolam1000752 <eniolam1000752@gmail.com>
2022-08-01 14:02:30 +0100

    wip: updated unit test on request

--- src/modules/wallet/components/request/request.test.js
+++ src/modules/wallet/components/request/request.test.js
@@ -31,11 +31,13 @@ describe('Request', () => {
     expect(wrapper.find(Request).state('shareLink')).toMatch(shareLink);
 
     evt = { target: { name: 'reference', value: 'test' } };
-    wrapper.find('.fieldGroup').at(1).find('AutoResizeTextarea').simulate('change', evt);
+    wrapper.find('.add-message-button').at(0).simulate('click');
+
+    wrapper.find('label textarea[placeholder="Write message"]').simulate('change', evt);
     expect(wrapper.find(Request).state('shareLink')).toMatch(`${shareLink}&${evt.target.name}=${evt.target.value}`);
 
     evt = { target: { name: 'amount', value: 1 } };
-    wrapper.find('.fieldGroup').at(0).find('input').simulate('change', evt);
+    wrapper.find('.fieldGroup').at(2).find('input').simulate('change', evt);
     expect(wrapper.find(Request).state('shareLink')).toMatch(`${shareLink}&${evt.target.name}=${evt.target.value}`);
   });
 });
