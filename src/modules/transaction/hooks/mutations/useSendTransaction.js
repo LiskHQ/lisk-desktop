b5c1b528eac262a05309e7f9d42c20fbbd50a3da
masoud soroush <masoud.s.geo@gmail.com>
2022-08-02 23:52:14 +0430

    feat: useMutation example

---
+++ src/modules/transaction/hooks/mutations/useSendTransaction.js
@@ -0,0 +1,29 @@
+import { useMutation } from '@tanstack/react-query';
+import { useCurrentApplication } from '@blockchainApplication/manage/hooks';
+import {
+  METHOD,
+  LIMIT as limit,
+  API_VERSION,
+  API_METHOD,
+} from 'src/const/config';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useSendTransaction = (options) => {
+  const [currentApplication] = useCurrentApplication();
+
+  return useMutation(async ({ path, customConfig }) => {
+    const config = {
+      baseUrl: currentApplication?.node[0][METHOD] ?? currentApplication?.node[0].rest,
+      path: `/api/${API_VERSION}/transactions/`,
+      method: 'POST',
+      event: 'get.network.status',
+      ...customConfig,
+      params: { limit, ...customConfig.params },
+    };
+    return API_METHOD[METHOD]({
+      path,
+      config,
+    });
+  },
+  options);
+};
