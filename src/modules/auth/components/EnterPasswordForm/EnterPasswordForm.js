04c710c1ae47dfe074ddd2aba288488ca7dc5074
eniolam1000752 <eniolam1000752@gmail.com>
2022-08-06 12:02:30 +0100

    wip: reactored enter password step

--- src/modules/auth/components/EnterPasswordForm/EnterPasswordForm.js
+++ src/modules/auth/components/EnterPasswordForm/EnterPasswordForm.js
@@ -3,27 +3,29 @@ import { useTranslation } from 'react-i18next';
 import { useForm } from 'react-hook-form';
 
 import WalletVisual from '@wallet/components/walletVisual';
-import { decryptAccount } from '@account/utils/encryptAccount';
+import { decryptAccount } from '@account/utils/decryptAccount';
+import { useCurrentAccount } from '@account/hooks';
 import { Input } from 'src/theme';
 import Box from 'src/theme/box';
 import BoxContent from 'src/theme/box/content';
 import { PrimaryButton } from 'src/theme/buttons';
 import styles from './EnterPasswordForm.css';
 
-const EnterPasswordForm = ({ encryptedAccount, onEnterPasswordSuccess }) => {
+const EnterPasswordForm = ({ onEnterPasswordSuccess }) => {
   const { t } = useTranslation();
   const {
     register,
     watch,
     handleSubmit,
   } = useForm();
+  const [currentAccount] = useCurrentAccount();
   const [feedbackError, setFeedbackError] = useState('');
 
   const formValues = watch();
 
   const onSubmit = async ({ password }) => {
     const { error, result } = await decryptAccount(
-      encryptedAccount.encryptedPassphrase, password,
+      currentAccount.encryptedPassphrase, password,
     );
 
     if (error) {
@@ -33,7 +35,6 @@ const EnterPasswordForm = ({ encryptedAccount, onEnterPasswordSuccess }) => {
 
     return onEnterPasswordSuccess({
       recoveryPhrase: result.recoveryPhrase,
-      encryptedAccount,
     });
   };
 
@@ -42,16 +43,16 @@ const EnterPasswordForm = ({ encryptedAccount, onEnterPasswordSuccess }) => {
       <BoxContent className={styles.content}>
         <h1>{t('Enter your password')}</h1>
         <p className={styles.subheader}>
-          {t('Please provide your device password to backup the recovery phrase.')}
+          {t('Please provide your device password to sign a transaction.')}
         </p>
         <WalletVisual
           className={styles.avatar}
-          address={encryptedAccount?.metadata?.address}
+          address={currentAccount?.metadata?.address}
         />
-        {encryptedAccount?.metadata?.name && (
-          <p className={styles.accountName}>{encryptedAccount?.metadata?.name}</p>
+        {currentAccount?.metadata?.name && (
+          <p className={styles.accountName}>{currentAccount?.metadata?.name}</p>
         )}
-        <p className={styles.accountAddress}>{encryptedAccount?.metadata?.address}</p>
+        <p className={styles.accountAddress}>{currentAccount?.metadata?.address}</p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Input
             secureTextEntry
