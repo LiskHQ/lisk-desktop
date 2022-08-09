470feefe20111e966c19a13da7490dcefeee0629
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-09 17:18:58 +0100

    Refactored tx connection methods and created useTransactions hook

---
+++ src/modules/transaction/hooks/queries/useTransactions.js
@@ -0,0 +1,54 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useCurrentApplication } from '@blockchainApplication/manage/hooks';
+import { TRANSACTIONS, APPLICATION } from 'src/const/queries';
+import {
+  METHOD,
+  LIMIT as limit,
+  API_VERSION,
+  API_METHOD,
+} from 'src/const/config';
+import { wsMethods } from '../../configuration';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useTransactions = ({ config: customConfig = {}, options } = { }) => {
+  const [currentApplication] = useCurrentApplication();
+  const config = {
+    baseURL: currentApplication?.apis[0][METHOD] ?? currentApplication?.apis[0].rest,
+    path: `/api/${API_VERSION}/transactions`,
+    event: wsMethods.transactionsChange,
+    ...customConfig,
+    params: { limit, ...customConfig.params },
+  };
+  const result = useInfiniteQuery(
+    [TRANSACTIONS, APPLICATION, METHOD, config],
+    async ({ pageParam }) => API_METHOD[METHOD]({
+      ...config,
+      params: {
+        ...(config.params || {}),
+        ...pageParam,
+      },
+    }),
+    {
+      ...options,
+      select: (data) => data.pages.reduce((prevPages, page) => ({
+        ...page,
+        data: prevPages?.data ? [...prevPages.data, ...page?.data] : page?.data,
+      }), {}),
+      getNextPageParam: (lastPage) => {
+        const offset = lastPage.meta.count + lastPage.meta.offset;
+        const hasMore = offset < lastPage.meta.total;
+        return !hasMore ? undefined : { offset };
+      },
+      placeholderData: {
+        data: [],
+        pages: [],
+        pageParams: [],
+      },
+    },
+  );
+  return {
+    ...result,
+    hasUpdate: false,
+    addUpdate: () => undefined,
+  };
+};
