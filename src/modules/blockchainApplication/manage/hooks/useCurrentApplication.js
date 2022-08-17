a6f09728e3923abfc26470e59a854a1e6f2620e8
masoud soroush <masoud.s.geo@gmail.com>
2022-08-17 13:58:06 +0200

    feat: add socket test coverage

--- src/modules/blockchainApplication/manage/hooks/useCurrentApplication.js
+++ src/modules/blockchainApplication/manage/hooks/useCurrentApplication.js
@@ -15,8 +15,8 @@ export function useCurrentApplication() {
     (application) => {
       dispatch(setCurrentApplication(application));
       /* istanbul ignore next */
-      socket.create({ baseUrl: (application?.apis[0]?.rpc ?? application?.apis[0]?.rest) || '' });
-      setCurrentNode(application.apis[0]);
+      socket.create({ baseUrl: application?.apis[0]?.rpc ?? application?.apis[0]?.rest});
+      setCurrentNode(application?.apis[0]);
     },
     [],
   );
