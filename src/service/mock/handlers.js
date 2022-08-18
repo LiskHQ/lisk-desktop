8abd8556c857c04175088663b2c71bcc7a655aa1
masoud soroush <masoud.s.geo@gmail.com>
2022-08-18 17:12:28 +0200

    chore: mock websocket api call

--- src/service/mock/handlers.js
+++ src/service/mock/handlers.js
@@ -1,7 +1,9 @@
+import * as common from 'src/modules/common/mocks';
 import * as blocks from '@block/mocks';
 import * as network from '@network/mocks';
 
 export default [
+  ...Object.values(common),
   ...Object.values(blocks),
   ...Object.values(network),
 ];
