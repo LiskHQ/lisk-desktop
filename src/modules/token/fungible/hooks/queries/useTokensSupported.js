5aa6ee82292b535628d9e7b604a02a4b4fd5d3ed
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-19 15:16:10 +0100

    Added tokens supported query and related mocks

---
+++ src/modules/token/fungible/hooks/queries/useTokensSupported.js
@@ -0,0 +1,21 @@
+/* istanbul ignore file */
+import { TOKENS_TOP_LSK_BALANCE, APPLICATION } from 'src/const/queries';
+import {
+  METHOD,
+  LIMIT as limit,
+  API_VERSION,
+} from 'src/const/config';
+import { useCustomInfiniteQuery } from 'src/modules/common/hooks/queries';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useTokensSupported = ({ config: customConfig = {}, options } = {}) => {
+  const config = {
+    url: `/api/${API_VERSION}/tokens/lsk/top`,
+    method: 'get',
+    event: 'get.tokens.supported',
+    ...customConfig,
+    params: { limit, ...(customConfig?.params || {}) },
+  };
+  const keys = [TOKENS_TOP_LSK_BALANCE, APPLICATION, METHOD, config];
+  return useCustomInfiniteQuery({ config, options, keys });
+};
