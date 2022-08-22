da50a1161f9af00477f62b9b6e46f441ee6523f0
masoud soroush <masoud.s.geo@gmail.com>
2022-08-22 13:37:22 +0200

    fixup! feat: Add newsfeed query

--- src/modules/common/hooks/queries/useNewsFeed.test.js
+++ src/modules/common/hooks/queries/useNewsFeed.test.js
@@ -1,13 +1,12 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { queryWrapper as wrapper } from 'src/utils/test/queryWrapper';
-import { LIMIT as defaultLimit } from 'src/const/config';
 import { mockNewsFeed } from '../../__fixtures__';
 import { useNewsFeed } from './useNewsFeed';
 
 jest.useRealTimers();
 
 describe('useNewsFeed hook', () => {
-  const limit = 15;
+  const limit = 5;
   const config = { params: { limit } };
 
   it('fetching data correctly', async () => {
@@ -28,7 +27,6 @@ describe('useNewsFeed hook', () => {
 
   it('should fetch next set of data correctly', async () => {
     const { result, waitFor } = renderHook(() => useNewsFeed({ config }), { wrapper });
-    // expect(result.current.isLoading).toBeTruthy();
     await waitFor(() => result.current.isFetched);
     act(() => {
       result.current.fetchNextPage();
@@ -38,32 +36,13 @@ describe('useNewsFeed hook', () => {
     const expectedResponse = {
       data: mockNewsFeed.data.slice(0, limit * 2),
       meta: {
+        ...mockNewsFeed.meta,
         count: limit,
         offset: limit,
-        total: 30,
       },
     };
     expect(result.current.data).toEqual(expectedResponse);
-    expect(result.current.hasNextPage).toBeFalsy();
-    act(() => {
-      result.current.fetchNextPage();
-    });
-    expect(result.current.hasNextPage).toBeFalsy();
+    expect(result.current.hasNextPage).toBeTruthy();
   });
 
-  it.skip('fetches data without params correctly', async () => {
-    const { result, waitFor } = renderHook(() => useNewsFeed({ config }), { wrapper });
-    await waitFor(() => result.current.isFetched);
-    expect(result.current.isSuccess).toBeTruthy();
-    const expectedResponse = {
-      data: mockNewsFeed.data.slice(0, defaultLimit),
-      meta: {
-        ...mockNewsFeed.meta,
-        count: defaultLimit,
-        offset: 0,
-      },
-    };
-
-    expect(result.current.data).toEqual(expectedResponse);
-  });
 });
