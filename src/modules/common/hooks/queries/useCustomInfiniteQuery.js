48a2f16503fd5dd35eeeb176c9cbccd707d5b9e1
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-23 14:34:13 +0100

    fixup! feat: implement usePeers hook

--- src/modules/common/hooks/queries/useCustomInfiniteQuery.js
+++ src/modules/common/hooks/queries/useCustomInfiniteQuery.js
@@ -5,6 +5,20 @@ import {
   API_METHOD,
 } from 'src/const/config';
 
+/**
+ * Creates a custom hook for inifinite queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {string[]} configuration.keys - the query keys
+ * @param {Object} configuration.config - the query config
+ * @param {Object} configuration.config.params - the query config params
+ * @param {number} configuration.config.params.limit - the query limit
+ * @param {number} configuration.config.params.offset - the query offset
+ * @param {string} configuration.config.params.sort - the query sort
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useCustomInfiniteQuery = ({
   keys = [],
