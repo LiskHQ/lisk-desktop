8cb48f559a9e59e7c2b8df27b7272bb44983ec72
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 03:43:15 +0100

    Added application statistics hook documentation

--- src/modules/blockchainApplication/explore/hooks/queries/useBlockchainApplicationStatistics.js
+++ src/modules/blockchainApplication/explore/hooks/queries/useBlockchainApplicationStatistics.js
@@ -6,11 +6,21 @@ import {
   API_METHOD,
 } from 'src/const/config';
 
+/**
+ * Creates a custom hook for blockchain applications list queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {object} configuration.config - the query config
+ * @param {object} configuration.config.params - the query config params
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useBlockchainApplicationStatistics = ({
   config: customConfig = {},
   options,
-} = { }) => {
+} = {}) => {
   const config = {
     url: `/api/${API_VERSION}/blockchain/apps/statistics`,
     method: 'get',
