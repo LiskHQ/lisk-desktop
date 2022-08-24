9fc9492604e3407d519fe7de7d2500646011908e
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 02:47:24 +0100

    fixup! Added market prices hook, related mocks and minor changes

--- src/modules/common/hooks/queries/useMarketPrices.js
+++ src/modules/common/hooks/queries/useMarketPrices.js
@@ -7,6 +7,15 @@ import {
   API_METHOD,
 } from 'src/const/config';
 
+/**
+ * Creates a custom hook for market prices queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {Object} configuration.config - the query config
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useMarketPrices = ({ config: customConfig = {}, options } = {}) => {
   const config = {
