98023b863376dc15f97f5fbf2dedf2742c704ee4
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 02:38:23 +0100

    fixup! Added ccm query, related mocks and minor changes

--- src/modules/common/hooks/queries/useCcm.js
+++ src/modules/common/hooks/queries/useCcm.js
@@ -6,6 +6,27 @@ import {
 import { CCM, APPLICATION } from 'src/const/queries';
 import { useCustomInfiniteQuery } from './useCustomInfiniteQuery';
 
+/**
+ * Creates a custom hook for ccm queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {Object} configuration.config - the query config
+ * @param {Object} configuration.config.params - the query config params
+ * @param {number} configuration.config.params.limit - the query limit
+ * @param {number} [configuration.config.params.offset] - the query offset
+ * @param {number} [configuration.config.params.sort] - the query sort
+ * @param {string} [configuration.config.params.id] - CCM ID
+ * @param {string} [configuration.config.params.transactionID] - CCM transactionID
+ * @param {string} [configuration.config.params.moduleCrossChainCommandID] - CCM transaction type
+ * @param {string} [configuration.config.params.moduleCrossChainCommandName] - CCM transaction name
+ * @param {string} [configuration.config.params.senderAddress] - CCM sender
+ * @param {string} [configuration.config.params.status] - CCM status
+ * @param {string} [configuration.config.params.timestamp] - CCM timestamp
+ * @param {string} [configuration.config.params.nonce] - CCM nonce used with sender address
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useCcm = ({ config: customConfig = {}, options } = { }) => {
   const config = {
