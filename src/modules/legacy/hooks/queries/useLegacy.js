7cf27bb498baff561dfdfb8b28abfc66b46d323c
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 04:29:20 +0100

    Added legacy account hook documentation

--- src/modules/legacy/hooks/queries/useLegacy.js
+++ src/modules/legacy/hooks/queries/useLegacy.js
@@ -8,6 +8,17 @@ import {
   API_METHOD,
 } from 'src/const/config';
 
+/**
+ * Creates a custom hook for legacy account queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {object} configuration.config - the query config
+ * @param {object} configuration.config.params - the query config params
+ * @param {string} configuration.config.params.publicKey - account public key
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useLegacy = ({ config: customConfig = {}, options } = {}) => {
   const [currentApplication] = useCurrentApplication();
