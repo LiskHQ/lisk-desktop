c461eb9334281553bbb6c62c223c6d0074f06c81
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 04:25:46 +0100

    Added votes unlocks hook documentation

--- src/modules/dpos/validator/hooks/queries/useUnlocks.js
+++ src/modules/dpos/validator/hooks/queries/useUnlocks.js
@@ -7,6 +7,21 @@ import {
 } from 'src/const/config';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
+/**
+ * Creates a custom hook for votes unlocked queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {object} configuration.config - the query config
+ * @param {object} configuration.config.params - the query config params
+ * @param {number} [configuration.config.params.limit] - the query limit
+ * @param {number} [configuration.config.params.offset] - the query offset
+ * @param {string} [configuration.config.params.address] - account address
+ * @param {string} [configuration.config.params.publicKey] - account public key
+ * @param {string} [configuration.config.params.name] - account name
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useUnlocks = ({ config: customConfig = {}, options } = { }) => {
   const config = {
