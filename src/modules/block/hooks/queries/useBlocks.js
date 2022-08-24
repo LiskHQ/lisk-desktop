9eceb44a714589a94ba100372176717c2c31e2fa
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 03:23:37 +0100

    fixup! feat: implement usePeers hook

--- src/modules/block/hooks/queries/useBlocks.js
+++ src/modules/block/hooks/queries/useBlocks.js
@@ -6,6 +6,23 @@ import {
 } from 'src/const/config';
 import { useCustomInfiniteQuery } from 'src/modules/common/hooks/queries';
 
+/**
+ * Creates a custom hook for block queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {Object} configuration.config - the query config
+ * @param {Object} configuration.config.params - the query config params
+ * @param {number} [configuration.config.params.limit] - the query limit
+ * @param {number} [configuration.config.params.offset] - the query offset
+ * @param {string} [configuration.config.params.sort] - the query sort
+ * @param {string} [configuration.config.params.blockID] - block ID
+ * @param {string} [configuration.config.params.height] - block height
+ * @param {string} [configuration.config.params.timestamp] - block timestamp
+ * @param {string} [configuration.config.params.generatorAddress] - block generator address
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useBlocks = ({ config: customConfig = {}, options } = { }) => {
   const config = {
