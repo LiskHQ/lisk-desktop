47b7c84256fa005d166d229ccf77482441221b97
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-24 03:58:51 +0100

    Added delegate hook documentation

--- src/modules/dpos/validator/hooks/queries/useDelegates.js
+++ src/modules/dpos/validator/hooks/queries/useDelegates.js
@@ -6,6 +6,21 @@ import {
 } from 'src/const/config';
 import { useCustomInfiniteQuery } from 'src/modules/common/hooks/queries';
 
+/**
+ * Creates a custom hook for delegates queries
+ *
+ * @param {object} configuration - the custom query configuration object
+ * @param {object} configuration.config - the query config
+ * @param {object} configuration.config.params - the query config params
+ * @param {number} [configuration.config.params.limit] - the query limit
+ * @param {number} [configuration.config.params.offset] - the query offset
+ * @param {string} [configuration.config.params.address] - Delegate sender
+ * @param {string} [configuration.config.params.name] - Delegate name
+ * @param {string} [configuration.config.params.status] - Delegate status
+ * @param {string} configuration.options - the query options
+ *
+ * @returns the query object
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const useDelegates = ({ config: customConfig = {}, options } = { }) => {
   const config = {
