db7bb142096e0101062aa7bd896af39f654f9140
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-08-26 12:53:41 +0100

    DeepScan fix

--- src/modules/block/components/blockDetails/blockDetails.js
+++ src/modules/block/components/blockDetails/blockDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import routes from 'src/routes/routes';
