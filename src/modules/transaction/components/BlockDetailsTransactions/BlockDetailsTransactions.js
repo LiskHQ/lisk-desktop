34624c0f0d2bcf620fd238d96cec989e2653be4b
eniolam1000752 <eniolam1000752@gmail.com>
2022-09-01 01:46:55 +0100

    wip: fixed text on block details section

--- src/modules/transaction/components/BlockDetailsTransactions/BlockDetailsTransactions.js
+++ src/modules/transaction/components/BlockDetailsTransactions/BlockDetailsTransactions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import {
   selectCurrentBlockHeight,
@@ -19,29 +19,23 @@ const BlockDetailsTransactions = ({
 }) => {
   const currentBlockHeight = useSelector(selectCurrentBlockHeight);
   const activeToken = useSelector(selectActiveToken);
-  const [config, setConfig] = useState({ params: {} });
   const {
     data: transactions, isLoading, isFetching, error,
-  } = useTransactions({ config });
-  useEffect(() => {
-    // Ensure query supports both ID and height
-    if (blockId && !height) {
-      setConfig({ params: { blockID: blockId } });
-    }
-    if (!blockId && height) {
-      setConfig({ params: { height } });
-    }
-    if (blockId && height) {
-      setConfig({ params: { blockID: blockId, height } });
-    }
-  }, [blockId, height]);
+  } = useTransactions({
+    config: {
+      params: {
+        ...blockId && { blockID: blockId },
+        ...height && { height },
+      },
+    },
+  });
 
   return (
     <Box main isLoading={isLoading} className="transactions-box">
       <BoxContent className={`${styles.content} transaction-results`}>
         <Table
           showHeader
-          data={transactions?.data.reduce((acc, item) => new Array(10).fill(item), []) || []}
+          data={transactions?.data || []}
           isLoading={isFetching}
           row={TransactionRow}
           additionalRowProps={{
