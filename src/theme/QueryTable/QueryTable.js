079b86035f652269985c998d49ee1eea4d6b44e8
masoud soroush <masoud.s.geo@gmail.com>
2022-09-01 10:28:41 +0200

    feat: init QueryTable

---
+++ src/theme/QueryTable/QueryTable.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Table from '../table';
+
+// eslint-disable-next-line import/prefer-default-export
+export const QueryTable = ({ queryHook, queryConfig, ...props }) => {
+  const {
+    data: response,
+    error,
+    isLoading,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+  } = queryHook(queryConfig);
+  return (
+    <Table
+      data={response?.data || []}
+      isLoading={isLoading}
+      isFetching={isFetching}
+      loadData={fetchNextPage}
+      canLoadMore={hasNextPage}
+      error={error}
+      {...props}
+    />
+  );
+};
