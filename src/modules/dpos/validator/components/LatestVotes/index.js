f66332721a52c8d3e419c29e671e40a7517bebf4
eniolam1000752 <eniolam1000752@gmail.com>
2022-09-13 18:28:26 +0100

    fix: resolved lint issues

--- src/modules/dpos/validator/components/LatestVotes/index.js
+++ src/modules/dpos/validator/components/LatestVotes/index.js
@@ -2,7 +2,7 @@ import React, { useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { QueryTable } from 'src/theme/QueryTable';
 import { useTransactions } from 'src/modules/transaction/hooks/queries';
-import { MODULE_COMMANDS_NAME_ID_MAP } from 'src/modules/transaction/configuration/moduleAssets';
+import { MODULE_COMMANDS_NAME_MAP } from 'src/modules/transaction/configuration/moduleCommand';
 import TransactionRow from '@transaction/components/TransactionRow';
 import header from './tableHeader';
 import { useDelegates } from '../../hooks/queries';
@@ -15,7 +15,7 @@ const LatestVotes = ({ filters }) => {
     config: {
       params: {
         ...filters,
-        moduleCommandID: MODULE_COMMANDS_NAME_ID_MAP.voteDelegate,
+        moduleCommandID: MODULE_COMMANDS_NAME_MAP.voteDelegate,
         sort: 'timestamp:desc',
       },
     },
