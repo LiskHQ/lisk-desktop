25b9216762cfe571a1def20014a6831d99c07d1b
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-09-20 04:31:27 +0100

    Updated table header, row and skeleton

--- src/modules/common/components/skeleton/Skeleton.js
+++ src/modules/common/components/skeleton/Skeleton.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './Skeleton.css';
 
-const Skeleton = ({
-  radius = 20, width = '50%', height = 15, right = false, theme = 'rect',
-}) => (
-  <div
-    data-testid="skeleton-wrapper"
-    className={`${styles.skeletonWrapper} ${styles[theme]}`}
-    style={{
-      width: theme === 'circle' ? 2 * radius : width,
-      height: theme === 'circle' ? 2 * radius : height,
-      textAlign: right ? 'right' : 'left',
-    }}
-  />
+const Skeleton = ({ radius = 20, width = '50%', height = 15, right = false, theme = 'rect' }) => (
+  <>
+    <div
+      data-testid="skeleton-wrapper"
+      className={`${styles.skeletonWrapper} ${styles[theme]}`}
+      style={{
+        width: theme === 'circle' ? 2 * radius : width,
+        height: theme === 'circle' ? 2 * radius : height,
+        textAlign: right ? 'right' : 'left',
+      }}
+    />
+    {theme === 'walletWithAddress' && <div className={styles.skeletonWrapper} />}
+  </>
 );
 
 export default Skeleton;
--- src/modules/transaction/components/TransactionMonitor/TransactionHeaderMap.js
+++ src/modules/transaction/components/TransactionMonitor/TransactionHeaderMap.js
@@ -1,39 +1,39 @@
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import styles from './transactionsTable.css';
 
-export default (changeSort, t) => ([
+export default (changeSort, t) => [
+  {
+    title: t('ID'),
+    classList: grid['col-xs-3'],
+    placeholder: 'walletWithAddress',
+  },
   {
     title: t('Sender'),
     classList: grid['col-xs-3'],
-    placeholder: 'circle',
+    placeholder: 'walletWithAddress',
   },
   {
-    title: t('Recipient'),
-    classList: grid['col-xs-3'],
-    placeholder: 'circle',
+    title: t('Height'),
+    classList: `${grid['col-xs-2']} ${styles.transactionFeeCell}`,
+    sort: {
+      fn: changeSort,
+      key: 'height',
+    },
+  },
+  {
+    title: t('Type'),
+    classList: `${grid['col-xs-3']} ${grid['col-md-2']}`,
   },
   {
     title: t('Date'),
-    classList: grid['col-xs-2'],
+    classList: grid['col-md-1'],
     sort: {
       fn: changeSort,
       key: 'timestamp',
     },
   },
   {
-    title: t('Amount'),
-    classList: `${grid['col-xs-3']} ${grid['col-md-2']}`,
-    sort: {
-      fn: changeSort,
-      key: 'amount',
-    },
-  },
-  {
-    title: t('Fee'),
-    classList: `${grid['col-md-1']} ${styles.transactionFeeCell}`,
-  },
-  {
     title: t('Status'),
     classList: grid['col-xs-1'],
   },
-]);
+];
