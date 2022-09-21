4163b2b2069e7853e23b6ccc42cf24a093064a5b
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-09-21 13:44:49 +0100

    Fixed coverage issue

--- src/modules/common/components/skeleton/Skeleton.js
+++ src/modules/common/components/skeleton/Skeleton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Skeleton.css';
 
-const Skeleton = ({ radius = 20, width = '50%', height = 15, right = false, theme = 'rect' }) => (
+const Skeleton = ({ radius = 20, width = '50%', height = 15, theme = 'rect' }) => (
   <>
     <div
       data-testid="skeleton-wrapper"
@@ -9,10 +9,11 @@ const Skeleton = ({ radius = 20, width = '50%', height = 15, right = false, them
       style={{
         width: theme === 'circle' ? 2 * radius : width,
         height: theme === 'circle' ? 2 * radius : height,
-        textAlign: right ? 'right' : 'left',
       }}
     />
-    {theme === 'walletWithAddress' && <div className={styles.skeletonWrapper} />}
+    {theme === 'walletWithAddress' && (
+      <div data-testid="wallet-address-skeleton-wrapper" className={styles.skeletonWrapper} />
+    )}
   </>
 );
 
