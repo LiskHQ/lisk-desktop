46721a3993b5756015744e66cce885010d4d4ee3
eniolam1000752 <eniolam1000752@gmail.com>
2022-10-18 22:48:14 +0100

    fix: reverted base url link on http function

--- src/utils/api/http.js
+++ src/utils/api/http.js
@@ -20,8 +20,7 @@ const http = ({
     // @todo remove the optional chain
     
     const url = new URL(baseUrl ? `${baseUrl}${path}`
-    // @Todo: this should be reverted when stable test service depoyment has been done
-      : `${network?.networks?.LSK?.serviceUrl ?? 'http://165.22.29.229:9901'}${path}`);
+    : `${network?.networks?.LSK?.serviceUrl ?? 'https://testnet-service.lisk.com'}${path}`);
     url.search = new URLSearchParams(params).toString();
 
     return fetch(url.toString(), {
