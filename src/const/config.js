db7fcd5c312a5d9c95cc003d7f4f65839c46d119
masoud soroush <masoud.s.geo@gmail.com>
2022-10-18 22:56:48 +0200

    feat: support event endpoint and expose metadataClient

--- src/const/config.js
+++ src/const/config.js
@@ -2,3 +2,4 @@ export const METHOD = 'rest';
 export const LIMIT = 20;
 export const API_VERSION = 'v3';
 export const MOCK_SERVICE_WORKER = process.env.REACT_APP_MSW;
+export const META_BASE_URL = 'http://165.227.246.146:9901';
--- src/utils/api/client.js
+++ src/utils/api/client.js
@@ -1,43 +1,63 @@
 /* istanbul ignore file */
 import io from 'socket.io-client';
 import axios from 'axios';
-import { METHOD } from 'src/const/config';
+import { META_BASE_URL, METHOD } from 'src/const/config';
 
 export class Client {
-  socket = null;
+  constructor(option) {
+    if (option) {
+      this.create(option);
+    }
+  }
 
   axiosConfig = {
     timeout: 10000,
   };
 
+  socket = null;
+
+  socketRPC = null;
+
   http = null;
 
   rpc = ({ event, params, data }) =>
     new Promise((resolve, reject) => {
-      if (this.socket.disconnected) {
+      if (this.socketRPC.disconnected) {
         reject(new Error('socket not connected'));
         return;
       }
-      this.socket.emit('request', { method: event, params: params || data || {} }, (response) => {
-        if (Object.keys(response).length && response.error) {
-          return reject(response);
+      this.socketRPC.emit(
+        'request',
+        { method: event, params: params || data || {} },
+        (response) => {
+          if (Object.keys(response).length && response.error) {
+            return reject(response);
+          }
+          return resolve(response);
         }
-        return resolve(response);
-      });
+      );
     });
 
   rest = (config) => this.http?.request({ ...this.http.defaults, ...config });
 
-  call = ({ transformResult = async (data) => data, ...args }) => this[METHOD](args).then(transformResult);
+  call = ({ transformResult = async (data) => data, ...args }) =>
+    this[METHOD](args).then(transformResult);
 
   create({ rpc, rest }) {
-    this.socket = io(rpc, { transports: ['websocket'] });
-    const request = axios.create({
-      ...this.axiosConfig,
-      baseURL: rest,
-    });
-    request.interceptors.response.use((res) => res.data);
-    this.http = request;
+    if (rpc) {
+      this.socket = io(rpc, { transports: ['websocket'], path: 'blockchain' });
+      this.socketRPC = io(rpc, { transports: ['websocket'], path: 'rpc-v3' });
+    }
+    if (rest) {
+      const request = axios.create({
+        ...this.axiosConfig,
+        baseURL: rest,
+      });
+      request.interceptors.response.use((res) => res.data);
+      this.http = request;
+    }
   }
 }
+
+export const clientMetaData = new Client({ rest: META_BASE_URL });
 export default new Client();
