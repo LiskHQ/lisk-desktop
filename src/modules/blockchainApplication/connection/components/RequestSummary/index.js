c859b895f34adcba278d073632def630eb72eb1f
Ali Haghighatkhah <ali@lightcurve.io>
2022-10-21 09:07:14 +0200

    Update RequestSummary to work with wcm correctly

--- src/modules/blockchainApplication/connection/components/RequestSummary/index.js
+++ src/modules/blockchainApplication/connection/components/RequestSummary/index.js
@@ -3,14 +3,20 @@ import { useTranslation } from 'react-i18next';
 import ConnectionContext from '@libs/wcm/context/connectionContext';
 import ValueAndLabel from '@transaction/components/TransactionDetails/valueAndLabel';
 import AccountRow from '@account/components/AccountRow';
+import { useAccounts } from '@account/hooks/useAccounts';
+import { extractAddressFromPublicKey } from '@wallet/utils/account';
 import { rejectLiskRequest } from '@libs/wcm/utils/requestHandlers';
 import { SIGNING_METHODS } from '@libs/wcm/constants/permissions';
 import { EVENTS } from '@libs/wcm/constants/lifeCycle';
-import { elementTxToDesktopTx, convertTxJSONToBinary } from '@transaction/utils/transaction';
+import { elementTxToDesktopTx } from '@transaction/utils/transaction';
+import { decodeTransaction } from '@transaction/utils/encoding';
+import { fromRawLsk } from '@token/fungible/utils/lsk';
 import { joinModuleAndCommand } from '@transaction/utils/moduleCommand';
 import { Link } from 'react-router-dom';
 import Icon from 'src/theme/Icon';
 import useSession from '@libs/wcm/hooks/useSession';
+import { useSchemas } from '@transaction/hooks/queries/useSchemas';
+import { useDeprecatedAccount } from '@account/hooks/useDeprecatedAccount';
 import { PrimaryButton, SecondaryButton } from 'src/theme/buttons';
 import Box from 'src/theme/box';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
@@ -20,24 +26,41 @@ const getTitle = (key, t) =>
   Object.values(SIGNING_METHODS).find(item => item.key === key)?.title
     ?? t('Method not found');
 
+const getRequestTransaction = (request) => {
+  const { payload, schema } = request.request.params;
+  return decodeTransaction(Buffer.from(payload, 'hex'), schema);
+};
+
 // eslint-disable-next-line max-statements
 const RequestSummary = ({ nextStep }) => {
   const { t } = useTranslation();
+  const { getAccountByAddress } = useAccounts();
   const { events } = useContext(ConnectionContext);
   const [request, setRequest] = useState(null);
+  const [transaction, setTransaction] = useState(null);
+  const [senderAccount, setSenderAccount] = useState(null);
   const { session } = useSession();
+  useDeprecatedAccount(senderAccount);
+  useSchemas();
 
   const approveHandler = () => {
-    const tx = request.params.request.params.rawTx;
-    const moduleCommandID = joinModuleAndCommand({
-      moduleID: tx.moduleID,
-      commandID: tx.commandID,
+    const moduleCommand = joinModuleAndCommand({
+      moduleID: transaction.module,
+      commandID: transaction.command,
     });
-    const binaryTx = convertTxJSONToBinary(
-      tx, moduleCommandID,
-    );
-    const rawTx = elementTxToDesktopTx(binaryTx, moduleCommandID);
-    nextStep({ rawTx });
+    const rawTx = elementTxToDesktopTx(transaction, moduleCommand);
+    nextStep({
+      rawTx: {
+        ...rawTx,
+        sendingChain: { chainID: request.chainId.replace('lisk:', '') },
+        recipientChain: { chainID: request.recipientChainID },
+        // @todo Remove composedFees
+        composedFees: {
+          transaction: `${fromRawLsk(transaction.fee)} LSK`,
+        },
+      },
+      selectedPriority: { title: 'Normal', selectedIndex: 0, value: 0 },
+    });
   };
   const rejectHandler = () => {
     rejectLiskRequest(request);
@@ -45,22 +68,37 @@ const RequestSummary = ({ nextStep }) => {
 
   useEffect(() => {
     const event = events.find(e => e.name === EVENTS.SESSION_REQUEST);
-    if (event) {
-      setRequest(event.meta);
+    if (event?.meta?.params?.request?.params) {
+      setRequest(event.meta.params);
     }
   }, []);
+
+  useEffect(() => {
+    if (request && !transaction) {
+      const tx = getRequestTransaction(request);
+      setTransaction(tx);
+      const address = extractAddressFromPublicKey(tx.senderPublicKey);
+      const account = getAccountByAddress(address);
+      // @todo if account doesn't exist, we should inform the user that the tx can't be signed
+      setSenderAccount({
+        pubkey: account.metadata.pubkey,
+        address,
+        name: account.metadata.name,
+      });
+    }
+  }, [request]);
 
   if (!session.request || !request) {
     return <div />;
   }
 
   const { icons, name, url } = session.request.peer.metadata;
-  const { params: { chainId } } = request;
+  const { chainId } = request;
 
   return (
     <div className={`${styles.wrapper} ${grid.row} ${grid['center-xs']}`}>
       <div className={styles.avatarContainer}>
-        <h2>{getTitle(request.params.request.method, t)}</h2>
+        <h2>{getTitle(request.request.method, t)}</h2>
         <img
           data-testid="logo"
           src={icons[0]}
@@ -98,7 +136,7 @@ const RequestSummary = ({ nextStep }) => {
             label={t('Selected account')}
           >
             <AccountRow
-              account={{ metadata: { name: 'Lisker', address: 'lskj34x8zh85zh4khjq64ofudmjax2hzc5hxw7vok' } }}
+              account={{ metadata: { name: senderAccount?.name, address: senderAccount?.address } }}
               truncate
               onSelect={() => {}}
               showRemove={false}
