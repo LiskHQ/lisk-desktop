cdff037e639cd6869e151d3d35fccfd999f7ad69
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-10-24 16:24:58 +0100

    Minor fixes

--- src/modules/legacy/hooks/queries/useCheckLegacyAccount.js
+++ src/modules/legacy/hooks/queries/useCheckLegacyAccount.js
@@ -3,6 +3,6 @@ import { useLegacy } from '@legacy/hooks/queries';
 export default function useCheckLegacyAccount(address) {
   const { data } = useLegacy({ config: { params: { publicKey: address } } });
   return {
-    isMigrated: data?.data.balance === '0',
+    isMigrated: data ? data.data.balance === '0' : true,
   };
 }
