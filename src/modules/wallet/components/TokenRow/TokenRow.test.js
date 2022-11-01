2efb524dcc9b680195303f4b21b2c0e6502198b3
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-11-01 13:54:57 +0100

    Fixed remaining tests

--- src/modules/wallet/components/TokenRow/TokenRow.test.js
+++ src/modules/wallet/components/TokenRow/TokenRow.test.js
@@ -1,7 +1,7 @@
 import { renderWithRouter } from 'src/utils/testHelpers';
 import { screen } from '@testing-library/react';
 import { fromRawLsk } from '@token/fungible/utils/lsk';
-import { mockTokensBalance } from '../../../token/fungible/__fixtures__/mockTokens';
+import { mockTokensBalance } from '@token/fungible/__fixtures__';
 import TokenRow from './TokenRow';
 
 describe('TokenRow', () => {
@@ -18,7 +18,7 @@ describe('TokenRow', () => {
     expect(screen.queryByText(`${fromRawLsk(lockedBalance)} ${symbol.toUpperCase()}`));
     expect(screen.queryByText(`${fromRawLsk(availableBalance)}`));
     expect(screen.queryByText(`${fromRawLsk(+availableBalance + lockedBalance)}`));
-    expect(screen.queryByTestId('fiat-balance').innerHTML.match(/~0\.05/g)).toBeTruthy();
+    expect(screen.queryByTestId('fiat-balance').innerHTML.match(/~10\.00/g)).toBeTruthy();
     expect(screen.getByAltText(symbol)).toBeTruthy();
     expect(screen.getByAltText('arrowRightInactive')).toBeTruthy();
   });
