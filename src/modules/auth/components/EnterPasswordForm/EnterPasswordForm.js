934d8afd3529e9e71236101402a02df1a5066e2e
Oskar Fransson <oskar.leonard@hotmail.com>
2022-11-16 17:47:34 +0100

    show error message to the user

--- src/modules/auth/components/EnterPasswordForm/EnterPasswordForm.js
+++ src/modules/auth/components/EnterPasswordForm/EnterPasswordForm.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
@@ -13,6 +13,8 @@ import BoxContent from 'src/theme/box/content';
 import { PrimaryButton } from 'src/theme/buttons';
 import styles from './EnterPasswordForm.css';
 
+const API_ERROR_NAME = 'API_ERROR_NAME';
+
 // eslint-disable-next-line max-statements
 const EnterPasswordForm = ({ onEnterPasswordSuccess, title, encryptedAccount }) => {
   const { t } = useTranslation();
@@ -20,65 +22,69 @@ const EnterPasswordForm = ({ onEnterPasswordSuccess, title, encryptedAccount }) 
     register,
     watch,
     handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
   } = useForm();
+  const apiError = errors[API_ERROR_NAME];
   const activeAccount = useSelector(selectActiveTokenAccount);
   const { getAccountByAddress } = useAccounts();
   const [currentAccount] = useCurrentAccount();
-  const [feedbackError, setFeedbackError] = useState('');
   const requestedAccount = getAccountByAddress(activeAccount.summary.address);
-  const account = useMemo(() => encryptedAccount || requestedAccount || currentAccount, [currentAccount]);
+  const account = useMemo(
+      () => encryptedAccount || requestedAccount || currentAccount,
+      [currentAccount]
+  );
   const formValues = watch();
 
   const onSubmit = async ({ password }) => {
-    const { error, result } = await decryptAccount(
-      account.encryptedPassphrase, password,
-    );
+    const { error, result } = await decryptAccount(account.encryptedPassphrase, password);
 
     if (error) {
-      const errorMessage = t('Unable to decrypt account. Please check your password');
-      return setFeedbackError(errorMessage);
+      setError(API_ERROR_NAME, {
+        type: 'custom',
+        message: t('Unable to decrypt account. Please check your password'),
+      });
+    } else {
+      onEnterPasswordSuccess({
+        recoveryPhrase: result.recoveryPhrase,
+        encryptedAccount: account,
+        privateKey: result.privateKey,
+      });
     }
-
-    return onEnterPasswordSuccess({
-      recoveryPhrase: result.recoveryPhrase,
-      encryptedAccount: account,
-      privateKey: result.privateKey,
-    });
   };
 
   return (
-    <Box className={styles.container}>
-      <BoxContent className={styles.content}>
-        <h1>{t('Enter your password')}</h1>
-        <p className={styles.subheader}>
-          {t(title || 'Please provide your device password to backup the recovery phrase.')}
-        </p>
-        <WalletVisual
-          className={styles.avatar}
-          address={account?.metadata?.address}
-        />
-        {account?.metadata?.name && (
-          <p className={styles.accountName}>{account?.metadata?.name}</p>
-        )}
-        <p className={styles.accountAddress}>{account?.metadata?.address}</p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            secureTextEntry
-            size="s"
-            placeholder={t('Enter password')}
-            feedback={feedbackError}
-            {...register('password')}
-          />
-          <PrimaryButton
-            type="submit"
-            disabled={!formValues.password}
-            className={`${styles.button} continue-btn`}
-          >
-            {t('Continue')}
-          </PrimaryButton>
-        </form>
-      </BoxContent>
-    </Box>
+      <Box className={styles.container}>
+        <BoxContent className={styles.content}>
+          <h1>{t('Enter your password')}</h1>
+          <p className={styles.subheader}>
+            {t(title || 'Please provide your device password to backup the recovery phrase.')}
+          </p>
+          <WalletVisual className={styles.avatar} address={account?.metadata?.address} />
+          {account?.metadata?.name && <p className={styles.accountName}>{account?.metadata?.name}</p>}
+          <p className={styles.accountAddress}>{account?.metadata?.address}</p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+                secureTextEntry
+                size="s"
+                placeholder={t('Enter password')}
+                feedback={apiError?.message}
+                error={!!apiError}
+                {...register('password', {
+                  onChange: () => clearErrors(API_ERROR_NAME),
+                })}
+            />
+            <PrimaryButton
+                type="submit"
+                disabled={!formValues.password}
+                className={`${styles.button} continue-btn`}
+            >
+              {t('Continue')}
+            </PrimaryButton>
+          </form>
+        </BoxContent>
+      </Box>
   );
 };
 
