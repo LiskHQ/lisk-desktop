0f0d9d7a719d411ab5b117873cb0969283bdb88e
eniolam1000752 <eniolam1000752@gmail.com>
2022-11-22 13:36:33 +0100

    fix: resolved comments from masoud

--- src/modules/transaction/components/TxSignatureCollector/TxSignatureCollector.js
+++ src/modules/transaction/components/TxSignatureCollector/TxSignatureCollector.js
@@ -1,16 +1,10 @@
 import React, { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
 import { TertiaryButton } from 'src/theme/buttons';
-// import { secondPassphraseRemoved } from '@auth/store/action';
 import { useCommandSchema } from '@network/hooks';
 import Icon from 'src/theme/Icon';
-// import Box from 'src/theme/box';
-// import Illustration from 'src/modules/common/components/illustration';
-// import BoxContent from 'src/theme/box/content';
 import { isEmpty } from 'src/utils/helpers';
 import EnterPasswordForm from 'src/modules/auth/components/EnterPasswordForm';
 import { useAuth } from '@auth/hooks/queries';
-// import { getDeviceType } from '@wallet/utils/hwManager';
 import { useCurrentAccount } from '@account/hooks';
 import styles from './txSignatureCollector.css';
 import { joinModuleAndCommand } from '../../utils';
@@ -45,12 +39,9 @@ const TxSignatureCollector = ({
     transactionJSON,
   });
 
-  // const deviceType = getDeviceType(account?.hwInfo?.deviceModel);
-  // const dispatch = useDispatch();
   const isTransactionAuthor = transactionJSON.senderPublicKey === sender.metadata.pubkey;
   const isAuthorAccountMultisignature =
-    [...(account?.data?.mandatoryKeys || []), ...(account?.data?.optionalKeys || [])].length > 0; // account.info.LSK.summary.isMultisignature;
-  // const isSignerAccountMultisignature = sender.data?.keys.numberOfSignatures > 0;
+    [...(account?.data?.mandatoryKeys || []), ...(account?.data?.optionalKeys || [])].length > 0;
   const moduleCommand = joinModuleAndCommand(transactionJSON);
   const isRegisterMultisignature =
     moduleCommand === MODULE_COMMANDS_NAME_MAP.registerMultisignature;
@@ -100,39 +91,13 @@ const TxSignatureCollector = ({
       moduleCommandSchemas,
       sender: { ...account.data }, // this is the account of the present user wanting to sign the transaction
     });
-
-    // Transaction authored from other account and current account is a non multisignature account
-    // if (transactionJSON.senderPublicKey !== currentAccount.metadata.pubkey && !account.info.LSK.summary.isMultisignature) {
-    //   // Skip the current member as all the required signature are collected
-    //   return signatureSkipped({ formProps, transactionJSON, });
-    // }
-
-    // if (signatureStatus === signatureCollectionStatus.fullySigned || signatureStatus === signatureCollectionStatus.overSigned) {
-
-    // }
   };
 
   const onEnterPasswordSuccess = ({ privateKey }) =>
     txVerification(privateKey, sender.metadata.pubkey);
 
-  // TODO: Revisit this code, latest multisignature protocol covers old second passphrase scenarios.
-  // We no longer need second passphrase logic as long as multisignature feature is working.
-
-  // useEffect(() => {
-  //   if (deviceType) {
-  //     txVerification();
-  //   }
-  //   return () => {
-  //     // Ensure second passphrase is removed to prevent automatically signing future transactions
-  //     if (account?.secondPassphrase) {
-  //       dispatch(secondPassphraseRemoved());
-  //     }
-  //   };
-  // }, []);
-
   useEffect(() => {
     if (!isEmpty(transactions.signedTransaction)) {
-      // const hasSecondPass = !!account.secondPassphrase;
       const isDoubleSigned = !transactions.signedTransaction.signatures.some(
         (sig) => sig.length === 0
       );
