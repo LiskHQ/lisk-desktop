ae570495e062f92ec28c627c8bbdd300dbaa413c
oskarleonard <oskar.leonard@hotmail.com>
2022-12-01 15:30:42 +0100

    Checkbox should not look like radio button (#4615)
    * Remove chrome console warnings related to controlled vs uncontrolled input
    
    * Remove chrome warning and make it compliant with react-hook-forms as well as when used independently
    
    * Make it possible to check and uncheck the checkbox by clicking the span info text
    
    * Use a smaller border radius so it wont look like a radio button
    
    * Always show an error if there is an error, error is defined both in status and in the error prop, would be good to refactor to only have one logic for error
    
    * Remove chrome error, label can also be used as a node
    
    * fix typo

--- src/theme/CheckBox/index.js
+++ src/theme/CheckBox/index.js
@@ -2,31 +2,33 @@ import React, { forwardRef } from 'react';
 import Icon from 'src/theme/Icon';
 import styles from './checkBox.css';
 
-const CheckBox = forwardRef(({
-  added, removed, onChange, accent, checked, className, name, readOnly, value,
-}, ref) => (
-  <label className={`${styles.checkbox} ${className} ${checked ? 'checked' : 'unchecked'}`}>
-    <input
-      type="checkbox"
-      data-testid={name}
-      checked={checked ? !!checked : undefined}
-      ref={ref}
-      value={value}
-      name={name}
-      readOnly={readOnly}
-      onChange={onChange}
-    />
-    <span className={`${(accent || added) ? styles.accent : ''} ${removed ? styles.removed : ''}`}>
-      <Icon name="checkboxFilled" />
-    </span>
-  </label>
-));
+const CheckBox = forwardRef(
+  ({ added, removed, onChange, accent, checked, className, name, readOnly, value, onBlur }, ref) => (
+      <label className={`${styles.checkbox} ${className} ${checked ? 'checked' : 'unchecked'}`}>
+        <input
+          type="checkbox"
+          data-testid={name}
+          checked={checked}
+          ref={ref}
+          value={value}
+          name={name}
+          readOnly={readOnly}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        <span
+          className={`${accent || added ? styles.accent : ''} ${removed ? styles.removed : ''}`}
+        >
+          <Icon name="checkboxFilled" />
+        </span>
+      </label>
+    )
+);
 
 CheckBox.defaultProps = {
   added: false,
   removed: false,
   accent: false,
-  checked: false,
   className: '',
   name: '',
 };
