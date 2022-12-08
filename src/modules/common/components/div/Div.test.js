8a19cba8a395088e43addcbc6fb387e85c9558fd
Oskar Fransson <oskar.leonard@hotmail.com>
2022-12-08 08:13:51 +0100

    Add simple render test

---
+++ src/modules/common/components/div/Div.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Div from './Div';
+
+describe('WalletDetails', () => {
+  it('Should render children', () => {
+    render(<Div><p>test</p></Div>);
+    expect(screen.getByText(test)).toBeTruthy();
+  });
+});
