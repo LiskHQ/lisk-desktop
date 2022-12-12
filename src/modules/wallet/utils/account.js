06f60842ea3b565d614d9495e8380e515c0e1354
oskarleonard <oskar.leonard@hotmail.com>
2022-12-12 14:13:14 +0100

    Fix custom derivation path (#4658)
    * Fix alignment of warning icon when Input is of size m
    
    * Remove custom derivation path from redux because it should not be persistent.
    Add error message when derivation path is invalid
    
    * Prevent the currently typed passphrase from being deleted when changing enableCustomDerivationPath setting
    
    * Remove test because customDerivationPath is not in redux store anymore
    
    * Use the defaultDerivationPath when enableCustomDerivationPath is true
    
    * Remove test for login component which we will deprecate
    
    * remove unused
    
    * Add tests for CustomDerivationPath
    
    * Use getByLabelText from render instead of importing it, also change the test name to correspond to what is does
    
    * Add test to make sure that onAddAccount is called with both a passphrase and a derivationPath
    
    * Add test for when passphrase is correct but derivation path is incorrect.
    
    * Fix typo
    
    * Remove this second failsafe check since the button is already disabled this function cannot be called if isSubmitDisabled, add test for checking if button is disabled instead
    
    * Make sure disabled buttons dont have cursor pointer
    
    * remove unused css rule
    
    * add link to issue

--- src/modules/wallet/utils/account.js
+++ src/modules/wallet/utils/account.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-lines, max-len */
 import { passphrase as LiskPassphrase, cryptography } from '@liskhq/lisk-client';
 import { regex } from 'src/const/regex';
+import i18next from 'i18next';
 
 /**
  * Extracts Lisk PrivateKey/PublicKey pair from a given valid Mnemonic passphrase
@@ -302,4 +303,49 @@ export const validate2ndPass = async (account, passphrase, error) => {
     messages.push('This passphrase does not belong to your account.');
   }
   return messages;
+};
+
+/**
+ * Validate a derivation path
+ * TODO: Replace this function when @liskhq (lisk-sdk) exposes the one they use: Issue #7877 https://github.com/LiskHQ/lisk-sdk/issues/7877
+ * @param {string} derivationPath
+ * @returns {string} - Error message
+ */
+export const getDerivationPathErrorMessage = (derivationPath) => {
+  if ((!derivationPath || !derivationPath.startsWith('m')) || !derivationPath.includes('/')) {
+    return i18next.t('Invalid path format');
+  }
+
+  try {
+    const MAX_UINT32 = 4294967295;
+    const HARDENED_OFFSET = 0x80000000;
+
+    derivationPath
+      .split('/')
+      // slice first element which is `m`
+      .slice(1)
+      .map((segment) => {
+        if (!/^[0-9']+$/g.test(segment)) {
+          throw new Error('Invalid path format');
+        }
+
+        // if segment includes apostrophe add HARDENED_OFFSET
+        if (segment.includes(`'`)) {
+          if (parseInt(segment.slice(0, -1), 10) > MAX_UINT32 / 2) {
+            throw new Error('Invalid path format');
+          }
+          return parseInt(segment, 10) + HARDENED_OFFSET;
+        }
+
+        if (parseInt(segment, 10) > MAX_UINT32) {
+          throw new Error('Invalid path format');
+        }
+
+        return parseInt(segment, 10);
+      });
+  } catch (error) {
+    return i18next.t(error.message);
+  }
+
+  return undefined;
 };
