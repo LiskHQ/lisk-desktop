dd2e8ca14a50e1590f5b30cf67af279a6aac577f
Oskar Fransson <oskar.leonard@hotmail.com>
2022-12-15 17:29:53 +0100

    Add back useless class to satisfy tests

--- src/modules/transaction/components/StakeItem/StakeItem.js
+++ src/modules/transaction/components/StakeItem/StakeItem.js
@@ -29,7 +29,7 @@ const StakeItem = ({ vote, address, title, truncate }) => {
           {title ?? (truncate ? truncateAddress(address) : address)}
         </span>
       </Link>
-      <span className={`${styles.value}`}>
+      <span className={`${styles.value} stake-item-value`}>
         {vote.confirmed && vote.unconfirmed ? (
           <>
             <span className={styles.confirmed}>
