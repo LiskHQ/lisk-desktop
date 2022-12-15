59348355b7050db58eb6f9ded6fadc84fe8f7b48
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-12-15 17:42:23 +0100

    Fixed minor coverage issue

--- src/modules/transaction/utils/showNotificationsForIncomingTransactions.test.js
+++ src/modules/transaction/utils/showNotificationsForIncomingTransactions.test.js
@@ -8,48 +8,80 @@ global.Notification = jest.fn().mockImplementation(() => ({
   close: jest.fn(),
 }));
 
+const txns = [
+  {
+    id: `ec2b25bafc284b064323ab12f391684eba8a89b4d8591f424e04bc5bb24e6233`,
+    type: 0,
+    moduleCommand: 'token:transfer',
+    fee: '10000000',
+    isPending: false,
+    sender: { address: 'lskdwsyfmcko6mcd357446yatromr9vzgu7eb8y11' },
+    params: {
+      amount: '200',
+      recipientAddress: 'lsk8nxtxs9ekqc3c2jjtcsusmfj2498qyjbk35teh',
+      data: 'test',
+    },
+    block: {
+      timestamp: 106359314,
+      height: 9381199,
+    },
+  },
+  {
+    id: `af2b25bafc284b068264ab12f132897eba8a89b4d8591f424e04bc5bb24e1980`,
+    type: 0,
+    moduleCommand: 'token:transfer',
+    fee: '10000000',
+    isPending: false,
+    sender: { address: 'lskqw2b528hc6ud7y56toq3kmaq6kj2fpvf9amvtx' },
+    params: {
+      amount: '200',
+      recipientAddress: 'lsk3ay4z7wqjczbo5ogcqxgxx23xyacxmycwxfh4d',
+      data: '',
+    },
+    block: {
+      timestamp: 106350086,
+      height: 9381127,
+    },
+  },
+];
+
 describe('showNotificationsForIncomingTransactions', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   it('should display notifications for transactions related to current account', () => {
-    const txns = [
-      {
-        id: `ec2b25bafc284b064323ab12f391684eba8a89b4d8591f424e04bc5bb24e6233`,
-        type: 0,
-        moduleCommand: 'token:transfer',
-        fee: '10000000',
-        isPending: false,
-        sender: { address: 'lskdwsyfmcko6mcd357446yatromr9vzgu7eb8y11' },
-        params: {
-          amount: '200',
-          recipientAddress: 'lsk8nxtxs9ekqc3c2jjtcsusmfj2498qyjbk35teh',
-          data: 'test',
-        },
-        block: {
-          timestamp: 106359314,
-          height: 9381199,
-        },
-      },
-      {
-        id: `af2b25bafc284b068264ab12f132897eba8a89b4d8591f424e04bc5bb24e1980`,
-        type: 0,
-        moduleCommand: 'token:transfer',
-        fee: '10000000',
-        isPending: false,
-        sender: { address: 'lskqw2b528hc6ud7y56toq3kmaq6kj2fpvf9amvtx' },
-        params: {
-          amount: '200',
-          recipientAddress: 'lsk3ay4z7wqjczbo5ogcqxgxx23xyacxmycwxfh4d',
-          data: '',
-        },
-        block: {
-          timestamp: 106350086,
-          height: 9381127,
-        },
-      },
-    ];
     const currAcct = mockSavedAccounts[0];
     showNotificationsForIncomingTransactions(txns, currAcct, 'LSK');
 
     expect(toast.info).toHaveBeenCalledTimes(1);
     expect(toast.info).toHaveBeenCalledWith('Your account just received 0.000002 LSK ');
+  });
+
+  it('should display notifications with additional info for transactions related to current account', () => {
+    const currAcct = mockSavedAccounts[0];
+    const updatedTxns = [...txns];
+    updatedTxns[1] = {
+      id: `bd2b83bafc284b068264ab12f132897eba8a89b4d8591f424e04bc5bb24e5628`,
+      type: 0,
+      moduleCommand: 'token:transfer',
+      fee: '10000000',
+      isPending: false,
+      sender: { address: 'lskqw2b528hc6ud7y56toq3kmaq6kj2fpvf9amvtx' },
+      params: {
+        amount: '150',
+        recipientAddress: 'lsk3ay4z7wqjczbo5ogcqxgxx23xyacxmycwxfh4d',
+        data: 'test',
+      },
+      block: {
+        timestamp: 106359582,
+        height: 9383851,
+      },
+    };
+    showNotificationsForIncomingTransactions(updatedTxns, currAcct, 'LSK');
+
+    expect(toast.info).toHaveBeenCalledTimes(1);
+    expect(toast.info).toHaveBeenCalledWith(
+      'Your account just received 0.0000015 LSK with message test'
+    );
   });
 });
