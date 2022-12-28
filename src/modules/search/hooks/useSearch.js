dd9a464a633d086834ba8b5c1d9c94df3820a029
Ali Haghighatkhah <ali@lightcurve.io>
2022-12-28 17:42:38 +0100

    Fix useSearch hook according #4646

--- src/modules/search/hooks/useSearch.js
+++ src/modules/search/hooks/useSearch.js
@@ -1,65 +1,48 @@
-/* eslint-disable complexity, max-statements, import/prefer-default-export */
-import { useMemo } from "react";
-import { API_VERSION } from "src/const/config";
-import { regex } from "src/const/regex";
-import { useCustomQuery } from "src/modules/common/hooks"
-import { validateAddress } from "src/utils/validators";
+import { regex } from 'src/const/regex';
+import { validateAddress } from 'src/utils/validators';
+import { useTransactions } from '@transaction/hooks/queries';
+import { useValidators } from '@pos/validator/hooks/queries';
+import { useBlocks } from '@block/hooks/queries/useBlocks';
 
+// eslint-disable-next-line complexity,max-statements
 export const useSearch = (search = '') => {
-  const config = {
-    method: 'get',
+  const isAddress = validateAddress(search) === 0;
+  const isTxId = regex.transactionId.test(search);
+  const isBlockHeight = regex.blockHeight.test(search);
+  const isValidator = search.length >= 3 && !isAddress && !isTxId && !isBlockHeight;
+
+  const addresses = useValidators({
+    config: { params: { address: search } },
+    options: { enabled: isAddress },
+  });
+
+  const validators = useValidators({
+    config: { params: { search } },
+    options: { enabled: isValidator },
+  });
+
+  const transactions = useTransactions({
+    config: { params: { transactionID: search } },
+    options: { enabled: isTxId },
+  });
+
+  const blocks = useBlocks({
+    config: { params: { height: search } },
+    options: { enabled: isBlockHeight },
+  });
+
+  const isLoading =
+    validators.isLoading || transactions.isLoading || addresses.isLoading || blocks.isLoading;
+
+  const isFetched =
+  (isValidator  && validators.isFetched) || (isTxId  && transactions.isFetched) || (isAddress  && addresses.isFetched) || (isBlockHeight  && blocks.isFetched);
+
+  return {
+    addresses: addresses.data?.data ?? [],
+    validators: validators.data?.data ?? [],
+    transactions: transactions.data?.data ?? [],
+    blocks: blocks.data?.data ?? [],
+    isLoading,
+    isFetched,
   };
-
-  const isAddress = validateAddress(search) === 0
-  const isTxId = regex.transactionId.test(search)
-  const isBlockHeight = regex.blockHeight.test(search)
-
-  const addressSearch = useCustomQuery({
-    keys: ['address-key'],
-    config: {
-      ...config,
-      url: `/api/${API_VERSION}/pos/validators`,
-      event: 'get.address',
-      params: { address: search }
-    }, options: { enabled: isAddress }
-  })
-  const addresses = useMemo(() =>  addressSearch.data?.data ?? [], [addressSearch.data?.data])
-
-  const validatorSearch = useCustomQuery({
-    keys: ['search-key'],
-    config: {
-      ...config,
-      url: `/api/${API_VERSION}/pos/validators`,
-      event: 'get.validators',
-      params: { search }
-    }, options: { enabled: search.length >= 3 && !isAddress && !isTxId && !isBlockHeight }
-  })
-  const validators = useMemo(() =>  validatorSearch.data?.data ?? [], [validatorSearch.data?.data])
-
-  const transactionsSearch = useCustomQuery({
-    keys: ['transactions-key'],
-    config: {
-      ...config,
-      url: `/api/${API_VERSION}/transactions`,
-      event: 'get.transaction',
-      params: { transactionID: search }
-    }, options: { enabled: isTxId }
-  })
-  const transactions = useMemo(() =>  transactionsSearch.data?.data ?? [], [transactionsSearch.data?.data])
-
-  const blockHeightSearch = useCustomQuery({
-    keys: ['block-key'],
-    config: {
-      ...config,
-      url: `/api/${API_VERSION}/blocks`,
-      event: 'get.block',
-      params: { height: search }
-    }, options: { enabled: isBlockHeight }
-  })
-
-  const blocks = useMemo(() =>  blockHeightSearch.data?.data ?? [], [blockHeightSearch.data?.data])
-
-  const isLoading = validatorSearch.isLoading || transactionsSearch.isLoading || addressSearch.isLoading || blockHeightSearch.isLoading
-
-  return { addresses, validators, transactions, blocks, isLoading }
-}
+};
