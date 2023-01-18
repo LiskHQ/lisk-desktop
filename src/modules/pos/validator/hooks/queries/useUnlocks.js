4186a7c4a4369af6610171f0495e6a7764700418
Oskar Fransson <oskar.leonard@hotmail.com>
2023-01-18 14:03:04 +0100

    Default to fetch all unlocks if a limit is not provided in customConfig

--- src/modules/pos/validator/hooks/queries/useUnlocks.js
+++ src/modules/pos/validator/hooks/queries/useUnlocks.js
@@ -1,10 +1,8 @@
 /* istanbul ignore file */
 import { UNLOCKS } from 'src/const/queries';
-import {
-  LIMIT as limit,
-  API_VERSION,
-} from 'src/const/config';
+import { API_VERSION } from 'src/const/config';
 import { useCustomInfiniteQuery } from 'src/modules/common/hooks';
+import { usePosConstants } from '@pos/validator/hooks/queries/usePosConstants';
 
 /**
  * Creates a custom hook for votes unlocks queries
@@ -23,6 +21,9 @@ import { useCustomInfiniteQuery } from 'src/modules/common/hooks';
  */
 
 export const useUnlocks = ({ config: customConfig = {}, options } = {}) => {
+  const { data: posConstants, isSuccess } = usePosConstants();
+  const maxNumberPendingUnlocks = posConstants?.data?.maxNumberPendingUnlocks;
+
   const hasRequiredParams =
     customConfig.params?.address || customConfig.params?.name || customConfig.params?.publicKey;
 
@@ -31,24 +32,25 @@ export const useUnlocks = ({ config: customConfig = {}, options } = {}) => {
     method: 'get',
     event: 'get.pos.unlocks',
     ...customConfig,
-    params: { limit, ...(customConfig?.params || {}) },
+    params: { limit: maxNumberPendingUnlocks, ...(customConfig?.params || {}) },
   };
   const customOptions = {
     ...options,
-    enabled: !!hasRequiredParams,
-    select: (data) => data.pages.reduce((prevPages, page) => {
-      const newData = page?.data || {};
-      const newUnlocks = page?.data.pendingUnlocks || [];
-      return {
-        ...page,
-        data: {
-          ...newData,
-          pendingUnlocks: prevPages.data
-            ? [...prevPages.data.pendingUnlocks, ...newUnlocks]
-            : newUnlocks,
-        },
-      };
-    }),
+    enabled: !!hasRequiredParams && isSuccess,
+    select: (data) =>
+      data.pages.reduce((prevPages, page) => {
+        const newData = page?.data || {};
+        const newUnlocks = page?.data.pendingUnlocks || [];
+        return {
+          ...page,
+          data: {
+            ...newData,
+            pendingUnlocks: prevPages.data
+              ? [...prevPages.data.pendingUnlocks, ...newUnlocks]
+              : newUnlocks,
+          },
+        };
+      }),
   };
   return useCustomInfiniteQuery({
     keys: [UNLOCKS],
