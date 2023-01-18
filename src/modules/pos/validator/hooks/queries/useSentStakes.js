71dfdb0e2ebbb34a22f6868839571f53424a5f40
Oskar Fransson <oskar.leonard@hotmail.com>
2023-01-18 09:39:16 +0100

    No need to merge validators to stakes, having the hook in the SentStakesRow is fine

--- src/modules/pos/validator/hooks/queries/useSentStakes.js
+++ src/modules/pos/validator/hooks/queries/useSentStakes.js
@@ -2,7 +2,6 @@
 import { STAKES_SENT } from 'src/const/queries';
 import { API_VERSION } from 'src/const/config';
 import { useCustomQuery } from 'src/modules/common/hooks';
-import defaultClient from 'src/utils/api/client';
 
 /**
  * Creates a custom hook for stakes sent queries
@@ -26,66 +25,9 @@ export const useSentStakes = ({ config: customConfig = {}, options } = {}) => {
     ...customConfig,
   };
 
-  console.log('11 useSentStakes config', config);
-
   return useCustomQuery({
     keys: [STAKES_SENT],
     options,
     config,
-  });
-};
-
-export const addValidatorToStakes =
-  ({ config, client }) =>
-  async (tokens) => {
-    console.log('useSentStakesWithValidators addMe');
-    try {
-      const metaRes = await client.call(config);
-      return metaRes;
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log('meta tokens Error:', e);
-      return tokens;
-    }
-  };
-
-const validatorConfig = {
-  url: `/api/${API_VERSION}/pos/validators`,
-  method: 'get',
-  event: 'get.pos.validators',
-  params: { address: 'lskhbxua8tpdckcewntcttfqfo4rbatampo2dgrno1' },
-};
-
-export const useSentStakesWithValidators = ({ config: customConfig = {}, options } = {}) => {
-  const transformStakes = addValidatorToStakes({
-    config: validatorConfig,
-    client: defaultClient,
-  });
-
-  const transformResult = async (res) => {
-    console.log('useSentStakesWithValidators res', res);
-    const validator = await transformStakes(res.data);
-    console.log('useSentStakesWithValidators validator', validator);
-    return {
-      ...res,
-      data: validator,
-    };
-  };
-  console.log('useSentStakesWithValidators customConfig', customConfig);
-
-  const ok = async () => {
-    console.log('useSentStakesWithValidators ok.....');
-  };
-
-  const config = {
-    ...customConfig,
-    transformResult: ok,
-  };
-
-  console.log('useSentStakesWithValidators config', config);
-
-  return useSentStakes({
-    config,
-    options,
   });
 };
