ec2c14746272617405611189eb71fa567c7080af
Oskar Fransson <oskar.leonard@hotmail.com>
2023-01-24 17:40:06 +0100

    React must be in scope

--- src/modules/transaction/components/ClaimRewardsSummaryContent/ClaimRewardsSummaryContent.test.js
+++ src/modules/transaction/components/ClaimRewardsSummaryContent/ClaimRewardsSummaryContent.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import mockSavedAccounts from '@tests/fixtures/accounts';
 import { useRewardsClaimable } from '@pos/reward/hooks/queries';
