c0479e1a86ed16e371812f3c82e03591b1ce4cde
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-24 12:20:31 +0100

    Define command fees and use it

---
+++ src/modules/transaction/hooks/useTransactionFee/useCommandFee.js
@@ -0,0 +1,65 @@
+import { useMemo } from 'react';
+import { useAuth, useGetInitializationFees } from '@auth/hooks/queries';
+import { useTokensBalance } from '@token/fungible/hooks/queries';
+import { usePosConstants } from '@pos/validator/hooks/queries';
+import { FEE_TYPES } from './constants';
+import { MODULE_COMMANDS_MAP } from '../../configuration/moduleCommand';
+import { joinModuleAndCommand } from '../../utils';
+
+const ZERO_FEE = { commandFee: 0, initFee: 0, initCCMFee: 0 };
+
+export const useCommandFee = (transaction) => {
+  const address = transaction?.params?.recipientAddress;
+  const queryConfig = {
+    options: { enabled: !!address },
+    config: { params: { address } },
+  }
+  const { data: auth, isLoading: isAuthLoading } = useAuth(queryConfig);
+  const { data: token, isLoading: isTokenLoading } = useTokensBalance(queryConfig);
+  const { data: posConstants, isLoading: isPosConstantsLosing } = usePosConstants();
+  const { data: initializationFees, isLoading: isInitializationFeesLoading } = useGetInitializationFees();
+
+  // Return Zero fees if data is not ready
+  if (
+    !address ||
+    isTokenLoading ||
+    isAuthLoading ||
+    isPosConstantsLosing ||
+    isInitializationFeesLoading
+  )
+    return ZERO_FEE;
+
+  const fees = useMemo(async () => {
+    // Define criteria
+    const isInitialized = auth.data.nonce !== '0' || token.data.balances.length;
+    const isSameChain = transaction.params.receivingChainID === transaction.params.chainID;
+    const isRegisterValidator =
+      joinModuleAndCommand(transaction) === MODULE_COMMANDS_MAP.REGISTER_VALIDATOR;
+
+    // Check and return values
+    return [
+      {
+        value: isSameChain && !isInitialized ? initializationFees.result.data.userAccount : 0,
+        type: FEE_TYPES.INITIALIZATION,
+      },
+      {
+        value: !isSameChain && !isInitialized ? initializationFees.result.data.escrowAccount : 0,
+        type: FEE_TYPES.ESCROW_INITIALIZATION,
+      },
+      {
+        value: isRegisterValidator
+          ? posConstants.data.extraCommandFees.validatorRegistrationFee
+          : 0,
+        type: FEE_TYPES.REGISTER_VALIDATOR,
+      },
+    ];
+  }, [
+    auth,
+    token,
+    transaction,
+    posConstants,
+    initializationFees,
+  ]);
+
+  return fees;
+};
--- src/modules/transaction/hooks/useTransactionFee/useTransactionFee.js
+++ src/modules/transaction/hooks/useTransactionFee/useTransactionFee.js
@@ -2,6 +2,12 @@ import { useMemo } from 'react';
 import { useCommandSchema } from '@network/hooks/useCommandsSchema';
 import { useAuth } from '@auth/hooks/queries/useAuth';
 import { computeTransactionMinFee, getParamsSchema } from './utils';
+import { useCommandFee } from './useCommandFee';
+import { FEE_TYPES } from './constants';
+
+const getTotalFee = (components) =>
+  components.reduce((acc, { value }) => acc + value, 0);
+
 /**
  *
  * @param {object} data
@@ -23,17 +29,25 @@ export const useTransactionFee = ({ isValid, senderAddress, transaction }) => {
     isFetched: isSchemaFetched,
   } = useCommandSchema();
   const schema = getParamsSchema(transaction, moduleCommandSchemas);
+  const commandFees = useCommandFee(transaction);
 
-  const fee = useMemo(
-    () => computeTransactionMinFee(transaction, schema, auth, isValid && !isSchemaLoading),
+  const bytesFee = useMemo(
+    () => {
+      const fee = computeTransactionMinFee(transaction, schema, auth, isValid && !isSchemaLoading);
+      return { value: fee, type: FEE_TYPES.BYTES_FEE };
+    },
     [transaction, schema, auth, isValid, isSchemaLoading]
   );
 
   // @todo Assert the returned value when all fee components are defined
+  const components = [
+    bytesFee,
+    ...commandFees,
+  ].filter(item => item.value > 0);
   return {
     isLoading: isSchemaLoading || /* istanbul ignore next */ isLoading,
     isFetched: isSchemaFetched && /* istanbul ignore next */ isFetched,
-    total: fee,
-    components: [{ value: fee, type: 'bytesFee' }],
+    total: getTotalFee(components),
+    components,
   };
 };
