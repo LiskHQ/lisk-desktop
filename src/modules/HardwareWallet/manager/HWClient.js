5e95faf6a5e3586201b390c43475e8f06acdd391
masoud soroush <masoud.s.geo@gmail.com>
2023-02-02 11:38:39 +0100

    feat: add HW Client

---
+++ src/modules/HardwareWallet/manager/HWClient.js
@@ -0,0 +1,25 @@
+import { CLIENT } from '../const';
+
+
+export class HWClient {
+  constructor() {
+    this.ipc = window.ipc
+  }
+
+  executeCommand(action, payload) {
+    return new Promise((resolve, reject) => {
+      // Listening for response
+      this.ipc.once(`${action}.${CLIENT.RESPONSE}`, (_event, response) => {
+        if (response.success) return resolve(response.data);
+        return reject(new Error(`${action} failed: ${response.error}`));
+      });
+      // Requesting data
+      this.ipc.send(`${action}.${CLIENT.REQUEST}`, payload);
+    })
+  }
+
+  subscribe(event, cb) {
+    this.ipc.on(event, cb);
+  }
+}
+
---
+++ src/modules/HardwareWallet/manager/HWManager.js
@@ -0,0 +1,88 @@
+import { IPC_MESSAGES } from '../const';
+import { HWClient } from './HWClient';
+
+class HwManager extends HWClient {
+  constructor() {
+    super();
+    this.activeDeviceID = null;
+    this.currentDeviceStatus = 'disconnected'
+    this.devices = [];
+  }
+
+  createAccount() {}
+
+  getAccounts() {}
+
+  getDeviceList() {
+    // this.executeCommand(IPC_MESSAGES.GET_CONNECTED_DEVICES_LIST, null)
+    return this.devices;
+  }
+
+
+  getCurrentDeviceInfo() {
+    // return this.device.filter((device) => device.id == this.activeDeviceID)
+  }
+  getDeviceInfoByID() {}
+
+  selectDevice(deviceId) {
+    this.activeDeviceID = deviceId;
+  }
+
+
+  persistConnection() {}
+
+
+  getPublicKey() {}
+
+  getAddress(accountIndex) {
+    const data = {
+      deviceId: this.activeDeviceID,
+      showOnDevice: true,
+      index: accountIndex,
+    };
+    return this.executeCommand(IPC_MESSAGES.HW_COMMAND, {
+      action: IPC_MESSAGES.GET_ADDRESS,
+      data,
+    });
+  } // dump this on #4767
+
+  listeningToDevices() {
+    // @todo may this devices list provided by the HWServer check for the event DEVICE_LIST_CHANGED or GET_CONNECTED_DEVICES_LIST
+    // this.subsscribe('connect', (newDevice) => {
+    //   const hasDevice = this.devices.find((device) => {
+    //     return // device.id === newDevice.id
+    //   })
+    //   if (!hasDevice) {
+    //     this.devices.push(device)
+    //   }
+    // })
+    //
+    // this.subsscribe('disconnected', (device) => {
+    //   this.devices = this.devices.find((device) => {
+    //     return // device.id !== newDevice.id
+    //   })
+    // })
+  }
+
+  signMessage(accountIndex, message) {
+    const signature = {
+      deviceId: this.activeDeviceID,
+      index: accountIndex,
+      message,
+    };
+    return this.executeCommand(IPC_MESSAGES.HW_COMMAND, {
+      action: IPC_MESSAGES.SIGN_MSG,
+      data: signature,
+    });
+  }
+
+  signTransaction() {}
+
+  checkIfInsideLiskApp() {}
+
+  validatePin() {}
+
+  reconnectDevice(cb) {}
+}
+
+export default new HwManager();
