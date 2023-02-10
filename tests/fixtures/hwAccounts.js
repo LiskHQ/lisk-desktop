4672364751ac857e5806eb0f2c9ce33d118d7a29
eniolam1000752 <eniolam1000752@gmail.com>
2023-02-10 13:33:14 +0100

    feature: added unit test to accountrow

---
+++ tests/fixtures/hwAccounts.js
@@ -0,0 +1,36 @@
+const hwAccountOneJSON = {
+  hw: {
+    deviceId: '',
+    model: '',
+    brand: '',
+  },
+  metadata: {
+    name: 'hw-acc-1',
+    pubkey: 'a07e06d3d21bfcbd6e6cb158a0adc18b6ccc92db66dc8a323c7462af9c539fab',
+    path: '',
+    isHW: true,
+    address: 'lskdgtenb76rf93bzd56cqn6ova46wfvoesbk4hnd',
+    creationTime: '2022-12-08T15:30:39.979Z',
+  },
+  version: 1,
+};
+
+const hwAccountTwoJSON = {
+  hw: {
+    deviceId: '',
+    model: '',
+    brand: '',
+  },
+  metadata: {
+    name: 'hw-acc-2',
+    pubkey: 'a2f860e484144dce56dc426bd962044428c7074724b289cd2a23b32e62da02c4',
+    path: '',
+    isHW: true,
+    address: 'lskqw2b528hc6ud7y56toq3kmaq6kj2fpvf9amvtx',
+    creationTime: '2022-12-08T15:30:39.979Z',
+  },
+  version: 1,
+};
+const savedHwAccounts = [hwAccountOneJSON, hwAccountTwoJSON];
+
+export default savedHwAccounts;
