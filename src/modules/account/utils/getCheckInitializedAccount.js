2f98eb5792656f740dd1a81e715b3e465e7a14cb
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-02-15 11:23:25 +0100

    Added test for getCheckInitializedAccount

--- src/modules/account/utils/getCheckInitializedAccount.js
+++ src/modules/account/utils/getCheckInitializedAccount.js
@@ -3,15 +3,16 @@ import { useTokensBalanceConfig } from '@token/fungible/hooks/queries';
 import defaultClient from 'src/utils/api/client';
 
 export const getCheckInitializedAccount = async ({ config, client = defaultClient }) => {
-  const authConfig = useAuthConfig(config)
-  const tokenBalanceConfig = useTokensBalanceConfig(config)
+  const authConfig = useAuthConfig(config);
+  const tokenBalanceConfig = useTokensBalanceConfig(config);
 
   const [tokens, auth] = await Promise.all([
     client.call(tokenBalanceConfig),
-    client.call(authConfig)
-  ])
-  const balances = tokens?.data.reduce((sum, {availableBalance}) => sum + availableBalance, 0)
+    client.call(authConfig),
+  ]);
+
+  const balances = tokens?.data?.reduce((sum, { availableBalance }) => sum + availableBalance, 0);
   const balanceCheck = parseInt(balances, 10) > 0;
-  const nonceCheck = parseInt(auth?.data.nonce ?? 0, 10) > 0;
-  return balanceCheck || nonceCheck
-}
+  const nonceCheck = parseInt(auth?.data?.nonce ?? 0, 10) > 0;
+  return tokens?.data && auth?.data ? balanceCheck || nonceCheck : false;
+};
