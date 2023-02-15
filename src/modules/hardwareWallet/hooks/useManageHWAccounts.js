fef2060f7f72106042bb41e18c5da0bdb0cff667
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-02-15 14:31:41 +0100

    Updated useManageHWAccounts test

--- src/modules/hardwareWallet/hooks/useManageHWAccounts.js
+++ src/modules/hardwareWallet/hooks/useManageHWAccounts.js
@@ -1,41 +1,32 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectSettings, selectHWStatus } from 'src/redux/selectors';
-import { storeAccounts, removeAccounts } from '@hardwareWallet/store/actions/actions';
+import { selectSettings, selectHW } from 'src/redux/selectors';
+import { storeHWAccounts, removeHWAccounts } from '@hardwareWallet/store/actions/actions';
+import { getNameFromAccount } from '../utils/getNameFromAccount';
 import { getHWAccounts } from '../utils/getHWAccounts';
 
 const useManageHWAccounts = () => {
   const dispatch = useDispatch();
   const settings = useSelector(selectSettings);
   const [prevDeviceId, setCurrentDeviceId] = useState();
-  const { deviceId, status } = useSelector(selectHWStatus);
-
-  const getNameFromAccount = (address) => {
-    const { hardwareAccounts } = settings;
-
-    if (Array.isArray(hardwareAccounts[deviceId])) {
-      const storedAccount = hardwareAccounts[deviceId].filter(
-        (account) => account.address === address
-      );
-      return storedAccount.length ? storedAccount[0].name : null;
-    }
-    return null;
-  };
+  const { deviceId, status } = useSelector(selectHW);
 
   const addAccounts = () => {
     if (prevDeviceId === deviceId || status !== 'connected') {
       return;
     }
     setCurrentDeviceId(deviceId);
-    getHWAccounts(getNameFromAccount).then((accList) => {
-      dispatch(storeAccounts(accList));
-    });
+    getHWAccounts(getNameFromAccount, settings, deviceId)
+      .then((accList) => {
+        dispatch(storeHWAccounts(accList));
+      })
+      .catch((err) => console.log({ err }));
   };
 
   useEffect(() => {
     addAccounts();
     if (status === 'disconnected') {
-      dispatch(removeAccounts());
+      dispatch(removeHWAccounts());
     }
   }, [status, deviceId]);
 };
--- src/modules/hardwareWallet/hooks/useManageHWAccounts.test.js
+++ src/modules/hardwareWallet/hooks/useManageHWAccounts.test.js
@@ -1,50 +1,76 @@
 import { renderHook } from '@testing-library/react-hooks';
-// import { cryptography } from '@liskhq/lisk-client';
-import useCheckInitializedAccount from '@account/hooks/queries/useCheckInitializedAccount';
-// import { storeAccounts, removeAccounts } from '../store/actions/actions';
+import { useSelector } from 'react-redux';
+import { storeHWAccounts, removeHWAccounts } from '../store/actions/actions';
 import { hwAccounts } from '../__fixtures__/hwAccounts';
+import { getHWAccounts } from '../utils/getHWAccounts';
 import useManageHWAccounts from './useManageHWAccounts';
 
 jest.useRealTimers();
-const mockHWAccounts = hwAccounts;
+
 const mockDispatch = jest.fn();
-const mockSelector = {
+const mockAppState = {
+  hardwareWallet: {
+    deviceId: 20231,
+    status: 'connected',
+  },
   settings: {
-    hardwareAccounts: {
-      'Nano S': mockHWAccounts,
-    },
+    hardwareAccounts: {},
   },
 };
 
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
   useDispatch: () => mockDispatch,
-  useSelector: (fn) => fn(mockSelector),
+  useSelector: jest.fn(),
 }));
 
-jest.mock('@hardwareWallet/manager/HWManager', () => ({
-  getActiveDeviceInfo: jest.fn(() => mockHWAccounts[0].hw),
-  getPublicKey: jest.fn(() => mockHWAccounts[0].metadata.pubkey),
-}));
-
-jest.mock('@liskhq/lisk-client', () => ({
-  ...jest.requireActual('@liskhq/lisk-client'),
-  cryptography: {
-    ...jest.requireActual('@liskhq/lisk-client').cryptography,
-    address: {
-      getAddressFromPublicKey: jest.fn(() => 'lskdgtenb76rf93bzd56cqn6ova46wfvoesbk4hnd'),
-    },
-  },
-}));
-
-jest.mock('@common/hooks/useCheckInitializedAccount');
-useCheckInitializedAccount.mockReturnValue(true);
+jest.mock('../utils/getHWAccounts');
+getHWAccounts.mockResolvedValue(hwAccounts);
 
 describe('useManageHWAccounts hook', () => {
-  renderHook(() => useManageHWAccounts());
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  it('stores the list of accounts in the hardware wallet', () => {
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
-    expect(mockDispatch).toMatchSnapshot();
+  it('stores the list of accounts in the hardware wallet', async () => {
+    useSelector.mockImplementation((callback) => callback(mockAppState));
+    const { waitFor } = renderHook(() => useManageHWAccounts());
+
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      const hwWalletAccountsDetails = storeHWAccounts(hwAccounts);
+      expect(mockDispatch).toHaveBeenCalledWith(expect.objectContaining(hwWalletAccountsDetails));
+    });
+  });
+
+  it('removes the list of accounts when the device is disconnected', async () => {
+    const updatedMockAppState = {
+      hardwareWallet: {
+        deviceId: 20231,
+        status: 'disconnected',
+        accounts: hwAccounts,
+      },
+      settings: {
+        hardwareAccounts: {
+          20231: [
+            {
+              address: 'a07e06d3d21bfcbd6e6cb158a0adc18b6ccc92db66dc8a323c7462af9c539fab',
+              name: 'masoud123',
+            },
+            {
+              address: '49dc55a205bf1fecbee41bde6e354eaa85fdc3c2809e85f5720a83986fad4b0a',
+              name: 'masoud456',
+            },
+          ],
+        },
+      },
+    };
+    useSelector.mockImplementation((callback) => callback(updatedMockAppState));
+    const { waitFor } = renderHook(() => useManageHWAccounts());
+
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(mockDispatch).toHaveBeenCalledWith(removeHWAccounts());
+    });
   });
 });
