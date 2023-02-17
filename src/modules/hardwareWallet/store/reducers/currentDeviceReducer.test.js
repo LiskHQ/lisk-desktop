4708d665bc341ba220016a8c35e5a1238423da3e
Oskar Fransson <oskar.leonard@hotmail.com>
2023-02-17 13:46:16 +0100

    Add test for HardwareWallet current device reducer

---
+++ src/modules/hardwareWallet/store/reducers/currentDeviceReducer.test.js
@@ -0,0 +1,26 @@
+import actionTypes from '../actions/actionTypes';
+import { currentDevice, initialState } from './currentDeviceReducer';
+
+describe('HardwareWallet current device reducer', () => {
+
+  it('updates current device', () => {
+    const action = {
+      type: actionTypes.deviceUpdate,
+      device: {
+        deviceId: '20231',
+        model: 'Nano S',
+        brand: 'Ledger',
+      }
+    };
+    const updatedState = currentDevice(initialState, action);
+    expect(updatedState).toEqual(action.device);
+  });
+
+  it('returns default state', () => {
+    const action = {
+      type: 'OTHER_ACTION_TYPE',
+    };
+    const updatedState = currentDevice(initialState, action);
+    expect(updatedState).toEqual(initialState);
+  });
+});
