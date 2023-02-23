a6e090d0e1402e7aa9354d51e95b2b1afb4a4148
eniolam1000752 <eniolam1000752@gmail.com>
2023-02-23 16:10:29 +0100

    fix: reverted removal of setApplications

--- src/modules/blockchainApplication/manage/store/actionTypes.js
+++ src/modules/blockchainApplication/manage/store/actionTypes.js
@@ -3,6 +3,7 @@ const actionTypes = {
   addApplicationByChainId: 'ADD_APPLICATION_BY_CHAIN_ID',
   deleteApplicationByChainId: 'DELETE_APPLICATION_BY_CHAIN_ID',
   setCurrentApplication: 'SET_CURRENT_APPLICATION',
+  setApplications: 'SET_APPLICATIONS',
 };
 
 export default actionTypes;
