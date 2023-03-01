17db5b257fec28fd0ebe8f5b58e08b65492b95d1
eniolam1000752 <eniolam1000752@gmail.com>
2023-03-01 01:44:11 +0100

    fix: replace convertion util on showNotificationsForIncomingTransaction

--- src/modules/transaction/utils/showNotificationsForIncomingTransactions.js
+++ src/modules/transaction/utils/showNotificationsForIncomingTransactions.js
@@ -1,11 +1,12 @@
 import { filterIncomingTransactions } from '@transaction/utils/helpers';
-import { fromRawLsk } from '@token/fungible/utils/lsk';
+import { convertFromRawDenom } from '@token/fungible/utils/lsk';
 import i18n from 'src/utils/i18n/i18n';
 import { toast } from 'react-toastify';
 
 export const showNotificationsForIncomingTransactions = (transactions, account, token) => {
   filterIncomingTransactions(transactions, account).forEach((transaction) => {
-    const amount = fromRawLsk(transaction.params.amount);
+    const amount = convertFromRawDenom(transaction.params.amount, token);
+
     if (amount > 0) {
       const message = transaction.params.data
         ? i18n.t('with message {{message}}', { message: transaction.params.data })
@@ -13,17 +14,17 @@ export const showNotificationsForIncomingTransactions = (transactions, account, 
       // @TODO: To be fixed in #4652
       // eslint-disable-next-line no-new
       new Notification(i18n.t('{{amount}} {{token}} Received', { amount, token }), {
-        body: i18n.t('Your account just received {{amount}} {{token}} {{message}}', {
+        body: i18n.t('Your account just received {{amount}} {{tokenSymbol}} {{message}}', {
           amount,
-          token,
           message,
+          tokenSymbol: token.symbol,
         }),
       });
       toast.info(
-        i18n.t('Your account just received {{amount}} {{token}} {{message}}', {
+        i18n.t('Your account just received {{amount}} {{tokenSymbol}} {{message}}', {
           amount,
-          token,
           message,
+          tokenSymbol: token.symbol,
         })
       );
     }
