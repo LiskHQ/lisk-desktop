a6b371bd9eca1489fbafd4288f78b29edc7699c6
eniolam1000752 <eniolam1000752@gmail.com>
2023-03-02 23:48:10 +0100

    fix: resolved unit test on MultiSignatureReview.js

--- src/modules/transaction/components/MultiSignatureReview/MultiSignatureReview.test.js
+++ src/modules/transaction/components/MultiSignatureReview/MultiSignatureReview.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { useTokensBalance } from '@token/fungible/hooks/queries';
+import { mockAppsTokens } from '@token/fungible/__fixtures__';
 import Review from './index';
+
+jest.mock('@token/fungible/hooks/queries');
 
 describe('Multisignature Review component', () => {
   let wrapper;
   const props = {
-    t: v => v,
+    t: (v) => v,
     members: [
       {
-        address: 'lskehj8am9afxdz8arztqajy52acnoubkzvmo9cjy', isMandatory: true,
+        address: 'lskehj8am9afxdz8arztqajy52acnoubkzvmo9cjy',
+        isMandatory: true,
       },
-      { address: 'lskehj0am9afxdz8arztqajy52acnoubkzvmo9cjy', isMandatory: false, publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a' },
+      {
+        address: 'lskehj0am9afxdz8arztqajy52acnoubkzvmo9cjy',
+        isMandatory: false,
+        publicKey: '0fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a',
+      },
       { address: 'lskehj1am9afxdz8arztqajy52acnoubkzvmo9cjy', isMandatory: false },
       { address: 'lskehj2am9afxdz8arztqajy52acnoubkzvmo9cjy', isMandatory: false },
       { address: 'lskehj3am9afxdz8arztqajy52acnoubkzvmo9cjy', isMandatory: false },
@@ -22,6 +31,8 @@ describe('Multisignature Review component', () => {
   beforeEach(() => {
     wrapper = mount(<Review {...props} />);
   });
+
+  useTokensBalance.mockReturnValue({ data: mockAppsTokens });
 
   it('Should render properly', () => {
     expect(wrapper).toContainMatchingElements(props.members.length, '.member-info');
