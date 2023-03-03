75e6b38e8bae2089432a73003559099a484a07db
eniolam1000752 <eniolam1000752@gmail.com>
2023-03-03 18:46:42 +0100

    fix: resolved broken test on Account

--- src/modules/wallet/components/walletList/index.js
+++ src/modules/wallet/components/walletList/index.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { QueryTable } from '@theme/QueryTable';
-import { useTokensTopLskBalance } from '@token/fungible/hooks/queries';
+import { useTokensBalance, useTokensTopLskBalance } from '@token/fungible/hooks/queries';
 import WalletRow from '../row';
 import header from './tableHeader';
 
 const WalletTable = () => {
   const [params, setParams] = useState({ sort: 'balance:desc' });
   const { t } = useTranslation();
+  const { data: tokens } = useTokensBalance();
+  const token = tokens?.data?.[0] || {};
 
   return (
     <QueryTable
@@ -17,6 +19,7 @@ const WalletTable = () => {
       queryConfig={{ config: { params } }}
       setParams={setParams}
       row={WalletRow}
+      additionalRowProps={{ token }}
       header={header(t)}
     />
   );
