e0ae4eb685d183a2e723e82e37a783dc0b8be07b
Oskar Fransson <oskar.leonard@hotmail.com>
2023-03-07 10:16:44 +0100

    Removed Dialog wrapper from HWReconnect

--- src/modules/hardwareWallet/components/HWReconnect/HWReconnect.js
+++ src/modules/hardwareWallet/components/HWReconnect/HWReconnect.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import Dialog from 'src/theme/dialog/dialog';
 import Box from '@theme/box';
 import BoxHeader from '@theme/box/header';
 import BoxContent from '@theme/box/content';
 import Illustration from '@common/components/illustration';
 import { TransactionConfirmFooter } from '@hardwareWallet/components/TransactionConfirmFooter/TransactionConfirmFooter';
+import { useTranslation } from 'react-i18next';
 import styles from './HWReconnect.css';
 
-const HWReconnect = () => (
-  <Dialog className={styles.container} hasClose hasBack size="sm">
-    <Box className={styles.wrapper}>
+const HWReconnect = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box width="medium" className={styles.HWReconnect}>
       <BoxHeader className={styles.header}>
         <div>
           <Illustration name="hwReconnection" />
         </div>
-        <h4>Reconnect to hardware wallet</h4>
+        <h4>{t('Reconnect to hardware wallet')}</h4>
       </BoxHeader>
       <BoxContent className={styles.content}>
         <div className={styles.details}>
-          <p>Your hardware wallet is disconnected.</p>
-          <p>Please reconnect your hardware wallet to sign this transaction</p>
+          <p>{t('Your hardware wallet is disconnected.')}</p>
+          <p>{t('Please reconnect your hardware wallet to sign this transaction')}</p>
         </div>
         <TransactionConfirmFooter />
       </BoxContent>
     </Box>
-  </Dialog>
-);
+  );
+};
 
 export default HWReconnect;
