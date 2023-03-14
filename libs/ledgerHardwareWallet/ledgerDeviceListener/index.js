9f736de0d262d1d85cd60ddc089ae2cded74455b
Oskar Fransson <oskar.leonard@hotmail.com>
2023-03-14 18:42:05 +0100

    Prettified

--- libs/ledgerHardwareWallet/ledgerDeviceListener/index.js
+++ libs/ledgerHardwareWallet/ledgerDeviceListener/index.js
@@ -3,7 +3,7 @@ import { LEDGER_HW_HID_EVENT } from '@libs/ledgerHardwareWallet/constants';
 
 export const ledgerDeviceListener = (win) => {
   TransportNodeHid.listen({
-    next: ({ type, device : fullDevice }) => {
+    next: ({ type, device: fullDevice }) => {
       const data = {
         type,
         device: {
