43399f07119a258eaa19b59893606ab7f8654920
Oskar Fransson <oskar.leonard@hotmail.com>
2023-03-21 16:32:47 +0100

    Added spaces

--- src/utils/usePrevious.js
+++ src/utils/usePrevious.js
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 
 export function usePrevious(value) {
   const ref = React.useRef();
+
   useEffect(() => {
     ref.current = value;
   }, [value]);
+
   return ref.current;
 }
