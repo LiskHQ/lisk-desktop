7d5f17d94565dac3f1cdfebdf958204ef5510916
eniolam1000752 <eniolam1000752@gmail.com>
2023-03-23 19:01:05 +0100

    fix: resolved unit test on SelectNode

--- src/modules/blockchainApplication/manage/components/SelectNode/SelectNode.test.js
+++ src/modules/blockchainApplication/manage/components/SelectNode/SelectNode.test.js
@@ -5,8 +5,10 @@ import mockBlockChainApplications, {
   applicationsMap,
 } from '@tests/fixtures/blockchainApplicationsManage';
 import SelectNode from './SelectNode';
+import { useApplicationManagement } from '../../hooks';
 
 const mockSetCurrentApplication = jest.fn();
+// const checkPinByChainId = jest.fn();
 const mockCurrentApplication = mockBlockChainApplications[0];
 
 const mockDispatch = jest.fn();
@@ -17,13 +19,20 @@ const mockState = {
     pins: [],
   },
 };
+jest.mock('@blockchainApplication/manage/hooks/useApplicationManagement');
 jest.mock('react-redux', () => ({
-  useSelector: jest.fn().mockImplementation((fn) => fn(mockState)),
+  useSelector: jest.fn().mockImplementation((fn) => fn({ devnet: mockState })),
   useDispatch: () => mockDispatch,
 }));
 
 jest.mock('../../hooks/useCurrentApplication', () => ({
   useCurrentApplication: () => [mockCurrentApplication, mockSetCurrentApplication],
+}));
+jest.mock('../../hooks/usePinBlockchainApplication', () => ({
+  usePinBlockchainApplication: () => ({
+    checkPinByChainId: jest.fn(),
+    pins: [],
+  }),
 }));
 jest.mock('@settings/hooks/useSettings');
 
@@ -44,9 +53,12 @@ describe('SelectNode', () => {
     mainChainNetwork: { name: 'devnet' },
     toggleSetting: mockToggleSetting,
   });
+  useApplicationManagement.mockReturnValue({
+    getApplicationByChainId: () => mockBlockChainApplications[4],
+  });
 
   it('Should render select node component', () => {
-    const selectedApplication = { ...mockBlockChainApplications[4], isPinned: false };
+    const selectedApplication = mockBlockChainApplications[4];
 
     renderWithRouterAndQueryClient(SelectNode, props);
     expect(screen.getByText('Kalipo')).toBeTruthy();
