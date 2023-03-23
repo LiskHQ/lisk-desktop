cb5d43eeca40d1e4df47cd80390fc01d57d7de05
eniolam1000752 <eniolam1000752@gmail.com>
2023-03-23 12:20:18 +0100

    fix: resolved inactive application list

--- src/modules/blockchainApplication/manage/hooks/useApplicationExploreAndMetaData.js
+++ src/modules/blockchainApplication/manage/hooks/useApplicationExploreAndMetaData.js
@@ -1,14 +1,16 @@
 import { useBlockchainApplicationExplore } from '../../explore/hooks/queries/useBlockchainApplicationExplore';
 import { useBlockchainApplicationMeta } from './queries/useBlockchainApplicationMeta';
+import { useCurrentApplication } from './useCurrentApplication';
 
 export const useApplicationExploreAndMetaData = ({ appState = 'active' } = {}) => {
+  const [currentApplication] = useCurrentApplication();
   const {
     data: { data: activeApps = [] } = {},
     isLoading: isLoadingActiveApps,
     error: errorGettingActiveApps,
   } = useBlockchainApplicationExplore({ config: { params: { status: appState } } });
 
-  const activeAppsList = activeApps.map((app) => app.chainID).join();
+  const activeAppsList = [currentApplication, ...activeApps].map((app) => app.chainID).join();
 
   const {
     data: { data: applications = [] } = {},
