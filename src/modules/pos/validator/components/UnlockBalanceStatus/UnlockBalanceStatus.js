17aaecd9f34c2642eadb9fa7d1520c4181065a4d
ManuGowda <manu@lightcurve.io>
2023-03-29 20:38:42 +0200

    :nail_care: Fix unlock view styles

--- src/modules/pos/validator/components/UnlockBalanceStatus/UnlockBalanceStatus.js
+++ src/modules/pos/validator/components/UnlockBalanceStatus/UnlockBalanceStatus.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { PrimaryButton } from 'src/theme/buttons';
 import TxBroadcaster from '@transaction/components/TxBroadcaster';
 import { getTransactionStatus, statusMessages } from '@transaction/configuration/statusConfig';
-import styles from './unlockBalanceStatus.css';
 
 const TransactionStatus = ({ account, transactions, t }) => {
   const status = getTransactionStatus(account, transactions, account.summary.isMultisignature);
   const template = statusMessages(t)[status.code];
 
   return (
-    <div className={`${styles.wrapper} transaction-status`}>
+    <div className="transaction-status">
       <TxBroadcaster
         illustration="default"
         status={status}
         title={template.title}
         message={template.message}
-        className={styles.content}
       >
         {template.button && (
           <PrimaryButton
