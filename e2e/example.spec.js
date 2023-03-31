ef853906308a1e0ecba6628938bdfc69d983c499
eniolam1000752 <eniolam1000752@gmail.com>
2023-03-31 12:47:35 +0100

    fix: updated spec file

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -3,12 +3,6 @@ const { test, expect } = require('@playwright/test');
 
 test('visit add account page', async ({ page }) => {
   await page.goto('/');
-  
-  await expect(page).toHaveTitle(/Lisk/);
-});
 
-test('visit add account page', async ({ page }) => {
-  await page.goto('/');
-  
   await expect(page).toHaveTitle(/Lisk/);
 });
