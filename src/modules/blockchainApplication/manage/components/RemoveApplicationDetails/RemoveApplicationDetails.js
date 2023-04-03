860bd6d83308c7b6107e9217a7dc67a92ebb4670
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-04-03 12:54:22 +0100

    Refactored application removal modal

--- src/modules/blockchainApplication/manage/components/RemoveApplicationDetails/RemoveApplicationDetails.js
+++ src/modules/blockchainApplication/manage/components/RemoveApplicationDetails/RemoveApplicationDetails.js
@@ -1,37 +1,66 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import moment from 'moment';
-import Box from 'src/theme/box';
+import Box from '@theme/box';
 import TokenAmount from '@token/fungible/components/tokenAmount';
-import ValueAndLabel from 'src/modules/transaction/components/TransactionDetails/valueAndLabel';
-import CopyToClipboard from 'src/modules/common/components/copyToClipboard';
-import { TertiaryButton, PrimaryButton, OutlineButton } from 'src/theme/buttons';
+import ValueAndLabel from '@transaction/components/TransactionDetails/valueAndLabel';
+import CopyToClipboard from '@common/components/copyToClipboard';
+import { TertiaryButton, PrimaryButton, OutlineButton } from '@theme/buttons';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
 import Dialog from '@theme/dialog/dialog';
-import Icon from 'src/theme/Icon';
-import Tooltip from 'src/theme/Tooltip';
+import Icon from '@theme/Icon';
+import Tooltip from '@theme/Tooltip';
 import { parseSearchParams } from 'src/utils/searchParams';
-import { usePinBlockchainApplication } from '@blockchainApplication/manage/hooks/usePinBlockchainApplication';
 import { getLogo } from '@token/fungible/utils/helpers';
+import Skeleton from '@common/components/skeleton/Skeleton';
+import Illustration from 'src/modules/common/components/illustration';
+import { usePinBlockchainApplication } from '../../hooks/usePinBlockchainApplication';
+import { useBlockchainApplicationExplore } from '../../../explore/hooks/queries/useBlockchainApplicationExplore';
+import { useBlockchainApplicationMeta } from '../../hooks/queries/useBlockchainApplicationMeta';
 import styles from './RemoveApplicationDetails.css';
 // import useApplicationManagement from '../../hooks/useApplicationManagement';
 
 const deposit = 5e10;
-const serviceUrl = 'https://lisk.com/';
 
-const RemoveApplicationDetails = ({ location, application, onCancel, nextStep }) => {
+// eslint-disable-next-line max-statements, complexity
+const RemoveApplicationDetails = ({ location, onCancel, nextStep }) => {
   const { t } = useTranslation();
   const chainId = parseSearchParams(location.search)?.chainId;
+  const {
+    data: onChainData,
+    refetch: refetchOnChainData,
+    isLoading: onChainLoading,
+    isError: isOnChainDataError,
+  } = useBlockchainApplicationExplore({
+    config: { params: { chainID: chainId } },
+    options: { enabled: !!chainId },
+  });
+  const {
+    data: offChainData,
+    refetch: refetchOffChainData,
+    isLoading: offChainLoading,
+    isError: isOffChainDataError,
+  } = useBlockchainApplicationMeta({
+    config: { params: { chainID: chainId } },
+    options: { enabled: !!chainId },
+  });
+  const application = { ...onChainData?.data[0], ...offChainData?.data[0] };
   // TODO: this needs to be reinstated when the set application flow is completed
   // because presently, there is no way to set current application on runtime
 
   // const { deleteApplicationByChainId } = useApplicationManagement();
   const { checkPinByChainId, togglePin } = usePinBlockchainApplication();
-  const { chainName, status, address, lastCertificateHeight, lastUpdated } = application.data;
+  const { chainName, status, address, lastCertificateHeight, lastUpdated, projectPage } =
+    application || {};
 
   const isPinned = checkPinByChainId(chainId);
   const toggleApplicationPin = () => {
     togglePin(chainId);
+  };
+
+  const reloadAppDetails = () => {
+    refetchOnChainData();
+    refetchOffChainData();
   };
 
   const footerDetails = [
@@ -51,7 +80,7 @@ const RemoveApplicationDetails = ({ location, application, onCancel, nextStep })
     {
       header: t('Last Update'),
       className: `${styles.detailContentText} last-update`,
-      content: moment(lastUpdated).format('DD MMM YYYY'),
+      content: moment(lastUpdated * 1000).format('DD MMM YYYY'),
     },
     {
       header: t('Last Certificate Height'),
@@ -68,78 +97,109 @@ const RemoveApplicationDetails = ({ location, application, onCancel, nextStep })
     nextStep({ application });
   };
 
+  if (isOnChainDataError || isOffChainDataError)
+    return (
+      <Dialog hasClose className={`${grid.row} ${grid['center-xs']}`}>
+        <div className={`${styles.wrapper} ${styles.errorWrapper}`}>
+          <Illustration name="applicationDetailsError" />
+          <div className={styles.errorText}>{t("Couldn't load application data")}</div>
+          <div className={styles.retryBtn}>
+            <TertiaryButton onClick={reloadAppDetails}>Try again</TertiaryButton>
+          </div>
+        </div>
+      </Dialog>
+    );
+
   return (
     <Dialog className={`${styles.dialogWrapper} ${grid.row} ${grid['center-xs']}`}>
       <div className={styles.wrapper}>
-        <Box className={styles.headerContainer}>
-          <p>{t('Remove application')}</p>
-          <div>
-            <img src={getLogo(application.data)} />
-          </div>
-        </Box>
+        {onChainLoading || offChainLoading ? (
+          <Skeleton className={styles.skeleton} width="25%" />
+        ) : (
+          <Box className={styles.headerContainer}>
+            <p>{t('Remove application')}</p>
+            <div>
+              <img src={getLogo(application)} />
+            </div>
+          </Box>
+        )}
         <Box className={styles.detailsWrapper}>
-          <div className={styles.chainNameWrapper}>
-            <span className="chain-name-text">{chainName}</span>
-            <TertiaryButton onClick={toggleApplicationPin}>
-              <Icon data-testid="pin-button" name={isPinned ? 'pinnedIcon' : 'unpinnedIcon'} />
-            </TertiaryButton>
-          </div>
-          <Box className={styles.addressRow}>
-            <ValueAndLabel className={styles.transactionId}>
-              <span className="copy-address-wrapper">
-                <CopyToClipboard
-                  text={address}
-                  value={address}
-                  className="tx-id"
-                  containerProps={{
-                    size: 'xs',
-                    className: 'copy-address',
-                  }}
-                />
-              </span>
-            </ValueAndLabel>
-          </Box>
-          <div className={styles.addressRow}>
-            <a
-              className={`${styles.appLink}`}
-              target="_blank"
-              // eslint-disable-next-line
-              // TODO: this is just a place holder link pending when its part of the response payload from service
-              href={serviceUrl}
-            >
-              <Icon name="chainLinkIcon" className={styles.hwWalletIcon} />
-              {t(serviceUrl)}
-            </a>
-          </div>
-          <div className={styles.balanceRow}>
-            <span>{t('Deposited:')}</span>
-            {/* TODO: this is a placeholder value pending when its part of service response */}
-            <span>
-              <TokenAmount isLsk val={deposit} />
-            </span>
-          </div>
-          <Box className={styles.footerDetailsRow}>
-            {footerDetails.map(({ header, content, className }, index) => (
-              <ValueAndLabel
-                key={index}
-                className={styles.detail}
-                label={
-                  <span className={styles.headerText}>
-                    <>
-                      {header.text || header}
-                      {header.toolTipText && (
-                        <Tooltip position="right">
-                          <p>{header.toolTipText}</p>
-                        </Tooltip>
-                      )}
-                    </>
-                  </span>
-                }
-              >
-                <span className={className}>{content}</span>
+          {onChainLoading || offChainLoading ? (
+            <Skeleton className={styles.skeleton} width="25%" />
+          ) : (
+            <div className={styles.chainNameWrapper}>
+              <span className="chain-name-text">{chainName}</span>
+              <TertiaryButton onClick={toggleApplicationPin}>
+                <Icon data-testid="pin-button" name={isPinned ? 'pinnedIcon' : 'unpinnedIcon'} />
+              </TertiaryButton>
+            </div>
+          )}
+          {onChainLoading || offChainLoading ? (
+            <Skeleton className={styles.skeleton} width="25%" />
+          ) : (
+            <Box className={styles.addressRow}>
+              <ValueAndLabel className={styles.transactionId}>
+                <span className="copy-address-wrapper">
+                  <CopyToClipboard
+                    text={address}
+                    value={address}
+                    className="tx-id"
+                    containerProps={{
+                      size: 'xs',
+                      className: 'copy-address',
+                    }}
+                  />
+                </span>
               </ValueAndLabel>
-            ))}
-          </Box>
+            </Box>
+          )}
+          {onChainLoading || offChainLoading ? (
+            <Skeleton className={styles.skeleton} width="25%" />
+          ) : (
+            <div className={styles.addressRow}>
+              <a className={`${styles.appLink}`} target="_blank" href={projectPage}>
+                <Icon name="chainLinkIcon" className={styles.hwWalletIcon} />
+                {t(projectPage)}
+              </a>
+            </div>
+          )}
+          {onChainLoading || offChainLoading ? (
+            <Skeleton className={styles.skeleton} width="25%" />
+          ) : (
+            <div className={styles.balanceRow}>
+              <span>{t('Deposited:')}</span>
+              {/* TODO: this is a placeholder value pending when its part of service response */}
+              <span>
+                <TokenAmount isLsk val={deposit} />
+              </span>
+            </div>
+          )}
+          {onChainLoading || offChainLoading ? (
+            <Skeleton className={styles.skeleton} width="25%" />
+          ) : (
+            <Box className={styles.footerDetailsRow}>
+              {footerDetails.map(({ header, content, className }, index) => (
+                <ValueAndLabel
+                  key={index}
+                  className={styles.detail}
+                  label={
+                    <span className={styles.headerText}>
+                      <>
+                        {header.text || header}
+                        {header.toolTipText && (
+                          <Tooltip position="right">
+                            <p>{header.toolTipText}</p>
+                          </Tooltip>
+                        )}
+                      </>
+                    </span>
+                  }
+                >
+                  <span className={className}>{content}</span>
+                </ValueAndLabel>
+              ))}
+            </Box>
+          )}
           <Box className={styles.actionsRow}>
             <OutlineButton
               className={`${styles.button} ${styles.cancelButton} cancel-remove-blockchain`}
