b159b0f40c59046d613c8defd19eb9e489041284
eniolam1000752 <eniolam1000752@gmail.com>
2023-04-05 17:42:42 +0100

    fix: claimRewards view UI

--- src/modules/pos/validator/components/ClaimRewardsView/index.js
+++ src/modules/pos/validator/components/ClaimRewardsView/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dialog from 'src/theme/dialog/dialog';
 import MultiStep from 'src/modules/common/components/OldMultiStep';
 import TxSignatureCollector from '@transaction/components/TxSignatureCollector';
@@ -6,15 +6,23 @@ import ClaimRewardsSummary from '@pos/validator/components/ClaimRewardsSummary';
 import TxBroadcasterWithStatus from '@transaction/components/TxBroadcasterWithStatus/TxBroadcasterWithStatus';
 import ClaimRewardsForm from '../ClaimRewardsForm';
 
-const ClaimRewardsView = () => (
-  <Dialog hasClose>
-    <MultiStep>
-      <ClaimRewardsForm />
-      <ClaimRewardsSummary />
-      <TxSignatureCollector />
-      <TxBroadcasterWithStatus />
-    </MultiStep>
-  </Dialog>
-);
+const ClaimRewardsView = () => {
+  const [isStepTxSignatureCollector, setIsStepTxSignatureCollector] = useState(false);
+
+  const onMultiStepChange = ({ step: { current } }) => {
+    setIsStepTxSignatureCollector(current === 2);
+  };
+
+  return (
+    <Dialog hasClose size={isStepTxSignatureCollector && 'sm'}>
+      <MultiStep onChange={onMultiStepChange}>
+        <ClaimRewardsForm />
+        <ClaimRewardsSummary />
+        <TxSignatureCollector />
+        <TxBroadcasterWithStatus />
+      </MultiStep>
+    </Dialog>
+  );
+};
 
 export default ClaimRewardsView;
