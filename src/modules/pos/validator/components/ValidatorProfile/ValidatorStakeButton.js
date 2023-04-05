eb7b6954ab0446ad43db6b356f23eb261ae39c7a
eniolam1000752 <eniolam1000752@gmail.com>
2023-04-05 17:04:03 +0100

    fix: resolved edit stake status modal

--- src/modules/pos/validator/components/ValidatorProfile/ValidatorStakeButton.js
+++ src/modules/pos/validator/components/ValidatorProfile/ValidatorStakeButton.js
@@ -7,7 +7,7 @@ import { selectStaking } from 'src/redux/selectors';
 import { PrimaryButton, SecondaryButton } from 'src/theme/buttons';
 import { useSentStakes } from '../../hooks/queries';
 
-function ValidatorStakeButton({ address, isBanned, currentAddress }) {
+function ValidatorStakeButton({ address, isBanned, currentAddress, isDisabled }) {
   const { t } = useTranslation();
   const staking = useSelector((state) => selectStaking(state));
   const { data: sentStakes, isLoading: sentStakesLoading } = useSentStakes({
@@ -26,11 +26,11 @@ function ValidatorStakeButton({ address, isBanned, currentAddress }) {
   return (
     <DialogLink component="editStake">
       {isEdit ? (
-        <SecondaryButton disabled={sentStakesLoading || isBanned || !currentAddress}>
+        <SecondaryButton disabled={sentStakesLoading || isBanned || !currentAddress || isDisabled}>
           {t('Edit stake')}
         </SecondaryButton>
       ) : (
-        <PrimaryButton>{t('Stake validator')}</PrimaryButton>
+        <PrimaryButton disabled={isDisabled}>{t('Stake validator')}</PrimaryButton>
       )}
     </DialogLink>
   );
