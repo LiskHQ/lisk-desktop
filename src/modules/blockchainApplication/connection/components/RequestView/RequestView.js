8077471700f89ca1101a5879a1fae1dfc3f1fbf6
eniolam1000752 <eniolam1000752@gmail.com>
2023-04-26 16:18:11 +0100

    fix: resoved issue password collection step on wc request

--- src/modules/blockchainApplication/connection/components/RequestView/RequestView.js
+++ src/modules/blockchainApplication/connection/components/RequestView/RequestView.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useState, useCallback } from 'react';
 import routes from 'src/routes/routes';
 import MultiStep from '@common/components/OldMultiStep';
 import TxSignatureCollector from '@transaction/components/TxSignatureCollector';
@@ -10,14 +9,23 @@ import RequestSummary from '../RequestSummary';
 import styles from './requestView.css';
 
 const RequestView = ({ history }) => {
-  // istanbul ignore next
+  const [isStepTxSignatureCollector, setIsStepTxSignatureCollector] = useState(false);
   const backToWallet = () => {
     history.push(routes.wallet.path);
   };
 
+  const onMultiStepChange = useCallback(({ step: { current } }) => {
+    setIsStepTxSignatureCollector([2, 3].includes(current));
+  }, []);
+
   return (
-    <Dialog hasClose className={styles.dialogWrapper}>
-      <MultiStep key="RequestView" finalCallback={backToWallet} className={styles.wrapper}>
+    <Dialog hasClose className={styles.dialogWrapper} size={isStepTxSignatureCollector && 'sm'}>
+      <MultiStep
+        onChange={onMultiStepChange}
+        key="RequestView"
+        finalCallback={backToWallet}
+        className={styles.wrapper}
+      >
         <RequestSummary history={history} />
         <Summary />
         <TxSignatureCollector />
