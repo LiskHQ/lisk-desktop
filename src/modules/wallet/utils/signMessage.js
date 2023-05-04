12a30c04e11d058b49a3af2e68fc640b87cffe34
Oskar Fransson <oskar.leonard@hotmail.com>
2023-05-04 15:33:16 +0200

    Renamed functions to not have same name

--- src/modules/wallet/utils/signMessage.js
+++ src/modules/wallet/utils/signMessage.js
@@ -2,34 +2,13 @@ import { cryptography } from '@liskhq/lisk-client';
 import { signMessageByHW } from './hwManager';
 
 /**
- * Signs a string with the users private key
- * @param {Object} signData - consists of the message and account information
- * @param {string} signData.message - A signed value of the message
- * @param {Object} signData.account - Account of the signed in user
- * @return {string} a signed value of the message
- */
-export const signUsingPrivateKey = ({ message, account }) => {
-  const msgBytes = cryptography.ed.digestMessage(message);
-  const signature = cryptography.ed.signDataWithPrivateKey(
-    msgBytes,
-    Buffer.from(account.summary.privateKey, 'hex')
-  );
-  const result = cryptography.ed.printSignedMessage({
-    message,
-    signature,
-    publicKey: account.summary.publicKey,
-  });
-  return result;
-};
-
-/**
  * Signs a string using hardware wallet
  * @param {Object} signData - consists of the message and account information
  * @param {string} signData.message - A signed value of the message
  * @param {Object} signData.account - Account of the signed in user
  * @return {string} a signed value of the message
  */
-export const signUsingHW = async ({ message, account }) => {
+export const signMessageUsingHW = async ({ message, account }) => {
   let signature = await signMessageByHW({
     account,
     message,
@@ -40,7 +19,7 @@ export const signUsingHW = async ({ message, account }) => {
   const result = cryptography.ed.printSignedMessage({
     message,
     signature,
-    publicKey: account.summary.publicKey,
+    publicKey: account.metadata.pubkey,
   });
   return result;
 };
