00277039cb0b43adc86e81d843339b98a412032b
Oskar Fransson <oskar.leonard@hotmail.com>
2023-05-15 10:42:34 +0200

    Added optional chaining

--- src/modules/hardwareWallet/store/selectors/hwSelectors.js
+++ src/modules/hardwareWallet/store/selectors/hwSelectors.js
@@ -1,5 +1,5 @@
-export const selectHardwareDevices = (state) => state.hardwareWallet.devices || [];
-export const selectCurrentHWDevice = (state) => state.hardwareWallet.currentDevice || {};
+export const selectHardwareDevices = (state) => state.hardwareWallet?.devices || [];
+export const selectCurrentHWDevice = (state) => state.hardwareWallet?.currentDevice || {};
 export const selectCurrentHWDevicePath = (state) => selectCurrentHWDevice(state).path;
 export const selectHWStatus = (state) => selectCurrentHWDevice(state).status;
 export const selectHWAccounts = (state) => state.hardwareWallet?.accounts || [];
