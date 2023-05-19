a4cd3c60e54747df1454ca19f492ea2f801285dc
ManuGowda <manu@lightcurve.io>
2023-05-19 12:10:05 +0200

    :recycle: Update outdated mapbox access token

--- src/modules/network/components/map/index.js
+++ src/modules/network/components/map/index.js
@@ -51,7 +51,7 @@ const getAttributionLinks = () => {
 
 const getTiles = () =>
   L.tileLayer(
-    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFudWxpZ2h0Y3VydmUiLCJhIjoiY2xodWVjYW52MGM0cDNobzYzZGRkOGU0dyJ9.aOMEZq0mbD6C-DgwU_ib_A',
     {
       id: 'mapbox/streets-v11',
       tileSize: 512,
