666cef57762f82704fc5179ec3b70b2e969b8521
eniolam1000752 <eniolam1000752@gmail.com>
2023-06-01 20:31:22 +0100

    feature: tested ApplicationBootstrap

---
+++ setup/react/app/ApplicationBootstrap.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { waitFor } from '@testing-library/react';
+import mockManagedApplications from '@tests/fixtures/blockchainApplicationsManage';
+import {
+  useApplicationManagement,
+  useCurrentApplication,
+} from '@blockchainApplication/manage/hooks';
+import { useBlockchainApplicationMeta } from '@blockchainApplication/manage/hooks/queries/useBlockchainApplicationMeta';
+import { mockBlockchainAppMeta } from '@blockchainApplication/manage/__fixtures__/mockBlockchainAppMeta';
+import useSettings from '@settings/hooks/useSettings';
+import networks from '@network/configuration/networks';
+import { renderWithQueryClient, smartRender } from 'src/utils/testHelpers';
+import ApplicationBootstrap from './ApplicationBootstrap';
+
+jest.mock('@blockchainApplication/manage/hooks/queries/useBlockchainApplicationMeta');
+jest.mock('@blockchainApplication/manage/hooks/useApplicationManagement');
+jest.mock('@settings/hooks/useSettings');
+jest.mock('@blockchainApplication/manage/hooks/useCurrentApplication');
+
+const props = {
+  children: <span>testing</span>,
+};
+
+const mockSetCurrentApplication = jest.fn();
+const mockSetApplications = jest.fn();
+
+const renderConfig = {
+  renderType: 'mount',
+  historyInfo: {
+    push: jest.fn(),
+    location: { search: '' },
+  },
+  queryClient: true,
+};
+
+describe('ApplicationBootstrap', () => {
+  useSettings.mockReturnValue({
+    customNetworks: [],
+    mainChainNetwork: networks.devnet,
+  });
+  useCurrentApplication.mockReturnValue([mockManagedApplications[1], mockSetCurrentApplication]);
+  useApplicationManagement.mockReturnValue({
+    applications: mockManagedApplications,
+    setApplications: mockSetApplications,
+  });
+
+  mockBlockchainAppMeta.data[0] = { ...mockBlockchainAppMeta.data[0], chainID: '04000000' };
+
+  useBlockchainApplicationMeta.mockReturnValue({
+    data: { data: mockBlockchainAppMeta.data },
+    isLoading: false,
+    isError: undefined,
+  });
+
+  it('Should set main chain application for the selected network', async () => {
+    smartRender(ApplicationBootstrap, props, renderConfig);
+
+    await waitFor(() => {
+      expect(mockSetApplications).toHaveBeenCalledWith([mockBlockchainAppMeta.data[0]]);
+      expect(mockSetCurrentApplication).toHaveBeenCalledWith(mockBlockchainAppMeta.data[1]);
+    });
+  });
+});
