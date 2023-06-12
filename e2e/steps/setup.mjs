6fce9af180cf9e8413b3ce1f5a4c7d908e7d037e
eniolam1000752 <eniolam1000752@gmail.com>
2023-06-12 15:55:03 +0100

    fix: removed headless mode

--- e2e/steps/setup.mjs
+++ e2e/steps/setup.mjs
@@ -6,7 +6,7 @@ class CustomWorld {
     this.pageBaseUrl = process.env.PW_BASE_URL;
 
     const browser = await playwright.chromium.launch({
-      headless: true,
+      headless: false,
     });
     const context = await browser.newContext();
 
