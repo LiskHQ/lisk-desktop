86be3c728588084fc3cfae57464ea318734387d9
Oskar Fransson <oskar.leonard@hotmail.com>
2023-06-16 15:36:03 +0200

    added test for checkCommission

--- src/modules/pos/validator/utils/checkCommissionValidity.test.js
+++ src/modules/pos/validator/utils/checkCommissionValidity.test.js
@@ -1,4 +1,4 @@
-import { checkCommissionValidity } from './checkCommissionValidity';
+import { checkCommissionValidity, isCommissionIncrease } from './checkCommissionValidity';
 
 describe('checkCommissionValidity', () => {
   it('returns true if new commission is less than old commission', () => {
@@ -11,5 +11,13 @@ describe('checkCommissionValidity', () => {
 
   it('returns false if new commission is greater than old commission and difference is more than 5%', () => {
     expect(checkCommissionValidity('70.00', '50.00')).toBe(false);
+  });
+
+  it('should correctly calculate if new commission is greater than old commission', () => {
+    expect(isCommissionIncrease('70.00', '50.00')).toBe(true);
+  });
+
+  it('should correctly calculate if new commission is greater than old commission', () => {
+    expect(isCommissionIncrease('40.00', '50.00')).toBe(false);
   });
 });
