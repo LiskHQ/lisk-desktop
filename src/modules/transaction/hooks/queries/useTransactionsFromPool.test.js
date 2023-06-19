50a9ec435d11b0dd3577fe84c5675756eb17b3f0
Oskar Fransson <oskar.leonard@hotmail.com>
2023-06-19 13:37:59 +0200

    added address to test

--- src/modules/transaction/hooks/queries/useTransactionsFromPool.test.js
+++ src/modules/transaction/hooks/queries/useTransactionsFromPool.test.js
@@ -8,7 +8,10 @@ beforeEach(() => jest.clearAllMocks());
 
 describe('useTransactionsFromPool hook', () => {
   it('should return status from request', async () => {
-    const { result, waitFor } = renderHook(() => useTransactionsFromPool(), { wrapper });
+    const { result, waitFor } = renderHook(
+      () => useTransactionsFromPool({ address: 'lsktzb4j7e3knk4mkxckdr3y69gtu2nwmsb3hjbkg' }),
+      { wrapper }
+    );
     expect(result.current.isLoading).toBeTruthy();
     await waitFor(() => result.current.isFetched);
     expect(result.current.isSuccess).toBeTruthy();
