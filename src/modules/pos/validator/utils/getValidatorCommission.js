a3231196fbfba1a89045613831f1768ccca07025
Oskar Fransson <oskar.leonard@hotmail.com>
2023-06-28 22:31:28 +0200

    Updated test and function to use toFixed(0)

--- src/modules/pos/validator/utils/getValidatorCommission.js
+++ src/modules/pos/validator/utils/getValidatorCommission.js
@@ -2,4 +2,7 @@ import { VALIDATOR_COMMISSION_DIVISOR } from '../consts/validators';
 
 export const convertCommissionToPercentage = (val = 0) =>
   Math.abs(val / VALIDATOR_COMMISSION_DIVISOR).toFixed(2);
-export const convertCommissionToNumber = (val = '0') => parseInt(val.replace('.', ''), 10);
+export const convertCommissionToNumber = (val = '0') => {
+  const floatString = parseFloat(val).toFixed(2);
+  return parseFloat(floatString.replace('.', ''))
+};
