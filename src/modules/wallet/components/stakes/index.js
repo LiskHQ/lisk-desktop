131f1514c06c302a738201ba8de054487e63816f
ManuGowda <manu@lightcurve.io>
2023-07-03 13:40:17 +0200

    :recycle: Remove unused stakes component

--- src/modules/wallet/components/stakes/index.js
+++ src/modules/wallet/components/stakes/index.js
@@ -2,22 +2,11 @@
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withTranslation } from 'react-i18next';
-import { getStakes } from '@pos/validator/api';
-import withData from 'src/utils/withData';
 import Stakes from './stakes';
-
-const apis = {
-  stakes: {
-    apiUtil: (network, params) => getStakes({ network, params }),
-    defaultData: [],
-    autoload: false,
-    transformResponse: (response) => response.data?.stakes ?? [],
-  },
-};
 
 const mapStateToProps = (state) => ({
   sentStakes: state.staking,
   isValidator: state.wallet?.info?.LSK?.summary.isValidator,
 });
 
-export default compose(connect(mapStateToProps), withData(apis), withTranslation())(Stakes);
+export default compose(connect(mapStateToProps), withTranslation())(Stakes);
