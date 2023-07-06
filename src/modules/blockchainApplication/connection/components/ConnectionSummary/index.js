58a4c19442acb20cf304efb13d48efa2e16321dc
Oskar Fransson <oskar.leonard@hotmail.com>
2023-07-06 15:02:07 +0200

    Show error dialog if requiredNamespaces doesnt contain only Lisk

--- src/modules/blockchainApplication/connection/components/ConnectionSummary/index.js
+++ src/modules/blockchainApplication/connection/components/ConnectionSummary/index.js
@@ -9,6 +9,7 @@ import { EVENTS, ACTIONS } from '@libs/wcm/constants/lifeCycle';
 import { addSearchParamsToUrl } from 'src/utils/searchParams';
 import { useEvents } from '@libs/wcm/hooks/useEvents';
 import { useSession } from '@libs/wcm/hooks/useSession';
+import classNames from 'classnames';
 import BlockchainAppDetailsHeader from '../../../explore/components/BlockchainAppDetailsHeader';
 import AccountsSelector from './AccountsSelector';
 import styles from './connectionSummary.css';
@@ -20,6 +21,24 @@ const ConnectionSummary = () => {
   const { t } = useTranslation();
   const { events } = useEvents();
   const { approve, reject } = useSession();
+
+  // istanbul ignore next
+  if (!events.length || events[events.length - 1].name !== EVENTS.SESSION_PROPOSAL) {
+    return <div>{t('Connection summary is not ready yet.')}</div>;
+  }
+
+  const { proposer, requiredNamespaces, pairingTopic } = events[events.length - 1].meta.params;
+  const nameSpaceKeys = Object.keys(requiredNamespaces);
+  const nameSpaceError = nameSpaceKeys?.length > 1 || !nameSpaceKeys.includes('lisk');
+
+  const application = {
+    data: {
+      name: proposer.metadata.name,
+      projectPage: proposer.metadata.url.replace(/\/$/, ''),
+      icon: proposer.metadata.icons[0],
+      address: `Chain ID: ${requiredNamespaces.lisk.chains[0].replace('lisk:', '')}`,
+    },
+  };
 
   const connectHandler = async () => {
     const result = await approve(addresses);
@@ -41,72 +60,79 @@ const ConnectionSummary = () => {
     });
   };
 
-  // istanbul ignore next
-  if (!events.length || events[events.length - 1].name !== EVENTS.SESSION_PROPOSAL) {
-    return <div>{t('Connection summary is not ready yet.')}</div>;
-  }
-
-  const { proposer, requiredNamespaces, pairingTopic } = events[events.length - 1].meta.params;
-  const application = {
-    data: {
-      name: proposer.metadata.name,
-      projectPage: proposer.metadata.url.replace(/\/$/, ''),
-      icon: proposer.metadata.icons[0],
-      address: `Chain ID: ${requiredNamespaces.lisk.chains[0].replace('lisk:', '')}`,
-    },
-  };
-
   return (
-    <Dialog hasClose className={`${styles.dialogWrapper} ${grid.row} ${grid['center-xs']}`}>
-      <BlockchainAppDetailsHeader application={application} />
-      <div className={styles.wrapper}>
-        <section className={styles.section}>
-          <ValueAndLabel
-            className={styles.labeledValue}
-            label={t('Account(s) to use on this application')}
-          >
-            <AccountsSelector setAddresses={setAddresses} addresses={addresses} />
-          </ValueAndLabel>
-        </section>
-        <section className={styles.section}>
-          <ValueAndLabel className={styles.labeledValue} label={t('Connection ID')}>
-            <span className="pairing-topic">{pairingTopic}</span>
-          </ValueAndLabel>
-        </section>
-        <section className={`${styles.section} ${styles.permissions}`}>
-          <span className={styles.label}>{t('Site permissions')}</span>
-          <div className={styles.twoColumn}>
-            <ValueAndLabel label={t('Methods')}>
-              <div className={`${styles.items} methods`}>
-                {requiredNamespaces.lisk.methods.map((method) => (
-                  <span key={method} className={styles.label}>
-                    {method}
-                  </span>
-                ))}
+    <Dialog
+      hasClose
+      className={classNames(
+        styles.dialogWrapper,
+        grid.row,
+        grid['center-xs'],
+        nameSpaceError && styles.dialogError
+      )}
+    >
+      {nameSpaceError ? (
+        <div className={classNames(styles.errorWrapper)}>
+          <h2 className={classNames(styles.unsupportedDAppTitle)}>{t('Unsupported connection')}</h2>
+          <p className={classNames(styles.text)}>
+            {t(`You are trying to connect: ${nameSpaceKeys.toString()}`)}
+          </p>
+          <p className={classNames(styles.text)}>
+            {t('Only the lisk dApp can connect to this application')}
+          </p>
+        </div>
+      ) : (
+        <>
+          <BlockchainAppDetailsHeader application={application} />
+          <div className={styles.wrapper}>
+            <section className={styles.section}>
+              <ValueAndLabel
+                className={styles.labeledValue}
+                label={t('Account(s) to use on this application')}
+              >
+                <AccountsSelector setAddresses={setAddresses} addresses={addresses} />
+              </ValueAndLabel>
+            </section>
+            <section className={styles.section}>
+              <ValueAndLabel className={styles.labeledValue} label={t('Connection ID')}>
+                <span className="pairing-topic">{pairingTopic}</span>
+              </ValueAndLabel>
+            </section>
+            <section className={`${styles.section} ${styles.permissions}`}>
+              <span className={styles.label}>{t('Site permissions')}</span>
+              <div className={styles.twoColumn}>
+                <ValueAndLabel label={t('Methods')}>
+                  <div className={`${styles.items} methods`}>
+                    {requiredNamespaces.lisk.methods.map((method) => (
+                      <span key={method} className={styles.label}>
+                        {method}
+                      </span>
+                    ))}
+                  </div>
+                </ValueAndLabel>
+                <ValueAndLabel label={t('Events')}>
+                  <div className={`${styles.items} events`}>
+                    {requiredNamespaces.lisk.events.length ? (
+                      requiredNamespaces.lisk.events.map((event) => (
+                        <span key={event} className={styles.label}>
+                          {event}
+                        </span>
+                      ))
+                    ) : (
+                      <span className={styles.label}>-</span>
+                    )}
+                  </div>
+                </ValueAndLabel>
               </div>
-            </ValueAndLabel>
-            <ValueAndLabel label={t('Events')}>
-              <div className={`${styles.items} events`}>
-                {requiredNamespaces.lisk.events.length ? (
-                  requiredNamespaces.lisk.events.map((event) => (
-                    <span key={event} className={styles.label}>
-                      {event}
-                    </span>
-                  ))
-                ) : (
-                  <span className={styles.label}>-</span>
-                )}
-              </div>
-            </ValueAndLabel>
+            </section>
+            <footer className={styles.section}>
+              <SecondaryButton onClick={rejectHandler}>{t('Cancel')}</SecondaryButton>
+              <PrimaryButton onClick={connectHandler} disabled={addresses.length === 0}>
+                {t('Connect')}
+              </PrimaryButton>
+            </footer>
           </div>
-        </section>
-        <footer className={styles.section}>
-          <SecondaryButton onClick={rejectHandler}>{t('Cancel')}</SecondaryButton>
-          <PrimaryButton onClick={connectHandler} disabled={addresses.length === 0}>
-            {t('Connect')}
-          </PrimaryButton>
-        </footer>
-      </div>
+        </>
+      )}
     </Dialog>
   );
 };
