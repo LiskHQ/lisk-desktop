b8f1657e27556cc48ec13cd1ed3538df062a56ef
eniolam1000752 <eniolam1000752@gmail.com>
2023-07-07 13:55:01 +0100

    fix: implemented changes

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,7 +1,32 @@
 const PERMISSION_WHITE_LIST = ['clipboard-read', 'notifications', 'openExternal'];
+const WHITE_LISTED_DEEP_LINKS = [
+  {
+    pathRegex: /^\/\/wallet\/?$/,
+    allowedSearchParams: ['modal', 'recipient', 'amount', 'token', 'recipientChain', 'reference'],
+  },
+];
 
 export const setRendererPermissions = (win) => {
   win.browser.webContents.session.setPermissionRequestHandler((_, permission, callback) => {
     callback(PERMISSION_WHITE_LIST.includes(permission));
   });
+};
+
+export const canExecuteDeepLinking = (url) => {
+  const { protocol, href, searchParams } = new URL(url);
+  if (protocol !== 'lisk:') return false;
+
+  const pathname = href.match(/(?<=(lisk:))(\/\/[\w|/]+)/g)?.[0];
+  const foundLink = WHITE_LISTED_DEEP_LINKS.find(({ pathRegex }) => pathRegex.test(pathname));
+  if (!foundLink) return false;
+
+  const urlSearchParams = [...searchParams.keys()];
+  const isSearchParamsAllowed = urlSearchParams.reduce((result, search) => {
+    if (!result) return false;
+    return foundLink.allowedSearchParams.includes(search);
+  }, true);
+
+  if (!isSearchParamsAllowed) return false;
+
+  return true;
 };
