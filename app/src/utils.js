34e73603f43c136689cb385319298b0b74eb2baf
eniolam1000752 <eniolam1000752@gmail.com>
2023-07-07 11:37:20 +0100

    fix: removed https check on permission request handler

--- app/src/utils.js
+++ app/src/utils.js
@@ -2,13 +2,7 @@ const PERMISSION_WHITE_LIST = ['clipboard-read', 'notifications', 'openExternal'
 export const WHITE_LISTED_DOMAIN = ['localhost', 'lisk.com'];
 
 export const setRendererPermissions = (win) => {
-  win.browser.webContents.session.setPermissionRequestHandler(
-    (webContents, permission, callback) => {
-      const parsedUrl = new URL(webContents.getURL());
-
-      if (parsedUrl.protocol !== 'https:') return callback(false);
-
-      return callback(PERMISSION_WHITE_LIST.includes(permission));
-    }
-  );
+  win.browser.webContents.session.setPermissionRequestHandler((_, permission, callback) => {
+    callback(PERMISSION_WHITE_LIST.includes(permission));
+  });
 };
