958b0982be3c8ebbecf1ce5f9fd00d64e7e00dd4
Oskar Fransson <oskar.leonard@hotmail.com>
2023-07-13 12:52:34 +0200

    Open connectionStatus on reject

--- src/modules/blockchainApplication/connection/components/ConnectionSummary/index.js
+++ src/modules/blockchainApplication/connection/components/ConnectionSummary/index.js
@@ -51,7 +51,7 @@ const ConnectionSummary = () => {
   const rejectHandler = async () => {
     const result = await reject();
     addSearchParamsToUrl(history, {
-      modal: 'connectionSuccess',
+      modal: 'connectionStatus',
       action: ACTIONS.REJECT,
       status: result.status,
       name: result.data?.params?.proposer.metadata.name ?? '',
