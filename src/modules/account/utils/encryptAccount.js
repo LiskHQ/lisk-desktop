9d417d25e6da3a910c2382ab9ac76716fc4a7d94
eniolam1000752 <eniolam1000752@gmail.com>
2023-07-19 18:46:56 +0200

    fix: removed kdfparams

--- src/modules/account/utils/encryptAccount.js
+++ src/modules/account/utils/encryptAccount.js
@@ -25,10 +25,7 @@ export const encryptAccount = async ({
     }
     const address = extractAddressFromPublicKey(publicKey);
     const plainText = JSON.stringify({ privateKey, recoveryPhrase });
-    // TODO: Remove the options after resolving it in https://github.com/LiskHQ/lisk-desktop/issues/5107
-    const crypto = await encrypt.encryptMessageWithPassword(plainText, password, {
-      kdfparams: { memorySize: 2048 },
-    });
+    const crypto = await encrypt.encryptMessageWithPassword(plainText, password);
 
     return {
       error: false,
--- src/modules/transaction/api/index.test.js
+++ src/modules/transaction/api/index.test.js
@@ -1,18 +1,14 @@
 /* eslint-disable max-lines */
 import { MODULE_COMMANDS_NAME_MAP } from 'src/modules/transaction/configuration/moduleCommand';
-import { getTotalSpendingAmount, convertBinaryToString } from '@transaction/utils/transaction';
+import { getTotalSpendingAmount } from '@transaction/utils/transaction';
 import { getState } from '@fixtures/transactions';
 import http from 'src/utils/api/http';
 import accounts from '@tests/constants/wallets';
 import { fromTransactionJSON } from '@transaction/utils/encoding';
-import { genKey, blsKey, pop } from '@tests/constants/keys';
-import { mockCommandParametersSchemas } from 'src/modules/common/__fixtures__';
-import { mockAppsTokens } from '@token/fungible/__fixtures__';
-import { getTransactionFee, dryRun } from './index';
+import { dryRun } from './index';
 
 const { stake } = MODULE_COMMANDS_NAME_MAP;
 const { network } = getState();
-const mockToken = mockAppsTokens.data[0];
 const encodedTransactionHex =
   '0a05746f6b656e12087472616e73666572180620002a20c094ebee7ec0c50ebee32918655e089f6e1a604b83bcaa760293c61e0f18ab6f32230a04040000001080c2d72f1a144662903af5e0c0662d9f1d43f087080c723096232200';
 jest.mock('@liskhq/lisk-client', () => ({
@@ -32,10 +28,6 @@ jest.mock('src/utils/api/ws', () =>
 );
 
 describe('API: LSK Transactions', () => {
-  const moduleCommandSchemas = mockCommandParametersSchemas.data.commands.reduce(
-    (result, { moduleCommand, schema }) => ({ ...result, [moduleCommand]: schema }),
-    {}
-  );
   const baseTx = {
     nonce: '6',
     senderPublicKey: 'c094ebee7ec0c50ebee32918655e089f6e1a604b83bcaa760293c61e0f18ab6f',
@@ -74,206 +66,6 @@ describe('API: LSK Transactions', () => {
       };
 
       expect(getTotalSpendingAmount(tx)).toEqual('200000000');
-    });
-  });
-
-  describe('getTransactionFee', () => {
-    const selectedPriority = {
-      value: 0,
-      title: 'LOW',
-    };
-
-    it('should return fee in Beddows', async () => {
-      const stakeTx = {
-        module: 'pos',
-        command: 'stake',
-        params: {
-          stakes: [
-            { validatorAddress: accounts.genesis.summary.address, amount: '100000000' },
-            { validatorAddress: accounts.validator.summary.address, amount: '-100000000' },
-          ],
-        },
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...stakeTx },
-        selectedPriority,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of stake tx', async () => {
-      const stakeTx = {
-        module: 'pos',
-        command: 'stake',
-        params: {
-          stakes: [
-            {
-              validatorAddress: 'lskz5kf62627u2n8kzqa8jpycee64pgxzutcrbzhz',
-              amount: 10,
-            },
-          ],
-        },
-      };
-
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...stakeTx },
-        selectedPriority,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of register validator tx', async () => {
-      const registerValidatorTx = {
-        module: 'pos',
-        command: 'registerValidator',
-        params: {
-          name: 'some_username',
-          generatorKey: genKey,
-          blsKey,
-          proofOfPossession: pop,
-        },
-      };
-
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...registerValidatorTx },
-        selectedPriority,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of reclaim tx', async () => {
-      const reclaimTx = {
-        module: 'legacy',
-        command: 'reclaimLSK',
-        params: {
-          amount: '4454300000',
-        },
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...reclaimTx },
-        selectedPriority,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of registerMultisignature tx', async () => {
-      const regMultisigTx = {
-        module: 'auth',
-        command: 'registerMultisignature',
-        params: {
-          numberOfSignatures: 2,
-          mandatoryKeys: [accounts.genesis.summary.publicKey, accounts.validator.summary.publicKey],
-          optionalKeys: [accounts.validator_candidate.summary.publicKey],
-          signatures: [],
-        },
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...regMultisigTx },
-        selectedPriority,
-        numberOfSignatures: 2,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of multisignature token transfer tx', async () => {
-      const multisigTransferTx = {
-        module: 'token',
-        command: 'transfer',
-        params: {
-          amount: 100000,
-          data: 'to test the instance',
-          recipientAddress: 'lskz5kf62627u2n8kzqa8jpycee64pgxzutcrbzhz',
-          tokenID: '00000000',
-        },
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...multisigTransferTx },
-        selectedPriority,
-        numberOfSignatures: 10,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of multisignature stake tx', async () => {
-      const multisigStakeTx = {
-        module: 'pos',
-        command: 'stake',
-        params: {
-          stakes: [
-            { validatorAddress: accounts.genesis.summary.address, amount: '100000000' },
-            { validatorAddress: accounts.validator.summary.address, amount: '-100000000' },
-          ],
-        },
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...multisigStakeTx },
-        selectedPriority,
-        numberOfSignatures: 10,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it('should calculate fee of multisignature registerValidator tx', async () => {
-      const multisigRegisterValidatorTx = {
-        module: 'pos',
-        command: 'registerValidator',
-        params: {
-          name: 'user_name',
-          generatorKey: convertBinaryToString(genKey),
-          blsKey: convertBinaryToString(blsKey),
-          proofOfPossession: convertBinaryToString(pop),
-        },
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...multisigRegisterValidatorTx },
-        selectedPriority,
-        numberOfSignatures: 64,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-
-      expect(Number(result.value)).toBeGreaterThan(0);
-    });
-
-    it.skip('should calculate fee of multisignature unlock tx', async () => {
-      const multisigUnlockTx = {
-        module: 'pos',
-        command: 'unlock',
-      };
-      const result = await getTransactionFee({
-        transactionJSON: { ...baseTx, ...multisigUnlockTx },
-        selectedPriority,
-        numberOfSignatures: 4,
-        network,
-        moduleCommandSchemas,
-        token: mockToken,
-      });
-
-      expect(Number(result.value)).toBeGreaterThan(0);
     });
   });
 
