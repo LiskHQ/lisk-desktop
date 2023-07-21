ef8b8d806f0d9484d1220a6856bdae63e0a8572a
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-07-21 16:17:37 +0100

    Added remaining banners

---
+++ src/modules/account/components/AccountDetails/AccountOverview/banners.js
@@ -0,0 +1,94 @@
+const banners = [
+  {
+    name: 'accountManagementPageBanner',
+    infoMessage: (t) => t('Introducing account management'),
+    infoDescription: (t) =>
+      t(
+        'Effortlessly manage multiple accounts in one interface with enhanced privacy and security. Seamlessly switch between accounts, allocate funds, and monitor balances.'
+      ),
+    illustrationName: 'accountManagement',
+  },
+  {
+    name: 'blockchainApplicationsPageBanner',
+    infoMessage: (t) => t('Introducing blockchain application exploring and management'),
+    infoDescription: (t) =>
+      t(
+        'A new management feature allows you to seamlessly add and switch between applications. The dedicated application tab provides a comprehensive overview of registered, active, and terminated blockchain applications, and statistics.'
+      ),
+    illustrationName: 'applicationManagement',
+  },
+  {
+    name: 'hardwareWalletManagementPageBanner',
+    infoMessage: (t) => t('Introducing hardware wallet management'),
+    infoDescription: (t) =>
+      t(
+        'Explore multiple hardware wallet devices simultaneously. Seamlessly access your accounts through the integrated functionality of our new account management feature.'
+      ),
+    illustrationName: 'hardwareWalletManagement',
+  },
+  {
+    name: 'walletConnectPageBanner',
+    infoMessage: (t) => t('Introducing wallet connect management and exploring'),
+    infoDescription: (t) =>
+      t(
+        'Enjoy a streamlined and secure experience of signing transactions for external applications. Unlock a world of possibilities with Wallet Connect Integration, and take full control of your digital assets.'
+      ),
+    illustrationName: 'walletConnect',
+  },
+  {
+    name: 'transactionEventsPageBanner',
+    infoMessage: (t) => t('Introducing events'),
+    infoDescription: (t) =>
+      t(
+        'Stay informed in real-time about crucial blockchain activities. With this new feature, track the end to end execution and its results for all transactions and blocks.'
+      ),
+    illustrationName: 'transactionEvents',
+  },
+  {
+    name: 'multiTokenBalancesPageBanner',
+    infoMessage: (t) => t('Introducing Multi-tokens'),
+    infoDescription: (t) =>
+      t(
+        'With Lisk interoperability, you can now store, manage, and transact with a variety of tokens within a single wallet interface. Seamlessly switch between different digital assets and diversify your portfolio effortlessly. Enjoy the convenience and flexibility of Multi-Tokens.'
+      ),
+    illustrationName: 'multiTokenBalances',
+  },
+  {
+    name: 'crossApplicationsSendRequestTokensPageBanner',
+    infoMessage: (t) => t('Introducing sending and requesting token within an application'),
+    infoDescription: (t) =>
+      t(
+        'Flawlessly move your assets within a specific blockchain application. Experience the power of cross-chain transfers, enabling you to expand your reach and optimize your asset management strategies.'
+      ),
+    illustrationName: 'crossApplicationsSendRequestTokens',
+  },
+  {
+    name: 'withinAndCrossApplicationsSendRequestTokensPageBanner',
+    infoMessage: (t) => t('Introducing sending and requesting token across applications'),
+    infoDescription: (t) =>
+      t(
+        'Flawlessly move your assets across different blockchain applications. Experience the power of cross-chain transfers, enabling you to expand your reach and optimize your asset management strategies.'
+      ),
+    illustrationName: 'withinAndCrossApplicationsSendRequestTokens',
+  },
+  {
+    name: 'networkManagementPageBanner',
+    infoMessage: (t) => t('Introducing Network and Application management'),
+    infoDescription: (t) =>
+      t(
+        'Take control of your blockchain network settings. Now you can customize your network preferences according to your specific needs.'
+      ),
+    illustrationName: 'networkManagement',
+  },
+  {
+    name: 'validatorsPageBanner',
+    infoMessage: (t) => t('Introducing sending and requesting token within an application'),
+    infoDescription: (t) =>
+      t(
+        'Flawlessly move your assets within a specific blockchain application. Experience the power of cross-chain transfers, enabling you to expand your reach and optimize your asset management strategies.'
+      ),
+    illustrationName: 'proofOfStake',
+  },
+];
+
+export default banners;
