e328b1f443244d7632062bb5800834ed90390e5f
Oskar Fransson <oskar.leonard@hotmail.com>
2023-07-21 15:06:00 +0200

    Added smoke test for RequestSignMessageConfirmation

--- src/modules/blockchainApplication/connection/components/RequestSignMessageDialog/RequestSignMessageConfirmation/index.test.js
+++ src/modules/blockchainApplication/connection/components/RequestSignMessageDialog/RequestSignMessageConfirmation/index.test.js
@@ -1,1 +1,23 @@
-describe('RequestSignMessageConfirmation', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { RequestSignMessageConfirmation } from './index';
+
+jest.mock('react-i18next');
+
+const props = {
+  nextStep: jest.fn(),
+  address: 'address',
+  message: 'message',
+};
+
+beforeEach(() => {
+  render(<RequestSignMessageConfirmation {...props} />);
+});
+
+describe('RequestSignMessageConfirmation', () => {
+  it('should render properly', async () => {
+    expect(screen.getByText('This request was initiated from another application.')).toBeTruthy();
+    expect(screen.getByText(props.address)).toBeTruthy();
+    expect(screen.getByText(props.message)).toBeTruthy();
+  });
+});
