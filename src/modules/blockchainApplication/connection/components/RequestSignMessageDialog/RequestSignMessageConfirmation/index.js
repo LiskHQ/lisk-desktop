89a49fb6e026df1d292b8e22e7f8bf74afc0af3c
Oskar Fransson <oskar.leonard@hotmail.com>
2023-07-21 14:35:24 +0200

    wc sign message flow

---
+++ src/modules/blockchainApplication/connection/components/RequestSignMessageDialog/RequestSignMessageConfirmation/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useCurrentAccount } from '@account/hooks';
+import { useDispatch } from 'react-redux';
+import { signMessage } from '@message/store/action';
+import CopyToClipboard from '@common/components/copyToClipboard';
+import { PrimaryButton } from '@theme/buttons';
+import styles from './RequestSignMessageConfirmation.css';
+
+export function RequestSignMessageConfirmation({ nextStep, address, message }) {
+  const { t } = useTranslation();
+  const [currentAccount] = useCurrentAccount();
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    nextStep({
+      message,
+      actionFunction: (formProps, _, privateKey) =>
+        dispatch(signMessage({ message, nextStep, privateKey, currentAccount })),
+    });
+  };
+
+  return (
+    <div className={styles.RequestSignMessageConfirmation}>
+      <p className={styles.description}>
+        {t('This request was initiated from another application.')}
+      </p>
+      <div className={styles.content}>
+        <p className={styles.label}>Address: </p>
+        <CopyToClipboard
+          text={address}
+          value={address}
+          className="tx-id"
+          containerProps={{
+            size: 'xs',
+            className: 'copy-address',
+          }}
+        />
+        <p className={styles.label}>Message</p>
+        <div className={styles.messageBox}>{message}</div>
+        <PrimaryButton className={styles.btnContinue} onClick={onClick}>
+          {t('Continue')}
+        </PrimaryButton>
+      </div>
+    </div>
+  );
+}
--- src/modules/blockchainApplication/connection/components/RequestSignMessageDialog/index.js
+++ src/modules/blockchainApplication/connection/components/RequestSignMessageDialog/index.js
@@ -1,43 +1,75 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 import Dialog from '@theme/dialog/dialog';
 import BlockchainAppDetailsHeader from '@blockchainApplication/explore/components/BlockchainAppDetailsHeader';
-import { SIGNING_METHODS } from '@libs/wcm/constants/permissions';
 import { useEvents } from '@libs/wcm/hooks/useEvents';
 import { EVENTS } from '@libs/wcm/constants/lifeCycle';
 import { useSession } from '@libs/wcm/hooks/useSession';
+import { useCurrentAccount } from '@account/hooks';
+import MultiStep from '@common/components/OldMultiStep';
+import TxSignatureCollector from '@transaction/components/TxSignatureCollector';
+import SignedMessage from '@message/components/signedMessage';
+import { RequestSignMessageConfirmation } from '@blockchainApplication/connection/components/RequestSignMessageDialog/RequestSignMessageConfirmation';
 import styles from './RequestSignMessageDialog.css';
 
 // eslint-disable-next-line max-statements
 const RequestSignMessageDialog = () => {
+  const [multiStepPosition, setMultiStepPosition] = useState(0);
   const { t } = useTranslation();
   const { events } = useEvents();
   const { sessionRequest } = useSession();
+  const [currentAccount] = useCurrentAccount();
+
+  const history = useHistory();
+  const { peer, requiredNamespaces } = sessionRequest || {};
+  const {
+    metadata: { pubkey },
+  } = currentAccount;
   const event = events?.find((e) => e.name === EVENTS.SESSION_REQUEST);
   const { message, address } = event?.meta?.params?.request?.params || {};
-  const { peer, requiredNamespaces } = sessionRequest || {};
 
   const { icons, name, url } = peer?.metadata || {};
 
+  const onMultiStepChange = useCallback(({ step: { current } }) => {
+    setMultiStepPosition(current);
+  }, []);
+
+  const isPasswordStep = multiStepPosition === 1;
+
   return (
-    <Dialog className={styles.RequestSignMessageDialog} hasClose>
-      <BlockchainAppDetailsHeader
-        headerText={SIGNING_METHODS.SIGN_MESSAGE.title}
-        application={{
-          data: {
-            name,
-            projectPage: url?.replace(/\/$/, ''),
-            icon: icons?.[0],
-          },
-        }}
-        clipboardCopyItems={requiredNamespaces?.lisk?.chains?.map((chain) => ({
-          label: 'Chain ID:',
-          value: chain?.replace(/\D+/g, ''),
-        }))}
-      />
-      <p>{t('This request was initiated from another application.')}</p>
-      <p>Address: {address}</p>
-      <p>Message: {message}</p>
+    <Dialog
+      className={classNames(styles.RequestSignMessageDialog, {
+        [styles.passwordStep]: isPasswordStep,
+      })}
+      hasClose
+    >
+      {!isPasswordStep && (
+        <BlockchainAppDetailsHeader
+          className={styles.blockchainAppDetailsHeaderProp}
+          headerText={multiStepPosition === 2 ? t('Signed message') : t('Sign message')}
+          application={{
+            data: {
+              name,
+              projectPage: url?.replace(/\/$/, ''),
+              icon: icons?.[0],
+            },
+          }}
+          clipboardCopyItems={requiredNamespaces?.lisk?.chains?.map((chain) => ({
+            label: 'Chain ID:',
+            value: chain?.replace(/\D+/g, ''),
+          }))}
+        />
+      )}
+      <MultiStep onChange={onMultiStepChange}>
+        <RequestSignMessageConfirmation message={message} address={address} />
+        <TxSignatureCollector
+          type="message"
+          transactionJSON={{ senderPublicKey: pubkey, params: {} }}
+        />
+        <SignedMessage history={history} account={currentAccount} />
+      </MultiStep>
     </Dialog>
   );
 };
