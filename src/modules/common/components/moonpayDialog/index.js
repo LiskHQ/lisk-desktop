aa553f02f29a8a43af6c7b30c3d65e3059db15f5
Oskar Fransson <oskar.leonard@hotmail.com>
2023-08-09 18:09:15 +0200

    use var for key

--- src/modules/common/components/moonpayDialog/index.js
+++ src/modules/common/components/moonpayDialog/index.js
@@ -4,13 +4,13 @@ import Dialog from '@theme/dialog/dialog';
 import styles from './MoonpayDialog.css';
 
 const MoonpayDialog = ({ className }) => {
-  console.log('MoonpayDialog');
+  const pkKey = 'YOUR TEST KEY';
 
   return (
     <Dialog className={classNames(className, styles.moonpayDialog)}>
       <iframe
         id="moonpayIframe"
-        src="https://buy-sandbox.moonpay.com/?apiKey=pk_test_pb5Z9pWiJOUsIrJPkbrZOKOnKkg8UJ&theme=dark"
+        src={`https://buy-sandbox.moonpay.com/?apiKey=${pkKey}}&theme=dark`}
         title="MoonPay"
       />
     </Dialog>
