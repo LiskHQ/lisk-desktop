70fbb7ca736d3315c3c835c5c9ad225d9edc75f5
Oskar Fransson <oskar.leonard@hotmail.com>
2023-08-09 18:40:09 +0200

    Fixed styling

--- src/modules/common/components/moonpayDialog/index.js
+++ src/modules/common/components/moonpayDialog/index.js
@@ -4,15 +4,17 @@ import Dialog from '@theme/dialog/dialog';
 import styles from './MoonpayDialog.css';
 
 const MoonpayDialog = ({ className }) => {
-  const pkKey = 'YOUR TEST KEY';
+  const pkKey = 'YOUR KEY';
 
   return (
     <Dialog className={classNames(className, styles.moonpayDialog)}>
-      <iframe
-        id="moonpayIframe"
-        src={`https://buy-sandbox.moonpay.com/?apiKey=${pkKey}&theme=dark`}
-        title="MoonPay"
-      />
+      <div  className={classNames(styles.iframeContainer)}>
+        <iframe
+          id="moonpayIframe"
+          src={`https://buy-sandbox.moonpay.com/?apiKey=${pkKey}&theme=dark`}
+          title="MoonPay"
+        />
+      </div>
     </Dialog>
   );
 };
