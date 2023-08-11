2e06422925b3a44026180ad73aea9e77ec5cbfe1
Oskar Fransson <oskar.leonard@hotmail.com>
2023-08-11 10:58:48 +0200

    This key is publishable

--- src/modules/common/components/moonpayDialog/index.js
+++ src/modules/common/components/moonpayDialog/index.js
@@ -4,14 +4,14 @@ import Dialog from '@theme/dialog/dialog';
 import styles from './MoonpayDialog.css';
 
 const MoonpayDialog = ({ className }) => {
-  const pkKey = 'YOUR KEY';
+  const theme = 'dark';
 
   return (
     <Dialog className={classNames(className, styles.moonpayDialog)}>
-      <div  className={classNames(styles.iframeContainer)}>
+      <div className={classNames(styles.iframeContainer)}>
         <iframe
           id="moonpayIframe"
-          src={`https://buy-sandbox.moonpay.com/?apiKey=${pkKey}&theme=dark`}
+          src={`https://buy-sandbox.moonpay.com/?apiKey=pk_test_pb5Z9pWiJOUsIrJPkbrZOKOnKkg8UJ&theme=${theme}`}
           title="MoonPay"
         />
       </div>
