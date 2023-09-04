a5c42466ba87da6f08e6d551b89f54787ac6daf0
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-04 11:28:56 +0200

    fixed tests

--- src/modules/message/store/action.test.js
+++ src/modules/message/store/action.test.js
@@ -1,18 +1,18 @@
 import { cryptography } from '@liskhq/lisk-client';
 import { mockHWAccounts } from '@hardwareWallet/__fixtures__';
 import * as signMessageUtil from '@wallet/utils/signMessage';
-import { getUnsignedNonProtocolMessage, signMessage } from './action';
+import { signMessage } from './action';
 
 jest.spyOn(cryptography.ed, 'signAndPrintMessage');
 jest.spyOn(cryptography.ed, 'printSignedMessage');
 jest.spyOn(signMessageUtil, 'signMessageUsingHW');
 
 describe('balanceReclaimed', () => {
+  const mockCurrentAccount = mockHWAccounts[0];
   const message = 'test-message';
   const nextStep = jest.fn();
   const privateKey =
     '314852d7afb0d4c283692fef8a2cb40e30c7a5df2ed79994178c10ac168d6d977ef45cd525e95b7a86244bbd4eb4550914ad06301013958f4dd64d32ef7bc588';
-  const publicKey = '7ef45cd525e95b7a86244bbd4eb4550914ad06301013958f4dd64d32ef7bc588';
   const signature =
     '68937004b6720d7e1902ef05a577e6d9f9ab2756286b1f2ae918f8a0e5153c15e4f410916076f750b708f8979be2430e4cfc7ebb523ae1905d2ea1f5d24ce700';
   const defaultPrintedMessage = `
@@ -20,7 +20,7 @@ describe('balanceReclaimed', () => {
       -----MESSAGE-----
       ${message}
       -----PUBLIC KEY-----
-      ${publicKey}
+      ${mockCurrentAccount.metadata.pubkey}
       -----SIGNATURE-----
       ${signature}
       -----END LISK SIGNED MESSAGE-----
@@ -46,9 +46,9 @@ describe('balanceReclaimed', () => {
     });
   });
 
-  it('should dispatch transactionCreatedSuccess', async () => {
-    const mockCurrentAccount = mockHWAccounts[0];
-    signMessageUtil.signMessageUsingHW.mockResolvedValue([undefined, defaultPrintedMessage]);
+  it('should call nextStep with signature', async () => {
+    signMessageUtil.signMessageUsingHW.mockResolvedValue(defaultPrintedMessage);
+    cryptography.ed.printSignedMessage.mockReturnValue(defaultPrintedMessage);
     await signMessage({ nextStep, privateKey, message, currentAccount: mockCurrentAccount })();
 
     expect(nextStep).toHaveBeenCalledWith({
@@ -57,13 +57,14 @@ describe('balanceReclaimed', () => {
     });
   });
 
-  it('should dispatch transactionCreatedSuccess', async () => {
-    const mockCurrentAccount = mockHWAccounts[0];
+  it('should call nextStep with error', async () => {
+    const error = {name: 'An error'};
+    signMessageUtil.signMessageUsingHW.mockRejectedValue(error);
     await signMessage({ nextStep, privateKey, message, currentAccount: mockCurrentAccount })();
 
-    expect(signMessageUtil.signMessageUsingHW).toHaveBeenCalledWith({
-      message: getUnsignedNonProtocolMessage(message),
-      account: mockCurrentAccount,
+    expect(nextStep).toHaveBeenCalledWith({
+      error,
+      message,
     });
   });
 });
