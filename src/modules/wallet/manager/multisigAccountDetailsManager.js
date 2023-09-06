b8bda4b230765b5598f87c429512fbccccaf1f2b
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-09-06 04:12:48 +0100

    Removed old multisig details view and minor code cleanup

--- src/modules/wallet/manager/multisigAccountDetailsManager.js
+++
@@ -1,7 +0,0 @@
-import { withTranslation } from 'react-i18next';
-import { compose } from 'redux';
-import { withRouter } from 'react-router';
-
-import MultisigAccountDetails from '../components/multisigAccountDetails';
-
-export default compose(withRouter, withTranslation())(MultisigAccountDetails);
