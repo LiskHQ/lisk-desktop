ca55cac563f47b0fb60bf2a3ce08d0453e9bd72a
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-07 14:40:29 +0200

    Use currentHwDevice path for signing since the one in account.hw can be outdated. Added error message if trying to sign with wrong path

--- libs/hardwareWallet/ledger/ledgerLiskAppIPCChannel/serverLedgerHWCommunication/utils.js
+++ libs/hardwareWallet/ledger/ledgerLiskAppIPCChannel/serverLedgerHWCommunication/utils.js
@@ -1,12 +1,19 @@
 import { LedgerAccount } from '@zondax/ledger-lisk';
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
 import { getPubKey } from '@libs/hardwareWallet/ledger/ledgerLiskAppIPCChannel/serverLedgerHWCommunication/index';
+import {LEDGER_CUSTOM_ERRORS} from "@libs/hardwareWallet/ledger/constants";
 
 export const getLedgerAccount = (index = 0) => {
   const ledgerAccount = new LedgerAccount();
   ledgerAccount.account(index);
   return ledgerAccount;
 };
+
+export function getCustomErrorCode(error) {
+  const errorMessage = error.message;
+  const customError = Object.values(LEDGER_CUSTOM_ERRORS).find(({ match }) => errorMessage.includes(match));
+  return customError?.return_code;
+}
 
 // eslint-disable-next-line max-statements
 export async function getDevicesFromPaths(devicePaths) {
