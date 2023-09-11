fcbf88d2998e25289ae580229060c3f5107a403e
Eniola Olatunji <eniolam1000752@gmail.com>
2023-09-11 15:46:19 +0200

    fix: resolved token symbol issue on stakes

--- src/modules/pos/validator/components/StakingQueue/index.js
+++ src/modules/pos/validator/components/StakingQueue/index.js
@@ -1,8 +1,6 @@
 /* istanbul ignore file */
-import React, { useEffect, useMemo, useState, useCallback } from 'react';
-
+import React, { useEffect, useState, useCallback } from 'react';
 import TxSignatureCollector from '@transaction/components/TxSignatureCollector';
-import { useTokenBalances } from 'src/modules/token/fungible/hooks/queries';
 import { removeSearchParamsFromUrl } from 'src/utils/searchParams';
 import MultiStep from 'src/modules/common/components/OldMultiStep';
 import Dialog from 'src/theme/dialog/dialog';
@@ -10,7 +8,7 @@ import StakeForm from '../StakeForm';
 import StakeSummary from '../StakeSummary';
 import StakeStatus from '../StakeStatus';
 import styles from './styles.css';
-import { usePosConstants } from '../../hooks/queries';
+import usePosToken from '../../hooks/usePosToken';
 
 const StakingQueue = ({ history, processLaunchProtocol }) => {
   const [isStepTxSignatureCollector, setIsStepTxSignatureCollector] = useState(false);
@@ -18,13 +16,7 @@ const StakingQueue = ({ history, processLaunchProtocol }) => {
     setIsStepTxSignatureCollector([2, 3].includes(current));
   }, []);
 
-  const { data: posConstants, isLoading: isGettingPosConstants } = usePosConstants();
-
-  const { data: tokens } = useTokenBalances({
-    config: { params: { tokenID: posConstants?.posTokenID } },
-    options: { enabled: !isGettingPosConstants },
-  });
-  const token = useMemo(() => tokens?.data?.[0] || {}, [tokens]);
+  const { token } = usePosToken();
 
   const closeModal = () => {
     removeSearchParamsFromUrl(history, ['modal'], true);
