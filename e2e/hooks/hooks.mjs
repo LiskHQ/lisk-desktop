a8ae8b7770b42f7ce708b38fb13d8d8ec7da9a2c
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-15 14:17:05 +0200

    close after feature and link with /

--- e2e/hooks/hooks.mjs
+++ e2e/hooks/hooks.mjs
@@ -1,8 +1,9 @@
-import { BeforeAll, AfterAll, Before } from '@cucumber/cucumber';
+import { BeforeAll, AfterAll, Before, After } from '@cucumber/cucumber';
 import playwright from 'playwright';
 import { fixture } from '../fixtures/page.mjs';
 
 let browser;
+let context;
 
 BeforeAll(async function () {
   browser = await playwright.chromium.launch({
@@ -12,7 +13,7 @@ BeforeAll(async function () {
 });
 
 Before(async function () {
-  const context = await browser.newContext({ permissions: ['clipboard-read', 'clipboard-write'] });
+  context = await browser.newContext({ permissions: ['clipboard-read', 'clipboard-write'] });
   const newPage = await context.newPage();
   await newPage.goto(`${process.env.PW_BASE_URL}`);
 
@@ -43,6 +44,11 @@ Before(async function () {
   });
 
   fixture.page = newPage;
+});
+
+After(async function () {
+  await fixture.page.close();
+  await context.close();
 });
 
 AfterAll(async function () {
