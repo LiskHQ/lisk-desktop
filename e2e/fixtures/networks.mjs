2b6d668ab504da7f1189ecb041c092d5eed39cdd
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-21 10:52:07 +0200

    use basenetwork for choosen base 2e2 network

--- e2e/fixtures/networks.mjs
+++ e2e/fixtures/networks.mjs
@@ -2,5 +2,10 @@ export const NETWORKS = {
   devnet: {
     name: 'devnet',
     serviceUrl: 'http://devnet-service.liskdev.net:9901',
-  }
-}
+  },
+};
+
+export const BASE_NETWORK = {
+  name: NETWORKS.devnet.name,
+  serviceUrl: NETWORKS.devnet.serviceUrl,
+};
