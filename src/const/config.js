022c0d052160345dfb33fc23e6571bca16545aed
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-27 12:54:50 +0200

    fix undefined check

--- src/const/config.js
+++ src/const/config.js
@@ -3,5 +3,5 @@ export const LIMIT = 20;
 export const API_VERSION = 'v3';
 export const MOCK_SERVICE_WORKER = process.env.REACT_APP_MSW;
 
-// eslint-disable-next-line no-extra-boolean-cast
-export const DEFAULT_NETWORK = !!REACT_APP_DEFAULT_NETWORK ? REACT_APP_DEFAULT_NETWORK : 'devnet';
+export const DEFAULT_NETWORK =
+  typeof REACT_APP_DEFAULT_NETWORK !== 'undefined' ? REACT_APP_DEFAULT_NETWORK : 'devnet';
