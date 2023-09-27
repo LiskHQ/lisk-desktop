26b612721284d300fc17ec77ad3b11cf1d8bb06c
Eniola Olatunji <eniolam1000752@gmail.com>
2023-09-27 12:13:16 +0200

    fix: changed localNet to customNode

--- e2e/fixtures/networks.mjs
+++ e2e/fixtures/networks.mjs
@@ -3,13 +3,12 @@ export const NETWORKS = {
     name: 'devnet',
     serviceUrl: 'http://devnet-service.liskdev.net:9901',
   },
-  local_network: {
-    name: 'local_network',
+  customNode: {
+    name: 'customNode',
     serviceUrl: 'http://localhost:9901',
   },
 };
 
 export const BASE_NETWORK = {
-  name: NETWORKS.local_network.name,
-  serviceUrl: NETWORKS.local_network.serviceUrl,
+  ...NETWORKS.customNode,
 };
