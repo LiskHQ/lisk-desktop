b3b56506486b871aa32651b3c702f78f13b02a8b
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-27 11:06:01 +0200

    added local_network to networks for e2e

--- e2e/fixtures/networks.mjs
+++ e2e/fixtures/networks.mjs
@@ -3,9 +3,13 @@ export const NETWORKS = {
     name: 'devnet',
     serviceUrl: 'http://devnet-service.liskdev.net:9901',
   },
+  local_network: {
+    name: 'local_network',
+    serviceUrl: 'http://localhost:9901',
+  },
 };
 
 export const BASE_NETWORK = {
-  name: NETWORKS.devnet.name,
-  serviceUrl: NETWORKS.devnet.serviceUrl,
+  name: NETWORKS.local_network.name,
+  serviceUrl: NETWORKS.local_network.serviceUrl,
 };
