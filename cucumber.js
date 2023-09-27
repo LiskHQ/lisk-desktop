054103cccc78d94c211679fe0d9589dfbe2f88b2
Eniola Olatunji <eniolam1000752@gmail.com>
2023-09-27 13:39:15 +0200

    fix: reinstated all feature file test

--- cucumber.js
+++ cucumber.js
@@ -2,8 +2,7 @@ module.exports = {
   default: {
     parallel: 2,
     format: ['html:./e2e/assets/cucumber-report.html'],
-    // paths: ['./e2e/features/**/*.feature'],
-    paths: ['./e2e/features/account/AddAccount.feature'],
+    paths: ['./e2e/features/**/*.feature'],
     import: ['./e2e/hooks/hooks.mjs', './e2e/steps/**/*.mjs', './e2e/steps/**/*.js'],
     dryRun: false,
     publishQuite: true,
