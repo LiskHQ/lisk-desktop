a6c3b8532f21b053fe75ddddc2c34789b7886cf7
Eniola Olatunji <eniolam1000752@gmail.com>
2023-09-27 12:43:52 +0200

    fix: resolved issue with default_network env

--- src/const/config.js
+++ src/const/config.js
@@ -3,4 +3,5 @@ export const LIMIT = 20;
 export const API_VERSION = 'v3';
 export const MOCK_SERVICE_WORKER = process.env.REACT_APP_MSW;
 
-export const DEFAULT_NETWORK = REACT_APP_DEFAULT_NETWORK || 'devnet';
+// eslint-disable-next-line no-extra-boolean-cast
+export const DEFAULT_NETWORK = !!REACT_APP_DEFAULT_NETWORK ? REACT_APP_DEFAULT_NETWORK : 'devnet';
