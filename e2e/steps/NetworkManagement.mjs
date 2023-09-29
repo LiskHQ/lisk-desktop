c619297cb3cac697970fdda3e2080e993f552487
Oskar Fransson <oskar.leonard@hotmail.com>
2023-09-29 15:32:05 +0200

    fixed alt text

--- e2e/steps/NetworkManagement.mjs
+++ e2e/steps/NetworkManagement.mjs
@@ -6,6 +6,6 @@ import { fixture } from '../fixtures/page.mjs';
 Given('I open edit network modal for network {string}', async function (networkName) {
   await fixture.page.getByTestId("selected-menu-item").click();
   await fixture.page.getByText(networkName, { exact: true }).hover();
-  await fixture.page.getByText(networkName, { exact: true }).locator('..').locator('..').getByAltText(altText).click();
+  await fixture.page.getByText(networkName, { exact: true }).locator('..').locator('..').getByAltText('edit').click();
   await expect(fixture.page.getByText("Edit network", { exact: true })).toBeVisible();
 });
