08bfaa70826f0bbcfed036123ac0680430041e32
ManuGowda <manu@lightcurve.io>
2023-10-13 16:01:43 +0200

    :bug: Fix property name

--- scripts/sign.js
+++ scripts/sign.js
@@ -1,9 +1,9 @@
 exports.default = async function (configuration) {
   if (configuration.path) {
     require('child_process').execSync(
-      `smctl sign --keypair-alias="${String(configuration.options.keyPairAlias)}" --input "${String(
-        configuration.path
-      )}"`,
+      `smctl sign --keypair-alias="${String(
+        configuration.options.certificateSubjectName
+      )}" --input "${String(configuration.path)}"`,
       {
         stdio: 'inherit',
       }
