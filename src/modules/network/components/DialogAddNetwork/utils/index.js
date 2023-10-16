88c18e2cdad94b1c8f9ba8c74fe26ff47e239997
Oskar Fransson <oskar.leonard@hotmail.com>
2023-10-16 13:12:27 +0200

    added quick draft to check if any serviceURLs are working

--- src/modules/network/components/DialogAddNetwork/utils/index.js
+++ src/modules/network/components/DialogAddNetwork/utils/index.js
@@ -3,6 +3,7 @@ import { Client } from 'src/utils/api/client';
 import { useNetworkStatus } from '@network/hooks/queries';
 import { useBlockchainApplicationMeta } from '@blockchainApplication/manage/hooks/queries/useBlockchainApplicationMeta';
 import { useDebounce } from 'src/modules/search/hooks/useDebounce';
+import axios from "axios";
 
 export const DEFAULT_NETWORK_FORM_STATE = {
   name: '',
@@ -50,8 +51,32 @@ export function getDuplicateNetworkFields(newNetwork, networks, networkToExclude
   return Object.keys(result).length > 0 ? result : undefined;
 }
 
+export async function isNetworkUrlSuccess(url) {
+  try {
+    await axios({ url, timeout: 4000 });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function getHasValidServiceUrl(serviceURLs) {
+  console.log('serviceURLs', serviceURLs);
+  if(!serviceURLs) {
+    return false;
+  }
+  const promises = [];
+  for (let i = 0; i < serviceURLs.length; i++) {
+    promises.push(isNetworkUrlSuccess(serviceURLs[i]?.http));
+  }
+  const responses = await Promise.all(promises);
+  console.log('getHasValidServiceUrl responses', responses);
+  const hasValidServiceUrl = responses.find((response) => response)
+  return hasValidServiceUrl;
+}
+
 /* istanbul ignore next */
-export function useNetworkCheck(serviceUrl) {
+export async function useNetworkCheck(serviceUrl) {
   serviceUrl = useDebounce(serviceUrl, 300);
   const client = useMemo(() => serviceUrl && new Client({ http: serviceUrl }), [serviceUrl]);
 
@@ -59,6 +84,7 @@ export function useNetworkCheck(serviceUrl) {
     options: { enabled: !!serviceUrl, retry: false },
     client,
   });
+  console.log('useNetworkCheck networkStatus', networkStatus);
 
   const blockchainAppsMeta = useBlockchainApplicationMeta({
     config: {
@@ -71,6 +97,12 @@ export function useNetworkCheck(serviceUrl) {
     options: { enabled: !!networkStatus?.data?.data, retry: false },
     client,
   });
+
+  const serviceUrls = blockchainAppsMeta.data?.data[0]?.serviceURLs;
+  const hasValid = await getHasValidServiceUrl(serviceUrls);
+  console.log('hasValid', hasValid);
+
+  console.log('useNetworkCheck blockchainAppsMeta', blockchainAppsMeta);
 
   const handleRefetch = () => {
     networkStatus.refetch();
