5d125d3778ceeb795a31ba43cb64a7cf28cb8464
ManuGowda <manu@lightcurve.io>
2023-10-18 13:44:25 +0200

    :recycle: Fix grammar for announcement

--- src/modules/common/constants.js
+++ src/modules/common/constants.js
@@ -6,7 +6,7 @@ export const INFO_BANNERS = {
     infoLabel: (t) => t('Announcement'),
     infoDescription: (t) =>
       t(
-        'This announcement is intended for all validators and node operators, please make sure to correctly migrate your nodes to the new network, in order to not miss any blocks after the network hard fork.'
+        'This announcement is intended for all validators and node operators. Please ensure that you correctly migrate your nodes to the new network to avoid missing any blocks after the network hard fork.'
       ),
     illustrationName: 'liskMigrationIllustration',
     infoLink: 'https://lisk.com/documentation/lisk-core/v4/management/migration.html',
