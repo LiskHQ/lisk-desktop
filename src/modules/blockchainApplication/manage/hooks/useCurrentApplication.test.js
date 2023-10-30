1a360b14feac5a10fd42f08bbac75f914dd2ecc1
Eniola Olatunji <eniolam1000752@gmail.com>
2023-10-30 16:53:17 +0100

    fix: resolved failing unit tests

--- src/modules/blockchainApplication/manage/hooks/useCurrentApplication.test.js
+++ src/modules/blockchainApplication/manage/hooks/useCurrentApplication.test.js
@@ -30,6 +30,7 @@ const mockHistory = {
   push: jest.fn(),
 };
 jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
   useSelector: jest.fn().mockImplementation((fn) => fn(mockState)),
   useDispatch: () => mockDispatch,
 }));
