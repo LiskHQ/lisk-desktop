2a58c6de4c4bce3e5c14daf9349a60e28e15b602
Oskar Fransson <oskar.leonard@hotmail.com>
2023-10-30 17:17:53 +0100

    Store pending transactions by address, network and chainID

--- src/modules/auth/hooks/useNonceSync.js
+++ src/modules/auth/hooks/useNonceSync.js
@@ -1,13 +1,15 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useAccounts, useCurrentAccount } from '@account/hooks';
+import { useCurrentApplication } from '@blockchainApplication/manage/hooks';
 import { useAuth } from './queries';
 
 // eslint-disable-next-line max-statements
 const useNonceSync = () => {
   const { setNonceByAccount, getNonceByAccount, resetNonceByAccount } = useAccounts();
+  const [{ chainID, chainName }] = useCurrentApplication();
+  const networkChainIDKey = `${chainName}:${chainID}`;
   const [currentAccount] = useCurrentAccount();
   const currentAccountAddress = currentAccount.metadata.address;
-  const currentAccountNonce = getNonceByAccount(currentAccountAddress);
 
   const { data: authData } = useAuth({
     config: { params: { address: currentAccountAddress } },
@@ -20,34 +22,36 @@ const useNonceSync = () => {
 
   // Store nonce by address in accounts store
   const handleLocalNonce = (currentNonce) => {
+    const currentAccountNonce = getNonceByAccount(currentAccountAddress, networkChainIDKey);
     const storedNonce = BigInt(currentAccountNonce || 0);
     const localNonce = storedNonce < currentNonce ? currentNonce : storedNonce;
     const localNonceStr = localNonce.toString();
-    setNonceByAccount(currentAccountAddress, localNonceStr, 'defaultNonce');
+    setNonceByAccount(currentAccountAddress, localNonceStr, 'defaultNonce', networkChainIDKey);
 
     setAccountNonce(localNonceStr);
   };
 
   useEffect(() => {
     if (isMultiSig) {
-      handleLocalNonce(onChainNonce);
+      handleLocalNonce(onChainNonce, networkChainIDKey);
     }
-  }, [onChainNonce, isMultiSig]);
+  }, [onChainNonce, isMultiSig, networkChainIDKey]);
 
   // Increment nonce after transaction signing
   const incrementNonce = useCallback(
     (transactionHex) => {
       if (isMultiSig) {
+        const currentAccountNonce = getNonceByAccount(currentAccountAddress, networkChainIDKey);
         const localNonce = BigInt(Math.max(currentAccountNonce, Number(accountNonce))) + BigInt(1);
-        setNonceByAccount(currentAccountAddress, localNonce.toString(), transactionHex);
+        setNonceByAccount(currentAccountAddress, localNonce.toString(), transactionHex, networkChainIDKey);
       }
     },
-    [isMultiSig]
+    [isMultiSig, networkChainIDKey]
   );
 
   const resetNonce = () => {
     if (isMultiSig) {
-      resetNonceByAccount(currentAccountAddress, authNonce);
+      resetNonceByAccount(currentAccountAddress, authNonce, networkChainIDKey);
     }
   };
 
