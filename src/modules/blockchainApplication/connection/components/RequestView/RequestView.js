95bfbb34c0ac945aa59aa0394ae451e16ab4c8b4
Oskar Fransson <oskar.leonard@hotmail.com>
2023-11-20 16:53:52 +0100

    Notify DAPP on cancel and closing connection request modal

--- src/modules/blockchainApplication/connection/components/RequestView/RequestView.js
+++ src/modules/blockchainApplication/connection/components/RequestView/RequestView.js
@@ -1,25 +1,43 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import routes from 'src/routes/routes';
 import MultiStep from '@common/components/OldMultiStep';
 import TxSignatureCollector from '@transaction/components/TxSignatureCollector';
 import Dialog from 'src/theme/dialog/dialog';
 import Summary from '@wallet/components/RequestSignSummary';
 import Status from '@wallet/components/RequestSignStatus';
+import { useSession } from '@libs/wcm/hooks/useSession';
+import { USER_REJECT_ERROR } from '@libs/wcm/utils/jsonRPCFormat';
 import RequestSummary from '../RequestSummary';
 import styles from './requestView.css';
 
 const RequestView = ({ history }) => {
-  const [isStepTxSignatureCollector, setIsStepTxSignatureCollector] = useState(false);
+  const { respond } = useSession();
+
+  const [currentStep, setCurrentStep] = useState(0);
   const backToWallet = () => {
     history.push(routes.wallet.path);
   };
 
   const onMultiStepChange = useCallback(({ step: { current } }) => {
-    setIsStepTxSignatureCollector([2, 3].includes(current));
+    setCurrentStep(current);
   }, []);
 
+  const onCloseIcon = async () => {
+    const isStatusView = currentStep === 3;
+    if (!isStatusView) {
+      await respond({ payload: USER_REJECT_ERROR });
+    }
+  };
+
+  const isStepTxSignatureCollector = [2, 3].includes(currentStep);
+
   return (
-    <Dialog hasClose className={styles.dialogWrapper} size={isStepTxSignatureCollector && 'sm'}>
+    <Dialog
+      hasClose
+      onCloseIcon={onCloseIcon}
+      className={styles.dialogWrapper}
+      size={isStepTxSignatureCollector && 'sm'}
+    >
       <MultiStep
         onChange={onMultiStepChange}
         key="RequestView"
