3bf7c9ede00fedd251299f320fe67ff3fe58ab7c
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-11-27 11:09:17 +0100

    Disable e2e tests

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,7 @@ module.exports = {
   default: {
     parallel: 2,
     format: ['html:./e2e/assets/cucumber-report.html'],
-    paths: ['./e2e/features/**/*.feature'],
+    paths: [],
     import: ['./e2e/hooks/hooks.mjs', './e2e/steps/**/*.mjs', './e2e/steps/**/*.js'],
     dryRun: false,
     failFast: false,
