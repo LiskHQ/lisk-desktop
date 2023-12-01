fbb3a23dda82f7a483cd72117792d3c447cf12a1
Eniola Olatunji <eniolam1000752@gmail.com>
2023-12-01 17:55:21 +0100

    fix: resolved failing unit test

--- src/modules/wallet/components/signMultisigStatus/share.test.js
+++ src/modules/wallet/components/signMultisigStatus/share.test.js
@@ -4,14 +4,18 @@ import TxBroadcaster from '@transaction/components/TxBroadcaster';
 import accounts from '@tests/constants/wallets';
 import { mockAuth } from '@auth/__fixtures__';
 import useTxInitiatorAccount from '@transaction/hooks/useTxInitiatorAccount';
+import { useValidators } from '@pos/validator/hooks/queries';
+import { mockValidators } from '@pos/validator/__fixtures__';
 import Status from './status';
 
 jest.mock('@libs/wcm/hooks/useSession', () => ({
   respond: jest.fn(),
 }));
 jest.mock('@transaction/hooks/useTxInitiatorAccount');
+jest.mock('@pos/validator/hooks/queries/useValidators');
 
 describe('Sign Multisignature Tx Status component', () => {
+  const mockFetchNextValidators = jest.fn();
   const props = {
     t: (str, dict) => (dict ? str.replace('{{errorMessage}}', dict.errorMessage) : str),
     sender: { data: accounts.multiSig },
@@ -34,6 +38,9 @@ describe('Sign Multisignature Tx Status component', () => {
         signatures: [],
       },
     },
+    account: accounts.multiSig,
+    bookmarks: { LSK: [] },
+    token: 'LSK',
   };
 
   const signedTransaction = {
@@ -54,6 +61,12 @@ describe('Sign Multisignature Tx Status component', () => {
   useTxInitiatorAccount.mockReturnValue({
     txInitiatorAccount: { ...mockAuth.data, ...mockAuth.meta, keys: { ...mockAuth.data } },
     isLoading: false,
+  });
+  useValidators.mockReturnValue({
+    data: mockValidators,
+    isFetching: false,
+    fetchNextPage: mockFetchNextValidators,
+    hasNextPage: false,
   });
 
   // @todo reinstate by #4506.
@@ -110,16 +123,19 @@ describe('Sign Multisignature Tx Status component', () => {
     };
 
     const wrapper = shallow(<Status {...propsWithError} />);
-    expect(wrapper.find(TxBroadcaster).props()).toEqual({
-      illustration: 'signMultisignature',
-      status: {
-        code: 'BROADCAST_ERROR',
-        message: JSON.stringify({ transaction: {}, error: 'error:test' }),
-      },
-      title: 'Transaction failed',
-      message: 'An error occurred while sending your transaction to the network. Please try again.',
-      className: 'content',
-    });
+    expect(wrapper.find(TxBroadcaster).props()).toEqual(
+      expect.objectContaining({
+        illustration: 'signMultisignature',
+        status: {
+          code: 'BROADCAST_ERROR',
+          message: JSON.stringify({ transaction: {}, error: 'error:test' }),
+        },
+        title: 'Transaction failed',
+        message:
+          'An error occurred while sending your transaction to the network. Please try again.',
+        className: 'content',
+      })
+    );
   });
 
   it('passes correct props to TxBroadcaster when partial signed transaction', () => {
@@ -134,14 +150,16 @@ describe('Sign Multisignature Tx Status component', () => {
     };
 
     const wrapper = shallow(<Status {...propsWithSignedTx} />);
-    expect(wrapper.find(TxBroadcaster).props()).toEqual({
-      illustration: 'signMultisignature',
-      status: { code: 'MULTISIG_SIGNATURE_PARTIAL_SUCCESS' },
-      title: 'The transaction is partially signed',
-      message:
-        'Your signature has been successfully included in the transaction. Kindly copy or download the partially signed transaction and share it with the remaining members to collect all required signatures before broadcasting.',
-      className: 'content',
-    });
+    expect(wrapper.find(TxBroadcaster).props()).toEqual(
+      expect.objectContaining({
+        illustration: 'signMultisignature',
+        status: { code: 'MULTISIG_SIGNATURE_PARTIAL_SUCCESS' },
+        title: 'The transaction is partially signed',
+        message:
+          'Your signature has been successfully included in the transaction. Kindly copy or download the partially signed transaction and share it with the remaining members to collect all required signatures before broadcasting.',
+        className: 'content',
+      })
+    );
   });
 
   // @todo reinstate by #4506.
@@ -164,13 +182,15 @@ describe('Sign Multisignature Tx Status component', () => {
     };
 
     const wrapper = shallow(<Status {...propsWithSignedTx} />);
-    expect(wrapper.find(TxBroadcaster).props()).toEqual({
-      illustration: 'signMultisignature',
-      status: { code: 'MULTISIG_SIGNATURE_SUCCESS' },
-      title: 'The transaction is now fully signed',
-      message:
-        'Now you can send it to the network. You may also copy or download it, if you wish to send the transaction using another device later.',
-      className: 'content',
-    });
+    expect(wrapper.find(TxBroadcaster).props()).toEqual(
+      expect.objectContaining({
+        illustration: 'signMultisignature',
+        status: { code: 'MULTISIG_SIGNATURE_SUCCESS' },
+        title: 'The transaction is now fully signed',
+        message:
+          'Now you can send it to the network. You may also copy or download it, if you wish to send the transaction using another device later.',
+        className: 'content',
+      })
+    );
   });
 });
