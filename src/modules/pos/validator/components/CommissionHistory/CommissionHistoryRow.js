96a3447bcce439405ffe1c1b9a5ff7386a0d00a9
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-12-05 15:25:38 +0100

    Implemented API and updated styling

--- src/modules/pos/validator/components/CommissionHistory/CommissionHistoryRow.js
+++ src/modules/pos/validator/components/CommissionHistory/CommissionHistoryRow.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import DateTimeFromTimestamp from 'src/modules/common/components/timestamp';
 import styles from './styles.css';
 
-const CommissionHistoryRow = ({ timestamp, oldCommission, newCommission, isLatest }) => (
-  <div className={`${grid.row} ${styles.historyDetailsRow}`}>
-    <div className={`${grid['col-md-6']} ${styles.timestamp}`}>
-      <span>{timestamp}</span>
+const CommissionHistoryRow = ({ data, index }) => {
+  const isLatest = index === 0;
+  return (
+    <div className={`${grid.row} ${styles.historyDetailsRow}`}>
+      <div className={`${grid['col-md-6']} ${styles.timestamp}`}>
+        <span>
+          <DateTimeFromTimestamp time={data.block.timestamp} fulltime />
+        </span>
+      </div>
+      <div className={`${grid['col-md-3']} ${styles.commission}`}>
+        <span>{data.data.oldCommission / 100}</span>
+      </div>
+      <div className={`${grid['col-md-3']} ${styles.commission} ${isLatest ? styles.latest : ''}`}>
+        <span>{data.data.newCommission / 100} </span>
+        {isLatest ? <span>(Latest)</span> : null}
+      </div>
     </div>
-    <div className={`${grid['col-md-3']} ${styles.commission}`}>
-      <span>{oldCommission}</span>
-    </div>
-    <div className={`${grid['col-md-3']} ${styles.commission} ${isLatest ? styles.latest : ''}`}>
-      <span>{newCommission} </span>
-      {isLatest ? <span>(Latest)</span> : null}
-    </div>
-  </div>
-);
+  );
+};
 
 export default CommissionHistoryRow;
--- src/modules/pos/validator/components/CommissionHistory/index.js
+++ src/modules/pos/validator/components/CommissionHistory/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import grid from 'flexboxgrid/dist/flexboxgrid.css';
+import { useCurrentAccount } from '@account/hooks';
+import { useTransactionEvents } from '@transaction/hooks/queries';
 import Dialog from '@theme/dialog/dialog';
 import Box from '@theme/box';
 import BoxHeader from '@theme/box/header';
 import BoxContent from '@theme/box/content';
+import { QueryTable } from '@theme/QueryTable';
+import header from './header';
 import CommissionHistoryRow from './CommissionHistoryRow';
 import styles from './styles.css';
 
 const CommissionHistory = () => {
   const { t } = useTranslation();
-  const timestamp = '20 Nov 2023, 10:12:28 AM';
-  const oldCommission = '100';
-  const newCommission = '90';
+  const [currentAccount] = useCurrentAccount();
   return (
     <Dialog hasClose>
       <Box className={`${grid.col} ${styles.historyContainer} history-container`}>
@@ -21,22 +23,20 @@ const CommissionHistory = () => {
           <h6>{t('Below are the commission history for this validator.')}</h6>
         </BoxHeader>
         <BoxContent className={styles.wrapper}>
-          <div
-            className={`${grid.row} ${styles.historyDetailsContainer} history-details-container`}
-          >
-            <div className={`${grid.col} ${grid['col-xs-12']} ${grid['col-md-6']}`}>Date</div>
-            <div className={`${grid.col} ${grid['col-xs-12']} ${grid['col-md-3']}`}>
-              Old commission (%)
-            </div>
-            <div className={`${grid.col} ${grid['col-xs-12']} ${grid['col-md-3']}`}>
-              New commission (%)
-            </div>
-          </div>
-          <CommissionHistoryRow
-            timestamp={timestamp}
-            oldCommission={oldCommission}
-            newCommission={newCommission}
-            isLatest
+          <QueryTable
+            showHeader
+            queryHook={useTransactionEvents}
+            queryConfig={{
+              config: {
+                params: {
+                  senderAddress: currentAccount.metadata.address,
+                  name: 'commissionChange',
+                },
+              },
+            }}
+            header={header(t)}
+            headerClassName={`${grid.row} ${styles.historyDetailsContainer} history-details-container`}
+            row={CommissionHistoryRow}
           />
         </BoxContent>
       </Box>
