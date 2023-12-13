173ba8df9a6ddf627aacbcb2e60fd0c62ddfac79
Eniola Olatunji <eniolam1000752@gmail.com>
2023-12-13 05:57:24 +0100

    fix: resolved faliing tests on useIndexStatus

--- src/modules/network/hooks/queries/useIndexStatus.test.js
+++ src/modules/network/hooks/queries/useIndexStatus.test.js
@@ -1,13 +1,24 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { mockIndexStatus } from '@network/__fixtures__';
 import { queryWrapper as wrapper } from 'src/utils/test/queryWrapper';
+import client from 'src/utils/api/client';
 import { useIndexStatus } from '.';
 
 jest.useRealTimers();
 
 describe('useIndexStatus hook', () => {
+  const mockClient = {
+    ...client,
+    socket: {
+      on: jest.fn(),
+      off: jest.fn(),
+    },
+  };
+
   it('fetching data correctly', async () => {
-    const { result, waitFor } = renderHook(() => useIndexStatus(), { wrapper });
+    const { result, waitFor } = renderHook(() => useIndexStatus({ client: mockClient }), {
+      wrapper,
+    });
     expect(result.current.isLoading).toBeTruthy();
     await waitFor(() => result.current.isFetched);
     expect(result.current.isSuccess).toBeTruthy();
