b0e6c918b320692ca6c1e800c841b4a6c4e07432
Ikemefuna Obioha <ikemoobioha@gmail.com>
2023-12-22 17:26:49 +0100

    Fixed validator list stake button link

--- src/modules/pos/validator/components/ValidatorSummary/ValidatorSummary.js
+++ src/modules/pos/validator/components/ValidatorSummary/ValidatorSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 import Box from 'src/theme/box';
 import BoxHeader from 'src/theme/box/header';
 import BoxContent from 'src/theme/box/content';
@@ -8,7 +9,7 @@ import { truncateAddress } from 'src/modules/wallet/utils/account';
 import DateTimeFromTimestamp from 'src/modules/common/components/timestamp';
 import WalletVisual from 'src/modules/wallet/components/walletVisual';
 import { PrimaryButton } from 'src/theme/buttons';
-import DialogLink from 'src/theme/dialog/link';
+import { addSearchParamsToUrl } from 'src/utils/searchParams';
 import styles from './ValidatorSummary.css';
 import { convertCommissionToPercentage } from '../../utils';
 import { usePosConstants } from '../../hooks/queries';
@@ -16,8 +17,14 @@ import { usePosConstants } from '../../hooks/queries';
 const ValidatorSummary = ({ validator, status, weight }) => {
   const { address, name, rank, commission, nextAllocatedTime } = validator;
   const { t } = useTranslation();
+  const history = useHistory();
   const { data: posConstants } = usePosConstants();
   const roundLength = posConstants?.data?.roundLength || '-';
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    addSearchParamsToUrl(history, { validatorAddress: address, modal: 'editStake' });
+  };
 
   return (
     <Box className={styles.wrapper}>
@@ -32,9 +39,9 @@ const ValidatorSummary = ({ validator, status, weight }) => {
             <p className={styles.validatorAddress}>{truncateAddress(address)}</p>
           </div>
           <div>
-            <DialogLink component="editStake">
-              <PrimaryButton disabled={status.className === 'banned'}>{t('Stake')}</PrimaryButton>
-            </DialogLink>
+            <PrimaryButton disabled={status.className === 'banned'} onClick={handleClick}>
+              {t('Stake')}
+            </PrimaryButton>
           </div>
         </div>
       </BoxHeader>
